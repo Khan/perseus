2e29b69e547b297cb8081666c848d9d571532c60
Ben Alpert <spicyjalapeno@gmail.com>
2013-01-25 14:00:17 -0800

    Add MathJax, not really integrated yet

--- src/core.js
+++ src/core.js
@@ -1,5 +1,25 @@
 (function(undefined) {
 
-window.Perseus = {};
+var Perseus = window.Perseus = {};
+
+Perseus.init = function() {
+    MathJax.Hub.Config({
+        messageStyle: "none",
+        skipStartupTypeset: "none",
+        "HTML-CSS": {
+            availableFonts: ["TeX"],
+            imageFont: null,
+            scale: 100,
+            showMathMenu: false
+        }
+    });
+    MathJax.Hub.Configured();
+
+    $("code").each(function() {
+        var $script = $('<script type="math/tex">').text($(this).text());
+        $(this).replaceWith($script);
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, $script[0]]);
+    });
+};
 
 })();
