ecc6bc17c6e7d1677f159dc0b72584411690890d
Ben Alpert <spicyjalapeno@gmail.com>
2013-06-05 01:48:37 -0700

    Fix widget focus on renderer
    Auditors: alex

--- src/renderer.js
+++ src/renderer.js
@@ -150,7 +150,7 @@ var Renderer = Perseus.Renderer = React.createClass({
             if (widget.focus && widget.focus()) {
                 return true;
             }
-        });
+        }, true);
     },
 
     toJSON: function(skipValidation) {
