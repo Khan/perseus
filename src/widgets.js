da2c86e5dd26b9e9a9f7cf04ebb907f50789983f
Ben Alpert <spicyjalapeno@gmail.com>
2013-07-19 17:24:54 -0700

    Remvoe unused Widget class and remove backbone
    Auditors: emily

--- src/widgets.js
+++ src/widgets.js
@@ -1,36 +1,5 @@
 (function(Perseus) {
 
-var Widget = Perseus.Widget = Backbone.View.extend({
-    // Abstract class, please subclass me!
-
-    render: function() {
-        return $.when(this);
-    },
-
-    reprocess: function() {
-        return $.when(this);
-    },
-
-    // TODO(alpert): Add a cleanup method
-
-    toJSON: function(skipValidation) {
-        // TODO(alpert): Separate out options like 'el' from actual state
-        return this.options;
-    },
-
-    set: function(attrs) {
-    }
-});
-
-
-// Static methods
-_.extend(Widget, {
-    validate: function(state, rubric) {
-        return {type: "invalid"};
-    }
-});
-
-
 var widgetTypes = {};
 
 var Widgets = Perseus.Widgets = {
