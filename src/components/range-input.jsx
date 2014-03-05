858e605b0a7f9b7a862c45bb71fef295e2a01005
Brian Merlob <brianmerlob@khanacademy.org>
2014-03-04 18:33:29 -0800

    Create RangeInput
    Summary: A minor abstraction on top of NumberInput; I'll improve this as I use it more in the other widgets.
    
    Test Plan: Used in number-line
    
    Reviewers: jack
    
    Reviewed By: jack
    
    CC: alpert
    
    Differential Revision: http://phabricator.khanacademy.org/D6901

---
+++ src/components/range-input.jsx
@@ -0,0 +1,45 @@
+/** @jsx React.DOM */
+
+var ValidatedNumberInput = require("../components/validated-number-input.jsx");
+
+/* A minor abstraction on top of NumberInput for ranges
+ *
+ */
+var RangeInput = React.createClass({
+    propTypes: {
+        value: React.PropTypes.array,
+        placeholder: React.PropTypes.array,
+        checkValidity: React.PropTypes.func
+    },
+
+    render: function() {
+        var value = this.props.value;
+        var checkValidity = this.props.checkValidity;
+
+        return <div className="range-input">
+            [
+            {this.transferPropsTo(<ValidatedNumberInput
+                value={value[0]}
+                checkValidity={(val) => checkValidity([val, value[1]])}
+                onChange={this.onChange.bind(this, 0)} />)}
+            ,
+            {this.transferPropsTo(<ValidatedNumberInput
+                value={value[1]}
+                checkValidity={(val) => checkValidity([value[0], val])}
+                onChange={this.onChange.bind(this, 1)} />)}
+            ]
+        </div>;
+    },
+
+    onChange: function(i, newVal) {
+        var value = this.props.value;
+        if (i === 0) {
+            this.props.onChange([newVal, value[1]]);
+        } else {
+            this.props.onChange([value[0], newVal]);
+        }
+    }
+
+});
+
+module.exports = RangeInput;
