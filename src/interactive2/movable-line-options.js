be6365c56d7e2bc865066c7403e11533d57caf07
Jack Toole <jack@khanacademy.org>
2014-04-14 23:53:42 -0700

    Interactive2: MovableLine options -> MovableLineOptions
    Summary:
    Refactors MovableLine to use the same options library style that
    MovablePoint does. This creates a separate file of MovableLineOptions
    that can be passed to the MovableLine constructor, along with nice
    defaults specified in said file.
    
    This also refactors MovableLine to use the new `update()`/`modify()`
    semantics.
    
    Test Plan: http://localhost:9000/test.html#content=%7B%22question%22%3A%7B%22content%22%3A%22%5B%5B%E2%98%83%20interactive-graph%201%5D%5D%22%2C%22images%22%3A%7B%7D%2C%22widgets%22%3A%7B%22interactive-graph%201%22%3A%7B%22type%22%3A%22interactive-graph%22%2C%22graded%22%3Atrue%2C%22options%22%3A%7B%22step%22%3A%5B1%2C1%5D%2C%22backgroundImage%22%3A%7B%22url%22%3Anull%2C%22scale%22%3A1%2C%22bottom%22%3A0%2C%22left%22%3A0%7D%2C%22markings%22%3A%22graph%22%2C%22labels%22%3A%5B%22x%22%2C%22y%22%5D%2C%22showProtractor%22%3Afalse%2C%22showRuler%22%3Afalse%2C%22rulerLabel%22%3A%22%22%2C%22rulerTicks%22%3A10%2C%22range%22%3A%5B%5B-10%2C10%5D%2C%5B-10%2C10%5D%5D%2C%22gridStep%22%3A%5B1%2C1%5D%2C%22snapStep%22%3A%5B0.5%2C0.5%5D%2C%22graph%22%3A%7B%22type%22%3A%22linear%22%7D%2C%22correct%22%3A%7B%22type%22%3A%22linear%22%2C%22coords%22%3A%5B%5B-6%2C0%5D%2C%5B0%2C6%5D%5D%7D%7D%7D%7D%7D%2C%22answerArea%22%3A%7B%22type%22%3A%22multiple%22%2C%22options%22%3A%7B%22content%22%3A%22%22%2C%22images%22%3A%7B%7D%2C%22widgets%22%3A%7B%7D%7D%2C%22calculator%22%3Afalse%7D%2C%22hints%22%3A%5B%5D%7D
    
    Reviewers: justinjaffray
    
    Reviewed By: justinjaffray
    
    Subscribers: alpert
    
    Differential Revision: http://phabricator.khanacademy.org/D8069

---
+++ src/interactive2/movable-line-options.js
@@ -0,0 +1,271 @@
+/**
+ * A library of options to pass to add/draw/remove/constraints
+ */
+
+var knumber = KhanUtil.knumber;
+var kvector = KhanUtil.kvector;
+var kpoint = KhanUtil.kpoint;
+
+/**
+ * Helper functions
+ */
+var getScaledPolarDiff = function(line) {
+    var scaledA = line.graphie.scalePoint(line.coord(0));
+    var scaledZ = line.graphie.scalePoint(line.coord(1));
+    var polarDiff = kvector.polarDegFromCart(
+        kvector.subtract(
+            scaledZ,
+            scaledA
+        )
+    );
+    return polarDiff;
+};
+
+// Given `coord` and `angle`, find the point where a line extended
+// from `coord` in the direction of `angle` would be clipped by the
+// edge of the graphie canvas. Then draw an arrowhead at that point
+// pointing in the direction of `angle`.
+var drawArrowAtClipPoint = function(graph, coord, angle, style) {
+    // Actually put the arrowheads 4px from the edge so they have
+    // a bit of room
+    var xExtent = graph.range[0][1] - graph.range[0][0];
+    var yExtent = graph.range[1][1] - graph.range[1][0];
+
+    // shoot a point off into the distance ...
+    var distance = xExtent + yExtent;
+    // we need to scale the point according to the scale of the axes
+    var angleVec = graph.unscaleVector(
+        kvector.cartFromPolarDeg([1, angle])
+    );
+    var distVec = kvector.scale(
+        kvector.normalize(angleVec),
+        distance
+    );
+    var farCoord = kvector.add(coord, distVec);
+    var scaledAngle = kvector.polarDegFromCart(angleVec)[1];
+    // ... and then bring it back
+    var clipPoint = graph.constrainToBoundsOnAngle(farCoord, 4,
+                                  scaledAngle * Math.PI / 180);
+    clipPoint = graph.scalePoint(clipPoint);
+
+    var arrowHead = graph.raphael.path("M-3 4 C-2.75 2.5 0 0.25 0.75 0C0 -0.25 -2.75 -2.5 -3 -4");
+    arrowHead.rotate(360 - angle, 0.75, 0)
+        .scale(1.4, 1.4, 0.75, 0)
+        .translate(clipPoint[0], clipPoint[1])
+        .attr(style)
+        .attr({ "stroke-linejoin": "round", "stroke-linecap": "round", "stroke-dasharray": "" });
+
+    return arrowHead;
+};
+
+
+/**
+ * MovableLine option functions
+ */
+var add = {
+    draw: function() {
+        this.draw();
+    },
+
+    pointsToFront: function(state) {
+        _.invoke(state.points, "toFront");
+    }
+};
+
+add.standard = [add.draw, add.pointsToFront];
+
+
+var draw = {
+    basic: function(state) {
+        var graphie = this.graphie;
+        if (!this.state.visibleShape) {
+            this.state.visibleShape = graphie.path(
+                [[0, 0]],
+                this.normalStyle()
+            );
+            this.state.visibleShape.attr({
+                path: KhanUtil.unscaledSvgPath([[0, 0], [1, 0]])
+            });
+        }
+
+
+        // Clip the line 5px from the edge of the graphie to allow for
+        // arrowheads
+        if (state.extendLine || state.extendRay) {
+            this.state.visibleShape.attr({
+                "clip-rect": "5 5 " + (graphie.dimensions[0] - 10) + " " +
+                    (graphie.dimensions[1] - 10)
+            });
+        }
+
+        var scaledA = graphie.scalePoint(this.coord(0));
+        var scaledZ = graphie.scalePoint(this.coord(1));
+        var polarDiff = getScaledPolarDiff(this);
+        var lineLength = polarDiff[0];
+        var angle = polarDiff[1];
+
+        var elements = [this.state.visibleShape];
+        if (this.mouseTarget()) {
+            elements.push(this.mouseTarget());
+        }
+        _.each(elements, function(element) {
+            element.translate(scaledA[0] - element.attr("translation").x,
+                    scaledA[1] - element.attr("translation").y);
+            element.rotate(angle, scaledA[0], scaledA[1]);
+            if (state.extendLine) {
+                element.translate(-0.5, 0);
+                lineLength = graphie.dimensions[0] + graphie.dimensions[1];
+                lineLength = 2 * lineLength;
+            } else if (state.extendRay) {
+                lineLength = graphie.dimensions[0] + graphie.dimensions[1];
+            }
+            element.scale(lineLength, 1, scaledA[0], scaledA[1]);
+        });
+    },
+
+    arrows: function(state) {
+        if (this._arrows != null) {
+            _.invoke(this._arrows, "remove");
+        }
+        this._arrows = [];
+
+        var polarDiff = getScaledPolarDiff(this);
+        var angle = polarDiff[1];
+
+        if (state.extendLine) {
+            this._arrows.push(drawArrowAtClipPoint(
+                this.graphie, this.state.points[0].coord(), 360 - angle,
+                this.normalStyle()));
+            this._arrows.push(drawArrowAtClipPoint(
+                this.graphie, this.state.points[1].coord(),
+                (540 - angle) % 360,
+                this.normalStyle()));
+        } else if (state.extendRay) {
+            this._arrows.push(drawArrowAtClipPoint(
+                this.graphie, this.state.points[0].coord(), 360 - angle,
+                this.normalStyle()));
+        }
+    },
+
+    highlight: function(state, prevState) {
+        // TODO(jack): Figure out a way to highlight the points attached to
+        // the line. Maybe this means an additional isHovering: []
+        // function to state of movable/movablepoint to define [additional?]
+        // times it should be highlighted
+        if (state.isHovering && !prevState.isHovering) {
+            state.visibleShape.animate(
+                state.highlightStyle,
+                50
+            );
+        } else if (!state.isHovering && prevState.isHovering) {
+            state.visibleShape.animate(
+                state.normalStyle,
+                50
+            );
+        }
+    }
+};
+
+draw.standard = [draw.basic, draw.arrows, draw.highlight];
+
+
+var remove = {
+    basic: function() {
+        if (this.state.visibleShape) {
+            this.state.visibleShape.remove();
+        }
+    },
+    arrows: function() {
+        if (this._arrows != null) {
+            _.invoke(this._arrows, "remove");
+        }
+    }
+};
+
+remove.standard = [remove.basic, remove.arrows];
+
+
+var constraints = {
+    fixed: function() {
+        return function() { return false; };
+    },
+
+    snap: function(snap) {
+        return function(coord, prevCoord) {
+            if (snap === null) {
+                return true;
+            }
+            var delta = kvector.subtract(coord, prevCoord);
+            snap = snap || this.graphie.snap;
+            delta = kpoint.roundTo(delta, snap);
+            return kvector.add(prevCoord, delta);
+        };
+    },
+
+    bound: function(range, snap, paddingPx) {
+        if (paddingPx === undefined) {
+            if (range === undefined) {
+                paddingPx = 10;
+            } else {
+                paddingPx = 0;
+            }
+        }
+        return function(coord, prevCoord) {
+            var graphie = this.graphie;
+            var delta = kvector.subtract(coord, prevCoord);
+            var range = range || graphie.range;
+            // A null snap means no snap; an undefined snap means
+            // default to graphie's
+            if (snap === undefined) {
+                snap = graphie.snap;
+            }
+
+            // Calculate the bounds for both points
+            var absoluteLower = graphie.unscalePoint([
+                paddingPx,
+                graphie.ypixels - paddingPx
+            ]);
+            var absoluteUpper = graphie.unscalePoint([
+                graphie.xpixels - paddingPx,
+                paddingPx
+            ]);
+            if (snap) {
+                absoluteLower = kpoint.ceilTo(absoluteLower, snap);
+                absoluteUpper = kpoint.floorTo(absoluteUpper, snap);
+            }
+
+            // Calculate the bounds for the delta.
+            var deltaBounds = _.map(this.coords(), function(coord, i) {
+                var max = kvector.subtract(absoluteUpper, coord);
+                var min = kvector.subtract(absoluteLower, coord);
+                return [min, max];
+            });
+
+            // bound the delta by the calculated bounds
+            var boundedDelta = _.reduce(deltaBounds,
+                    function(delta, bound) {
+                var lower = bound[0];
+                var upper = bound[1];
+                var deltaX = Math.max(lower[0], Math.min(upper[0], delta[0]));
+                var deltaY = Math.max(lower[1], Math.min(upper[1], delta[1]));
+                return [deltaX, deltaY];
+            }, delta);
+
+            return kvector.add(prevCoord, boundedDelta);
+        };
+    }
+};
+
+constraints.standard = null;
+
+
+module.exports = {
+    add: add,
+    draw: draw,
+    remove: remove,
+
+    onMoveStart: {standard: null},
+    constraints: constraints,
+    onMove: {standard: null},
+    onMoveEnd: {standard: null},
+};
--- src/interactive2/movable-line.js
+++ src/interactive2/movable-line.js
@@ -3,7 +3,9 @@
  */
 
 var Movable = require("./movable.js");
+var MovableLineOptions = require("./movable-line-options.js");
 var InteractiveUtil = require("./interactive-util.js");
+var objective_ = require("./objective_.js");
 var assert = InteractiveUtil.assert;
 var normalizeOptions = InteractiveUtil.normalizeOptions;
 
@@ -36,39 +38,6 @@ var DEFAULT_PROPERTIES = {
     highlightStyle: null,  // likewise
     extendLine: false,
     extendRay: false
-};
-
-// Given `coord` and `angle`, find the point where a line extended
-// from `coord` in the direction of `angle` would be clipped by the
-// edge of the graphie canvas. Then draw an arrowhead at that point
-// pointing in the direction of `angle`.
-var drawArrowAtClipPoint = function(graph, coord, angle, style) {
-    // Actually put the arrowheads 4px from the edge so they have
-    // a bit of room
-    var xExtent = graph.range[0][1] - graph.range[0][0];
-    var yExtent = graph.range[1][1] - graph.range[1][0];
-
-    // shoot a point off into the distance ...
-    var distance = xExtent + yExtent;
-    // we need to scale the point according to the scale of the axes
-    var xDist = distance * Math.cos(angle * Math.PI / 180) *
-                    xExtent / yExtent;
-    var yDist = distance * Math.sin(angle * Math.PI / 180);
-    var farCoord = [coord[0] + xDist, coord[1] + yDist];
-    var scaledAngle = KhanUtil.findAngle(farCoord, coord);
-    // ... and then bring it back
-    var clipPoint = graph.constrainToBoundsOnAngle(farCoord, 4,
-                                  scaledAngle * Math.PI / 180);
-    clipPoint = graph.scalePoint(clipPoint);
-
-    var arrowHead = graph.raphael.path("M-3 4 C-2.75 2.5 0 0.25 0.75 0C0 -0.25 -2.75 -2.5 -3 -4");
-    arrowHead.rotate(360 - angle, 0.75, 0)
-        .scale(1.4, 1.4, 0.75, 0)
-        .translate(clipPoint[0], clipPoint[1])
-        .attr(style)
-        .attr({ "stroke-linejoin": "round", "stroke-linecap": "round", "stroke-dasharray": "" });
-
-    return arrowHead;
 };
 
 var MovableLine = function(graphie, options) {
@@ -106,6 +75,7 @@ var MovableLine = function(graphie, options) {
     this.modify(options);
 };
 
+_.extend(MovableLine, MovableLineOptions);
 InteractiveUtil.createGettersFor(MovableLine, DEFAULT_PROPERTIES);
 InteractiveUtil.addMovableHelperMethodsTo(MovableLine);
 
@@ -117,14 +87,47 @@ _.extend(MovableLine.prototype, {
         }, this.state);
     },
 
+    _createDefaultState: function() {
+        return _.extend({
+            id: this.state.id,
+        }, normalizeOptions(
+            FUNCTION_ARRAY_OPTIONS,
+            // Defaults are copied from MovableLineOptions.*.standard
+            // These defaults are set here instead of DEFAULT_PROPERTIES
+            // because they:
+            //    - are objects, not primitives (and need a deeper copy)
+            //    - they don't need getters created for them
+            // TODO(jack): Consider "default" once we es3ify perseus
+            objective_.pluck(MovableLineOptions, "standard")
+        ), DEFAULT_PROPERTIES);
+    },
+
+    /**
+     * Adjusts constructor parameters without changing previous settings
+     * for any option not specified
+     */
+    update: function(options) {
+        this.remove();  // Must be called here to modify this.state
+                        // *before* we pass this.state into this.modify
+                        // Otherwise, we'd end up re-adding the removed
+                        // raphael elements :(.
+        this.modify(_.extend({}, this.state, options));
+    },
+
+    /**
+     * Resets the object to its state as if it were constructed with
+     * `options` originally. The only state maintained is `state.id`
+     */
     modify: function(options) {
         var self = this;
         var graphie = this.graphie;
 
         this.remove();
 
-        var state = _.extend(self.state, DEFAULT_PROPERTIES,
-            normalizeOptions(FUNCTION_ARRAY_OPTIONS, options));
+        var state = self.state = _.extend(
+            self._createDefaultState(),
+            normalizeOptions(FUNCTION_ARRAY_OPTIONS, options)
+        );
 
         // Default things inside the state.normalStyle object, because
         // _.extend is not deep.
@@ -275,205 +278,6 @@ _.extend(MovableLine.prototype, {
 
     mouseTarget: function() {
         return this.movable.mouseTarget();
-    }
-});
-
-var getScaledPolarDiff = function(line) {
-    var scaledA = line.graphie.scalePoint(line.coord(0));
-    var scaledZ = line.graphie.scalePoint(line.coord(1));
-    var polarDiff = kvector.polarDegFromCart(
-        kvector.subtract(
-            scaledZ,
-            scaledA
-        )
-    );
-    return polarDiff;
-};
-
-_.extend(MovableLine, {
-    add: {
-        draw: function() {
-            this.draw();
-        },
-
-        pointsToFront: function(state) {
-            _.invoke(state.points, "toFront");
-        }
-    },
-
-    draw: {
-        basic: function(state) {
-            var graphie = this.graphie;
-            if (!this.state.visibleShape) {
-                this.state.visibleShape = graphie.path(
-                    [[0, 0]],
-                    this.normalStyle()
-                );
-                this.state.visibleShape.attr({
-                    path: KhanUtil.unscaledSvgPath([[0, 0], [1, 0]])
-                });
-            }
-
-
-            // Clip the line 5px from the edge of the graphie to allow for
-            // arrowheads
-            if (state.extendLine || state.extendRay) {
-                this.state.visibleShape.attr({
-                    "clip-rect": "5 5 " + (graphie.dimensions[0] - 10) + " " +
-                        (graphie.dimensions[1] - 10)
-                });
-            }
-
-            var scaledA = graphie.scalePoint(this.coord(0));
-            var scaledZ = graphie.scalePoint(this.coord(1));
-            var polarDiff = getScaledPolarDiff(this);
-            var lineLength = polarDiff[0];
-            var angle = polarDiff[1];
-
-            var elements = [this.state.visibleShape];
-            if (this.mouseTarget()) {
-                elements.push(this.mouseTarget());
-            }
-            _.each(elements, function(element) {
-                element.translate(scaledA[0] - element.attr("translation").x,
-                        scaledA[1] - element.attr("translation").y);
-                element.rotate(angle, scaledA[0], scaledA[1]);
-                if (state.extendLine) {
-                    element.translate(-0.5, 0);
-                    lineLength = graphie.dimensions[0] + graphie.dimensions[1];
-                    lineLength = 2 * lineLength;
-                } else if (state.extendRay) {
-                    lineLength = graphie.dimensions[0] + graphie.dimensions[1];
-                }
-                element.scale(lineLength, 1, scaledA[0], scaledA[1]);
-            });
-        },
-
-        arrows: function(state) {
-            if (this._arrows != null) {
-                _.invoke(this._arrows, "remove");
-            }
-            this._arrows = [];
-
-            var polarDiff = getScaledPolarDiff(this);
-            var angle = polarDiff[1];
-
-            if (state.extendLine) {
-                this._arrows.push(drawArrowAtClipPoint(
-                    this.graphie, this.state.points[0].coord(), 360 - angle,
-                    this.normalStyle()));
-                this._arrows.push(drawArrowAtClipPoint(
-                    this.graphie, this.state.points[1].coord(),
-                    (540 - angle) % 360,
-                    this.normalStyle()));
-            } else if (state.extendRay) {
-                this._arrows.push(drawArrowAtClipPoint(
-                    this.graphie, this.state.points[0].coord(), 360 - angle,
-                    this.normalStyle()));
-            }
-        },
-
-        highlight: function(state, prevState) {
-            // TODO(jack): Figure out a way to highlight the points attached to
-            // the line. Maybe this means an additional isHovering: []
-            // function to state of movable/movablepoint to define [additional?]
-            // times it should be highlighted
-            if (state.isHovering && !prevState.isHovering) {
-                state.visibleShape.animate(
-                    state.highlightStyle,
-                    50
-                );
-            } else if (!state.isHovering && prevState.isHovering) {
-                state.visibleShape.animate(
-                    state.normalStyle,
-                    50
-                );
-            }
-        }
-    },
-
-    remove: {
-        basic: function() {
-            if (this.state.visibleShape) {
-                this.state.visibleShape.remove();
-            }
-        },
-        arrows: function() {
-            if (this._arrows != null) {
-                _.invoke(this._arrows, "remove");
-            }
-        }
-    },
-
-    constraints: {
-        fixed: function() {
-            return function() { return false; };
-        },
-
-        snap: function(snap) {
-            return function(coord, prevCoord) {
-                if (snap === null) {
-                    return true;
-                }
-                var delta = kvector.subtract(coord, prevCoord);
-                snap = snap || this.graphie.snap;
-                delta = kpoint.roundTo(delta, snap);
-                return kvector.add(prevCoord, delta);
-            };
-        },
-
-        bound: function(range, snap, paddingPx) {
-            if (paddingPx === undefined) {
-                if (range === undefined) {
-                    paddingPx = 10;
-                } else {
-                    paddingPx = 0;
-                }
-            }
-            return function(coord, prevCoord) {
-                var graphie = this.graphie;
-                var delta = kvector.subtract(coord, prevCoord);
-                var range = range || graphie.range;
-                // A null snap means no snap; an undefined snap means
-                // default to graphie's
-                if (snap === undefined) {
-                    snap = graphie.snap;
-                }
-
-                // Calculate the bounds for both points
-                var absoluteLower = graphie.unscalePoint([
-                    paddingPx,
-                    graphie.ypixels - paddingPx
-                ]);
-                var absoluteUpper = graphie.unscalePoint([
-                    graphie.xpixels - paddingPx,
-                    paddingPx
-                ]);
-                if (snap) {
-                    absoluteLower = kpoint.ceilTo(absoluteLower, snap);
-                    absoluteUpper = kpoint.floorTo(absoluteUpper, snap);
-                }
-
-                // Calculate the bounds for the delta.
-                var deltaBounds = _.map(this.coords(), function(coord, i) {
-                    var max = kvector.subtract(absoluteUpper, coord);
-                    var min = kvector.subtract(absoluteLower, coord);
-                    return [min, max];
-                });
-
-                // bound the delta by the calculated bounds
-                var boundedDelta = _.reduce(deltaBounds,
-                        function(delta, bound) {
-                    var lower = bound[0];
-                    var upper = bound[1];
-                    var deltaX = Math.max(lower[0], Math.min(upper[0], delta[0]));
-                    var deltaY = Math.max(lower[1], Math.min(upper[1], delta[1]));
-                    return [deltaX, deltaY];
-                }, delta);
-
-                return kvector.add(prevCoord, boundedDelta);
-            };
-        }
     }
 });
 
