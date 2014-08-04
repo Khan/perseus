52493655a8778de1eb5b66b4f9225120f01d8916
Jack Toole <jack@khanacademy.org>
2014-08-04 16:52:52 -0700

    Move input-number test to new test infrastructure
    Test Plan:
    `make test`
    
    Auditors: michelle

---
+++ src/widgets/__tests__/input-number-test.jsx
@@ -0,0 +1,28 @@
+/** @jsx React.DOM */
+
+/**
+ * Disclaimer: Definitely not thorough enough
+ */
+
+var assert = require("assert");
+var React = require('react');
+var InputNumber = require("../input-number.jsx");
+
+var TestUtils = React.addons.TestUtils;
+
+var transform = InputNumber.transform;
+
+describe("input-number", function() {
+    it("'s transform should remove the `value` field", function() {
+        var editorProps = {
+            value: 5,
+            simplify: "required",
+            size: "normal",
+            inexact: false,
+            maxError: 0.1,
+            answerType: "number"
+        };
+        var widgetProps = transform(editorProps);
+        assert.strictEqual(_.has(widgetProps, "value"), false);
+    });
+});
