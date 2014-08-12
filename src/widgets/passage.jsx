71055ab09f099536c3ab8f20b45c3368941ec4fc
Michelle Todd <michelle@khanacademy.org>
2014-08-12 16:14:23 -0400

    Add title and footnotes to the passage widget
    Summary:
    The title field allows question writers to specify an (optional) title for each passage, which is used when there are 2 or more passages in the question (some SAT passage questions ask the reader to compare and contrast multiple passages, and they are usually titled "Passage 1", "Passage 2", ...). The title is added above the passage in bold with the same font and indentation as used in the passage itself.
    
    The footnote section allows the question writer to add superscripts in the passage text (using KaTeX, e.g. `${}^1$` or `${}^*$`) and then add the corresponding footnote below. The footnotes are styled with the same font and indentation as used in the passage, with a smaller font-size and no line numbers.
    
    Test Plan: Add an SAT passage widget and play with the footnotes and title
    
    Reviewers: aria
    
    Reviewed By: aria
    
    Subscribers: emily, alpert, marcos, eater
    
    Differential Revision: http://phabricator.khanacademy.org/D11874

--- src/widgets/passage.jsx
+++ src/widgets/passage.jsx
@@ -4,6 +4,7 @@ var Changeable   = require("../mixins/changeable.jsx");
 var JsonifyProps = require("../mixins/jsonify-props.jsx");
 
 var Editor = require("../editor.jsx");
+var InfoTip = require("react-components/info-tip.jsx");
 var PropCheckBox  = require("../components/prop-check-box.jsx");
 var Renderer = require("../renderer.jsx");
 var TextListEditor = require("../components/text-list-editor.jsx");
@@ -17,13 +18,17 @@ var Passage = React.createClass({
     mixins: [JsonifyProps, Changeable],
 
     propTypes: {
+        passageTitle: React.PropTypes.string,
         passageText: React.PropTypes.string,
+        footnotes: React.PropTypes.string,
         showLineNumbers: React.PropTypes.bool
     },
 
     getDefaultProps: function() {
         return {
+            passageTitle: "",
             passageText: "",
+            footnotes: "",
             showLineNumbers: true
         };
     },
@@ -49,12 +54,19 @@ var Passage = React.createClass({
                     (lineN % 5 === 0 ? lineN : "\n");
             });
         }
-
         return <div className="perseus-widget-passage">
+            <div className="passage-title">
+                {Renderer({ content: this.props.passageTitle })}
+            </div>
             {lineNumbers && <div className="line-numbers">
                 {lineNumbers}
             </div>}
-            {this._renderContent()}
+            <div className="passage-text">
+                {this._renderContent()}
+            </div>
+            <div className="footnotes">
+                {Renderer({ content: this.props.footnotes })}
+            </div>
         </div>;
     },
 
@@ -145,19 +157,23 @@ var PassageEditor = React.createClass({
     mixins: [JsonifyProps, Changeable],
 
     propTypes: {
+        passageTitle: React.PropTypes.string,
         passageText: React.PropTypes.string,
+        footnotes: React.PropTypes.string,
         showLineNumbers: React.PropTypes.bool
     },
 
     getDefaultProps: function() {
         return {
+            passageTitle: "",
             passageText: "",
+            footnotes: "",
             showLineNumbers: true
         };
     },
 
     render: function() {
-        var editor = Editor({
+        var passageEditor = Editor({
             ref: "passage-editor",
             content: this.props.passageText,
             widgetEnabled: false,
@@ -166,16 +182,50 @@ var PassageEditor = React.createClass({
                 this.change({ passageText: newProps.content });
             }
         });
+        var footnotesEditor = Editor({
+            ref: "passage-footnotes-editor",
+            content: this.props.footnotes,
+            widgetEnabled: false,
+            placeholder: "Type footnotes here...",
+            onChange: (newProps) => {
+                this.change({ footnotes: newProps.content });
+            }
+        });
         return <div className="perseus-widget-passage-editor">
-
-            Passage Text:
-            {editor}
             <div className="perseus-widget-row">
                 <PropCheckBox
                     label="Show line numbers"
                     labelAlignment="right"
                     showLineNumbers={this.props.showLineNumbers}
                     onChange={this.props.onChange} />
+            </div>
+            <div>
+                Passage title:
+                <InfoTip>
+                    <p>An optional title that will appear directly above the
+                    passage in the same font style. (E.g. Passage 1)</p>
+                </InfoTip>
+                <div>
+                    <input
+                        type="text"
+                        defaultValue={this.props.passageTitle}
+                        onChange={(e) => {
+                            this.change({ passageTitle: e.target.value });
+                        }} />
+                </div>
+            </div>
+            <div>
+                Passage Text:
+                {passageEditor}
+            </div>
+            <div>
+                Footnotes:
+                <InfoTip>
+                    <p>To add a footnote, use a superscript number ${}^1$ or
+                    asterisk ${}^*$ in the passage text and reference it in
+                    the footnote section.</p>
+                </InfoTip>
+                {footnotesEditor}
             </div>
         </div>;
     },
@@ -187,6 +237,7 @@ module.exports = {
     widget: Passage,
     editor: PassageEditor,
     transform: (editorProps) => {
-        return _.pick(editorProps, "passageText", "showLineNumbers");
+        return _.pick(editorProps, "passageTitle", "passageText", "footnotes",
+            "showLineNumbers");
     }
 };
