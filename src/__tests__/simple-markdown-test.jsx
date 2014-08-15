c721d726e02cf4de5288e0a725ff06f02da4307d
Jack Toole <jack@toole1.com>
2014-08-15 12:52:29 -0700

    Add alternative, extensible markdown parsing library
    Summary:
    This will let us
     * only support bold/italic/underline/paragraphs in the passage
       widget, avoiding awkward cases which don't work such as images
       or headings (since these break line numbers and don't make
       sense in a passage)
     * easily add support for passage ref parsing in a non-hacky way
     * eventually replace our smartypants markdown hacks in the renderer
    
    The design goal of simple markdown is simple:
    Provide an easily extensible markdown parser library that we can
    customize to our needs without modifying the library itself.
    
    Test Plan: `make test`
    
    Reviewers: joel, alex
    
    Reviewed By: joel, alex
    
    Subscribers: alpert, michelle
    
    Differential Revision: http://phabricator.khanacademy.org/D12256

---
+++ src/__tests__/simple-markdown-test.jsx
@@ -0,0 +1,244 @@
+var assert = require("assert");
+var _ = require("underscore");
+
+var SimpleMarkdown = require("../simple-markdown.jsx");
+var {
+    defaultParse,
+    defaultOutput
+} = SimpleMarkdown;
+
+var validateParse = (parsed, expected) => {
+    if (!_.isEqual(parsed, expected)) {
+        var parsedStr = JSON.stringify(parsed, null, 4);
+        var expectedStr = JSON.stringify(expected, null, 4);
+        console.error("Parsed:", parsedStr);
+        console.error("Expected:", expectedStr);
+        assert.fail(
+            parsedStr,
+            expectedStr,
+            "parsed did not match expected",
+            "<>"
+        );
+    }
+};
+
+describe("simple markdown", () => {
+    describe("parser", () => {
+        it("should parse a plain string", () => {
+            var parsed = defaultParse("hi there");
+            validateParse(parsed, [{
+                type: "text",
+                content: "hi there"
+            }]);
+        });
+
+        it("should parse bold", () => {
+            var parsed = defaultParse("**hi**");
+            validateParse(parsed, [{
+                type: "strong",
+                content: [{
+                    type: "text",
+                    content: "hi"
+                }]
+            }]);
+        });
+
+        it("should parse italics", () => {
+            var parsed = defaultParse("*hi*");
+            validateParse(parsed, [{
+                type: "em",
+                content: [{
+                    type: "text",
+                    content: "hi"
+                }]
+            }]);
+        });
+
+        it("should parse underlines", () => {
+            var parsed = defaultParse("__hi__");
+            validateParse(parsed, [{
+                type: "u",
+                content: [{
+                    type: "text",
+                    content: "hi"
+                }]
+            }]);
+        });
+
+        it("should parse nested bold/italics", () => {
+            var parsed = defaultParse("***hi***");
+            validateParse(parsed, [{
+                type: "strong",
+                content: [{
+                    type: "em",
+                    content: [{
+                        type: "text",
+                        content: "hi"
+                    }]
+                }]
+            }]);
+        });
+
+        it("should parse nested bold/italics/underline", () => {
+            var parsed1 = defaultParse("***__hi__***");
+            validateParse(parsed1, [{
+                type: "strong",
+                content: [{
+                    type: "em",
+                    content: [{
+                        type: "u",
+                        content: [{
+                            type: "text",
+                            content: "hi"
+                        }]
+                    }]
+                }]
+            }]);
+
+            var parsed2 = defaultParse("*__**hi**__*");
+            validateParse(parsed2, [{
+                type: "em",
+                content: [{
+                    type: "u",
+                    content: [{
+                        type: "strong",
+                        content: [{
+                            type: "text",
+                            content: "hi"
+                        }]
+                    }]
+                }]
+            }]);
+        });
+
+        it("should parse multiple bold/italics/underlines", () => {
+            var parsed = defaultParse(
+                "*some* of this __sentence__ is **bold**"
+            );
+            validateParse(parsed, [
+                {
+                    type: "em",
+                    content: [{
+                        type: "text",
+                        content: "some"
+                    }]
+                },
+                {
+                    type: "text",
+                    content: " of this "
+                },
+                {
+                    type: "u",
+                    content: [{
+                        type: "text",
+                        content: "sentence"
+                    }]
+                },
+                {
+                    type: "text",
+                    content: " is "
+                },
+                {
+                    type: "strong",
+                    content: [{
+                        type: "text",
+                        content: "bold"
+                    }]
+                }
+            ]);
+        });
+
+        it("should parse a single top-level paragraph", () => {
+            var parsed = defaultParse("hi\n\n");
+            validateParse(parsed, [{
+                type: "paragraph",
+                content: [{
+                    type: "text",
+                    content: "hi"
+                }]
+            }]);
+        });
+
+        it("should parse multiple top-level paragraphs", () => {
+            var parsed = defaultParse("hi\n\nbye\n\nthere\n\n");
+            validateParse(parsed, [
+                {
+                    type: "paragraph",
+                    content: [{
+                        type: "text",
+                        content: "hi"
+                    }]
+                },
+                {
+                    type: "paragraph",
+                    content: [{
+                        type: "text",
+                        content: "bye"
+                    }]
+                },
+                {
+                    type: "paragraph",
+                    content: [{
+                        type: "text",
+                        content: "there"
+                    }]
+                },
+            ]);
+        });
+
+        it("should not parse single newlines as paragraphs", () => {
+            var parsed = defaultParse("hi\nbye\nthere\n");
+            validateParse(parsed, [
+                {
+                    type: "text",
+                    content: "hi"
+                },
+                {
+                    type: "newline",
+                },
+                {
+                    type: "text",
+                    content: "bye"
+                },
+                {
+                    type: "newline",
+                },
+                {
+                    type: "text",
+                    content: "there"
+                },
+                {
+                    type: "newline",
+                },
+            ]);
+        });
+
+        it("should not parse a single newline as a new paragraph", () => {
+            var parsed = defaultParse("hi\nbye\nthere\n\n");
+            validateParse(parsed, [{
+                type: "paragraph",
+                content: [
+                    {
+                        type: "text",
+                        content: "hi"
+                    },
+                    {
+                        type: "newline",
+                    },
+                    {
+                        type: "text",
+                        content: "bye"
+                    },
+                    {
+                        type: "newline",
+                    },
+                    {
+                        type: "text",
+                        content: "there"
+                    },
+                ]
+            }]);
+        });
+
+    });
+});
---
+++ src/simple-markdown.jsx
@@ -0,0 +1,144 @@
+/** @jsx React.DOM */
+var _ = require("underscore");
+
+/**
+ * Creates a parser for a given set of rules, with the precedence
+ * specified as a list of rules.
+ *
+ * @rules: an object containing rule type -> {regex, parse} objects
+ * @ruleList: an array of rule types, specifying the precedence rules
+ *   are evaluated in (earlier in the array is higher precendence)
+ *
+ * @returns The resulting parse function, with the following parameters:
+ *   @source: the input source string to be parsed
+ *   @state: an optional object to be threaded through parse
+ *     calls. Allows clients to add stateful operations to
+ *     parsing, such as keeping track of how many levels deep
+ *     some nesting is. For an example use-case, see passage-ref
+ *     parsing in src/widgets/passage/passage-markdown.jsx
+ */
+var parserFor = (rules, ruleList) => {
+    var nestedParse = (source, state) => {
+        var result = [];
+        state = state || {};
+        while (source) {
+            var i = 0;
+            while (i < ruleList.length) {
+                var ruleType = ruleList[i];
+                var rule = rules[ruleType];
+                var capture = rule.regex.exec(source);
+                if (capture) {
+                    source = source.substring(capture[0].length);
+                    var parsed = _.extend(
+                        {type: ruleType},
+                        rule.parse(capture, nestedParse, state)
+                    );
+                    result.push(parsed);
+                    break;
+                }
+                i++;
+            }
+            if (i === rules.length) {
+                throw new Error(
+                    "could not find rule to match content: " + source
+                );
+            }
+        }
+        return result;
+    };
+    return nestedParse;
+};
+
+var outputFor = (outputFunc) => {
+    var nestedOutput = (ast) => {
+        if (_.isArray(ast)) {
+            return _.map(ast, nestedOutput);
+        } else {
+            return outputFunc(ast, nestedOutput);
+        }
+    };
+    return nestedOutput;
+};
+
+var parseCapture = (capture, parse, state) => {
+    return {
+        content: parse(capture[1], state)
+    };
+};
+var ignoreCapture = () => ({});
+
+var defaultRules = {
+    paragraph: {
+        regex: /^((?:[^\n]|\n[^\n])+)\n\n+/,
+        parse: parseCapture,
+        output: (node, output) => {
+            return <div className="paragraph">{output(node.content)}</div>;
+        }
+    },
+    strong: {
+        regex: /^\*\*([\s\S]+?)\*\*(?!\*)/,
+        parse: parseCapture,
+        output: (node, output) => {
+            return <strong>{output(node.content)}</strong>;
+        }
+    },
+    u: {
+        regex: /^__([\s\S]+?)__(?!_)/,
+        parse: parseCapture,
+        output: (node, output) => {
+            return <u>{output(node.content)}</u>;
+        }
+    },
+    em: {
+        regex: /^\b_((?:__|[\s\S])+?)_\b|^\*((?:\*\*|[\s\S])+?)\*(?!\*)/,
+        parse: (capture, parse, state) => {
+            return {
+                content: parse(capture[2] || capture[1], state)
+            };
+        },
+        output: (node, output) => {
+            return <em>{output(node.content)}</em>;
+        }
+    },
+    newline: {
+        regex: /^\n+/,
+        parse: ignoreCapture,
+        output: (node, output) => " "
+    },
+    text: {
+        // This is finicky since it relies on not matching _ and *
+        // If people add other rules like {{refs}}, this will need
+        // to be changed/replaced.
+        regex: /^[\s\S]+?(?=[\\<!\[_*`\n]| {2,}\n|$)/,
+        parse: (capture, parse, state) => {
+            return {
+                content: capture[0]
+            };
+        },
+        output: (node, output) => {
+            return node.content;
+        }
+    }
+};
+
+var defaultPriorities = Object.keys(defaultRules);
+
+var ruleOutput = (rules) => {
+    var nestedRuleOutput = (ast, outputFunc) => {
+        return rules[ast.type].output(ast, outputFunc);
+    };
+    return nestedRuleOutput;
+};
+
+var defaultParse = parserFor(defaultRules, defaultPriorities);
+var defaultOutput = outputFor(ruleOutput(defaultRules));
+
+module.exports = {
+    parserFor: parserFor,
+    outputFor: outputFor,
+    defaultRules: defaultRules,
+    defaultPriorities: defaultPriorities,
+    ruleOutput: ruleOutput,
+    defaultParse: defaultParse,
+    defaultOutput: defaultOutput
+};
