29a7b5ae436e1ef4d1af1588d35e2c2d70a030f4
Aria Buckles <aria@khanacademy.org>
2014-10-28 17:25:28 -0700

    Why number-lines never draw the last tick mark
    Summary:
    Well, now they do draw the last tick mark. They didn't because
    they looped through `numDivisions` times, but if we want
    `numDivisions` units on the number line, we need `numDivisions + 1`
    ticks.
    
    Test Plan:
    Reload my particle with a number line in it; see that the last
    tick is now drawn and labeled.
    
    Auditors: michelle

--- src/widgets/number-line.jsx
+++ src/widgets/number-line.jsx
@@ -111,7 +111,7 @@ TickMarks = Graphie.createSimpleClass((graphie, props) => {
     var base;
     if (props.labelStyle === "non-reduced") {
         var fractions = [leftLabel, rightLabel];
-        for (var i = 0; i < props.numDivisions; i++) {
+        for (var i = 0; i <= props.numDivisions; i++) {
             var x = range[0] + i * props.tickStep;
             fractions.push(x);
         }
@@ -123,7 +123,7 @@ TickMarks = Graphie.createSimpleClass((graphie, props) => {
     }
 
     // Draw and save the tick marks and tick labels
-    for (var i = 0; i < props.numDivisions; i++) {
+    for (var i = 0; i <= props.numDivisions; i++) {
         var x = range[0] + i * props.tickStep;
         results.push(graphie.line([x, -0.2], [x, 0.2]));
 
