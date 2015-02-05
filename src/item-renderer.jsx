20a4aa05a79b1ea0f6eb73fc9608cbbe94d47d38
Emily Eisenberg <xymostech@gmail.com>
2015-02-04 17:12:46 -0800

    Lint: Remove the duplicate HintsRenderer, reorder imports
    Summary: HintsRenderer was pulled out of item-renderer.jsx, but the old
    HintsRenderer wasn't removed! Also, the imports weren't sorted. This
    fixes both of those things.
    
    Test plan:
     - Visit test.html
     - See that hints work
    
    Auditors: aria, johnsullivan

--- src/item-renderer.jsx
+++ src/item-renderer.jsx
@@ -2,78 +2,13 @@ var React = require('react');
 var _ = require("underscore");
 
 var AnswerAreaRenderer = require("./answer-area-renderer.jsx");
+var ApiOptions = require("./perseus-api.jsx").Options;
+var EnabledFeatures = require("./enabled-features.jsx");
 var HintsRenderer = require("./hints-renderer.jsx");
 var Renderer = require("./renderer.jsx");
 var Util = require("./util.js");
-var ApiOptions = require("./perseus-api.jsx").Options;
-var EnabledFeatures = require("./enabled-features.jsx");
 
 var {mapObject} = require("./interactive2/objective_.js");
-
-var HintsRenderer = React.createClass({
-    render: function() {
-        var hintsVisible = this._hintsVisible();
-        var hints = this.props.hints
-            .slice(0, hintsVisible)
-            .map(function(hint, i) {
-                var shouldBold = i === this.props.hints.length - 1 &&
-                                 !(/\*\*/).test(hint.content);
-                return <HintRenderer
-                            bold={shouldBold}
-                            hint={hint}
-                            ref={"hintRenderer" + i}
-                            key={"hintRenderer" + i}
-                            enabledFeatures={this.props.enabledFeatures}
-                            apiOptions={this.props.apiOptions} />;
-            }, this);
-
-        return <div>{hints}</div>;
-    },
-
-    _hintsVisible: function() {
-        if (this.props.hintsVisible == null ||
-                this.props.hintsVisible === -1) {
-            return this.props.hints.length;
-        } else {
-            return this.props.hintsVisible;
-        }
-    },
-
-    getSerializedState: function() {
-        return _.times(this._hintsVisible(), (i) => {
-            return this.refs["hintRenderer" + i].getSerializedState();
-        });
-    },
-
-    restoreSerializedState: function(state, callback) {
-        // We need to wait until all the renderers are finished restoring their
-        // state before we fire our callback.
-        var numCallbacks = 1;
-        var fireCallback = () => {
-            --numCallbacks;
-            if (callback && numCallbacks === 0) {
-                callback();
-            }
-        };
-
-        _.each(state, (hintState, i) => {
-            var hintRenderer = this.refs["hintRenderer" + i];
-            // This is not ideal in that it doesn't restore state
-            // if the hint isn't visible, but we can't exactly restore
-            // the state to an unmounted renderer, so...
-            // If you want to restore state to hints, make sure to
-            // have the appropriate number of hints visible already.
-            if (hintRenderer) {
-                ++numCallbacks;
-                hintRenderer.restoreSerializedState(hintState, fireCallback);
-            }
-        });
-
-        // This makes sure that the callback is fired if there aren't any
-        // mounted renderers.
-        fireCallback();
-    },
-});
 
 var ItemRenderer = React.createClass({
     getDefaultProps: function() {
