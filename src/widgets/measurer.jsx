0292c7fc59b602661d8a21e26b7b581bee495c32
Aria Buckles <aria@khanacademy.org>
2015-02-20 13:33:14 -0800

    [measurer] MeasurerEditor cleanup!
    Summary:
    I'm trying to get rid of the NumberInput `label` prop, so
    I moved these labels to `<label>` elements.
    
    Test Plan:
    Open [this item with a measurer][1]
    
    [1]: http://localhost:9000/test.html#content=%7B%22question%22%3A%7B%22content%22%3A%22%5B%5B%E2%98%83%20measurer%201%5D%5D%5Cn%5Cn%22%2C%22images%22%3A%7B%7D%2C%22widgets%22%3A%7B%22measurer%201%22%3A%7B%22type%22%3A%22measurer%22%2C%22graded%22%3Atrue%2C%22options%22%3A%7B%22box%22%3A%5B480%2C480%5D%2C%22image%22%3A%7B%22url%22%3A%22http%3A%2F%2Flocalhost%3A9000%2Fimages%2Fface-smiley.png%22%2C%22top%22%3A50%2C%22left%22%3A100%7D%2C%22showProtractor%22%3Atrue%2C%22showRuler%22%3Atrue%2C%22rulerLabel%22%3A%22mi%22%2C%22rulerTicks%22%3A10%2C%22rulerPixels%22%3A80%2C%22rulerLength%22%3A5%7D%2C%22version%22%3A%7B%22major%22%3A1%2C%22minor%22%3A0%7D%7D%7D%7D%2C%22answerArea%22%3A%7B%22type%22%3A%22multiple%22%2C%22options%22%3A%7B%22content%22%3A%22%22%2C%22images%22%3A%7B%7D%2C%22widgets%22%3A%7B%7D%7D%2C%22calculator%22%3Afalse%7D%2C%22itemDataVersion%22%3A%7B%22major%22%3A0%2C%22minor%22%3A1%7D%2C%22hints%22%3A%5B%5D%7D
    
    Reviewers: alex
    
    Reviewed By: alex
    
    Differential Revision: https://phabricator.khanacademy.org/D16308

--- src/widgets/measurer.jsx
+++ src/widgets/measurer.jsx
@@ -217,20 +217,24 @@ var MeasurerEditor = React.createClass({
             </InfoTip>
             </div>
             {image.url && <div className="perseus-widget-row">
-                <div className="perseus-widget-left-col">
-                    <NumberInput label="Pixels from top:"
+                <label className="perseus-widget-left-col">
+                    Pixels from top:
+                    {" "}
+                    <NumberInput
                         placeholder={0}
                         onChange={this._changeTop}
                         value={image.top}
                         useArrowKeys={true} />
-                </div>
-                <div className="perseus-widget-right-col">
-                    <NumberInput label="Pixels from left:"
+                </label>
+                <label className="perseus-widget-right-col">
+                    Pixels from left:
+                    {" "}
+                    <NumberInput
                         placeholder={0}
                         onChange={this._changeLeft}
                         value={image.left}
                         useArrowKeys={true} />
-                </div>
+                </label>
             </div>}
             <div>Containing area [width, height]:{' '}
                 <RangeInput
@@ -292,18 +296,26 @@ var MeasurerEditor = React.createClass({
                 </label>
             </div>
             <div>
-                <NumberInput label="Ruler pixels per unit:"
-                    placeholder={40}
-                    onChange={this.change("rulerPixels")}
-                    value={this.props.rulerPixels}
-                    useArrowKeys={true} />
+                <label>
+                    Ruler pixels per unit:
+                    {" "}
+                    <NumberInput
+                        placeholder={40}
+                        onChange={this.change("rulerPixels")}
+                        value={this.props.rulerPixels}
+                        useArrowKeys={true} />
+                </label>
             </div>
             <div>
-                <NumberInput label="Ruler length in units:"
-                    placeholder={10}
-                    onChange={this.change("rulerLength")}
-                    value={this.props.rulerLength}
-                    useArrowKeys={true} />
+                <label>
+                    Ruler length in units:
+                    {" "}
+                    <NumberInput
+                        placeholder={10}
+                        onChange={this.change("rulerLength")}
+                        value={this.props.rulerLength}
+                        useArrowKeys={true} />
+                </label>
             </div>
             </div>}
         </div>;
