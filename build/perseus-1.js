beaa1bd715650b59dc85a2218e9eb2f3a01ed554
Ins Huang <ins.huang@gmail.com>
2015-07-11 23:08:34 +0800

    Update perseus-1.js
    build it.

--- build/perseus-1.js
+++ build/perseus-1.js
@@ -1,5 +1,5 @@
 /*! Perseus | http://github.com/Khan/perseus */
-// commit 67ed29b9d625159fd5ab03024109236d28bcad40
+// commit 9fe7f1e8d48b6e7dbc4c759e892a753fef530a3b
 // branch gh-pages
 !function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.Perseus=e():"undefined"!=typeof global?global.Perseus=e():"undefined"!=typeof self&&(self.Perseus=e())}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
@@ -8626,9 +8626,9 @@ var Editor = React.createClass({displayName: 'Editor',
         } else if (templateType === "overline") {
             template = "$\\overline{ABC}$";
         } else if (templateType === "frown") {
-            template = "$\\buildrel\\frown\\over {ABC}$";
+            template = "$\\overset{\\LARGE{\\frown{\\small ABC}}}$";
         } else if (templateType === "vec") {
-            template = "$\\vec{AB}$";
+            template = "$\\overset{\\large \\rightharpoonup}{\\small{AB}}$";
         } else if (templateType === "table") {
             template = "header 1 | header 2 | header 3\n" +
                        "- | - | -\n" +
