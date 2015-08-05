79025506a2ff06eac3940e054c2e221ccdeeee6a
Sam Lau <sam@khanacademy.org>
2015-08-05 12:17:20 -0700

    [Static Widgets 4] Stop interaction with a div on top of static widgets
    Summary:
    Before, widgets that were set to static rendered with some initial
    state but users could still interact with / modify them.
    
    This is pretty much a hack to prevent interaction with static widgets.
    We add a div that is overlaid on top of the widget container to block
    any clicks on the widget.
    
    Ideally, we'd want the widget / renderer itself to prevent interaction
    but this is a convenient way to do so right now. Static widgets also
    render in the exact same way to the user so there's no way to tell
    whether a widget is static or not.
    
    Test Plan:
    Visit [this Perseus link](http://localhost:9000/article.html#content=%5B%7B%22content%22%3A%22Hi%20I%27m%20a%20particle%21%5Cn%5Cn%5B%5B%E2%98%83%20categorizer%201%5D%5D%5Cn%5Cn%22%2C%22images%22%3A%7B%7D%2C%22widgets%22%3A%7B%22categorizer%201%22%3A%7B%22type%22%3A%22categorizer%22%2C%22alignment%22%3A%22default%22%2C%22static%22%3Atrue%2C%22graded%22%3Atrue%2C%22options%22%3A%7B%22items%22%3A%5B%22c%22%2C%22d%22%5D%2C%22categories%22%3A%5B%22a%22%2C%22b%22%5D%2C%22values%22%3A%5B0%2C1%5D%2C%22randomizeItems%22%3Afalse%7D%2C%22version%22%3A%7B%22major%22%3A0%2C%22minor%22%3A0%7D%7D%7D%7D%5D) and verify that clicking the rendered
    Categorizer doesn't change it.
    
    Reviewers: aria
    
    Subscribers:
    
    Differential Revision: https://phabricator.khanacademy.org/D20189

--- src/widget-container.jsx
+++ src/widget-container.jsx
@@ -31,8 +31,6 @@ var WidgetContainer = React.createClass({
         }
 
         var alignment = this.state.widgetProps.alignment;
-        var style = {};
-
         if (alignment === "default") {
             alignment = Widgets.getDefaultAlignment(type,
                             this.props.enabledFeatures);
@@ -40,8 +38,32 @@ var WidgetContainer = React.createClass({
 
         className += " widget-" + alignment;
 
-        return <div className={className} style={style}>
+        // Hack to prevent interaction with static widgets: we overlay a big
+        // div on top of the widget and overflow: hidden the container.
+        // Ideally widgets themselves should know how to prevent interaction.
+        var isStatic = this.state.widgetProps.static;
+        var staticContainerStyles = {
+            position: 'relative',
+            overflow: 'hidden',
+        };
+        var staticOverlayStyles = {
+            width: 10000,
+            height: 10000,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            // Since the zIndex is only relative to the nearest parent with a
+            // position:, scratchpads are still able to draw over this overlay.
+            zIndex: 100,
+        };
+
+        // We default to an empty object for style instead of null
+        // because of a strange bug where the static styles aren't applied
+        // after toggling static mode.
+        return <div className={className}
+                style={isStatic ? staticContainerStyles : {}}>
             <WidgetType {...this.state.widgetProps} ref="widget" />
+            {isStatic && <div style={staticOverlayStyles} />}
         </div>;
     },
 
