99171e5bd5d9f1051abc45f75bbfa4cd08fc7258
Jon Colverson <github@vcxz.co.uk>
2016-04-11 17:09:37 +0100

    Remove ESLint exceptions for 'no-undef' rule

--- src/mixins/widget-jsonify-deprecated.jsx
+++ src/mixins/widget-jsonify-deprecated.jsx
@@ -1,5 +1,5 @@
 /* TODO(csilvers): fix these lint errors (http://eslint.org/docs/rules): */
-/* eslint-disable no-undef, no-var */
+/* eslint-disable no-var */
 /* To fix, remove an entry above, run ka-lint, and fix errors. */
 
 /* Free implementation of getUserInput. This should be used sparingly, since it
