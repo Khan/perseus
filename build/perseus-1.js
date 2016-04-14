006e321bf1e68076e48e186480ecd75aa0cc75f6
hsJma <bright.moe@gmail.com>
2016-04-14 16:50:44 +0800

    build perseus-1 and fix minor bug

--- build/perseus-1.js
+++ build/perseus-1.js
@@ -1,6 +1,6 @@
 /*! Perseus | http://github.com/Khan/perseus */
-// commit 5cb0ac79bc81278a0962cc2ede9d0066eca3555b
-// branch modify_perseus_css
+// commit 8232c5e32083440ad7e9019ea7387f525be686ec
+// branch add_div_button
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Perseus = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
 Software License Agreement (BSD License)
@@ -6931,7 +6931,7 @@ var basicbuttons = [
                 }
             
         ];},
-        function() {return [TeX( {style:slightlyBig}, "\\div"), "\\div"];}
+        function()  {return [TeX( {style:slightlyBig}, "\\div"), "\\div"];}
     ]
 
 ];
