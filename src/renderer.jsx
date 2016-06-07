Khan/perseus

commit 44784b7f7df9e32887ee9d0f030bde5bb05e10b8

Account for multiple render calls when measuring
Summary:
It turns out `onRender` can be called multiple times, as math changes, though the node is only
provided on the first call.

Test Plan:
- Open up some math in the editor ([link](http://localhost:9000/#content=%7B%22question%22%3A%7B%22content%22%3A%22%2412345%24%22%2C%22images%22%3A%7B%7D%2C%22widgets%22%3A%7B%7D%7D%2C%22answerArea%22%3A%7B%22calculator%22%3Afalse%2C%22chi2Table%22%3Afalse%2C%22periodicTable%22%3Afalse%2C%22tTable%22%3Afalse%2C%22zTable%22%3Afalse%7D%2C%22itemDataVersion%22%3A%7B%22major%22%3A0%2C%22minor%22%3A1%7D%2C%22hints%22%3A%5B%5D%7D)).
- Start modifying the math.
    - Verify that there are no JS errors of the form: `Cannot read property 'querySelector' of undefined`

Auditors: kevinb

src/renderer.jsx
@@ -830,15 +830,18 @@ var Renderer = React.createClass({
                 this.props.onRender && this.props.onRender(node);
 
                 if (apiOptions.xomManatee) {
-                    const katex = node.querySelector('.katex');
-                    const mathjax = node.querySelector('.MathJax');
+                    // `onRender` only returns a node on the initial render.
+                    if (node) {
+                        const katex = node.querySelector(".katex");
+                        const mathjax = node.querySelector(".MathJax");
 
-                    if (katex) {
-                        waitForKatexFonts().then(() => deferred.resolve());
-                    } else if (mathjax) {
-                        waitForMathjaxFonts().then(() => deferred.resolve());
-                    } else {
-                        throw new Error(`there's no math`);
+                        if (katex) {
+                            waitForKatexFonts().then(() => deferred.resolve());
+                        } else if (mathjax) {
+                            waitForMathjaxFonts().then(() => deferred.resolve());
+                        } else {
+                            throw new Error("No math present in Renderer");
+                        }
                     }
                 }
             };

commit 5ce5b310f12607478689c311477a131eb56747e4

Fix lint error in Renderer
Summary:
Introduced in `44784b7f7`, which was an audit.

Test Plan: arc lint

Auditors: kevinb

src/renderer.jsx
@@ -836,9 +836,13 @@ var Renderer = React.createClass({
                         const mathjax = node.querySelector(".MathJax");
 
                         if (katex) {
-                            waitForKatexFonts().then(() => deferred.resolve());
+                            waitForKatexFonts().then(() => {
+                                deferred.resolve();
+                            });
                         } else if (mathjax) {
-                            waitForMathjaxFonts().then(() => deferred.resolve());
+                            waitForMathjaxFonts().then(() => {
+                                deferred.resolve();
+                            });
                         } else {
                             throw new Error("No math present in Renderer");
                         }