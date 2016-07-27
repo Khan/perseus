f5d65e5a7b6197b97ea95be78b80a3b304974ba5
Jared Forsyth <jared@jaredforsyth.com>
2016-07-27 15:05:26 -0600

    Some lint fixes
    Summary:
    NFC
    the lots of change is b/c I needed to rearrange the functions.
    
    The only one left is react/forbid-prop-types; because of the `object` proptype
    which we lint against.
    
    Test Plan: `arc lint`
    
    Reviewers: kevinb, riley
    
    Reviewed By: riley
    
    Differential Revision: https://phabricator.khanacademy.org/D29385

--- src/widgets/graded-group.jsx
+++ src/widgets/graded-group.jsx
@@ -1,38 +1,36 @@
-/* TODO(csilvers): fix these lint errors (http://eslint.org/docs/rules): */
-/* eslint-disable comma-dangle, no-var, react/forbid-prop-types, react/jsx-closing-bracket-location, react/jsx-sort-prop-types, react/prop-types, react/sort-comp */
-/* To fix, remove an entry above, run ka-lint, and fix errors. */
+/* eslint-disable react/forbid-prop-types */
 
 /* globals i18n */
-var classNames = require("classnames");
-var React = require("react");
-var _ = require("underscore");
+const classNames = require("classnames");
+const React = require("react");
+const _ = require("underscore");
 
-var ApiOptions = require("../perseus-api.jsx").Options;
-var Changeable   = require("../mixins/changeable.jsx");
-var {iconOk, iconRemove, icon} = require("../icon-paths.js");
-var InlineIcon = require("../components/inline-icon.jsx");
-var Renderer = require("../renderer.jsx");
-var GradedGroupAnswerBar = require("./graded-group-answer-bar.jsx");
-var {gray76, phoneMargin, negativePhoneMargin, tableBackgroundAccent, kaGreen} = require("../styles/constants.js");
-var {StyleSheet, css} = require("aphrodite");
+const ApiOptions = require("../perseus-api.jsx").Options;
+const Changeable   = require("../mixins/changeable.jsx");
+const {iconOk, iconRemove, icon} = require("../icon-paths.js");
+const InlineIcon = require("../components/inline-icon.jsx");
+const Renderer = require("../renderer.jsx");
+const GradedGroupAnswerBar = require("./graded-group-answer-bar.jsx");
+const {gray76, phoneMargin, negativePhoneMargin, tableBackgroundAccent, kaGreen} = require("../styles/constants.js");
+const {StyleSheet, css} = require("aphrodite");
 
 // A Graded Group is more or less a Group widget that displays a check
 // answer button below the rendered content. When clicked, the widget grades
 // the stuff inside and displays feedback about whether the inputted answer was
 // correct or not.
 
-var GRADING_STATUSES = {
+const GRADING_STATUSES = {
     ungraded: 'ungraded',
     correct: 'correct',
     incorrect: 'incorrect',
     invalid: 'invalid',
 };
 
-var ANSWER_BAR_STATES = GradedGroupAnswerBar.ANSWER_BAR_STATES;
+const ANSWER_BAR_STATES = GradedGroupAnswerBar.ANSWER_BAR_STATES;
 
 // Update answer bar state based on current state and whether the question is
 // answerable (all parts have been filled out) or not.
-var getNextState = (currentState, answerable) => {
+const getNextState = (currentState, answerable) => {
     switch (currentState) {
         case ANSWER_BAR_STATES.HIDDEN:
             return answerable ? ANSWER_BAR_STATES.ACTIVE : currentState;
@@ -50,21 +48,25 @@ var getNextState = (currentState, answerable) => {
 };
 
 // Prepended to all invalid messages to make the widget messages a bit clearer
-var INVALID_MESSAGE_PREFIX = "We couldn't grade your answer.";
-var DEFAULT_INVALID_MESSAGE = "It looks like you left something blank or " +
+const INVALID_MESSAGE_PREFIX = "We couldn't grade your answer.";
+const DEFAULT_INVALID_MESSAGE = "It looks like you left something blank or " +
     "entered in an invalid answer.";
 
-var GradedGroup = React.createClass({
-    mixins: [Changeable],
-
+const GradedGroup = React.createClass({
     propTypes: {
-        title: React.PropTypes.string,
-        content: React.PropTypes.string,
-        widgets: React.PropTypes.object,
-        images: React.PropTypes.object,
         apiOptions: ApiOptions.propTypes,
+        content: React.PropTypes.string,
+        hasHint: React.PropTypes.bool,
+        hint: React.PropTypes.object,
+        images: React.PropTypes.object,
+        onBlur: React.PropTypes.func,
+        onFocus: React.PropTypes.func,
+        title: React.PropTypes.string,
         trackInteraction: React.PropTypes.func.isRequired,
+        widgets: React.PropTypes.object,
     },
+
+    mixins: [Changeable],
 
     getDefaultProps: function() {
         return {
@@ -84,8 +86,90 @@ var GradedGroup = React.createClass({
         };
     },
 
+    shouldComponentUpdate: function(nextProps, nextState) {
+        return nextProps !== this.props || nextState !== this.state;
+    },
+
+    // This is a little strange because the id of the widget that actually
+    // changed is going to be lost in favor of the group widget's id. The
+    // widgets prop also wasn't actually changed, and this only serves to
+    // alert our renderer (our parent) of the fact that some interaction
+    // has occurred.
+    _onInteractWithWidget: function(id) {
+        // Reset grading display when user changes answer
+        this.setState({
+            status: GRADING_STATUSES.ungraded,
+            message: "",
+        });
+
+        if (this.refs.renderer) {
+            this.change("widgets", this.props.widgets);
+            const emptyWidgets = this.refs.renderer.emptyWidgets();
+            const answerable = emptyWidgets.length === 0;
+            const answerBarState = this.state.answerBarState;
+            this.setState({
+                answerBarState: getNextState(answerBarState, answerable),
+            });
+        }
+    },
+
+    _checkAnswer: function() {
+        const score = this.refs.renderer.score();
+
+        let status;
+        let message;
+        if (score.type === "points") {
+            status = score.total === score.earned ?
+                GRADING_STATUSES.correct : GRADING_STATUSES.incorrect;
+            message = score.message || "";
+        } else { // score.type is "invalid"
+            status = GRADING_STATUSES.invalid;
+            message = score.message ?
+                `${INVALID_MESSAGE_PREFIX} ${score.message}` :
+                `${INVALID_MESSAGE_PREFIX} ${DEFAULT_INVALID_MESSAGE}`;
+        }
+
+        this.setState({
+            status: status,
+            message: message,
+            // TODO(kevinb) handle 'invalid' status
+            answerBarState: status === 'correct'
+                ? ANSWER_BAR_STATES.CORRECT
+                : ANSWER_BAR_STATES.INCORRECT,
+        });
+
+        this.props.trackInteraction({
+            status: status,
+        });
+    },
+
+    // Mobile API
+    getInputPaths: function() {
+        return this.refs.renderer.getInputPaths();
+    },
+
+    setInputValue: function(path, newValue, cb) {
+        return this.refs.renderer.setInputValue(path, newValue, cb);
+    },
+
+    getAcceptableFormatsForInputPath: function(path) {
+        return this.refs.renderer.getAcceptableFormatsForInputPath(path);
+    },
+
+    focus: function() {
+        return this.refs.renderer.focus();
+    },
+
+    focusInputPath: function(path) {
+        this.refs.renderer.focusPath(path);
+    },
+
+    blurInputPath: function(path) {
+        this.refs.renderer.blurPath(path);
+    },
+
     render: function() {
-        var apiOptions = _.extend(
+        const apiOptions = _.extend(
             {},
             ApiOptions.defaults,
             this.props.apiOptions,
@@ -99,12 +183,12 @@ var GradedGroup = React.createClass({
                     if (newFocus) {
                         this.props.onFocus(newFocus);
                     }
-                }
+                },
             }
         );
 
 
-        var icon = null;
+        let icon = null;
         // Colors are 10% darker than the colors in graded-group.less
         if (this.state.status === GRADING_STATUSES.correct) {
             icon = <InlineIcon {...iconOk} style={{color: "#526f03"}} />;
@@ -112,7 +196,7 @@ var GradedGroup = React.createClass({
             icon = <InlineIcon {...iconRemove} style={{color: "#ff5454"}} />;
         }
 
-        var classes = classNames({
+        const classes = classNames({
             [css(styles.gradedGroup)]: apiOptions.xomManatee,
             "perseus-graded-group": true,
             "answer-correct": apiOptions.xomManatee
@@ -141,7 +225,8 @@ var GradedGroup = React.createClass({
                 {...this.props}
                 ref="renderer"
                 apiOptions={{...apiOptions, readOnly}}
-                onInteractWithWidget={this._onInteractWithWidget} />
+                onInteractWithWidget={this._onInteractWithWidget}
+            />
             {!apiOptions.xomManatee && icon && <div className="group-icon">
                 {icon}
             </div>}
@@ -151,7 +236,8 @@ var GradedGroup = React.createClass({
                 value={i18n._("Check Answer")}
                 className="simple-button"
                 disabled={this.props.apiOptions.readOnly}
-                onClick={this._checkAnswer} />}
+                onClick={this._checkAnswer}
+            />}
             {this.props.hasHint &&
              (this.state.showHint ?
                 <div>
@@ -183,91 +269,9 @@ var GradedGroup = React.createClass({
                 />}
         </div>;
     },
-
-    // This is a little strange because the id of the widget that actually
-    // changed is going to be lost in favor of the group widget's id. The
-    // widgets prop also wasn't actually changed, and this only serves to
-    // alert our renderer (our parent) of the fact that some interaction
-    // has occurred.
-    _onInteractWithWidget: function(id) {
-        // Reset grading display when user changes answer
-        this.setState({
-            status: GRADING_STATUSES.ungraded,
-            message: "",
-        });
-
-        if (this.refs.renderer) {
-            this.change("widgets", this.props.widgets);
-            const emptyWidgets = this.refs.renderer.emptyWidgets();
-            const answerable = emptyWidgets.length === 0;
-            const answerBarState = this.state.answerBarState;
-            this.setState({
-                answerBarState: getNextState(answerBarState, answerable),
-            });
-        }
-    },
-
-    _checkAnswer: function() {
-        var score = this.refs.renderer.score();
-
-        var status;
-        var message;
-        if (score.type === "points") {
-            status = score.total === score.earned ?
-                GRADING_STATUSES.correct : GRADING_STATUSES.incorrect;
-            message = score.message || "";
-        } else { // score.type is "invalid"
-            status = GRADING_STATUSES.invalid;
-            message = score.message ?
-                `${INVALID_MESSAGE_PREFIX} ${score.message}` :
-                `${INVALID_MESSAGE_PREFIX} ${DEFAULT_INVALID_MESSAGE}`;
-        }
-
-        this.setState({
-            status: status,
-            message: message,
-            // TODO(kevinb) handle 'invalid' status
-            answerBarState: status === 'correct'
-                ? ANSWER_BAR_STATES.CORRECT
-                : ANSWER_BAR_STATES.INCORRECT,
-        });
-
-        this.props.trackInteraction({
-            status: status,
-        });
-    },
-
-    shouldComponentUpdate: function(nextProps, nextState) {
-        return nextProps !== this.props || nextState !== this.state;
-    },
-
-    // Mobile API
-    getInputPaths: function() {
-        return this.refs.renderer.getInputPaths();
-    },
-
-    setInputValue: function(path, newValue, cb) {
-        return this.refs.renderer.setInputValue(path, newValue, cb);
-    },
-
-    getAcceptableFormatsForInputPath: function(path) {
-        return this.refs.renderer.getAcceptableFormatsForInputPath(path);
-    },
-
-    focus: function() {
-        return this.refs.renderer.focus();
-    },
-
-    focusInputPath: function(path) {
-        this.refs.renderer.focusPath(path);
-    },
-
-    blurInputPath: function(path) {
-        this.refs.renderer.blurPath(path);
-    },
 });
 
-var traverseChildWidgets = function(props, traverseRenderer) {
+const traverseChildWidgets = function(props, traverseRenderer) {
     return _.extend({}, props, traverseRenderer(props));
 };
 
