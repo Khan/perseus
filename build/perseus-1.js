11958faaecbf09ec866ef1de382782852f99dfdc
Giddy Lin <giddytoroh@gmail.com>
2016-09-08 16:45:47 +0800

    dynamic button div height

--- build/perseus-1.js
+++ build/perseus-1.js
@@ -1,6 +1,6 @@
 /*! Perseus | http://github.com/Khan/perseus */
-// commit 086d968f1cb5f30fe4af216312ad4a669acd1721
-// branch expression-style
+// commit 736fd4f76358b7502996ccdd07f47e5f6bfac960
+// branch fix-expression-button
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Perseus = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
 Software License Agreement (BSD License)
@@ -5762,12 +5762,14 @@ var MathInput = React.createClass({displayName: 'MathInput',
         });
 
         var buttons = null;
+        var button_height = "0px";
         if (this._shouldShowButtons()) {
             buttons = TexButtons(
                 {className:"math-input-buttons absolute",
                 convertDotToTimes:this.props.convertDotToTimes,
-                onInsert:this.insert, 
+                onInsert:this.insert,
                 sets:this.props.buttonSets} );
+            button_height = (6 + 58 * this.props.buttonSets.length).toString() + "px";
         }
 
         return React.DOM.div( {style:{display: "inline-block"}}, 
@@ -5777,7 +5779,7 @@ var MathInput = React.createClass({displayName: 'MathInput',
                       onFocus:this.handleFocus,
                       onBlur:this.handleBlur} )
             ),
-            React.DOM.div( {style:{position: "relative"}}, 
+            React.DOM.div( {style:{position: "relative", height: button_height}}, 
                 buttons
             )
         );
