7bd3bc241e86799ad6ed5642d56e9c28f68d38e2
Kevin Barabash <kevinb@khanacademy.org>
2016-09-17 18:58:27 -0700

    Replace non-breaking spaces with real spaces before rendering math with KaTeX
    Summary: https://app.asana.com/0/74945521838608/174138257679861
    
    Test Plan:
    - make serve
    - localhost:9000
    - write some math containing a non-breaking space
    - verify that the math is rendered with KaTeX instead of MathJax
    
    Reviewers: jared
    
    Reviewed By: jared
    
    Differential Revision: https://phabricator.khanacademy.org/D30837

--- src/util/katex-preprocess.js
+++ src/util/katex-preprocess.js
@@ -3,10 +3,12 @@
  * to things is does.
  */
 
-module.exports = (texCode) => {
+module.exports = (texCode) => texCode
     // Replace uses of \begin{align}...\end{align} which KaTeX doesn't
     // support (yet) with \begin{aligned}...\end{aligned} which renders
     // the same is supported by KaTeX.  It does the same for align*.
     // TODO(kevinb) update content to use aligned instead of align.
-    return texCode.replace(/\{align[*]?\}/g, '{aligned}');
-};
+    .replace(/\{align[*]?\}/g, '{aligned}')
+
+    // Replace non-breaking spaces with regular spaces.
+    .replace(/[\u00a0]/g, ' ');
