69357c900af97ede26dca3c9250d824586dfad89
Kevin Lin <kevinptt0323@gmail.com>
2017-02-26 03:31:15 +0800

    return React Component after render

--- src/render.js
+++ src/render.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var render = function(Component, dom, props) {
-    ReactDOM.render(<Component {...props} />, dom);
+    return ReactDOM.render(<Component {...props} />, dom);
 };
 
 module.exports = render;
