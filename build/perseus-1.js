96e37a6618a36a761a5d0d45ee22d234ca272d4d
Kevin Lin <kevinptt0323@gmail.com>
2017-06-16 16:26:40 +0800

    build perseus

--- build/perseus-1.js
+++ build/perseus-1.js
@@ -1,5 +1,5 @@
 /*! Perseus | http://github.com/Khan/perseus */
-// commit 38c138613f474b0c168244a94f75aa416c45ac49
+// commit 604e0e1dae9592e89f5a20d0e03411c63b5b2944
 // branch react-15
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Perseus = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
@@ -29406,7 +29406,7 @@ var NumberInput = React.createClass({
 
     parseInputValue: function parseInputValue(value) {
         if (value === "") {
-            placeholder = this.props.placeholder;
+            var placeholder = this.props.placeholder;
             return _.isFinite(placeholder) ? +placeholder : null;
         } else {
             var result = firstNumericalParse(value);
