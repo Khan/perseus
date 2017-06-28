4af860f40a119ca16577154270678757e20c975c
Kevin Lin <kevinptt0323@gmail.com>
2017-06-28 17:02:25 +0800

    increase width of text-list-editor

--- build/perseus-1.js
+++ build/perseus-1.js
@@ -1,5 +1,5 @@
 /*! Perseus | http://github.com/Khan/perseus */
-// commit ae15748cc98ef568882bf6db8234425b228b60ef
+// commit 06c6d66351d28428d37f123b89421080b409b73b
 // branch react-15
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Perseus = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
@@ -30423,7 +30423,7 @@ function getTextWidth(text) {
     if (!textWidthCache[text]) {
         // Hacky way to guess the width of an input box
         var $test = $("<span>").text(text).appendTo("body");
-        textWidthCache[text] = $test.width() + 5;
+        textWidthCache[text] = $test.width() + 20;
         $test.remove();
     }
     return textWidthCache[text];
