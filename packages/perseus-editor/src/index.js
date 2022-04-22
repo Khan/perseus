185b1c8df21beae9cec3c741011d53c579142fef
Kevin Barabash <kevinb@khanacademy.org>
2022-04-22 09:58:07 -0400

    [TEST]: make sure that 'Check Build' step is reporting size deltas

--- packages/perseus-editor/src/index.js
+++ packages/perseus-editor/src/index.js
@@ -3,3 +3,6 @@ import {foo} from "@khanacademy/perseus";
 
 // eslint-disable-next-line no-console
 export const bar = (): void => console.log(`foo() = ${foo()}`);
+
+export const sum = (...args: Array<number>): number =>
+    args.reduce((a, b) => a + b, 0);
