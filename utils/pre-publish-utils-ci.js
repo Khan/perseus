28bbc3ad55a18dc96f8bc3fa03f36910740b73f9
Kevin Barabash <kevinb@khanacademy.org>
2022-04-22 15:27:46 -0400

    Add missing utils files

---
+++ utils/pre-publish-utils-ci.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console, import/no-commonjs */
+/**
+ * Pre-publish checks to verify that our publish will go smoothly.
+ */
+const path = require("path");
+const fg = require("fast-glob");
+
+const {
+    checkPrivate,
+    checkEntrypoints,
+    checkSource,
+    checkPublishConfig,
+} = require("./internal/pre-publish-utils.js");
+
+// eslint-disable-next-line promise/catch-or-return
+fg(path.join(__dirname, "..", "packages", "**", "package.json")).then(
+    (pkgPaths) => {
+        // eslint-disable-next-line promise/always-return
+        for (const pkgPath of pkgPaths) {
+            const pkgJson = require(path.relative(__dirname, pkgPath));
+
+            if (!checkPrivate(pkgJson)) {
+                checkPublishConfig(pkgJson);
+                checkEntrypoints(pkgJson);
+                checkSource(pkgJson);
+            }
+        }
+    },
+);
