3dc91b7eb5237494dac01780e58c68d5800757e7
Kevin Barabash <kevinb@khanacademy.org>
2022-04-26 11:18:33 -0400

    copy perseus-merged-editor-package to the right place

--- packages/perseus-editor/src/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import {foo} from "@khanacademy/perseus";
-import * as i18n from "@khanacademy/wonder-blocks-i18n";
-
-export const bar = (): void => {
-    const msg = i18n._("foo() = %(foo)s", {foo: foo()});
-    // eslint-disable-next-line no-console
-    console.log(msg);
-};
