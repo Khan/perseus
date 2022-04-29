d5e0da55e168214b4d7a3c64d98c77598d6080ab
Jeremy Wiebe <jeremy@khanacademy.org>
2022-04-29 11:11:15 -0700

    Remove in-package tooling configs (we have them at the root)

--- packages/math-input/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    verbose: true,
-    testMatch: ["**/test/*_spec.js"],
-    setupFilesAfterEnv: [
-        "./node_modules/jest-enzyme/lib/index.js",
-        "<rootDir>/jest.setup.js",
-    ],
-};
