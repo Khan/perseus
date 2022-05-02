7d80f924facd895c7593b9ecbebdab8eeb5bff24
Jeremy Wiebe <jeremy@khanacademy.org>
2022-05-02 12:40:37 -0700

    [math-input] Remove in-package tooling configs (we have them at the root) (#168)
    ## Summary:
    
    Our build config is mostly the same across all packages, so we don't need the ones that came from the math-input repo. I'll do a final review of the built files at the end of this PR series to make sure I haven't broken the bundled files.
    
    Issue: LP-11432
    
    ## Test plan:
    
    Author: jeremywiebe
    
    Reviewers: kevinbarabash
    
    Required Reviewers:
    
    Approved By: kevinbarabash
    
    Checks: ⌛ Test (ubuntu-latest, 16.x), ⌛ Lint, flow, and coverage check (ubuntu-latest, 16.x), ⌛ Extract i18n strings (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ❌ Test (ubuntu-latest, 16.x), ❌ Lint, flow, and coverage check (ubuntu-latest, 16.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/168

--- packages/math-input/jest.setup.js
+++
@@ -1,8 +0,0 @@
-const {StyleSheetTestUtils} = require("aphrodite");
-
-const Enzyme = require("enzyme");
-const EnzymeAdapter = require("enzyme-adapter-react-16");
-
-StyleSheetTestUtils.suppressStyleInjection();
-
-Enzyme.configure({adapter: new EnzymeAdapter()});
