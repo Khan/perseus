ac2796706e570f26dd342813e878ae1353b2a64d
Jeremy Wiebe <jeremy@khanacademy.org>
2022-05-09 15:47:40 -0700

    Package.json cleanup (#218)
    ## Summary:
    This is very much a housekeeping PR. It re-indents all package.json files to 4 spaces and then orders the top-level keys in them by a "conventional" order (as decided by me, for now). 
    
    The proposed order is (and this can _easily_ be changed and these files re-generated): 
    ```
    name, 
    description, 
    Author: jeremywiebe
    license, 
    version, 
    publishConfig, 
    repository, 
    bugs, 
    module, 
    main, 
    source, 
    scripts, 
    dependencies, 
    devDependencies, 
    peerDependencies, 
    keywords, 
    ```
    
    Please discuss ordering if you have strong feelings about them. If not, at least we'll have a consistent ordering in every package.json in this repo after this PR lands. :) 
    
    You definitely want to review this PR with "Hide whitespace" turned on. 👇 
    
    <img width="373" alt="image" src="https://user-images.githubusercontent.com/77138/167472865-7f713bb2-ddb0-4778-a6f8-654166fa17ed.png">
    
    
    Issue: "none"
    
    ## Test plan:
    
    Reviewers: jeremywiebe, kevinbarabash
    
    Required Reviewers:
    
    Approved By: kevinbarabash
    
    Checks: ✅ codecov/project, ✅ Lint, Flow, and Test (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ gerald, ✅ Test Coverage (ubuntu-latest, 16.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/218

---
+++ utils/format-package-json.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+## This script re-formats each `package.json` in the `packages` directory and
+## orders the top-level keys by a consistent order. It also forces a few values
+## to be standardized (such as 'author' always being 'Khan Academy').
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq not be found in PATH (you can install using 'brew install jq'"
+    exit
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd "$SCRIPT_DIR/.." > /dev/null 2>&1 || exit
+
+# We loop over all packages and re-order them (and re-indent)
+# writing to a temp file and then overwriting the original file to avoid
+# truncation errors (especially in the face of failures in the processing
+# pipeline).
+for dir in ./packages/*; do
+    echo "$dir"
+    PKG_NAME="$(basename "$dir")"
+
+    jq --indent 4 ". | {
+        name: .name,
+        description: .description,
+        author: \"Khan Academy\",
+        license: \"MIT\",
+        version: .version,
+        publishConfig: {
+            access: \"public\"
+        },
+        repository: {
+            type: \"git\",
+            url: \"https://github.com/Khan/$PKG_NAME.git\"
+        },
+        bugs: {
+            url: \"https://github.com/Khan/perseus/issues\",
+        },
+        module: .module,
+        main: .main,
+        source: .source,
+        scripts: (.scripts // {}),
+        dependencies: (.dependencies // {}),
+        devDependencies: .devDependencies,
+        peerDependencies: .peerDependencies,
+        keywords: (.keywords // [])
+    }" "$dir/package.json" > "$dir/package.out.json"
+    mv "$dir/package.out.json" "$dir/package.json"
+done
