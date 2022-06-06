96288b872256edd19a3629f3218813b17a517e23
Jeremy Wiebe <jeremy@khanacademy.org>
2022-06-06 14:15:04 -0700

    Export CursorContext (#259)
    ## Summary:
    
    @khanacademy/math-input clients sometimes use the CursorContext as part of using the math-input project. Instead of re-defining the values in the object, this PR adds it as an export from the main module.
    
    Issue: MOB-4525
    
    ## Test plan:
    
    Author: jeremywiebe
    
    Reviewers: kevinbarabash
    
    Required Reviewers:
    
    Approved By: kevinbarabash
    
    Checks: ✅ Lint, Flow, and Test (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ Check for .changeset file (ubuntu-latest, 16.x), ✅ Cypress Coverage (ubuntu-latest, 16.x), ✅ Jest Coverage (ubuntu-latest, 16.x), ❌ codecov/project, ✅ gerald, ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/259

--- packages/math-input/src/index.js
+++ packages/math-input/src/index.js
@@ -12,3 +12,7 @@ export {
 } from "./components/prop-types.js";
 export {default as Keypad} from "./components/provided-keypad.js";
 export {KeypadTypes} from "./consts.js";
+
+import * as CursorContexts from "./components/input/cursor-contexts.js";
+
+export {CursorContexts};
