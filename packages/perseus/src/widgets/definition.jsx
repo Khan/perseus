4c6e303021b1f03c18a73180fb5c748c5d818dc5
Jeremy Wiebe <jeremy@khanacademy.org>
2022-06-08 17:05:09 -0700

    Remove unushed import

--- packages/perseus/src/widgets/definition.jsx
+++ packages/perseus/src/widgets/definition.jsx
@@ -3,7 +3,6 @@
 import Button from "@khanacademy/wonder-blocks-button";
 import Color from "@khanacademy/wonder-blocks-color";
 import {Popover, PopoverContentCore} from "@khanacademy/wonder-blocks-popover";
-import Spacing from "@khanacademy/wonder-blocks-spacing";
 import * as React from "react";
 
 import Renderer from "../renderer.jsx";
@@ -91,7 +90,6 @@ const styles = {
         fontSize: 20,
         fontWeight: 500,
         lineHeight: "30px",
-        margin: Spacing.xSmall_8,
     },
 };
 
