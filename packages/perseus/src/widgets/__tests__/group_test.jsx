6bc8a479210503587465bf1986df707094163d8e
Nicole Watts <nicole@khanacademy.org>
2022-06-14 14:27:15 -0600

    wip: fixup allllll of the tests

--- packages/perseus/src/widgets/__tests__/group_test.jsx
+++ packages/perseus/src/widgets/__tests__/group_test.jsx
@@ -347,7 +347,7 @@ describe("group widget", () => {
 
         // Arrange
         const {renderer} = renderQuestion(question1);
-        userEvent.click(screen.getAllByRole("radio")[4]);
+        userEvent.click(screen.getAllByRole("button")[4]);
         userEvent.type(screen.getAllByRole("textbox")[0], "1000");
         userEvent.type(screen.getAllByRole("textbox")[1], "9999");
 
@@ -360,7 +360,10 @@ describe("group widget", () => {
         renderer1.restoreSerializedState(state);
 
         // Assert
-        expect(screen.getAllByRole("radio")[4]).toBeChecked();
+        expect(screen.getAllByRole("button")[4]).toHaveAttribute(
+            "aria-selected",
+            "true",
+        );
         expect(screen.getAllByRole("textbox")[0]).toHaveValue("1000");
         expect(screen.getAllByRole("textbox")[1]).toHaveValue("9999");
     });
@@ -369,7 +372,7 @@ describe("group widget", () => {
         // Arrange
         const {renderer} = renderQuestion(question1);
         // Answer all widgets correctly
-        userEvent.click(screen.getAllByRole("radio")[4]);
+        userEvent.click(screen.getAllByRole("button")[4]);
         userEvent.type(screen.getAllByRole("textbox")[0], "230");
         userEvent.type(screen.getAllByRole("textbox")[1], "200");
 
@@ -453,7 +456,7 @@ describe("group widget", () => {
     it("should show rationales for contained widgets", () => {
         // Arrange
         const {renderer} = renderQuestion(question1);
-        userEvent.click(screen.getAllByRole("radio")[2]); // Incorrect!
+        userEvent.click(screen.getAllByRole("button")[2]); // Incorrect!
 
         // Act
         renderer.showRationalesForCurrentlySelectedChoices();
