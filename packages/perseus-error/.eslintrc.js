b4e6c4bc6ab6694af390c07944c82c10412cb5c4
Kevin Barabash <kevinb@khanacademy.org>
2022-10-05 10:38:47 -0400

    Update eslint config to look at package.json at the root for deps

--- packages/perseus-error/.eslintrc.js
+++ packages/perseus-error/.eslintrc.js
@@ -1,6 +1,11 @@
 /* eslint-disable import/no-commonjs */
+const path = require("path");
+
 module.exports = {
     rules: {
-        "import/no-extraneous-dependencies": "error",
+        "import/no-extraneous-dependencies": [
+            "error",
+            {packageDir: [__dirname, path.join(__dirname, "../../")]},
+        ],
     },
 };
