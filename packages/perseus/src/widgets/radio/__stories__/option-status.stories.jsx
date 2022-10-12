a7586ac2deeca797aef54570b8c3e6b0ae1addb3
Tamara Bozich <tamara.bozich_c@khanacademy.org>
2022-10-12 11:07:19 -0400

    adds interactive story for option-status; converts option-status to functional component

--- packages/perseus/src/widgets/radio/__stories__/option-status.stories.jsx
+++ packages/perseus/src/widgets/radio/__stories__/option-status.stories.jsx
@@ -3,132 +3,162 @@ import * as React from "react";
 
 import OptionStatus from "../option-status.jsx";
 
-type StoryArgs = {||};
+type StoryArgs = {|
+    crossedOut: boolean,
+    checked: boolean,
+    correct: boolean,
+    previouslyAnswered: boolean,
+    reviewMode: boolean,
+    satStyling: boolean,
+|};
 
 type Story = {|
     title: string,
+    args: StoryArgs,
 |};
 
 export default ({
     title: "Perseus/Widgets/Radio/Option Status",
+    args: {
+        crossedOut: false,
+        checked: false,
+        correct: false,
+        previouslyAnswered: false,
+        reviewMode: true,
+        satStyling: false,
+    },
 }: Story);
 
-export const CheckedCorrectPreviouslyAnswered = (
-    args: StoryArgs,
-): React.Node => {
-    return (
-        <OptionStatus
-            crossedOut={false}
-            checked={true}
-            correct={true}
-            previouslyAnswered={true}
-            reviewMode={true}
-            satStyling={false}
-        />
-    );
+export const Interactive = (args: StoryArgs): React.Node => {
+    return <OptionStatus {...args} />;
 };
 
-export const CheckedCorrectNotPreviouslyAnswered = (
-    args: StoryArgs,
-): React.Node => {
-    return (
-        <OptionStatus
-            crossedOut={false}
-            checked={true}
-            correct={true}
-            previouslyAnswered={false}
-            reviewMode={true}
-            satStyling={false}
-        />
-    );
-};
+// type StoryArgs = {||};
 
-export const CheckedNotCorrectPreviouslyAnswered = (
-    args: StoryArgs,
-): React.Node => {
-    return (
-        <OptionStatus
-            crossedOut={false}
-            checked={true}
-            correct={false}
-            previouslyAnswered={true}
-            reviewMode={true}
-            satStyling={false}
-        />
-    );
-};
+// type Story = {|
+//     title: string,
+// |};
 
-export const CheckedNotCorrectNotPreviouslyAnswered = (
-    args: StoryArgs,
-): React.Node => {
-    return (
-        <OptionStatus
-            crossedOut={false}
-            checked={true}
-            correct={false}
-            previouslyAnswered={false}
-            reviewMode={true}
-            satStyling={false}
-        />
-    );
-};
+// export default ({
+//     title: "Perseus/Widgets/Radio/Option Status",
+// }: Story);
 
-export const NotCheckedCorrectPreviouslyAnswered = (
-    args: StoryArgs,
-): React.Node => {
-    return (
-        <OptionStatus
-            crossedOut={false}
-            checked={false}
-            correct={true}
-            previouslyAnswered={true}
-            reviewMode={true}
-            satStyling={false}
-        />
-    );
-};
+// export const CheckedCorrectPreviouslyAnswered = (
+//     args: StoryArgs,
+// ): React.Node => {
+//     return (
+//         <OptionStatus
+//             crossedOut={false}
+//             checked={true}
+//             correct={true}
+//             previouslyAnswered={true}
+//             reviewMode={true}
+//             satStyling={false}
+//         />
+//     );
+// };
 
-export const NotCheckedCorrectNotPreviouslyAnswered = (
-    args: StoryArgs,
-): React.Node => {
-    return (
-        <OptionStatus
-            crossedOut={false}
-            checked={false}
-            correct={true}
-            previouslyAnswered={false}
-            reviewMode={true}
-            satStyling={false}
-        />
-    );
-};
+// export const CheckedCorrectNotPreviouslyAnswered = (
+//     args: StoryArgs,
+// ): React.Node => {
+//     return (
+//         <OptionStatus
+//             crossedOut={false}
+//             checked={true}
+//             correct={true}
+//             previouslyAnswered={false}
+//             reviewMode={true}
+//             satStyling={false}
+//         />
+//     );
+// };
 
-export const NotCheckedNotCorrectPreviouslyAnswered = (
-    args: StoryArgs,
-): React.Node => {
-    return (
-        <OptionStatus
-            crossedOut={false}
-            checked={false}
-            correct={false}
-            previouslyAnswered={true}
-            reviewMode={true}
-            satStyling={false}
-        />
-    );
-};
+// export const CheckedNotCorrectPreviouslyAnswered = (
+//     args: StoryArgs,
+// ): React.Node => {
+//     return (
+//         <OptionStatus
+//             crossedOut={false}
+//             checked={true}
+//             correct={false}
+//             previouslyAnswered={true}
+//             reviewMode={true}
+//             satStyling={false}
+//         />
+//     );
+// };
 
-export const NotCheckedNotCorrectNotPreviouslyAnswered = (
-    args: StoryArgs,
-): React.Node => {
-    return (
-        <OptionStatus
-            crossedOut={false}
-            checked={false}
-            correct={false}
-            previouslyAnswered={false}
-            reviewMode={true}
-            satStyling={false}
-        />
-    );
-};
+// export const CheckedNotCorrectNotPreviouslyAnswered = (
+//     args: StoryArgs,
+// ): React.Node => {
+//     return (
+//         <OptionStatus
+//             crossedOut={false}
+//             checked={true}
+//             correct={false}
+//             previouslyAnswered={false}
+//             reviewMode={true}
+//             satStyling={false}
+//         />
+//     );
+// };
+
+// export const NotCheckedCorrectPreviouslyAnswered = (
+//     args: StoryArgs,
+// ): React.Node => {
+//     return (
+//         <OptionStatus
+//             crossedOut={false}
+//             checked={false}
+//             correct={true}
+//             previouslyAnswered={true}
+//             reviewMode={true}
+//             satStyling={false}
+//         />
+//     );
+// };
+
+// export const NotCheckedCorrectNotPreviouslyAnswered = (
+//     args: StoryArgs,
+// ): React.Node => {
+//     return (
+//         <OptionStatus
+//             crossedOut={false}
+//             checked={false}
+//             correct={true}
+//             previouslyAnswered={false}
+//             reviewMode={true}
+//             satStyling={false}
+//         />
+//     );
+// };
+
+// export const NotCheckedNotCorrectPreviouslyAnswered = (
+//     args: StoryArgs,
+// ): React.Node => {
+//     return (
+//         <OptionStatus
+//             crossedOut={false}
+//             checked={false}
+//             correct={false}
+//             previouslyAnswered={true}
+//             reviewMode={true}
+//             satStyling={false}
+//         />
+//     );
+// };
+
+// export const NotCheckedNotCorrectNotPreviouslyAnswered = (
+//     args: StoryArgs,
+// ): React.Node => {
+//     return (
+//         <OptionStatus
+//             crossedOut={false}
+//             checked={false}
+//             correct={false}
+//             previouslyAnswered={false}
+//             reviewMode={true}
+//             satStyling={false}
+//         />
+//     );
+// };
--- packages/perseus/src/widgets/radio/option-status.jsx
+++ packages/perseus/src/widgets/radio/option-status.jsx
@@ -12,7 +12,7 @@ import * as i18n from "@khanacademy/wonder-blocks-i18n";
 import {StyleSheet, css} from "aphrodite";
 import * as React from "react";
 
-class OptionStatus extends React.Component<{
+type OptionStatusProps = {|
     // Was this option the correct answer?
     correct: boolean,
     // Did the user select this option as the answer?
@@ -23,10 +23,11 @@ class OptionStatus extends React.Component<{
     previouslyAnswered: boolean,
     reviewMode: boolean,
     satStyling?: boolean,
-    ...
-}> {
-    _renderText(): string {
-        const {checked, correct, crossedOut} = this.props;
+|};
+
+function OptionStatus(props: OptionStatusProps): React.Node {
+    function renderText(): string {
+        const {checked, correct, crossedOut} = props;
         if (correct) {
             // For correct answers, we surface checked _or_ crossedOut state,
             // because any interaction with the correct answer is noteworthy!
@@ -46,39 +47,90 @@ class OptionStatus extends React.Component<{
         return i18n._("Incorrect");
     }
 
-    render(): React.Node {
-        const {checked, correct, previouslyAnswered, reviewMode, satStyling} =
-            this.props;
+    const {checked, correct, previouslyAnswered, reviewMode, satStyling} =
+        props;
 
-        // Option status is exclued for SAT
-        if (satStyling) {
-            return null;
-        }
-
-        // Option status is shown only in review mode, or for incorrectly
-        // answered items.
-        if (!reviewMode && !previouslyAnswered) {
-            return null;
-        }
-
-        let textStyle;
-        if (correct) {
-            textStyle = styles.correct;
+    let textStyle;
+    if (correct) {
+        textStyle = styles.correct;
+    } else {
+        if (checked || previouslyAnswered) {
+            textStyle = styles.incorrectAnswered;
         } else {
-            if (checked || previouslyAnswered) {
-                textStyle = styles.incorrectAnswered;
-            } else {
-                textStyle = styles.incorrect;
-            }
+            textStyle = styles.incorrect;
         }
-
-        return (
-            <div className={css(styles.text, textStyle)}>
-                {this._renderText()}
-            </div>
-        );
     }
+
+    return <div className={css(styles.text, textStyle)}>{renderText()}</div>;
 }
+
+// class OptionStatus extends React.Component<{
+//     // Was this option the correct answer?
+//     correct: boolean,
+//     // Did the user select this option as the answer?
+//     checked: boolean,
+//     // Did the user cross out this option?
+//     crossedOut: boolean,
+//     // Did the user select this option as the answer earlier?
+//     previouslyAnswered: boolean,
+//     reviewMode: boolean,
+//     satStyling?: boolean,
+//     ...
+// }> {
+//     _renderText(): string {
+//         const {checked, correct, crossedOut} = this.props;
+//         if (correct) {
+//             // For correct answers, we surface checked _or_ crossedOut state,
+//             // because any interaction with the correct answer is noteworthy!
+//             if (checked) {
+//                 return i18n._("Correct (selected)");
+//             }
+//             if (crossedOut) {
+//                 return i18n._("Correct (but you crossed it out)");
+//             }
+//             return i18n._("Correct");
+//         }
+//         // But, for incorrect answers, we only surface checked state,
+//         // because crossing out an incorrect answer is not noteworthy.
+//         if (checked) {
+//             return i18n._("Incorrect (selected)");
+//         }
+//         return i18n._("Incorrect");
+//     }
+
+//     render(): React.Node {
+//         const {checked, correct, previouslyAnswered, reviewMode, satStyling} =
+//             this.props;
+
+//         // Option status is exclued for SAT
+//         if (satStyling) {
+//             return null;
+//         }
+
+//         // Option status is shown only in review mode, or for incorrectly
+//         // answered items.
+//         if (!reviewMode && !previouslyAnswered) {
+//             return null;
+//         }
+
+//         let textStyle;
+//         if (correct) {
+//             textStyle = styles.correct;
+//         } else {
+//             if (checked || previouslyAnswered) {
+//                 textStyle = styles.incorrectAnswered;
+//             } else {
+//                 textStyle = styles.incorrect;
+//             }
+//         }
+
+//         return (
+//             <div className={css(styles.text, textStyle)}>
+//                 {this._renderText()}
+//             </div>
+//         );
+//     }
+// }
 
 const styles = StyleSheet.create({
     text: {
