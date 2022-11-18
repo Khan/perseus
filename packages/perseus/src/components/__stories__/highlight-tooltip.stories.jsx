76df95a207b94fb7319e25ebd252c27dec6c7e1b
Nicole Watts <nicole@khanacademy.org>
2022-11-18 13:10:51 -0700

    wip: add storybook skeleton

---
+++ packages/perseus/src/components/__stories__/highlight-tooltip.stories.jsx
@@ -0,0 +1,12 @@
+// @flow
+
+import * as React from "react";
+
+import * as IconPaths from "../../icon-paths.js";
+import IconComponent from "../icon.jsx";
+
+export default {
+    title: "Perseus/Components/Highlight Toolip",
+};
+
+export const Main = (args: any): React.Node => <p>Hello</p>;
