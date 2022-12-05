31374cd4f27e5fc4afdfc3665aa03516d4febb68
Matthew <matthewcurtis@khanacademy.org>
2022-12-05 09:44:50 -0600

    Add basic interactive story for numeric-input (#327)
    ## Summary:
    Just kind of an outline of an interactive story. There's not a whole lot to the widget that I could find, but I figured this would give us an example of how to render the component while providing _some_ interactivity and providing Storybook Action feedback.
    
    Issue: LP-13032
    
    ## Test plan:
    everything should be the same, now with a new story
    
    <img width="797" alt="Screen Shot 2022-12-05 at 9 36 00 AM" src="https://user-images.githubusercontent.com/16308368/205677854-c7e59eea-7c40-417d-b9f9-bf0b7e1db7c4.png">
    
    Author: handeyeco
    
    Reviewers: jeremywiebe
    
    Required Reviewers:
    
    Approved By: jeremywiebe
    
    Checks: ✅ codecov/project, ✅ Lint, Flow, and Test (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ Cypress Coverage (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ Jest Coverage (ubuntu-latest, 16.x), ✅ Check for .changeset file (ubuntu-latest, 16.x), ✅ gerald, ✅ gerald, ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ Cypress Coverage (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ Check for .changeset file (ubuntu-latest, 16.x), ✅ Jest Coverage (ubuntu-latest, 16.x), ✅ gerald, ✅ Lint, Flow, and Test (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ Jest Coverage (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ Lint, Flow, and Test (ubuntu-latest, 16.x), ✅ Check for .changeset file (ubuntu-latest, 16.x), ✅ gerald, ❌ Cypress Coverage (ubuntu-latest, 16.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/327

--- packages/perseus/src/widgets/__stories__/numeric-input.stories.jsx
+++ packages/perseus/src/widgets/__stories__/numeric-input.stories.jsx
@@ -1,19 +1,60 @@
 // @flow
+import {action} from "@storybook/addon-actions";
 import * as React from "react";
 
 import {RendererWithDebugUI} from "../../../../../testing/renderer-with-debug-ui.jsx";
 import {question1} from "../__testdata__/numeric-input_testdata.js";
+import {NumericInput} from "../numeric-input.jsx";
 
-type StoryArgs = {||};
+type StoryArgs = {|
+    currentValue: string,
+|};
 
 type Story = {|
     title: string,
+    args: StoryArgs,
 |};
+
+function generateProps(overwrite) {
+    const base = {
+        alignment: "",
+        answers: [],
+        containerSizeClass: "medium",
+        isLastUsedWidget: true,
+        currentValue: "",
+        problemNum: 0,
+        reviewModeRubric: {
+            answers: [],
+            labelText: "",
+            size: "medium",
+            coefficient: false,
+            static: false,
+        },
+        static: false,
+        widgetId: "widgetId",
+        findWidgets: action("findWidgets"),
+        onBlur: action("onBlur"),
+        onChange: action("onChange"),
+        onFocus: action("onFocus"),
+        trackInteraction: action("trackInteraction"),
+    };
+
+    return {...base, ...overwrite};
+}
 
 export default ({
     title: "Perseus/Widgets/NumericInput",
+    args: {
+        currentValue: "8675309",
+    },
 }: Story);
 
-export const Question1 = (args: StoryArgs): React.Node => {
+export const Question1 = (): React.Node => {
     return <RendererWithDebugUI question={question1} />;
+};
+
+export const Interactive = (args: StoryArgs): React.Node => {
+    const props = generateProps(args);
+
+    return <NumericInput {...props} />;
 };
