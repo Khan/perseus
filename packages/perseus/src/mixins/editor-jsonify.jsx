c710f4091ac5a6f335a206130222c9e5a27fc8e3
Tamara Bozich <tamara.bozich_c@khanacademy.org>
2022-12-07 09:38:43 -0500

    removes underscore import and simplifies function;

--- packages/perseus/src/mixins/editor-jsonify.jsx
+++ packages/perseus/src/mixins/editor-jsonify.jsx
@@ -1,12 +1,11 @@
 // @flow
-import _ from "underscore";
 
-import WIDGET_PROP_BLACKLIST from "./widget-prop-blacklist.js";
+import {removeDenylistProps} from "./widget-prop-denylist.js";
 
 const EditorJsonify = {
     serialize: function (): $FlowFixMe {
         // Omit props that get passed to all widgets
-        return _.omit(this.props, WIDGET_PROP_BLACKLIST);
+        return removeDenylistProps(this.props);
     },
 };
 
