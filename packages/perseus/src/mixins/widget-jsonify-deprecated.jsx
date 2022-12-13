09fd500ab85e38d6b67adb047775063f6929ab17
Tamara Bozich <tamara.bozich_c@khanacademy.org>
2022-12-13 14:30:05 -0500

    reverts another change not needed on this PR

--- packages/perseus/src/mixins/widget-jsonify-deprecated.jsx
+++ packages/perseus/src/mixins/widget-jsonify-deprecated.jsx
@@ -3,18 +3,20 @@
  * just returns all the widget's props rather than picking out those which were
  * input by the user.
  */
-import {removeDenylistProps} from "./widget-prop-blacklist.js";
+import _ from "underscore";
+
+import WIDGET_PROP_BLACKLIST from "./widget-prop-blacklist.js";
 
 const WidgetJsonifyDeprecated = {
-    getUserInput: function (): {...} {
+    getUserInput: function (): $FlowFixMe {
         // Omit props that get passed to all widgets
-        return removeDenylistProps(this.props);
+        return _.omit(this.props, WIDGET_PROP_BLACKLIST);
     },
 
     // Static version of `WidgetJsonifyDeprecated.getUserInput`
-    getUserInputFromProps: function (props: {...}): {...} {
+    getUserInputFromProps: function (props: $FlowFixMe): $FlowFixMe {
         // Omit props that get passed to all widgets
-        return removeDenylistProps(props);
+        return _.omit(props, WIDGET_PROP_BLACKLIST);
     },
 };
 
