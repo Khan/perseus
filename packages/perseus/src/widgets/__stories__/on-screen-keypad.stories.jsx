b3d9a0a779717aa7fd09829ea99de3b7fb2f6fe4
Kevin Barabash <kevinb@khanacademy.org>
2023-01-30 18:40:43 -0500

    Create an demo story show how to integrate the on-screen keypad with ItemRenderer

---
+++ packages/perseus/src/widgets/__stories__/on-screen-keypad.stories.jsx
@@ -0,0 +1,122 @@
+// @flow
+import {Keypad} from "@khanacademy/math-input";
+import {View} from "@khanacademy/wonder-blocks-core";
+import {StyleSheet} from "aphrodite";
+import * as React from "react";
+
+import {ItemRenderer} from "../../index.js";
+import KeypadContext from "../../keypad-context.js";
+import {expressionItem3} from "../__testdata__/expression_testdata.js";
+
+type StoryArgs = {||};
+
+type Story = {|
+    title: string,
+|};
+
+export default ({
+    title: "Perseus/Demos/On Screen Keypad",
+}: Story);
+
+const Content = (): React.Node => {
+    return (
+        <KeypadContext.Consumer>
+            {({keypadElement, setRenderer, scrollableElement}) => (
+                <>
+                    <ItemRenderer
+                        ref={setRenderer}
+                        problemNum={0}
+                        apiOptions={{
+                            customKeypad: true,
+                            onFocusChange: (
+                                newFocusPath,
+                                oldFocusPath,
+                                keypadElement,
+                                focusedElement,
+                            ) => {},
+                        }}
+                        item={expressionItem3}
+                        savedState={null}
+                    />
+                    <div id="workarea" />
+                    <div id="hintsarea" />
+                </>
+            )}
+        </KeypadContext.Consumer>
+    );
+};
+
+const Footer = (): React.Node => {
+    // TODO: get this from problemProgress from the exercise state store.
+
+    return (
+        <View
+            style={[
+                styles.keypadContainer,
+                {
+                    height: 240,
+                },
+            ]}
+        >
+            <KeypadContext.Consumer>
+                {({keypadElement, setKeypadElement, renderer}) => (
+                    <Keypad
+                        onElementMounted={setKeypadElement}
+                        onDismiss={() => renderer && renderer.blur()}
+                        style={styles.keypad}
+                    />
+                )}
+            </KeypadContext.Consumer>
+        </View>
+    );
+};
+
+const Demo = () => {
+    const [keypadElement, setKeypadElement] = React.useState(null);
+    const [renderer, setRenderer] = React.useState(null);
+    const [scrollableElement, setScrollableElement] = React.useState(
+        document.body,
+    );
+
+    return (
+        <KeypadContext.Provider
+            value={{
+                setKeypadElement,
+                keypadElement,
+                setRenderer,
+                renderer,
+                setScrollableElement,
+                scrollableElement,
+            }}
+        >
+            <Content />
+            <Footer />
+        </KeypadContext.Provider>
+    );
+};
+
+export const CustomKeypad = (args: StoryArgs): React.Node => <Demo />;
+
+// export const ExpressionItem3 = (args: StoryArgs): React.Node => {
+//     return <RendererWithDebugUI question={expressionItem3.question} />;
+// };
+
+const styles = StyleSheet.create({
+    keypad: {
+        position: "absolute",
+        // The keypad itself needs to respond to events even though
+        // we've set its container to pointer-events: none;
+        pointerEvents: "all",
+    },
+    keypadContainer: {
+        position: "absolute",
+        width: "100%",
+        bottom: 0,
+        // Hide the overflow of this container so when the keypad is
+        // dismissed, it doesn't overlap the toolbar.
+        overflow: "hidden",
+        // Prevent container from swallowing events that the exercise
+        // below it needs to respond to.
+        pointerEvents: "none",
+    },
+});
