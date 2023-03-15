c611db67e968f81ee7123e708b1d6494a409cddc
Jeremy Wiebe <jeremy@khanacademy.org>
2023-03-15 09:36:04 -0700

    Add comments to publish_snapshot.sh

--- utils/publish-snapshot.sh
+++ utils/publish-snapshot.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+# This script uses the `changeset` tool to version each package as a snapshot
+# release (with an version suffix representing the PR the snapshot came from)
+# and then publishes that release to npm with a tag named after the PR.
+#
+# It is designed to be run in a Github Action, and by default will abort if the
+# `CI`, `GITHUB_EVENT_NAME`, or `GITHUB_REF` environment variables are unset.
+
+
 # https://www.gnu.org/software/bash/manual/bash.html#The-Set-Builtin
 set -e # Exit immediately if a command exits with a non-zero status.
 set -o pipefail
 set -u # Treat unset variables as an error when substituting.
-
-# This script uses the `changeset` tool to version each package as a snapshot
-# release and then publishes that release to npm with a tag named after the PR.
-#
-# It is designed to be run in a Github Action, and by default will abort if the
-# `CI`, `GITHUB_EVENT_NAME`, or `GITHUB_REF` environment variables are unset.
 
 # Identifies the path that the script is in (http://stackoverflow.com/a/246128/11807)
 MYPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -73,9 +75,24 @@ yarn build
 yarn extract-strings
 
 # Now version the packages and publish a snapshot
+# By using the `--snapshot` option we are asking Changeset to version the
+# packages using a snapshot release. We add the formatted PR number as a prefix
+# (that's what the option to `--snapshot` signifies. This results in a version
+# number that looks like this: "0.0.0-PR423-20230314222716"
+# The trailing numbers are a timestamp of when the version was made.
 yarn changeset version --snapshot "$PR_NUMBER"
+
+# Now we publish this snapshot version to npm. We use `--tag` to tag this
+# version in npm. npm supports any number of tags; in fact, when you `yarn add
+# xyz` you are really saying `yarn add xyz@latest` (where latest is a special
+# tag that always exists).
+# You can see this for yourself by running:
+# `yarn info react --json | jq '.data["dist-tags"]'
 yarn changeset publish --no-git-tag --tag "${PR_NUMBER}"
 
+# Now we export the npm tag name so that later Github Action steps have access
+# to this value in the form of:
+# `steps.<name-of-step-that-calls-this-script>.outputs.npm_snapshot_tag`
 echo "npm_snapshot_tag=$PR_NUMBER" >> "$GITHUB_OUTPUT"
 
 # Now throw away all local changes (we've published a snapshot and that's all
