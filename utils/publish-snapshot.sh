fd4c6fb39159554343aef4a8bbc4b066b1b759f6
Jeremy Wiebe <jeremy@khanacademy.org>
2023-03-16 15:50:20 -0700

    Publish npm snapshot on pull request create/update (#413)
    ## Summary:
    
    This PR introduces a new Github Action job to publish an npm snapshot each time a PR is created/updated. The intent is to make it as easy as possible to test Perseus changes in other projects. 
    
    The snapshot publish follows a very similar path to the `publish:ci` yarn script except that it publishes the packages under a tag named with the pattern `PR${pull_reqeust_number` (ie. `PR522`). This allows us to easily install the published packages by installing them using a command such as `yarn add @khanacademy/perseus@PR522`. 
    
    ### npm tags 
    
    > Tags can be used to provide an alias instead of version numbers.
    > 
    > For example, a project might choose to have multiple streams of development and use a different tag for each stream, e.g., stable, beta, dev, canary.
    > 
    > By default, the latest tag is used by npm to identify the current version of a package, and npm install <pkg> (without any @<version> or @<tag> specifier) installs the latest tag. Typically, projects only use the latest tag for stable release versions, and use other tags for unstable versions such as prereleases.
    > 
    > The next tag is used by some projects to identify the upcoming version.
    > 
    > Other than latest, no tag has any special significance to npm itself.
    
    ### Commenting 
    
    This action will comment on the PR if it is able to publish a snapshot or not. I'm not able to check the success comment here because we currently don't have any change sets on `main`.  I'll keep an eye on this as other PRs land. Here's the failure comment though. 
    
    <img width="450" alt="image" src="https://user-images.githubusercontent.com/77138/225444144-7821be8f-c1ba-4602-bfa1-dc07848d6d4e.png">
    
    
    https://docs.npmjs.com/cli/v9/commands/npm-dist-tag#purpose
    
    Issue: LC-590
    
    ## Test plan:
    
    - Land this PR
    - Start a new PR and make some changes to Perseus (and one or two other packages in this repo). 
    - Open a new PR
    - Wait until the new `publish_snapshot` job has completed and verify it did the right thing(s)
    - Update the PR with new changes
    - Ensure that the `publish_snapshot` job re-published and overwrote the existing npm tag
    
    Author: jeremywiebe
    
    Reviewers: kevinbarabash, handeyeco, jeremywiebe, jeresig, jandrade
    
    Required Reviewers:
    
    Approved By: kevinbarabash
    
    Checks: ✅ codecov/project, ✅ Publish npm snapshot (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ Jest Coverage (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ Lint, Flow, and Test (ubuntu-latest, 16.x), ✅ Check for .changeset file (ubuntu-latest, 16.x), ✅ Cypress Coverage (ubuntu-latest, 16.x), ✅ gerald, ✅ gerald, ✅ Publish npm snapshot (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ Lint, Flow, and Test (ubuntu-latest, 16.x), ✅ Jest Coverage (ubuntu-latest, 16.x), ✅ Check for .changeset file (ubuntu-latest, 16.x), ✅ gerald, ❌ Cypress Coverage (ubuntu-latest, 16.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/413

---
+++ utils/publish-snapshot.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+
+# This script uses the `changeset` tool to version each package as a snapshot
+# release (with an version suffix representing the PR the snapshot came from)
+# and then publishes that release to npm with a tag named after the PR.
+#
+# It is designed to be run in a Github Action, and by default will abort if the
+# `CI`, `GITHUB_EVENT_NAME`, or `GITHUB_REF` environment variables are unset.
+
+# https://www.gnu.org/software/bash/manual/bash.html#The-Set-Builtin
+set -e # Exit immediately if a command exits with a non-zero status.
+set -o pipefail
+set -u # Treat unset variables as an error when substituting.
+
+# Identifies the path that the script is in (http://stackoverflow.com/a/246128/11807)
+MYPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# ROOT is the root directory of our project.
+ROOT="$MYPATH/.."
+
+pushd "$ROOT"
+
+if [ -z ${CI+CI_UNSET} ]; then
+    echo "CI environment variable is unset. Exiting!"
+    exit 1
+fi
+
+if [ -z ${GITHUB_EVENT_NAME+GITHUB_EVENT_NAME_UNSET} ]; then
+    echo "GITHUB_EVENT_NAME environment variable is unset. Exiting!"
+    exit 1
+fi
+
+if [ -z ${GITHUB_REF+GITHUB_REF_UNSET} ]; then
+    echo "GITHUB_REF environment variable is unset. Exiting!"
+    exit 1
+fi
+
+if [[
+    "$GITHUB_EVENT_NAME" != "workflow_dispatch" \
+    && "$GITHUB_EVENT_NAME" != "pull_request"
+]]; then
+    exit
+fi
+
+# Check if we need to do any work
+# NOTE: changeset's --output flag has a bug where it always prefixes whatever
+# you pass to it with `cwd` (the code does `path.join(cwd, outputParam)`). So
+# we just allow it to write the file in our local dir (although I would prefer
+# to use `mktemp`).
+yarn changeset status --verbose --output changeset-status.json
+# We use jq to check if the json outpu has changesets. If not, we exit the
+# process (but not with a non-zero exit status because we don't want to cause
+# the github action to exit with a failure status).
+jq -e '.releases | length | if . > 0 then . else "No changesets found" | halt_error(1) end' \
+    < "changeset-status.json" || \
+    exit 0
+
+
+echo "Running for $GITHUB_EVENT_NAME @ $GITHUB_REF"
+
+# Example GITHUB_REF
+# refs/pull/:prNumber/merge
+if [[ "$GITHUB_REF" =~ refs/pull/([[:digit:]]+)/merge ]]; then
+    echo "Found PR #${BASH_REMATCH[1]}"
+    PR_NUMBER="PR${BASH_REMATCH[1]}"
+else
+    echo "Pull Request number not found in ref. Exiting!"
+    exit 1
+fi
+
+# publish:ci steps
+node "$ROOT/utils/pre-publish-check-ci.js"
+
+if ! git diff --stat --exit-code HEAD; then
+    echo "Git repo is dirty. This is unexpected when running in CI."
+    echo "Please review the logs leading up to this error to figure out why " \
+         "the repo was touched."
+    exit 1
+fi
+
+yarn build
+yarn extract-strings
+
+# Now version the packages and publish a snapshot
+# By using the `--snapshot` option we are asking Changeset to version the
+# packages using a snapshot release. We add the formatted PR number as a prefix
+# (that's what the option to `--snapshot` signifies. This results in a version
+# number that looks like this: "0.0.0-PR423-20230314222716"
+# The trailing numbers are a timestamp of when the version was made.
+yarn changeset version --snapshot "$PR_NUMBER"
+
+# Now we publish this snapshot version to npm. We use `--tag` to tag this
+# version in npm. npm supports any number of tags; in fact, when you `yarn add
+# xyz` you are really saying `yarn add xyz@latest` (where latest is a special
+# tag that always exists).
+# You can see this for yourself by running:
+# `yarn info react --json | jq '.data["dist-tags"]'
+yarn changeset publish --no-git-tag --tag "${PR_NUMBER}"
+
+# Now we export the npm tag name so that later Github Action steps have access
+# to this value in the form of:
+# `steps.<name-of-step-that-calls-this-script>.outputs.npm_snapshot_tag`
+echo "npm_snapshot_tag=$PR_NUMBER" >> "$GITHUB_OUTPUT"
+
+# Now throw away all local changes (we've published a snapshot and that's all
+# we needed).
+git reset --hard
