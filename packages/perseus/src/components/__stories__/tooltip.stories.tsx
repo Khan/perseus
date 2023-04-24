240d3539b07a4bef9288651f6ea439a2c4857308
Jeremy Wiebe <jeremy@khanacademy.org>
2023-04-24 11:41:28 -0700

    Type and modernize tooltip.tsx

---
+++ packages/perseus/src/components/__stories__/tooltip.stories.tsx
@@ -0,0 +1,53 @@
+import {View} from "@khanacademy/wonder-blocks-core";
+import {Body} from "@khanacademy/wonder-blocks-typography";
+import * as React from "react";
+
+import Tooltip from "../tooltip";
+
+import type {Meta} from "@storybook/react";
+
+const meta: Meta<typeof Tooltip> = {
+    title: "Perseus/Components/Tooltip",
+};
+
+export const Shown = () => {
+    return (
+        <View style={{margin: "20px"}}>
+            Hover over{" "}
+            <Tooltip
+                show={true}
+                horizontalPosition="left"
+                horizontalAlign="left"
+                verticalPosition="bottom"
+            >
+                <span>this</span>
+                <View style={{backgroundColor: "white"}}>
+                    You can read so much more if you want...
+                </View>
+            </Tooltip>{" "}
+            to see more information
+        </View>
+    );
+};
+
+export const Hidden = () => {
+    return (
+        <View style={{margin: "20px"}}>
+            Hover over{" "}
+            <Tooltip
+                show={false}
+                horizontalPosition="left"
+                horizontalAlign="left"
+                verticalPosition="bottom"
+            >
+                <span>this</span>
+                <View style={{backgroundColor: "white"}}>
+                    You can read so much more if you want...
+                </View>
+            </Tooltip>{" "}
+            to see more information
+        </View>
+    );
+};
+
+export default meta;
