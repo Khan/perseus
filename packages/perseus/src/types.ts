8493a6f6cfb701c2511d1f65e7f85fc0305d5d55
Kevin Barabash <kevinb@khanacademy.org>
2023-04-27 17:06:27 -0400

    Require all enums to have initializers

--- packages/perseus/src/types.ts
+++ packages/perseus/src/types.ts
@@ -352,7 +352,7 @@ export type APIOptionsWithDefaults = Readonly<
 export type LinterContextProps = {
     contentType: string;
     highlightLint: boolean;
-    paths: ReadonlyArray<string>;
+    path: ReadonlyArray<string>;
     stack: ReadonlyArray<string>;
     // additional properties can be added to the context by widgets
 };
