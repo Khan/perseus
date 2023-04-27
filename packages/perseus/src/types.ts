cf1c1d185835da6b2408ac31270039af77a62f5c
Kevin Barabash <kevinb@khanacademy.org>
2023-04-27 17:13:53 -0400

    revert spurious change

--- packages/perseus/src/types.ts
+++ packages/perseus/src/types.ts
@@ -352,7 +352,7 @@ export type APIOptionsWithDefaults = Readonly<
 export type LinterContextProps = {
     contentType: string;
     highlightLint: boolean;
-    path: ReadonlyArray<string>;
+    paths: ReadonlyArray<string>;
     stack: ReadonlyArray<string>;
     // additional properties can be added to the context by widgets
 };
