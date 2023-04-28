5f381787dc6c95fdd13fbeb1469766dd2aa0d1f7
Jeremy Wiebe <jeremy@khanacademy.org>
2023-04-28 13:08:06 -0700

    Streamline exporting Keys

--- packages/math-input/src/index.ts
+++ packages/math-input/src/index.ts
@@ -4,12 +4,10 @@
 
 import "../less/main.less";
 
-import Keys from "./data/keys";
-
 export {CursorContext} from "./components/input/cursor-contexts";
 export {default as KeypadInput} from "./components/input/math-input";
 export {keypadElementPropType} from "./components/prop-types";
 export {default as Keypad} from "./components/provided-keypad";
 export {default as KeyConfigs} from "./data/key-configs";
-export {Keys};
+export {default as Keys} from "./data/keys";
 export {KeyType, KeypadType} from "./enums";
