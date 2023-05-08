3992002a36aee017f881cd1963ac0e08121a2da8
Ned Redmond <edward@khanacademy.org>
2023-05-08 10:57:21 -0400

    create dynamic KeyPad story

--- packages/math-input/src/components/keypad/keypad.stories.tsx
+++ packages/math-input/src/components/keypad/keypad.stories.tsx
@@ -1,8 +1,9 @@
 import {action} from "@storybook/addon-actions";
 import {INITIAL_VIEWPORTS} from "@storybook/addon-viewport";
+import {ComponentStory} from "@storybook/react";
 import * as React from "react";
 
-import Keypad from "./index";
+import Keypad, {Props as KeypadProps} from "./index";
 
 export default {
     title: "Full Keypad",
@@ -12,12 +13,29 @@ export default {
         },
         viewport: {defaultViewport: "iphone6", viewports: INITIAL_VIEWPORTS},
     },
+    component: Keypad,
+    argTypes: {
+        trigonometry: {control: "boolean"},
+        preAlgebra: {control: "boolean"},
+        logarithms: {control: "boolean"},
+        relations: {control: "boolean"},
+        advancedRelations: {control: "boolean"},
+    },
 };
 
-export const PreAlgebra = (): React.ReactElement => (
-    <Keypad onClickKey={action("onClickKey")} preAlgebra />
-);
+const Template: ComponentStory<typeof Keypad> = (
+    args: KeypadProps,
+): React.ReactElement => <Keypad {...args} onClickKey={action("onClickKey")} />;
 
-export const Trigonometry = (): React.ReactElement => (
-    <Keypad onClickKey={action("onClickKey")} preAlgebra trigonometry />
-);
+export const Default = Template.bind({});
+
+export const PreAlgebra = Template.bind({});
+PreAlgebra.args = {
+    preAlgebra: true,
+};
+
+export const Trigonometry = Template.bind({});
+Trigonometry.args = {
+    preAlgebra: true,
+    trigonometry: true,
+};
