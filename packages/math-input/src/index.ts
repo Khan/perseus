c991e3fff992c996ad580f3c5148e28806b411bf
Matthew Curtis <matthewcurtis@khanacademy.org>
2023-05-11 14:18:29 -0500

    rename per Ned's request

--- packages/math-input/src/index.ts
+++ packages/math-input/src/index.ts
@@ -7,7 +7,7 @@ import "../less/main.less";
 export {CursorContext} from "./components/input/cursor-contexts";
 export {default as KeypadInput} from "./components/input/math-input";
 export {keypadElementPropType} from "./components/prop-types";
-export {default as Keypad} from "./components/keypad-v1/provided-keypad";
+export {default as Keypad} from "./components/keypad-legacy/provided-keypad";
 export {default as KeyConfigs} from "./data/key-configs";
 export {default as Keys} from "./data/keys";
 export {KeyType, KeypadType} from "./enums";
