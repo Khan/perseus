4f59cca5a9bf4106ca8c6cb0347c9633d0a08bba
Ned Redmond <edward@khanacademy.org>
2023-05-11 12:12:35 -0400

    fix React dom errors in `ButtonAssets` as well as warnings in some stories

--- packages/math-input/src/components/keypad/button.stories.tsx
+++ packages/math-input/src/components/keypad/button.stories.tsx
@@ -9,15 +9,17 @@ import {KeypadButton, KeypadButtonProps} from "./keypad-page-items";
 
 export default {
     title: "Keypad Button",
+    args: {
+        keyConfig: KeyConfigs["PLUS"],
+        tintColor: "#F6F6F7",
+    },
     argTypes: {
         keyConfig: {
             control: "select",
             options: {...KeyConfigs},
-            defaultValue: KeyConfigs[Keys.PLUS],
         },
         tintColor: {
             control: "color",
-            defaultValue: "#F6F6F7",
         },
     },
 };
@@ -58,6 +60,7 @@ export const AllButtons: ComponentStory<typeof KeypadButton> = ({
     >
         {Object.keys(KeyConfigs).map((key) => (
             <div
+                key={key}
                 style={{
                     width: 58,
                     margin: "auto",
