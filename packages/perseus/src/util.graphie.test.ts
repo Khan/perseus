f6dc62d1ade83e00881ccddbb7d40c67c984e594
Nicole Watts <nicole@khanacademy.org>
2023-08-07 14:25:04 -0600

    wip: start factoring out graphie pieces

---
+++ packages/perseus/src/util.graphie.test.ts
@@ -0,0 +1,19 @@
+import {describe, it} from "@jest/globals";
+
+import {getRealImageUrl} from "./util.graphie";
+
+describe("getRealImageUrl", () => {
+    it("when passed a graphie URL, it transforms the URL to an http url", () => {
+        const input =
+            "web+graphie://ka-perseus-graphie.s3.amazonaws.com/b4a76d480e49f89fddb973c88c53fc301ba0d927";
+        const result =
+            "https://ka-perseus-graphie.s3.amazonaws.com/b4a76d480e49f89fddb973c88c53fc301ba0d927.svg";
+        expect(getRealImageUrl(input)).toBe(result);
+    });
+
+    it("when it's passed an HTTP URL, it returns the URL", () => {
+        const input =
+            "https://cdn.kastatic.org/ka-perseus-images/c633a1d04970211586b413985a7959876ca6c909.png";
+        expect(getRealImageUrl(input)).toBe(input);
+    });
+});
