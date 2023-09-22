c70bfbc874a5c9b8894e0606a166bf01911acdda
Matthew Curtis <matthewcurtis@khanacademy.org>
2023-09-22 12:12:45 -0500

    add mobile expression tests

---
+++ packages/perseus/src/widgets/__tests__/expression-mobile.test.tsx
@@ -0,0 +1,350 @@
+import "@testing-library/jest-dom";
+import {KeypadContext, MobileKeypad} from "@khanacademy/math-input";
+import {RenderStateRoot} from "@khanacademy/wonder-blocks-core";
+import {
+    fireEvent,
+    render,
+    screen,
+    waitFor,
+    within,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import * as React from "react";
+
+import {
+    testDependencies,
+    testDependenciesV2,
+} from "../../../../../testing/test-dependencies";
+import * as Dependencies from "../../dependencies";
+import WrappedServerItemRenderer from "../../server-item-renderer";
+import {registerWidget} from "../../widgets";
+import {expressionItem2} from "../__testdata__/expression.testdata";
+import ExpressionExport from "../expression";
+
+import type {KeypadAPI} from "@khanacademy/math-input";
+
+function RendererWithContext() {
+    return (
+        <KeypadContext.Consumer>
+            {({keypadElement}) => {
+                return (
+                    <WrappedServerItemRenderer
+                        apiOptions={{
+                            isMobile: true,
+                            customKeypad: true,
+                            useV2Keypad: true,
+                        }}
+                        item={expressionItem2}
+                        problemNum={0}
+                        reviewMode={false}
+                        dependencies={testDependenciesV2}
+                        keypadElement={keypadElement}
+                    />
+                );
+            }}
+        </KeypadContext.Consumer>
+    );
+}
+
+function KeypadWithContext() {
+    return (
+        <KeypadContext.Consumer>
+            {({setKeypadElement}) => {
+                return (
+                    <MobileKeypad
+                        onElementMounted={setKeypadElement}
+                        onDismiss={() => {}}
+                        onAnalyticsEvent={async () => {}}
+                        useV2Keypad
+                    />
+                );
+            }}
+        </KeypadContext.Consumer>
+    );
+}
+
+function ConnectedRenderer() {
+    const [keypadElement, setKeypadElement] =
+        React.useState<KeypadAPI | null>();
+    const [renderer, setRenderer] = React.useState<any>(null);
+    const [scrollableElement, setScrollableElement] =
+        React.useState<HTMLElement | null>();
+
+    return (
+        <RenderStateRoot>
+            <KeypadContext.Provider
+                value={{
+                    setKeypadElement,
+                    keypadElement,
+                    setRenderer,
+                    renderer,
+                    setScrollableElement,
+                    scrollableElement,
+                }}
+            >
+                <RendererWithContext />
+                <KeypadWithContext />
+            </KeypadContext.Provider>
+        </RenderStateRoot>
+    );
+}
+
+describe("expression mobile", () => {
+    beforeAll(() => {
+        registerWidget("expression", ExpressionExport);
+    });
+
+    beforeEach(() => {
+        jest.spyOn(Dependencies, "getDependencies").mockReturnValue(
+            testDependencies,
+        );
+    });
+
+    afterEach(() => {
+        // The Renderer uses a timer to wait for widgets to complete rendering.
+        // If we don't spin the timers here, then the timer fires in the test
+        // _after_ and breaks it because we do setState() in the callback,
+        // and by that point the component has been unmounted.
+        jest.runOnlyPendingTimers();
+    });
+
+    it("renders input", () => {
+        render(<ConnectedRenderer />);
+
+        expect(
+            screen.getByLabelText(
+                "Math input box Tap with one or two fingers to open keyboard",
+            ),
+        ).toBeInTheDocument();
+    });
+
+    it("shows the keypad after input interaction", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+    });
+
+    it("is possible to use the keypad", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+
+        userEvent.click(screen.getByRole("button", {name: "1"}));
+
+        // MathQuill is problematic,
+        // this is the only way I know how to test the "input"
+        const mathquillInput =
+            // eslint-disable-next-line testing-library/no-node-access
+            document.getElementsByClassName("mq-root-block")[0] as HTMLElement;
+        const span1 = within(mathquillInput).getByText("1");
+
+        expect(span1).toBeVisible();
+    });
+
+    it("is possible to type many numbers", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+
+        const testNumbers = [8, 6, 7, 5, 3, 0, 9];
+        testNumbers.forEach((num) => {
+            userEvent.click(screen.getByRole("button", {name: `${num}`}));
+        });
+
+        // MathQuill is problematic,
+        // this is the only way I know how to test the "input"
+        const mathquillInput =
+            // eslint-disable-next-line testing-library/no-node-access
+            document.getElementsByClassName("mq-root-block")[0] as HTMLElement;
+
+        testNumbers.forEach((num) => {
+            const span = within(mathquillInput).getByText(`${num}`);
+
+            expect(span).toBeVisible();
+        });
+    });
+
+    it("can handle symbols", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+
+        userEvent.click(screen.getByRole("button", {name: "4"}));
+        userEvent.click(screen.getByRole("button", {name: "2"}));
+        userEvent.click(screen.getByRole("button", {name: "Percent"}));
+
+        // MathQuill is problematic,
+        // this is the only way I know how to test the "input"
+        const mathquillInput =
+            // eslint-disable-next-line testing-library/no-node-access
+            document.getElementsByClassName("mq-root-block")[0] as HTMLElement;
+        const span4 = within(mathquillInput).getByText("4");
+        const span2 = within(mathquillInput).getByText("2");
+        const spanPercent = within(mathquillInput).getByText("%");
+
+        expect(span4).toBeVisible();
+        expect(span2).toBeVisible();
+        expect(spanPercent).toBeVisible();
+    });
+
+    it("can do arithmetic", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+
+        userEvent.click(screen.getByRole("button", {name: "4"}));
+        userEvent.click(screen.getByRole("button", {name: "Plus"}));
+        userEvent.click(screen.getByRole("button", {name: "2"}));
+
+        // MathQuill is problematic,
+        // this is the only way I know how to test the "input"
+        const mathquillInput =
+            // eslint-disable-next-line testing-library/no-node-access
+            document.getElementsByClassName("mq-root-block")[0] as HTMLElement;
+        const span4 = within(mathquillInput).getByText("4");
+        const spanPlus = within(mathquillInput).getByText("+");
+        const span2 = within(mathquillInput).getByText("2");
+
+        expect(span4).toBeVisible();
+        expect(spanPlus).toBeVisible();
+        expect(span2).toBeVisible();
+    });
+
+    it("can navigate to operators page", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+
+        userEvent.click(screen.getByRole("tab", {name: "Operators"}));
+        expect(screen.getByRole("button", {name: "Equals sign"})).toBeVisible();
+    });
+
+    it("can navigate to geometry page", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+
+        userEvent.click(screen.getByRole("tab", {name: "Geometry"}));
+        expect(screen.getByRole("button", {name: "Sine"})).toBeVisible();
+    });
+
+    it("can navigate to extras page", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+
+        userEvent.click(screen.getByRole("tab", {name: "Extras"}));
+        expect(screen.getByRole("button", {name: "x"})).toBeVisible();
+    });
+
+    it("can write a full equation", async () => {
+        render(<ConnectedRenderer />);
+
+        const input = screen.getByLabelText(
+            "Math input box Tap with one or two fingers to open keyboard",
+        );
+
+        fireEvent.touchStart(input);
+
+        // waiting because `visibility` is animated
+        await waitFor(() =>
+            expect(screen.getByRole("button", {name: "1"})).toBeVisible(),
+        );
+
+        userEvent.click(screen.getByRole("tab", {name: "Extras"}));
+        userEvent.click(screen.getByRole("button", {name: "x"}));
+        userEvent.click(screen.getByRole("tab", {name: "Operators"}));
+        userEvent.click(screen.getByRole("button", {name: "Equals sign"}));
+        userEvent.click(screen.getByRole("tab", {name: "Geometry"}));
+        userEvent.click(screen.getByRole("button", {name: "Sine"}));
+        userEvent.click(screen.getByRole("tab", {name: "Numbers"}));
+        userEvent.click(screen.getByRole("button", {name: "9"}));
+
+        // MathQuill is problematic,
+        // this is the only way I know how to test the "input"
+        const mathquillInput =
+            // eslint-disable-next-line testing-library/no-node-access
+            document.getElementsByClassName("mq-root-block")[0] as HTMLElement;
+
+        expect(within(mathquillInput).getByText("x")).toBeVisible();
+        expect(within(mathquillInput).getByText("=")).toBeVisible();
+        expect(within(mathquillInput).getByText("s")).toBeVisible();
+        expect(within(mathquillInput).getByText("i")).toBeVisible();
+        expect(within(mathquillInput).getByText("n")).toBeVisible();
+        expect(within(mathquillInput).getByText("(")).toBeVisible();
+        expect(within(mathquillInput).getByText("9")).toBeVisible();
+        expect(within(mathquillInput).getByText(")")).toBeVisible();
+    });
+});
