3cea1a086c9a3fbd5c621b44dc4d472f3bcd9e57
Ioana Crant <ioana@khanacademy.org>
2023-11-07 15:31:32 -0500

    Add "do not track" query parameter to vimeo links in the video widget to ensure tracking/analytics cookies don't get set.
    Issue: https://khanacademy.atlassian.net/browse/INFRA-9620
    
    Test plan:
    - tested that parameter got added in video widget in storybook

--- packages/perseus/src/widgets/__testdata__/video.testdata.ts
+++ packages/perseus/src/widgets/__testdata__/video.testdata.ts
@@ -19,3 +19,43 @@ export const question1: PerseusRenderer = {
     },
     replace: false,
 };
+
+export const question2: PerseusRenderer = {
+    content:
+        "Watch the WMF at Angkor Wat video to find the answer.\n\n[[\u2603 video 1]]\n\n",
+    images: {},
+    widgets: {
+        "video 1": {
+            graded: true,
+            version: {major: 0, minor: 0},
+            static: false,
+            type: "video",
+            options: {
+                static: false,
+                location: "https://player.vimeo.com/video/4754041",
+            },
+            alignment: "block",
+        },
+    },
+    replace: false,
+};
+
+export const question3: PerseusRenderer = {
+    content:
+        "Watch the WMF at Angkor Wat video to find the answer.\n\n[[\u2603 video 1]]\n\n",
+    images: {},
+    widgets: {
+        "video 1": {
+            graded: true,
+            version: {major: 0, minor: 0},
+            static: false,
+            type: "video",
+            options: {
+                static: false,
+                location: "https://player.vimeo.com/video/4754041?h=64fbc32a6e",
+            },
+            alignment: "block",
+        },
+    },
+    replace: false,
+};
