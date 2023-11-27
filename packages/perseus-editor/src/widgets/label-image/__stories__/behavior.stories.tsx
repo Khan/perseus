301d01f8e542487ef0334ca7ab4c765ac24e8a4c
Nicole Watts <nicole@khanacademy.org>
2023-11-27 16:26:06 -0700

    wip: fixup type error

--- packages/perseus-editor/src/widgets/label-image/__stories__/behavior.stories.tsx
+++ packages/perseus-editor/src/widgets/label-image/__stories__/behavior.stories.tsx
@@ -27,5 +27,5 @@ export const Default = (args: StoryArgs): React.ReactElement => {
             setState({...state, ...newState});
         },
     } as const;
-    return <Behavior {...props} />;
+    return <Behavior preferredPopoverDirection="NONE" {...props} />;
 };
