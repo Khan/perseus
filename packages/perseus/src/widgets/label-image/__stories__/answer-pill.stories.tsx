8abd1442376a92abe3f5ca4a2fd77473b08659d1
Ned Redmond <edward@khanacademy.org>
2023-11-29 16:21:18 -0500

    Pill color for correct & incorrect answers (#824)
    ## Summary
    Match marker color for correct & incorrect answers.
    Also disables interaction for correct answers, matching marker behavior.
    
    Isolates component and adds interactive story doc.
    
    Issue: LC-1498
    
    <img width="196" alt="Screenshot 2023-11-29 at 10 28 25 AM" src="https://github.com/Khan/perseus/assets/23404711/7bd33caf-6687-461a-9b56-02ddb0bb7474">
    
    https://github.com/Khan/perseus/assets/23404711/64157cdc-ed97-4546-89e0-a5382ab3d2a5
    
    ## Testing Strategy:
    Fire up storybook and play in the new story
    
    Author: nedredmond
    
    Reviewers: jeremywiebe, nixterrimus
    
    Required Reviewers:
    
    Approved By: jeremywiebe
    
    Checks: ✅ codecov/project, ✅ codecov/patch, ✅ Upload Coverage, ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 16.x), ✅ Publish npm snapshot (ubuntu-latest, 16.x), ✅ Jest Coverage (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ Cypress (ubuntu-latest, 16.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 16.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/824

---
+++ packages/perseus/src/widgets/label-image/__stories__/answer-pill.stories.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+
+import {AnswerPill} from "../answer-pill";
+
+import type {Meta} from "@storybook/react";
+
+const story: Meta<React.ComponentProps<typeof AnswerPill>> = {
+    title: "Perseus/Widgets/Label Image/Answer Pill",
+    component: AnswerPill,
+    render: (args) => <AnswerPill {...args} />,
+    argTypes: {
+        showCorrectness: {
+            options: ["not answered", "correct", "incorrect"],
+            control: {
+                type: "radio",
+            },
+        },
+    },
+};
+export default story;
+
+export const SingleAnswer = {
+    args: {
+        id: "1",
+        selectedAnswers: ["Answer Pill!"],
+        markerRef: null,
+        side: "top",
+        onClick: () => {},
+    },
+};
+
+export const MultipleAnswers = {
+    args: {
+        id: "1",
+        selectedAnswers: ["Answer 1", "Answer 2"],
+        markerRef: null,
+        side: "top",
+        onClick: () => {},
+    },
+};
+
+export const Correct = {
+    args: {
+        id: "1",
+        selectedAnswers: ["Right Answer!"],
+        showCorrectness: "correct",
+        markerRef: null,
+        side: "top",
+        onClick: () => {},
+    },
+};
+
+export const Incorrect = {
+    args: {
+        id: "1",
+        selectedAnswers: ["Wrong Answer!"],
+        showCorrectness: "incorrect",
+        markerRef: null,
+        side: "top",
+        onClick: () => {},
+    },
+};
