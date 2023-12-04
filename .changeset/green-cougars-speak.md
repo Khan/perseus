07537aae870866f4418055b5956e9387f85f72c4
Ned Redmond <edward@khanacademy.org>
2023-12-04 14:47:16 -0500

    Minor editor bugfix for periodic table: uncheck key with table (#835)
    When table option unchecked, ensure key is unchecked.
    This would only have made the setting "sticky" for the exercise and would have had no effect on the published, rendered result.
    
    Issue: LC-1534
    
    Testing strategy:
    Unit tests added. In ZND or after deploy, try:
    - Check table option
    - Check key option
    - Uncheck table option
    - Verify that no "unsaved change" is registered
    - Verify that checking table option again shows unchecked key
    
    Author: nedredmond
    
    Reviewers: jeremywiebe, nedredmond, nixterrimus
    
    Required Reviewers:
    
    Approved By: jeremywiebe
    
    Checks: ❌ codecov/project, ✅ codecov/patch, ✅ Upload Coverage, ✅ Publish npm snapshot (ubuntu-latest, 16.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 16.x), ✅ Extract i18n strings (ubuntu-latest, 16.x), ✅ Cypress (ubuntu-latest, 16.x), ✅ Check builds for changes in size (ubuntu-latest, 16.x), ✅ Jest Coverage (ubuntu-latest, 16.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 16.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 16.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/835

---
+++ .changeset/green-cougars-speak.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/perseus-editor": patch
+---
+
+Unchecking periodic table option unchecks key as well
---
+++ packages/perseus-editor/src/__tests__/items-extras-editors.test.tsx
@@ -0,0 +1,68 @@
+import "@testing-library/jest-dom";
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+
+import ItemExtrasEditor from "../item-extras-editor";
+
+describe("ItemExtrasEditor", () => {
+    it("should render correctly with default props", () => {
+        // Act
+        render(<ItemExtrasEditor onChange={() => {}} />);
+
+        // Assert
+        expect(screen.getByLabelText("Show calculator:")).not.toBeChecked();
+        expect(screen.getByLabelText("Show periodic table:")).not.toBeChecked();
+        expect(
+            screen.queryByText("Include key/legend with periodic table:"),
+        ).not.toBeInTheDocument();
+        expect(
+            screen.getByLabelText("Show z table (statistics):"),
+        ).not.toBeChecked();
+        expect(
+            screen.getByLabelText("Show t table (statistics):"),
+        ).not.toBeChecked();
+        expect(
+            screen.getByLabelText("Show chi-squared table (statistics):"),
+        ).not.toBeChecked();
+    });
+
+    it("should call onChange with updated calculator value", () => {
+        // Arrange
+        const onChangeMock = jest.fn();
+        render(<ItemExtrasEditor calculator={false} onChange={onChangeMock} />);
+        const checkbox = screen.getByLabelText("Show calculator:");
+
+        // Act
+        userEvent.click(checkbox);
+
+        // Assert
+        expect(onChangeMock).toHaveBeenCalledWith({calculator: true});
+    });
+
+    it("should call onChange with updated periodicTableWithKey value when periodicTable is unchecked", () => {
+        // Arrange
+        const onChangeMock = jest.fn();
+        render(
+            <ItemExtrasEditor
+                periodicTable={true}
+                periodicTableWithKey={true}
+                onChange={onChangeMock}
+            />,
+        );
+        const checkbox = screen.getByLabelText("Show periodic table:");
+
+        // Act
+        userEvent.click(checkbox);
+
+        // Assert
+        // visible when periodicTable is checked
+        expect(
+            screen.getByText("Include key/legend with periodic table:"),
+        ).toBeInTheDocument();
+        expect(onChangeMock).toHaveBeenCalledWith({
+            periodicTable: false,
+            periodicTableWithKey: false,
+        });
+    });
+});
