0f0a104623bca19a789490260768361287f1ea07
Matthew Curtis <matthewcurtis@khanacademy.org>
2023-12-20 12:28:23 -0600

    add tests for SorterEditor

---
+++ packages/perseus-editor/src/widgets/__stories__/sorter-editor.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import SorterEditor from "../sorter-editor";
+
+type StoryArgs = Record<any, any>;
+
+type Story = {
+    title: string;
+};
+
+export default {
+    title: "Perseus/Editor/Widgets/Sorter Editor",
+} as Story;
+
+export const Default = (args: StoryArgs): React.ReactElement => {
+    return <SorterEditor onChange={() => {}} />;
+};
