d727a311e16e6c08b661177ad1b80a33e5428339
Matthew Curtis <matthewcurtis@khanacademy.org>
2023-12-20 12:39:07 -0600

    add tests for MatcherEditor

---
+++ packages/perseus-editor/src/widgets/__stories__/matcher-editor.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import MatcherEditor from "../matcher-editor";
+
+type StoryArgs = Record<any, any>;
+
+type Story = {
+    title: string;
+};
+
+export default {
+    title: "Perseus/Editor/Widgets/Matcher Editor",
+} as Story;
+
+export const Default = (args: StoryArgs): React.ReactElement => {
+    return <MatcherEditor onChange={() => {}} />;
+};
