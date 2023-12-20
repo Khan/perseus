94c854cb0e68f02bdb9e5f3f8de6bac71abc683a
Matthew Curtis <matthewcurtis@khanacademy.org>
2023-12-20 11:21:36 -0600

    add tests for NumericInputEditor

---
+++ packages/perseus-editor/src/widgets/__stories__/numeric-input-editor.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import NumericInputEditor from "../numeric-input-editor";
+
+type StoryArgs = Record<any, any>;
+
+type Story = {
+    title: string;
+};
+
+export default {
+    title: "Perseus/Editor/Widgets/NumericInput Editor",
+} as Story;
+
+export const Default = (args: StoryArgs): React.ReactElement => {
+    return <NumericInputEditor onChange={() => {}} />;
+};
