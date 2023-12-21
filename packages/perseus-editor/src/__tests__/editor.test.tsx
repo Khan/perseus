1f0423b8a5867300dab97eefdb2771c3e5d9399d
Jeremy Wiebe <jeremy@khanacademy.org>
2023-12-21 12:07:13 -0800

    Add confirmation before deleting a widget config

--- packages/perseus-editor/src/__tests__/editor.test.tsx
+++ packages/perseus-editor/src/__tests__/editor.test.tsx
@@ -1,115 +1,141 @@
-import {ApiOptions, Dependencies, Widgets} from "@khanacademy/perseus";
+import {
+    ApiOptions,
+    Dependencies,
+    Widgets,
+    widgets,
+    Util,
+} from "@khanacademy/perseus";
 import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import * as React from "react";
 import "@testing-library/jest-dom"; // Imports custom mathers
 
 import {testDependencies} from "../../../../testing/test-dependencies";
-import {wait} from "../../../../testing/wait";
-import {question1} from "../__testdata__/input-number.testdata";
 import Editor from "../editor";
+import ImageEditor from "../widgets/image-editor";
+
+import type {PropsFor} from "@khanacademy/wonder-blocks-core";
+
+const Harnessed = (props: Partial<PropsFor<typeof Editor>>) => {
+    return (
+        <Editor
+            apiOptions={ApiOptions.defaults}
+            onChange={() => {}}
+            content="[[☃ image 1]]"
+            widgets={{
+                "image 1": {
+                    type: "image",
+                    options: {
+                        backgroundImage: {
+                            url: "http://placekitten.com/200/300",
+                        },
+                    },
+                },
+            }}
+            {...props}
+        />
+    );
+};
 
 describe("Editor", () => {
+    beforeAll(() => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const ImageWidget = widgets.find((w) => w.name === "image")!;
+        expect(ImageWidget).toBeDefined();
+        Widgets.registerWidget("image", ImageWidget);
+        Widgets.registerEditors([ImageEditor]);
+    });
+
     beforeEach(() => {
         jest.spyOn(Dependencies, "getDependencies").mockReturnValue(
             testDependencies,
         );
     });
 
-    describe("input-number widget", () => {
-        beforeEach(async () => {
-            const [InputNumberWidget, InputNumberEditor] = await Promise.all([
-                import("@khanacademy/perseus").then((m) => m.InputNumber),
-                import("../widgets/input-number-editor").then((m) => m.default),
-            ]);
-            Widgets.registerWidgets([InputNumberWidget]);
-            Widgets.registerEditors([InputNumberEditor]);
-            jest.useRealTimers();
-        });
+    it("should delete widget if confirmed", async () => {
+        const onChangeMock = jest.fn();
+        jest.spyOn(window, "confirm").mockReturnValue(true);
 
+        // Arrange
+        render(<Harnessed onChange={onChangeMock} />);
+
+        // Act
+        userEvent.click(
+            screen.getByRole("button", {name: "Remove image widget"}),
+        );
+
+        // Assert
+        expect(onChangeMock).toHaveBeenCalledWith({content: ""});
+    });
+
+    it("should NOT delete widget if not confirmed", async () => {
+        const onChangeMock = jest.fn();
+        jest.spyOn(window, "confirm").mockReturnValue(false);
+
+        // Arrange
+        render(<Harnessed onChange={onChangeMock} />);
+
+        // Act
+        userEvent.click(
+            screen.getByRole("button", {name: "Remove image widget"}),
+        );
+
+        // Assert
+        expect(onChangeMock).not.toHaveBeenCalled();
+    });
+
+    describe("input-number widget", () => {
         test("clicking on the widget editor should open it", async () => {
             // Arrange
-            render(
-                <Editor
-                    apiOptions={ApiOptions.defaults}
-                    content={question1.content}
-                    placeholder=""
-                    widgets={question1.widgets}
-                    images={question1.images}
-                    disabled={false}
-                    widgetEnabled={true}
-                    immutableWidgets={false}
-                    showWordCount={true}
-                    warnNoPrompt={true}
-                    warnNoWidgets={true}
-                    onChange={(props) => {}}
-                />,
-            );
-            await wait();
+            render(<Harnessed />);
 
             // Act
             const widgetDisclosure = screen.getByRole("link", {
-                name: "input-number 1",
+                name: "image 1",
             });
             userEvent.click(widgetDisclosure);
-            const correctAnswerInput = screen.getByLabelText("Correct answer:");
 
             // Assert
-            expect(correctAnswerInput).toHaveValue("0.5");
+            const previewImage = screen.getByAltText("Editor preview of image");
+            expect(previewImage).toHaveAttribute(
+                "src",
+                "http://placekitten.com/200/300",
+            );
         });
 
         it("should update values", async () => {
             // Arrange
+            jest.spyOn(Util, "getImageSizeModern").mockResolvedValue([
+                200, 200,
+            ]);
+
             const changeFn = jest.fn();
-            render(
-                <Editor
-                    apiOptions={ApiOptions.defaults}
-                    content={question1.content}
-                    placeholder=""
-                    widgets={question1.widgets}
-                    images={question1.images}
-                    disabled={false}
-                    widgetEnabled={true}
-                    immutableWidgets={false}
-                    showWordCount={true}
-                    warnNoPrompt={true}
-                    warnNoWidgets={true}
-                    onChange={changeFn}
-                />,
-            );
-            await wait();
+            render(<Harnessed onChange={changeFn} />);
 
             // Act
             const widgetDisclosure = screen.getByRole("link", {
-                name: "input-number 1",
+                name: "image 1",
             });
             userEvent.click(widgetDisclosure);
-            const correctAnswerInput = screen.getByLabelText("Correct answer:");
 
-            userEvent.clear(correctAnswerInput);
-            userEvent.paste(correctAnswerInput, "0.75");
+            const captionInput = screen.getByLabelText(/Caption:/);
+
+            userEvent.clear(captionInput);
+            userEvent.type(captionInput, "A picture of kittens");
             userEvent.tab(); // blurring the input triggers onChange to be called
+            jest.runOnlyPendingTimers();
 
             // Assert
             expect(changeFn).toHaveBeenCalledWith(
                 {
                     widgets: {
-                        "input-number 1": {
+                        "image 1": expect.objectContaining({
+                            type: "image",
                             graded: true,
-                            version: {major: 0, minor: 0},
-                            static: false,
-                            type: "input-number",
-                            options: {
-                                value: 0.75,
-                                simplify: "required",
-                                size: "normal",
-                                inexact: false,
-                                maxError: 0.1,
-                                answerType: "number",
-                                rightAlign: false,
-                            },
-                            alignment: "default",
-                        },
+                            options: expect.objectContaining({
+                                caption: "A picture of kittens",
+                            }),
+                        }),
                     },
                 },
                 undefined,
