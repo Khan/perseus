f580e21302c88e2b5ec13f13cd3c7eeebf93eb36
Jeremy Wiebe <jeremy@khanacademy.org>
2023-12-21 14:05:11 -0800

    Revert unrelated JsonEditor rewrite

--- packages/perseus-editor/src/components/json-editor.tsx
+++ packages/perseus-editor/src/components/json-editor.tsx
@@ -1,70 +1,94 @@
+/* eslint-disable @babel/no-invalid-this */
+/* eslint-disable react/no-unsafe */
+/* eslint-disable react/sort-comp */
+import createReactClass from "create-react-class";
 import * as React from "react";
-import ReactJson from "react-json-view";
 import _ from "underscore";
 
-const JsonEditor = (props: any) => {
-    const [currentValue, setCurrentValue] = React.useState(
-        JSON.stringify(props.value, null, 4),
-    );
-    const [valid, setValid] = React.useState(true);
+const JsonEditor: any = createReactClass({
+    displayName: "JsonEditor",
 
-    React.useEffect(() => {
+    getInitialState: function () {
+        return {
+            currentValue: JSON.stringify(this.props.value, null, 4),
+            valid: true,
+        };
+    },
+
+    UNSAFE_componentWillReceiveProps: function (nextProps) {
         const shouldReplaceContent =
-            !valid || !_.isEqual(currentValue, JSON.parse(currentValue));
+            !this.state.valid ||
+            !_.isEqual(nextProps.value, JSON.parse(this.state.currentValue));
 
         if (shouldReplaceContent) {
-            setCurrentValue(JSON.stringify(props.value, null, 4));
-            setValid(true);
+            this.setState(this.getInitialState());
         }
-    }, [currentValue, props.value, valid]);
+    },
 
-    const handleChange = React.useCallback(
-        (e) => {
-            const nextString = e.target.value;
-            try {
-                let json = JSON.parse(nextString);
-                // Some extra handling to allow copy-pasting from /api/vi
-                if (_.isString(json)) {
-                    json = JSON.parse(json);
-                }
-                // This callback unfortunately causes multiple renders,
-                // but seems to be necessary to avoid componentWillReceiveProps
-                // being called before setState has gone through
-                setCurrentValue(nextString);
-                setValid(true);
+    render: function () {
+        const classes =
+            "perseus-json-editor " + (this.state.valid ? "valid" : "invalid");
 
-                props.onChange(json);
-            } catch (ex: any) {
-                setCurrentValue(nextString);
-                setValid(true);
+        return (
+            <textarea
+                className={classes}
+                value={this.state.currentValue}
+                onChange={this.handleChange}
+                onKeyDown={this.handleKeyDown}
+                onBlur={this.handleBlur}
+            />
+        );
+    },
+
+    handleKeyDown: function (e) {
+        // This handler allows the tab character to be entered by pressing
+        // tab, instead of jumping to the next (non-existant) field
+        if (e.key === "Tab") {
+            const cursorPos = e.target.selectionStart;
+            const v = e.target.value;
+            const textBefore = v.substring(0, cursorPos);
+            const textAfter = v.substring(cursorPos, v.length);
+            e.target.value = textBefore + "    " + textAfter;
+            e.target.selectionStart = textBefore.length + 4;
+            e.target.selectionEnd = textBefore.length + 4;
+
+            e.preventDefault();
+            this.handleChange(e);
+        }
+    },
+
+    handleChange: function (e) {
+        const nextString = e.target.value;
+        try {
+            let json = JSON.parse(nextString);
+            // Some extra handling to allow copy-pasting from /api/vi
+            if (_.isString(json)) {
+                json = JSON.parse(json);
             }
-        },
-        [props],
-    );
-
-    const handleKeyDown = React.useCallback(
-        (e) => {
-            // This handler allows the tab character to be entered by pressing
-            // tab, instead of jumping to the next (non-existant) field
-            if (e.key === "Tab") {
-                const cursorPos = e.target.selectionStart;
-                const v = e.target.value;
-                const textBefore = v.substring(0, cursorPos);
-                const textAfter = v.substring(cursorPos, v.length);
-                e.target.value = textBefore + "    " + textAfter;
-                e.target.selectionStart = textBefore.length + 4;
-                e.target.selectionEnd = textBefore.length + 4;
-
-                e.preventDefault();
-                handleChange(e);
-            }
-        },
-        [handleChange],
-    );
+            // This callback unfortunately causes multiple renders,
+            // but seems to be necessary to avoid componentWillReceiveProps
+            // being called before setState has gone through
+            this.setState(
+                {
+                    currentValue: nextString,
+                    valid: true,
+                },
+                function () {
+                    // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+                    this.props.onChange(json);
+                },
+            );
+        } catch (ex: any) {
+            this.setState({
+                currentValue: nextString,
+                valid: false,
+            });
+        }
+    },
 
     // You can type whatever you want as you're typing, but if it's not valid
     // when you blur, it will revert to the last valid value.
-    const handleBlur = React.useCallback((e) => {
+    handleBlur: function (e) {
         const nextString = e.target.value;
         try {
             let json = JSON.parse(nextString);
@@ -91,21 +115,7 @@ const JsonEditor = (props: any) => {
                 valid: true,
             });
         }
-    }, []);
-
-    const classes = "perseus-json-editor " + (valid ? "valid" : "invalid");
-
-    return (
-        <textarea
-            className={classes}
-            value={currentValue}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            onBlur={handleBlur}
-        />
-    );
-};
-
-JsonEditor.displayName = "JsonEditor";
+    },
+});
 
 export default JsonEditor;
