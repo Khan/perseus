9fa6e8c76cdd9f43f1ae081841d444b8aaecceb0
Ned Redmond <edward@khanacademy.org>
2024-01-05 17:42:13 -0500

    COMMIT TO DEBUG CI -- WILL REVERT

--- packages/perseus/src/widgets/label-image/answer-choices.tsx
+++ packages/perseus/src/widgets/label-image/answer-choices.tsx
@@ -85,6 +85,10 @@ const AnswerChoices = (props: AnswerChoicesProps) => {
     React.useEffect(() => {
         const locale = getLocale();
         if (SRE.locales.get(locale)) {
+            // eslint-disable-next-line no-console
+            console.log({locales: SRE.locales, locale});
+            // eslint-disable-next-line no-console
+            console.trace("Debugging CI issue");
             SRE.setup({domain: "clearspeak", locale}).then(() =>
                 setChildren(
                     choices.map(({content}) => (
