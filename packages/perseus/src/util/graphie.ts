c67ea57bcd055e1f5209322a934751c1eb1d52dd
Ben Christel <benchristel@khanacademy.org>
2024-01-25 16:03:45 -0800

    Simplify

--- packages/perseus/src/util/graphie.ts
+++ packages/perseus/src/util/graphie.ts
@@ -1438,7 +1438,7 @@ GraphUtils.createGraphie = function (el: any): Graphie {
             return xy;
         };
         const clippedFn = (x) => clipper(fn(x));
-        const clippedFn2 = (x: number) => clipper(((t) => [t, 0])(x));
+        const clippedFn2 = (x: number) => clipper([x, 0]);
 
         if (!thisGraphie.currentStyle.strokeLinejoin) {
             thisGraphie.currentStyle.strokeLinejoin = "round";
