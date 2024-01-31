247f8b1be0647c39a5cabd9ce72a216d750a707b
Matthew <matthewcurtis@khanacademy.org>
2024-01-31 11:38:04 -0600

    rename helper to parsePerseusItem (#951)
    * rename helper to parsePerseusItem
    
    * changeset

--- packages/perseus/src/util/parse-perseus-json.test.ts
+++ packages/perseus/src/util/parse-perseus-json.test.ts
@@ -1,8 +1,8 @@
-import parsePerseusJSON from "./parse-perseus-json";
+import {parsePerseusItem} from "./parse-perseus-json";
 
-describe("parsePerseusJSON", () => {
+describe("parsePerseusItem", () => {
     it("should parse JSON", () => {
-        const result = parsePerseusJSON(
+        const result = parsePerseusItem(
             `{ "question": { "content": "idk why I'm testing this" }}`,
         );
         expect(result.question.content).toBe("idk why I'm testing this");
