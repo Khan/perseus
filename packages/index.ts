5881fa7d3593cdc7b7170a698c8752f8ac215482
jiaojiaojiaoCpbr <jiaojiaojiao.cpbr@gmail.com>
2024-02-01 12:35:17 +0800

    new: export perseus-core interface

--- packages/index.ts
+++ packages/index.ts
@@ -1,2 +1,10 @@
-export {init, Widgets, Renderer} from "./perseus/src";
+import {init, Widgets, Renderer} from "./perseus/src";
+
+export interface PerseusCore {
+    init: typeof init;
+    Widgets: typeof Widgets;
+    Renderer: typeof Renderer;
+}
+export {init, Widgets, Renderer};
+
 export type {WidgetExports} from "./perseus/src";
