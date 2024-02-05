4d8bbe26a261542caa7c802412c5fe2c40476808
Ben Christel <benchristel@khanacademy.org>
2024-02-05 12:49:09 -0800

    Delete old prototype-based Movable implementation

--- packages/perseus/src/interactive2/movable.test.ts
+++ packages/perseus/src/interactive2/movable.test.ts
@@ -2,72 +2,124 @@ import {fireEvent} from "@testing-library/react";
 
 import {Graphie} from "../util/graphie";
 
-import OldMovable, {MovableClassRenameMe} from "./movable";
+import {Movable} from "./movable";
 
 // Yay for side-effect imports!
 // eslint-disable-next-line import/no-unassigned-import
 import "../util/interactive";
 
 describe("Movable", () => {
-    const constructors = [
-        ["the old implementation", OldMovable],
-        ["the class-based implementation", MovableClassRenameMe],
+    const properties = [
+        "graphie",
+        "state",
+        "prevState",
+        "cursor",
+        "added",
+        "isHovering",
+        "isMouseOver",
+        "isDragging",
+        "mouseTarget",
+        "_fireEvent",
+        "_applyConstraints",
+        "draw",
+        "listen",
+        "unlisten",
+        "cloneState",
+        "_createDefaultState",
+        "modify",
+        "grab",
+        "update",
+        "remove",
+        "toBack",
+        "toFront",
     ];
 
-    describe.each(constructors)("%s", (_: string, Movable: any) => {
-        const properties = [
-            "graphie",
-            "state",
-            "prevState",
-            "cursor",
-            "added",
-            "isHovering",
-            "isMouseOver",
-            "isDragging",
-            "mouseTarget",
-            "_fireEvent",
-            "_applyConstraints",
-            "draw",
-            "listen",
-            "unlisten",
-            "cloneState",
-            "_createDefaultState",
-            "modify",
-            "grab",
-            "update",
-            "remove",
-            "toBack",
-            "toFront",
-        ];
+    it.each(properties)("has a %s property", (property) => {
+        const dummyGraphie: any = {};
+        const movable = new Movable(dummyGraphie, {});
+        expect(movable[property]).toBeDefined();
+    });
 
-        it.each(properties)("has a %s property", (property) => {
+    const getters = [
+        "cursor",
+        "added",
+        "isHovering",
+        "isMouseOver",
+        "isDragging",
+        "mouseTarget",
+    ];
+
+    it.each(getters)(
+        "returns the corresponding state property from %s()",
+        (getter) => {
             const dummyGraphie: any = {};
             const movable = new Movable(dummyGraphie, {});
-            expect(movable[property]).toBeDefined();
+            expect(movable[getter]()).toBe(movable.state[getter]);
+        },
+    );
+
+    it("sets default state when constructed", () => {
+        const dummyGraphie: any = {};
+        const movable = new Movable(dummyGraphie, {});
+        expect(movable.state).toEqual({
+            id: expect.any(String),
+            add: [],
+            added: true,
+            cursor: null,
+            draw: [],
+            isDragging: false,
+            isHovering: false,
+            isMouseOver: false,
+            modify: [],
+            mouseTarget: null,
+            onClick: [],
+            onMove: [],
+            onMoveEnd: [],
+            onMoveStart: [],
+            remove: [],
         });
+    });
 
-        const getters = [
-            "cursor",
-            "added",
-            "isHovering",
-            "isMouseOver",
-            "isDragging",
-            "mouseTarget",
-        ];
+    it("calls the modify listener when constructed", () => {
+        const dummyGraphie: any = {};
+        const modifySpy = jest.fn().mockName("modify");
 
-        it.each(getters)(
-            "returns the corresponding state property from %s()",
-            (getter) => {
-                const dummyGraphie: any = {};
-                const movable = new Movable(dummyGraphie, {});
-                expect(movable[getter]()).toBe(movable.state[getter]);
-            },
+        const _ = new Movable(dummyGraphie, {modify: modifySpy});
+
+        const baseExpectedState = {
+            add: [],
+            added: false,
+            cursor: null,
+            draw: [],
+            id: expect.any(String),
+            isDragging: false,
+            isHovering: false,
+            isMouseOver: false,
+            modify: [modifySpy],
+            mouseTarget: null,
+            onClick: [],
+            onMove: [],
+            onMoveEnd: [],
+            onMoveStart: [],
+            remove: [],
+        };
+        expect(modifySpy.mock.calls).toEqual([
+            [baseExpectedState, {}],
+            [
+                {...baseExpectedState, added: true},
+                {...baseExpectedState, added: true},
+            ],
+        ]);
+    });
+
+    test("cloneState()", () => {
+        const movable = new Movable(
+            new Graphie(document.createElement("div")),
+            {},
         );
 
-        it("sets default state when constructed", () => {
-            const dummyGraphie: any = {};
-            const movable = new Movable(dummyGraphie, {});
-            expect(movable.state).toEqual({
+        expect(movable.cloneState()).toEqual(
+            expect.objectContaining({
                 id: expect.any(String),
                 add: [],
                 added: true,
@@ -83,192 +135,115 @@ describe("Movable", () => {
                 onMoveEnd: [],
                 onMoveStart: [],
                 remove: [],
-            });
+            }),
+        );
+    });
+
+    test("cloneState() makes a copy", () => {
+        const movable = new Movable(
+            new Graphie(document.createElement("div")),
+            {},
+        );
+
+        expect(movable.cloneState()).not.toBe(movable.state);
+    });
+
+    test("modify() retains original ID", () => {
+        const movable = new Movable(
+            new Graphie(document.createElement("div")),
+            {},
+        );
+        const id = movable.state.id;
+
+        movable.modify({added: true});
+
+        expect(movable.state.id).toBe(id);
+    });
+
+    test("grab() fires onMoveStart", () => {
+        const graphie = new Graphie(document.createElement("div"));
+        graphie.init({
+            range: [
+                [-10, 10],
+                [-10, 10],
+            ],
         });
+        const onMoveStart = jest.fn();
+        const movable = new Movable(graphie, {onMoveStart});
 
-        it("calls the modify listener when constructed", () => {
-            const dummyGraphie: any = {};
-            const modifySpy = jest.fn().mockName("modify");
+        movable.grab([0, 0]);
 
-            const _ = new Movable(dummyGraphie, {modify: modifySpy});
+        expect(onMoveStart).toHaveBeenCalledWith([0, 0], [0, 0]);
+    });
 
-            const baseExpectedState = {
-                add: [],
-                added: false,
-                cursor: null,
-                draw: [],
-                id: expect.any(String),
-                isDragging: false,
-                isHovering: false,
-                isMouseOver: false,
-                modify: [modifySpy],
-                mouseTarget: null,
-                onClick: [],
-                onMove: [],
-                onMoveEnd: [],
-                onMoveStart: [],
-                remove: [],
-            };
-            expect(modifySpy.mock.calls).toEqual([
-                [baseExpectedState, {}],
-                [
-                    {...baseExpectedState, added: true},
-                    {...baseExpectedState, added: true},
-                ],
-            ]);
+    test("grab() fires onMove when mouse moves", () => {
+        const graphie = new Graphie(document.createElement("div"));
+        graphie.init({
+            range: [
+                [-10, 10],
+                [-10, 10],
+            ],
+            scale: [10, 10],
         });
+        const onMove = jest.fn();
+        const movable = new Movable(graphie, {onMove});
 
-        test("cloneState()", () => {
-            const movable = new Movable(
-                new Graphie(document.createElement("div")),
-                {},
-            );
+        movable.grab([0, 0]);
+        // eslint-disable-next-line testing-library/prefer-user-event
+        fireEvent.mouseMove(document, {clientX: 120, clientY: 80});
 
-            expect(movable.cloneState()).toEqual(
-                expect.objectContaining({
-                    id: expect.any(String),
-                    add: [],
-                    added: true,
-                    cursor: null,
-                    draw: [],
-                    isDragging: false,
-                    isHovering: false,
-                    isMouseOver: false,
-                    modify: [],
-                    mouseTarget: null,
-                    onClick: [],
-                    onMove: [],
-                    onMoveEnd: [],
-                    onMoveStart: [],
-                    remove: [],
-                }),
-            );
-        });
+        expect(onMove).toHaveBeenCalledWith([2, 2], [0, 0]);
+    });
 
-        test("cloneState() makes a copy", () => {
-            const movable = new Movable(
-                new Graphie(document.createElement("div")),
-                {},
-            );
+    test("grab() fires onMoveEnd when mouse up", () => {
+        const dummyGraphie: any = {};
+        const onMoveEnd = jest.fn();
+        const movable = new Movable(dummyGraphie, {onMoveEnd});
 
-            expect(movable.cloneState()).not.toBe(movable.state);
-        });
+        movable.grab([0, 0]);
+        // eslint-disable-next-line testing-library/prefer-user-event
+        fireEvent.mouseUp(document);
 
-        test("modify() resets movable to initial state when no params given", () => {
-            const movable = new Movable(
-                new Graphie(document.createElement("div")),
-                {},
-            );
+        expect(onMoveEnd).toHaveBeenCalledWith([0, 0], [0, 0]);
+    });
 
-            const state = movable.cloneState();
-            movable.state.added = false;
+    test("grab() fires onClick if still hovering when mouse up", () => {
+        const dummyGraphie: any = {};
+        const onClick = jest.fn();
+        const movable = new Movable(dummyGraphie, {onClick});
 
-            // It seems that all usages of this function in our production code
-            // pass the original state + mutations to modify(). However, it _does_
-            // work to pass no arguments so we'll keep this Characterization test
-            // here for now.
-            movable.modify();
+        movable.grab([0, 0]);
+        // eslint-disable-next-line testing-library/prefer-user-event
+        fireEvent.mouseUp(document);
 
-            expect(movable.cloneState()).toEqual(state);
-        });
+        expect(onClick).toHaveBeenCalledWith([0, 0], [0, 0]);
+    });
 
-        test("modify() retains original ID", () => {
-            const movable = new Movable(
-                new Graphie(document.createElement("div")),
-                {},
-            );
-            const id = movable.state.id;
+    test("draw() fires a draw event", () => {
+        const dummyGraphie: any = {};
+        const drawSpy = jest.fn();
+        const movable = new Movable(dummyGraphie, {draw: drawSpy});
 
-            movable.modify({added: true});
+        movable.draw();
 
-            expect(movable.state.id).toBe(id);
-        });
+        const expectedState = {
+            add: [],
+            added: true,
+            cursor: null,
+            draw: [drawSpy],
+            id: expect.any(String),
+            isDragging: false,
+            isHovering: false,
+            isMouseOver: false,
+            modify: [],
+            mouseTarget: null,
+            onClick: [],
+            onMove: [],
+            onMoveEnd: [],
+            onMoveStart: [],
+            remove: [],
+        };
 
-        test("grab() fires onMoveStart", () => {
-            const graphie = new Graphie(document.createElement("div"));
-            graphie.init({
-                range: [
-                    [-10, 10],
-                    [-10, 10],
-                ],
-            });
-            const onMoveStart = jest.fn();
-            const movable = new Movable(graphie, {onMoveStart});
-
-            movable.grab([0, 0]);
-
-            expect(onMoveStart).toHaveBeenCalledWith([0, 0], [0, 0]);
-        });
-
-        test("grab() fires onMove when mouse moves", () => {
-            const graphie = new Graphie(document.createElement("div"));
-            graphie.init({
-                range: [
-                    [-10, 10],
-                    [-10, 10],
-                ],
-                scale: [10, 10],
-            });
-            const onMove = jest.fn();
-            const movable = new Movable(graphie, {onMove});
-
-            movable.grab([0, 0]);
-            // eslint-disable-next-line testing-library/prefer-user-event
-            fireEvent.mouseMove(document, {clientX: 120, clientY: 80});
-
-            expect(onMove).toHaveBeenCalledWith([2, 2], [0, 0]);
-        });
-
-        test("grab() fires onMoveEnd when mouse up", () => {
-            const dummyGraphie: any = {};
-            const onMoveEnd = jest.fn();
-            const movable = new Movable(dummyGraphie, {onMoveEnd});
-
-            movable.grab([0, 0]);
-            // eslint-disable-next-line testing-library/prefer-user-event
-            fireEvent.mouseUp(document);
-
-            expect(onMoveEnd).toHaveBeenCalledWith([0, 0], [0, 0]);
-        });
-
-        test("grab() fires onClick if still hovering when mouse up", () => {
-            const dummyGraphie: any = {};
-            const onClick = jest.fn();
-            const movable = new Movable(dummyGraphie, {onClick});
-
-            movable.grab([0, 0]);
-            // eslint-disable-next-line testing-library/prefer-user-event
-            fireEvent.mouseUp(document);
-
-            expect(onClick).toHaveBeenCalledWith([0, 0], [0, 0]);
-        });
-
-        test("draw() fires a draw event", () => {
-            const dummyGraphie: any = {};
-            const drawSpy = jest.fn();
-            const movable = new Movable(dummyGraphie, {draw: drawSpy});
-
-            movable.draw();
-
-            const expectedState = {
-                add: [],
-                added: true,
-                cursor: null,
-                draw: [drawSpy],
-                id: expect.any(String),
-                isDragging: false,
-                isHovering: false,
-                isMouseOver: false,
-                modify: [],
-                mouseTarget: null,
-                onClick: [],
-                onMove: [],
-                onMoveEnd: [],
-                onMoveStart: [],
-                remove: [],
-            };
-
-            expect(drawSpy).toHaveBeenCalledWith(expectedState, expectedState);
-        });
+        expect(drawSpy).toHaveBeenCalledWith(expectedState, expectedState);
     });
 });
