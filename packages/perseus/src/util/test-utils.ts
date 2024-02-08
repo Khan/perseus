6f39bd785c332d1cc3efed6ebb2dfd493d982c4b
Tamara <tamara@khanacademy.org>
2024-02-08 12:27:56 -0600

    Add generic item and generate function

---
+++ packages/perseus/src/util/test-utils.ts
@@ -0,0 +1,42 @@
+import type {PerseusItem} from "../perseus-types";
+
+const genericPerseusItemData: PerseusItem = {
+    question: {
+        content: "",
+        images: {},
+        widgets: {},
+    },
+    answerArea: {
+        calculator: false,
+        chi2Table: false,
+        periodicTable: false,
+        tTable: false,
+        zTable: false,
+        financialCalculatorMonthlyPayment: false,
+        financialCalculatorTotalAmount: false,
+        financialCalculatorTimeToPayOff: false,
+        periodicTableWithKey: false,
+    },
+    itemDataVersion: {
+        major: 0,
+        minor: 1,
+    },
+    hints: [],
+    _multi: null,
+    answer: null,
+} as const;
+
+/**
+ * Generate a Perseus item object for testing.
+ *
+ * In order to better type Perseus objects used in testing, this function
+ * uses a basic Perseus object and updates it with custom values as needed.
+ *
+ * @param {Partial<PerseusItem>} customFields
+ * @returns PerseusItem
+ */
+export default function generateTestPerseusItem(
+    customFields: Partial<PerseusItem> = {},
+): PerseusItem {
+    return {...genericPerseusItemData, ...customFields};
+}
