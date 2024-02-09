ad66d3d74cdd8c44666bcd010a050bd632f2a471
Ned Redmond <edward@khanacademy.org>
2024-02-08 20:52:32 -0500

    Update utils.ts

--- packages/math-input/src/utils.ts
+++ packages/math-input/src/utils.ts
@@ -63,5 +63,6 @@ export function convertDotToTimesByLocale(convertDotToTimes: boolean): boolean {
 /**
  * Use this to avoid running code that should not run in Jest.
  **/
-export const inJest = process !== undefined && !!process?.env?.JEST_WORKER_ID;
+export const inJest =
+    typeof process !== "undefined" && !!process?.env?.JEST_WORKER_ID;
 // Explicitly checking for undefined because Cypress throws an error
