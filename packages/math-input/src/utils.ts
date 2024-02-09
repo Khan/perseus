d21568249bc65e1483ecc632958e3e43b2247ca8
Ned Redmond <edward@khanacademy.org>
2024-02-08 20:39:04 -0500

    prevent from running in jest

--- packages/math-input/src/utils.ts
+++ packages/math-input/src/utils.ts
@@ -59,3 +59,6 @@ export function convertDotToTimesByLocale(convertDotToTimes: boolean): boolean {
 
     return convertDotToTimes;
 }
+
+// Use this to avoid running code that should not run in Jest.
+export const inJest = process.env.JEST_WORKER_ID !== undefined;
