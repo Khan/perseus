1a5c3d52683bdeba8ae1d36d7fd70c046e0bb859
Ben Christel <benchristel@khanacademy.org>
2024-02-13 15:10:53 -0800

    Fix imports not working in Vite dev UI
    The fix involves creating aliases for all packages in the Perseus
    monorepo so Vite knows where to find them. I took a similar approach to
    the one used in `.storybook/main.ts`.
    
    Issue: none
    
    Test plan:
    
    ```
    cd /tmp
    git clone git@github.com:khan/perseus.git
    cd perseus
    yarn install
    yarn dev
    open http://localhost:5173
    ```
    
    You should see a webpage with many interactive graphs.

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,12 +1,25 @@
-import {resolve} from "node:path";
-
+import {resolve, dirname, join} from "node:path";
+import fs from "node:fs";
 import {defineConfig} from "vite";
+import glob from "fast-glob";
+
+const aliases = {};
+glob.sync(resolve("../packages/*/package.json")).forEach(
+    (packageJsonPath) => {
+        const pkg = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+        aliases[pkg.name] = join(
+            dirname(packageJsonPath),
+            pkg.source,
+        );
+    },
+);
 
 export default defineConfig({
     resolve: {
         alias: {
             hubble: resolve("../vendor/hubble/hubble.js"),
             raphael: resolve("../vendor/raphael/raphael.js"),
+            ...aliases,
         },
     },
     css: {
