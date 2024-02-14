5612d8e0dce35fdbecef0bf2cd9205600b85d94b
Ben Christel <benchristel@khanacademy.org>
2024-02-13 16:01:24 -0800

    WIP: Introduce and use new WrappedEllipse class

--- packages/perseus/src/interactive2/wrapped-ellipse.ts
+++ packages/perseus/src/interactive2/wrapped-ellipse.ts
@@ -5,6 +5,7 @@ import InteractiveUtil from "./interactive-util";
 import WrappedDefaults from "./wrapped-defaults";
 
 import type {Coord} from "./types";
+import {Graphie} from "../util/graphie";
 
 const DEFAULT_OPTIONS = {
     maxScale: 1,
@@ -13,7 +14,7 @@ const DEFAULT_OPTIONS = {
     disableMouseEventsOnWrapper: false,
 } as const;
 
-const WrappedEllipse = function (
+const WrappedEllipseOld = function (
     graphie: any,
     center: Coord,
     radii: [number, number],
@@ -88,6 +89,88 @@ const WrappedEllipse = function (
         this.visibleShape.node.style.pointerEvents = "auto";
     }
 };
+
+_.extend(WrappedEllipseOld.prototype, WrappedDefaults);
+
+class WrappedEllipse {
+    graphie: Graphie
+    initialPoint: Coord
+
+    constructor(
+        graphie: any,
+        center: Coord,
+        radii: [number, number],
+        options: any,
+    ) {
+        options = _.extend({}, DEFAULT_OPTIONS, options);
+
+        // Add `wrapper`, `visibleShape`, and remaining properties.
+        const fixedEllipse = graphie.fixedEllipse(
+            center,
+            radii,
+            options.maxScale,
+            options.padding,
+        );
+        this.graphie = graphie;
+        this.initialPoint = center;
+        _.extend(this, fixedEllipse);
+
+        if (options.interactiveKindForTesting) {
+            // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+            this.wrapper.setAttribute(
+                "data-interactive-kind-for-testing",
+                options.interactiveKindForTesting,
+            );
+        }
+
+        // Add to appropriate graphie layer
+        if (options.mouselayer) {
+            // Disable browser handling of all panning and zooming gestures on the
+            // movable wrapper so that when moved the browser does not scroll page
+            // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+            this.wrapper.style.touchAction = "none";
+
+            // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation. | TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+            this.graphie.addToMouseLayerWrapper(this.wrapper);
+        } else {
+            // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation. | TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+            this.graphie.addToVisibleLayerWrapper(this.wrapper);
+        }
+
+        if (options.shadow) {
+            const filter = "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))";
+            // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+            const wrapper = this.wrapper;
+            wrapper.style.filter = filter;
+
+            // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+            this.moveTo = function (point: any) {
+                const delta = kvector.subtract(
+                    this.graphie.scalePoint(point),
+                    this.graphie.scalePoint(this.initialPoint),
+                );
+                const do3dTransform = InteractiveUtil.getCanUse3dTransform();
+                const transform =
+                    "translateX(" +
+                    Math.round(delta[0]) +
+                    "px) " +
+                    "translateY(" +
+                    Math.round(delta[1]) +
+                    "px)" +
+                    (do3dTransform ? " translateZ(0)" : "");
+                // @ts-expect-error // FIXME
+                this.transform(transform);
+            };
+        }
+
+        if (options.disableMouseEventsOnWrapper) {
+            // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+            this.wrapper.style.pointerEvents = "none";
+            // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+            this.visibleShape.node.style.pointerEvents = "auto";
+        }
+    }
+}
 
 _.extend(WrappedEllipse.prototype, WrappedDefaults);
 
