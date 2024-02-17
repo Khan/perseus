d68b9f53549455aa906225326a9d6dac11f86f56
Jeremy Wiebe <jeremy@khanacademy.org>
2024-02-16 16:07:24 -0800

    Change to use Vite with Cypress

---
+++ config/cypress/cypress.config.ts
@@ -0,0 +1,66 @@
+import fs from "fs";
+import path from "path";
+import {mergeConfig} from "vite";
+
+import {defineConfig} from "cypress";
+import viteConfig from "../../dev/vite.config";
+
+const aliases = {};
+fs.readdirSync(path.join(__dirname, "../../packages")).forEach((name) => {
+    if (name.startsWith(".")) {
+        return;
+    }
+    const stat = fs.statSync(path.join(__dirname, "../../packages", name));
+    if (stat.isFile()) {
+        return;
+    }
+    const pkgPath = path.join("../../packages", name, "package.json");
+    const pkgJson = require(pkgPath);
+    aliases["@khanacademy/" + name] = path.join(
+        __dirname,
+        "../../packages",
+        name,
+        pkgJson.source,
+    );
+});
+fs.readdirSync(path.join(__dirname, "../../vendor")).forEach((name) => {
+    aliases[name] = path.join(__dirname, "../../vendor", name);
+});
+
+export default defineConfig({
+    fixturesFolder: false,
+    video: false,
+    // Prevent Cypress from scrolling to elements before clicking them.
+    scrollBehavior: false,
+    // iPhone 14/15 Pro Max
+    viewportWidth: 430,
+    viewportHeight: 932,
+    component: {
+        specPattern: ["packages/**/*.cypress.{js,ts,jsx,tsx}"],
+        indexHtmlFile: "config/cypress/component-index.html",
+        supportFile: "config/cypress/support.ts",
+
+        devServer: {
+            bundler: "vite",
+            framework: "react",
+            viteConfig: async (config) => {
+                console.log(config);
+
+                return mergeConfig(config, {
+                    ...viteConfig,
+                });
+            },
+        },
+
+        setupNodeEvents: async (on, config) => {
+            if (config.env["CYPRESS_COVERAGE"]) {
+                const task = await import("@cypress/code-coverage/task");
+                task.default(on, config);
+            }
+
+            config.env.reactDevtools = true;
+
+            return config;
+        },
+    },
+});
