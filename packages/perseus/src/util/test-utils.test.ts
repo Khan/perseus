7d4d098dbdbc3f41e89d7e118c8cd83fbcaeb928
Tamara <60857422+Myranae@users.noreply.github.com>
2024-02-22 10:14:22 -0600

    Create a helper function for generating correctly typed Perseus test items (#980)
    ## Summary:
    There are many places in webapp where custom types are created for Perseus items, frequently related to testing. This change adds a helper function that creates a generic Perseus test item that can have specific sections overridden with custom input. It will ensure tests are properly typed and help us know where updates need to be made if any changes happen to the types in Perseus.
    
    Issue: LC-1658
    
    ## Test plan:
    - Confirm all checks pass
    - Test the new function in webapp with the npm snapshot
    - In webapp, confirm related tests pass and related stories still work
    
    Author: Myranae
    
    Reviewers: Myranae, handeyeco, nixterrimus, SonicScrewdriver
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ✅ codecov/project, ✅ codecov/patch, ✅ Upload Coverage, ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Extract i18n strings (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Jest Coverage (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/980

---
+++ packages/perseus/src/util/test-utils.test.ts
@@ -0,0 +1,34 @@
+import {generateTestPerseusItem} from "./test-utils";
+import {
+    basicObject,
+    customQuestionInfo,
+    expectedQuestionInfoAdded,
+    customAnswerAreaInfo,
+    expectedAnswerAreaInfoAdded,
+    customHintsInfo,
+    expectedHintsInfoAdded,
+} from "./test-utils.testdata";
+
+describe("generateTestPerseusItem", () => {
+    it("should provide a basic Perseus item object with no inputs", () => {
+        expect(generateTestPerseusItem()).toEqual(basicObject);
+    });
+
+    it("should replace question parts when given question custom info", () => {
+        expect(generateTestPerseusItem(customQuestionInfo)).toEqual(
+            expectedQuestionInfoAdded,
+        );
+    });
+
+    it("should replace answer area parts when given answer area custom info", () => {
+        expect(generateTestPerseusItem(customAnswerAreaInfo)).toEqual(
+            expectedAnswerAreaInfoAdded,
+        );
+    });
+
+    it("should add hints when given custom info containing hints", () => {
+        expect(generateTestPerseusItem(customHintsInfo)).toEqual(
+            expectedHintsInfoAdded,
+        );
+    });
+});
