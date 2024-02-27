688ae2f1afde0d3e17e3acaa01ff3791ad43daba
nishasy <nisha@khanacademy.org>
2024-02-27 19:25:37 +0000

    Deploying to gh-pages from @ Khan/perseus@0c9fe476de5e6712c0f0abdd42768d1c1fbfdba5 🚀

---
+++ assets/number-line.stories-2o5AkQpc.js
@@ -0,0 +1,38 @@
+import{j as r}from"./jsx-runtime-BGVbfQ2Z.js";import"./prop-types-ax1AYuB3.js";import"./key-translator--aXlYrr4.js";import{a as y}from"./index-WaOzG1Ar.js";import"./button-assets-K2ZoY3Yc.js";import{R as g}from"./renderer-with-debug-ui-0JnAGIxx.js";import{S as R}from"./server-item-renderer-with-debug-ui-KEIUsvE8.js";import{T as S}from"./test-keypad-context-wrapper-fO43HHmB.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./version-akiLXZts.js";import"./index-mdqImiHB.js";import"./index-V35CFGao.js";import"./index-sjmtTzi4.js";import"./index-awljIyHI.js";import"./index-E09jvG0x.js";import"./index-tvtfaFq4.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./index-eZ2N530f.js";import"./tabbar-mLybaP46.js";import"./item-q5VEoFtv.js";import"./index-FNX3GwpG.js";import"./keypad-button-rYY5VIp2.js";import"./operators-page-pgHIcrjU.js";import"./navigation-pad-nnktsHUY.js";import"./index-uu39Elyn.js";import"./choice-wvBsaMTt.js";import"./index-ouXaYoW-.js";import"./index-iPN1eFIx.js";import"./Popper-FSPFYGkT.js";import"./index-SM3muJE2.js";import"./index-K7FSCCGN.js";import"./index-GVhAzXpB.js";import"./index-BH4Zx_EU.js";import"./index-ObSi-eBV.js";import"./index-awCzqCwC.js";import"./icon-HbAIhp4d.js";import"./perseus-api-Ly41NhN4.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./jquery-5v7aFUvu.js";import"./index-default-4_ZsnO94.js";import"./media-queries-4x-ifIrJ.js";import"./constants-5iWyYZaE.js";import"./choice-icon-UvDvB5O3.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-fAaGW1jp.js";import"./focus-ring-XJLNkFEA.js";import"./option-status-TK8KGDE4.js";import"./side-by-side-3giG2cJk.js";import"./article-renderer-E7y8nJCi.js";import"./compare-VLxlEkSV.js";import"./enums-x5qaTru7.js";import"./renderer-Y-r37Tvv.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-O4Zh6lWM.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-G-g1AweO.js";import"./fixed-to-responsive-X0KGwmEJ.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./lint-yitF-R_f.js";import"./math-input-HGKhnj9M.js";import"./unit-tpB2tohZ.js";import"./input-with-examples-Q1IyqHpU.js";import"./math-output-o5-UebzW.js";import"./text-input-dyr-RWux.js";import"./index-s56UOS9p.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-WECRmbnm.js";import"./base-radio-ceh1uvp2.js";import"./shared-cRfnFlyj.js";import"./choice-none-above-neIJSFiL.js";import"./video-transcript-link-7IHhb-72.js";import"./answer-choices-nCF4MMce.js";import"./button-group-eBTrRsKy.js";import"./graph-HZ4jbR1b.js";import"./marker-N3KCo3Mu.js";import"./answer-pill-nPJ9bwuS.js";import"./sortable-JGhtlYWP.js";import"./number-input-fy1LqA4g.js";import"./index-vV577VS-.js";import"./info-tip-8U9pfYW-.js";import"./multi-renderer-8o9J3Suf.js";import"./hints-renderer-nOIGY2RX.js";import"./hud-J4t6ihs4.js";import"./multi-button-group-rHU9dGVb.js";import"./prop-check-box-oy1Hh3jc.js";import"./range-input-qgKts6Pq.js";import"./register-all-widgets-for-testing-fX-NCpub.js";import"./ke-score-ui-IpVdC5OC.js";import"./test-dependencies-JCnwoxif.js";import"./chunk-WFFRPTHA-zlNDxfrq.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";const f={content:`$E=2.5$
+
+**Move the dot to $-E$ on the number line.**
+
+
+[[☃ number-line 1]]`,images:{},widgets:{"number-line 1":{graded:!0,version:{major:0,minor:0},static:!1,type:"number-line",options:{labelRange:[null,null],initialX:null,tickStep:1,labelStyle:"decimal",labelTicks:!0,snapDivisions:2,range:[-4,4],static:!1,correctRel:"eq",numDivisions:null,divisionRange:[1,10],correctX:-2.5},alignment:"default"}}},b={content:`$E=2.5$
+
+**Move the dot to $-E$ on the number line.**
+
+
+[[☃ number-line 1]]`,images:{},widgets:{"number-line 1":{type:"number-line",alignment:"default",static:!1,graded:!0,options:{static:!1,range:[0,1],labelRange:[null,null],labelStyle:"improper",labelTicks:!1,divisionRange:[1,12],numDivisions:1,snapDivisions:1,tickStep:null,correctRel:"eq",correctX:.5,initialX:null,showTooltip:!1,isTickCtrl:!0}}}},ut={title:"Perseus/Widgets/Number Line"},t=i=>r(g,{question:f}),e=i=>r(g,{question:b}),o=i=>r(S,{children:r(y.Consumer,{children:({keypadElement:h})=>r(R,{item:{question:b,_multi:null,answer:null,answerArea:null,itemDataVersion:{major:0,minor:1},hints:[]},apiOptions:{isMobile:!0,customKeypad:!0},keypadElement:h})})});var n,m,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <RendererWithDebugUI question={question1} />;
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var s,a,l;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <RendererWithDebugUI question={question2} />;
+}`,...(l=(a=e.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};var u,c,d;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <TestKeypadContextWrapper>
+            <KeypadContext.Consumer>
+                {({
+        keypadElement
+      }) => {
+        return <ServerItemRendererWithDebugUI item={({
+          question: question2,
+          _multi: null,
+          answer: null,
+          answerArea: null,
+          itemDataVersion: {
+            major: 0,
+            minor: 1
+          },
+          hints: []
+        } as PerseusItem)} apiOptions={{
+          isMobile: true,
+          customKeypad: true
+        }} keypadElement={keypadElement} />;
+      }}
+            </KeypadContext.Consumer>
+        </TestKeypadContextWrapper>;
+}`,...(d=(c=o.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const ct=["Question1","ShowTickController","ShowTickControllerMobile"];export{t as Question1,e as ShowTickController,o as ShowTickControllerMobile,ct as __namedExportsOrder,ut as default};
---
+++ assets/register-all-widgets-for-testing-fX-NCpub.js
@@ -0,0 +1,1 @@
+import{w as r}from"./article-renderer-E7y8nJCi.js";import{r as e,a as t}from"./renderer-Y-r37Tvv.js";const a=()=>{e(r),t("transformer","deprecated-standin")};export{a as r};
