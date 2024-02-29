ca680d091c5f63a3572cd1b5ccf406d5b67853e4
Kim716 <yichen.hsieh_c7_alumni@teach4taiwan.org>
2024-02-29 20:15:11 +0800

    feat: export GraphUtils from perseus

--- packages/index.ts
+++ packages/index.ts
@@ -1,10 +1,12 @@
 import {init, Widgets, Renderer} from "./perseus/src";
+import GraphUtils from "./perseus/src/util/graphie";
 
 export interface PerseusCore {
     init: typeof init;
     Widgets: typeof Widgets;
     Renderer: typeof Renderer;
+    GraphUtils: typeof GraphUtils;
 }
-export {init, Widgets, Renderer};
+export {init, Widgets, Renderer, GraphUtils};
 
 export type {WidgetExports} from "./perseus/src";
