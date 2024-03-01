db1f8e70ec25e3e87dee02b86127adc255fda085
Nisha Yerunkar <nisha@khanacademy.org>
2024-02-29 16:28:36 -0800

    Update InteractiveGraphEditor stories: included Controlled story

--- packages/perseus-editor/src/components/__stories__/interactive-graph-settings.stories.tsx
+++ packages/perseus-editor/src/components/__stories__/interactive-graph-settings.stories.tsx
@@ -4,6 +4,8 @@ import InteractiveGraphSettings from "../interactive-graph-settings";
 
 import InteractiveGraphSettingsArgTypes from "./interactive-graph-settings.argtypes";
 
+import type {StoryObj} from "@storybook/react";
+
 export default {
     title: "Perseus/Editor/Components/Interactive Graph Settings",
     component: InteractiveGraphSettings,
@@ -14,11 +16,42 @@ export const Default = (args): React.ReactElement => {
     return <InteractiveGraphSettings {...args} />;
 };
 
+type StoryComponentType = StoryObj<typeof InteractiveGraphSettings>;
+
+// Set the default values in the control panel.
 Default.args = {
-    // Separating the array props out because trying to editing them in
-    // the controls panel without a default value causes the story to crash.
+    box: [288, 288],
+    gridStep: [1, 1],
+    labels: ["x", "y"],
+    markings: "graph",
     range: [
         [-10, 10],
         [-10, 10],
     ],
+    rulerLabel: "",
+    rulerTicks: 10,
+    showProtractor: false,
+    showRuler: false,
+    showTooltips: false,
+    snapStep: [1, 1],
+    step: [1, 1],
+};
+
+/**
+ * Example of what the InteractiveGraphEditor experience is when all
+ * props are controlled by the parent. (Checkboxes update when clicked, etc.)
+ */
+export const Controlled: StoryComponentType = {
+    render: function Render() {
+        const reducer = (state, newState) => {
+            return {
+                ...state,
+                ...newState,
+            };
+        };
+
+        const [state, dispatch] = React.useReducer(reducer, {});
+
+        return <InteractiveGraphSettings {...state} onChange={dispatch} />;
+    },
 };
