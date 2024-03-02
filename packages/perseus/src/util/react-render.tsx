b4f1eb77e42851b1217b4e78677d8733702b826d
Jeremy Wiebe <jeremy@khanacademy.org>
2024-03-01 16:39:49 -0800

    Migrate react-render to use ReactDOMClient.createRoot()

--- packages/perseus/src/util/react-render.tsx
+++ packages/perseus/src/util/react-render.tsx
@@ -1,27 +1,24 @@
 import {RenderStateRoot} from "@khanacademy/wonder-blocks-core";
 import * as React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 
-export default function render<ElementType extends React.ElementType>(
+export default function render(
     element: React.ReactNode,
     container: HTMLElement,
-    callback?: () => void,
-): React.ElementRef<ElementType> {
-    // TODO(LP-11406): Replace this, or callers, with React Portal
-    // @ts-expect-error - TS2322 - Type 'void' is not assignable to type 'ElementRef<ElementType>'.
-    // eslint-disable-next-line no-restricted-syntax
-    return ReactDOM.render(
-        /**
-         * `RenderStateRoot` is responsible for tracking whether it's
-         * the initial render or subsequent renders.  It's used by
-         * `useUniqueId` and will be used by `UniqueIDProvider` and
-         * `WithSSRPlaceholder` in the future.  We're placing it as
-         * high up in the render tree as possible to ensure that any
-         * components using that hook or those components will function
-         * correctly.
-         */
-        <RenderStateRoot throwIfNested={false}>{element}</RenderStateRoot>,
-        container,
-        callback,
+) {
+    /**
+     * `RenderStateRoot` is responsible for tracking whether it's
+     * the initial render or subsequent renders.  It's used by
+     * `useUniqueId` and will be used by `UniqueIDProvider` and
+     * `WithSSRPlaceholder` in the future.  We're placing it as
+     * high up in the render tree as possible to ensure that any
+     * components using that hook or those components will function
+     * correctly.
+     */
+    const children = (
+        <RenderStateRoot throwIfNested={false}>{element}</RenderStateRoot>
     );
+
+    const root = ReactDOMClient.createRoot(container);
+    root.render(children);
 }
