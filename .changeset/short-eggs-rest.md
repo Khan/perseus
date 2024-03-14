8b72295802550f2b5885c39bfb0c70ff70efe749
Nisha Yerunkar <nisha@khanacademy.org>
2024-03-13 18:49:57 -0700

    Add changeset. Fix tests

---
+++ .changeset/short-eggs-rest.md
@@ -0,0 +1,6 @@
+---
+"@khanacademy/perseus": minor
+"@khanacademy/perseus-editor": minor
+---
+
+Add "add a locked figure" UI to interactive graph editor + adding points (mafs graphs only)
--- packages/perseus/src/components/__tests__/graph.test.tsx
+++ packages/perseus/src/components/__tests__/graph.test.tsx
@@ -17,39 +17,4 @@ describe("graph", () => {
 
         expect(container).toMatchSnapshot();
     });
-
-    describe("locked shapes", () => {
-        it("should render locked points", () => {
-            const {container} = render(
-                <Graph
-                    box={[100, 100]}
-                    range={[
-                        [0, 10],
-                        [0, 10],
-                    ]}
-                    markings="none"
-                    lockedFigures={[
-                        {
-                            type: "point" as const,
-                            coord: [5, 5],
-                        },
-                        {
-                            type: "point" as const,
-                            coord: [1, 9],
-                        },
-                    ]}
-                />,
-            );
-
-            // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-            const points = container.querySelectorAll("ellipse");
-            expect(points).toHaveLength(2);
-
-            expect(points[0].getAttribute("cx")).toBe("50");
-            expect(points[0].getAttribute("cy")).toBe("50");
-
-            expect(points[1].getAttribute("cx")).toBe("10");
-            expect(points[1].getAttribute("cy")).toBe("10");
-        });
-    });
 });
