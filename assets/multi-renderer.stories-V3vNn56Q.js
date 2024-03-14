47b9cf4824bf9693c8514458a5c6c1d35539460a
jeremywiebe <jeremy.wiebe@gmail.com>
2024-03-14 17:29:49 +0000

    Deploying to gh-pages from @ Khan/perseus@81f7def7a9c0ff082d28f327532b76c446282b90 🚀

---
+++ assets/multi-renderer.stories-V3vNn56Q.js
@@ -0,0 +1,56 @@
+import{j as e,a as o,F as _}from"./jsx-runtime-BGVbfQ2Z.js";import{V as n}from"./index-eZ2N530f.js";import{d}from"./index-V35CFGao.js";import{H as u}from"./index-A0DvNMpb.js";import{l as I}from"./index-awljIyHI.js";import{B as A}from"./choice-aOZUTZb9.js";import{r as g}from"./index-qhcEwEpg.js";import"./article-renderer-rs0Bd9em.js";import"./unit-tpB2tohZ.js";import"./fixed-to-responsive-X0KGwmEJ.js";import"./dependencies-fnqF3NiV.js";import"./video-transcript-link-JriNv8Pc.js";import"./index-default-4_ZsnO94.js";import"./perseus-api-Ly41NhN4.js";import{s as l}from"./multi-renderer--Ls73Xxf.js";import"./hints-renderer-90NmaX-7.js";import"./renderer-kLXXIAmW.js";import"./base-radio-605B5-jp.js";import"./button-group-eBTrRsKy.js";import"./graph-81WtTZFU.js";import"./svg-image-A3RqGh-W.js";import"./hud-J4t6ihs4.js";import"./icon-HbAIhp4d.js";import"./info-tip-Lk8Jr-mP.js";import"./inline-icon-MJBS_-2U.js";import"./math-input-_yQGip9z.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-z5dO9Uz4.js";import"./prop-check-box-TM2oQvxZ.js";import"./range-input-J32YkzGG.js";import"./text-input-HIX1uEpz.js";import"./text-list-editor-bJphcq85.js";import"./util-1QHV8SS2.js";import"./jquery-5v7aFUvu.js";import{M as S}from"./multi-items-k0jfeAf4.js";import{K as x}from"./ke-score-ui-PUk9WSUn.js";import{S as T}from"./side-by-side-nEB6BDl1.js";import{t as C}from"./test-dependencies-dkU3M6aY.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-4JcgVDnF.js";import"./index-tvtfaFq4.js";import"./index-ACL0N2lY.js";import"./index-hYQ6Pa3_.js";import"./index-e6QWP3dX.js";import"./index-FQM5b1mh.js";import"./index-E09jvG0x.js";import"./index-uu39Elyn.js";import"./index-K7FSCCGN.js";import"./index-mXvZDrvB.js";import"./index-SM3muJE2.js";import"./index-vbayBp87.js";import"./Popper-2p8US95Y.js";import"./index-awCzqCwC.js";import"./media-queries-4x-ifIrJ.js";import"./constants-5iWyYZaE.js";import"./choice-icon-bFtFAuec.js";import"./icon-paths-fAaGW1jp.js";import"./focus-ring-XJLNkFEA.js";import"./option-status--OOacwxI.js";import"./compare-VLxlEkSV.js";import"./version-akiLXZts.js";import"./prop-types-MgD_VJJS.js";import"./index-J2t_5nK1.js";import"./index-k9JCoeOY.js";import"./tabbar-IAxGP4dM.js";import"./item-fHYIN_c3.js";import"./button-assets-K2ZoY3Yc.js";import"./keypad-button-qf5RMNu9.js";import"./operators-page-xJJKH4dX.js";import"./navigation-pad-cRy775HL.js";import"./key-translator-NuuNqL6m.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./perseus-error-OpXxk17X.js";import"./input-with-examples-N47Vk9eJ.js";import"./math-output-o5-UebzW.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-tp8U2Nw1.js";import"./shared-cRfnFlyj.js";import"./answer-choices-_9VnX3G0.js";import"./index-S25SDe0M.js";import"./asset-context-pmjKTqqL.js";import"./bundle.esm-3RuEOGQS.js";import"./marker-f6UsUoR-.js";import"./answer-pill-g3pXvFXe.js";import"./sortable-oBAYwvTX.js";import"./stub-tag-editor-CaJYfBue.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./lint-yitF-R_f.js";import"./choice-none-above-AsCnCS9f.js";import"./image-loader-BrzYBUzY.js";import"./register-all-widgets-for-testing-NZfJH4T5.js";const v=l.shape({blurb:l.content,question:l.content,hints:l.hints}),f={_multi:{blurb:{__type:"content",content:"",images:{},widgets:{}},hints:[{__type:"hint",content:"If two triangles are congruent, then they have the same side lengths and angle measures.",images:{},replace:!1,widgets:{}},{__type:"hint",content:"A triangle congruent to triangle $ABC$ must also have side lengths of $12$, $14$ and $20$.",images:{},replace:!1,widgets:{}},{__type:"hint",content:`The following triangle is congruent to triangle $ABC$:
+
+* A triangle with side lengths of $12$, $14$, and $20$`,images:{},replace:!1,widgets:{}}],question:{__type:"content",content:`Triangle $ABC$ has side lengths of $12$, $14$, and $20$. Which of the following triangles is congruent to triangle $ABC$ ?
+
+[[☃ radio 1]]
+
+Enter the number 3 into this field: [[☃ input-number 1]]`,widgets:{"radio 1":{alignment:"default",graded:!0,options:{choices:[{clue:"Congruent triangles have the same side lengths.",content:"A triangle with side lengths of $3$, $4$, and $5$",correct:!1},{clue:`Congruent triangles have the same side lengths.
+
+This choice is similar to triangle $ABC$.`,content:"A triangle with side lengths of $6$, $7$, and $10$",correct:!1},{clue:"Congruent triangles have the same side lengths.",content:"A triangle with side lengths of $10$, $12$, and $18$",correct:!1,isNoneOfTheAbove:!1},{clue:"Congruent triangles have the same side lengths.",content:"A triangle with side lengths of $12$, $14$, and $20$",correct:!0,isNoneOfTheAbove:!1},{clue:`Congruent triangles have the same side lengths.
+
+This choice is similar to triangle $ABC$.`,content:"A triangle with side lengths of $24$, $28$, and $40$",correct:!1,isNoneOfTheAbove:!1}],countChoices:!1,deselectEnabled:!1,displayCount:null,hasNoneOfTheAbove:!1,multipleSelect:!1,randomize:!1},static:!1,type:"radio",version:{major:1,minor:0}},"input-number 1":{type:"input-number",graded:!0,options:{answerType:"number",value:"-42",simplify:"required",size:"normal",inexact:!1,maxError:.1}}}}}},c=({children:m,simpleItem:s,apiOptions:h})=>{const i=g.useRef(null),[p,r]=g.useState(null);return e(T,{leftTitle:"Multi Renderer",left:o(_,{children:[e(S.MultiRenderer,{item:s,shape:v,ref:i,dependencies:C,children:w=>m(w)}),e("div",{id:"workarea"}),e("div",{id:"hintsarea"}),e(n,{style:{flexDirection:"row",alignItems:"center"},children:e(A,{onClick:()=>{i.current&&r(i.current.score())},children:"Check"})}),e(x,{score:p})]}),jsonObject:s})};try{c.displayName="MultiItemRendererWithDebugUI",c.__docgenInfo={description:"",displayName:"MultiItemRendererWithDebugUI",props:{simpleItem:{defaultValue:null,description:"",name:"simpleItem",required:!0,type:{name:"Item"}},apiOptions:{defaultValue:null,description:"",name:"apiOptions",required:!1,type:{name:"Readonly<{ isArticle?: boolean; onInputError?: ((widgetId: any, value: string, message?: string | null) => unknown) | undefined; onFocusChange?: ((newFocusPath: FocusPath, oldFocusPath: FocusPath, keypadHeight?: number | undefined, focusedElement?: HTMLElement | undefined) => unknown) | undef..."}}}}}catch{}const a=m=>{const s={_multi:{...f._multi,blurb:{...f._multi.blurb,content:"This is a short snippet to help you understand the context of the question. We call it the 'blurb'."}}};return e(c,{simpleItem:s,children:({renderers:h})=>{const{blurb:i,question:p,hints:r}=h;return o(n,{children:[o(n,{style:t.section,children:[e(u,{style:t.heading,children:d("Blurb")}),i]}),o(n,{style:t.section,children:[e(u,{style:t.heading,children:d("Question")}),p]}),o(n,{style:t.section,children:[e(u,{style:t.heading,children:d("Hints")}),e(n,{style:t.hints,children:r==null?void 0:r.firstN(2)})]})]})}})},t=I.StyleSheet.create({section:{backgroundColor:"#F5F5F5",padding:"5px",borderWidth:"1px",marginTop:"5px",marginBottom:"5px"},heading:{backgroundColor:"#A9A9A9",margin:"-5px",padding:"5px"},hints:{marginLeft:"50px"}}),gt={title:"Perseus/Renderers/Multi Renderer"};var $,y,b;a.parameters={...a.parameters,docs:{...($=a.parameters)==null?void 0:$.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const item = ({
+    _multi: {
+      ...question1._multi,
+      blurb: {
+        ...question1._multi.blurb,
+        content: "This is a short snippet to help you understand the context of the question. We call it the 'blurb'."
+      }
+    }
+  } as const);
+  return <MultiItemRendererWithDebugUI simpleItem={item}>
+            {({
+      renderers
+    }) => {
+      const {
+        blurb,
+        question,
+        hints
+      } = renderers;
+      return <View>
+                        <View style={styles.section}>
+                            <HeadingSmall style={styles.heading}>
+                                {i18n.doNotTranslate("Blurb")}
+                            </HeadingSmall>
+                            {blurb}
+                        </View>
+                        <View style={styles.section}>
+                            <HeadingSmall style={styles.heading}>
+                                {i18n.doNotTranslate("Question")}
+                            </HeadingSmall>
+                            {question}
+                        </View>
+                        <View style={styles.section}>
+                            <HeadingSmall style={styles.heading}>
+                                {i18n.doNotTranslate("Hints")}
+                            </HeadingSmall>
+                            <View style={styles.hints}>
+                                {
+            // @ts-expect-error [FEI-5003] - TS2339 - Property 'firstN' does not exist on type 'readonly ReactNode[]'.
+            hints?.firstN(2)}
+                            </View>
+                        </View>
+                    </View>;
+    }}
+        </MultiItemRendererWithDebugUI>;
+}`,...(b=(y=a.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};const ft=["SingleItem"];export{a as SingleItem,ft as __namedExportsOrder,gt as default};
