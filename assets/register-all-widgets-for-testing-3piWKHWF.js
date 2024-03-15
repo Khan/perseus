5ee6298e8691cdc02e9fd006e634c2541087b0b4
nedredmond <nedredmond@pm.me>
2024-03-15 19:19:47 +0000

    Deploying to gh-pages from @ Khan/perseus@512265cc5ae75e2ea658255291ef75539b1c1702 🚀

--- assets/register-all-widgets-for-testing-3piWKHWF.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-ra4RuYTW.js";import{r,a as s}from"./renderer-p50lW1jT.js";const a=()=>{r(e),s()};export{a as r};
---
+++ assets/register-all-widgets-for-testing-7uSNXJM_.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-1wBBQATi.js";import{r,a as s}from"./renderer-uh8HDrJD.js";const a=()=>{r(e),s()};export{a as r};
