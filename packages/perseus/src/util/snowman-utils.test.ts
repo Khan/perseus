27d4597717ad774322f731890b1d9ecaea853f02
Tamara <tamara@khanacademy.org>
2024-03-15 09:43:32 -0500

    Fix fn names and add test for addWidget

--- packages/perseus/src/util/snowman-utils.test.ts
+++ packages/perseus/src/util/snowman-utils.test.ts
@@ -1,15 +1,15 @@
 import {
     addWidget,
-    getAllWidgetNames,
+    getAllWidgetIds,
     WidgetType,
-    getQuestionWidgetNames,
+    getQuestionWidgetIds,
 } from "./snowman-utils";
 
 describe("getWidgetNamesFromContent", () => {
     it("returns all widget names present in a string", () => {
         const content =
             "> [[☃ passage 3]]\n\n=====\n\n###Questions for Passage 1\n\n[[☃ group 1]]  \n[[☃ group 12]]  \n\n$$\n";
-        const actual = getAllWidgetNames(content);
+        const actual = getAllWidgetIds(content);
         // console.log(getAllWidgetNames(content));
         const expected = ["passage 3", "group 1", "group 12"];
         expect(actual).toEqual(expected);
@@ -25,7 +25,7 @@ describe("Widget getter", () => {
         )} foo ${addWidget(WidgetType.Radio, 1)}`;
 
         // Act
-        const questionWidget = getAllWidgetNames(questionContent);
+        const questionWidget = getAllWidgetIds(questionContent);
 
         // Assert
         expect(questionWidget).toEqual(["numeric-input 1", "radio 1"]);
@@ -39,9 +39,27 @@ describe("Widget getter", () => {
         )} ${addWidget(WidgetType.Video, 1)} ${addWidget(WidgetType.Radio, 1)}`;
 
         // Act
-        const questionWidgetNames = getQuestionWidgetNames(questionContent);
+        const questionWidgetNames = getQuestionWidgetIds(questionContent);
 
         // Assert
         expect(questionWidgetNames).toEqual(["numeric-input 1", "radio 1"]);
+    });
+});
+
+describe("Add widget", () => {
+    it("correctly adds a widget placeholder", () => {
+        // Arrange
+        const content = "Please answer this question [[☃ radio 1]]";
+        const testWidgetType = WidgetType.Radio;
+        const testWidgetInstance = 1;
+
+        // Act
+        const newContent = `Please answer this question ${addWidget(
+            testWidgetType,
+            testWidgetInstance,
+        )}`;
+
+        // Assert
+        expect(newContent).toEqual(content);
     });
 });
