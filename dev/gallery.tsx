52ab4475ce9ccdb9ba6b68af1931369501ed8294
Ned Redmond <edward@khanacademy.org>
2024-03-19 19:45:23 -0400

    add search params to dev ui

--- dev/gallery.tsx
+++ dev/gallery.tsx
@@ -2,12 +2,13 @@
 import {useUniqueIdWithMock, View} from "@khanacademy/wonder-blocks-core";
 import {OptionItem, MultiSelect} from "@khanacademy/wonder-blocks-dropdown";
 import {Strut} from "@khanacademy/wonder-blocks-layout";
+import SearchField from "@khanacademy/wonder-blocks-search-field";
 import Spacing from "@khanacademy/wonder-blocks-spacing";
 import Switch from "@khanacademy/wonder-blocks-switch";
 import {color} from "@khanacademy/wonder-blocks-tokens";
 import {css, StyleSheet} from "aphrodite";
 import * as React from "react";
-import {useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 
 import {Renderer} from "../packages/perseus/src";
 import * as grapher from "../packages/perseus/src/widgets/__testdata__/grapher.testdata";
@@ -76,33 +77,78 @@ const styles = StyleSheet.create({
 
 export function Gallery() {
     const ids = useUniqueIdWithMock();
+    const params = useMemo(
+        () => new URLSearchParams(window.location.search),
+        [],
+    );
 
-    const [isMobile, setIsMobile] = useState(false);
-    const [mafsFlags, setMafsFlags] = useState<Array<string>>([]);
+    const [isMobile, setIsMobile] = useState(params.get("mobile") === "true");
+    const [mafsFlags, setMafsFlags] = useState<Array<string>>(
+        params.get("flags")?.split(",") || [],
+    );
+    const [search, setSearch] = useState<string>(params.get("search") || "");
+
+    useEffect(() => {
+        const url = new URL(window.location.href);
+        if (isMobile) {
+            url.searchParams.set("mobile", "true");
+        } else {
+            url.searchParams.delete("mobile");
+        }
+        if (mafsFlags.length === 0) {
+            url.searchParams.delete("flags");
+        } else {
+            url.searchParams.set("flags", mafsFlags.join(","));
+        }
+        if (!search) {
+            url.searchParams.delete("search");
+        } else {
+            url.searchParams.set("search", search);
+        }
+        window.history.replaceState({}, "", url.toString());
+    }, [isMobile, mafsFlags, params, search]);
 
     const mafsFlagsObject = mafsFlags.reduce((acc, flag) => {
         acc[flag] = true;
         return acc;
     }, {});
 
+    const switchId = ids.get("mobile");
+    const flagsId = ids.get("flags");
+    const searchId = ids.get("search");
+
     return (
         <View className={css(styles.page)}>
             <header className={css(styles.header)}>
                 <Switch
-                    id={ids.get("mobile")}
+                    id={switchId}
                     checked={isMobile}
                     onChange={setIsMobile}
                 />
                 <Strut size={Spacing.xSmall_8} />
-                <label htmlFor={ids.get("mobile")}>Mobile</label>
+                <label htmlFor={switchId}>Mobile</label>
                 <Strut size={Spacing.medium_16} />
-                <MultiSelect onChange={setMafsFlags} selectedValues={mafsFlags}>
+                <MultiSelect
+                    id={flagsId}
+                    onChange={setMafsFlags}
+                    selectedValues={mafsFlags}
+                >
                     <OptionItem value="segment" label="Segment" />
                     <OptionItem value="linear" label="Linear" />
                     <OptionItem value="linear-system" label="Linear System" />
                     <OptionItem value="point" label="Point" />
                     <OptionItem value="ray" label="Ray" />
                 </MultiSelect>
+                <Strut size={Spacing.xSmall_8} />
+                <label htmlFor={flagsId}>Mafs Flags</label>
+                <Strut size={Spacing.medium_16} />
+                <SearchField
+                    id={searchId}
+                    value={search}
+                    onChange={setSearch}
+                />
+                <Strut size={Spacing.xSmall_8} />
+                <label htmlFor={searchId}>Search Types</label>
                 <Strut size={Spacing.medium_16} />
                 <nav>
                     <a href="#flipbook">Flipbook</a>
@@ -113,16 +159,49 @@ export function Gallery() {
                     style={styles.cards}
                     className={isMobile ? "perseus-mobile" : ""}
                 >
-                    {questions.map((question, i) => (
-                        <QuestionRenderer
-                            key={i}
-                            question={question}
-                            apiOptions={{
-                                isMobile,
-                                flags: {mafs: mafsFlagsObject},
-                            }}
-                        />
-                    ))}
+                    {questions
+                        .filter((question) => {
+                            if (!search) {
+                                return true;
+                            }
+                            const widgetKey = Object.keys(question.widgets)[0];
+                            const widget = question.widgets[widgetKey];
+                            switch (widget.type) {
+                                case "grapher":
+                                    if (
+                                        widget.options.availableTypes.some(
+                                            (type: string) =>
+                                                type.includes(search),
+                                        )
+                                    ) {
+                                        return true;
+                                    }
+                                    break;
+                                case "interactive-graph":
+                                    if (
+                                        widget.options.graph.type.includes(
+                                            search,
+                                        )
+                                    ) {
+                                        return true;
+                                    }
+                                    break;
+                                case "number-line":
+                                    return widget.type.includes(search);
+                                default:
+                                    return false;
+                            }
+                        })
+                        .map((question, i) => (
+                            <QuestionRenderer
+                                key={i}
+                                question={question}
+                                apiOptions={{
+                                    isMobile,
+                                    flags: {mafs: mafsFlagsObject},
+                                }}
+                            />
+                        ))}
                 </View>
             </main>
         </View>
