4dc9db87b17191d11e58015ea8938f1bb2ca041b
SonicScrewdriver <sarahthird@gmail.com>
2024-03-20 13:30:49 +0000

    Deploying to gh-pages from @ Khan/perseus@20a23a3976a70378e86c8871e47d5497ddc9d51e 🚀

---
+++ assets/marker.stories-Xe_Nl0lf.js
@@ -0,0 +1,31 @@
+import{j as p}from"./jsx-runtime-BGVbfQ2Z.js";import{a as i}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{l}from"./index-awljIyHI.js";import{M as u}from"./marker-hHOZZgS-.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./article-renderer-z9qEqjFM.js";import"./compare-VLxlEkSV.js";import"./version-akiLXZts.js";import"./index-default-4_ZsnO94.js";import"./prop-types-Gn33kjDK.js";import"./index-hYQ6Pa3_.js";import"./index-V35CFGao.js";import"./index-J2t_5nK1.js";import"./index-E09jvG0x.js";import"./index-S-YHCjun.js";import"./index-eZ2N530f.js";import"./tabbar-IAxGP4dM.js";import"./item-fHYIN_c3.js";import"./index-4JcgVDnF.js";import"./index-tvtfaFq4.js";import"./index-ACL0N2lY.js";import"./button-assets-K2ZoY3Yc.js";import"./keypad-button-qf5RMNu9.js";import"./operators-page-xJJKH4dX.js";import"./navigation-pad-cRy775HL.js";import"./index-uu39Elyn.js";import"./key-translator-M5qX6vAz.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-uh8HDrJD.js";import"./index-awCzqCwC.js";import"./jquery-5v7aFUvu.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-nxiRA6cF.js";import"./index-e6QWP3dX.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-1QHV8SS2.js";import"./fixed-to-responsive-G5J_wmel.js";import"./constants-I_nlPaPx.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-fAaGW1jp.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./perseus-api-Ly41NhN4.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./lint-4QkP-VXi.js";import"./index-vbayBp87.js";import"./index-FQM5b1mh.js";import"./index-A0DvNMpb.js";import"./index-K7FSCCGN.js";import"./index-mXvZDrvB.js";import"./index-SM3muJE2.js";import"./Popper-2p8US95Y.js";import"./math-input-XjO7tPX5.js";import"./unit-tpB2tohZ.js";import"./input-with-examples-dUPfCvEV.js";import"./math-output-o5-UebzW.js";import"./text-input-HIX1uEpz.js";import"./index-S25SDe0M.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-9ElZ8vOy.js";import"./base-radio-AAPbZcYa.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-to-DgQWK.js";import"./icon-HbAIhp4d.js";import"./choice-icon-Sx0010jw.js";import"./focus-ring-4m7DGTUl.js";import"./option-status--OOacwxI.js";import"./choice-none-above-GhVZpnru.js";import"./video-transcript-link-JriNv8Pc.js";import"./answer-choices-rFhkCaUm.js";import"./button-group-eBTrRsKy.js";import"./graph-kPzu5CWq.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-Lk8Jr-mP.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-z5dO9Uz4.js";import"./prop-check-box-TM2oQvxZ.js";import"./range-input-J32YkzGG.js";import"./marker-QeWiWf29.js";import"./answer-pill-jlonfmYf.js";import"./sortable-8cIjJAwG.js";import"./multi-renderer-3tpGlAmF.js";import"./hints-renderer-gT7nZ0XA.js";import"./components-4K8BQnI7.js";import"./util-r1Dnrca2.js";import"./form-wrapped-text-field-aagVPGg6.js";const mo={title:"Perseus/Editor/Widgets/Label Image/Marker"},v=l.StyleSheet.create({wrapper:{marginTop:150,position:"relative",width:32,height:32}}),h=m=>p("div",{className:l.css(v.wrapper),children:p(u,{...m})}),o=m=>p(h,{...{answers:[],choices:[],label:"",onChange:(...r)=>{i("onChange")(...r)},onRemove:(...r)=>{i("onRemove")(...r)},x:50,y:50}}),t=m=>p(h,{...{answers:["BMW","Ferrari"],choices:["Lamborghini","BMW","Volkswagen","Fiat","Porsche","Ferrari"],label:"Automotive",onChange:(...r)=>{i("onChange")(...r)},onRemove:(...r)=>{i("onRemove")(...r)},x:50,y:50}});var e,s,n;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const props = ({
+    answers: [],
+    choices: [],
+    label: "",
+    onChange: (...args) => {
+      action("onChange")(...args);
+    },
+    onRemove: (...args) => {
+      action("onRemove")(...args);
+    },
+    x: 50,
+    y: 50
+  } as const);
+  return <Wrapper {...props} />;
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var a,c,g;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const props = ({
+    answers: ["BMW", "Ferrari"],
+    choices: ["Lamborghini", "BMW", "Volkswagen", "Fiat", "Porsche", "Ferrari"],
+    label: "Automotive",
+    onChange: (...args) => {
+      action("onChange")(...args);
+    },
+    onRemove: (...args) => {
+      action("onRemove")(...args);
+    },
+    x: 50,
+    y: 50
+  } as const);
+  return <Wrapper {...props} />;
+}`,...(g=(c=t.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const eo=["Empty","WithAnswers"];export{o as Empty,t as WithAnswers,eo as __namedExportsOrder,mo as default};
