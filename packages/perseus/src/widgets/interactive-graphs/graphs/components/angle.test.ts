07cca10fc34c2630814d12012eafe9f7eafc9cdc
Ned Redmond <edward@khanacademy.org>
2024-03-25 16:08:45 -0400

    renorm tests

--- packages/perseus/src/widgets/interactive-graphs/graphs/components/angle.test.ts
+++ packages/perseus/src/widgets/interactive-graphs/graphs/components/angle.test.ts
@@ -1,5 +1,6 @@
 import {shouldDrawArcOutside} from "./angle";
 
+import type {CollinearTuple} from "../../../../perseus-types";
 import type {vec, Interval} from "mafs";
 
 describe("shouldDrawArcOutside", () => {
@@ -7,74 +8,87 @@ describe("shouldDrawArcOutside", () => {
         [-1, 6],
         [-1, 6],
     ] satisfies [Interval, Interval];
-    const polygonPoints = [
-        [3.5, 1.5],
-        [3.5, 3.5],
-        [1.5, 3.5],
-        [1.5, 1.5],
-    ] satisfies readonly vec.Vector2[];
+    const polygonLines = [
+        [
+            [3.5, 1.5],
+            [3.5, 3.5],
+        ],
+        [
+            [3.5, 3.5],
+            [1.5, 3.5],
+        ],
+        [
+            [1.5, 3.5],
+            [1.5, 1.5],
+        ],
+        [
+            [1.5, 1.5],
+            [3.5, 1.5],
+        ],
+    ] satisfies readonly CollinearTuple[];
 
     it.each<{
         midpoint: vec.Vector2;
         vertex: vec.Vector2;
         range: [Interval, Interval];
-        polygonPoints: readonly vec.Vector2[];
+        polygonLines: readonly CollinearTuple[];
     }>([
         {
             midpoint: [3.2, 1.8],
             vertex: [3.5, 1.5],
             range,
-            polygonPoints,
+            polygonLines,
         },
         {
             midpoint: [3.2, 3.2],
             vertex: [3.5, 3.5],
             range,
-            polygonPoints,
+            polygonLines,
         },
         {
             midpoint: [1.8, 3.2],
             vertex: [1.5, 3.5],
             range,
-            polygonPoints,
+            polygonLines,
         },
         {
             midpoint: [1.8, 1.8],
             vertex: [1.5, 1.5],
             range,
-            polygonPoints,
+            polygonLines,
         },
     ])("should return false for all four angles in a quadrilateral", (args) => {
-        const {midpoint, vertex, range, polygonPoints} = args;
+        const {midpoint, vertex, range, polygonLines} = args;
         expect(
-            shouldDrawArcOutside(midpoint, vertex, range, polygonPoints),
+            shouldDrawArcOutside(midpoint, vertex, range, polygonLines),
         ).toBe(false);
     });
 
     it("should return true for a reflex angle inside a polygon", () => {
         expect(
             shouldDrawArcOutside(
-                [2.810263340389897, 2.1897366596101024],
-                [3, 2],
+                [2.2395270573626624, 2.415106216609137],
+                [2.5, 2.75],
                 range,
                 [
-                    [3.5, 1.5],
-                    [3.5, 3.5],
-                    [3, 2],
-                    [1.5, 1.5],
+                    [
+                        [0.5, 2.5],
+                        [3.25, 3.75],
+                    ],
+                    [
+                        [3.25, 3.75],
+                        [2.75, 0.75],
+                    ],
+                    [
+                        [2.75, 0.75],
+                        [2.5, 2.75],
+                    ],
+                    [
+                        [2.5, 2.75],
+                        [0.5, 2.5],
+                    ],
                 ],
             ),
-        ).toBe(true);
-    });
-
-    it("should return true for a 270 degree (reverse of right) angle inside a polygon", () => {
-        expect(
-            shouldDrawArcOutside([1.2, 1.2], [1.5, 1.5], range, [
-                [1.5, -0.5],
-                [2, 5.25],
-                [-0.25, 1.5],
-                [1.5, 1.5],
-            ]),
         ).toBe(true);
     });
 });
