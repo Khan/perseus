ed0412b17927834046683a1034973192d4cc4bc6
Matthew Curtis <matthewcurtis@khanacademy.org>
2024-04-02 11:13:17 -0500

    add some tests

--- packages/perseus/src/widgets/interactive-graphs/grid.tsx
+++ packages/perseus/src/widgets/interactive-graphs/grid.tsx
@@ -14,24 +14,39 @@ interface GridProps {
     markings: "graph" | "grid" | "none";
 }
 
-const renderLineLabel = (
+/**
+ * lineLabelText get the text that should
+ * be displayed for a specific tick place
+ *
+ * @param {number} n the tick number
+ * @param {number} tickStep how frequently a label should appear
+ * @param {vec.Vector2} range the min/max range on the axis
+ */
+export const lineLabelText = (
     n: number,
-    axisTickStep: number,
-    axisRange: vec.Vector2,
-) => {
-    const [min, max] = axisRange;
-    const isOnStep = n % axisTickStep === 0;
+    tickStep: number,
+    range: vec.Vector2,
+): string => {
+    const [min, max] = range;
+    const isOnStep = n % tickStep === 0;
     const isNegativeOne = n === -1;
     const isMin = n === min;
     const isMax = n === max;
     const shouldRender = isOnStep && !isNegativeOne && !isMin && !isMax;
-    return shouldRender ? n : "";
+    return shouldRender ? `${n}` : "";
 };
 
-// axisIndex is for grabbing data in an array that contains
-// data for multiple axes. For example range: [[-10, 10], [-10, 10]]
-// range[0] is data for the x axis and range[1] is data for the y axis
-const axisOptions = (
+/**
+ * axisOptions determine axis options for Mafs
+ *
+ * axisIndex is for grabbing data in an array that contains
+ * data for multiple axes. For example range: [[-10, 10], [-10, 10]]
+ * range[0] is data for the x axis and range[1] is data for the y axis
+ *
+ * @param {GridProps} props
+ * @param {number} axisIndex which axis we're getting options for
+ */
+export const axisOptions = (
     props: Omit<GridProps, "containerSizeClass">,
     axisIndex: number,
 ) => {
@@ -40,7 +55,7 @@ const axisOptions = (
     return {
         axis: props.markings === "graph",
         lines: props.gridStep[axisIndex],
-        labels: (n: number) => renderLineLabel(n, axisStep, axisRange),
+        labels: (n: number) => lineLabelText(n, axisStep, axisRange),
     };
 };
 
