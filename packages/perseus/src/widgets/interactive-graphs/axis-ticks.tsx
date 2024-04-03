22d17bbdca098b8bc7252aa3f773e1acc8d8b95c
Nicole Watts <nicole@khanacademy.org>
2024-04-03 12:04:46 -0600

    wip: working ticks

--- packages/perseus/src/widgets/interactive-graphs/axis-ticks.tsx
+++ packages/perseus/src/widgets/interactive-graphs/axis-ticks.tsx
@@ -1,65 +1,94 @@
-// import {color as WBColor} from "@khanacademy/wonder-blocks-tokens";
 import * as React from "react";
 
 import {useTransform} from "./graphs/use-transform";
 
 import type {vec} from "mafs";
 
-// import type {vec} from "mafs";
+const tickSize = 10;
+
+const tickStyle: React.CSSProperties = {
+    stroke: "black",
+    strokeWidth: 1,
+};
+
+const YGridTick = ({y}: {y: number}) => {
+    const startingPoint: vec.Vector2 = [0, y];
+    let [startPtPx] = useTransform(startingPoint);
+
+    const endPtPx = [startPtPx[0] + tickSize / 2, startPtPx[1]];
+    startPtPx = [startPtPx[0] - tickSize / 2, startPtPx[1]];
+
+    return (
+        <g className="y-axis-ticks" tabIndex={0} style={{} as any}>
+            <line
+                x1={startPtPx[0]}
+                y1={startPtPx[1]}
+                x2={endPtPx[0]}
+                y2={endPtPx[1]}
+                style={tickStyle}
+            />
+        </g>
+    );
+};
+
+const XGridTick = ({x}: {x: number}) => {
+    const startingPoint: vec.Vector2 = [x, 0];
+    let [startPtPx] = useTransform(startingPoint);
+
+    const endPtPx = [startPtPx[0], startPtPx[1] + tickSize / 2];
+    startPtPx = [startPtPx[0], startPtPx[1] - tickSize / 2];
+
+    return (
+        <g className="x-axis-ticks" tabIndex={0} style={{} as any}>
+            <line
+                x1={startPtPx[0]}
+                y1={startPtPx[1]}
+                x2={endPtPx[0]}
+                y2={endPtPx[1]}
+                style={tickStyle}
+            />
+        </g>
+    );
+};
+
+export function generateTickLocations(
+    tickStep: number,
+    min: number,
+    max: number,
+): number[] {
+    const ticks: number[] = [];
+    for (let i = 0 + tickStep; i < max; i += tickStep) {
+        ticks.push(i);
+    }
+    for (let i = 0 - tickStep; i > min; i -= tickStep) {
+        ticks.push(i);
+    }
+    return ticks;
+}
 
 type Props = {
     tickStep: [number, number];
     range: [[number, number], [number, number]];
 };
 
-//      -
-const YGridTick = ({y}: {y: number}) => {
-    const tickSize = 0.3;
-    const startingPoint: vec.Vector2 = [-tickSize, y];
-    const endingPoint: vec.Vector2 = [tickSize, y];
-
-    const [startPtPx, endPtPx] = useTransform(startingPoint, endingPoint);
-
-    return (
-        <g className="axis-ticks" tabIndex={0} style={{} as any}>
-            <line
-                x1={startPtPx[0]}
-                y1={startPtPx[1]}
-                x2={endPtPx[0]}
-                y2={endPtPx[1]}
-                style={{
-                    stroke: "red",
-                    strokeWidth: 2,
-                }}
-            />
-        </g>
-    );
-};
-
 export const AxisTicks = (props: Props) => {
-    // from (0 + gridStep) to (max y - gridStep) stepping by gridstep
-
-    // We want to loop through y grids and create one at each grid step + -
     const range = props.range;
-    // const [xMin, xMax] = range[0];
+    const [xMin, xMax] = range[0];
     const [yMin, yMax] = range[1];
 
     const yTickStep = props.tickStep[1];
+    const xTickStep = props.tickStep[0];
 
-    const yGridTicks: number[] = [];
-    for (let i = 0 + yTickStep; i < yMax; i += yTickStep) {
-        yGridTicks.push(i);
-    }
-    for (let i = 0 - yTickStep; i > yMin; i -= yTickStep) {
-        yGridTicks.push(i);
-    }
-
-    console.log(yGridTicks);
+    const yGridTicks = generateTickLocations(yTickStep, yMin, yMax);
+    const xGridTicks = generateTickLocations(xTickStep, xMin, xMax);
 
     return (
         <g className="axis-ticks" tabIndex={0} style={{} as any}>
             {yGridTicks.map((y) => {
                 return <YGridTick y={y} key={`y-grid-tick-${y}`} />;
+            })}
+            {xGridTicks.map((x) => {
+                return <XGridTick x={x} key={`x-grid-tick-${x}`} />;
             })}
         </g>
     );
