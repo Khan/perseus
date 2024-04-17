c31a2f74654057be2045aae5b5b26a0f473e244c
Ben Christel <benchristel@khanacademy.org>
2024-04-17 14:58:55 -0700

    Remove unused dispatch function from GraphModel

--- packages/perseus/src/widgets/interactive-graphs/mafs-graph.tsx
+++ packages/perseus/src/widgets/interactive-graphs/mafs-graph.tsx
@@ -122,7 +122,6 @@ export const MafsGraph = (props: MafsGraphProps) => {
         <GraphModelContext.Provider
             value={{
                 state,
-                dispatch,
             }}
         >
             <View
--- packages/perseus/src/widgets/interactive-graphs/reducer/use-graph-model.ts
+++ packages/perseus/src/widgets/interactive-graphs/reducer/use-graph-model.ts
@@ -1,16 +1,13 @@
 import React, {createContext} from "react";
 
-import type {InteractiveGraphAction} from "./interactive-graph-action";
 import type {InteractiveGraphState} from "../types";
 
 type GraphModel = {
     state: InteractiveGraphState;
-    dispatch: React.Dispatch<InteractiveGraphAction>;
 };
 
 const defaultGraphModel = {
     state: {} as any,
-    dispatch: () => {},
 };
 
 export const GraphModelContext = createContext<GraphModel>(defaultGraphModel);
