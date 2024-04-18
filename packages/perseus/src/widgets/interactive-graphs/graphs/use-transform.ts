36f46f093631e198ad7f05f906878fc1f283f183
Tamara <tamara@khanacademy.org>
2024-04-18 09:39:36 -0500

    Update to not use mafs hook
    Need to be able to use these functions outside of a mafs component

--- packages/perseus/src/widgets/interactive-graphs/graphs/use-transform.ts
+++ packages/perseus/src/widgets/interactive-graphs/graphs/use-transform.ts
@@ -1,10 +1,42 @@
-import {useTransformContext, vec} from "mafs";
+import {type Interval, vec} from "mafs";
 
-export const useTransform = (...points: vec.Vector2[]) => {
-    const {viewTransform, userTransform} = useTransformContext();
-    const transformToPx = vec.matrixMult(viewTransform, userTransform);
-    const transformedPoints = points.map((point) =>
-        vec.transform(point, transformToPx),
-    );
-    return transformedPoints;
+import useGraphState from "../reducer/use-graph-state";
+
+const matrixBuilder = vec.matrixBuilder;
+
+export type GraphDimensions = {
+    state: {
+        range: [Interval, Interval];
+    };
+    width: number; // pixels
+    height: number; // pixels
+};
+
+export function vectorToPixel(
+    points: vec.Vector2[],
+    graphState: GraphDimensions,
+    translation: vec.Vector2 = [0, 0],
+) {
+    const {state, width, height} = graphState;
+    const [[xMin, xMax], [yMin, yMax]] = state.range;
+    const transformToPx = matrixBuilder()
+        .translate(...translation)
+        .scale(width / (xMax - xMin), -height / (yMax - yMin))
+        .get();
+    return points.map((p) => vec.transform(p, transformToPx));
+}
+
+export function pointToPixel(
+    points: vec.Vector2[],
+    graphState: GraphDimensions,
+) {
+    const {state} = graphState;
+    const [[xMin], [, yMax]] = state.range;
+    const [a, b] = [-xMin, -yMax];
+    return vectorToPixel(points, graphState, [a, b]);
+}
+
+export const useTransformVectorToPixel = (...points: vec.Vector2[]) => {
+    const graphState = useGraphState();
+    return vectorToPixel(points, graphState);
 };
