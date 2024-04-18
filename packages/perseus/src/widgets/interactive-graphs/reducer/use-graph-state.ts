909df9ca5a5459776c5a26d3f014fdf48b888c0e
Tamara <tamara@khanacademy.org>
2024-04-18 10:02:56 -0500

    Refactor to not use graphOptions
    To be in alignment with a potential future update and to remove a layer of nesting

--- packages/perseus/src/widgets/interactive-graphs/reducer/use-graph-state.ts
+++ packages/perseus/src/widgets/interactive-graphs/reducer/use-graph-state.ts
@@ -3,16 +3,20 @@ import React, {createContext} from "react";
 import type {InteractiveGraphAction} from "./interactive-graph-action";
 import type {InteractiveGraphState} from "../types";
 
-export const GraphStateContext = createContext<{
+type GraphContext = {
     state: InteractiveGraphState;
     dispatch: React.Dispatch<InteractiveGraphAction>;
-    graphOptions: {
-        [key: string]: any;
-    };
-}>({
+    width: number; // pixels
+    height: number; // pixels
+    labels: readonly string[];
+};
+
+export const GraphStateContext = createContext<GraphContext>({
     state: {} as any,
     dispatch: () => {},
-    graphOptions: {},
+    labels: [],
+    width: 0,
+    height: 0,
 });
 
 export default function useGraphState() {
