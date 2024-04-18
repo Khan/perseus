102ea882dc527e9a505ff1c0a89f83c5a23e6df3
Tamara <tamara@khanacademy.org>
2024-04-18 09:34:59 -0500

    Render labels as TeX, use new transform
    Refactors to use new transform and renders labels as TeX outside the mafs components. Also clears comments

--- packages/perseus/src/widgets/interactive-graphs/axis-labels.tsx
+++ packages/perseus/src/widgets/interactive-graphs/axis-labels.tsx
@@ -1,7 +1,8 @@
-import {MathJaxRenderer} from "@khanacademy/mathjax-renderer";
 import React from "react";
 
-import {useTransform} from "./graphs/use-transform";
+import {getDependencies} from "../../dependencies";
+
+import {pointToPixel} from "./graphs/use-transform";
 import useGraphState from "./reducer/use-graph-state";
 
 import type {vec} from "mafs";
@@ -10,58 +11,38 @@ import "@khanacademy/mathjax-renderer/src/css/safari-hacks.css";
 import "@khanacademy/mathjax-renderer/src/css/selectable.css";
 
 export default function AxisLabels(props) {
-    const {state, graphOptions} = useGraphState();
+    const {state, graphOptions, width, height} = useGraphState();
 
-    const yAxisLabelLocation: vec.Vector2 = [0, state.range[1][1]];
+    const yAxisLabelLocation: vec.Vector2 = [0, state.range[1][1] + 1];
     const xAxisLabelLocation: vec.Vector2 = [state.range[0][1], 0];
+
     const [xAxisLabelText, yAxisLabelText] = graphOptions.labels;
-    // console.log(graphOptions.labels);
-
-    const [[x1, y1]] = useTransform(xAxisLabelLocation);
-    const [[x2, y2]] = useTransform(yAxisLabelLocation);
-
-    const renderer = new MathJaxRenderer({
-        // shouldFixUnicodeLayout ensures that non-ASCII text is correctly
-        // measured and positioned in e.g. \overbrace and \underbrace expressions.
-        // Set shouldFixUnicodeLayout to false if you're rendering in an
-        // environment without a layout engine, e.g. jsdom.
-        shouldFixUnicodeLayout: true,
-        fontURL: "https://cdn.kastatic.org/fonts/mathjax",
-    });
-
-    // const {domElement} = renderer.render(xAxisLabelText);
-    // console.log(domElement);
-
-    type Props = {
-        children: string;
-        onRender?: (root?: any) => unknown;
+    const graphInfo = {
+        state: {
+            range: state.range,
+        },
+        width,
+        height,
     };
+    const [[x1, y1]] = pointToPixel([xAxisLabelLocation], graphInfo);
+    const [[x2, y2]] = pointToPixel([yAxisLabelLocation], graphInfo);
 
-    function useMathjax({children: tex, onRender}: Props) {
-        const {domElement} = renderer.render(tex);
-
-        React.useEffect(() => {
-            renderer.updateStyles();
-            onRender?.();
-        }, [tex, onRender]);
-        return (
-            <span dangerouslySetInnerHTML={{__html: domElement.outerHTML}} />
-        );
-    }
+    const {TeX} = getDependencies();
 
     return (
-        // Find the length of the label, divide in half, then move the x and y over by that much!
-        // can do this with CSS with transform by moving it left 50%
-        // Need to add some padding left of x axis label and bottom of y axis label
         <>
-            {/*{domElement}*/}
-            {/*{useMathjax(xAxisLabelText)}*/}
-            <text id="xAxisLabel" x={x1} y={y1}>
-                {xAxisLabelText}
-            </text>
-            <text id="yAxisLabel" x={x2} y={y2}>
-                {yAxisLabelText}
-            </text>
+            <span
+                id="x-axis-label"
+                style={{position: "absolute", left: x1, top: y1}}
+            >
+                <TeX>{xAxisLabelText}</TeX>
+            </span>
+            <span
+                id="y-axis-label"
+                style={{position: "absolute", left: x2, top: y2}}
+            >
+                <TeX>{yAxisLabelText}</TeX>
+            </span>
         </>
     );
 }
