ea0bb7869cf7d7ea62b1b4c90ae0447efe5cbf59
jeremywiebe <jeremy.wiebe@gmail.com>
2024-04-22 21:30:00 +0000

    Deploying to gh-pages from @ Khan/perseus@09627c8dde74136234a97a6e708e783797daf511 🚀

---
+++ assets/number-line.stories-bLEoIoHT.js
@@ -0,0 +1,43 @@
+import{j as r}from"./jsx-runtime-BGVbfQ2Z.js";import"./prop-types-1g6fzmKF.js";import"./key-translator-e7mZtIzr.js";import{a as f}from"./index-2LjNjJpa.js";import"./button-assets-_Am9ApvA.js";import{R as m}from"./renderer-with-debug-ui-C9cTUpE-.js";import{S as C}from"./server-item-renderer-with-debug-ui-906WD5sx.js";import{T as k}from"./test-keypad-context-wrapper-uqaGtoIy.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./version-akiLXZts.js";import"./index-V35CFGao.js";import"./index-fuyzzUuV.js";import"./index-J2t_5nK1.js";import"./index-awljIyHI.js";import"./index-E09jvG0x.js";import"./index-tvtfaFq4.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./index-4c2J3ov1.js";import"./tabbar-4UDQk7cN.js";import"./item-P3WrK4af.js";import"./index-1P5txiDe.js";import"./keypad-button-Yf_O0DZf.js";import"./operators-page-jaQPWpBb.js";import"./navigation-pad-JIlZRGXW.js";import"./index-uu39Elyn.js";import"./choice-7Bfg02MO.js";import"./index-Fg8WJp4t.js";import"./index-ntsnGU3X.js";import"./index-fdRf1OJX.js";import"./index-_15Y2y0p.js";import"./index-RrkX9P05.js";import"./index-cRKslbtI.js";import"./Popper-2p8US95Y.js";import"./index-awCzqCwC.js";import"./icon-HbAIhp4d.js";import"./perseus-api-Ly41NhN4.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./jquery-5v7aFUvu.js";import"./index-default-4_ZsnO94.js";import"./media-queries-MaBBbpNq.js";import"./constants-I_nlPaPx.js";import"./choice-icon-YaMguKwM.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-fAaGW1jp.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-hT8t_5a4.js";import"./article-renderer-GO8-X2km.js";import"./compare-VLxlEkSV.js";import"./enums-x5qaTru7.js";import"./renderer-Egk7x_Xi.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-S4XnA7Yr.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-HWMzrHku.js";import"./fixed-to-responsive-G5J_wmel.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./lint-4QkP-VXi.js";import"./math-input-ye6imNwR.js";import"./unit-tpB2tohZ.js";import"./input-with-examples-7DE9GVQy.js";import"./math-output-o5-UebzW.js";import"./text-input-aDhxqzgd.js";import"./index-to4e0yK9.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-AZdDUF7E.js";import"./base-radio-oeuKArw6.js";import"./shared-3pf9YZIg.js";import"./choice-none-above-Bz4XzcPl.js";import"./video-transcript-link-bbZ3NvJD.js";import"./answer-choices-pKfuNCYh.js";import"./button-group-eBTrRsKy.js";import"./graph-NP6c8P1T.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-Fae-O4h-.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-33S79dZV.js";import"./prop-check-box-wRngoh_G.js";import"./range-input-zsZZAHYZ.js";import"./marker-ekh7P5T3.js";import"./answer-pill-Ob0OzX26.js";import"./sortable-nKhXgoyl.js";import"./side-by-side-IRJMha8r.js";import"./multi-renderer-xj0fuLCC.js";import"./hints-renderer-Awf7BYnz.js";import"./register-all-widgets-for-testing-lPs27beE.js";import"./ke-score-ui-2Isq5cmw.js";import"./test-dependencies-dzrq64iv.js";import"./chunk-WFFRPTHA-zlNDxfrq.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";const D={content:`$E=2.5$
+
+**Move the dot to $-E$ on the number line.**
+
+
+[[☃ number-line 1]]`,images:{},widgets:{"number-line 1":{graded:!0,version:{major:0,minor:0},static:!1,type:"number-line",options:{labelRange:[null,null],initialX:null,tickStep:1,labelStyle:"decimal",labelTicks:!0,snapDivisions:2,range:[-4,4],static:!1,correctRel:"eq",numDivisions:null,divisionRange:[1,10],correctX:-2.5},alignment:"default"}}},p={content:`$E=2.5$
+
+**Move the dot to $-E$ on the number line.**
+
+
+[[☃ number-line 1]]`,images:{},widgets:{"number-line 1":{type:"number-line",alignment:"default",static:!1,graded:!0,options:{static:!1,range:[0,1],labelRange:[null,null],labelStyle:"improper",labelTicks:!1,divisionRange:[1,12],numDivisions:1,snapDivisions:1,tickStep:null,correctRel:"eq",correctX:.5,initialX:null,showTooltip:!1,isTickCtrl:!0}}}},bt={title:"Perseus/Widgets/Number Line"},t=n=>r(m,{question:D}),e=n=>r(m,{question:p}),o=n=>r(k,{children:r(f.Consumer,{children:({keypadElement:q})=>r(C,{item:{question:p,_multi:null,answer:null,answerArea:null,itemDataVersion:{major:0,minor:1},hints:[]},apiOptions:{isMobile:!0,customKeypad:!0},keypadElement:q})})}),i=n=>r(m,{apiOptions:{isMobile:!0,customKeypad:!0},question:p});var s,a,l;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <RendererWithDebugUI question={question1} />;
+}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};var u,c,d;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <RendererWithDebugUI question={question2} />;
+}`,...(d=(c=e.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var g,b,y;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <TestKeypadContextWrapper>
+            <KeypadContext.Consumer>
+                {({
+        keypadElement
+      }) => {
+        return <ServerItemRendererWithDebugUI item={({
+          question: question2,
+          _multi: null,
+          answer: null,
+          answerArea: null,
+          itemDataVersion: {
+            major: 0,
+            minor: 1
+          },
+          hints: []
+        } as PerseusItem)} apiOptions={{
+          isMobile: true,
+          customKeypad: true
+        }} keypadElement={keypadElement} />;
+      }}
+            </KeypadContext.Consumer>
+        </TestKeypadContextWrapper>;
+}`,...(y=(b=o.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var R,h,S;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <RendererWithDebugUI apiOptions={{
+    isMobile: true,
+    customKeypad: true
+  }} question={question2} />;
+}`,...(S=(h=i.parameters)==null?void 0:h.docs)==null?void 0:S.source}}};const yt=["Question1","ShowTickController","ShowTickControllerMobile","Question2"];export{t as Question1,i as Question2,e as ShowTickController,o as ShowTickControllerMobile,yt as __namedExportsOrder,bt as default};
--- assets/register-all-widgets-for-testing-tWQnGQDd.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-5XXUYWl6.js";import{r,a as s}from"./renderer-IESC4U7A.js";const a=()=>{r(e),s()};export{a as r};
