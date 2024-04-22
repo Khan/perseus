310e269cd34a0e1b958aec99d4a7ad2f4ca4927f
Ben Christel <benchristel@khanacademy.org>
2024-04-22 13:51:40 -0700

    Add stories

--- packages/perseus/src/widgets/__stories__/interactive-graph.stories.tsx
+++ packages/perseus/src/widgets/__stories__/interactive-graph.stories.tsx
@@ -61,6 +61,134 @@ export const MafsWithFractionalAxisTicks = (
             .build()} />
 );
 
+export const MafsWithGridMarkings = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withMarkings("grid")
+            .build()} />
+);
+
+export const MafsWithNoMarkings = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withMarkings("none")
+            .build()} />
+);
+
+export const MafsWithSmallRange = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withXRange(-2, 2)
+            .withYRange(-2, 2)
+            .build()} />
+);
+
+export const MafsWithLargeRange = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withXRange(-50, 50)
+            .withYRange(-50, 50)
+            .build()} />
+);
+
+export const MafsWithYAxisAtLeft = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withXRange(0, 20)
+            .build()} />
+);
+
+export const MafsWithYAxisNearLeft = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withXRange(-1, 20)
+            .build()} />
+);
+
+export const MafsWithYAxisOffLeft = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withXRange(1, 20)
+            .build()} />
+);
+
+export const MafsWithYAxisAtRight = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withXRange(-20, 0)
+            .build()} />
+);
+
+export const MafsWithYAxisOffRight = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withXRange(-20, -1)
+            .build()} />
+);
+
+export const MafsWithXAxisAtBottom = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withYRange(0, 20)
+            .build()} />
+);
+
+export const MafsWithXAxisNearBottom = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withYRange(-1, 20)
+            .build()} />
+);
+
+export const MafsWithXAxisOffBottom = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withYRange(1, 20)
+            .build()} />
+);
+
+export const MafsWithXAxisAtTop = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withYRange(-20, 0)
+            .build()} />
+);
+
+export const MafsWithXAxisOffTop = (
+    args: StoryArgs,
+): React.ReactElement => (
+    <MafsQuestionRenderer question={
+        interactiveGraphQuestionBuilder()
+            .withYRange(-20, -1)
+            .build()} />
+);
+
 
 export const Angle = (args: StoryArgs): React.ReactElement => (
     <RendererWithDebugUI question={angleQuestion} />
