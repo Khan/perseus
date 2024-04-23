93b55f7f0aace828b9c81bf264d20bc6aec196dc
jeanettehead <1103787+jeanettehead@users.noreply.github.com>
2024-04-23 17:29:31 +0000

    Deploying to gh-pages from @ Khan/perseus@7f77487f092d4f5876ffad70677d5e4d586bcb67 🚀

---
+++ assets/input-number.stories--kzyGGmc.js
@@ -0,0 +1,43 @@
+import{j as n}from"./jsx-runtime-BGVbfQ2Z.js";import{R as p}from"./renderer-with-debug-ui-2lvdDkfO.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./choice-7Bfg02MO.js";import"./index-1P5txiDe.js";import"./index-awljIyHI.js";import"./index-tvtfaFq4.js";import"./index-4c2J3ov1.js";import"./index-fuyzzUuV.js";import"./index-Fg8WJp4t.js";import"./index-ntsnGU3X.js";import"./index-fdRf1OJX.js";import"./index-E09jvG0x.js";import"./index-uu39Elyn.js";import"./index-_15Y2y0p.js";import"./index-RrkX9P05.js";import"./index-V35CFGao.js";import"./index-cRKslbtI.js";import"./Popper-2p8US95Y.js";import"./index-awCzqCwC.js";import"./icon-HbAIhp4d.js";import"./perseus-api-Ly41NhN4.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./jquery-5v7aFUvu.js";import"./index-default-4_ZsnO94.js";import"./media-queries-MaBBbpNq.js";import"./constants-I_nlPaPx.js";import"./choice-icon-YaMguKwM.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-fAaGW1jp.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-hT8t_5a4.js";import"./article-renderer-VVo0Q1SL.js";import"./compare-VLxlEkSV.js";import"./version-akiLXZts.js";import"./prop-types-1g6fzmKF.js";import"./index-J2t_5nK1.js";import"./index-2LjNjJpa.js";import"./tabbar-4UDQk7cN.js";import"./item-P3WrK4af.js";import"./button-assets-_Am9ApvA.js";import"./keypad-button-Yf_O0DZf.js";import"./operators-page-jaQPWpBb.js";import"./navigation-pad-JIlZRGXW.js";import"./key-translator-e7mZtIzr.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-Egk7x_Xi.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-S4XnA7Yr.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-HWMzrHku.js";import"./fixed-to-responsive-G5J_wmel.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./lint-4QkP-VXi.js";import"./math-input-ye6imNwR.js";import"./unit-tpB2tohZ.js";import"./input-with-examples-7DE9GVQy.js";import"./math-output-o5-UebzW.js";import"./text-input-aDhxqzgd.js";import"./index-to4e0yK9.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-AZdDUF7E.js";import"./base-radio-oeuKArw6.js";import"./shared-3pf9YZIg.js";import"./choice-none-above-Bz4XzcPl.js";import"./video-transcript-link-bbZ3NvJD.js";import"./answer-choices-pKfuNCYh.js";import"./button-group-eBTrRsKy.js";import"./graph-NP6c8P1T.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-Fae-O4h-.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-33S79dZV.js";import"./prop-check-box-wRngoh_G.js";import"./range-input-zsZZAHYZ.js";import"./marker-ekh7P5T3.js";import"./answer-pill-Ob0OzX26.js";import"./sortable-nKhXgoyl.js";import"./side-by-side-IRJMha8r.js";import"./multi-renderer-vUSQFoHT.js";import"./hints-renderer-MGnuzrWI.js";import"./register-all-widgets-for-testing-V-HxjxWH.js";const Z={content:`Denis baked a peach pie and cut it into $3$ equal-sized pieces.  Denis's dad eats $1$ section of the pie.  
+
+**What fraction of the pie did Denis's dad eat?**  
+![](https://ka-perseus-graphie.s3.amazonaws.com/74a2b7583a2c26ebfb3ad714e29867541253fc97.png)    
+[[☃ input-number 1]]  
+
+
+
+`,images:{"https://ka-perseus-graphie.s3.amazonaws.com/74a2b7583a2c26ebfb3ad714e29867541253fc97.png":{width:200,height:200}},widgets:{"input-number 1":{version:{major:0,minor:0},type:"input-number",graded:!0,alignment:"default",options:{maxError:.1,inexact:!1,value:.3333333333333333,simplify:"optional",answerType:"rational",size:"normal"}}}},w={content:`A washing machine is being redesigned to handle a greater volume of water.  One part is a pipe with a radius of $3 \\,\\text{cm}$ and a length of $11\\,\\text{cm}$.  It gets replaced with a pipe of radius $4\\,\\text{cm}$, and the same length. 
+
+**How many more cubic centimeters of water can the new pipe hold?**
+
+ [[☃ input-number 1]] $\\text{cm}^3$`,images:Object.freeze({}),widgets:{"input-number 1":{type:"input-number",graded:!0,options:{maxError:.1,inexact:!1,value:241.90263432641407,simplify:"required",answerType:"pi",size:"normal"}}}},f={content:`Akshat works in a hospital lab.
+
+To project blood quantities, he wants to know the probability that more than $1$ of the next $7$ donors will have type-A blood. From his previous work, Sorin knows that $\\dfrac14$ of donors have type-A blood.
+
+Akshat uses a computer to produce many samples that simulate the next $7$ donors. The first $8$ samples are shown in the table below where "$\\text{\\red{A}}$" represents a donor *with* type-A blood, and "$\\text{\\blue{Z}}$" represents a donor *without* type-A blood.
+
+**Based on the samples below, estimate the probability that  more than $1$ of the next $7$ donors will have type-A blood.** If necessary, round your answer to the nearest hundredth. [[☃ input-number 1]]
+
+*Note: This a small sample to practice with. A larger sample could give a much better estimate.*
+
+ | Sample |
+:-: | :-: | 
+$1$ | $\\text{\\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\red{A}, \\blue{Z}, \\blue{Z}}$
+$2$ | $\\text{\\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}}$
+$3$ | $\\text{\\blue{Z}, \\blue{Z}, \\red{A}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}}$
+$4$ | $\\text{\\red{A}, \\red{A}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}}$
+$5$ | $\\text{\\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\red{A}, \\red{A}}$
+$6$ | $\\text{\\blue{Z}, \\red{A}, \\red{A}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}}$
+$7$ | $\\text{\\blue{Z}, \\red{A}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\red{A}, \\blue{Z}}$
+$8$ | $\\text{\\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\red{A}, \\blue{Z}, \\blue{Z}}$
+
+`,images:Object.freeze({}),widgets:{"input-number 1":{type:"input-number",graded:!0,options:{maxError:.1,inexact:!1,value:.5,simplify:"optional",answerType:"percent",size:"small"}}}},nt={title:"Perseus/Widgets/InputNumber",argTypes:{maxError:{control:{type:"range",min:0,max:1,step:.1}},inexact:{control:{type:"boolean"}},value:{control:{type:"number"}},simplify:{control:{type:"select",options:["required","optional","enforced"]}},answerType:{control:{type:"select",options:["number","decimal","integer","rational","improper","mixed","percent","pi"]}},size:{control:{type:"select",options:["normal","small"]}},rightAlign:{control:{type:"boolean"}}}},s=(e,t,y)=>{const a=e.widgets[t];return{...e,widgets:{[t]:{...a,options:{...a.options,...y}}}}},r=e=>{const t=s(Z,"input-number 1",e);return n(p,{question:t})};r.args=Z.widgets["input-number 1"].options;const o=e=>{const t=s(w,"input-number 1",e);return n(p,{question:t})};o.args=w.widgets["input-number 1"].options;const i=e=>{const t=s(f,"input-number 1",e);return n(p,{question:t})};i.args=f.widgets["input-number 1"].options;var m,u,l;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`(args: InputNumberOptions): React.ReactElement => {
+  const question = updateWidgetOptions(question1, "input-number 1", args);
+  return <RendererWithDebugUI question={question} />;
+}`,...(l=(u=r.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var b,d,c;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`(args: InputNumberOptions): React.ReactElement => {
+  const question = updateWidgetOptions(question2, "input-number 1", args);
+  return <RendererWithDebugUI question={question} />;
+}`,...(c=(d=o.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var $,h,g;i.parameters={...i.parameters,docs:{...($=i.parameters)==null?void 0:$.docs,source:{originalSource:`(args: InputNumberOptions): React.ReactElement => {
+  const question = updateWidgetOptions(question3, "input-number 1", args);
+  return <RendererWithDebugUI question={question} />;
+}`,...(g=(h=i.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const pt=["Rational","PiSimplify","Percent"];export{i as Percent,o as PiSimplify,r as Rational,pt as __namedExportsOrder,nt as default};
