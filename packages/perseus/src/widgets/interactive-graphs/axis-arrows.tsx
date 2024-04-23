8b040c98b138b885de7b0313e0649ace8be721a8
Ben Christel <benchristel@khanacademy.org>
2024-04-23 11:27:47 -0700

    Reorder Arrowhead elements

--- packages/perseus/src/widgets/interactive-graphs/axis-arrows.tsx
+++ packages/perseus/src/widgets/interactive-graphs/axis-arrows.tsx
@@ -12,9 +12,9 @@ export default function AxisArrows() {
 
     return (
         <>
+            <Arrowhead color={axisColor} x={xMin} y={0} angle={180} />
             <Arrowhead color={axisColor} x={xMax} y={0} angle={0} />
             <Arrowhead color={axisColor} x={0} y={yMin} angle={270} />
-            <Arrowhead color={axisColor} x={xMin} y={0} angle={180} />
             <Arrowhead color={axisColor} x={0} y={yMax} angle={90} />
         </>
     );
