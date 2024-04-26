e4699f00d145cae72191ca9093bcd1b5bd488460
nishasy <nisha@khanacademy.org>
2024-04-26 21:03:32 +0000

    Deploying to gh-pages from @ Khan/perseus@2d3c3b49a652020e5bf662b7b19682fa94212755 🚀

---
+++ assets/editor-jsonify-TeR0H2Xw.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index-default-4_ZsnO94.js";import{d as i}from"./unit-1L4OY2_r.js";const s={serialize:function(){return o.omit(this.props,i)}};export{s as E};
--- assets/unit.stories-ExaGxAlu.js
+++
@@ -1,7 +0,0 @@
-import{j as i}from"./jsx-runtime-BGVbfQ2Z.js";import{A as n}from"./perseus-api-Ly41NhN4.js";import{O as s}from"./unit-XlEkhd7t.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-tvtfaFq4.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./jquery-5v7aFUvu.js";import"./index-E09jvG0x.js";import"./index-default-4_ZsnO94.js";import"./compare-59FC1ybr.js";import"./version-akiLXZts.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./perseus-error-OpXxk17X.js";import"./dependencies-fnqF3NiV.js";const j={title:"Perseus/Widgets/Unit"},t=p=>i(s,{apiOptions:{...n.defaults},value:"2 tbsp",onFocus:()=>{},onBlur:()=>{},onChange:({value:a})=>{}});var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
-  return <OldUnitInput apiOptions={{
-    ...ApiOptions.defaults
-  }} value="2 tbsp" onFocus={() => {}} onBlur={() => {}} onChange={({
-    value
-  }) => {}} />;
-}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const B=["NonStaticRender"];export{t as NonStaticRender,B as __namedExportsOrder,j as default};
