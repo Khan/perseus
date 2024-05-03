307803021ff665fb3f1f7360c061bf3786d15009
Ben Christel <benchristel@khanacademy.org>
2024-05-03 12:12:41 -0700

    Use Segment component in Ray graphs

--- packages/perseus/src/widgets/interactive-graphs/graphs/ray.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/ray.tsx
@@ -2,11 +2,9 @@ import * as React from "react";
 
 import {moveControlPoint, moveLine} from "../reducer/interactive-graph-action";
 
-import {MovableLine} from "./components/movable-line";
-import {StyledMovablePoint} from "./components/movable-point";
-
 import type {MafsGraphProps, RayGraphState} from "../types";
 import type {vec} from "mafs";
+import {Segment} from "./components/segment";
 
 type Props = MafsGraphProps<RayGraphState>;
 
@@ -14,32 +12,22 @@ export const RayGraph = (props: Props) => {
     const {dispatch} = props;
     const {coords: lines, range} = props.graphState;
 
-    // a ray only has one line
-    const [start, end] = lines[0];
-
     const handleMoveLine = (delta: vec.Vector2) => dispatch(moveLine(0, delta));
-    const handleMovePoint = (newPoint: vec.Vector2, pointIndex: number) =>
+    const handleMovePoint = (pointIndex: number, newPoint: vec.Vector2) =>
         dispatch(moveControlPoint(pointIndex, newPoint, 0));
 
     return (
         <>
-            <MovableLine
-                start={start}
-                end={end}
-                onMove={handleMoveLine}
+            <Segment
+                // a ray only has one line
+                points={lines[0]}
+                onMoveLine={handleMoveLine}
+                onMovePoint={handleMovePoint}
                 extend={{
-                    end: true,
                     start: false,
+                    end: true,
                     range,
                 }}
-            />
-            <StyledMovablePoint
-                point={start}
-                onMove={(newPoint) => handleMovePoint(newPoint, 0)}
-            />
-            <StyledMovablePoint
-                point={end}
-                onMove={(newPoint) => handleMovePoint(newPoint, 1)}
             />
         </>
     );
