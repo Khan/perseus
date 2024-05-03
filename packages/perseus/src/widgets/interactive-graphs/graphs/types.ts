9031d61f232038274a349a6cc6fb9355a2563e69
Ben Christel <benchristel@khanacademy.org>
2024-05-03 12:23:17 -0700

    Delete unused type

--- packages/perseus/src/widgets/interactive-graphs/graphs/types.ts
+++
@@ -1,8 +0,0 @@
-import type {vec} from "mafs";
-
-// FIXME: this type can probably be removed after the current refactoring
-export interface InteractiveLineProps {
-    points: Readonly<[vec.Vector2, vec.Vector2]>;
-    onMovePoint: (endpointIndex: number, destination: vec.Vector2) => unknown;
-    onMoveLine: (delta: vec.Vector2) => unknown;
-}
