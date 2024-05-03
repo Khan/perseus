52b197d8b160f035301f79529c0f5cd11bb858db
Ben Christel <benchristel@khanacademy.org>
2024-05-03 12:18:22 -0700

    Inline LineView

--- packages/perseus/src/widgets/interactive-graphs/graphs/linear.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/linear.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import {moveControlPoint, moveLine} from "../reducer/interactive-graph-action";
 import useGraphConfig from "../reducer/use-graph-config";
 
-import type {InteractiveLineProps} from "./types";
 import type {MafsGraphProps, LinearGraphState} from "../types";
 import type {vec} from "mafs";
 import {Segment} from "./components/segment";
@@ -14,16 +13,23 @@ export const LinearGraph = (props: LinearGraphProps) => {
     const {dispatch} = props;
     const {coords: lines} = props.graphState;
 
+    const {range} = useGraphConfig();
+
     const colors = ["var(--movable-line-stroke-color)", "var(--mafs-violet)"];
 
     return (
         <>
             {lines?.map((line, i) => (
-                <LineView
+                <Segment
                     key={i}
                     points={line}
                     onMoveLine={(delta: vec.Vector2) => {
                         dispatch(moveLine(i, delta));
+                    }}
+                    extend={{
+                        start: true,
+                        end: true,
+                        range,
                     }}
                     onMovePoint={(
                         endpointIndex: number,
@@ -36,37 +42,6 @@ export const LinearGraph = (props: LinearGraphProps) => {
                     stroke={colors[i]}
                 />
             ))}
-        </>
-    );
-};
-
-interface LineViewProps extends InteractiveLineProps {
-    stroke: string;
-}
-
-const LineView = (props: LineViewProps) => {
-    const {
-        onMoveLine,
-        onMovePoint,
-        points,
-        stroke,
-    } = props;
-
-    const {range} = useGraphConfig();
-
-    return (
-        <>
-            <Segment
-                points={points}
-                onMoveLine={onMoveLine}
-                onMovePoint={onMovePoint}
-                extend={{
-                    start: true,
-                    end: true,
-                    range,
-                }}
-                stroke={stroke}
-            />
         </>
     );
 };
