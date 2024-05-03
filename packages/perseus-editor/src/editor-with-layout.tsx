ab0922d61a8bd33f6ccea6033fdafbd86314ba38
Jeremy Wiebe <jeremy@khanacademy.org>
2024-05-03 16:28:12 -0700

    Introduce EditorWithLayout

---
+++ packages/perseus-editor/src/editor-with-layout.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import type {APIOptions} from "@khanacademy/perseus";
+
+type Props = {
+    apiOptions: APIOptions;
+};
+
+function EditorWithLayout(props: Props) {
+    return <div>A very nice editor, indeed!</div>;
+}
+
+export default EditorWithLayout;
