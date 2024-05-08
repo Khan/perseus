04c75e1456c5c478a83db77fe85dd12049a21992
jeresig <jeresig@gmail.com>
2024-05-08 20:26:15 +0000

    Deploying to gh-pages from @ Khan/perseus@9fff34db46d9cdb82366b3f847bacfeb30c124c5 🚀

---
+++ assets/register-all-widgets-for-testing-X3jJrJs9.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-S9mLKjV0.js";import{r,a as s}from"./renderer-V6_4AklZ.js";const a=()=>{r(e),s()};export{a as r};
--- assets/register-all-widgets-for-testing-ZJ236cyI.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-bAZOtgAk.js";import{r,a as s}from"./renderer-yhix436O.js";const a=()=>{r(e),s()};export{a as r};
