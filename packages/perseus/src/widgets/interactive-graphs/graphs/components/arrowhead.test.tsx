d341a6646ad34bb4c504552d18e6afbba2b8d656
Ben Christel <benchristel@users.noreply.github.com>
2024-05-08 12:25:04 -0700

    Suppress spurious errors logged by tests (#1249)
    ## Summary:
    Our Jest tests were logging that `<g>` is not a recognized HTML element.
    That's because it's not HTML; it's only valid in SVGs.
    
    The noisy errors made it harder to spot actual test failures in the
    output. This commit wraps the rendered elements in an `<svg>` tag to
    prevent the errors.
    
    Issue: none
    
    Test plan:
    
    `yarn test`
    
    Author: benchristel
    
    Reviewers: jeremywiebe
    
    Required Reviewers:
    
    Approved By: jeremywiebe
    
    Checks: ✅ codecov/project, ✅ codecov/patch, ✅ Upload Coverage, ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Jest Coverage (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/1249

--- packages/perseus/src/widgets/interactive-graphs/graphs/components/arrowhead.test.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/components/arrowhead.test.tsx
@@ -6,7 +6,9 @@ import {Arrowhead} from "./arrowhead";
 describe("Arrowhead", () => {
     it("Uses provided color to fill the arrow", () => {
         const {container} = render(
-            <Arrowhead tip={[0, 0]} angle={0} color="green" />,
+            <svg>
+                <Arrowhead tip={[0, 0]} angle={0} color="green" />
+            </svg>,
         );
         // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
         const svgArrow = container.querySelector("path");
@@ -14,7 +16,11 @@ describe("Arrowhead", () => {
     });
 
     it("Inherits the color of the parent element when no color is specified", () => {
-        const {container} = render(<Arrowhead tip={[0, 0]} angle={0} />);
+        const {container} = render(
+            <svg>
+                <Arrowhead tip={[0, 0]} angle={0} />
+            </svg>,
+        );
         // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
         const svgArrow = container.querySelector("path");
         expect(svgArrow?.getAttribute("style")).toContain("stroke: inherit");
