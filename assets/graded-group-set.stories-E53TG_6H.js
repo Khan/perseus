bb0a118b436a1f5f11aa45d474d2fd85cbe055ee
benchristel <benchristel@users.noreply.github.com>
2024-05-08 19:26:29 +0000

    Deploying to gh-pages from @ Khan/perseus@d341a6646ad34bb4c504552d18e6afbba2b8d656 🚀

---
+++ assets/graded-group-set.stories-E53TG_6H.js
@@ -0,0 +1,93 @@
+import{j as o}from"./jsx-runtime-BGVbfQ2Z.js";import{R as s}from"./renderer-with-debug-ui-AeG51Rdn.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./choice-iXWfcVOh.js";import"./index-IxqgEL7X.js";import"./index-awljIyHI.js";import"./index-tvtfaFq4.js";import"./index-4c2J3ov1.js";import"./index-25YgVP-A.js";import"./index-Fg8WJp4t.js";import"./index-Or3qlYxC.js";import"./index-PurVa-Tf.js";import"./index-E09jvG0x.js";import"./index-pb777vIT.js";import"./index-_15Y2y0p.js";import"./index-BrnICqZg.js";import"./index-wpOEyefj.js";import"./index-BDbMqVg2.js";import"./Popper-2p8US95Y.js";import"./index-awCzqCwC.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./icon-HbAIhp4d.js";import"./perseus-api-Ly41NhN4.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./jquery-5v7aFUvu.js";import"./index-default-4_ZsnO94.js";import"./media-queries-MaBBbpNq.js";import"./constants-I_nlPaPx.js";import"./choice-icon-5Wq30UUO.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-AuJwhOz7.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-_ONChKTK.js";import"./article-renderer-xc40E-D8.js";import"./compare-VLxlEkSV.js";import"./version-akiLXZts.js";import"./prop-types-YcoB0qCk.js";import"./index-J2t_5nK1.js";import"./index-NYf4zfpa.js";import"./tabbar-0ADFFMFf.js";import"./item-Xl3u6w2R.js";import"./button-assets-cmoMUwP4.js";import"./keypad-button-Xf3EMv8Q.js";import"./operators-page-oziijydk.js";import"./navigation-pad-tBxIIqLP.js";import"./key-translator-Aeoa5q9U.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-V6_4AklZ.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-6q36axAB.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-WvQWLN3r.js";import"./fixed-to-responsive-G5J_wmel.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./lint-4QkP-VXi.js";import"./math-input-fbL2xS9C.js";import"./unit-1L4OY2_r.js";import"./input-with-examples-3Q0GkN8q.js";import"./math-output-o5-UebzW.js";import"./text-input-77oq4TCL.js";import"./index-sXD0g-3O.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-BXoHdEoM.js";import"./base-radio-T9lap48E.js";import"./shared-3pf9YZIg.js";import"./choice-none-above-3ZColgvQ.js";import"./video-transcript-link-wp-Fzerj.js";import"./answer-choices-dZYesy7J.js";import"./button-group-eBTrRsKy.js";import"./graph-kG_TBAvu.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-IhIrlX7t.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-CBSxyfjJ.js";import"./prop-check-box-OYeLup7m.js";import"./range-input-YYhLn03P.js";import"./marker-KayRHL5C.js";import"./answer-pill-TDnlkTLI.js";import"./sortable-6KjeHSiM.js";import"./side-by-side-qocKyGpV.js";import"./multi-renderer-0kvc0mM6.js";import"./hints-renderer-TcKGyWce.js";import"./register-all-widgets-for-testing-Wdv1F8yJ.js";const n={content:`#Section 1: Adding tenths less than one
+
+[[☃ graded-group-set 1]]
+
+
+Beautiful, let's move on to problems with whole numbers and tenths.`,images:{},widgets:{"graded-group-set 1":{type:"graded-group-set",alignment:"default",static:!1,graded:!0,options:{gradedGroups:[{title:"Problem 1a",content:"$0.5 + 0.4 =$   [[☃ numeric-input 1]]",widgets:{"numeric-input 1":{type:"numeric-input",alignment:"default",static:!1,graded:!0,options:{static:!1,answers:[{value:.9,status:"correct",message:"",simplify:"required",strict:!1,maxError:null}],size:"normal",coefficient:!1,labelText:""},version:{major:0,minor:0}}},images:{},hasHint:!0,hint:{content:`There are many ways to solve this problem. Let's see two student solutions.
+
+###Student A's solution:
+
+I thought in terms of tenths.
+
+$\\phantom{=}0.5 + 0.4$
+
+$=5$ tenths $+ ~4$ tenths
+
+$=9$ tenths
+
+$=0.9$
+
+###Student B's solution:
+
+I used tenths grids.
+
+[[☃ image 1]]
+
+[[☃ image 2]]
+
+[[☃ image 3]]
+
+$\\blueD{0.5} + \\greenD{0.4} = 0.9$
+
+###The answer:
+
+$0.5 + 0.4 = 0.9$`,images:{},widgets:{"image 3":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[234,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/cda00c119dac3e52c8ed150ef4a9a37355f5c713",width:234,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 5 of the rows are shaded in blue and 4 of the rows are shaded in green.",caption:""},version:{major:0,minor:0}},"image 2":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/217d580bc0baddb903bbcb648fc8d3ea3d0f4408",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 4 of the rows are shaded to represent 4 tenths.",caption:""},version:{major:0,minor:0}},"image 1":{options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/2a56a60275b7227ed9c5b89e489587c8cb13eb7b",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 5 of the rows are shaded to represent 5 tenths.",caption:""},type:"image",version:{major:0,minor:0},graded:!0,alignment:"block",static:!1}}},widgetEnabled:!0,immutableWidgets:!1},{title:"Problem 1b",content:"$0.6 + 0.4 =$   [[☃ numeric-input 1]]",widgets:{"numeric-input 1":{type:"numeric-input",alignment:"default",static:!1,graded:!0,options:{static:!1,answers:[{value:1,status:"correct",message:"",simplify:"required",strict:!1,maxError:null}],size:"normal",coefficient:!1,labelText:""},version:{major:0,minor:0}}},images:{},hasHint:!0,hint:{content:`There are many ways to solve this problem. Let's see two student solutions.
+
+###Student A's solution:
+
+I thought in terms of tenths.
+
+$\\phantom{=}0.6 + 0.4$
+
+$=6$ tenths $+ ~4$ tenths
+
+$=10$ tenths
+
+$=1$
+
+###Student B's solution:
+
+I used tenths grids.
+
+[[☃ image 1]]
+
+[[☃ image 2]]
+
+[[☃ image 3]]
+
+$\\blueD{0.6} + \\greenD{0.4} = 1$
+
+###The answer:
+
+$0.6 + 0.4 = 1$`,images:{},widgets:{"image 3":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[234,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/54fbc06d92097119f1be5d1679391596209296a7",width:234,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 6 of the rows are shaded in blue and 4 of the rows are shaded in green.",caption:""},version:{major:0,minor:0}},"image 2":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/217d580bc0baddb903bbcb648fc8d3ea3d0f4408",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 4 of the rows are shaded to represent 4 tenths.",caption:""},version:{major:0,minor:0}},"image 1":{options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/8e353e65446a4322f53d640aba33dccd69b0874c",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 6 of the rows are shaded to represent 6 tenths.",caption:""},type:"image",version:{major:0,minor:0},graded:!0,alignment:"block",static:!1}}},widgetEnabled:!0,immutableWidgets:!1},{title:"Problem 1c",content:"$0.8 + 0.4 =$   [[☃ numeric-input 1]]",widgets:{"numeric-input 1":{type:"numeric-input",alignment:"default",static:!1,graded:!0,options:{static:!1,answers:[{value:1.2,status:"correct",message:"",simplify:"required",strict:!1,maxError:null}],size:"normal",coefficient:!1,labelText:""},version:{major:0,minor:0}}},images:{},hasHint:!0,hint:{content:`There are many ways to solve this problem. Let's see two student solutions.
+
+###Student A's solution:
+
+I thought in terms of tenths.
+
+$\\phantom{=}0.8 + 0.4$
+
+$=8$ tenths $+ ~4$ tenths
+
+$=12$ tenths
+
+$=1.2$
+
+###Student B's solution:
+
+I used tenths grids.
+
+[[☃ image 1]]
+
+[[☃ image 2]]
+
+[[☃ image 3]]
+$\\blueD{0.8} + \\greenD{0.4} = 1.2$
+
+###The answer:
+
+$ 0.8+0.4=1.2 $`,images:{},widgets:{"image 3":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[354,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/4b595ea53e5edc2b5991f2171f384a7eae2eeb24",width:354,height:132},labels:[],alt:"Two equal sized squares. Each square is divided into 10 rows to show tenths. In the first square, 8 of the rows are shaded in blue and 2 of the rows are shaded in green. In the second square, 2 of the rows are shaded in green.",caption:""},version:{major:0,minor:0}},"image 2":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/217d580bc0baddb903bbcb648fc8d3ea3d0f4408",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 4 of the rows are shaded to represent 4 tenths.",caption:""},version:{major:0,minor:0}},"image 1":{options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/58d09bc7bdfadbd860c55734bf146578f047edbc",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 8 of the rows are shaded to represent 8 tenths.",caption:""},type:"image",version:{major:0,minor:0},graded:!0,alignment:"block",static:!1}}},widgetEnabled:!0,immutableWidgets:!1}]},version:{major:0,minor:0}}}},e=a=>o(s,{apiOptions:{isMobile:a.isMobile},question:n}),Ke={title:"Perseus/Widgets/Graded Group Set",args:{isMobile:!1}};var t,i,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <RendererWithDebugUI apiOptions={{
+    isMobile: args.isMobile
+  }} question={article1} />;
+}`,...(r=(i=e.parameters)==null?void 0:i.docs)==null?void 0:r.source}}};const Ne=["Article1"];export{e as Article1,Ne as __namedExportsOrder,Ke as default};
