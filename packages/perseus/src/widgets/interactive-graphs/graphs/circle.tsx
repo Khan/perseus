08cceabc311a967033cce96bafb3369c3758b1ac
Ben Christel <benchristel@khanacademy.org>
2024-05-09 11:28:42 -0700

    Make the whole circle draggable

--- packages/perseus/src/widgets/interactive-graphs/graphs/circle.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/circle.tsx
@@ -1,5 +1,5 @@
 import {color} from "@khanacademy/wonder-blocks-tokens";
-import {Circle, vec} from "mafs";
+import {Circle, useMovable, vec} from "mafs";
 import * as React from "react";
 
 import {moveCenter, moveRadiusPoint} from "../reducer/interactive-graph-action";
@@ -8,6 +8,9 @@ import {getRadius} from "../reducer/interactive-graph-state";
 import {StyledMovablePoint} from "./components/movable-point";
 
 import type {CircleGraphState, MafsGraphProps} from "../types";
+import {useRef} from "react";
+import {snap} from "../utils";
+import useGraphConfig from "../reducer/use-graph-config";
 
 type CircleGraphProps = MafsGraphProps<CircleGraphState>;
 
@@ -34,18 +37,25 @@ export function CircleGraph(props: CircleGraphProps) {
 
 function MovableCircle(props: {center: vec.Vector2, radius: number, onMove: (newCenter: vec.Vector2) => unknown}) {
     const {center, radius, onMove} = props;
+    const {snapStep} = useGraphConfig();
+
+    const draggableRef = useRef<SVGGElement>(null);
+
+    const {dragging} = useMovable({
+        gestureTarget: draggableRef,
+        point: center,
+        onMove,
+        constrain: (p) => snap(snapStep, p),
+    })
+
     return (
-        <>
+        <g ref={draggableRef} className={`movable-circle ${dragging ? "movable-circle--dragging" : ""}`}>
             <Circle
                 center={center}
                 radius={radius}
                 fillOpacity={0}
                 color={color.blue}
             />
-            <StyledMovablePoint
-                point={center}
-                onMove={onMove}
-            />
-        </>
+        </g>
     )
 }
