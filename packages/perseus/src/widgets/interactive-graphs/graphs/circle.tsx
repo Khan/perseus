269732a2232d723b04076b98aed32fc6eab091e4
Ben Christel <benchristel@khanacademy.org>
2024-05-09 11:55:15 -0700

    Add drag handle

--- packages/perseus/src/widgets/interactive-graphs/graphs/circle.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/circle.tsx
@@ -11,6 +11,7 @@ import type {CircleGraphState, MafsGraphProps} from "../types";
 import {useRef} from "react";
 import {snap} from "../utils";
 import useGraphConfig from "../reducer/use-graph-config";
+import {useTransformVectorsToPixels} from "./use-transform";
 
 type CircleGraphProps = MafsGraphProps<CircleGraphState>;
 
@@ -56,6 +57,35 @@ function MovableCircle(props: {center: vec.Vector2, radius: number, onMove: (new
                 fillOpacity={0}
                 color={color.blue}
             />
+            <DragHandle center={center}/>
         </g>
     )
+}
+
+const dragHandleDimensions: vec.Vector2 = [24, 14];
+const dragHandlePointPositions = crossProduct([-4.4, 0, 4.4], [-2.1, 2.1])
+function DragHandle(props: { center: [x: number, y: number] }) {
+    const {center} = props;
+    const cornerRadius = Math.min(...dragHandleDimensions) / 2;
+    const [centerPx] = useTransformVectorsToPixels(center);
+    const topLeft = vec.sub(centerPx, vec.scale(dragHandleDimensions, 0.5));
+
+    // FIXME make fill color a WB color
+    return <>
+        <rect x={topLeft[0]} y={topLeft[1]} width={dragHandleDimensions[0]} height={dragHandleDimensions[1]} rx={cornerRadius} ry={cornerRadius} fill="#777" stroke="#fff" strokeWidth={2}/>
+        {dragHandlePointPositions.map((offsetPx) => {
+            const [xPx, yPx] = vec.add(offsetPx, centerPx)
+            return <circle cx={xPx} cy={yPx} r={1.25} fill="#fff" />
+        })}
+    </>;
+}
+
+function crossProduct<A, B>(as: A[], bs: B[]): [A, B][] {
+    const result: [A, B][] = [];
+    for (const a of as) {
+        for (const b of bs) {
+            result.push([a, b]);
+        }
+    }
+    return result;
 }
