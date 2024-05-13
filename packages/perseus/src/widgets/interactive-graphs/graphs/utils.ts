0183a5c3a9536557532f88d56a558e6dbb62e33f
Ben Christel <benchristel@khanacademy.org>
2024-05-13 13:05:34 -0700

    Add paragraph breaks

--- packages/perseus/src/widgets/interactive-graphs/graphs/utils.ts
+++ packages/perseus/src/widgets/interactive-graphs/graphs/utils.ts
@@ -38,11 +38,13 @@ export const getIntersectionOfRayWithBox = (
             return [xMax, yAtXMax]
         case yDiff > 0 && xDiff >= 0:
             return [xAtYMax, yMax];
+
         // 3 o'clock to 5:59
         case yDiff < 0 && xDiff >= 0 && xAtYMin > xMax:
             return [xMax, yAtXMax]
         case yDiff < 0 && xDiff >= 0:
             return [xAtYMin, yMin];
+
         // 9 o'clock to 11:59
         case yDiff === 0 && xDiff < 0:
             return [xMin, aY]
@@ -50,6 +52,7 @@ export const getIntersectionOfRayWithBox = (
             return [xMin, yAtXMin]
         case yDiff >= 0 && xDiff < 0:
             return [xAtYMax, yMax]
+
         // 6 o'clock to 8:59
         case yDiff < 0 && xDiff < 0 && xAtYMin < xMin:
             return [xMin, yAtXMin]
