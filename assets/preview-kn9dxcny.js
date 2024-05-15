228e3de21140a828392e9857534fd4035f287e2a
nishasy <nisha@khanacademy.org>
2024-05-15 20:28:37 +0000

    Deploying to gh-pages from @ Khan/perseus@307e6fa87b086bac6960dea5c01263a5fb5a693c 🚀

---
+++ assets/preview-kn9dxcny.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-UicyBb38.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-_xQyY7b3.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-_xQyY7b3.js","./iframe-UicyBb38.js","./index-qhcEwEpg.js","./_commonjsHelpers-4gQjN7DL.js","./react-16-GaxUwBdR.js","./index-E09jvG0x.js","./_commonjs-dynamic-modules-h-SxKiO4.js","./bundle.esm-3RuEOGQS.js","./index-ogXoivrg.js","./_getPrototype-bjD8Yebc.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
