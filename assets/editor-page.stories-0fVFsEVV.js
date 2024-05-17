be85925d8f1e7e6d54b40583bda59a8f69ec129b
jeremywiebe <jeremy.wiebe@gmail.com>
2024-05-17 20:08:40 +0000

    Deploying to gh-pages from @ Khan/perseus@9d27c76ad32bc9d5efab952457441b6a679c8786 🚀

---
+++ assets/editor-page.stories-0fVFsEVV.js
@@ -0,0 +1,49 @@
+import{j as g}from"./jsx-runtime-BGVbfQ2Z.js";import{a as R}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{r as t}from"./index-qhcEwEpg.js";import{r as P,E as D}from"./register-all-widgets-and-editors-for-testing-eevccUcm.js";import{s as j}from"./interactive-graph.testdata-eTkQk7u1.js";import{f as S}from"./flags-for-api-options-tjTmv1xD.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./version-akiLXZts.js";import"./article-renderer-J8R3N0me.js";import"./compare-VLxlEkSV.js";import"./index-default-4_ZsnO94.js";import"./prop-types-18HXJOop.js";import"./index-lUErx3pE.js";import"./index-J2t_5nK1.js";import"./index-awljIyHI.js";import"./index-E09jvG0x.js";import"./index-halg33Zp.js";import"./index-4c2J3ov1.js";import"./tabbar-_HYg3Kcf.js";import"./item-MgaZQyPg.js";import"./index-VhM44oCk.js";import"./index-tvtfaFq4.js";import"./button-assets-cmoMUwP4.js";import"./keypad-button-BUF7auet.js";import"./operators-page-H6dLcd0l.js";import"./navigation-pad-URlj9thj.js";import"./index-JT1-kTlx.js";import"./key-translator-t-qTln-I.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-eBV-jUn6.js";import"./index-dnMhQZ-1.js";import"./jquery-5v7aFUvu.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-K2-sSw7D.js";import"./index-mohBxQl_.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-WvQWLN3r.js";import"./fixed-to-responsive-ybwlrogx.js";import"./constants-I_nlPaPx.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./perseus-api-Ly41NhN4.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./lint-4QkP-VXi.js";import"./index-bfZeEiF5.js";import"./index-Fk0JNZQD.js";import"./index-2Es7CZJJ.js";import"./index-_15Y2y0p.js";import"./index-h47zdzUa.js";import"./Popper-uHddJoXq.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./math-input-AcRqfdqV.js";import"./index-hU7TnBqE.js";import"./unit-1L4OY2_r.js";import"./input-with-examples-PijOsKvS.js";import"./math-output-o5-UebzW.js";import"./text-input-GIma_XqC.js";import"./index-KktidPha.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-A9uEOuum.js";import"./base-radio-SPAHpvSB.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-PH0yrPYg.js";import"./icon-HbAIhp4d.js";import"./choice-icon-8EqwF5gf.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-atX5Qolq.js";import"./choice-none-above-3HDuJF2A.js";import"./video-transcript-link-g12jSPmn.js";import"./answer-choices-Z3F9i2A_.js";import"./button-group-eBTrRsKy.js";import"./graph-gX7mFe7S.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-BoVrgmxI.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-QmtkIb5a.js";import"./prop-check-box-U5TrzrfJ.js";import"./range-input-IMzA4hKn.js";import"./marker-u0YQKknS.js";import"./answer-pill-kIv9e4aq.js";import"./sortable-ohg8J1KD.js";import"./multi-renderer-WwLICTWu.js";import"./hints-renderer-0MiU1LZB.js";import"./components-fyuLUWh0.js";import"./device-framer-y1VWNoNx.js";import"./constants-hQyjCzwz.js";import"./section-control-button-5XLBBQ4a.js";import"./editor-ep7BLQht.js";import"./tex-error-view-_FdVLqF4.js";import"./toggleable-caret-s4JdKAQK.js";import"./viewport-resizer-Mquowdfg.js";import"./structured-item-diff-wVZPgabL.js";import"./text-diff-Nv9lE-EA.js";import"./tags-diff-wmC0LCH-.js";import"./item-extras-editor-qZf7Gy0o.js";import"./multi-items-DWkibvb2.js";/* empty css                       */import"./categorizer-editor-XV2jS29Z.js";import"./editor-jsonify-TeR0H2Xw.js";import"./blur-input-0MWX_AIX.js";import"./definition-editor-4XiLoA4X.js";import"./dropdown-editor-XddNdNPI.js";import"./explanation-editor-ZQTphhfm.js";import"./expression-editor-iKFZjJTf.js";import"./graph-settings-XxLmeG-G.js";import"./image-editor-PgCElwHa.js";import"./input-number-editor-vJpGewUJ.js";import"./interaction-editor-sMGPlHit.js";import"./label-image-editor-wUgDTV_W.js";import"./form-wrapped-text-field-uYUKBg_x.js";import"./answer-choices-k19KKAnC.js";import"./behavior-k1OVOu8F.js";import"./question-markers-TC-54YpX.js";import"./marker-qPrYjwjK.js";import"./util-cblgOEF4.js";import"./select-image-Wys901Re.js";import"./matcher-editor-mw2djnwP.js";import"./number-line-editor-AvPG1bug.js";import"./numeric-input-editor-C_gi8fmW.js";import"./python-program-editor-1JlyNv3T.js";import"./editor-P7giBJK9.js";import"./sorter-editor-BfFgp2K2.js";import"./interactive-graph-settings-jdQoRCl7.js";import"./locked-figures-section-6JfJmTnZ.js";import"./locked-line-settings-068Z17dY.js";import"./color-select-yj5Sk9Ub.js";import"./color-swatch-CmRKRicr.js";import"./locked-figure-settings-actions-42nSP4Ua.js";import"./locked-point-settings-0o9KBBJ9.js";P();const po={title:"PerseusEditor/EditorPage"},b=R("onChange"),o=()=>{const[r,n]=t.useState("phone"),[s,p]=t.useState(!1),[m,a]=t.useState(),[c,u]=t.useState(),[d,v]=t.useState();return g(D,{apiOptions:{isMobile:!1,flags:S},previewDevice:r,onPreviewDeviceChange:e=>n(e),developerMode:!0,jsonMode:s,answerArea:m,question:c,hints:d,frameSource:"about:blank",previewURL:"about:blank",itemId:"1",onChange:e=>{b(e),"jsonMode"in e&&p(e.jsonMode),"answerArea"in e&&a(e.answerArea),"question"in e&&u(e.question),"hints"in e&&v(e.hints)}})},i=()=>{const[r,n]=t.useState("phone"),[s,p]=t.useState(!1),[m,a]=t.useState(),[c,u]=t.useState(j),[d,v]=t.useState();return g(D,{apiOptions:{isMobile:!1,flags:S},previewDevice:r,onPreviewDeviceChange:e=>n(e),developerMode:!0,jsonMode:s,answerArea:m,question:c,hints:d,frameSource:"about:blank",previewURL:"about:blank",itemId:"1",onChange:e=>{b(e),"jsonMode"in e&&p(e.jsonMode),"answerArea"in e&&a(e.answerArea),"question"in e&&u(e.question),"hints"in e&&v(e.hints)}})};var w,f,l;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`(): React.ReactElement => {
+  const [previewDevice, setPreviewDevice] = React.useState<DeviceType>("phone");
+  const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
+  const [answerArea, setAnswerArea] = React.useState<PerseusAnswerArea | undefined | null>();
+  const [question, setQuestion] = React.useState<PerseusRenderer | undefined>();
+  const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
+  return <EditorPage apiOptions={{
+    isMobile: false,
+    flags
+  }} previewDevice={previewDevice} onPreviewDeviceChange={newDevice => setPreviewDevice(newDevice)} developerMode={true} jsonMode={jsonMode} answerArea={answerArea} question={question} hints={hints} frameSource="about:blank" previewURL="about:blank" itemId="1" onChange={props => {
+    onChangeAction(props);
+    if ("jsonMode" in props) {
+      setJsonMode(props.jsonMode);
+    }
+    if ("answerArea" in props) {
+      setAnswerArea(props.answerArea);
+    }
+    if ("question" in props) {
+      setQuestion(props.question);
+    }
+    if ("hints" in props) {
+      setHints(props.hints);
+    }
+  }} />;
+}`,...(l=(f=o.parameters)==null?void 0:f.docs)==null?void 0:l.source}}};var A,h,M;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`(): React.ReactElement => {
+  const [previewDevice, setPreviewDevice] = React.useState<DeviceType>("phone");
+  const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
+  const [answerArea, setAnswerArea] = React.useState<PerseusAnswerArea | undefined | null>();
+  const [question, setQuestion] = React.useState<PerseusRenderer | undefined>(segmentWithLockedFigures);
+  const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
+  return <EditorPage apiOptions={{
+    isMobile: false,
+    flags
+  }} previewDevice={previewDevice} onPreviewDeviceChange={newDevice => setPreviewDevice(newDevice)} developerMode={true} jsonMode={jsonMode} answerArea={answerArea} question={question} hints={hints} frameSource="about:blank" previewURL="about:blank" itemId="1" onChange={props => {
+    onChangeAction(props);
+    if ("jsonMode" in props) {
+      setJsonMode(props.jsonMode);
+    }
+    if ("answerArea" in props) {
+      setAnswerArea(props.answerArea);
+    }
+    if ("question" in props) {
+      setQuestion(props.question);
+    }
+    if ("hints" in props) {
+      setHints(props.hints);
+    }
+  }} />;
+}`,...(M=(h=i.parameters)==null?void 0:h.docs)==null?void 0:M.source}}};const mo=["Demo","MafsWithLockedFigures"];export{o as Demo,i as MafsWithLockedFigures,mo as __namedExportsOrder,po as default};
---
+++ assets/register-all-widgets-for-testing-zFQZrpcH.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-J8R3N0me.js";import{r,a as s}from"./renderer-eBV-jUn6.js";const a=()=>{r(e),s()};export{a as r};
