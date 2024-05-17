ca1f8487abe264a4b5153be4912a2fbb2cbc24c4
mark-fitzgerald <13896410+mark-fitzgerald@users.noreply.github.com>
2024-05-17 18:47:34 +0000

    Deploying to gh-pages from @ Khan/perseus@dc0adedebbae0c4a1940d67f64e19b0104ac85f4 🚀

---
+++ assets/article-renderer.stories-SI_5znn_.js
@@ -0,0 +1,197 @@
+import{j as t}from"./jsx-runtime-BGVbfQ2Z.js";import"./prop-types-VEqj8M-u.js";import"./key-translator-uZU3CxVJ.js";import{a as N}from"./index-sCIolE-R.js";import"./button-assets-cmoMUwP4.js";import{a as T}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{s as n}from"./test-dependencies-QeE6Wjdn.js";import{A as r}from"./article-renderer-IpYkmycc.js";import{T as C}from"./test-keypad-context-wrapper-GdSjyZ20.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./version-akiLXZts.js";import"./index-lUErx3pE.js";import"./index-J2t_5nK1.js";import"./index-awljIyHI.js";import"./index-E09jvG0x.js";import"./invariant-bu5zBsRS.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./index-4c2J3ov1.js";import"./tabbar-_HYg3Kcf.js";import"./item-MgaZQyPg.js";import"./index-VhM44oCk.js";import"./index-tvtfaFq4.js";import"./keypad-button-BUF7auet.js";import"./operators-page-H6dLcd0l.js";import"./navigation-pad-URlj9thj.js";import"./index-JT1-kTlx.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./register-all-widgets-for-testing-UWHV2zWJ.js";import"./renderer-g-jl5vfe.js";import"./index-dnMhQZ-1.js";import"./jquery-5v7aFUvu.js";import"./index-default-4_ZsnO94.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-1xirUsO_.js";import"./index-mohBxQl_.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-EDQcR2F7.js";import"./compare-59FC1ybr.js";import"./fixed-to-responsive-ybwlrogx.js";import"./constants-I_nlPaPx.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./perseus-api-9NPhLZtS.js";import"./index-0C4KXdeC.js";import"./stub-tag-editor-f1WdYARp.js";import"./text-list-editor-xQBHt64k.js";import"./lint-4QkP-VXi.js";import"./enums-x5qaTru7.js";import"./index-bfZeEiF5.js";import"./index-Fk0JNZQD.js";import"./index-2Es7CZJJ.js";import"./index-_15Y2y0p.js";import"./index-h47zdzUa.js";import"./Popper-uHddJoXq.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./math-input-4VziEXpz.js";import"./index-hU7TnBqE.js";import"./unit-sO4C0-pw.js";import"./input-with-examples-3X2C50zD.js";import"./math-output-tIDciKOM.js";import"./text-input-GIma_XqC.js";import"./index-KktidPha.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-6x94GZ43.js";import"./base-radio-h0G4pLuh.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-BWX3hVwo.js";import"./icon-HbAIhp4d.js";import"./choice-icon-8EqwF5gf.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-atX5Qolq.js";import"./choice-none-above-Erl5tYxm.js";import"./video-transcript-link-g12jSPmn.js";import"./answer-choices-05fXIkSu.js";import"./button-group-eBTrRsKy.js";import"./graph-leroHitF.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-BoVrgmxI.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-k4bRs4vK.js";import"./prop-check-box-tfbgjAwO.js";import"./range-input-zCtWZXYe.js";import"./marker-hQyTwk8C.js";import"./answer-pill--gBepQOV.js";import"./sortable-3H8wox3S.js";const E={content:`[[☃ image 2]]
+
+**Part A:** Return to the main characters from your three favorite films. 
+
+- What was one important choice they had to make where the stakes were **high**?
+
+-  What were the **stakes**?
+
+- Can you identify them as internal, external or philosophical?
+
+**Part B:** Think about a difficult choice you had to make in your own life.  What was at stake? 
+
+**Part C:** Return to one of the obstacles your character might face from the previous exercise. Now think of the **choice** this obstacle forces them to make. Answer the following:
+
+- What are the possible stakes of this choice?
+
+- Can you come up with an internal, external or philosophical stake which applies to this choice? 
+`,images:{},widgets:{"image 2":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[1258,703],backgroundImage:{url:"https://ka-perseus-images.s3.amazonaws.com/b08029bc1786fbe54468a2ddc96aaa20be7a663a.png",width:1258,height:703},labels:[],alt:`A scene from Pixar's film "Toy Story 3" where the characters are swimming in a sea of trash and look very afraid.`,caption:`A scene from Pixar's film "Toy Story 3" where the characters are swimming in a sea of trash and look very afraid."`},version:{major:0,minor:0}}}},j={content:`###Group/Pair Activity 
+
+This passage is adapted from Ed Yong, “Turtles Use the Earth’s Magnetic Field as Global GPS.” ©2011 by Kalmbach Publishing Co.
+
+[[☃ passage 1]]
+
+**Question 9**
+
+The passage most strongly suggests that Adelita used which of the following to navigate her 9,000-mile journey?
+
+A) The current of the North Atlantic gyre
+
+B) Cues from electromagnetic coils designed by Putman and Lohmann
+
+C) The inclination and intensity of Earth’s magnetic field
+
+D) A simulated “magnetic signature” configured by Lohmann
+
+10) Which choice provides the best evidence for the answer to the previous question?
+
+A) Lines 1–2 (“In 1996...way”)
+
+B) Lines 20–21 (“Using...surface”)
+
+C) Lines 36–37 (“In the wild...stars”)
+
+D) Lines 43–45 (“Neither...it is”)
+
+**Question 12** 
+
+Based on the passage, which choice best describes the relationship between Putman’s and Lohmann’s research?
+
+A) Putman’s research contradicts Lohmann’s.
+
+B) Putman’s research builds on Lohmann’s.
+
+C) Lohmann’s research confirms Putman’s.
+
+D) Lohmann’s research corrects Putman’s.`,images:{},widgets:{"passage 1":{type:"passage",alignment:"default",static:!1,graded:!0,options:{static:!1,passageTitle:"",passageText:`In 1996, a loggerhead turtle called Adelita swam across 9,000 miles from Mexico to Japan, crossing the entire Pacific on her way. Wallace J. Nichols tracked this epic journey with a satellite tag. But Adelita herself had no such technology at her disposal. How did she steer a route across two oceans to find her destination?
+
+Nathan Putman has the answer. By testing hatchling turtles in a special tank, he has found that they can use the Earth’s magnetic field as their own Global Positioning System (GPS). By sensing the field, they can work out both their latitude and longitude and head in the right direction.
+
+Putman works in the lab of Ken Lohmann, who has been studying the magnetic abilities of loggerheads for over 20 years. In his lab at the University of North Carolina, Lohmann places hatchlings in a large water tank surrounded by a large grid of electromagnetic coils. In 1991, he found that the babies started in the opposite direction if he used the coils to reverse the direction of the magnetic field around them. They could use the field as a compass to get their bearing.
+
+Later, Lohmann showed that they can also use the magnetic field to work out their position. For them, this is literally a matter of life or death. Hatchlings born off the sea coast of Florida spend their early lives in the North Atlantic gyre, a warm current that circles between North America and Africa. If they’re swept towards the cold waters outside the gyre, they die. Their magnetic sense keeps them safe.
+
+Using his coil-surrounded tank, Lohmann could mimic the magnetic field at different parts of the Earth’s surface. If he simulated the field at the northern edge of the gyre, the hatchlings swam southwards. If he simulated the field at the gyre’s southern edge, the turtles swam west-northwest. These experiments showed that the turtles can use their magnetic sense to work out their latitude—their position on a north-south axis. Now, Putman has shown that they can also determine their longitude—their position on an east-west axis.
+
+He tweaked his magnetic tanks to simulate the fields in two positions with the same latitude at opposite ends of the Atlantic. If the field simulated the west Atlantic near Puerto Rico, the turtles swam northeast. If the field matched that on the east Atlantic near the Cape Verde Islands, the turtles swam southwest. In the wild, both headings would keep them within the safe, warm embrace of the North Atlantic gyre.
+Before now, we knew that several animal migrants, from loggerheads to reed warblers to sparrows, had some way of working out longitude, but no one knew how. By keeping the turtles in the same conditions, with only the magnetic fields around them changing, Putman clearly showed that they can use these fields to find their way. In the wild, they might well also use other landmarks like the position of the sea, sun and stars.
+
+Putman thinks that the turtles work out their position using two features of the Earth’s magnetic field that change over its surface. They can sense the field’s inclination, or the angle at which it dips towards the surface. At the poles, this angle is roughly 90 degrees and at the equator, it’s roughly zero degrees. They can also sense its intensity, which is strongest near the poles and weakest near the Equator. Different parts of the world have unique combinations of these two variables. Neither corresponds directly to either latitude or longitude, but together, they provide a “magnetic signature” that tells the turtle where it is.
+`,footnotes:"",showLineNumbers:!0},version:{major:0,minor:0}}}},R={content:`### Practice Problem
+
+$8\\cdot(11i+2)=$ [[☃ expression 1]]  
+*Your answer should be a complex number in the form $a+bi$ where $a$ and $b$ are real numbers.*`,images:{},widgets:{"expression 1":{alignment:"default",graded:!0,options:{answerForms:[{considered:"correct",form:!0,simplify:!1,value:"16+88i"}],buttonSets:["basic"],functions:["f","g","h"],times:!1},static:!1,type:"expression",version:{major:1,minor:0}}}},W=[{content:`## What is a matrix?
+
+A matrix is a rectangular array of numbers arranged in rows and columns.
+
+Matrices can be useful for organizing and manipulating data. They can also be used as a tool to help solve systems of equations.`,images:{},widgets:{}},{content:`## How do we multiply a matrix by a scalar?
+
+To multiply a matrix by a scalar, we multiply each entry in the matrix by the scalar. For example, if 
+
+$A = \\begin{bmatrix} 
+2 & 3 \\\\
+1 & 4 \\\\
+\\end{bmatrix}$
+
+and we want to multiply $A$ by $2$, we can multiply each entry by $2$ to get:
+
+$2A = \\begin{bmatrix} 
+4 & 6 \\\\
+2 & 8 \\\\
+\\end{bmatrix}$`,images:{},widgets:{}},{content:`## How do we add or subtract two matrices?
+
+To add or subtract two matrices, we add or subtract corresponding entries. So if 
+
+$A = \\begin{bmatrix} 
+2 & 3 \\\\
+1 & 4 \\\\
+\\end{bmatrix}$ 
+
+and 
+
+$B = \\begin{bmatrix} 
+5 & 2 \\\\
+3 & 1 \\\\
+\\end{bmatrix}$,
+
+then 
+
+$A+B = \\begin{bmatrix} 
+7 & 5 \\\\
+4 & 5 \\\\
+\\end{bmatrix}$
+
+and 
+
+$A-B = \\begin{bmatrix} 
+-3 & 1 \\\\
+-2 & 3 \\\\
+\\end{bmatrix}$`,images:{},widgets:{}},{content:`## How do we multiply two matrices together?
+
+This one's a bit trickier. To multiply two matrices together, we take the dot product of the rows from the first matrix with the columns from the second matrix. The result is a new matrix. For example, if 
+
+$A = \\begin{bmatrix} 
+2 & 3 \\\\
+1 & 4 \\\\
+\\end{bmatrix}$ 
+
+and 
+
+$B = \\begin{bmatrix} 
+5 & 2 \\\\
+3 & 1 \\\\
+\\end{bmatrix}$,
+
+then 
+
+$AB = \\begin{bmatrix} 
+2\\cdot5+3\\cdot3 & 2\\cdot2+3\\cdot1 \\\\
+1\\cdot5+4\\cdot3 & 1\\cdot2+4\\cdot1 \\\\
+\\end{bmatrix} = \\begin{bmatrix} 
+19 & 7 \\\\
+17 & 6 \\\\
+\\end{bmatrix}.$`,images:{},widgets:{}},{content:`## What can we do with the inverse of a matrix?
+
+Finding the inverse of a matrix can be useful for solving linear systems of equations. If $A$ is the matrix representing the system of equations, and $b$ is the vector of solutions, then $Ax=b$. If we can find the inverse of $A$, we can multiply both sides of the equation by it to isolate $x$:
+
+$A^{-1}Ax=A^{-1}b \\Rightarrow x=A^{-1}b.$`,images:{},widgets:{}},{content:`## Where are matrices used in the real world?
+
+Matrices have tons of real-world applications! They can be used in computer graphics to perform transformations on images, in physics to model physical systems, and in statistics to analyze data, just to name a few.`,images:{},widgets:{}}],B=[{content:`### Practice Problem
+
+$8\\cdot(11i+2)=$ [[☃ expression 1]]  
+*Your answer should be a complex number in the form $a+bi$ where $a$ and $b$ are real numbers.*`,images:{},widgets:{"expression 1":{alignment:"default",graded:!0,options:{answerForms:[{considered:"correct",form:!0,simplify:!1,value:"16+88i"}],buttonSets:["basic"],functions:["f","g","h"],times:!1},static:!1,type:"expression",version:{major:1,minor:0}}}},{content:`### Practice Problem
+
+$8\\cdot(11i+2)=$ [[☃ expression 2]]  
+*Your answer should be a complex number in the form $a+bi$ where $a$ and $b$ are real numbers.*`,images:{},widgets:{"expression 2":{alignment:"default",graded:!0,options:{answerForms:[{considered:"correct",form:!0,simplify:!1,value:"16+88i"}],buttonSets:["basic"],functions:["f","g","h"],times:!1},static:!1,type:"expression",version:{major:1,minor:0}}}},{content:`### Practice Problem
+
+$8\\cdot(11i+2)=$ [[☃ expression 3]]  
+*Your answer should be a complex number in the form $a+bi$ where $a$ and $b$ are real numbers.*`,images:{},widgets:{"expression 3":{alignment:"default",graded:!0,options:{answerForms:[{considered:"correct",form:!0,simplify:!1,value:"16+88i"}],buttonSets:["basic"],functions:["f","g","h"],times:!1},static:!1,type:"expression",version:{major:1,minor:0}}}}],St={title:"Perseus/Renderers/Article Renderer",argTypes:{useNewStyles:{control:"boolean"}}},i=e=>t(r,{json:E,dependencies:n,useNewStyles:e.useNewStyles}),s=e=>t(r,{json:W,dependencies:n,useNewStyles:e.useNewStyles}),o=({useNewStyles:e})=>t(r,{json:j,dependencies:n,useNewStyles:e}),a=({useNewStyles:e})=>t(C,{children:t(N.Consumer,{children:({keypadElement:c,setRenderer:h})=>t(r,{ref:l=>{h(l)},json:R,dependencies:n,useNewStyles:e,apiOptions:{isMobile:!0,customKeypad:!0,onFocusChange:T("onFocusChange")},keypadElement:c})})}),m=({useNewStyles:e})=>t(C,{children:t(N.Consumer,{children:({keypadElement:c,setRenderer:h})=>t(r,{ref:l=>{h(l)},json:B,dependencies:n,useNewStyles:e,apiOptions:{isMobile:!0,customKeypad:!0},keypadElement:c})})});var d,p,u;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`(args: {
+  useNewStyles;
+}): React.ReactElement => {
+  return <ArticleRenderer json={singleSectionArticle} dependencies={storybookDependenciesV2} useNewStyles={args.useNewStyles} />;
+}`,...(u=(p=i.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var g,f,w;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`(args: {
+  useNewStyles;
+}): React.ReactElement => {
+  return <ArticleRenderer json={multiSectionArticle} dependencies={storybookDependenciesV2} useNewStyles={args.useNewStyles} />;
+}`,...(w=(f=s.parameters)==null?void 0:f.docs)==null?void 0:w.source}}};var y,b,x;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`({
+  useNewStyles
+}): any => <ArticleRenderer json={passageArticle} dependencies={storybookDependenciesV2} useNewStyles={useNewStyles} />`,...(x=(b=o.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var $,A,S;a.parameters={...a.parameters,docs:{...($=a.parameters)==null?void 0:$.docs,source:{originalSource:`({
+  useNewStyles
+}): any => <TestKeypadContextWrapper>
+        <KeypadContext.Consumer>
+            {({
+      keypadElement,
+      setRenderer
+    }) => <ArticleRenderer ref={node => {
+      setRenderer(node);
+    }} json={articleSectionWithExpression} dependencies={storybookDependenciesV2} useNewStyles={useNewStyles} apiOptions={{
+      isMobile: true,
+      customKeypad: true,
+      onFocusChange: action("onFocusChange")
+    }} keypadElement={keypadElement} />}
+        </KeypadContext.Consumer>
+    </TestKeypadContextWrapper>`,...(S=(A=a.parameters)==null?void 0:A.docs)==null?void 0:S.source}}};var k,v,P;m.parameters={...m.parameters,docs:{...(k=m.parameters)==null?void 0:k.docs,source:{originalSource:`({
+  useNewStyles
+}): any => <TestKeypadContextWrapper>
+        <KeypadContext.Consumer>
+            {({
+      keypadElement,
+      setRenderer
+    }) => <ArticleRenderer ref={node => {
+      setRenderer(node);
+    }} json={multiSectionArticleWithExpression} dependencies={storybookDependenciesV2} useNewStyles={useNewStyles} apiOptions={{
+      isMobile: true,
+      customKeypad: true
+    }} keypadElement={keypadElement} />}
+        </KeypadContext.Consumer>
+    </TestKeypadContextWrapper>`,...(P=(v=m.parameters)==null?void 0:v.docs)==null?void 0:P.source}}};const kt=["ASingleSectionArticle","BMultiSectionArticle","PassageArticle","ExpressionArticle","MultiSectionedExpressionArticle"];export{i as ASingleSectionArticle,s as BMultiSectionArticle,a as ExpressionArticle,m as MultiSectionedExpressionArticle,o as PassageArticle,kt as __namedExportsOrder,St as default};
---
+++ assets/editor-page.stories--i4Guu9L.js
@@ -0,0 +1,49 @@
+import{j as g}from"./jsx-runtime-BGVbfQ2Z.js";import{a as R}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{r as t}from"./index-qhcEwEpg.js";import{r as P,E as D}from"./register-all-widgets-and-editors-for-testing-pqJLc3xD.js";import{s as j}from"./interactive-graph.testdata-eTkQk7u1.js";import{f as S}from"./flags-for-api-options-dcgRa_u0.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./version-akiLXZts.js";import"./article-renderer-IpYkmycc.js";import"./compare-59FC1ybr.js";import"./prop-types-VEqj8M-u.js";import"./index-lUErx3pE.js";import"./index-J2t_5nK1.js";import"./index-awljIyHI.js";import"./index-E09jvG0x.js";import"./index-sCIolE-R.js";import"./index-4c2J3ov1.js";import"./tabbar-_HYg3Kcf.js";import"./item-MgaZQyPg.js";import"./index-VhM44oCk.js";import"./index-tvtfaFq4.js";import"./button-assets-cmoMUwP4.js";import"./keypad-button-BUF7auet.js";import"./operators-page-H6dLcd0l.js";import"./navigation-pad-URlj9thj.js";import"./index-JT1-kTlx.js";import"./key-translator-uZU3CxVJ.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./invariant-bu5zBsRS.js";import"./enums-x5qaTru7.js";import"./renderer-g-jl5vfe.js";import"./index-dnMhQZ-1.js";import"./jquery-5v7aFUvu.js";import"./index-default-4_ZsnO94.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-1xirUsO_.js";import"./index-mohBxQl_.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-EDQcR2F7.js";import"./fixed-to-responsive-ybwlrogx.js";import"./constants-I_nlPaPx.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./perseus-api-9NPhLZtS.js";import"./index-0C4KXdeC.js";import"./stub-tag-editor-f1WdYARp.js";import"./text-list-editor-xQBHt64k.js";import"./lint-4QkP-VXi.js";import"./index-bfZeEiF5.js";import"./index-Fk0JNZQD.js";import"./index-2Es7CZJJ.js";import"./index-_15Y2y0p.js";import"./index-h47zdzUa.js";import"./Popper-uHddJoXq.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./math-input-4VziEXpz.js";import"./index-hU7TnBqE.js";import"./unit-sO4C0-pw.js";import"./input-with-examples-3X2C50zD.js";import"./math-output-tIDciKOM.js";import"./text-input-GIma_XqC.js";import"./index-KktidPha.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-6x94GZ43.js";import"./base-radio-h0G4pLuh.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-BWX3hVwo.js";import"./icon-HbAIhp4d.js";import"./choice-icon-8EqwF5gf.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-atX5Qolq.js";import"./choice-none-above-Erl5tYxm.js";import"./video-transcript-link-g12jSPmn.js";import"./answer-choices-05fXIkSu.js";import"./button-group-eBTrRsKy.js";import"./graph-leroHitF.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-BoVrgmxI.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-k4bRs4vK.js";import"./prop-check-box-tfbgjAwO.js";import"./range-input-zCtWZXYe.js";import"./marker-hQyTwk8C.js";import"./answer-pill--gBepQOV.js";import"./sortable-3H8wox3S.js";import"./multi-renderer-toAMmttg.js";import"./hints-renderer-7ntX7ZD3.js";import"./components-vlL0HFOu.js";import"./device-framer-KLZBf14y.js";import"./constants-hQyjCzwz.js";import"./section-control-button-5KetgskL.js";import"./editor-LA38bzrI.js";import"./index-k-0mNqHS.js";import"./tex-error-view-_FdVLqF4.js";import"./toggleable-caret-s4JdKAQK.js";import"./viewport-resizer-wTjwjjZD.js";import"./structured-item-diff-wJFVF3R7.js";import"./text-diff-ZVX3FndL.js";import"./tags-diff-ILnWjlma.js";import"./item-extras-editor-1yt_43KQ.js";import"./multi-items-gvdXQtwF.js";/* empty css                       */import"./categorizer-editor-5R1eO-8V.js";import"./editor-jsonify-ve9GjDmT.js";import"./blur-input-0MWX_AIX.js";import"./definition-editor-PhW4USDK.js";import"./dropdown-editor--DKlJMNP.js";import"./explanation-editor-f8aIx8DB.js";import"./expression-editor-IDqlRy4X.js";import"./graph-settings-URyI4qjD.js";import"./image-editor-fokLxt9r.js";import"./input-number-editor-qNkGAc4v.js";import"./interaction-editor-MgqQqGHR.js";import"./label-image-editor-hn4mM-go.js";import"./form-wrapped-text-field-skVx6swA.js";import"./answer-choices-tiBd17L7.js";import"./behavior-86y0GBsl.js";import"./question-markers-GKAMUxjb.js";import"./marker-BS47XggZ.js";import"./util-cblgOEF4.js";import"./select-image-_YHbYbkx.js";import"./matcher-editor-JQnoY0K2.js";import"./number-line-editor-FIzOBKYw.js";import"./numeric-input-editor-pF5zQAVe.js";import"./python-program-editor-IZHdlt2D.js";import"./editor-8Jyu2lew.js";import"./sorter-editor-AX58gVIt.js";import"./interactive-graph-settings-NeTfHlnk.js";import"./locked-figures-section-5w9HvciL.js";import"./locked-line-settings-KgrPJOB2.js";import"./color-select-4XnmrZkq.js";import"./color-swatch-XfQDCYUK.js";import"./locked-figure-settings-actions-I_B-NuQF.js";import"./locked-point-settings-h5MeJ29O.js";P();const co={title:"PerseusEditor/EditorPage"},b=R("onChange"),o=()=>{const[r,n]=t.useState("phone"),[s,p]=t.useState(!1),[m,a]=t.useState(),[c,u]=t.useState(),[d,v]=t.useState();return g(D,{apiOptions:{isMobile:!1,flags:S},previewDevice:r,onPreviewDeviceChange:e=>n(e),developerMode:!0,jsonMode:s,answerArea:m,question:c,hints:d,frameSource:"about:blank",previewURL:"about:blank",itemId:"1",onChange:e=>{b(e),"jsonMode"in e&&p(e.jsonMode),"answerArea"in e&&a(e.answerArea),"question"in e&&u(e.question),"hints"in e&&v(e.hints)}})},i=()=>{const[r,n]=t.useState("phone"),[s,p]=t.useState(!1),[m,a]=t.useState(),[c,u]=t.useState(j),[d,v]=t.useState();return g(D,{apiOptions:{isMobile:!1,flags:S},previewDevice:r,onPreviewDeviceChange:e=>n(e),developerMode:!0,jsonMode:s,answerArea:m,question:c,hints:d,frameSource:"about:blank",previewURL:"about:blank",itemId:"1",onChange:e=>{b(e),"jsonMode"in e&&p(e.jsonMode),"answerArea"in e&&a(e.answerArea),"question"in e&&u(e.question),"hints"in e&&v(e.hints)}})};var w,f,l;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`(): React.ReactElement => {
+  const [previewDevice, setPreviewDevice] = React.useState<DeviceType>("phone");
+  const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
+  const [answerArea, setAnswerArea] = React.useState<PerseusAnswerArea | undefined | null>();
+  const [question, setQuestion] = React.useState<PerseusRenderer | undefined>();
+  const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
+  return <EditorPage apiOptions={{
+    isMobile: false,
+    flags
+  }} previewDevice={previewDevice} onPreviewDeviceChange={newDevice => setPreviewDevice(newDevice)} developerMode={true} jsonMode={jsonMode} answerArea={answerArea} question={question} hints={hints} frameSource="about:blank" previewURL="about:blank" itemId="1" onChange={props => {
+    onChangeAction(props);
+    if ("jsonMode" in props) {
+      setJsonMode(props.jsonMode);
+    }
+    if ("answerArea" in props) {
+      setAnswerArea(props.answerArea);
+    }
+    if ("question" in props) {
+      setQuestion(props.question);
+    }
+    if ("hints" in props) {
+      setHints(props.hints);
+    }
+  }} />;
+}`,...(l=(f=o.parameters)==null?void 0:f.docs)==null?void 0:l.source}}};var A,h,M;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`(): React.ReactElement => {
+  const [previewDevice, setPreviewDevice] = React.useState<DeviceType>("phone");
+  const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
+  const [answerArea, setAnswerArea] = React.useState<PerseusAnswerArea | undefined | null>();
+  const [question, setQuestion] = React.useState<PerseusRenderer | undefined>(segmentWithLockedFigures);
+  const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
+  return <EditorPage apiOptions={{
+    isMobile: false,
+    flags
+  }} previewDevice={previewDevice} onPreviewDeviceChange={newDevice => setPreviewDevice(newDevice)} developerMode={true} jsonMode={jsonMode} answerArea={answerArea} question={question} hints={hints} frameSource="about:blank" previewURL="about:blank" itemId="1" onChange={props => {
+    onChangeAction(props);
+    if ("jsonMode" in props) {
+      setJsonMode(props.jsonMode);
+    }
+    if ("answerArea" in props) {
+      setAnswerArea(props.answerArea);
+    }
+    if ("question" in props) {
+      setQuestion(props.question);
+    }
+    if ("hints" in props) {
+      setHints(props.hints);
+    }
+  }} />;
+}`,...(M=(h=i.parameters)==null?void 0:h.docs)==null?void 0:M.source}}};const uo=["Demo","MafsWithLockedFigures"];export{o as Demo,i as MafsWithLockedFigures,uo as __namedExportsOrder,co as default};
