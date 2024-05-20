5fcf82d73da95ec468a6299d8f3fd20f4c67982b
mark-fitzgerald <13896410+mark-fitzgerald@users.noreply.github.com>
2024-05-20 21:42:26 +0000

    Deploying to gh-pages from @ Khan/perseus@55039a430ef298f3d8a28e27798481130b28ca24 🚀

---
+++ assets/interactive-graph-editor.stories-92zD-KjX.js
@@ -0,0 +1,117 @@
+import{j as u}from"./jsx-runtime-BGVbfQ2Z.js";import{r as h}from"./index-qhcEwEpg.js";import{I as s,f as D}from"./flags-for-api-options-UAZiE_ok.js";import{g as M}from"./util-cblgOEF4.js";import{I as _}from"./interactive-graph-settings.argtypes-6T40gg9D.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./article-renderer-y-xybXk3.js";import"./compare-VLxlEkSV.js";import"./version-akiLXZts.js";import"./index-default-4_ZsnO94.js";import"./prop-types-18HXJOop.js";import"./index-lUErx3pE.js";import"./index-J2t_5nK1.js";import"./index-awljIyHI.js";import"./index-E09jvG0x.js";import"./index-halg33Zp.js";import"./index-4c2J3ov1.js";import"./tabbar-_HYg3Kcf.js";import"./item-MgaZQyPg.js";import"./index-VhM44oCk.js";import"./index-tvtfaFq4.js";import"./button-assets-cmoMUwP4.js";import"./keypad-button-BUF7auet.js";import"./operators-page-H6dLcd0l.js";import"./navigation-pad-URlj9thj.js";import"./index-JT1-kTlx.js";import"./key-translator-t-qTln-I.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-eBV-jUn6.js";import"./index-dnMhQZ-1.js";import"./jquery-5v7aFUvu.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-K2-sSw7D.js";import"./index-mohBxQl_.js";import"./dependencies-fnqF3NiV.js";import"./perseus-error-OpXxk17X.js";import"./util-WvQWLN3r.js";import"./fixed-to-responsive-ybwlrogx.js";import"./constants-I_nlPaPx.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./perseus-api-Ly41NhN4.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./lint-4QkP-VXi.js";import"./index-bfZeEiF5.js";import"./index-Fk0JNZQD.js";import"./index-2Es7CZJJ.js";import"./index-_15Y2y0p.js";import"./index-h47zdzUa.js";import"./Popper-uHddJoXq.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./math-input-AcRqfdqV.js";import"./index-hU7TnBqE.js";import"./unit-1L4OY2_r.js";import"./input-with-examples-PijOsKvS.js";import"./math-output-o5-UebzW.js";import"./text-input-GIma_XqC.js";import"./index-KktidPha.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-A9uEOuum.js";import"./base-radio-SPAHpvSB.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-PH0yrPYg.js";import"./icon-HbAIhp4d.js";import"./choice-icon-8EqwF5gf.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-atX5Qolq.js";import"./choice-none-above-3HDuJF2A.js";import"./video-transcript-link-g12jSPmn.js";import"./answer-choices-Z3F9i2A_.js";import"./button-group-eBTrRsKy.js";import"./graph-gX7mFe7S.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-BoVrgmxI.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-QmtkIb5a.js";import"./prop-check-box-U5TrzrfJ.js";import"./range-input-IMzA4hKn.js";import"./marker-u0YQKknS.js";import"./answer-pill-kIv9e4aq.js";import"./sortable-ohg8J1KD.js";import"./multi-renderer-vHizBnBz.js";import"./hints-renderer-fqHvSwEE.js";import"./components-fyuLUWh0.js";import"./interactive-graph-settings-K6OiydXw.js";import"./locked-figures-section-lrjRa8rr.js";import"./locked-line-settings-2e9yi_R0.js";import"./color-select-NTUYFXRR.js";import"./color-swatch-1XIP4nc3.js";import"./locked-figure-settings-actions-rd9_8Vv1.js";import"./locked-point-settings-u2rF24Z7.js";const U={apiOptions:{control:{type:"object"},type:{name:"object",required:!0}},correct:{control:{type:null},type:{name:"object",required:!1}},graph:{control:{type:"object"},type:{name:"object",required:!0}},lockedFigures:{control:{type:"object"},type:{name:"Array<LockedFigure>",required:!1}},onChange:{control:{type:"function"},type:{name:"(props: Partial<Props>) => void",required:!0}},..._},g={apiOptions:{flags:D},graph:{type:"segment"},correct:{type:"segment"}},e=M("point"),Ht={title:"PerseusEditor/Widgets/Interactive Graph Editor",component:s,argTypes:U},l=a=>u(s,{...a});l.args={box:[288,288],gridStep:[1,1],labels:["x","y"],markings:"graph",range:[[-10,10],[-10,10]],rulerLabel:"",rulerTicks:10,showProtractor:!1,showRuler:!1,showTooltips:!1,snapStep:[1,1],step:[1,1]};const p={render:function(){const t=(n,i)=>({...n,...i}),[r,o]=h.useReducer(t,{});return u(s,{...r,onChange:o})}},c={render:function(){const t=(n,i)=>({...n,...i}),[r,o]=h.useReducer(t,g);return u(s,{...r,onChange:o})}},d={render:function(){const t=(n,i)=>({...n,...i}),[r,o]=h.useReducer(t,{...g,lockedFigures:[{...e,coord:[1,1]},{...e,coord:[-1,-1]}]});return u(s,{...r,onChange:o})}},m={render:function(){const t=(n,i)=>({...n,...i}),[r,o]=h.useReducer(t,{...g,lockedFigures:[{type:"line",kind:"line",points:[{...e,coord:[0,2]},{...e,coord:[2,3]}],color:"green",lineStyle:"solid",showPoint1:!1,showPoint2:!1},{type:"line",kind:"ray",points:[{...e,color:"pink",coord:[0,0]},{...e,color:"pink",coord:[4,2]}],color:"pink",lineStyle:"solid",showPoint1:!0,showPoint2:!1},{type:"line",kind:"segment",points:[{...e,color:"grayH",coord:[0,-2]},{...e,color:"grayH",coord:[4,0]}],color:"grayH",lineStyle:"solid",showPoint1:!0,showPoint2:!0}]});return u(s,{...r,onChange:o})}};var f,y,P;l.parameters={...l.parameters,docs:{...(f=l.parameters)==null?void 0:f.docs,source:{originalSource:`(args): React.ReactElement => {
+  return <InteractiveGraphEditor {...args} />;
+}`,...(P=(y=l.parameters)==null?void 0:y.docs)==null?void 0:P.source}}};var k,w,R,S,E;p.parameters={...p.parameters,docs:{...(k=p.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: function Render() {
+    const reducer = (state, newState) => {
+      return {
+        ...state,
+        ...newState
+      };
+    };
+    const [state, dispatch] = React.useReducer(reducer, {});
+    return <InteractiveGraphEditor {...state} onChange={dispatch} />;
+  }
+}`,...(R=(w=p.parameters)==null?void 0:w.docs)==null?void 0:R.source},description:{story:`Example of what the InteractiveGraphEditor experience is when all
+props are controlled by the parent. (Checkboxes update when clicked, etc.)`,...(E=(S=p.parameters)==null?void 0:S.docs)==null?void 0:E.description}}};var v,b,I,G,C;c.parameters={...c.parameters,docs:{...(v=c.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: function Render() {
+    const reducer = (state, newState) => {
+      return {
+        ...state,
+        ...newState
+      };
+    };
+    const [state, dispatch] = React.useReducer(reducer, mafsOptions);
+    return <InteractiveGraphEditor {...state} onChange={dispatch} />;
+  }
+}`,...(I=(b=c.parameters)==null?void 0:b.docs)==null?void 0:I.source},description:{story:`Example of what the InteractiveGraphEditor experience is when using
+a Mafs-based InteractiveGraph.`,...(C=(G=c.parameters)==null?void 0:G.docs)==null?void 0:C.description}}};var x,L,T,j,F;d.parameters={...d.parameters,docs:{...(x=d.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: function Render() {
+    const reducer = (state, newState) => {
+      return {
+        ...state,
+        ...newState
+      };
+    };
+    const [state, dispatch] = React.useReducer(reducer, {
+      // Use locked figures with mafs only.
+      ...mafsOptions,
+      lockedFigures: [{
+        ...defaultPointProps,
+        coord: [1, 1]
+      }, {
+        ...defaultPointProps,
+        coord: [-1, -1]
+      }]
+    });
+    return <InteractiveGraphEditor {...state} onChange={dispatch} />;
+  }
+}`,...(T=(L=d.parameters)==null?void 0:L.docs)==null?void 0:T.source},description:{story:`This InteractiveGraphEditor has locked points.
+
+Locked figures are graph elements such as points, lines, line segements,
+etc. that are locked in place and not interactive. They can be added
+with the "Add element" dropdown at the bottom.`,...(F=(j=d.parameters)==null?void 0:j.docs)==null?void 0:F.description}}};var O,W,H,q,A;m.parameters={...m.parameters,docs:{...(O=m.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  render: function Render() {
+    const reducer = (state, newState) => {
+      return {
+        ...state,
+        ...newState
+      };
+    };
+    const [state, dispatch] = React.useReducer(reducer, {
+      // Use locked figures with mafs only.
+      ...mafsOptions,
+      lockedFigures: [{
+        type: "line",
+        kind: "line",
+        points: [{
+          ...defaultPointProps,
+          coord: [0, 2]
+        }, {
+          ...defaultPointProps,
+          coord: [2, 3]
+        }],
+        color: "green",
+        lineStyle: "solid",
+        showPoint1: false,
+        showPoint2: false
+      }, {
+        type: "line",
+        kind: "ray",
+        points: [{
+          ...defaultPointProps,
+          color: "pink",
+          coord: [0, 0]
+        }, {
+          ...defaultPointProps,
+          color: "pink",
+          coord: [4, 2]
+        }],
+        color: "pink",
+        lineStyle: "solid",
+        showPoint1: true,
+        showPoint2: false
+      }, {
+        type: "line",
+        kind: "segment",
+        points: [{
+          ...defaultPointProps,
+          color: "grayH",
+          coord: [0, -2]
+        }, {
+          ...defaultPointProps,
+          color: "grayH",
+          coord: [4, 0]
+        }],
+        color: "grayH",
+        lineStyle: "solid",
+        showPoint1: true,
+        showPoint2: true
+      }]
+    });
+    return <InteractiveGraphEditor {...state} onChange={dispatch} />;
+  }
+}`,...(H=(W=m.parameters)==null?void 0:W.docs)==null?void 0:H.source},description:{story:`This InteractiveGraphEditor has a locked line segment, line, and ray.
+
+Locked figures are graph elements such as points, lines, line segements,
+etc. that are locked in place and not interactive. They can be added
+with the "Add element" dropdown at the bottom.`,...(A=(q=m.parameters)==null?void 0:q.docs)==null?void 0:A.description}}};const qt=["Default","Controlled","WithMafs","WithLockedPoints","WithLockedLines"];export{p as Controlled,l as Default,m as WithLockedLines,d as WithLockedPoints,c as WithMafs,qt as __namedExportsOrder,Ht as default};
---
+++ assets/register-all-widgets-for-testing-eRdHHshy.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-y-xybXk3.js";import{r,a as s}from"./renderer-eBV-jUn6.js";const a=()=>{r(e),s()};export{a as r};
