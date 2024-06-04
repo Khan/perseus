5b88b1bb7c5801684b372714a826b0872eb878c8
Mark Fitzgerald <markfitzgerald@markfitgeraldm3.home>
2024-06-04 15:56:17 -0700

    Organize vector coordinates in the settings screen.

---
+++ packages/perseus-editor/src/components/__tests__/locked-vector-settings.test.tsx
@@ -0,0 +1,201 @@
+import {RenderStateRoot} from "@khanacademy/wonder-blocks-core";
+import {render, screen} from "@testing-library/react";
+import {userEvent as userEventLib} from "@testing-library/user-event";
+import * as React from "react";
+
+import LockedVectorSettings from "../locked-vector-settings";
+import {getDefaultFigureForType} from "../util";
+
+import type {Props} from "../locked-vector-settings";
+import type {UserEvent} from "@testing-library/user-event";
+
+const defaultProps = {
+    ...getDefaultFigureForType("vector"),
+    onChangeProps: () => {},
+    onRemove: () => {},
+} as Props;
+
+describe("Locked Vector Settings", () => {
+    let userEvent: UserEvent;
+    beforeEach(() => {
+        userEvent = userEventLib.setup({
+            advanceTimers: jest.advanceTimersByTime,
+        });
+    });
+
+    test("renders as expected with default values", () => {
+        // Act
+        render(<LockedVectorSettings {...defaultProps} />, {
+            wrapper: RenderStateRoot,
+        });
+
+        // Assert
+        const titleText = screen.getByText("Vector (0, 0), (2, 2)");
+        expect(titleText).toBeInTheDocument();
+    });
+
+    describe("Heading interactions", () => {
+        test("should show the vector's color by default", () => {
+            // Arrange
+            render(<LockedVectorSettings {...defaultProps} />, {
+                wrapper: RenderStateRoot,
+            });
+
+            // Act
+            const lineSwatch = screen.getByLabelText("grayH, solid");
+
+            // Assert
+            expect(lineSwatch).toBeInTheDocument();
+        });
+
+        test("should use the supplied color in the label", () => {
+            // Arrange
+            render(<LockedVectorSettings {...defaultProps} color="green" />, {
+                wrapper: RenderStateRoot,
+            });
+
+            // Act
+            const lineSwatch = screen.getByLabelText("green, solid");
+
+            // Assert
+            expect(lineSwatch).toBeInTheDocument();
+        });
+
+        test("calls 'onToggle' when header is clicked", async () => {
+            // Arrange
+            const onToggle = jest.fn();
+            render(
+                <LockedVectorSettings {...defaultProps} onToggle={onToggle} />,
+                {
+                    wrapper: RenderStateRoot,
+                },
+            );
+
+            // Act
+            const header = screen.getByRole("button", {
+                name: "Vector (0, 0), (2, 2) grayH, solid",
+            });
+            await userEvent.click(header);
+
+            // Assert
+            expect(onToggle).toHaveBeenCalled();
+        });
+
+        test("expands coordinate points when point headers are clicked", async () => {
+            // Arrange
+            render(<LockedVectorSettings {...defaultProps} />, {
+                wrapper: RenderStateRoot,
+            });
+
+            // Verify initial state
+            const tailHeader = screen.getByRole("button", {
+                name: "Tail (0, 0)",
+            });
+            expect(tailHeader).toHaveAttribute("aria-expanded", "false");
+            const tipHeader = screen.getByRole("button", {
+                name: "Tip (2, 2)",
+            });
+            expect(tipHeader).toHaveAttribute("aria-expanded", "false");
+
+            // Act
+            await userEvent.click(tailHeader);
+            expect(tailHeader).toHaveAttribute("aria-expanded", "true");
+            await userEvent.click(tipHeader);
+            expect(tipHeader).toHaveAttribute("aria-expanded", "true");
+        });
+    });
+
+    describe("Settings interactions", () => {
+        test("calls 'onChangeProps' when color is changed", async () => {
+            // Arrange
+            const onChangeProps = jest.fn();
+            render(
+                <LockedVectorSettings
+                    {...defaultProps}
+                    expanded={true}
+                    onChangeProps={onChangeProps}
+                />,
+                {wrapper: RenderStateRoot},
+            );
+
+            // Act
+            // Change the color
+            const colorSwitch = screen.getByLabelText("color");
+            await userEvent.click(colorSwitch);
+            const colorOption = screen.getByText("green");
+            await userEvent.click(colorOption);
+
+            // Assert
+            expect(onChangeProps).toHaveBeenCalledWith({color: "green"});
+        });
+
+        test("shows an error when the vector length is zero", () => {
+            // Act
+            render(
+                <LockedVectorSettings
+                    {...defaultProps}
+                    points={[
+                        [0, 0],
+                        [0, 0],
+                    ]}
+                    onChangeProps={() => {}}
+                />,
+                {wrapper: RenderStateRoot},
+            );
+
+            // Assert
+            const errors = screen.getByText("The vector cannot have length 0.");
+            // Show error for both points
+            expect(errors).toBeInTheDocument();
+        });
+
+        test("does NOT show an error when the length is greater than zero", () => {
+            // Arrange
+
+            // Act
+            render(
+                <LockedVectorSettings
+                    {...defaultProps}
+                    points={[
+                        [0, 0],
+                        [0, 1],
+                    ]}
+                    onChangeProps={() => {}}
+                />,
+                {wrapper: RenderStateRoot},
+            );
+
+            // Assert
+            const errors = screen.queryAllByText(
+                "The line cannot have length 0.",
+            );
+            expect(errors).toHaveLength(0);
+        });
+
+        test("updates props when changes made to coordinates", async () => {
+            // Arrange
+            const onChangePropsMock = jest.fn();
+            render(
+                <LockedVectorSettings
+                    {...defaultProps}
+                    onChangeProps={onChangePropsMock}
+                />,
+                {
+                    wrapper: RenderStateRoot,
+                },
+            );
+
+            // Act
+            const coordInputs = screen.getAllByRole(
+                "spinbutton",
+            ) as HTMLInputElement[];
+            for (let index = 0; index < coordInputs.length; index++) {
+                const input = coordInputs[index];
+                const currentValue = parseInt(input.value);
+                await userEvent.type(input, `${currentValue + 1}`);
+                expect(onChangePropsMock).toHaveBeenCalledTimes(1);
+                onChangePropsMock.mockClear();
+            }
+        });
+    });
+});
--- packages/perseus-editor/src/components/locked-vector-settings.tsx
+++ packages/perseus-editor/src/components/locked-vector-settings.tsx
@@ -4,10 +4,11 @@
  *
  * Used in the interactive graph editor's locked figures section.
  */
+import {vector as kvector} from "@khanacademy/kmath";
 import {View} from "@khanacademy/wonder-blocks-core";
 import {Strut} from "@khanacademy/wonder-blocks-layout";
 import {spacing, color as wbColor} from "@khanacademy/wonder-blocks-tokens";
-import {LabelLarge} from "@khanacademy/wonder-blocks-typography";
+import {LabelLarge, LabelMedium} from "@khanacademy/wonder-blocks-typography";
 import {StyleSheet} from "aphrodite";
 import * as React from "react";
 import {useState} from "react";
@@ -25,6 +26,8 @@ import type {
     LockedFigureColor,
     LockedVectorType,
 } from "@khanacademy/perseus";
+
+const lengthErrorMessage = "The vector cannot have length 0.";
 
 export type Props = LockedVectorType &
     AccordionProps & {
@@ -45,8 +48,11 @@ const LockedVectorSettings = (props: Props) => {
     const [tail, tip] = points;
     const lineLabel = `Vector (${tail[0]}, ${tail[1]}), (${tip[0]}, ${tip[1]})`;
 
+    // Check if the line has length 0.
+    const isInvalid = kvector.equal(tail, tip);
+
     function handleChangePoint(newCoord: Coord | undefined, index: 0 | 1) {
-        if (newCoord) {
+        if (typeof newCoord !== "undefined") {
             const newPoints = [...points] as [Coord, Coord];
             newPoints[index] = [...newCoord];
             onChangeProps({
@@ -81,6 +87,13 @@ const LockedVectorSettings = (props: Props) => {
                 />
             </View>
 
+            {/* Zero length error message */}
+            {isInvalid && (
+                <LabelMedium style={styles.errorText}>
+                    {lengthErrorMessage}
+                </LabelMedium>
+            )}
+
             <LockedFigureSettingsAccordion
                 expanded={tailCoordExpanded}
                 onToggle={() => setTailCoordExpanded(!tailCoordExpanded)}
@@ -95,7 +108,7 @@ const LockedVectorSettings = (props: Props) => {
             >
                 <CoordinatePairInput
                     coord={tail}
-                    // error={!!error}
+                    error={isInvalid}
                     onChangeProps={(newProps) => {
                         handleChangePoint(newProps.coord, 0);
                     }}
@@ -116,7 +129,7 @@ const LockedVectorSettings = (props: Props) => {
             >
                 <CoordinatePairInput
                     coord={tip}
-                    // error={!!error}
+                    error={isInvalid}
                     onChangeProps={(newProps) => {
                         handleChangePoint(newProps.coord, 1);
                     }}
@@ -144,6 +157,9 @@ const styles = StyleSheet.create({
         marginLeft: -spacing.xxxSmall_4,
         marginRight: -spacing.xxxSmall_4,
         backgroundColor: wbColor.white,
+    },
+    errorText: {
+        color: wbColor.red,
     },
     row: {
         flexDirection: "row",
