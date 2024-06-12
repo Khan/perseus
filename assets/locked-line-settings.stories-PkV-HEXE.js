1f14d8b5459788f756b7fa4e660dc5f060f19be9
nishasy <nisha@khanacademy.org>
2024-06-12 19:36:30 +0000

    Deploying to gh-pages from @ Khan/perseus@18be26f35f735e6ec985d65d48d9859e5227fa39 🚀

---
+++ assets/locked-line-settings.stories-PkV-HEXE.js
@@ -0,0 +1,66 @@
+import{j as c}from"./jsx-runtime-BGVbfQ2Z.js";import{r as p}from"./index-qhcEwEpg.js";import{L as s}from"./locked-line-settings-iOafhCGe.js";import{g as n}from"./util-NNtyu1l3.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./util-EDQcR2F7.js";import"./index-default-4_ZsnO94.js";import"./perseus-error-OpXxk17X.js";import"./dependencies-fnqF3NiV.js";import"./compare-59FC1ybr.js";import"./version-akiLXZts.js";import"./jquery-5v7aFUvu.js";import"./svg-image-e9ScUZD0.js";import"./index-mohBxQl_.js";import"./index-awljIyHI.js";import"./index-4c2J3ov1.js";import"./index-lUErx3pE.js";import"./index-dnMhQZ-1.js";import"./fixed-to-responsive-ybwlrogx.js";import"./constants-I_nlPaPx.js";import"./index-E09jvG0x.js";import"./index-J2t_5nK1.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-BrzYBUzY.js";import"./answer-choices-S7xRMWa3.js";import"./index-qh_wob3p.js";import"./index-VhM44oCk.js";import"./index-tvtfaFq4.js";import"./index-h47zdzUa.js";import"./index-_15Y2y0p.js";import"./index-8OBRI-bh.js";import"./index-kutQl4v0.js";import"./index-JT1-kTlx.js";import"./Popper-uHddJoXq.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./renderer-X_-ZNtau.js";import"./invariant-bu5zBsRS.js";import"./asset-context-pmjKTqqL.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./perseus-api-XkoWPDUZ.js";import"./index-k-0mNqHS.js";import"./stub-tag-editor-f1WdYARp.js";import"./text-list-editor-xQBHt64k.js";import"./lint-4QkP-VXi.js";import"./locked-figure-settings-actions-YNYZLhO-.js";import"./choice-rFAFMrR4.js";import"./index-3tBZ6RgE.js";import"./index-xE_wKg8s.js";import"./icon-HbAIhp4d.js";import"./media-queries-MaBBbpNq.js";import"./choice-icon-8EqwF5gf.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-atX5Qolq.js";import"./color-select-8DXorrBN.js";import"./article-renderer-99EgFiZG.js";import"./prop-types-6blYoVJL.js";import"./index-halg33Zp.js";import"./tabbar-_HYg3Kcf.js";import"./item-MgaZQyPg.js";import"./button-assets-cmoMUwP4.js";import"./keypad-button-BUF7auet.js";import"./operators-page-H6dLcd0l.js";import"./navigation-pad-URlj9thj.js";import"./key-translator-t-qTln-I.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./math-input-SkZCBhNw.js";import"./unit-ovoXKu5O.js";import"./input-with-examples-ifHlUmph.js";import"./math-output-me3cXzXD.js";import"./text-input-DttuQkw0.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-Yf867jpU.js";import"./base-radio-CkXWQyrR.js";import"./shared-3pf9YZIg.js";import"./choice-none-above-CmMdvRha.js";import"./video-transcript-link-g12jSPmn.js";import"./button-group-eBTrRsKy.js";import"./graph-q0IbaOlx.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-7OZQMH1X.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-k4bRs4vK.js";import"./prop-check-box-GJ7Xmp8F.js";import"./range-input-zCtWZXYe.js";import"./marker-S6XAW3NZ.js";import"./answer-pill-n_QOxv7f.js";import"./sortable-lY63oqxH.js";import"./multi-renderer-UDqKIQbY.js";import"./hints-renderer-3l5YUqZx.js";import"./color-swatch-OONT2VsD.js";import"./labeled-switch-Ud4hRrtF.js";import"./line-swatch-TEP3l1x8.js";const g=[[-10,10],[-10,10]],vr={title:"PerseusEditor/Components/Locked Line Settings",component:s},m=i=>c(s,{...i});m.args={...n("line"),onChangeProps:()=>{},onRemove:()=>{}};const d={render:function(){const[e,o]=p.useState({...n("line"),onRemove:()=>{}});return c(s,{...e,range:g,onChangeProps:t=>{o({...e,...t})}})}};d.parameters={chromatic:{disableSnapshot:!0}};const a={render:function(){const[e,o]=p.useState({...n("line"),onRemove:()=>{}}),r=t=>{o({...e,...t})};return c(s,{...e,points:[n("point"),n("point")],range:g,expanded:!0,onChangeProps:r})}},u={render:function(){const[e,o]=p.useState(!0),[r,t]=p.useState({...n("line"),onRemove:()=>{}});return c(s,{...r,range:g,expanded:e,onToggle:o,onChangeProps:P=>{t({...r,...P})}})}},l={render:function(){const[e,o]=p.useState(!0),[r,t]=p.useState({...n("line"),onRemove:()=>{},kind:"segment",color:"green",lineStyle:"dashed"});return c(s,{...r,range:g,expanded:e,onToggle:o,onChangeProps:P=>{t({...r,...P})}})}};var h,f,R;m.parameters={...m.parameters,docs:{...(h=m.parameters)==null?void 0:h.docs,source:{originalSource:`(args): React.ReactElement => {
+  return <LockedLineSettings {...args} />;
+}`,...(R=(f=m.parameters)==null?void 0:f.docs)==null?void 0:R.source}}};var S,x,L;d.parameters={...d.parameters,docs:{...(S=d.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: function Render() {
+    const [props, setProps] = React.useState({
+      ...getDefaultFigureForType("line"),
+      onRemove: () => {}
+    });
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedLineSettings {...props} range={defaultRange} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(L=(x=d.parameters)==null?void 0:x.docs)==null?void 0:L.source}}};var U,E,F,v,C;a.parameters={...a.parameters,docs:{...(U=a.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: function Render() {
+    const [props, setProps] = React.useState({
+      ...getDefaultFigureForType("line"),
+      onRemove: () => {}
+    });
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedLineSettings {...props} points={[getDefaultFigureForType("point"), getDefaultFigureForType("point")]} range={defaultRange} expanded={true} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(F=(E=a.parameters)==null?void 0:E.docs)==null?void 0:F.source},description:{story:`If the two points defining the line are the same, the line is invalid
+as that would give it a length of 0. An error message is displayed
+in this case.`,...(C=(v=a.parameters)==null?void 0:v.docs)==null?void 0:C.description}}};var y,T,k;u.parameters={...u.parameters,docs:{...(y=u.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: function Render() {
+    const [expanded, setExpanded] = React.useState(true);
+    const [props, setProps] = React.useState({
+      ...getDefaultFigureForType("line"),
+      onRemove: () => {}
+    });
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedLineSettings {...props} range={defaultRange} expanded={expanded} onToggle={setExpanded} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(k=(T=u.parameters)==null?void 0:T.docs)==null?void 0:k.source}}};var w,D,I;l.parameters={...l.parameters,docs:{...(w=l.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: function Render() {
+    const [expanded, setExpanded] = React.useState(true);
+    const [props, setProps] = React.useState({
+      ...getDefaultFigureForType("line"),
+      onRemove: () => {},
+      kind: ("segment" as const),
+      color: ("green" as const),
+      lineStyle: ("dashed" as const)
+    });
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedLineSettings {...props} range={defaultRange} expanded={expanded} onToggle={setExpanded} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(I=(D=l.parameters)==null?void 0:D.docs)==null?void 0:I.source}}};const Cr=["Default","Controlled","WithInvalidPoints","Expanded","ExpandedNondefaultProps"];export{d as Controlled,m as Default,u as Expanded,l as ExpandedNondefaultProps,a as WithInvalidPoints,Cr as __namedExportsOrder,vr as default};
--- assets/text-list-editor.stories-MEcEdeir.js
+++
@@ -1,8 +0,0 @@
-import{j as s}from"./jsx-runtime-BGVbfQ2Z.js";import{a as n}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{T as a}from"./text-list-editor-bJphcq85.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./jquery-5v7aFUvu.js";import"./index-tvtfaFq4.js";import"./index-E09jvG0x.js";import"./index-default-4_ZsnO94.js";const v={title:"Perseus/Components/Text List Editor"},p={onChange:(...e)=>{n("onChange")(...e)},options:["Test option 1","Test option 2","Test option 3"]},m="framework-perseus orderer",t=e=>s("div",{class:m,children:s(a,{...p})});var r,o,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
-  return (
-    // @ts-expect-error [FEI-5003] - TS2322 - Type '{ children: Element; class: string; }' is not assignable to type 'DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>'.
-    <div class={ClassName}>
-            <TextListEditor {...defaultObject} />
-        </div>
-  );
-}`,...(i=(o=t.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const C=["SimpleListOfOptions"];export{t as SimpleListOfOptions,C as __namedExportsOrder,v as default};
