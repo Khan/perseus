0c4e396572b88b5a0535004078a60b5b00a3fec9
Ben Christel <benchristel@khanacademy.org>
2024-06-18 13:41:51 -0700

    Make protractor draggable

--- packages/perseus/src/widgets/interactive-graphs/stateful-protractor.tsx
+++ packages/perseus/src/widgets/interactive-graphs/stateful-protractor.tsx
@@ -1,6 +1,19 @@
 import * as React from "react";
+import {useRef, useState} from "react";
+import {useMovable, vec} from "mafs";
 import {Protractor} from "./protractor";
 
 export function StatefulProtractor() {
-    return <Protractor center={[0, -9]} />
+    const [center, setCenter] = useState<vec.Vector2>([0, -9])
+
+    const draggableRef = useRef<SVGGElement>(null);
+
+    useMovable({
+        gestureTarget: draggableRef,
+        onMove: setCenter,
+        point: center,
+        constrain: (p) => p,
+    })
+
+    return <g ref={draggableRef}><Protractor center={center} /></g>
 }
