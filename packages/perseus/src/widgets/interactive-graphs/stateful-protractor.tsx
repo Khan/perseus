36c889b9da8d852a42b4aa0e7b6b0f4fe0ea8325
Ben Christel <benchristel@khanacademy.org>
2024-06-18 13:55:23 -0700

    Take graph range into account when positioning protractor

--- packages/perseus/src/widgets/interactive-graphs/stateful-protractor.tsx
+++ packages/perseus/src/widgets/interactive-graphs/stateful-protractor.tsx
@@ -2,12 +2,17 @@ import * as React from "react";
 import {useRef, useState} from "react";
 import {useMovable, vec} from "mafs";
 import {Protractor} from "./protractor";
+import useGraphConfig from "./reducer/use-graph-config";
 
 export function StatefulProtractor() {
-    const [center, setCenter] = useState<vec.Vector2>([0, -9])
+    const {range: [[xMin, xMax], [yMin, yMax]]} = useGraphConfig()
+    const initialCenter: vec.Vector2 = [
+        lerp(xMin, xMax, 0.5),
+        lerp(yMin, yMax, 0.05),
+    ]
+    const [center, setCenter] = useState<vec.Vector2>(initialCenter)
 
     const draggableRef = useRef<SVGGElement>(null);
-
     useMovable({
         gestureTarget: draggableRef,
         onMove: setCenter,
@@ -16,4 +21,13 @@ export function StatefulProtractor() {
     })
 
     return <g ref={draggableRef}><Protractor center={center} /></g>
+}
+
+// [L]inear Int[erp]olation: gets the weighted average of two values `a` and
+// `b`, with the given `fraction` specifying how much weight `a` and `b` get:
+// - if `fraction` is 0, `lerp` returns `a`.
+// - if `fraction` is 0.5, `lerp` returns the average of `a` and `b`.
+// - if `fraction` is 1, `lerp` returns `b`.
+function lerp(a: number, b: number, fraction: number): number {
+    return (b - a) * fraction + a
 }
