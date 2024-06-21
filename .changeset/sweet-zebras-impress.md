8966083f5d35c84939258555bcd4acd4421a795f
Nisha Yerunkar <nisha@khanacademy.org>
2024-06-21 14:59:59 -0700

    [Storybook: Editor Page] Add a working preview for the editor page in storybook
    The preview iframe doesn't work in storybook, because in links to a local URL
    to webapp when it's in prod. It has never worked in storybook.
    
    We need to be able to see what the initial state of the questions on the editor
    page would look like without making any changes to the actual preview (yet).
    
    I've added a preview here that shows the question and the hints in the editor
    page. This works for all widgets. It's sticky, so it follows as you scroll
    down a long editor. You can also close it if it's taking too much space.
    
    This is a prerequisite to viewing an interactive graph with initial coordinates.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-2051
    
    Test plan:
    http://localhost:6006/?path=/story/perseuseditor-editorpage--demo

---
+++ .changeset/sweet-zebras-impress.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/perseus-editor": patch
+---
+
+[Storybook: Editor Page] Add a storybook-only preview for the questions and hints in the EditorPage component
--- packages/perseus-editor/src/__stories__/editor-page.stories.tsx
+++ packages/perseus-editor/src/__stories__/editor-page.stories.tsx
@@ -10,6 +10,7 @@ import {EditorPage} from "..";
 import {segmentWithLockedFigures} from "../../../perseus/src/widgets/__testdata__/interactive-graph.testdata";
 import {registerAllWidgetsAndEditorsForTesting} from "../util/register-all-widgets-and-editors-for-testing";
 
+import EditorPageUtil from "./editor-page-util";
 import {flags} from "./flags-for-api-options";
 
 import type {
@@ -28,67 +29,12 @@ export default {
 const onChangeAction = action("onChange");
 
 export const Demo = (): React.ReactElement => {
-    const [previewDevice, setPreviewDevice] =
-        React.useState<DeviceType>("phone");
-    const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
-    const [answerArea, setAnswerArea] = React.useState<
-        PerseusAnswerArea | undefined | null
-    >();
-    const [question, setQuestion] = React.useState<
-        PerseusRenderer | undefined
-    >();
-    const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
-
-    return (
-        <EditorPage
-            apiOptions={{
-                isMobile: false,
-                flags,
-            }}
-            previewDevice={previewDevice}
-            onPreviewDeviceChange={(newDevice) => setPreviewDevice(newDevice)}
-            developerMode={true}
-            jsonMode={jsonMode}
-            answerArea={answerArea}
-            question={question}
-            hints={hints}
-            frameSource="about:blank"
-            previewURL="about:blank"
-            itemId="1"
-            onChange={(props) => {
-                onChangeAction(props);
-
-                if ("jsonMode" in props) {
-                    setJsonMode(props.jsonMode);
-                }
-                if ("answerArea" in props) {
-                    setAnswerArea(props.answerArea);
-                }
-                if ("question" in props) {
-                    setQuestion(props.question);
-                }
-                if ("hints" in props) {
-                    setHints(props.hints);
-                }
-            }}
-        />
-    );
+    return <EditorPageUtil />;
 };
 
 export const MafsWithLockedFiguresCurrent = (): React.ReactElement => {
-    const [previewDevice, setPreviewDevice] =
-        React.useState<DeviceType>("phone");
-    const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
-    const [answerArea, setAnswerArea] = React.useState<
-        PerseusAnswerArea | undefined | null
-    >();
-    const [question, setQuestion] = React.useState<PerseusRenderer | undefined>(
-        segmentWithLockedFigures,
-    );
-    const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
-
     return (
-        <EditorPage
+        <EditorPageUtil
             apiOptions={{
                 isMobile: false,
                 flags: {
@@ -99,32 +45,7 @@ export const MafsWithLockedFiguresCurrent = (): React.ReactElement => {
                     },
                 },
             }}
-            previewDevice={previewDevice}
-            onPreviewDeviceChange={(newDevice) => setPreviewDevice(newDevice)}
-            developerMode={true}
-            jsonMode={jsonMode}
-            answerArea={answerArea}
-            question={question}
-            hints={hints}
-            frameSource="about:blank"
-            previewURL="about:blank"
-            itemId="1"
-            onChange={(props) => {
-                onChangeAction(props);
-
-                if ("jsonMode" in props) {
-                    setJsonMode(props.jsonMode);
-                }
-                if ("answerArea" in props) {
-                    setAnswerArea(props.answerArea);
-                }
-                if ("question" in props) {
-                    setQuestion(props.question);
-                }
-                if ("hints" in props) {
-                    setHints(props.hints);
-                }
-            }}
+            question={segmentWithLockedFigures}
         />
     );
 };
@@ -138,19 +59,8 @@ MafsWithLockedFiguresCurrent.parameters = {
 };
 
 export const MafsWithLockedFiguresM2Flag = (): React.ReactElement => {
-    const [previewDevice, setPreviewDevice] =
-        React.useState<DeviceType>("phone");
-    const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
-    const [answerArea, setAnswerArea] = React.useState<
-        PerseusAnswerArea | undefined | null
-    >();
-    const [question, setQuestion] = React.useState<PerseusRenderer | undefined>(
-        segmentWithLockedFigures,
-    );
-    const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
-
     return (
-        <EditorPage
+        <EditorPageUtil
             apiOptions={{
                 isMobile: false,
                 flags: {
@@ -161,32 +71,7 @@ export const MafsWithLockedFiguresM2Flag = (): React.ReactElement => {
                     },
                 },
             }}
-            previewDevice={previewDevice}
-            onPreviewDeviceChange={(newDevice) => setPreviewDevice(newDevice)}
-            developerMode={true}
-            jsonMode={jsonMode}
-            answerArea={answerArea}
-            question={question}
-            hints={hints}
-            frameSource="about:blank"
-            previewURL="about:blank"
-            itemId="1"
-            onChange={(props) => {
-                onChangeAction(props);
-
-                if ("jsonMode" in props) {
-                    setJsonMode(props.jsonMode);
-                }
-                if ("answerArea" in props) {
-                    setAnswerArea(props.answerArea);
-                }
-                if ("question" in props) {
-                    setQuestion(props.question);
-                }
-                if ("hints" in props) {
-                    setHints(props.hints);
-                }
-            }}
+            question={segmentWithLockedFigures}
         />
     );
 };
@@ -200,51 +85,7 @@ MafsWithLockedFiguresM2Flag.parameters = {
 };
 
 export const MafsWithLockedFiguresM2bFlag = (): React.ReactElement => {
-    const [previewDevice, setPreviewDevice] =
-        React.useState<DeviceType>("phone");
-    const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
-    const [answerArea, setAnswerArea] = React.useState<
-        PerseusAnswerArea | undefined | null
-    >();
-    const [question, setQuestion] = React.useState<PerseusRenderer | undefined>(
-        segmentWithLockedFigures,
-    );
-    const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
-
-    return (
-        <EditorPage
-            apiOptions={{
-                isMobile: false,
-                flags,
-            }}
-            previewDevice={previewDevice}
-            onPreviewDeviceChange={(newDevice) => setPreviewDevice(newDevice)}
-            developerMode={true}
-            jsonMode={jsonMode}
-            answerArea={answerArea}
-            question={question}
-            hints={hints}
-            frameSource="about:blank"
-            previewURL="about:blank"
-            itemId="1"
-            onChange={(props) => {
-                onChangeAction(props);
-
-                if ("jsonMode" in props) {
-                    setJsonMode(props.jsonMode);
-                }
-                if ("answerArea" in props) {
-                    setAnswerArea(props.answerArea);
-                }
-                if ("question" in props) {
-                    setQuestion(props.question);
-                }
-                if ("hints" in props) {
-                    setHints(props.hints);
-                }
-            }}
-        />
-    );
+    return <EditorPageUtil question={segmentWithLockedFigures} />;
 };
 
 MafsWithLockedFiguresM2bFlag.parameters = {
