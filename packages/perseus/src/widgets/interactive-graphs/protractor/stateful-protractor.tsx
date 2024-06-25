c3a6e6a4c4d85966514c7c450034315403ab1352
Ben Christel <benchristel@khanacademy.org>
2024-06-25 15:47:04 -0700

    Add copyright notice

--- packages/perseus/src/widgets/interactive-graphs/protractor/stateful-protractor.tsx
+++ packages/perseus/src/widgets/interactive-graphs/protractor/stateful-protractor.tsx
@@ -53,6 +53,22 @@ function constrainToCircle(edgePoint: vec.Vector2) {
     return vec.withMag(edgePoint, protractorRadius);
 }
 
+/* The implementation of useDraggablePx is derived from
+ * https://github.com/stevenpetryk/mafs/blob/4520319379a2cc2df8148d8baaef1f85db117103/src/interaction/useMovable.tsx#L20-L83
+ * and copied here under the terms of the MIT license.
+ *
+ * Copyright 2021 Steven Petryk
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
 function useDraggablePx(args: {
     gestureTarget: RefObject<Element>;
     onMove?: (point: vec.Vector2) => unknown;
