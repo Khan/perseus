921300fb9d884e5336cd8af8225786adc7eed73c
Ben Christel <benchristel@khanacademy.org>
2024-06-25 14:38:58 -0700

    Create a new useDraggablePx hook

--- packages/perseus/src/widgets/interactive-graphs/stateful-protractor.tsx
+++ packages/perseus/src/widgets/interactive-graphs/stateful-protractor.tsx
@@ -1,15 +1,18 @@
-import {vec} from "mafs";
+import {useTransformContext, vec} from "mafs";
 import * as React from "react";
-import {useRef, useState} from "react";
+import {RefObject, useRef, useState} from "react";
 
 import {useDraggable} from "./graphs/use-draggable";
 import {centerToRotationHandle, Protractor} from "./protractor";
 import useGraphConfig from "./reducer/use-graph-config";
 import {bound} from "./utils";
+import {useDrag} from "@use-gesture/react";
 
 export function StatefulProtractor() {
     const {range, snapStep} = useGraphConfig();
     const [[xMin, xMax], [yMin, yMax]] = range;
+    // Position the protractor in the center of the graph (horizontally), and
+    // 95% of the way to the bottom of the graph (vertically).
     const initialCenter: vec.Vector2 = [
         lerp(xMin, xMax, 0.5),
         lerp(yMin, yMax, 0.05),
@@ -27,11 +30,11 @@ export function StatefulProtractor() {
     });
 
     const rotationHandleRef = useRef<SVGGElement>(null);
-    useDraggable({
+    useDraggablePx({
         gestureTarget: rotationHandleRef,
-        onDrag: ({deltaPx}) =>
-            setRotationHandleOffset(constrainToCircle(deltaPx)),
+        onMove: (pointPx) => setRotationHandleOffset(pointPx),
         point: rotationHandleOffset,
+        constrain: constrainToCircle,
     });
 
     return (
@@ -48,6 +51,39 @@ export function StatefulProtractor() {
 const protractorRadius = vec.mag(centerToRotationHandle)
 function constrainToCircle(edgePoint: vec.Vector2) {
     return vec.withMag(edgePoint, protractorRadius);
+}
+
+function useDraggablePx(args: {
+    gestureTarget: RefObject<Element>;
+    onMove?: (point: vec.Vector2) => unknown;
+    point: vec.Vector2;
+    constrain?: (point: vec.Vector2) => vec.Vector2;
+}): void {
+    const {
+        gestureTarget: target,
+        onMove,
+        point,
+        constrain = (p) => p,
+    } = args;
+
+    const pickupPx = React.useRef<vec.Vector2>([0, 0]);
+
+    useDrag(
+        (state) => {
+            const {event, first, movement: pixelMovement} = state;
+            event?.stopPropagation();
+
+            if (first) {
+                pickupPx.current = point;
+            }
+            if (vec.mag(pixelMovement) === 0) {
+                return;
+            }
+
+            onMove?.(constrain(vec.add(pickupPx.current, pixelMovement)));
+        },
+        {target, eventOptions: {passive: false}},
+    );
 }
 
 // [L]inear Int[erp]olation: gets the weighted average of two values `a` and
