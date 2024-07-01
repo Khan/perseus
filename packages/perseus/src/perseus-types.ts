8578b70611fa9457e86526f01f292fca6aca36e5
Mark Fitzgerald <markfitzgerald@markfitgeraldm3.home>
2024-07-01 16:13:09 -0700

    Add unit tests for question builder. Add comment to clarify difference between "equation" and "equationParsed".

--- packages/perseus/src/perseus-types.ts
+++ packages/perseus/src/perseus-types.ts
@@ -740,8 +740,12 @@ export type LockedFunctionType = {
     type: "function";
     color: LockedFigureColor;
     strokeStyle: "solid" | "dashed";
-    equation: string;
+    equation: string; // This is the user-defined equation (as it was typed)
     equationParsed?: {
+        // This is the parsed (tokenized) version of the equation.
+        // Since the function that is passed to Mafs is executed many times,
+        //    it would be expensive to have KAS parse the equation each time.
+        // This is parsed version is included to aid in performance.
         // KAS doesn't have any types, so making this generic
         [k: string]: any;
     };
