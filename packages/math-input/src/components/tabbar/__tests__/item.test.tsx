ff4a2412c6dfedc61e82582abae24d006a56fabd
Cat Johnson <catjohnson@khanacademy.org>
2024-07-02 15:56:16 -0700

    Cleaning up linting issue in items.test

--- packages/math-input/src/components/tabbar/__tests__/item.test.tsx
+++ packages/math-input/src/components/tabbar/__tests__/item.test.tsx
@@ -1,4 +1,4 @@
-import {fireEvent, render, screen} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import {userEvent as userEventLib} from "@testing-library/user-event";
 import * as React from "react";
 
