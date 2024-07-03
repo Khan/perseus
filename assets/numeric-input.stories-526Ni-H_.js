230d892edc4a8efe3dbe14b888b1347c828e2be5
benchristel <benchristel@users.noreply.github.com>
2024-07-03 22:03:06 +0000

    Deploying to gh-pages from @ Khan/perseus@7e6ccf38da2f385cbd7a1db4cf81f858997ffba8 🚀

---
+++ assets/numeric-input.stories-526Ni-H_.js
@@ -0,0 +1,44 @@
+import{j as o,a as e}from"./jsx-runtime-BGVbfQ2Z.js";import{a as i}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{R as A}from"./renderer-with-debug-ui-H_eM-nvf.js";import{N as n}from"./article-renderer-xi_h8_ou.js";import"./index-qhcEwEpg.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./choice-unuhbNi1.js";import"./index-TeOhC3cV.js";import"./index-awljIyHI.js";import"./index-tvtfaFq4.js";import"./index-oMd7J_hd.js";import"./index-lUErx3pE.js";import"./index-OY3Waz-e.js";import"./index-TymzEsVR.js";import"./index-DDXzYFOx.js";import"./index-E09jvG0x.js";import"./index-JT1-kTlx.js";import"./index-O-KD2pfb.js";import"./index-KIBY7gd7.js";import"./index-abFgrC9j.js";import"./index-1qqOuAdm.js";import"./Popper-uHddJoXq.js";import"./index-dnMhQZ-1.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./icon-HbAIhp4d.js";import"./perseus-api-BF68z3pH.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./jquery-5v7aFUvu.js";import"./index-default-4_ZsnO94.js";import"./media-queries-MaBBbpNq.js";import"./constants-I_nlPaPx.js";import"./choice-icon-8EqwF5gf.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-AuJwhOz7.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-atX5Qolq.js";import"./side-by-side-N0r3j8vl.js";import"./util-WvQWLN3r.js";import"./perseus-error-OpXxk17X.js";import"./dependencies-fnqF3NiV.js";import"./compare-VLxlEkSV.js";import"./version-akiLXZts.js";import"./unit-uxf7imOh.js";import"./multi-renderer-_ObSf2O4.js";import"./renderer-0WTyF9ZX.js";import"./index-J2t_5nK1.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-xT-AdWRe.js";import"./fixed-to-responsive-ybwlrogx.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./lint-4QkP-VXi.js";import"./hints-renderer-EQV0-CzM.js";import"./shared-3pf9YZIg.js";import"./button-group-eBTrRsKy.js";import"./base-radio-ZMSPcM4W.js";import"./choice-none-above-Hcuv7pqR.js";import"./graph-cl_EfieJ.js";import"./hud-Q7Km3N9P.js";import"./info-tip-8pHiOl5T.js";import"./math-input-Z6lNLhZg.js";import"./prop-types-oYjo1VuK.js";import"./index-3xb9Ri9D.js";import"./tabbar-_VM3Ijmc.js";import"./item-HTAlbNMZ.js";import"./button-assets-cmoMUwP4.js";import"./keypad-button-41QiUrLb.js";import"./operators-page-jZhMvB4n.js";import"./navigation-pad-GvGLKm0w.js";import"./key-translator-yjRRNSuW.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-QmtkIb5a.js";import"./prop-check-box-yQKE-Nit.js";import"./index-ZObC1LzG.js";import"./minus-bold-ONmDo3Ve.js";import"./range-input-IMzA4hKn.js";import"./text-input-yDsqFbe3.js";import"./register-all-widgets-for-testing-fCPMZsPY.js";import"./enums-x5qaTru7.js";import"./input-with-examples-qIcgAf54.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-NKfzL8jd.js";import"./video-transcript-link-BKfVoN1g.js";import"./answer-choices-Ay8Xtf93.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./marker-OdTsGYa1.js";import"./answer-pill-O4c6x4l9.js";import"./sortable-1YPVk0eX.js";const N={content:"$5008 \\div 4 =$ [[☃ numeric-input 1]] ",images:{},widgets:{"numeric-input 1":{graded:!0,version:{major:0,minor:0},static:!1,type:"numeric-input",options:{coefficient:!1,static:!1,answers:[{status:"correct",maxError:null,strict:!1,value:1252,simplify:"required",message:""}],labelText:"",size:"normal"},alignment:"default"}}};function s(r){return{...{alignment:"",answers:[],containerSizeClass:"medium",isLastUsedWidget:!0,coefficient:!1,currentValue:"",problemNum:0,reviewModeRubric:{answers:[],labelText:"",size:"medium",coefficient:!1,static:!1},rightAlign:!1,size:"normal",static:!1,widgetId:"widgetId",findWidgets:i("findWidgets"),onBlur:i("onBlur"),onChange:i("onChange"),onFocus:i("onFocus"),trackInteraction:i("trackInteraction")},...r}}const dt={title:"Perseus/Widgets/NumericInput",args:{coefficient:!1,currentValue:"8675309",rightAlign:!1},argTypes:{size:{options:["normal","small"],control:{type:"radio"},defaultValue:"normal"}}},m=()=>o(A,{question:N}),p=r=>{const t=s(r);return o(n,{...t})},a=r=>{const t=s({...r,size:"small"}),c=s({...r,size:"normal"});return e("div",{children:[e("label",{children:["Small:",o(n,{...t})]}),e("label",{children:["Normal:",o(n,{...c})]})]})},l=r=>{const t=s({...r,rightAlign:!1}),c=s({...r,rightAlign:!0});return e("div",{children:[e("label",{children:["Left:",o(n,{...t})]}),e("label",{children:["Right:",o(n,{...c})]})]})};var u,g,d;m.parameters={...m.parameters,docs:{...(u=m.parameters)==null?void 0:u.docs,source:{originalSource:`(): React.ReactElement => {
+  return <RendererWithDebugUI question={question1} />;
+}`,...(d=(g=m.parameters)==null?void 0:g.docs)==null?void 0:d.source}}};var f,h,b;p.parameters={...p.parameters,docs:{...(f=p.parameters)==null?void 0:f.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const props = generateProps(args);
+  return <NumericInput {...props} />;
+}`,...(b=(h=p.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var P,I,R;a.parameters={...a.parameters,docs:{...(P=a.parameters)==null?void 0:P.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const smallProps = generateProps({
+    ...args,
+    size: "small"
+  });
+  const normalProps = generateProps({
+    ...args,
+    size: "normal"
+  });
+  return <div>
+            <label>
+                Small:
+                <NumericInput {...smallProps} />
+            </label>
+            <label>
+                Normal:
+                <NumericInput {...normalProps} />
+            </label>
+        </div>;
+}`,...(R=(I=a.parameters)==null?void 0:I.docs)==null?void 0:R.source}}};var v,S,z;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const leftProps = generateProps({
+    ...args,
+    rightAlign: false
+  });
+  const rightProps = generateProps({
+    ...args,
+    rightAlign: true
+  });
+  return <div>
+            <label>
+                Left:
+                <NumericInput {...leftProps} />
+            </label>
+            <label>
+                Right:
+                <NumericInput {...rightProps} />
+            </label>
+        </div>;
+}`,...(z=(S=l.parameters)==null?void 0:S.docs)==null?void 0:z.source}}};const ft=["Question1","Interactive","Sizes","TextAlignment"];export{p as Interactive,m as Question1,a as Sizes,l as TextAlignment,ft as __namedExportsOrder,dt as default};
