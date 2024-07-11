16883b1503b36ca9c89f999074f172f75bad71af
Ben Christel <benchristel@khanacademy.org>
2024-07-11 15:07:23 -0700

    Fix warnings about missing React keys

--- packages/perseus/src/widgets/passage/passage-markdown.tsx
+++ packages/perseus/src/widgets/passage/passage-markdown.tsx
@@ -289,7 +289,7 @@ const rules = {
                 >
                     <span style={SQUARE_LABEL_STYLE}>{node.content}</span>
                 </span>,
-                <AltText id="questionMarker" number={node.content} />,
+                <AltText key="alt-text" id="questionMarker" number={node.content} />,
                 node.space ? "\u00A0" : null,
             ];
         },
@@ -322,7 +322,7 @@ const rules = {
                 >
                     <span style={CIRCLE_LABEL_STYLE}>{node.content}</span>
                 </span>,
-                <AltText id="circleMarker" number={node.content} />,
+                <AltText key="alt-text" id="circleMarker" number={node.content} />,
                 node.space ? "\u00A0" : null,
             ];
         },
@@ -352,7 +352,7 @@ const rules = {
                 >
                     [{node.content}]
                 </span>,
-                <AltText id="sentenceMarker" number={node.content} />,
+                <AltText key="alt-text" id="sentenceMarker" number={node.content} />,
                 node.space ? "\u00A0" : null,
             ];
         },
