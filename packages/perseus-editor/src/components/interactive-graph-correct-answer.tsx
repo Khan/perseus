850496d376cb4a8cec89a9996ad7317f91ad3c07
Nicole Watts <nicole@khanacademy.org>
2024-07-16 13:19:36 -0600

    wip: lint

--- packages/perseus-editor/src/components/interactive-graph-correct-answer.tsx
+++ packages/perseus-editor/src/components/interactive-graph-correct-answer.tsx
@@ -1,21 +1,12 @@
-import * as perseus from "@khanacademy/perseus";
 import {View} from "@khanacademy/wonder-blocks-core";
-import {spacing} from "@khanacademy/wonder-blocks-tokens";
+import {spacing, color} from "@khanacademy/wonder-blocks-tokens";
 import {
     BodyMonospace,
     LabelXSmall,
 } from "@khanacademy/wonder-blocks-typography";
 import * as React from "react";
-import {color} from "@khanacademy/wonder-blocks-tokens";
 
-import LabeledRow from "./graph-locked-figures/labeled-row";
 import Heading from "./heading";
-import {
-    paddingMini,
-    paddingSmall,
-} from "../../../perseus/src/styles/global-constants";
-
-const {InfoTip} = perseus.components;
 
 type Props = {
     equationString: string;
