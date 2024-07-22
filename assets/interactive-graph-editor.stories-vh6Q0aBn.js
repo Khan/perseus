53921ea99c79bf1e7c43b837674249493ae43502
handeyeco <matthew.c.tx@gmail.com>
2024-07-22 17:32:37 +0000

    Deploying to gh-pages from @ Khan/perseus@1e4e7f38a52db66b5d3ea233ad3c8abb9df9724f 🚀

---
+++ assets/interactive-graph-editor.stories-vh6Q0aBn.js
@@ -0,0 +1,86 @@
+import{j as e,a as b}from"./jsx-runtime-BGVbfQ2Z.js";import{V as y}from"./index-oMd7J_hd.js";import{S as Pe}from"./index-KIBY7gd7.js";import{s as i,c as k}from"./index-lUErx3pE.js";import{L as be,B as ye}from"./index-B3k62xyQ.js";import{a as ke}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{l as Ee}from"./index-awljIyHI.js";import{r as o}from"./index-qhcEwEpg.js";import"./index-cjtFLnYt.js";import{s as qe,a as Me,l as Le,b as Qe,r as Ae,c as Ge,q as Ie,d as Fe,p as xe,e as W}from"./interactive-graph.testdata-fguzZ3Pf.js";import{r as De,E as je}from"./editor-page-uYHTfju9.js";import{E as r}from"./editor-page-with-storybook-preview-a1_vI-Ha.js";import{f as R}from"./flags-for-api-options-Q2y6yNaP.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./version-akiLXZts.js";import"./article-renderer-Re2WVwo8.js";import"./compare-VLxlEkSV.js";import"./index-default-4_ZsnO94.js";import"./prop-types-oYjo1VuK.js";import"./index-J2t_5nK1.js";import"./index-E09jvG0x.js";import"./index-3xb9Ri9D.js";import"./tabbar-_VM3Ijmc.js";import"./item-HTAlbNMZ.js";import"./index-TeOhC3cV.js";import"./index-tvtfaFq4.js";import"./button-assets-cmoMUwP4.js";import"./keypad-button-41QiUrLb.js";import"./operators-page-jZhMvB4n.js";import"./navigation-pad-GvGLKm0w.js";import"./index-JT1-kTlx.js";import"./key-translator-yjRRNSuW.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./perseus-error-l3K_anoI.js";import"./renderer--qvWlSJl.js";import"./index-dnMhQZ-1.js";import"./jquery-5v7aFUvu.js";import"./asset-context-pmjKTqqL.js";import"./svg-image-ImGr8npy.js";import"./index-TymzEsVR.js";import"./dependencies-fnqF3NiV.js";import"./util-Kf-UsGkl.js";import"./fixed-to-responsive-ybwlrogx.js";import"./constants-I_nlPaPx.js";import"./inline-icon-MJBS_-2U.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-BrzYBUzY.js";import"./tex-SwZEYYPF.js";import"./zoomable-QW77xrsT.js";import"./zoomable-tex-LJgLrbXQ.js";import"./perseus-api-BF68z3pH.js";import"./stub-tag-editor-CaJYfBue.js";import"./text-list-editor-bJphcq85.js";import"./lint-4QkP-VXi.js";import"./index-JK7f6wub.js";import"./index-1lR5LStt.js";import"./index-O-KD2pfb.js";import"./Popper-uHddJoXq.js";import"./i18n-context-9-s9cJ--.js";import"./strings-YJ61eiUN.js";import"./math-input-Ah4BQ4X3.js";import"./index-_6fd2Dz3.js";import"./unit-3mV5sRes.js";import"./input-with-examples-iGYlZivN.js";import"./text-input-yDsqFbe3.js";import"./index-ZObC1LzG.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-K2JbXmT6.js";import"./simple-keypad-input-NKfzL8jd.js";import"./base-radio-L2CQ04LW.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-U2lXOTna.js";import"./icon-HbAIhp4d.js";import"./choice-icon-8EqwF5gf.js";import"./focus-ring-SGFL5fpl.js";import"./option-status-atX5Qolq.js";import"./choice-none-above-MDBaQstQ.js";import"./video-transcript-link-BKfVoN1g.js";import"./answer-choices-Y9Xz84Nm.js";import"./button-group-eBTrRsKy.js";import"./graph-Bysq6csc.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-3RuEOGQS.js";import"./hud-Q7Km3N9P.js";import"./info-tip-_E95qi8X.js";import"./multi-button-group-rHU9dGVb.js";import"./number-input-nytuiJQZ.js";import"./prop-check-box-s_QybB1E.js";import"./range-input-qxBl8f4w.js";import"./marker-Y5tkNjao.js";import"./answer-pill-hif8yB8_.js";import"./sortable-PbO9y_mO.js";import"./multi-renderer-E2m2EDWw.js";import"./hints-renderer-0ujxAU8-.js";import"./components-IoB9EuXw.js";import"./device-framer-FeWA0GYq.js";import"./constants-hQyjCzwz.js";import"./section-control-button-Rv8DtNJ_.js";import"./editor-xRlEMW5e.js";import"./tex-error-view-v4keycbh.js";import"./toggleable-caret-1K5GCJBX.js";import"./viewport-resizer-Lf_9VVmQ.js";import"./structured-item-diff-iF1hoClH.js";import"./text-diff-vULwiqdC.js";import"./tags-diff-V311L9T1.js";import"./multi-items-kvLgs9ow.js";/* empty css                       */import"./interactive-graph-question-builder-4ueePKDD.js";import"./categorizer-editor-UxadJgHG.js";import"./editor-jsonify-lRJL2fNS.js";import"./blur-input-0MWX_AIX.js";import"./definition-editor-wFlqZNe_.js";import"./dropdown-editor-12wVwPP_.js";import"./explanation-editor--JcElSla.js";import"./expression-editor-zXft8F52.js";import"./graph-settings-QRRnHwVY.js";import"./image-editor-uyssZ4ZS.js";import"./input-number-editor-tzM6xDYq.js";import"./interaction-editor-oKh7iHoV.js";import"./label-image-editor-uT7vuLcB.js";import"./form-wrapped-text-field-r_uB0yly.js";import"./answer-choices-a_FDdxg5.js";import"./behavior-VjNEnhuA.js";import"./question-markers-ujWqX6op.js";import"./marker-S2CLFUwK.js";import"./util-RR67UMDN.js";import"./select-image-PKST75iK.js";import"./matcher-editor-Od0YWcI8.js";import"./number-line-editor-i5_iAJ6F.js";import"./numeric-input-editor-amdfv_Fk.js";import"./python-program-editor-ZR9iAAZ_.js";import"./editor-9qUOoMAb.js";import"./sorter-editor-mbTFjDLJ.js";import"./item-extras-editor-zJKeiRLz.js";import"./interactive-graph-settings-NbZnnuHH.js";import"./heading-T1HN2tJ9.js";import"./locked-figures-section-_-Fhcwsb.js";import"./locked-ellipse-settings-kEbbjCXe.js";import"./locked-figure-settings-actions-kAOXNEKS.js";import"./color-select-QJpnMZmK.js";import"./color-swatch-ZDQkIqmr.js";import"./locked-line-settings-OimxISLn.js";import"./labeled-switch-Xe0Qv-z2.js";import"./line-swatch-41PCTnZr.js";import"./locked-point-settings-kOkajudc.js";import"./locked-polygon-settings-QwIHIo2H.js";import"./locked-vector-settings-pYpI6FXT.js";De();const Lo={title:"PerseusEditor/Widgets/Interactive Graph"},_e=ke("onChange"),c=()=>e(r,{question:qe}),u=()=>e(r,{question:Me}),d=()=>e(r,{question:Le}),g=()=>e(r,{question:Qe}),l=()=>e(r,{question:Ae}),h=()=>e(r,{question:Ge}),f=()=>e(r,{question:Ie}),S=()=>e(r,{question:Fe}),v=()=>e(r,{question:xe}),s=()=>e(r,{apiOptions:{isMobile:!1,flags:{mafs:{...R.mafs,"start-coords-ui":!1,"interactive-graph-locked-features-m2":!1,"interactive-graph-locked-features-m2b":!1}}},question:W});s.parameters={chromatic:{disable:!0}};const a=()=>e(r,{apiOptions:{isMobile:!1,flags:{mafs:{...R.mafs,"start-coords-ui":!1,"interactive-graph-locked-features-m2":!0,"interactive-graph-locked-features-m2b":!1}}},question:W});a.parameters={chromatic:{disable:!0}};const n=()=>e(r,{question:W});n.parameters={chromatic:{disable:!0}};const m=()=>{const[de,ge]=o.useState("phone"),[le,he]=o.useState(!1),[fe,Se]=o.useState(),[C,ve]=o.useState(W),[P,We]=o.useState(),[we,Re]=o.useState([]),p=o.useRef(null);return o.useEffect(()=>{if(p.current){const t=p.current.getSaveWarnings();Re(t)}},[p,C,P]),b(y,{style:w.container,children:[e(je,{ref:p,apiOptions:{isMobile:!1,flags:R},previewDevice:de,onPreviewDeviceChange:t=>ge(t),developerMode:!0,jsonMode:le,answerArea:fe,question:C,hints:P,frameSource:"about:blank",previewURL:"about:blank",itemId:"1",onChange:t=>{_e(t),"jsonMode"in t&&he(t.jsonMode),"answerArea"in t&&Se(t.answerArea),"question"in t&&ve(t.question),"hints"in t&&We(t.hints)}}),b(y,{style:w.errorContainer,children:[e(be,{children:"Save Warnings:"}),e(Pe,{size:i.small_12}),we.map((t,Ce)=>e(ye,{style:w.errorMessage,children:t},Ce))]})]})};m.parameters={chromatic:{disable:!0}};const w=Ee.StyleSheet.create({container:{flexDirection:"row"},errorContainer:{border:`1px solid ${k.offBlack}`,padding:i.medium_16,flexGrow:1,marginRight:i.xLarge_32,height:"80vh",position:"sticky",top:i.xLarge_32,overflowY:"auto"},errorMessage:{color:k.red,marginBottom:i.small_12}});var E,q,M;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={segmentWithStartingCoordsQuestion} />;
+}`,...(M=(q=c.parameters)==null?void 0:q.docs)==null?void 0:M.source}}};var L,Q,A;u.parameters={...u.parameters,docs:{...(L=u.parameters)==null?void 0:L.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={segmentsWithStartingCoordsQuestion} />;
+}`,...(A=(Q=u.parameters)==null?void 0:Q.docs)==null?void 0:A.source}}};var G,I,F;d.parameters={...d.parameters,docs:{...(G=d.parameters)==null?void 0:G.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={linearWithStartingCoordsQuestion} />;
+}`,...(F=(I=d.parameters)==null?void 0:I.docs)==null?void 0:F.source}}};var x,D,j;g.parameters={...g.parameters,docs:{...(x=g.parameters)==null?void 0:x.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={linearSystemWithStartingCoordsQuestion} />;
+}`,...(j=(D=g.parameters)==null?void 0:D.docs)==null?void 0:j.source}}};var _,O,B;l.parameters={...l.parameters,docs:{...(_=l.parameters)==null?void 0:_.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={rayWithStartingCoordsQuestion} />;
+}`,...(B=(O=l.parameters)==null?void 0:O.docs)==null?void 0:B.source}}};var V,H,J;h.parameters={...h.parameters,docs:{...(V=h.parameters)==null?void 0:V.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={circleWithStartingCoordsQuestion} />;
+}`,...(J=(H=h.parameters)==null?void 0:H.docs)==null?void 0:J.source}}};var z,T,U;f.parameters={...f.parameters,docs:{...(z=f.parameters)==null?void 0:z.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={quadraticWithStartingCoordsQuestion} />;
+}`,...(U=(T=f.parameters)==null?void 0:T.docs)==null?void 0:U.source}}};var Y,$,K;S.parameters={...S.parameters,docs:{...(Y=S.parameters)==null?void 0:Y.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={sinusoidWithStartingCoordsQuestion} />;
+}`,...(K=($=S.parameters)==null?void 0:$.docs)==null?void 0:K.source}}};var N,X,Z;v.parameters={...v.parameters,docs:{...(N=v.parameters)==null?void 0:N.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={polygonQuestion} />;
+}`,...(Z=(X=v.parameters)==null?void 0:X.docs)==null?void 0:Z.source}}};var ee,te,re;s.parameters={...s.parameters,docs:{...(ee=s.parameters)==null?void 0:ee.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview apiOptions={{
+    isMobile: false,
+    flags: {
+      mafs: {
+        ...flags.mafs,
+        "start-coords-ui": false,
+        "interactive-graph-locked-features-m2": false,
+        "interactive-graph-locked-features-m2b": false
+      }
+    }
+  }} question={segmentWithLockedFigures} />;
+}`,...(re=(te=s.parameters)==null?void 0:te.docs)==null?void 0:re.source}}};var oe,ie,se;a.parameters={...a.parameters,docs:{...(oe=a.parameters)==null?void 0:oe.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview apiOptions={{
+    isMobile: false,
+    flags: {
+      mafs: {
+        ...flags.mafs,
+        "start-coords-ui": false,
+        "interactive-graph-locked-features-m2": true,
+        "interactive-graph-locked-features-m2b": false
+      }
+    }
+  }} question={segmentWithLockedFigures} />;
+}`,...(se=(ie=a.parameters)==null?void 0:ie.docs)==null?void 0:se.source}}};var ae,ne,me;n.parameters={...n.parameters,docs:{...(ae=n.parameters)==null?void 0:ae.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={segmentWithLockedFigures} />;
+}`,...(me=(ne=n.parameters)==null?void 0:ne.docs)==null?void 0:me.source}}};var pe,ce,ue;m.parameters={...m.parameters,docs:{...(pe=m.parameters)==null?void 0:pe.docs,source:{originalSource:`(): React.ReactElement => {
+  const [previewDevice, setPreviewDevice] = React.useState<DeviceType>("phone");
+  const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
+  const [answerArea, setAnswerArea] = React.useState<PerseusAnswerArea | undefined | null>();
+  const [question, setQuestion] = React.useState<PerseusRenderer | undefined>(segmentWithLockedFigures);
+  const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
+  const [saveWarnings, setSaveWarnings] = React.useState<string[]>([]);
+  const editorPageRef = React.useRef<EditorPage>(null);
+  React.useEffect(() => {
+    if (editorPageRef.current) {
+      const warnings = editorPageRef.current.getSaveWarnings();
+      setSaveWarnings(warnings);
+    }
+  }, [editorPageRef, question, hints]);
+  return <View style={styles.container}>
+            <EditorPage ref={editorPageRef} apiOptions={{
+      isMobile: false,
+      flags
+    }} previewDevice={previewDevice} onPreviewDeviceChange={newDevice => setPreviewDevice(newDevice)} developerMode={true} jsonMode={jsonMode} answerArea={answerArea} question={question} hints={hints} frameSource="about:blank" previewURL="about:blank" itemId="1" onChange={props => {
+      onChangeAction(props);
+      if ("jsonMode" in props) {
+        setJsonMode(props.jsonMode);
+      }
+      if ("answerArea" in props) {
+        setAnswerArea(props.answerArea);
+      }
+      if ("question" in props) {
+        setQuestion(props.question);
+      }
+      if ("hints" in props) {
+        setHints(props.hints);
+      }
+    }} />
+            <View style={styles.errorContainer}>
+                <LabelLarge>Save Warnings:</LabelLarge>
+                <Strut size={spacing.small_12} />
+                {saveWarnings.map((warning, index) => <Body key={index} style={styles.errorMessage}>
+                        {warning}
+                    </Body>)}
+            </View>
+        </View>;
+}`,...(ue=(ce=m.parameters)==null?void 0:ce.docs)==null?void 0:ue.source}}};const Qo=["InteractiveGraphSegmentWithStartingCoords","InteractiveGraphSegmentsWithStartingCoords","InteractiveGraphLinearWithStartingCoords","InteractiveGraphLinearSystemWithStartingCoords","InteractiveGraphRayWithStartingCoords","InteractiveGraphCircleWithStartingCoords","InteractiveGraphQuadraticWithStartingCoords","InteractiveGraphSinusoidWithStartingCoords","InteractiveGraphPolygon","MafsWithLockedFiguresCurrent","MafsWithLockedFiguresM2Flag","MafsWithLockedFiguresM2bFlag","WithSaveWarnings"];export{h as InteractiveGraphCircleWithStartingCoords,g as InteractiveGraphLinearSystemWithStartingCoords,d as InteractiveGraphLinearWithStartingCoords,v as InteractiveGraphPolygon,f as InteractiveGraphQuadraticWithStartingCoords,l as InteractiveGraphRayWithStartingCoords,c as InteractiveGraphSegmentWithStartingCoords,u as InteractiveGraphSegmentsWithStartingCoords,S as InteractiveGraphSinusoidWithStartingCoords,s as MafsWithLockedFiguresCurrent,a as MafsWithLockedFiguresM2Flag,n as MafsWithLockedFiguresM2bFlag,m as WithSaveWarnings,Qo as __namedExportsOrder,Lo as default};
