eaa82303cc01f9854b03772b316d9aa3f62cebe8
Sarah Third <sarahthird@gmail.com>
2024-07-22 12:25:42 -0700

    Fix for angle graph scoring issue

--- packages/perseus/src/widgets/interactive-graphs/reducer/interactive-graph-state.test.ts
+++ packages/perseus/src/widgets/interactive-graphs/reducer/interactive-graph-state.test.ts
@@ -1,3 +1,5 @@
+import invariant from "tiny-invariant";
+
 import {getGradableGraph} from "./interactive-graph-state";
 
 import type {InteractiveGraphState} from "../types";
@@ -27,5 +29,94 @@ describe("getGradableGraph", () => {
         const result = getGradableGraph(state, initialGraph);
 
         expect(result).toEqual(initialGraph);
+    });
+
+    // (LEMS-2190): This test is to ensure that the new Mafs graph is reversing coordinates when the angle graph is reflexive
+    // in a clockwise direction in order to temporarily maintain the same angle scoring with the legacy graph.
+    it("returns reversed coordinates if the angle graph is reflexive when not allowed", () => {
+        const state: InteractiveGraphState = {
+            type: "angle",
+            coords: [
+                [-5, 0],
+                [0, 0],
+                [5, 5],
+            ],
+            hasBeenInteractedWith: true,
+            range: [
+                [-10, 10],
+                [-10, 10],
+            ],
+            snapStep: [1, 1],
+            showAngles: true,
+            allowReflexAngles: false,
+        };
+        const initialGraph: PerseusGraphType = {
+            type: "angle",
+        };
+        const result = getGradableGraph(state, initialGraph);
+        invariant(result.type === "angle");
+        expect(result.coords).toEqual([
+            [5, 5],
+            [0, 0],
+            [-5, 0],
+        ]);
+    });
+
+    it("does not reverse coordinates if the angle graph is not reflexive", () => {
+        const state: InteractiveGraphState = {
+            type: "angle",
+            coords: [
+                [5, 0],
+                [0, 0],
+                [5, 5],
+            ],
+            hasBeenInteractedWith: true,
+            range: [
+                [-10, 10],
+                [-10, 10],
+            ],
+            snapStep: [1, 1],
+            showAngles: true,
+            allowReflexAngles: false,
+        };
+        const initialGraph: PerseusGraphType = {
+            type: "angle",
+        };
+        const result = getGradableGraph(state, initialGraph);
+        invariant(result.type === "angle");
+        expect(result.coords).toEqual([
+            [5, 0],
+            [0, 0],
+            [5, 5],
+        ]);
+    });
+
+    it("does not reverse coordinates if the angle graph is allowed to be reflexive", () => {
+        const state: InteractiveGraphState = {
+            type: "angle",
+            coords: [
+                [5, 0],
+                [0, 0],
+                [-5, -5],
+            ],
+            hasBeenInteractedWith: true,
+            range: [
+                [-10, 10],
+                [-10, 10],
+            ],
+            snapStep: [1, 1],
+            showAngles: true,
+            allowReflexAngles: true,
+        };
+        const initialGraph: PerseusGraphType = {
+            type: "angle",
+        };
+        const result = getGradableGraph(state, initialGraph);
+        invariant(result.type === "angle");
+        expect(result.coords).toEqual([
+            [5, 0],
+            [0, 0],
+            [-5, -5],
+        ]);
     });
 });
