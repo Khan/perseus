252452981f7b9bc5404bebadc2a06fba8a2a0244
benchristel <benchristel@users.noreply.github.com>
2024-07-24 16:13:07 +0000

    Deploying to gh-pages from @ Khan/perseus@83bebcfafb7691475dda6a34aa4c92156d56a6e4 🚀

---
+++ assets/hints-renderer.stories-QOnhN1fy.js
@@ -0,0 +1,34 @@
+import{j as r}from"./jsx-runtime-5BUNAZ9W.js";import{V as c}from"./index-e4P84RkC.js";import{H as l}from"./hints-renderer-5MYO6gBp.js";import{i as d}from"./interactive-graph-question-builder-4ueePKDD.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-awljIyHI.js";import"./renderer-jXtsf37s.js";import"./version-akiLXZts.js";import"./perseus-error-l3K_anoI.js";import"./index-tvtfaFq4.js";import"./index-J2t_5nK1.js";import"./index-dnMhQZ-1.js";import"./jquery-5v7aFUvu.js";import"./index-jmm5gWkb.js";import"./index-default-4_ZsnO94.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-D5zktulZ.js";import"./index-zXbQRqKp.js";import"./index-lUErx3pE.js";import"./dependencies-9B_Bv_mA.js";import"./util-Kf-UsGkl.js";import"./compare-VLxlEkSV.js";import"./fixed-to-responsive-Q2lYGnje.js";import"./constants-I_nlPaPx.js";import"./client-MU6fCXSs.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-A74C9PE_.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./perseus-api-cFDIds9P.js";import"./stub-tag-editor-HTvc7FsQ.js";import"./text-list-editor-A-LpoxgU.js";import"./lint-IvfTv29b.js";import"./i18n-context-3gTlIcWM.js";import"./strings-YJ61eiUN.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";const $={title:"Perseus/Renderers/Hints Renderer",component:l,decorators:[m=>r(c,{style:{left:80},children:r(m,{})})],argTypes:{hintsVisible:{control:{min:0},defaultValue:3}}},t={args:{hints:[{content:"this is hint 1",images:{},replace:!1,widgets:{}},{content:"this is hint 2",images:{},replace:!1,widgets:{}},{content:"this is hint 3",images:{},replace:!1,widgets:{}}]}},e={args:{apiOptions:{flags:{mafs:{segment:!0}}},hints:[{...d().build(),replace:!1}]}};var i,s,n;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    hints: [{
+      content: "this is hint 1",
+      images: {},
+      replace: false,
+      widgets: {}
+    }, {
+      content: "this is hint 2",
+      images: {},
+      replace: false,
+      widgets: {}
+    }, {
+      content: "this is hint 3",
+      images: {},
+      replace: false,
+      widgets: {}
+    }]
+  }
+}`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var o,a,p;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    apiOptions: {
+      flags: {
+        mafs: {
+          segment: true
+        }
+      }
+    },
+    hints: [{
+      ...interactiveGraphQuestionBuilder().build(),
+      replace: false
+    }]
+  }
+}`,...(p=(a=e.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};const tt=["Interactive","WithInteractiveGraph"];export{t as Interactive,e as WithInteractiveGraph,tt as __namedExportsOrder,$ as default};
--- assets/register-all-widgets-for-testing-KtBrWPWq.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer--mm2tkQt.js";import{r,a as s}from"./renderer-2ebjVYNa.js";const a=()=>{r(e),s()};export{a as r};
