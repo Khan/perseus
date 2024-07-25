130ab94465da943c1582851122a409d72c6a96e1
Nicole W. <76132106+nicolecomputer@users.noreply.github.com>
2024-07-25 11:41:09 -0600

    Throttle events in polygon (#1444)
    ## Summary:
    
    Throttles the events in polygon to increase performance and decrease CPU load. This is especially important for Safari. This change lowers CPU usage from 87% to 30% on a M3 macbook. The increase in performance will be more on phones and iPads where this will also decrease energy usage.
    
    40 FPS was chosen as a mark that feels response and good to use but doesn't put too much stress on the machine.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-2185
    
    ## Test plan:
    
    To test before:
    - Load the dev environment on Safari
    - Open the Timelines pane and start recording
    - The CPU usage should be high (it'll vary based on your machine)
    
    To test after:
    - Repeat test above with this change
    - **The CPU usage should be lower**
    
    Author: nicolecomputer
    
    Reviewers: benchristel, nicolecomputer, jeremywiebe, mark-fitzgerald
    
    Required Reviewers:
    
    Approved By: benchristel
    
    Checks: ✅ codecov/project, ❌ codecov/patch, ✅ Upload Coverage (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Jest Coverage (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/1444

--- packages/perseus/src/widgets/interactive-graphs/graphs/polygon.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/polygon.tsx
@@ -44,6 +44,7 @@ export const PolygonGraph = (props: Props) => {
     });
 
     const active = hovered || focused || dragging;
+    const lastMoveTime = React.useRef<number>(0);
 
     const lines = getLines(points);
 
@@ -120,9 +121,16 @@ export const PolygonGraph = (props: Props) => {
                     key={"point-" + i}
                     snapTo={snapTo}
                     point={point}
-                    onMove={(destination: vec.Vector2) =>
-                        dispatch(actions.polygon.movePoint(i, destination))
-                    }
+                    onMove={(destination: vec.Vector2) => {
+                        const now = Date.now();
+                        const targetFPS = 40;
+                        const moveThresholdTime = 1000 / targetFPS;
+
+                        if (now - lastMoveTime.current > moveThresholdTime) {
+                            dispatch(actions.polygon.movePoint(i, destination));
+                            lastMoveTime.current = now;
+                        }
+                    }}
                 />
             ))}
         </>
