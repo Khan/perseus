5d7f3762e94cf48b4f7796947f1face78c4e457f
Jeremy Wiebe <jeremy@khanacademy.org>
2024-07-26 14:47:45 -0700

    ContentRenderer: Implement nochrome

--- packages/perseus-editor/src/preview/content-renderer.tsx
+++ packages/perseus-editor/src/preview/content-renderer.tsx
@@ -3,10 +3,12 @@ import {
     MobileKeypad,
     StatefulKeypadContextProvider,
 } from "@khanacademy/math-input";
-import {Renderer} from "@khanacademy/perseus";
+import {Renderer, constants} from "@khanacademy/perseus";
 // eslint-disable-next-line monorepo/no-internal-import
 import {mockStrings} from "@khanacademy/perseus/strings";
 import {View} from "@khanacademy/wonder-blocks-core";
+import {spacing} from "@khanacademy/wonder-blocks-tokens";
+import {StyleSheet} from "aphrodite";
 import * as React from "react";
 
 import type {APIOptions, PerseusRenderer} from "@khanacademy/perseus";
@@ -14,28 +16,30 @@ import type {APIOptions, PerseusRenderer} from "@khanacademy/perseus";
 function ComponentRenderer({
     question,
     apiOptions,
+    seamless,
 }: {
     question?: PerseusRenderer;
     apiOptions?: APIOptions;
+    seamless?: boolean;
 }) {
     return (
-        <View style={{marginRight: "40px"}}>
+        <View style={[styles.container, !seamless ? styles.gutter : undefined]}>
             <StatefulKeypadContextProvider>
                 <KeypadContext.Consumer>
                     {({setKeypadActive, keypadElement, setKeypadElement}) => (
                         <>
-                <Renderer
-                    strings={mockStrings}
-                    apiOptions={apiOptions}
+                            <Renderer
+                                strings={mockStrings}
+                                apiOptions={apiOptions}
                                 keypadElement={keypadElement}
-                    linterContext={{
-                        contentType: "exercise",
-                        highlightLint: true,
-                        paths: [],
-                        stack: [],
-                    }}
-                    {...question}
-                />
+                                linterContext={{
+                                    contentType: "exercise",
+                                    highlightLint: true,
+                                    paths: [],
+                                    stack: [],
+                                }}
+                                {...question}
+                            />
 
                             <MobileKeypad
                                 onAnalyticsEvent={() => Promise.resolve()}
@@ -50,4 +54,8 @@ function ComponentRenderer({
     );
 }
 
+const styles = StyleSheet.create({
+    container: {padding: spacing.xxxSmall_4},
+    gutter: {marginRight: constants.lintGutterWidth},
+});
 export default ComponentRenderer;
