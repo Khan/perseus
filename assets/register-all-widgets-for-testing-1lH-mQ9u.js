5fba396e22c54729444e986c27145873414bb0e2
nishasy <nisha@khanacademy.org>
2024-07-30 22:39:01 +0000

    Deploying to gh-pages from @ Khan/perseus@79a09d62f532fd3373fea4838c0d72812de14046 🚀

--- assets/register-all-widgets-for-testing-1lH-mQ9u.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-c9wJCiG_.js";import{r,a as s}from"./renderer-hf8U9G1r.js";const a=()=>{r(e),s()};export{a as r};
---
+++ assets/register-all-widgets-for-testing-bMZsp0Z9.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-L7KUz41O.js";import{r,a as s}from"./renderer-jUEWD-kS.js";const a=()=>{r(e),s()};export{a as r};
