5a49b0fc1bce6a48215513305ebe81081fb5320e
nishasy <nisha@khanacademy.org>
2024-07-31 22:39:51 +0000

    Deploying to gh-pages from @ Khan/perseus@6e1ec850c7efb186444dcd9023e2a2c37cd731d2 🚀

---
+++ assets/graded-group-set.stories-9y4OXCHB.js
@@ -0,0 +1,93 @@
+import{j as a}from"./jsx-runtime-5BUNAZ9W.js";import{R as s}from"./renderer-with-debug-ui-l8nyDYaJ.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./choice-qG_WG2X3.js";import"./index-Cz55-Vre.js";import"./index-awljIyHI.js";import"./index-tvtfaFq4.js";import"./index-e4P84RkC.js";import"./index-lUErx3pE.js";import"./index-QR_wlop5.js";import"./index-zXbQRqKp.js";import"./index-skotlSua.js";import"./index-zE8cp1oq.js";import"./index-wjVcXLkf.js";import"./index-wgZGcu4m.js";import"./index-jmm5gWkb.js";import"./x-6ZxseNgc.js";import"./Popper-D86xJ3go.js";import"./index-dnMhQZ-1.js";import"./i18n-context-DsDGD6dy.js";import"./strings-3jqcfOzr.js";import"./icon-TA3bBVIW.js";import"./perseus-api-cFDIds9P.js";import"./stub-tag-editor-HTvc7FsQ.js";import"./text-list-editor-A-LpoxgU.js";import"./jquery-5v7aFUvu.js";import"./index-default-4_ZsnO94.js";import"./media-queries-MaBBbpNq.js";import"./constants-I_nlPaPx.js";import"./choice-icon-401hL_KJ.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./focus-ring-dCiEd0Hc.js";import"./option-status-DOdQ-QjF.js";import"./article-renderer-AxicPl9z.js";import"./util-Q3BXsyUV.js";import"./version-akiLXZts.js";import"./perseus-error-l3K_anoI.js";import"./prop-types-ew9eayR0.js";import"./index-J2t_5nK1.js";import"./mobile-keypad-8hkmmP-T.js";import"./index-FsYHUvK_.js";import"./tabbar-tY-vWj59.js";import"./item-kdyp1JvI.js";import"./button-assets-W0P3gTHH.js";import"./keypad-button-dkA26ccQ.js";import"./operators-page-AaL31SSd.js";import"./navigation-pad-J3QJzD-6.js";import"./key-translator-NrLPOiYy.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-JrrbFnUG.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-1N9MukZl.js";import"./dependencies-9B_Bv_mA.js";import"./fixed-to-responsive-Q2lYGnje.js";import"./client-MU6fCXSs.js";import"./image-loader-A74C9PE_.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./lint-IvfTv29b.js";import"./info-tip-7k4XWhfR.js";import"./index-zHEZmPKZ.js";import"./math-input-NQxFBc18.js";import"./input-with-examples-oId8yfSS.js";import"./text-input-Q1V0RBx1.js";import"./index-_rEjQfTM.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-vkx2pUmk.js";import"./simple-keypad-input-tVloz1vV.js";import"./base-radio-jjzrjL5j.js";import"./shared-3pf9YZIg.js";import"./choice-none-above-aC8Dh0vL.js";import"./video-transcript-link-WioFGlG-.js";import"./answer-choices-NDfgNgvH.js";import"./button-group-KR3umc1e.js";import"./graph-xQwCqgdo.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-ylPat90q.js";import"./hud-CJYktPgf.js";import"./multi-button-group-AxfQn4Cq.js";import"./number-input-qa6WcWGj.js";import"./prop-check-box-f7I2HerX.js";import"./range-input-8CEPNQ51.js";import"./marker-tCR8bInb.js";import"./answer-pill-kCSNcNzQ.js";import"./sortable-jy7O6trk.js";import"./side-by-side-qr1qIzeQ.js";import"./multi-renderer-bBGToKbn.js";import"./hints-renderer-cyjup-_j.js";import"./register-all-widgets-for-testing-cQiND53r.js";const n={content:`#Section 1: Adding tenths less than one
+
+[[☃ graded-group-set 1]]
+
+
+Beautiful, let's move on to problems with whole numbers and tenths.`,images:{},widgets:{"graded-group-set 1":{type:"graded-group-set",alignment:"default",static:!1,graded:!0,options:{gradedGroups:[{title:"Problem 1a",content:"$0.5 + 0.4 =$   [[☃ numeric-input 1]]",widgets:{"numeric-input 1":{type:"numeric-input",alignment:"default",static:!1,graded:!0,options:{static:!1,answers:[{value:.9,status:"correct",message:"",simplify:"required",strict:!1,maxError:null}],size:"normal",coefficient:!1,labelText:""},version:{major:0,minor:0}}},images:{},hasHint:!0,hint:{content:`There are many ways to solve this problem. Let's see two student solutions.
+
+###Student A's solution:
+
+I thought in terms of tenths.
+
+$\\phantom{=}0.5 + 0.4$
+
+$=5$ tenths $+ ~4$ tenths
+
+$=9$ tenths
+
+$=0.9$
+
+###Student B's solution:
+
+I used tenths grids.
+
+[[☃ image 1]]
+
+[[☃ image 2]]
+
+[[☃ image 3]]
+
+$\\blueD{0.5} + \\greenD{0.4} = 0.9$
+
+###The answer:
+
+$0.5 + 0.4 = 0.9$`,images:{},widgets:{"image 3":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[234,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/cda00c119dac3e52c8ed150ef4a9a37355f5c713",width:234,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 5 of the rows are shaded in blue and 4 of the rows are shaded in green.",caption:""},version:{major:0,minor:0}},"image 2":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/217d580bc0baddb903bbcb648fc8d3ea3d0f4408",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 4 of the rows are shaded to represent 4 tenths.",caption:""},version:{major:0,minor:0}},"image 1":{options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/2a56a60275b7227ed9c5b89e489587c8cb13eb7b",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 5 of the rows are shaded to represent 5 tenths.",caption:""},type:"image",version:{major:0,minor:0},graded:!0,alignment:"block",static:!1}}},widgetEnabled:!0,immutableWidgets:!1},{title:"Problem 1b",content:"$0.6 + 0.4 =$   [[☃ numeric-input 1]]",widgets:{"numeric-input 1":{type:"numeric-input",alignment:"default",static:!1,graded:!0,options:{static:!1,answers:[{value:1,status:"correct",message:"",simplify:"required",strict:!1,maxError:null}],size:"normal",coefficient:!1,labelText:""},version:{major:0,minor:0}}},images:{},hasHint:!0,hint:{content:`There are many ways to solve this problem. Let's see two student solutions.
+
+###Student A's solution:
+
+I thought in terms of tenths.
+
+$\\phantom{=}0.6 + 0.4$
+
+$=6$ tenths $+ ~4$ tenths
+
+$=10$ tenths
+
+$=1$
+
+###Student B's solution:
+
+I used tenths grids.
+
+[[☃ image 1]]
+
+[[☃ image 2]]
+
+[[☃ image 3]]
+
+$\\blueD{0.6} + \\greenD{0.4} = 1$
+
+###The answer:
+
+$0.6 + 0.4 = 1$`,images:{},widgets:{"image 3":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[234,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/54fbc06d92097119f1be5d1679391596209296a7",width:234,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 6 of the rows are shaded in blue and 4 of the rows are shaded in green.",caption:""},version:{major:0,minor:0}},"image 2":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/217d580bc0baddb903bbcb648fc8d3ea3d0f4408",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 4 of the rows are shaded to represent 4 tenths.",caption:""},version:{major:0,minor:0}},"image 1":{options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/8e353e65446a4322f53d640aba33dccd69b0874c",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 6 of the rows are shaded to represent 6 tenths.",caption:""},type:"image",version:{major:0,minor:0},graded:!0,alignment:"block",static:!1}}},widgetEnabled:!0,immutableWidgets:!1},{title:"Problem 1c",content:"$0.8 + 0.4 =$   [[☃ numeric-input 1]]",widgets:{"numeric-input 1":{type:"numeric-input",alignment:"default",static:!1,graded:!0,options:{static:!1,answers:[{value:1.2,status:"correct",message:"",simplify:"required",strict:!1,maxError:null}],size:"normal",coefficient:!1,labelText:""},version:{major:0,minor:0}}},images:{},hasHint:!0,hint:{content:`There are many ways to solve this problem. Let's see two student solutions.
+
+###Student A's solution:
+
+I thought in terms of tenths.
+
+$\\phantom{=}0.8 + 0.4$
+
+$=8$ tenths $+ ~4$ tenths
+
+$=12$ tenths
+
+$=1.2$
+
+###Student B's solution:
+
+I used tenths grids.
+
+[[☃ image 1]]
+
+[[☃ image 2]]
+
+[[☃ image 3]]
+$\\blueD{0.8} + \\greenD{0.4} = 1.2$
+
+###The answer:
+
+$ 0.8+0.4=1.2 $`,images:{},widgets:{"image 3":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[354,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/4b595ea53e5edc2b5991f2171f384a7eae2eeb24",width:354,height:132},labels:[],alt:"Two equal sized squares. Each square is divided into 10 rows to show tenths. In the first square, 8 of the rows are shaded in blue and 2 of the rows are shaded in green. In the second square, 2 of the rows are shaded in green.",caption:""},version:{major:0,minor:0}},"image 2":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/217d580bc0baddb903bbcb648fc8d3ea3d0f4408",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 4 of the rows are shaded to represent 4 tenths.",caption:""},version:{major:0,minor:0}},"image 1":{options:{static:!1,title:"",range:[[0,10],[0,10]],box:[180,132],backgroundImage:{url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/58d09bc7bdfadbd860c55734bf146578f047edbc",width:180,height:132},labels:[],alt:"A square divided into 10 rows to show tenths. 8 of the rows are shaded to represent 8 tenths.",caption:""},type:"image",version:{major:0,minor:0},graded:!0,alignment:"block",static:!1}}},widgetEnabled:!0,immutableWidgets:!1}]},version:{major:0,minor:0}}}},e=o=>a(s,{apiOptions:{isMobile:o.isMobile},question:n}),Je={title:"Perseus/Widgets/Graded Group Set",args:{isMobile:!1}};e.__docgenInfo={description:"",methods:[],displayName:"Article1",props:{isMobile:{required:!0,tsType:{name:"boolean"},description:""}}};var t,i,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <RendererWithDebugUI apiOptions={{
+    isMobile: args.isMobile
+  }} question={article1} />;
+}`,...(r=(i=e.parameters)==null?void 0:i.docs)==null?void 0:r.source}}};const Ke=["Article1"];export{e as Article1,Ke as __namedExportsOrder,Je as default};
---
+++ assets/interactive-graph-editor.stories-p5VqsyMT.js
@@ -0,0 +1,84 @@
+import{j as e,a as P}from"./jsx-runtime-5BUNAZ9W.js";import{V as b}from"./index-e4P84RkC.js";import{S as Re}from"./index-wjVcXLkf.js";import{s as m,c as I}from"./index-lUErx3pE.js";import{L as Pe,B as be}from"./index-QR_wlop5.js";import{a as Ie}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{l as ke}from"./index-awljIyHI.js";import{r as o}from"./index-4g5l5LRQ.js";import"./index-5CyAFdB_.js";import{s as Ee,a as Me,l as qe,b as _e,r as Le,c as Ge,q as Qe,d as Ae,p as Fe,e as W}from"./interactive-graph.testdata-uxc_klQ7.js";import{r as xe}from"./register-all-widgets-and-editors-for-testing-ClpDg8FG.js";import{E as r}from"./editor-page-with-storybook-preview-HF8fsztf.js";import{f as C}from"./flags-for-api-options-iDm-C5Q6.js";import{E as De}from"./editor-page-3vTy0E3o.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./version-akiLXZts.js";import"./article-editor-5q-w3aPs.js";import"./article-renderer-AxicPl9z.js";import"./util-Q3BXsyUV.js";import"./perseus-error-l3K_anoI.js";import"./index-default-4_ZsnO94.js";import"./jquery-5v7aFUvu.js";import"./prop-types-ew9eayR0.js";import"./index-J2t_5nK1.js";import"./index-jmm5gWkb.js";import"./mobile-keypad-8hkmmP-T.js";import"./index-FsYHUvK_.js";import"./tabbar-tY-vWj59.js";import"./item-kdyp1JvI.js";import"./index-Cz55-Vre.js";import"./index-tvtfaFq4.js";import"./button-assets-W0P3gTHH.js";import"./keypad-button-dkA26ccQ.js";import"./operators-page-AaL31SSd.js";import"./navigation-pad-J3QJzD-6.js";import"./key-translator-NrLPOiYy.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-JrrbFnUG.js";import"./index-dnMhQZ-1.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-1N9MukZl.js";import"./index-zXbQRqKp.js";import"./dependencies-9B_Bv_mA.js";import"./fixed-to-responsive-Q2lYGnje.js";import"./constants-I_nlPaPx.js";import"./client-MU6fCXSs.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-A74C9PE_.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./perseus-api-cFDIds9P.js";import"./stub-tag-editor-HTvc7FsQ.js";import"./text-list-editor-A-LpoxgU.js";import"./lint-IvfTv29b.js";import"./info-tip-7k4XWhfR.js";import"./index-zE8cp1oq.js";import"./index-zHEZmPKZ.js";import"./index-skotlSua.js";import"./x-6ZxseNgc.js";import"./Popper-D86xJ3go.js";import"./i18n-context-DsDGD6dy.js";import"./strings-3jqcfOzr.js";import"./math-input-NQxFBc18.js";import"./index-wgZGcu4m.js";import"./input-with-examples-oId8yfSS.js";import"./text-input-Q1V0RBx1.js";import"./index-_rEjQfTM.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-vkx2pUmk.js";import"./simple-keypad-input-tVloz1vV.js";import"./base-radio-jjzrjL5j.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-qG_WG2X3.js";import"./icon-TA3bBVIW.js";import"./choice-icon-401hL_KJ.js";import"./focus-ring-dCiEd0Hc.js";import"./option-status-DOdQ-QjF.js";import"./choice-none-above-aC8Dh0vL.js";import"./video-transcript-link-WioFGlG-.js";import"./answer-choices-NDfgNgvH.js";import"./button-group-KR3umc1e.js";import"./graph-xQwCqgdo.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-ylPat90q.js";import"./hud-CJYktPgf.js";import"./multi-button-group-AxfQn4Cq.js";import"./number-input-qa6WcWGj.js";import"./prop-check-box-f7I2HerX.js";import"./range-input-8CEPNQ51.js";import"./marker-tCR8bInb.js";import"./answer-pill-kCSNcNzQ.js";import"./sortable-jy7O6trk.js";import"./multi-renderer-bBGToKbn.js";import"./hints-renderer-cyjup-_j.js";import"./components-ksQqDbqT.js";import"./device-framer-3oVOo47F.js";import"./constants-hQyjCzwz.js";import"./section-control-button-R8IAdsHL.js";import"./editor-h8jJLEGG.js";import"./tex-error-view-Vkpz9Wgd.js";import"./toggleable-caret-Ffhbha3L.js";import"./viewport-resizer-JKqSIi_b.js";import"./structured-item-diff-Z0HUzOPU.js";import"./text-diff-38SrUhTB.js";import"./tags-diff-yJY04aKr.js";import"./multi-items-Hda408jK.js";/* empty css                       */import"./interactive-graph-question-builder-ZWvM702j.js";import"./categorizer-editor-iiYQ-hXa.js";import"./editor-jsonify-NOjPB5zA.js";import"./blur-input-nr2CGk0J.js";import"./definition-editor-UQszsEJK.js";import"./dropdown-editor-KCd9hiM7.js";import"./explanation-editor-cY14Ba5e.js";import"./expression-editor-fZ8bHeSI.js";import"./graph-settings-EeSJWZ-r.js";import"./image-editor-uraD1O84.js";import"./input-number-editor-1rX7h6lv.js";import"./index-fQ2ygC-H.js";import"./interactive-graph-editor-a30cNnus.js";import"./interactive-graph-settings-ifv9go_f.js";import"./heading-m7-VnwH_.js";import"./locked-figures-section-kMIbUw-_.js";import"./util-WFrqcobO.js";import"./locked-ellipse-settings-njMHJzid.js";import"./locked-figure-settings-actions-8x3nUSLl.js";import"./color-select-VrLqwY4x.js";import"./color-swatch-ZODig9QN.js";import"./locked-line-settings-OIjY3ajX.js";import"./labeled-switch-cqMmCkIE.js";import"./line-swatch-4kZraVfZ.js";import"./locked-point-settings-FhU5rgey.js";import"./locked-polygon-settings-WSC0oWW0.js";import"./locked-vector-settings-EInQId2-.js";import"./label-image-editor-Vc9FrDro.js";import"./form-wrapped-text-field-0NplYznP.js";import"./answer-choices-tdiktbsr.js";import"./behavior-sBthuSRi.js";import"./question-markers-IlBdm-aT.js";import"./marker-LB0Pz-QX.js";import"./select-image-D9qOWNYR.js";import"./matcher-editor-mo7iTcKo.js";import"./number-line-editor-UZ4DfCK-.js";import"./numeric-input-editor-M6y41et7.js";import"./python-program-editor-4n9_AdBb.js";import"./editor-pWaTCM31.js";import"./sorter-editor-eRTmxaE0.js";import"./item-extras-editor-dZFaIW6h.js";xe();const Go={title:"PerseusEditor/Widgets/Interactive Graph"},Ne=Ie("onChange"),p=()=>e(r,{question:Ee}),c=()=>e(r,{question:Me}),d=()=>e(r,{question:qe}),u=()=>e(r,{question:_e}),g=()=>e(r,{question:Le}),h=()=>e(r,{question:Ge}),l=()=>e(r,{question:Qe}),f=()=>e(r,{question:Ae}),S=()=>e(r,{question:Fe}),i=()=>e(r,{apiOptions:{isMobile:!1,flags:{mafs:{...C.mafs,"interactive-graph-locked-features-m2":!1,"interactive-graph-locked-features-m2b":!1}}},question:W});i.parameters={chromatic:{disable:!0}};const a=()=>e(r,{apiOptions:{isMobile:!1,flags:{mafs:{...C.mafs,"interactive-graph-locked-features-m2":!0,"interactive-graph-locked-features-m2b":!1}}},question:W});a.parameters={chromatic:{disable:!0}};const s=()=>e(r,{question:W});s.parameters={chromatic:{disable:!0}};const n=()=>{const[ue,ge]=o.useState("phone"),[he,le]=o.useState(!1),[fe,Se]=o.useState(),[w,ve]=o.useState(W),[R,We]=o.useState(),[ye,Ce]=o.useState([]),v=o.useRef(null);return o.useEffect(()=>{if(v.current){const t=v.current.getSaveWarnings();Ce(t)}},[v,w,R]),P(b,{style:y.container,children:[e(De,{ref:v,apiOptions:{isMobile:!1,flags:C},previewDevice:ue,onPreviewDeviceChange:t=>ge(t),developerMode:!0,jsonMode:he,answerArea:fe,question:w,hints:R,frameSource:"about:blank",previewURL:"about:blank",itemId:"1",onChange:t=>{Ne(t),"jsonMode"in t&&le(t.jsonMode),"answerArea"in t&&Se(t.answerArea),"question"in t&&ve(t.question),"hints"in t&&We(t.hints)}}),P(b,{style:y.errorContainer,children:[e(Pe,{children:"Save Warnings:"}),e(Re,{size:m.small_12}),ye.map((t,we)=>e(be,{style:y.errorMessage,children:t},we))]})]})};n.parameters={chromatic:{disable:!0}};const y=ke.StyleSheet.create({container:{flexDirection:"row"},errorContainer:{border:`1px solid ${I.offBlack}`,padding:m.medium_16,flexGrow:1,marginRight:m.xLarge_32,height:"80vh",position:"sticky",top:m.xLarge_32,overflowY:"auto"},errorMessage:{color:I.red,marginBottom:m.small_12}});p.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphSegmentWithStartingCoords"};c.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphSegmentsWithStartingCoords"};d.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphLinearWithStartingCoords"};u.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphLinearSystemWithStartingCoords"};g.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphRayWithStartingCoords"};h.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphCircleWithStartingCoords"};l.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphQuadraticWithStartingCoords"};f.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphSinusoidWithStartingCoords"};S.__docgenInfo={description:"",methods:[],displayName:"InteractiveGraphPolygon"};i.__docgenInfo={description:"",methods:[],displayName:"MafsWithLockedFiguresCurrent"};a.__docgenInfo={description:"",methods:[],displayName:"MafsWithLockedFiguresM2Flag"};s.__docgenInfo={description:"",methods:[],displayName:"MafsWithLockedFiguresM2bFlag"};n.__docgenInfo={description:"",methods:[],displayName:"WithSaveWarnings"};var k,E,M;p.parameters={...p.parameters,docs:{...(k=p.parameters)==null?void 0:k.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={segmentWithStartingCoordsQuestion} />;
+}`,...(M=(E=p.parameters)==null?void 0:E.docs)==null?void 0:M.source}}};var q,_,L;c.parameters={...c.parameters,docs:{...(q=c.parameters)==null?void 0:q.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={segmentsWithStartingCoordsQuestion} />;
+}`,...(L=(_=c.parameters)==null?void 0:_.docs)==null?void 0:L.source}}};var G,Q,A;d.parameters={...d.parameters,docs:{...(G=d.parameters)==null?void 0:G.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={linearWithStartingCoordsQuestion} />;
+}`,...(A=(Q=d.parameters)==null?void 0:Q.docs)==null?void 0:A.source}}};var F,x,D;u.parameters={...u.parameters,docs:{...(F=u.parameters)==null?void 0:F.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={linearSystemWithStartingCoordsQuestion} />;
+}`,...(D=(x=u.parameters)==null?void 0:x.docs)==null?void 0:D.source}}};var N,j,O;g.parameters={...g.parameters,docs:{...(N=g.parameters)==null?void 0:N.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={rayWithStartingCoordsQuestion} />;
+}`,...(O=(j=g.parameters)==null?void 0:j.docs)==null?void 0:O.source}}};var B,V,H;h.parameters={...h.parameters,docs:{...(B=h.parameters)==null?void 0:B.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={circleWithStartingCoordsQuestion} />;
+}`,...(H=(V=h.parameters)==null?void 0:V.docs)==null?void 0:H.source}}};var J,z,T;l.parameters={...l.parameters,docs:{...(J=l.parameters)==null?void 0:J.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={quadraticWithStartingCoordsQuestion} />;
+}`,...(T=(z=l.parameters)==null?void 0:z.docs)==null?void 0:T.source}}};var U,Y,$;f.parameters={...f.parameters,docs:{...(U=f.parameters)==null?void 0:U.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={sinusoidWithStartingCoordsQuestion} />;
+}`,...($=(Y=f.parameters)==null?void 0:Y.docs)==null?void 0:$.source}}};var K,X,Z;S.parameters={...S.parameters,docs:{...(K=S.parameters)==null?void 0:K.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={polygonQuestion} />;
+}`,...(Z=(X=S.parameters)==null?void 0:X.docs)==null?void 0:Z.source}}};var ee,te,re;i.parameters={...i.parameters,docs:{...(ee=i.parameters)==null?void 0:ee.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview apiOptions={{
+    isMobile: false,
+    flags: {
+      mafs: {
+        ...flags.mafs,
+        "interactive-graph-locked-features-m2": false,
+        "interactive-graph-locked-features-m2b": false
+      }
+    }
+  }} question={segmentWithLockedFigures} />;
+}`,...(re=(te=i.parameters)==null?void 0:te.docs)==null?void 0:re.source}}};var oe,ie,ae;a.parameters={...a.parameters,docs:{...(oe=a.parameters)==null?void 0:oe.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview apiOptions={{
+    isMobile: false,
+    flags: {
+      mafs: {
+        ...flags.mafs,
+        "interactive-graph-locked-features-m2": true,
+        "interactive-graph-locked-features-m2b": false
+      }
+    }
+  }} question={segmentWithLockedFigures} />;
+}`,...(ae=(ie=a.parameters)==null?void 0:ie.docs)==null?void 0:ae.source}}};var se,ne,me;s.parameters={...s.parameters,docs:{...(se=s.parameters)==null?void 0:se.docs,source:{originalSource:`(): React.ReactElement => {
+  return <EditorPageWithStorybookPreview question={segmentWithLockedFigures} />;
+}`,...(me=(ne=s.parameters)==null?void 0:ne.docs)==null?void 0:me.source}}};var pe,ce,de;n.parameters={...n.parameters,docs:{...(pe=n.parameters)==null?void 0:pe.docs,source:{originalSource:`(): React.ReactElement => {
+  const [previewDevice, setPreviewDevice] = React.useState<DeviceType>("phone");
+  const [jsonMode, setJsonMode] = React.useState<boolean | undefined>(false);
+  const [answerArea, setAnswerArea] = React.useState<PerseusAnswerArea | undefined | null>();
+  const [question, setQuestion] = React.useState<PerseusRenderer | undefined>(segmentWithLockedFigures);
+  const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>();
+  const [saveWarnings, setSaveWarnings] = React.useState<string[]>([]);
+  const editorPageRef = React.useRef<EditorPage>(null);
+  React.useEffect(() => {
+    if (editorPageRef.current) {
+      const warnings = editorPageRef.current.getSaveWarnings();
+      setSaveWarnings(warnings);
+    }
+  }, [editorPageRef, question, hints]);
+  return <View style={styles.container}>
+            <EditorPage ref={editorPageRef} apiOptions={{
+      isMobile: false,
+      flags
+    }} previewDevice={previewDevice} onPreviewDeviceChange={newDevice => setPreviewDevice(newDevice)} developerMode={true} jsonMode={jsonMode} answerArea={answerArea} question={question} hints={hints} frameSource="about:blank" previewURL="about:blank" itemId="1" onChange={props => {
+      onChangeAction(props);
+      if ("jsonMode" in props) {
+        setJsonMode(props.jsonMode);
+      }
+      if ("answerArea" in props) {
+        setAnswerArea(props.answerArea);
+      }
+      if ("question" in props) {
+        setQuestion(props.question);
+      }
+      if ("hints" in props) {
+        setHints(props.hints);
+      }
+    }} />
+            <View style={styles.errorContainer}>
+                <LabelLarge>Save Warnings:</LabelLarge>
+                <Strut size={spacing.small_12} />
+                {saveWarnings.map((warning, index) => <Body key={index} style={styles.errorMessage}>
+                        {warning}
+                    </Body>)}
+            </View>
+        </View>;
+}`,...(de=(ce=n.parameters)==null?void 0:ce.docs)==null?void 0:de.source}}};const Qo=["InteractiveGraphSegmentWithStartingCoords","InteractiveGraphSegmentsWithStartingCoords","InteractiveGraphLinearWithStartingCoords","InteractiveGraphLinearSystemWithStartingCoords","InteractiveGraphRayWithStartingCoords","InteractiveGraphCircleWithStartingCoords","InteractiveGraphQuadraticWithStartingCoords","InteractiveGraphSinusoidWithStartingCoords","InteractiveGraphPolygon","MafsWithLockedFiguresCurrent","MafsWithLockedFiguresM2Flag","MafsWithLockedFiguresM2bFlag","WithSaveWarnings"];export{h as InteractiveGraphCircleWithStartingCoords,u as InteractiveGraphLinearSystemWithStartingCoords,d as InteractiveGraphLinearWithStartingCoords,S as InteractiveGraphPolygon,l as InteractiveGraphQuadraticWithStartingCoords,g as InteractiveGraphRayWithStartingCoords,p as InteractiveGraphSegmentWithStartingCoords,c as InteractiveGraphSegmentsWithStartingCoords,f as InteractiveGraphSinusoidWithStartingCoords,i as MafsWithLockedFiguresCurrent,a as MafsWithLockedFiguresM2Flag,s as MafsWithLockedFiguresM2bFlag,n as WithSaveWarnings,Qo as __namedExportsOrder,Go as default};
