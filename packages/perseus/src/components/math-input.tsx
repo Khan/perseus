40ff5b4f1ab66b51716fc3e34ffe44bb46af4f18
Cat Johnson <catjohnson@khanacademy.org>
2024-08-01 15:06:04 -0700

    Updating test implementation for Perseus!

--- packages/perseus/src/components/math-input.tsx
+++ packages/perseus/src/components/math-input.tsx
@@ -329,6 +329,7 @@ class InnerMathInput extends React.Component<InnerProps, State> {
                     <Popover
                         opened={this.state.keypadOpen}
                         onClose={() => this.closeKeypad()}
+                        rootBoundary="document"
                         dismissEnabled
                         content={() => (
                             <PopoverContentCore
