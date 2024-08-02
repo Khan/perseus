1d03ea23de439407ff97dcedbebf93d133addd12
Anna Mistele <aemandine@gmail.com>
2024-08-01 21:08:36 -0700

    Changed URL from string to URL type

--- packages/perseus/src/widgets/phet-sim.tsx
+++ packages/perseus/src/widgets/phet-sim.tsx
@@ -10,11 +10,8 @@ import * as React from "react";
 
 import {getDependencies} from "../dependencies";
 import * as Changeable from "../mixins/changeable";
-import Util from "../util";
 
 import type {WidgetExports} from "../types";
-
-const {updateQueryString} = Util;
 
 /* This renders the PhET sim */
 class PhetSim extends React.Component<any> {
@@ -25,6 +22,8 @@ class PhetSim extends React.Component<any> {
         url: PropTypes.string,
         description: PropTypes.string,
     };
+
+    private url: URL | null = null;
 
     constructor(props) {
         super(props);
@@ -40,7 +39,13 @@ class PhetSim extends React.Component<any> {
 
     async componentDidMount() {
         const {kaLocale} = getDependencies();
-        if (!(await this.checkForLocale(kaLocale))) {
+        this.url = new URL(this.props.url);
+        this.url.searchParams.set("locale", kaLocale);
+        if (this.url?.origin !== "https://phet.colorado.edu") {
+            // TODO(Anna): Do we want to report an error on this?
+            this.url = null;
+        }
+        if (await this.showLocaleWarning(kaLocale)) {
             this.setState({
                 errMessage:
                     "Sorry, this simulation isn't available in your language!",
@@ -53,9 +58,6 @@ class PhetSim extends React.Component<any> {
             width: String(this.props.width),
             height: String(this.props.height),
         } as const;
-
-        const {kaLocale} = getDependencies();
-
         // Add "px" to unitless numbers
         Object.entries(style).forEach(([key, value]: [any, any]) => {
             if (!value.endsWith("%") && !value.endsWith("px")) {
@@ -63,27 +65,11 @@ class PhetSim extends React.Component<any> {
             }
         });
 
-        let url: string = this.props.url;
-        url = updateQueryString(url, "locale", kaLocale);
-
-        // The URL needs to start with https://phet.colorado.edu/
-        // Do we want to allow users to paste in a relative path instead of
-        // just a full link?
-        if (
-            url &&
-            url.length &&
-            !url.startsWith("https://phet.colorado.edu/")
-        ) {
-            // TODO(Anna): Do we want to report an error on this?
-            url = "";
-        }
-
-        const sandboxProperties = "allow-same-origin allow-scripts";
-
         // We sandbox the iframe so that we allowlist only the functionality
         // that we need. This makes it a bit safer in case some content
         // creator "went wild".
         // http://www.html5rocks.com/en/tutorials/security/sandboxed-iframes/
+        const sandboxProperties = "allow-same-origin allow-scripts";
         return (
             <>
                 {this.state["errMessage"] && (
@@ -98,11 +84,13 @@ class PhetSim extends React.Component<any> {
                 )}
                 <iframe
                     ref={this.state["iframeRef"]}
+                    title={this.props.description}
                     sandbox={sandboxProperties}
                     style={style}
-                    src={url}
-                    srcDoc={url ? undefined : "Could not load PhET simulation."}
-                    title={this.props.description}
+                    src={this.url?.toString()}
+                    srcDoc={
+                        this.url ? undefined : "Could not load PhET simulation."
+                    }
                     allow="fullscreen"
                 />
             </>
@@ -119,25 +107,26 @@ class PhetSim extends React.Component<any> {
         return PhetSim.validate(this.getUserInput(), rubric);
     };
 
-    async checkForLocale(kaLocale: string): Promise<boolean> {
-        return fetch(this.props.url)
+    async showLocaleWarning(kaLocale: string): Promise<boolean> {
+        // If there is no actual simulation, we shouldn't show any locale warnings
+        if (!this.url) {
+            return false;
+        }
+        return fetch(this.url)
             .then((response: Response): Promise<string> => response.text())
             .then((html: string): boolean => {
-                try {
-                    // Find where window.phet.chipper.localeData is set.
-                    const localeDataVar: string =
-                        "window.phet.chipper.localeData = ";
-                    const startIndex = html.indexOf(localeDataVar);
-                    html = html.substring(startIndex + localeDataVar.length);
-                    const endIndex = html.indexOf(";");
-                    html = html.substring(0, endIndex);
-                    const locales: string[] = Object.keys(JSON.parse(html));
-                    return locales.includes(kaLocale);
-                } catch {
-                    // TODO(Anna): Feels like we should throw a different error if locale data
-                    // isn't accessible... But what do we return in that case?
-                    return false;
-                }
+                // Find where window.phet.chipper.localeData is set.
+                const localeDataVar: string =
+                    "window.phet.chipper.localeData = ";
+                const startIndex = html.indexOf(localeDataVar);
+                html = html.substring(startIndex + localeDataVar.length);
+                const endIndex = html.indexOf(";");
+                html = html.substring(0, endIndex);
+                const locales: string[] = Object.keys(JSON.parse(html));
+                return !locales.includes(kaLocale);
+            })
+            .catch((error: any) => {
+                return false;
             });
     }
 }
