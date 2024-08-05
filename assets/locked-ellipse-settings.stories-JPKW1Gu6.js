e5d5ec569427455f645f73c31205c7053d904520
nishasy <nishasy@gmail.com>
2024-08-05 19:39:29 +0000

    Deploying to gh-pages from @ Khan/perseus@a35be719fdf177ea3a028679d518c5da1535ee23 🚀

---
+++ assets/locked-ellipse-settings.stories-JPKW1Gu6.js
@@ -0,0 +1,26 @@
+import{j as d}from"./jsx-runtime-5BUNAZ9W.js";import{r as a}from"./index-4g5l5LRQ.js";import{L as e}from"./locked-ellipse-settings-p8QRYfP5.js";import{g as C}from"./util-qlCtRj9Y.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./article-renderer-dfZCEgJI.js";import"./util-KY8kJaBW.js";import"./version-akiLXZts.js";import"./jquery-yG1GhClm.js";import"./prop-types-ew9eayR0.js";import"./index-lUErx3pE.js";import"./index-J2t_5nK1.js";import"./index-awljIyHI.js";import"./index-jmm5gWkb.js";import"./mobile-keypad-8hkmmP-T.js";import"./index-FsYHUvK_.js";import"./index-e4P84RkC.js";import"./tabbar-tY-vWj59.js";import"./item-kdyp1JvI.js";import"./index-Cz55-Vre.js";import"./index-tvtfaFq4.js";import"./button-assets-W0P3gTHH.js";import"./keypad-button-dkA26ccQ.js";import"./operators-page-AaL31SSd.js";import"./navigation-pad-J3QJzD-6.js";import"./key-translator-NrLPOiYy.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-oy_Lc-D5.js";import"./index-dnMhQZ-1.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-UfCByHtE.js";import"./index-zXbQRqKp.js";import"./dependencies-9B_Bv_mA.js";import"./fixed-to-responsive-Q2lYGnje.js";import"./constants-I_nlPaPx.js";import"./client-MU6fCXSs.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-s-naDkf8.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./perseus-api-GqFoxb6z.js";import"./stub-tag-editor--1JgemZ6.js";import"./text-list-editor--7RKQsFY.js";import"./lint-IvfTv29b.js";import"./index-unb9_VrH.js";import"./index-zE8cp1oq.js";import"./index-zHEZmPKZ.js";import"./index-skotlSua.js";import"./index-QR_wlop5.js";import"./x-6ZxseNgc.js";import"./index-wjVcXLkf.js";import"./Popper-D86xJ3go.js";import"./i18n-context-DsDGD6dy.js";import"./strings-3jqcfOzr.js";import"./math-input-TH7UZ2rn.js";import"./index-wgZGcu4m.js";import"./input-with-examples-05qPHCmY.js";import"./text-input-Q1V0RBx1.js";import"./index-_rEjQfTM.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-vkx2pUmk.js";import"./simple-keypad-input-tVloz1vV.js";import"./base-radio-JGHr_AaJ.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-9W9dDTV4.js";import"./icon-TA3bBVIW.js";import"./choice-icon-401hL_KJ.js";import"./focus-ring-dCiEd0Hc.js";import"./option-status-DOdQ-QjF.js";import"./choice-none-above-pNDEyvJb.js";import"./video-transcript-link-WioFGlG-.js";import"./answer-choices-99eGZVAv.js";import"./button-group-KR3umc1e.js";import"./graph-VKYZ1zR1.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-ylPat90q.js";import"./hud-CJYktPgf.js";import"./multi-button-group-AxfQn4Cq.js";import"./number-input-9C90QKz-.js";import"./range-input--rysMEnL.js";import"./marker-u70BHlat.js";import"./answer-pill-B78kpmfJ.js";import"./sortable-_U62NJ9c.js";import"./multi-renderer-6SfhE39l.js";import"./hints-renderer-qD9-HtxR.js";import"./components-fJk0EzkW.js";import"./scrollless-number-text-field-A6-KLyao.js";import"./locked-figure-settings-actions-YLvABO1y.js";import"./color-select-cow5Kwgl.js";import"./color-swatch-RdiPka16.js";const co={title:"PerseusEditor/Components/Locked Ellipse Settings",component:e},r=s=>d(e,{...s}),c={...C("ellipse"),onChangeProps:()=>{},onMove:()=>{},onRemove:()=>{}};r.args=c;const o={render:function(){const[t,i]=a.useState(c);return d(e,{...t,onChangeProps:n=>{i({...t,...n})}})}};o.parameters={chromatic:{disableSnapshot:!0}};const p={render:function(){const[t,i]=a.useState(!0),[m,n]=a.useState(c);return d(e,{...m,expanded:t,onToggle:i,onChangeProps:R=>{n({...m,...R})}})}};r.__docgenInfo={description:"",methods:[],displayName:"Default"};var l,u,P;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`(args): React.ReactElement => {
+  return <LockedEllipseSettings {...args} />;
+}`,...(P=(u=r.parameters)==null?void 0:u.docs)==null?void 0:P.source}}};var g,f,h;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: function Render() {
+    const [props, setProps] = React.useState(defaultProps);
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedEllipseSettings {...props} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(h=(f=o.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var S,E,x;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: function Render() {
+    const [expanded, setExpanded] = React.useState(true);
+    const [props, setProps] = React.useState(defaultProps);
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedEllipseSettings {...props} expanded={expanded} onToggle={setExpanded} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(x=(E=p.parameters)==null?void 0:E.docs)==null?void 0:x.source}}};const lo=["Default","Controlled","Expanded"];export{o as Controlled,r as Default,p as Expanded,lo as __namedExportsOrder,co as default};
