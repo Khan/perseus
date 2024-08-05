76c29a3b3cf305d83d10b7b23addc884830d18e2
nishasy <nishasy@gmail.com>
2024-08-05 20:00:41 +0000

    Deploying to gh-pages from @ Khan/perseus@f920a4cc7f0e8e8072ee55598e8e13698d6dc2cf 🚀

---
+++ assets/register-all-widgets-for-testing-P0m6xSl9.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-5mtZQgsz.js";import{r,a as s}from"./renderer-oy_Lc-D5.js";const a=()=>{r(e),s()};export{a as r};
---
+++ assets/server-item-renderer.stories-xGlohfrZ.js
@@ -0,0 +1,145 @@
+import{j as t,a as s,F as K}from"./jsx-runtime-5BUNAZ9W.js";import{r as x}from"./index-4g5l5LRQ.js";import{S as i}from"./server-item-renderer-with-debug-ui-o4t5uiNZ.js";import{s as G}from"./test-dependencies-5ysmSGBn.js";import{I as Q}from"./article-renderer-5mtZQgsz.js";import{S as Y}from"./multi-renderer-cnQlWINP.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./choice-9W9dDTV4.js";import"./index-Cz55-Vre.js";import"./index-awljIyHI.js";import"./index-tvtfaFq4.js";import"./index-e4P84RkC.js";import"./index-lUErx3pE.js";import"./index-QR_wlop5.js";import"./index-zXbQRqKp.js";import"./index-skotlSua.js";import"./index-zE8cp1oq.js";import"./index-wjVcXLkf.js";import"./index-wgZGcu4m.js";import"./index-jmm5gWkb.js";import"./x-6ZxseNgc.js";import"./Popper-D86xJ3go.js";import"./index-dnMhQZ-1.js";import"./i18n-context-DsDGD6dy.js";import"./strings-3jqcfOzr.js";import"./icon-TA3bBVIW.js";import"./perseus-api-GqFoxb6z.js";import"./stub-tag-editor--1JgemZ6.js";import"./text-list-editor--7RKQsFY.js";import"./jquery-yG1GhClm.js";import"./media-queries-MaBBbpNq.js";import"./constants-I_nlPaPx.js";import"./choice-icon-401hL_KJ.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./focus-ring-dCiEd0Hc.js";import"./option-status-DOdQ-QjF.js";import"./util-KY8kJaBW.js";import"./version-akiLXZts.js";import"./dependencies-9B_Bv_mA.js";import"./hints-renderer-qD9-HtxR.js";import"./renderer-oy_Lc-D5.js";import"./index-J2t_5nK1.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-UfCByHtE.js";import"./fixed-to-responsive-Q2lYGnje.js";import"./client-MU6fCXSs.js";import"./image-loader-s-naDkf8.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./lint-IvfTv29b.js";import"./shared-3pf9YZIg.js";import"./base-radio-JGHr_AaJ.js";import"./choice-none-above-pNDEyvJb.js";import"./button-group-KR3umc1e.js";import"./graph-VKYZ1zR1.js";import"./hud-CJYktPgf.js";import"./index-unb9_VrH.js";import"./index-zHEZmPKZ.js";import"./index-FsYHUvK_.js";import"./math-input-TH7UZ2rn.js";import"./prop-types-ew9eayR0.js";import"./mobile-keypad-8hkmmP-T.js";import"./tabbar-tY-vWj59.js";import"./item-kdyp1JvI.js";import"./button-assets-W0P3gTHH.js";import"./keypad-button-dkA26ccQ.js";import"./operators-page-AaL31SSd.js";import"./navigation-pad-J3QJzD-6.js";import"./key-translator-NrLPOiYy.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./multi-button-group-AxfQn4Cq.js";import"./number-input-9C90QKz-.js";import"./range-input--rysMEnL.js";import"./text-input-Q1V0RBx1.js";import"./index-_rEjQfTM.js";import"./minus-bold-ONmDo3Ve.js";import"./ke-score-ui-ouvHLec5.js";import"./side-by-side-qr1qIzeQ.js";import"./register-all-widgets-for-testing-P0m6xSl9.js";import"./enums-x5qaTru7.js";import"./input-with-examples-05qPHCmY.js";import"./tooltip-vkx2pUmk.js";import"./simple-keypad-input-tVloz1vV.js";import"./video-transcript-link-WioFGlG-.js";import"./answer-choices-99eGZVAv.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-ylPat90q.js";import"./marker-u70BHlat.js";import"./answer-pill-B78kpmfJ.js";import"./sortable-_U62NJ9c.js";const X={question:{content:"Enter the number $$-42$$ in the box: [[☃ input-number 1]]",images:{},widgets:{"input-number 1":{type:"input-number",graded:!0,options:{answerType:"number",value:"-42",simplify:"required",size:"normal",inexact:!1,maxError:.1}}}},hints:[{content:"Hint #1",images:{},widgets:{}},{content:"Hint #2",images:{},widgets:{}},{content:"Hint #3",images:{},widgets:{}}],answerArea:null,_multi:null,itemDataVersion:{major:0,minor:0},answer:null},ee={question:{content:`Enter the number $$1$$ in box one: [[☃ input-number 1]] 
+
+ Enter the number $$2$$ in box two: [[☃ input-number 2]]`,images:{},widgets:{"input-number 1":{type:"input-number",graded:!0,options:{answerType:"number",value:"1",simplify:"required",size:"normal",inexact:!1,maxError:.1}},"input-number 2":{type:"input-number",graded:!0,options:{answerType:"number",value:"2",simplify:"required",size:"normal",inexact:!1,maxError:.1}}}},hints:[{content:"Hint #1",images:{},widgets:{}},{content:"Hint #2",images:{},widgets:{}},{content:"Hint #3",images:{},widgets:{}}],answerArea:null,_multi:null,itemDataVersion:{major:0,minor:0},answer:null},te={question:{content:`Here's a radio widget: [[☃ radio 1]] 
+
+ Here's an expression widget: [[☃ expression 1]]`,images:{},widgets:{"radio 1":{graded:!0,version:{major:0,minor:0},static:!1,numCorrect:1,hasNoneOfTheAbove:!1,multipleSelect:!1,countChoices:!1,deselectEnabled:!1,type:"radio",options:{static:!1,countChoices:!1,deselectEnabled:!1,displayCount:null,hasNoneOfTheAbove:!1,multipleSelect:!1,randomize:!0,choices:[{content:"Content 1",correct:!0,clue:"Clue 1"},{content:"Content 2",correct:!1,clue:"Clue 2"},{content:"Content 3",correct:!1,clue:"Clue 3"},{content:"Content 4",correct:!1,clue:"Clue 4"}]},alignment:"default"},"expression 1":{type:"expression",graded:!0,version:{major:1,minor:0},static:!1,options:{answerForms:[{considered:"correct",form:!0,simplify:!1,value:"x^2"},{considered:"wrong",form:!0,simplify:!1,value:"x^3"}],times:!0,buttonSets:["basic"],functions:["f","g","h"],buttonsVisible:"always",alignment:"default"}}}},hints:[{content:"Hint #1",images:{},widgets:{}},{content:"Hint #2",images:{},widgets:{}},{content:"Hint #3",images:{},widgets:{}}],answerArea:null,_multi:null,itemDataVersion:{major:0,minor:0},answer:null},ae={answerArea:Object.fromEntries(Q.map(a=>[a,!1])),_multi:null,answer:null,hints:[],itemDataVersion:{major:0,minor:1},question:{content:`Catherine created a chart and a bar graph to show how many dogs of each breed the animal shelter placed into good homes last year. 
+
+Dog breed  | Number of dogs 
+:- | :-: 
+Bulldog | $32$ 
+Greyhound | $72$ 
+Mastiff | $56$ 
+Collie | $40$ 
+
+**Label each bar on the bar graph.**
+
+[[☃ label-image 1]]
+
+
+
+`,images:{"web+graphie://ka-perseus-graphie.s3.amazonaws.com/2f60b533892fe6cb9351f704ffdada8ae4bc655c":{height:365,width:503},"web+graphie://ka-perseus-graphie.s3.amazonaws.com/75f937c6ea133d655dd9f698a58182efd6cc85d4":{height:293,width:331}},widgets:{"label-image 1":{alignment:"default",graded:!0,static:!1,type:"label-image",version:{major:0,minor:0},options:{choices:["Bulldog","Greyhound","Mastiff","Collie"],hideChoicesFromInstructions:!0,imageAlt:"A bar graph with four bar lines shows the horizontal axis labeled Dog breed and the vertical axis labeled Number of dogs. The vertical axis is labeled from the bottom of the axis to the top of the axis as follows: 0, 8, 16, 24, 32, 40, 48, 56, 64, 72, and 80. The horizontal axis has, from left to right, four unlabeled bar lines as follows: the first unlabeled bar line extends to 56, the second unlabeled bar line extends to 72, the third unlabeled bar line extends to 40, and the fourth unlabeled bar line extends to 32.",imageHeight:293,imageUrl:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/64ace45942546183c3dc842cb0e9e4f64a312727",imageWidth:331,markers:[{answers:["Bulldog"],label:"The fourth unlabeled bar line.",x:87.3,y:90.2},{answers:["Collie"],label:"The third unlabeled bar line.",x:67.7,y:90.5},{answers:["Greyhound"],label:"The second unlabeled bar line.",x:47.7,y:90.2},{answers:["Mastiff"],label:"The first unlabeled bar line.",x:27.8,y:90.5}],multipleAnswers:!1,static:!1}}}}},ne={question:{content:"# h1s aren't allowed",images:{},widgets:{}},hints:[],answerArea:null,_multi:null,itemDataVersion:{major:0,minor:0},answer:null},re={question:{content:`_An example of the different images and their alignments._
+
+All articles in the main content of Articles and Exercises should be **centered**.
+
+A small portrait image:
+[[☃ image 1]]
+
+A large portrait Jpeg image:
+[[☃ image 2]]
+
+A small landscape image:
+[[☃ image 3]]
+
+A large portrait image:
+[[☃ image 4]]
+
+And what follows are _hints_...
+`,images:{},widgets:{"image 1":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",backgroundImage:{url:"https://ka-perseus-graphie.s3.amazonaws.com/532e339d4d95f9cf6423e66bdc70dd06f1143a97.png",width:300,height:270},alt:"A right triangle word problem. How far does the parachute fall?",caption:"A right triangle word problem. How far does the parachute fall?"},version:{major:0,minor:0}},"image 2":{type:"image",alignment:"block",static:!1,graded:!0,options:{backgroundImage:{url:"https://ka-perseus-images.s3.amazonaws.com/12d7324ae8a09074f029d778bc4939aa85a6ee63.jpg",width:2317,height:2535},alt:"The patron, Enrico Scrovegni presenting the Arena Chapel to the Three Marys (detail), Giotto, Last Judgment, Arena (Scrovegni) Chapel, 1305-06, fresco, Padua (photo: Steven Zucker: CC BY-NC-SA 2.0)",caption:"The patron, Enrico Scrovegni presenting the Arena Chapel to the Three Marys (detail), Giotto, *Last Judgment*, Arena (Scrovegni) Chapel, 1305-06, fresco, Padua (photo: Steven Zucker: CC BY-NC-SA 2.0)"},version:{major:0,minor:0}},"image 3":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",range:[[0,10],[0,10]],box:[228,89],backgroundImage:{url:"https://ka-perseus-images.s3.amazonaws.com/d0f2df15737d3feeaeea4cf4ec640413c493a875.png",width:228,height:89},labels:[],alt:"A chemical diagram with O atom in the center connected on one side to an R atom by a single bond and on the other side to an H atom by a single bond.",caption:"A chemical diagram with O atom in the center connected on one side to an R atom by a single bond and on the other side to an H atom by a single bond."},version:{major:0,minor:0}},"image 4":{type:"image",alignment:"block",static:!1,graded:!0,options:{static:!1,title:"",backgroundImage:{url:"https://ka-perseus-images.s3.amazonaws.com/c983f96a29a07e5c3369a85f2c9b6ae9d2d07d2b.jpg",height:2503,width:3587},alt:"Milo dreaming he is riding on a bird's back to the moon.",caption:"Milo dreaming he is riding on a bird's back to the moon."},version:{major:0,minor:0}}}},hints:[{content:`A image widget with a portrait orientation:
+
+_should be left-aligned in hints_
+[[☃ image 1]]
+
+`,images:{},widgets:{"image 1":{type:"image",alignment:"block",static:!1,graded:!0,options:{backgroundImage:{url:"https://ka-perseus-images.s3.amazonaws.com/12d7324ae8a09074f029d778bc4939aa85a6ee63.jpg",width:2317,height:2535},alt:"The patron, Enrico Scrovegni presenting the Arena Chapel to the Three Marys (detail), Giotto, Last Judgment, Arena (Scrovegni) Chapel, 1305-06, fresco, Padua (photo: Steven Zucker: CC BY-NC-SA 2.0)",caption:"The patron, Enrico Scrovegni presenting the Arena Chapel to the Three Marys (detail), Giotto, *Last Judgment*, Arena (Scrovegni) Chapel, 1305-06, fresco, Padua (photo: Steven Zucker: CC BY-NC-SA 2.0)"},version:{major:0,minor:0}}}},{content:`A image widget with a portrait orientation:
+
+_should be left-aligned in hints_
+[[☃ image 1]]
+
+`,images:{},widgets:{"image 1":{type:"image",alignment:"block",static:!1,graded:!0,options:{backgroundImage:{url:"https://ka-perseus-images.s3.amazonaws.com/12d7324ae8a09074f029d778bc4939aa85a6ee63.jpg",width:2317,height:2535},alt:"The patron, Enrico Scrovegni presenting the Arena Chapel to the Three Marys (detail), Giotto, Last Judgment, Arena (Scrovegni) Chapel, 1305-06, fresco, Padua (photo: Steven Zucker: CC BY-NC-SA 2.0)",caption:"The patron, Enrico Scrovegni presenting the Arena Chapel to the Three Marys (detail), Giotto, *Last Judgment*, Arena (Scrovegni) Chapel, 1305-06, fresco, Padua (photo: Steven Zucker: CC BY-NC-SA 2.0)"},version:{major:0,minor:0}}}},{content:`A Portrait Markdown image:
+![The patron, Enrico Scrovegni presenting the Arena Chapel to the Three Marys (detail), Giotto, Last Judgment, Arena (Scrovegni) Chapel, 1305-06, fresco, Padua (photo: Steven Zucker: CC BY-NC-SA 2.0)](https://ka-perseus-images.s3.amazonaws.com/12d7324ae8a09074f029d778bc4939aa85a6ee63.jpg)
+
+`,images:{"https://ka-perseus-images.s3.amazonaws.com/12d7324ae8a09074f029d778bc4939aa85a6ee63.jpg":{width:2317,height:2535}},widgets:{}},{content:`A small Landscape Markdown image:
+
+![Some building](https://ka-perseus-images.s3.amazonaws.com/84c9521c5f9ec8c6d2a06435e48295817db54b65.jpeg)
+
+`,images:{"https://ka-perseus-images.s3.amazonaws.com/84c9521c5f9ec8c6d2a06435e48295817db54b65.jpeg":{width:450/2,height:288/2}},widgets:{}}],answerArea:null,_multi:null,itemDataVersion:{major:0,minor:0},answer:null},Vt={title:"Perseus/Renderers/Server Item Renderer"},m=a=>t(i,{item:X}),l=a=>t(i,{item:ae}),c=a=>t(i,{item:re}),d=a=>t(Y,{problemNum:0,item:ne,dependencies:G,linterContext:{contentType:"",highlightLint:!0,paths:[],stack:[]}}),p=a=>t(i,{item:ee,apiOptions:{interactionCallback:r=>{console.log(r)}}}),g=a=>t(i,{item:te,apiOptions:{interactionCallback:r=>{console.log(r)}}}),u=()=>{const[a,r]=x.useState(""),[h,Z]=x.useState(!0);function b(n){var I,v,y,S,C,k;const e=JSON.parse(n),w=((y=(v=(I=e==null?void 0:e.data)==null?void 0:I.assessmentItem)==null?void 0:v.item)==null?void 0:y.itemData)||((C=(S=e==null?void 0:e.assessmentItem)==null?void 0:S.item)==null?void 0:C.itemData)||((k=e==null?void 0:e.item)==null?void 0:k.itemData)||(e==null?void 0:e.itemData);if(w)return JSON.parse(w);if(e.question&&e.hints)return e}function F(){const n=b(a);r(JSON.stringify(n,null,2))}let o=null,f=!1;try{a&&(o=b(a))}catch(n){f=!0,console.error(n)}return s("div",{children:[s("div",{style:{padding:"2rem"},children:[s("label",{children:["Dump Perseus data here",t("textarea",{value:a,onChange:n=>r(n.target.value),style:{display:"block",width:"100%",height:"20rem"}})]}),o&&s("div",{children:[t("button",{onClick:F,children:"Extract item data"}),t("button",{onClick:()=>Z(!h),children:h?"Use ServerItemRenderer":"Use ServerItemRendererWithDebugUI"})]})]}),s("div",{style:{padding:"2rem"},children:[f&&t("p",{children:"There was a problem parsing the JSON"}),o&&t(K,{children:h?t(i,{item:o}):t(Y,{problemNum:0,item:o,dependencies:G,linterContext:{contentType:"",highlightLint:!0,paths:[],stack:[]}})})]})]})};m.__docgenInfo={description:"",methods:[],displayName:"InputNumberItem"};l.__docgenInfo={description:"",methods:[],displayName:"LabelImageItem"};c.__docgenInfo={description:"",methods:[],displayName:"ImageExamplesItem"};d.__docgenInfo={description:"",methods:[],displayName:"WithLintingError"};p.__docgenInfo={description:"",methods:[],displayName:"InputNumberWithInteractionCallback"};g.__docgenInfo={description:"",methods:[],displayName:"MultiWidgetWithInteractionCallback"};u.__docgenInfo={description:"",methods:[],displayName:"Interactive"};var A,D,E;m.parameters={...m.parameters,docs:{...(A=m.parameters)==null?void 0:A.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <ServerItemRendererWithDebugUI item={itemWithInput} />;
+}`,...(E=(D=m.parameters)==null?void 0:D.docs)==null?void 0:E.source}}};var N,W,R;l.parameters={...l.parameters,docs:{...(N=l.parameters)==null?void 0:N.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <ServerItemRendererWithDebugUI item={labelImageItem} />;
+}`,...(R=(W=l.parameters)==null?void 0:W.docs)==null?void 0:R.source}}};var T,_,j;c.parameters={...c.parameters,docs:{...(T=c.parameters)==null?void 0:T.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <ServerItemRendererWithDebugUI item={itemWithImages} />;
+}`,...(j=(_=c.parameters)==null?void 0:_.docs)==null?void 0:j.source}}};var L,$,z;d.parameters={...d.parameters,docs:{...(L=d.parameters)==null?void 0:L.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <ServerItemRenderer problemNum={0} item={itemWithLintingError} dependencies={storybookDependenciesV2} linterContext={{
+    contentType: "",
+    highlightLint: true,
+    paths: [],
+    stack: []
+  }} />;
+}`,...(z=($=d.parameters)==null?void 0:$.docs)==null?void 0:z.source}}};var M,O,H;p.parameters={...p.parameters,docs:{...(M=p.parameters)==null?void 0:M.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <ServerItemRendererWithDebugUI item={itemWithMultipleInputNumbers} apiOptions={{
+    interactionCallback: data => {
+      // We are logging the interaction callback data to the console
+      // eslint-disable-next-line no-console
+      console.log(data);
+    }
+  }} />;
+}`,...(H=(O=p.parameters)==null?void 0:O.docs)==null?void 0:H.source}}};var J,U,V;g.parameters={...g.parameters,docs:{...(J=g.parameters)==null?void 0:J.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <ServerItemRendererWithDebugUI item={itemWithRadioAndExpressionWidgets} apiOptions={{
+    interactionCallback: data => {
+      // We are logging the interaction callback data to the console
+      // eslint-disable-next-line no-console
+      console.log(data);
+    }
+  }} />;
+}`,...(V=(U=g.parameters)==null?void 0:U.docs)==null?void 0:V.source}}};var q,P,B;u.parameters={...u.parameters,docs:{...(q=u.parameters)==null?void 0:q.docs,source:{originalSource:`() => {
+  const [value, setValue] = useState("");
+  const [debug, setDebug] = useState(true);
+  function findItemData(input) {
+    const parsed = JSON.parse(input);
+    const itemData = parsed?.data?.assessmentItem?.item?.itemData || parsed?.assessmentItem?.item?.itemData || parsed?.item?.itemData || parsed?.itemData;
+    if (itemData) {
+      return JSON.parse(itemData);
+    } else if (parsed.question && parsed.hints) {
+      return parsed;
+    }
+  }
+  function extractItemData() {
+    const itemData = findItemData(value);
+    setValue(JSON.stringify(itemData, null, 2));
+  }
+  let content = null;
+  let parsingError = false;
+  try {
+    if (value) {
+      content = findItemData(value);
+    }
+  } catch (e) {
+    parsingError = true;
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
+  return <div>
+            <div style={{
+      padding: "2rem"
+    }}>
+                <label>
+                    Dump Perseus data here
+                    <textarea value={value} onChange={e => setValue(e.target.value)} style={{
+          display: "block",
+          width: "100%",
+          height: "20rem"
+        }} />
+                </label>
+                {content && <div>
+                        <button onClick={extractItemData}>
+                            Extract item data
+                        </button>
+                        <button onClick={() => setDebug(!debug)}>
+                            {debug ? "Use ServerItemRenderer" : "Use ServerItemRendererWithDebugUI"}
+                        </button>
+                    </div>}
+            </div>
+            <div style={{
+      padding: "2rem"
+    }}>
+                {parsingError && <p>There was a problem parsing the JSON</p>}
+                {content && <>
+                        {debug ? <ServerItemRendererWithDebugUI item={content} /> : <ServerItemRenderer problemNum={0} item={content} dependencies={storybookDependenciesV2} linterContext={{
+          contentType: "",
+          highlightLint: true,
+          paths: [],
+          stack: []
+        }} />}
+                    </>}
+            </div>
+        </div>;
+}`,...(B=(P=u.parameters)==null?void 0:P.docs)==null?void 0:B.source}}};const qt=["InputNumberItem","LabelImageItem","ImageExamplesItem","WithLintingError","InputNumberWithInteractionCallback","MultiWidgetWithInteractionCallback","Interactive"];export{c as ImageExamplesItem,m as InputNumberItem,p as InputNumberWithInteractionCallback,u as Interactive,l as LabelImageItem,g as MultiWidgetWithInteractionCallback,d as WithLintingError,qt as __namedExportsOrder,Vt as default};
