0f31984d0c6e368aca0a1d83d94b4ded906c3d28
jeremywiebe <jeremy.wiebe@gmail.com>
2024-08-06 21:39:25 +0000

    Deploying to gh-pages from @ Khan/perseus@de2883b3fdc7a6e294c39b33cd2989aed75cd969 🚀

--- assets/editor-jsonify-xKhBftBy.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./jquery-yG1GhClm.js";import{d as i}from"./article-renderer-K4Kq0STP.js";const s={serialize:function(){return o.omit(this.props,i)}};export{s as E};
---
+++ assets/image.stories-iu_Wh1EZ.js
@@ -0,0 +1,46 @@
+import{j as d}from"./jsx-runtime-5BUNAZ9W.js";import{R as c}from"./renderer-with-debug-ui-UHn3tKPY.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./choice-S5THRw8V.js";import"./index-Cz55-Vre.js";import"./index-awljIyHI.js";import"./index-tvtfaFq4.js";import"./index-e4P84RkC.js";import"./index-lUErx3pE.js";import"./index-QR_wlop5.js";import"./index-zXbQRqKp.js";import"./index-skotlSua.js";import"./index-zE8cp1oq.js";import"./index-wjVcXLkf.js";import"./index-GPnNv0mI.js";import"./index-jmm5gWkb.js";import"./x-6ZxseNgc.js";import"./Popper-D86xJ3go.js";import"./index-dnMhQZ-1.js";import"./i18n-context-SHL040F7.js";import"./strings-U4LenZoO.js";import"./icon-TA3bBVIW.js";import"./perseus-api-GqFoxb6z.js";import"./stub-tag-editor--1JgemZ6.js";import"./text-list-editor--7RKQsFY.js";import"./jquery-yG1GhClm.js";import"./media-queries-MaBBbpNq.js";import"./constants-I_nlPaPx.js";import"./choice-icon-Fa6RfAAR.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./focus-ring-dCiEd0Hc.js";import"./option-status-_NngQfjZ.js";import"./article-renderer-vWtlF3q2.js";import"./util-KY8kJaBW.js";import"./version-akiLXZts.js";import"./prop-types-ew9eayR0.js";import"./index-J2t_5nK1.js";import"./mobile-keypad-8hkmmP-T.js";import"./index-FsYHUvK_.js";import"./tabbar-tY-vWj59.js";import"./item-kdyp1JvI.js";import"./button-assets-W0P3gTHH.js";import"./keypad-button-dkA26ccQ.js";import"./operators-page-AaL31SSd.js";import"./navigation-pad-J3QJzD-6.js";import"./key-translator-NrLPOiYy.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-bN75HO46.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-UfCByHtE.js";import"./dependencies-9B_Bv_mA.js";import"./fixed-to-responsive-Q2lYGnje.js";import"./client-MU6fCXSs.js";import"./image-loader-s-naDkf8.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./lint-IvfTv29b.js";import"./index-unb9_VrH.js";import"./index-zHEZmPKZ.js";import"./math-input-WlXFmeVO.js";import"./input-with-examples-TCaUQG9B.js";import"./text-input-Q1V0RBx1.js";import"./index-_rEjQfTM.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-vkx2pUmk.js";import"./simple-keypad-input-tVloz1vV.js";import"./base-radio-oSiEycJG.js";import"./shared-3pf9YZIg.js";import"./choice-none-above-qe5ccICH.js";import"./video-transcript-link-jDAvsjOz.js";import"./answer-choices-ZWrmm1Sg.js";import"./button-group-KR3umc1e.js";import"./graph-VKYZ1zR1.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-ylPat90q.js";import"./hud-CJYktPgf.js";import"./multi-button-group-AxfQn4Cq.js";import"./number-input-_vmR2dDD.js";import"./range-input-W6BaCFKq.js";import"./marker-XyKMwKgn.js";import"./answer-pill-PjLcSVyV.js";import"./sortable-BJ0XVHDz.js";import"./side-by-side-qr1qIzeQ.js";import"./multi-renderer-l0GQx02I.js";import"./hints-renderer-_cfyGynV.js";import"./register-all-widgets-for-testing-SUTqSkYR.js";const t={content:`[[☃ image 1]]
+
+=====
+
+A quilter wants to make the design shown at left using the Golden Ratio. Specifically, he wants the ratio of the triangle heights $A:B$ and $B:C$ to each equal $1.62$. If the quilter makes the triangle height $A=8\\ \\text{in}$, approximately how tall should he make triangle height $C$?`,images:{"https://ka-perseus-graphie.s3.amazonaws.com/511d44e43c38d8c76812517e9c6f48e64ea42b20.png":{height:377,width:460}},widgets:{"image 1":{alignment:"block",graded:!0,options:{alt:"An array of isosceles triangles. A triangle has height A. Two smaller triangle, one with height B and one with height C, have approximately the same combined height as A.",title:"Image Title",caption:"Image Caption",backgroundImage:{height:345,url:"web+graphie://ka-perseus-graphie.s3.amazonaws.com/2ac8f769a7323f55e41c12cfa39e774be08bc138",width:420},box:[420,345],labels:[],range:[[0,10],[0,10]],static:!1},static:!1,type:"image",version:{major:0,minor:0}}}},e=i=>{const n={isMobile:i.isMobile},s=t.widgets["image 1"].options,r={...t,widgets:{...t.widgets,"image 1":{...t.widgets["image 1"],options:{...s,title:i.title,caption:i.caption}}}};return d(c,{question:r,apiOptions:n})},o=i=>{const n={isMobile:i.isMobile},s=t.widgets["image 1"].options,r={...t,widgets:{...t.widgets,"image 1":{...t.widgets["image 1"],options:{...s,alignment:"full-width",title:i.title,caption:"There is neither happiness nor unhappiness in this world; there is only the comparison of one state with another. Only a man who has felt ultimate despair is capable of feeling ultimate bliss. It is necessary to have wished for death in order to know how good it is to live.....the sum of all human wisdom will be contained in these two words: Wait and Hope"}}}};return d(c,{question:r,apiOptions:n})},Xt={title:"Perseus/Widgets/Image",args:{isMobile:!1,title:"",caption:""}};e.__docgenInfo={description:"",methods:[],displayName:"Question1",props:{isMobile:{required:!0,tsType:{name:"boolean"},description:""},title:{required:!0,tsType:{name:"string"},description:""},caption:{required:!0,tsType:{name:"string"},description:""}}};o.__docgenInfo={description:"",methods:[],displayName:"Question2",props:{isMobile:{required:!0,tsType:{name:"boolean"},description:""},title:{required:!0,tsType:{name:"string"},description:""},caption:{required:!0,tsType:{name:"string"},description:""}}};var a,p,m;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const apiOptions: APIOptions = {
+    isMobile: args.isMobile
+  };
+  const imageOptions = question.widgets["image 1"].options;
+  const questionWithArgs = ({
+    ...question,
+    widgets: {
+      ...question.widgets,
+      "image 1": {
+        ...question.widgets["image 1"],
+        options: {
+          ...imageOptions,
+          title: args.title,
+          caption: args.caption
+        }
+      }
+    }
+  } as const);
+  return <RendererWithDebugUI question={questionWithArgs} apiOptions={apiOptions} />;
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var g,l,h;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const apiOptions: APIOptions = {
+    isMobile: args.isMobile
+  };
+  const imageOptions = question.widgets["image 1"].options;
+  const questionWithCaptionAndArgs = ({
+    ...question,
+    widgets: {
+      ...question.widgets,
+      "image 1": {
+        ...question.widgets["image 1"],
+        options: {
+          ...imageOptions,
+          alignment: "full-width",
+          title: args.title,
+          caption: "There is neither happiness nor unhappiness in this world; there is only the comparison of one state with another. Only a man who has felt ultimate despair is capable of feeling ultimate bliss. It is necessary to have wished for death in order to know how good it is to live.....the sum of all human wisdom will be contained in these two words: Wait and Hope"
+        }
+      }
+    }
+  } as const);
+  return <RendererWithDebugUI question={questionWithCaptionAndArgs} apiOptions={apiOptions} />;
+}`,...(h=(l=o.parameters)==null?void 0:l.docs)==null?void 0:h.source}}};const Yt=["Question1","Question2"];export{e as Question1,o as Question2,Yt as __namedExportsOrder,Xt as default};
