ee27578f3be4b406f0308c09e33dfd7544a06cb1
Jeremy Wiebe <jeremy@khanacademy.org>
2024-08-07 16:52:14 -0700

    Remove Storybook-specific preview for EditorPage - real preview now works!

--- packages/perseus-editor/src/__stories__/editor-page-with-storybook-preview.tsx
+++ packages/perseus-editor/src/__stories__/editor-page-with-storybook-preview.tsx
@@ -1,24 +1,15 @@
 import {
-    Renderer,
     type APIOptions,
     type DeviceType,
     type Hint,
     type PerseusAnswerArea,
     type PerseusRenderer,
 } from "@khanacademy/perseus";
-import Button from "@khanacademy/wonder-blocks-button";
 import {View} from "@khanacademy/wonder-blocks-core";
-import IconButton from "@khanacademy/wonder-blocks-icon-button";
-import {Strut} from "@khanacademy/wonder-blocks-layout";
-import {color, spacing} from "@khanacademy/wonder-blocks-tokens";
-import {LabelLarge} from "@khanacademy/wonder-blocks-typography";
-import xIcon from "@phosphor-icons/core/regular/x.svg";
 import {action} from "@storybook/addon-actions";
-import {StyleSheet} from "aphrodite";
 import * as React from "react";
 
 // eslint-disable-next-line import/no-relative-packages
-import {mockStrings} from "../../../perseus/src/strings";
 import EditorPage from "../editor-page";
 
 import {flags} from "./flags-for-api-options";
@@ -44,8 +35,6 @@ function EditorPageWithStorybookPreview(props: Props) {
     const [hints, setHints] = React.useState<ReadonlyArray<Hint> | undefined>(
         props.hints,
     );
-
-    const [panelOpen, setPanelOpen] = React.useState<boolean>(true);
 
     const apiOptions = props.apiOptions ?? {
         isMobile: false,
@@ -85,85 +74,8 @@ function EditorPageWithStorybookPreview(props: Props) {
                     }
                 }}
             />
-
-            {/* Button to open panel */}
-            {!panelOpen && (
-                <Button
-                    onClick={() => setPanelOpen(!panelOpen)}
-                    style={styles.openPanelButton}
-                >
-                    Open preview (storybook only)
-                </Button>
-            )}
-
-            {/* Panel to show the question/hint previews */}
-            {panelOpen && (
-                <View style={styles.panel}>
-                    {/* Close button */}
-                    <IconButton
-                        icon={xIcon}
-                        onClick={() => setPanelOpen(!panelOpen)}
-                        style={styles.closeButton}
-                    />
-
-                    <View style={styles.panelInner}>
-                        {/* Question preview */}
-                        <Renderer
-                            strings={mockStrings}
-                            apiOptions={apiOptions}
-                            {...question}
-                        />
-                    </View>
-
-                    {/* Hints preview */}
-                    {hints?.map((hint, index) => (
-                        <View key={index} style={styles.panelInner}>
-                            <Strut size={spacing.medium_16} />
-                            <LabelLarge>{`Hint ${index + 1}`}</LabelLarge>
-                            <Renderer
-                                strings={mockStrings}
-                                apiOptions={apiOptions}
-                                hintMode={true}
-                                {...hint}
-                            />
-                        </View>
-                    ))}
-                </View>
-            )}
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    panel: {
-        position: "fixed",
-        right: 0,
-        height: "90vh",
-        overflow: "auto",
-        flex: "none",
-        backgroundColor: color.fadedBlue16,
-        padding: spacing.medium_16,
-        borderRadius: spacing.small_12,
-        alignItems: "end",
-    },
-    panelInner: {
-        flex: "none",
-        backgroundColor: color.white,
-        borderRadius: spacing.xSmall_8,
-        marginTop: spacing.medium_16,
-        width: "100%",
-        padding: spacing.xSmall_8,
-    },
-    closeButton: {
-        margin: 0,
-    },
-    openPanelButton: {
-        position: "fixed",
-        right: spacing.medium_16,
-        // Extra space so it doesn't get covered up by storybook's
-        // "Style warnings" button.
-        bottom: spacing.xxxLarge_64,
-    },
-});
 
 export default EditorPageWithStorybookPreview;
