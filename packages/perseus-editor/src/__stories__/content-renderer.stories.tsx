3d5fcf2109ad2f1d79f5d02c102d692ce2ba661d
Jeremy Wiebe <jeremy@khanacademy.org>
2024-08-07 17:23:15 -0700

    ContentRenderer: Extend linting story

--- packages/perseus-editor/src/__stories__/content-renderer.stories.tsx
+++ packages/perseus-editor/src/__stories__/content-renderer.stories.tsx
@@ -77,8 +77,27 @@ export const WithLintErrors: Story = {
 
 Here is some unclosed math: $1+1=3
 
-We should use \`\\dfrac{}\` instead of \`\\frac{}\`: $\\frac{3}{5}$`,
-            widgets: {},
+We should use \`\\dfrac{}\` instead of \`\\frac{}\`: $\\frac{3}{5}$
+
+What is the best color in the world?
+
+[[☃ radio 1]]`,
+            widgets: {
+                "radio 1": {
+                    type: "radio",
+                    options: {
+                        choices: [
+                            {content: "Red"},
+                            {content: "# Green"},
+                            {content: "Blue", correct: true},
+                            {
+                                content: "None of these!",
+                                isNoneOfTheAbove: true,
+                            },
+                        ],
+                    },
+                },
+            },
             images: {},
         },
     },
