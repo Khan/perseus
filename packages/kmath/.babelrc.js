164e700181678881186fa09ee81311c3721ff89a
Jeremy Wiebe <jeremy@khanacademy.org>
2024-08-09 11:13:50 -0700

    Remove .babelrc.js files in package dirs that are no longer needed (Jest is bugfixed)

--- packages/kmath/.babelrc.js
+++
@@ -1,8 +0,0 @@
-/**
- * HACK(somewhatabstract): Due to https://github.com/facebook/jest/issues/11741,
- * we need to have this file, or updating inline snapshots can fail rather
- * cryptically.
- *
- * We should remove this when jest is fixed.
- */
-module.exports = require("../../config/build/babel.config");
