c8047edf04ac71cc5dc5d9e8a979e3c5095ba09b
anakaren-rojas <anakaren@khanacademy.org>
2024-08-12 16:22:39 +0000

    Deploying to gh-pages from @ Khan/perseus@3f18a22118ad9b952009a78c30bb1c8242e81d9c 🚀

---
+++ assets/editor.stories-xmQvEc9M.js
@@ -0,0 +1,75 @@
+import{j as r}from"./jsx-runtime-5BUNAZ9W.js";import{V as R}from"./index-e4P84RkC.js";import{a as O}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{r as e}from"./index-4g5l5LRQ.js";import"./index-GRR5bG5k.js";import{S as C}from"./side-by-side-qr1qIzeQ.js";import{r as x}from"./register-all-widgets-and-editors-for-testing-s_G1UH26.js";import{a as u}from"./flags-for-api-options-J2YtfDjL.js";import{E as f}from"./editor-riGD61M5.js";import"./index-awljIyHI.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./version-akiLXZts.js";import"./article-editor-k7-UMhYQ.js";import"./article-renderer-Y-vUpWci.js";import"./util-KY8kJaBW.js";import"./jquery-yG1GhClm.js";import"./prop-types-t1BcTQrU.js";import"./index-lUErx3pE.js";import"./index-J2t_5nK1.js";import"./index-jmm5gWkb.js";import"./mobile-keypad-ALx0ZCBn.js";import"./index-FsYHUvK_.js";import"./tabbar-bBu8F0LR.js";import"./item-m8Nh1pVx.js";import"./index-Cz55-Vre.js";import"./index-tvtfaFq4.js";import"./button-assets-gSR_ngpH.js";import"./keypad-button-i3yjQmDW.js";import"./operators-page-AY25AXJ7.js";import"./navigation-pad-Z2fVjJac.js";import"./key-translator-Jx2gkvzi.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-99ueejEY.js";import"./index-dnMhQZ-1.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-UfCByHtE.js";import"./index-zXbQRqKp.js";import"./dependencies-9B_Bv_mA.js";import"./fixed-to-responsive-Q2lYGnje.js";import"./constants-I_nlPaPx.js";import"./client-MU6fCXSs.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-s-naDkf8.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./perseus-api-GqFoxb6z.js";import"./stub-tag-editor--1JgemZ6.js";import"./text-list-editor--7RKQsFY.js";import"./lint-IvfTv29b.js";import"./index-unb9_VrH.js";import"./index-zE8cp1oq.js";import"./index-zHEZmPKZ.js";import"./index-skotlSua.js";import"./index-QR_wlop5.js";import"./x-6ZxseNgc.js";import"./index-wjVcXLkf.js";import"./Popper-D86xJ3go.js";import"./i18n-context-SHL040F7.js";import"./strings-U4LenZoO.js";import"./math-input-OWrHDEJF.js";import"./index-GPnNv0mI.js";import"./input-with-examples-tBM0seUr.js";import"./text-input-Q1V0RBx1.js";import"./index-_rEjQfTM.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-vkx2pUmk.js";import"./simple-keypad-input-u5NVwwdg.js";import"./base-radio-KtCtV82W.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-S5THRw8V.js";import"./icon-TA3bBVIW.js";import"./choice-icon-Fa6RfAAR.js";import"./focus-ring-dCiEd0Hc.js";import"./option-status-_NngQfjZ.js";import"./choice-none-above-M75Bkp1a.js";import"./video-transcript-link-jDAvsjOz.js";import"./answer-choices-v1ar7P_1.js";import"./button-group-KR3umc1e.js";import"./graph-VKYZ1zR1.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-ylPat90q.js";import"./hud-CJYktPgf.js";import"./multi-button-group-AxfQn4Cq.js";import"./number-input-_vmR2dDD.js";import"./range-input-W6BaCFKq.js";import"./marker-JQle5UJy.js";import"./answer-pill-dr6O-Smt.js";import"./sortable-uVWUFvwS.js";import"./multi-renderer-E9nMBBzp.js";import"./hints-renderer-nzd-fpaT.js";import"./components-PouSBNA4.js";import"./device-framer-Z1UGUNph.js";import"./constants-hQyjCzwz.js";import"./section-control-button-AlXk6E_b.js";import"./viewport-resizer-pCaaAop5.js";import"./structured-item-diff-2eht-l43.js";import"./text-diff-lrJOl-S9.js";import"./tags-diff-aT_pVOlD.js";import"./editor-page-oceQOo-o.js";import"./item-extras-editor-D3MBgDhY.js";import"./multi-items-7gyx0Csj.js";/* empty css                       */import"./categorizer-editor-MiZq5MBI.js";import"./editor-jsonify-wfXZ_kZQ.js";import"./blur-input-nr2CGk0J.js";import"./definition-editor-V4PDUzSy.js";import"./dropdown-editor-Twv1AWmx.js";import"./explanation-editor-IdXuT8Ly.js";import"./expression-editor-0RkHFTlf.js";import"./graph-settings-a8uZsNa8.js";import"./image-editor-2Gtnm_kv.js";import"./input-number-editor-1myG4c0e.js";import"./index-R88rGBQp.js";import"./interactive-graph-editor-bX7C9rDx.js";import"./interactive-graph-settings-ab8wOEtp.js";import"./heading-m7-VnwH_.js";import"./toggleable-caret-Ffhbha3L.js";import"./locked-figures-section-HbM25RQy.js";import"./util-ISaCdl55.js";import"./locked-ellipse-settings-W3q31iMN.js";import"./scrollless-number-text-field-A6-KLyao.js";import"./coordinate-pair-input-SwzNCOo3.js";import"./locked-figure-settings-actions-knzh9UTc.js";import"./color-select-5cH1UF-0.js";import"./color-swatch-qhC6FnT5.js";import"./line-stroke-select-iqTUpkYn.js";import"./locked-function-settings-LhoqfaUh.js";import"./line-swatch-iMJVA1mv.js";import"./locked-line-settings-a8SzDzoR.js";import"./labeled-switch-FwaAuEJ9.js";import"./locked-point-settings-okGuOPB2.js";import"./locked-polygon-settings-M154j6GY.js";import"./locked-vector-settings-d3fLcLD1.js";import"./label-image-editor-7VZsdUYv.js";import"./form-wrapped-text-field-1itcayGc.js";import"./answer-choices-Wufe6-ju.js";import"./behavior-62Lq5Ut7.js";import"./question-markers-RayJYmhN.js";import"./marker-GkqB6Fxp.js";import"./select-image-j_ffz465.js";import"./matcher-editor-GFy49ii9.js";import"./number-line-editor-HpSvCVHI.js";import"./numeric-input-editor-CocwV_Ak.js";import"./python-program-editor-Jc2vBglZ.js";import"./editor-XDFHfD-G.js";import"./sorter-editor-gv_Z6t9R.js";import"./tex-error-view-Vkpz9Wgd.js";const p={content:`A sequence is defined recursively as follows:
+
+
+$\\qquad\\displaystyle{{a}_{n}}=-\\frac{1}{a_{n-1}-1} 
+~~~~~~\\text{ with}\\qquad\\displaystyle{{a}_{0}}=\\frac{1}{2}\\,$
+
+
+Find the term $a_3$ in the sequence.
+
+[[☃ input-number 1]]`,images:{},widgets:{"input-number 1":{graded:!0,version:{major:0,minor:0},static:!1,type:"input-number",options:{maxError:.1,inexact:!1,value:.5,simplify:"required",answerType:"number",size:"normal"},alignment:"default"}}};x();const Sr={title:"PerseusEditor/Editor"},i=()=>r(f,{apiOptions:u,content:p.content,placeholder:"",widgets:p.widgets,images:p.images,disabled:!1,widgetEnabled:!0,immutableWidgets:!1,showWordCount:!0,warnNoPrompt:!0,warnNoWidgets:!0,onChange:n=>{}}),o=()=>{const n=e.useRef(null),[h,w]=e.useState({}),[b,S]=e.useState("[[☃ interactive-graph 1]]"),[y,W]=e.useState({}),[v,E]=e.useState({"interactive-graph 1":{options:{labels:["x","y"],range:[[-10,10],[-10,10]],gridStep:[1,1],snapStep:[1,1],step:[1,1],backgroundImage:{url:null},markings:"graph",showProtractor:!1,showTooltips:!1,graph:{type:"linear",coords:[[1,1],[5,5]]},correct:{type:"linear"}},type:"interactive-graph",version:{major:0,minor:0}}});return r("div",{className:"framework-perseus",children:r(C,{leftTitle:"Editor",left:r(R,{style:{width:"360px",margin:"20px"},children:r(f,{ref:n,apiOptions:u,content:b,placeholder:"",widgets:v,images:y,disabled:!1,widgetEnabled:!0,immutableWidgets:!1,showWordCount:!0,warnNoPrompt:!1,warnNoWidgets:!0,onChange:t=>{O("onChange")(t),t.content?S(t.content):t.widgets?E(t.widgets):t.images&&W(t.images),setTimeout(()=>{var s;w(((s=n.current)==null?void 0:s.serialize())||{})},0)}})}),rightTitle:"Serialized Widget Options",jsonObject:h})})};i.__docgenInfo={description:"",methods:[],displayName:"Demo"};o.__docgenInfo={description:"",methods:[],displayName:"DemoInteractiveGraph"};var m,a,d;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`(): React.ReactElement => {
+  return <Editor apiOptions={apiOptionsWithDefaults} content={question1.content} placeholder="" widgets={question1.widgets} images={question1.images} disabled={false} widgetEnabled={true} immutableWidgets={false} showWordCount={true} warnNoPrompt={true} warnNoWidgets={true} onChange={props => {}} />;
+}`,...(d=(a=i.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};var l,c,g;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`(): React.ReactElement => {
+  const editorRef = React.useRef<Editor>(null);
+  const [options, setOptions] = React.useState({});
+  const [content, setContent] = React.useState("[[\\u2603 interactive-graph 1]]");
+  const [images, setImages] = React.useState<PerseusRenderer["images"]>({});
+  const [widgets, setWidgets] = React.useState<PerseusRenderer["widgets"]>({
+    "interactive-graph 1": {
+      options: {
+        labels: ["x", "y"],
+        range: [[-10, 10], [-10, 10]],
+        gridStep: [1, 1],
+        snapStep: [1, 1],
+        step: [1, 1],
+        backgroundImage: {
+          url: null
+        },
+        markings: "graph",
+        showProtractor: false,
+        showTooltips: false,
+        graph: {
+          type: "linear",
+          coords: [[1, 1], [5, 5]]
+        },
+        correct: {
+          type: "linear"
+        }
+      },
+      type: "interactive-graph",
+      version: {
+        major: 0,
+        minor: 0
+      }
+    }
+  });
+  return (
+    // Many of the editor components use scoped CSS that requires this
+    // class to be above it.
+    // TODO: Refactor to aphrodite styles instead of scoped CSS in Less.
+    <div className="framework-perseus">
+            <SideBySide leftTitle="Editor" left={<View style={{
+        width: "360px",
+        margin: "20px"
+      }}>
+                        <Editor ref={editorRef} apiOptions={apiOptionsWithDefaults} content={content} placeholder="" widgets={widgets} images={images} disabled={false} widgetEnabled={true} immutableWidgets={false} showWordCount={true} warnNoPrompt={false} warnNoWidgets={true} onChange={(props: Partial<PerseusRenderer>) => {
+          action("onChange")(props);
+          if (props.content) {
+            setContent(props.content);
+          } else if (props.widgets) {
+            setWidgets(props.widgets);
+          } else if (props.images) {
+            setImages(props.images);
+          }
+          // We need to wait for one tick so that the editor
+          // has been re-rendered with the changed props. If
+          // we don't wait, we get the values from the n-1
+          // render and miss the latest change.
+          setTimeout(() => {
+            setOptions(editorRef.current?.serialize() || {});
+          }, 0);
+        }} />
+                    </View>} rightTitle="Serialized Widget Options" jsonObject={options} />
+        </div>
+  );
+}`,...(g=(c=o.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const yr=["Demo","DemoInteractiveGraph"];export{i as Demo,o as DemoInteractiveGraph,yr as __namedExportsOrder,Sr as default};
---
+++ assets/keypad-mathquill.stories-1uRWvNnt.js
@@ -0,0 +1,59 @@
+import{a as k,j as n}from"./jsx-runtime-5BUNAZ9W.js";import{P as F,a as g}from"./index-GPnNv0mI.js";import{c as v}from"./index-lUErx3pE.js";import{r as o}from"./index-4g5l5LRQ.js";import{m as K}from"./button-assets-gSR_ngpH.js";import{C as b,K as M}from"./mobile-keypad-ALx0ZCBn.js";import{c as S,g as c,a as R}from"./key-translator-Jx2gkvzi.js";import"./index-jmm5gWkb.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-awljIyHI.js";import"./x-6ZxseNgc.js";import"./index-Cz55-Vre.js";import"./index-tvtfaFq4.js";import"./index-e4P84RkC.js";import"./Popper-D86xJ3go.js";import"./index-FsYHUvK_.js";import"./index-J2t_5nK1.js";import"./tabbar-bBu8F0LR.js";import"./item-m8Nh1pVx.js";import"./keypad-button-i3yjQmDW.js";import"./operators-page-AY25AXJ7.js";import"./navigation-pad-Z2fVjJac.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";const X={title:"math-input/components/v2 Keypad With Mathquill"};function a(){const i=o.useRef(null),[t,h]=o.useState(),[s,l]=o.useState(!0),[y,d]=o.useState(b.NONE);o.useEffect(()=>{if(!t&&i.current){const e=S(i.current,"en",K,r=>({...r,handlers:{edit:x=>{d(c(x))}}}));h(e)}},[t]);const f=R("en");function C(e){if(!t)return;e==="DISMISS"&&l(!1);const r=f[e];r?(r(t,e),d(c(t))):console.warn(`No translation to Mathquill for: ${e}`)}return k("div",{style:{maxWidth:"400px",margin:"2em"},children:[n(F,{content:n(g,{style:{padding:10,maxWidth:"initial"},children:n(M,{extraKeys:["x","y","PI","THETA"],onClickKey:C,cursorContext:y,advancedRelations:!0,basicRelations:!0,divisionKey:!0,logarithms:!0,convertDotToTimes:!0,preAlgebra:!0,trigonometry:!0,onAnalyticsEvent:async e=>{console.log("Send Event:",e)},showDismiss:!0})}),dismissEnabled:!0,opened:s,children:n("div",{style:{width:"100%",marginBottom:"1em",border:`1px solid ${v.offBlack16}`},ref:i})}),n("button",{onClick:()=>l(!s),children:s?"close keypad":"open keypad"})]})}a.__docgenInfo={description:"",methods:[],displayName:"V2KeypadWithMathquill"};var p,m,u;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`function V2KeypadWithMathquill() {
+  const mathFieldWrapperRef = React.useRef<HTMLDivElement>(null);
+  const [mathField, setMathField] = React.useState<MathFieldInterface>();
+  const [keypadOpen, setKeypadOpen] = React.useState<boolean>(true);
+  const [cursorContext, setCursorContext] = React.useState<(typeof CursorContext)[keyof typeof CursorContext]>(CursorContext.NONE);
+  React.useEffect(() => {
+    if (!mathField && mathFieldWrapperRef.current) {
+      const mathFieldInstance = createMathField(mathFieldWrapperRef.current, "en", mockStrings, baseConfig => ({
+        ...baseConfig,
+        handlers: {
+          edit: (_mathField: MathFieldInterface) => {
+            setCursorContext(getCursorContext(_mathField));
+          }
+        }
+      }));
+      setMathField(mathFieldInstance);
+    }
+  }, [mathField]);
+  const keyTranslator = getKeyTranslator("en");
+  function handleClickKey(key: Key) {
+    if (!mathField) {
+      return;
+    }
+    if (key === "DISMISS") {
+      setKeypadOpen(false);
+    }
+    const mathFieldCallback = keyTranslator[key];
+    if (mathFieldCallback) {
+      mathFieldCallback(mathField, key);
+      setCursorContext(getCursorContext(mathField));
+    } else {
+      // eslint-disable-next-line no-console
+      console.warn(\`No translation to Mathquill for: \${key}\`);
+    }
+  }
+  return <div style={{
+    maxWidth: "400px",
+    margin: "2em"
+  }}>
+            <Popover content={<PopoverContentCore style={{
+      padding: 10,
+      maxWidth: "initial"
+    }}>
+                        <Keypad extraKeys={["x", "y", "PI", "THETA"]} onClickKey={handleClickKey} cursorContext={cursorContext} advancedRelations basicRelations divisionKey logarithms convertDotToTimes preAlgebra trigonometry onAnalyticsEvent={async event => {
+        // eslint-disable-next-line no-console
+        console.log("Send Event:", event);
+      }} showDismiss />
+                    </PopoverContentCore>} dismissEnabled opened={keypadOpen}>
+                <div style={{
+        width: "100%",
+        marginBottom: "1em",
+        border: \`1px solid \${color.offBlack16}\`
+      }} ref={mathFieldWrapperRef} />
+            </Popover>
+            <button onClick={() => setKeypadOpen(!keypadOpen)}>
+                {keypadOpen ? "close keypad" : "open keypad"}
+            </button>
+        </div>;
+}`,...(u=(m=a.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};const Y=["V2KeypadWithMathquill"];export{a as V2KeypadWithMathquill,Y as __namedExportsOrder,X as default};
