a63e2aa97e419d13da053f5d17a5803b06267fb7
Ben Christel <benchristel@khanacademy.org>
2024-08-12 15:40:10 -0700

    Lint against static widgets in question stems

--- packages/perseus-linter/src/__tests__/rules.test.ts
+++ packages/perseus-linter/src/__tests__/rules.test.ts
@@ -31,11 +31,12 @@ import unbalancedCodeDelimitersRule from "../rules/unbalanced-code-delimiters";
 import unescapedDollarRule from "../rules/unescaped-dollar";
 import widgetInTableRule from "../rules/widget-in-table";
 import TreeTransformer from "../tree-transformer";
+import {staticWidgetInQuestionStem} from "../rules/static-widget-in-question-stem";
 
 type Rule = any;
 
 describe("Individual lint rules tests", () => {
-    function testRule(rule: Rule, markdown: string, context) {
+    function testRule(rule: Rule, markdown: string, context): {message: string}[] | null {
         const tree = PureMarkdown.parse(markdown);
         const tt = new TreeTransformer(tree);
         const warnings = [];
@@ -532,4 +533,79 @@ describe("Individual lint rules tests", () => {
         "This is definitely okay. Yeah.",
         "$a == 3.  125$",
     ]);
+
+    test("Rule static-widget-in-question-stem allows static widgets in hints", () => {
+        const problems = testRule(staticWidgetInQuestionStem, "[[☃ radio 1]]", {
+            contentType: "exercise",
+            stack: ["hint"],
+            widgets: {
+                "radio 1": {
+                    options: {
+                        static: true,
+                    },
+                },
+            },
+        })
+
+        expect(problems).toBe(null)
+    });
+
+    test("Rule static-widget-in-question-stem allows static widgets in articles", () => {
+        const problems = testRule(staticWidgetInQuestionStem, "[[☃ radio 1]]", {
+            contentType: "article",
+            stack: [],
+            widgets: {
+                "radio 1": {
+                    options: {
+                        static: true,
+                    },
+                },
+            },
+        })
+
+        expect(problems).toBe(null)
+    });
+
+    test("Rule static-widget-in-question-stem allows non-static widgets in question stems", () => {
+        const problems = testRule(staticWidgetInQuestionStem, "[[☃ radio 1]]", {
+            contentType: "exercise",
+            stack: [],
+            widgets: {
+                "radio 1": {
+                    options: {
+                        static: false,
+                    },
+                },
+            },
+        })
+
+        expect(problems).toBe(null)
+    });
+
+    test("Rule static-widget-in-question-stem tolerates widget with no definition", () => {
+        const problems = testRule(staticWidgetInQuestionStem, "[[☃ radio 1]]", {
+            contentType: "exercise",
+            stack: [],
+            widgets: {},
+        })
+
+        expect(problems).toBe(null)
+    });
+
+    test("Rule static-widget-in-question-stem allows warns about static widgets in question stems", () => {
+        const problems = testRule(staticWidgetInQuestionStem, "[[☃ radio 1]]", {
+            contentType: "exercise",
+            stack: [],
+            widgets: {
+                "radio 1": {
+                    options: {
+                        static: true,
+                    },
+                },
+            },
+        })
+
+        expect(problems?.length).toBe(1)
+        expect(problems?.[0]?.message).toBe("Widget in question stem is static (non-interactive).")
+    });
 });
---
+++ packages/perseus-linter/src/rules/static-widget-in-question-stem.ts
@@ -0,0 +1,25 @@
+import {Rule} from "@khanacademy/perseus-linter";
+
+export const staticWidgetInQuestionStem = Rule.makeRule({
+    name: "static-widget-in-question-stem",
+    severity: Rule.Severity.WARNING,
+    selector: "widget",
+    lint: (state, content, nodes, match, context) => {
+        if (context.contentType !== "exercise") {
+            return;
+        }
+
+        if (context.stack.includes("hint")) {
+            return;
+        }
+
+        const widget = context?.widgets?.[state.currentNode().id];
+        if (!widget) {
+            return;
+        }
+
+        if (widget.options.static) {
+            return `Widget in question stem is static (non-interactive).`
+        }
+    }
+})
