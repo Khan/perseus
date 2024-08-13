3e91042a2f37bda8f10b49fa9ff7995ea76499d2
handeyeco <matthew.c.tx@gmail.com>
2024-08-13 19:18:04 +0000

    Deploying to gh-pages from @ Khan/perseus@182c8f6600bbefa817c4553e3827498b8d425bbe 🚀

---
+++ assets/editor.stories-RAalWPcu.js
@@ -0,0 +1,75 @@
+import{j as r}from"./jsx-runtime-5BUNAZ9W.js";import{V as R}from"./index-e4P84RkC.js";import{a as O}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{r as e}from"./index-4g5l5LRQ.js";import"./index-DwY7jG48.js";import{S as C}from"./side-by-side-iJDU0aQd.js";import{r as x}from"./register-all-widgets-and-editors-for-testing-LjXPKOE4.js";import{a as u}from"./flags-for-api-options-IJJk3Krc.js";import{E as f}from"./editor-qSs3LEpp.js";import"./index-awljIyHI.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./version-akiLXZts.js";import"./article-editor-AgS42DYQ.js";import"./article-renderer-jdm2qII-.js";import"./util-KY8kJaBW.js";import"./jquery-yG1GhClm.js";import"./prop-types-t1BcTQrU.js";import"./index-lUErx3pE.js";import"./index-J2t_5nK1.js";import"./index-jmm5gWkb.js";import"./mobile-keypad-ALx0ZCBn.js";import"./index-FsYHUvK_.js";import"./tabbar-bBu8F0LR.js";import"./item-m8Nh1pVx.js";import"./index-Cz55-Vre.js";import"./index-tvtfaFq4.js";import"./button-assets-gSR_ngpH.js";import"./keypad-button-i3yjQmDW.js";import"./operators-page-AY25AXJ7.js";import"./navigation-pad-Z2fVjJac.js";import"./key-translator-Jx2gkvzi.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-99ueejEY.js";import"./index-dnMhQZ-1.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-UfCByHtE.js";import"./index-zXbQRqKp.js";import"./dependencies-9B_Bv_mA.js";import"./fixed-to-responsive-Q2lYGnje.js";import"./constants-I_nlPaPx.js";import"./client-MU6fCXSs.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-s-naDkf8.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./perseus-api-GqFoxb6z.js";import"./stub-tag-editor--1JgemZ6.js";import"./text-list-editor--7RKQsFY.js";import"./lint-IvfTv29b.js";import"./index-8nUh1h5D.js";import"./index-zE8cp1oq.js";import"./index-hihFCi2M.js";import"./index-skotlSua.js";import"./index-KZ9xT8RQ.js";import"./x-6ZxseNgc.js";import"./index-wjVcXLkf.js";import"./Popper-D86xJ3go.js";import"./i18n-context-SHL040F7.js";import"./strings-U4LenZoO.js";import"./math-input-6UW2fpyL.js";import"./index-GPnNv0mI.js";import"./input-with-examples-F8R76a_c.js";import"./text-input-wkUsDgzz.js";import"./index-YQYcT5qz.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-vkx2pUmk.js";import"./simple-keypad-input-u5NVwwdg.js";import"./base-radio-a-Wq1fCE.js";import"./media-queries-MaBBbpNq.js";import"./shared-3pf9YZIg.js";import"./choice-oTvvoS3K.js";import"./icon-TA3bBVIW.js";import"./choice-icon-Fa6RfAAR.js";import"./focus-ring-dCiEd0Hc.js";import"./option-status-_NngQfjZ.js";import"./choice-none-above-dxZyAPvK.js";import"./video-transcript-link-jDAvsjOz.js";import"./answer-choices-fwvZcHnZ.js";import"./button-group-KR3umc1e.js";import"./graph-VKYZ1zR1.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-ylPat90q.js";import"./hud-CJYktPgf.js";import"./multi-button-group-AxfQn4Cq.js";import"./number-input-_vmR2dDD.js";import"./range-input-W6BaCFKq.js";import"./marker-wE4QqXr_.js";import"./answer-pill-tk8Rsa9s.js";import"./sortable-uVWUFvwS.js";import"./multi-renderer-ixfBZDJG.js";import"./hints-renderer-nzd-fpaT.js";import"./components-H7GUB-j5.js";import"./device-framer-AUyaq8TI.js";import"./constants-hQyjCzwz.js";import"./section-control-button-6T-UbhJ1.js";import"./viewport-resizer-Z0pcu31_.js";import"./structured-item-diff-sygPUmXL.js";import"./text-diff-wyoKkxjk.js";import"./tags-diff-bxwDkm8u.js";import"./editor-page-192w9usI.js";import"./item-extras-editor-yw15_tCx.js";import"./multi-items-9NsdALfm.js";/* empty css                       */import"./categorizer-editor-BArNj1Qg.js";import"./editor-jsonify-HF14xJ4r.js";import"./blur-input-nr2CGk0J.js";import"./definition-editor-tcs-dUMs.js";import"./dropdown-editor-ihqqBnfH.js";import"./explanation-editor-TPL7d2xT.js";import"./expression-editor-Bo5QtCNR.js";import"./graph-settings-pA0s-AuQ.js";import"./image-editor-AlxbeEjI.js";import"./input-number-editor-b4Pa7q3H.js";import"./index-d92MFDEp.js";import"./interactive-graph-editor-sFZFX-zp.js";import"./interactive-graph-settings-Jo6pXysv.js";import"./heading-JjOPNpXt.js";import"./toggleable-caret-Ffhbha3L.js";import"./locked-figures-section-VnE7a0re.js";import"./util-iPH_KZgB.js";import"./locked-ellipse-settings-Ow7j8yiu.js";import"./scrollless-number-text-field-GDkKIt-C.js";import"./coordinate-pair-input-QWaIoUlf.js";import"./locked-figure-settings-actions-jM5CLNQG.js";import"./color-select-0oIlq_RF.js";import"./color-swatch-YTm3Ngbh.js";import"./line-stroke-select-R1g85yK-.js";import"./locked-function-settings-Jhn-PyEK.js";import"./line-swatch-9fMtvt-S.js";import"./locked-line-settings-q1DIsH-a.js";import"./labeled-switch-iFPLrrQE.js";import"./locked-point-settings-yWP8J4sc.js";import"./locked-polygon-settings-6it59GTH.js";import"./locked-vector-settings-QENw7mZC.js";import"./label-image-editor-S8sqg16z.js";import"./form-wrapped-text-field-Z56bK9IU.js";import"./answer-choices-zVIzpgh4.js";import"./behavior-Oihdp1Fr.js";import"./question-markers-eeUTFkPa.js";import"./marker-lBFyYlae.js";import"./select-image-78SaU5jC.js";import"./matcher-editor-QcuO7pIo.js";import"./number-line-editor-J-4COk1Y.js";import"./numeric-input-editor-aMpQdsbd.js";import"./python-program-editor-gW1Ic1UN.js";import"./editor-WybOf6GR.js";import"./sorter-editor-zM08SSd9.js";import"./tex-error-view-Vkpz9Wgd.js";const p={content:`A sequence is defined recursively as follows:
+
+
+$\\qquad\\displaystyle{{a}_{n}}=-\\frac{1}{a_{n-1}-1} 
+~~~~~~\\text{ with}\\qquad\\displaystyle{{a}_{0}}=\\frac{1}{2}\\,$
+
+
+Find the term $a_3$ in the sequence.
+
+[[☃ input-number 1]]`,images:{},widgets:{"input-number 1":{graded:!0,version:{major:0,minor:0},static:!1,type:"input-number",options:{maxError:.1,inexact:!1,value:.5,simplify:"required",answerType:"number",size:"normal"},alignment:"default"}}};x();const Sr={title:"PerseusEditor/Editor"},i=()=>r(f,{apiOptions:u,content:p.content,placeholder:"",widgets:p.widgets,images:p.images,disabled:!1,widgetEnabled:!0,immutableWidgets:!1,showWordCount:!0,warnNoPrompt:!0,warnNoWidgets:!0,onChange:n=>{}}),o=()=>{const n=e.useRef(null),[h,w]=e.useState({}),[b,S]=e.useState("[[☃ interactive-graph 1]]"),[y,W]=e.useState({}),[v,E]=e.useState({"interactive-graph 1":{options:{labels:["x","y"],range:[[-10,10],[-10,10]],gridStep:[1,1],snapStep:[1,1],step:[1,1],backgroundImage:{url:null},markings:"graph",showProtractor:!1,showTooltips:!1,graph:{type:"linear",coords:[[1,1],[5,5]]},correct:{type:"linear"}},type:"interactive-graph",version:{major:0,minor:0}}});return r("div",{className:"framework-perseus",children:r(C,{leftTitle:"Editor",left:r(R,{style:{width:"360px",margin:"20px"},children:r(f,{ref:n,apiOptions:u,content:b,placeholder:"",widgets:v,images:y,disabled:!1,widgetEnabled:!0,immutableWidgets:!1,showWordCount:!0,warnNoPrompt:!1,warnNoWidgets:!0,onChange:t=>{O("onChange")(t),t.content?S(t.content):t.widgets?E(t.widgets):t.images&&W(t.images),setTimeout(()=>{var s;w(((s=n.current)==null?void 0:s.serialize())||{})},0)}})}),rightTitle:"Serialized Widget Options",jsonObject:h})})};i.__docgenInfo={description:"",methods:[],displayName:"Demo"};o.__docgenInfo={description:"",methods:[],displayName:"DemoInteractiveGraph"};var m,a,d;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`(): React.ReactElement => {
+  return <Editor apiOptions={apiOptionsWithDefaults} content={question1.content} placeholder="" widgets={question1.widgets} images={question1.images} disabled={false} widgetEnabled={true} immutableWidgets={false} showWordCount={true} warnNoPrompt={true} warnNoWidgets={true} onChange={props => {}} />;
+}`,...(d=(a=i.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};var l,c,g;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`(): React.ReactElement => {
+  const editorRef = React.useRef<Editor>(null);
+  const [options, setOptions] = React.useState({});
+  const [content, setContent] = React.useState("[[\\u2603 interactive-graph 1]]");
+  const [images, setImages] = React.useState<PerseusRenderer["images"]>({});
+  const [widgets, setWidgets] = React.useState<PerseusRenderer["widgets"]>({
+    "interactive-graph 1": {
+      options: {
+        labels: ["x", "y"],
+        range: [[-10, 10], [-10, 10]],
+        gridStep: [1, 1],
+        snapStep: [1, 1],
+        step: [1, 1],
+        backgroundImage: {
+          url: null
+        },
+        markings: "graph",
+        showProtractor: false,
+        showTooltips: false,
+        graph: {
+          type: "linear",
+          coords: [[1, 1], [5, 5]]
+        },
+        correct: {
+          type: "linear"
+        }
+      },
+      type: "interactive-graph",
+      version: {
+        major: 0,
+        minor: 0
+      }
+    }
+  });
+  return (
+    // Many of the editor components use scoped CSS that requires this
+    // class to be above it.
+    // TODO: Refactor to aphrodite styles instead of scoped CSS in Less.
+    <div className="framework-perseus">
+            <SideBySide leftTitle="Editor" left={<View style={{
+        width: "360px",
+        margin: "20px"
+      }}>
+                        <Editor ref={editorRef} apiOptions={apiOptionsWithDefaults} content={content} placeholder="" widgets={widgets} images={images} disabled={false} widgetEnabled={true} immutableWidgets={false} showWordCount={true} warnNoPrompt={false} warnNoWidgets={true} onChange={(props: Partial<PerseusRenderer>) => {
+          action("onChange")(props);
+          if (props.content) {
+            setContent(props.content);
+          } else if (props.widgets) {
+            setWidgets(props.widgets);
+          } else if (props.images) {
+            setImages(props.images);
+          }
+          // We need to wait for one tick so that the editor
+          // has been re-rendered with the changed props. If
+          // we don't wait, we get the values from the n-1
+          // render and miss the latest change.
+          setTimeout(() => {
+            setOptions(editorRef.current?.serialize() || {});
+          }, 0);
+        }} />
+                    </View>} rightTitle="Serialized Widget Options" jsonObject={options} />
+        </div>
+  );
+}`,...(g=(c=o.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const yr=["Demo","DemoInteractiveGraph"];export{i as Demo,o as DemoInteractiveGraph,yr as __namedExportsOrder,Sr as default};
---
+++ assets/scrollless-number-text-field.stories-OeeRWqBa.js
@@ -0,0 +1,31 @@
+import{j as e,a as F,F as R}from"./jsx-runtime-5BUNAZ9W.js";import{V as c}from"./index-e4P84RkC.js";import{L as i}from"./index-KZ9xT8RQ.js";import{r as C}from"./index-4g5l5LRQ.js";import{S as n}from"./scrollless-number-text-field-GDkKIt-C.js";import"./index-awljIyHI.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-YQYcT5qz.js";import"./index-lUErx3pE.js";import"./minus-bold-ONmDo3Ve.js";const q={title:"PerseusEditor/Components/Scrollless Number Text Field",component:n},t=s=>e(n,{...s}),T={value:"",onChange:()=>{}};t.args=T;const r={render:function(){const[a,l]=C.useState("");return e(n,{value:a,onChange:l})}};r.parameters={chromatic:{disable:!0}};const o={render:function(){const[a,l]=C.useState("");return F(R,{children:[e(i,{children:"Scroll down to see the input."}),e(c,{style:{height:"100vh"}}),e(i,{children:"Observe that scrolling on the input field with a mouse wheel changes the number, but does not scroll the page."}),e(n,{value:a,onChange:l}),e(c,{style:{height:"100vh"}})]})}};o.parameters={chromatic:{disable:!0}};t.__docgenInfo={description:`Uncontrolled story. Interact with the control panel to see the component
+reflect the props.`,methods:[],displayName:"Default"};var u,d,p,h,m;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`(args): React.ReactElement => {
+  return <ScrolllessNumberTextField {...args} />;
+}`,...(p=(d=t.parameters)==null?void 0:d.docs)==null?void 0:p.source},description:{story:`Uncontrolled story. Interact with the control panel to see the component
+reflect the props.`,...(m=(h=t.parameters)==null?void 0:h.docs)==null?void 0:m.description}}};var g,b,f,S,w;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: function Render() {
+    const [value, setValue] = React.useState("");
+    return <ScrolllessNumberTextField value={value} onChange={setValue} />;
+  }
+}`,...(f=(b=r.parameters)==null?void 0:b.docs)==null?void 0:f.source},description:{story:`Controlled story. The text field's state is managed by its parent.
+Typing in the input field should work as expected.`,...(w=(S=r.parameters)==null?void 0:S.docs)==null?void 0:w.description}}};var v,x,y,L,V;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: function Render() {
+    const [value, setValue] = React.useState("");
+    return <>
+                <LabelLarge>Scroll down to see the input.</LabelLarge>
+                <View style={{
+        height: "100vh"
+      }} />
+                <LabelLarge>
+                    Observe that scrolling on the input field with a mouse wheel
+                    changes the number, but does not scroll the page.
+                </LabelLarge>
+                <ScrolllessNumberTextField value={value} onChange={setValue} />
+                <View style={{
+        height: "100vh"
+      }} />
+            </>;
+  }
+}`,...(y=(x=o.parameters)==null?void 0:x.docs)==null?void 0:y.source},description:{story:`In this example, we can see how the input field behaves when it is placed
+in a long page. Scrolling on the input field with a mouse wheel or trackpad
+changes the number, but does not scroll the page.`,...(V=(L=o.parameters)==null?void 0:L.docs)==null?void 0:V.description}}};const z=["Default","Controlled","LongPageScroll"];export{r as Controlled,t as Default,o as LongPageScroll,z as __namedExportsOrder,q as default};
