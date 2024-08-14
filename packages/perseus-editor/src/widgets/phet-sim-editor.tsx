ad11609ddb0a5f7480b8f5704eb5546ac3bd0fac
Anna Mistele <aemandine@gmail.com>
2024-08-14 15:54:09 -0700

    Make editor responsive to changes

--- packages/perseus-editor/src/widgets/phet-sim-editor.tsx
+++ packages/perseus-editor/src/widgets/phet-sim-editor.tsx
@@ -1,16 +1,17 @@
 /* eslint-disable @khanacademy/ts-no-error-suppressions */
 /* eslint-disable react/sort-comp */
-import {Changeable, EditorJsonify} from "@khanacademy/perseus";
-import {View} from "@khanacademy/wonder-blocks-core";
-import {LabeledTextField} from "@khanacademy/wonder-blocks-form";
-import {LabelMedium} from "@khanacademy/wonder-blocks-typography";
-import PropTypes from "prop-types";
+import {EditorJsonify} from "@khanacademy/perseus";
 import * as React from "react";
 
+import {LabeledTextField} from "@khanacademy/wonder-blocks-form";
+
 type Props = {
-    onChange: (args: any) => void;
     url: string;
     description: string;
+    onChange: (arg1: {
+        url?: Props["url"];
+        description?: Props["description"];
+    }) => void;
 };
 
 type DefaultProps = {
@@ -19,64 +20,29 @@ type DefaultProps = {
 };
 
 class PhetSimEditor extends React.Component<Props> {
-    static propTypes = {
-        onChange: PropTypes.func,
-        url: PropTypes.string,
-        description: PropTypes.string,
-    };
-/*
     static defaultProps: DefaultProps = {
         url: "",
         description: "",
     };
- */
+
     static widgetName = "phet-sim" as const;
 
-    change(...args) {
-        this.props.onChange(args);
-    }
-
     render(): React.ReactNode {
-        const sandboxProperties = "allow-same-origin allow-scripts";
-
         return (
             <div>
                 <LabeledTextField
-                    label="URL"
+                    label={"URL"}
                     value={this.props.url}
-                    onChange={(url: string) => this.change({url})}
+                    onChange={(url: string) => this.props.onChange({url})}
                 />
                 <br />
                 <LabeledTextField
-                    label="Description"
+                    label={"Description"}
                     value={this.props.description}
-                    onChange={(description: string) => this.change({description})}
+                    onChange={(description: string) =>
+                        this.props.onChange({description})
+                    }
                 />
-                <br />
-                <LabelMedium>
-                    Preview
-                    <View
-                        style={{
-                            position: "relative",
-                            width: "100%",
-                            paddingBottom: "100%",
-                            height: 0,
-                        }}
-                    >
-                        <iframe
-                            sandbox={sandboxProperties}
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                                position: "absolute",
-                                maxWidth: 400,
-                                maxHeight: 400,
-                            }}
-                            src={this.props.url}
-                            srcDoc={this.props.description}
-                        />
-                    </View>
-                </LabelMedium>
             </div>
         );
     }
