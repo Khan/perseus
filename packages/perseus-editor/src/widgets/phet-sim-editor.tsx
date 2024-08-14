b8d21630d01e1d2ca9edcd803477f60976e38818
Anna Mistele <aemandine@gmail.com>
2024-08-13 20:00:18 -0700

    Editor first draft

--- packages/perseus-editor/src/widgets/phet-sim-editor.tsx
+++ packages/perseus-editor/src/widgets/phet-sim-editor.tsx
@@ -1,39 +1,75 @@
 /* eslint-disable @khanacademy/ts-no-error-suppressions */
 /* eslint-disable react/sort-comp */
 import {Changeable, EditorJsonify} from "@khanacademy/perseus";
+import {View} from "@khanacademy/wonder-blocks-core";
+import {LabeledTextField} from "@khanacademy/wonder-blocks-form";
+import {LabelMedium} from "@khanacademy/wonder-blocks-typography";
+import PropTypes from "prop-types";
 import * as React from "react";
-
-import BlurInput from "../components/blur-input";
 
 type PhetSimEditorProps = any;
 
 class PhetSimEditor extends React.Component<PhetSimEditorProps> {
     static propTypes = {
         ...Changeable.propTypes,
+        url: PropTypes.string,
+        description: PropTypes.string,
     };
-
-    static widgetName = "phet-sim" as const;
 
     static defaultProps: PhetSimEditorProps = {
         url: "",
         description: "",
     };
 
-    change: (arg1: any, arg2: any) => any = (...args) => {
+    static widgetName = "phet-sim" as const;
+
+    change: (arg1: any) => any = (...args) => {
         return Changeable.change.apply(this, args);
     };
 
     render(): React.ReactNode {
+        const sandboxProperties = "allow-same-origin allow-scripts";
+
         return (
             <div>
-                <label>
-                    URL:
-                    <BlurInput
-                        value={this.props.url}
-                        // @ts-expect-error - TS2554 - Expected 2 arguments, but got 1.
-                        onChange={this.change("url")}
-                    />
-                </label>
+                <LabeledTextField
+                    label="URL"
+                    value={this.props.url}
+                    onChange={this.change("url")}
+                />
+                <br />
+                <LabeledTextField
+                    label="Description"
+                    value={this.props.description}
+                    onChange={(value) => {
+                        this.props.onChange({description: value});
+                    }}
+                />
+                <br />
+                <LabelMedium>
+                    Preview
+                    <View
+                        style={{
+                            position: "relative",
+                            width: "100%",
+                            paddingBottom: "100%",
+                            height: 0,
+                        }}
+                    >
+                        <iframe
+                            sandbox={sandboxProperties}
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                position: "absolute",
+                                maxWidth: 400,
+                                maxHeight: 400,
+                            }}
+                            src={this.props.url}
+                            srcDoc={this.props.description}
+                        />
+                    </View>
+                </LabelMedium>
             </div>
         );
     }
