d6d5c70c30c98694437ee6764410e07d51cf4505
Nisha Yerunkar <nisha@khanacademy.org>
2024-08-14 16:00:04 -0700

    [Interactive Graph Locked Figures] Add controls to move a whole locked polygon

--- packages/perseus-editor/src/components/graph-locked-figures/locked-polygon-settings.tsx
+++ packages/perseus-editor/src/components/graph-locked-figures/locked-polygon-settings.tsx
@@ -9,9 +9,13 @@ import Button from "@khanacademy/wonder-blocks-button";
 import {View} from "@khanacademy/wonder-blocks-core";
 import {OptionItem, SingleSelect} from "@khanacademy/wonder-blocks-dropdown";
 import IconButton from "@khanacademy/wonder-blocks-icon-button";
-import {Strut} from "@khanacademy/wonder-blocks-layout";
+import {Spring, Strut} from "@khanacademy/wonder-blocks-layout";
 import {spacing, color as wbColor} from "@khanacademy/wonder-blocks-tokens";
 import {LabelLarge, LabelMedium} from "@khanacademy/wonder-blocks-typography";
+import arrowFatDown from "@phosphor-icons/core/regular/arrow-fat-down.svg";
+import arrowFatLeft from "@phosphor-icons/core/regular/arrow-fat-left.svg";
+import arrowFatRight from "@phosphor-icons/core/regular/arrow-fat-right.svg";
+import arrowFatUp from "@phosphor-icons/core/regular/arrow-fat-up.svg";
 import minusCircle from "@phosphor-icons/core/regular/minus-circle.svg";
 import plusCircle from "@phosphor-icons/core/regular/plus-circle.svg";
 import {StyleSheet} from "aphrodite";
@@ -52,6 +56,31 @@ const LockedPolygonSettings = (props: Props) => {
 
     function handleColorChange(newValue: LockedFigureColor) {
         onChangeProps({color: newValue});
+    }
+
+    function handlePolygonMove(movement: "up" | "down" | "left" | "right") {
+        switch (movement) {
+            case "up":
+                onChangeProps({
+                    points: points.map(([x, y]) => [x, y + 1]),
+                });
+                break;
+            case "down":
+                onChangeProps({
+                    points: points.map(([x, y]) => [x, y - 1]),
+                });
+                break;
+            case "left":
+                onChangeProps({
+                    points: points.map(([x, y]) => [x - 1, y]),
+                });
+                break;
+            case "right":
+                onChangeProps({
+                    points: points.map(([x, y]) => [x + 1, y]),
+                });
+                break;
+        }
     }
 
     return (
@@ -172,17 +201,55 @@ const LockedPolygonSettings = (props: Props) => {
                         </View>
                     );
                 })}
-                <Button
-                    kind="tertiary"
-                    startIcon={plusCircle}
-                    onClick={() => {
-                        props.onChangeProps({
-                            points: [...points, [0, 0]],
-                        });
-                    }}
-                >
-                    Add point
-                </Button>
+                <View style={[styles.row, styles.polygonActionsContainer]}>
+                    <Button
+                        kind="tertiary"
+                        startIcon={plusCircle}
+                        onClick={() => {
+                            props.onChangeProps({
+                                points: [...points, [0, 0]],
+                            });
+                        }}
+                    >
+                        Add point
+                    </Button>
+
+                    <Spring />
+
+                    {/* Buttons to move the entire polygon */}
+                    <View style={styles.movementButtonsContainer}>
+                        <IconButton
+                            aria-label="Move polygon up"
+                            style={styles.iconButton}
+                            size="small"
+                            icon={arrowFatUp}
+                            onClick={() => handlePolygonMove("up")}
+                        />
+                        <View style={styles.row}>
+                            <IconButton
+                                aria-label="Move polygon left"
+                                style={styles.iconButton}
+                                size="small"
+                                icon={arrowFatLeft}
+                                onClick={() => handlePolygonMove("left")}
+                            />
+                            <IconButton
+                                aria-label="Move polygon down"
+                                style={styles.iconButton}
+                                size="small"
+                                icon={arrowFatDown}
+                                onClick={() => handlePolygonMove("down")}
+                            />
+                            <IconButton
+                                aria-label="Move polygon right"
+                                style={styles.iconButton}
+                                size="small"
+                                icon={arrowFatRight}
+                                onClick={() => handlePolygonMove("right")}
+                            />
+                        </View>
+                    </View>
+                </View>
             </PerseusEditorAccordion>
 
             {/* Actions */}
@@ -209,6 +276,18 @@ const styles = StyleSheet.create({
     },
     icon: {
         marginInlineStart: spacing.xxxSmall_4,
+    },
+    polygonActionsContainer: {
+        width: "100%",
+    },
+    iconButton: {
+        margin: 0,
+    },
+    movementButtonsContainer: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minWidth: "fit-content",
     },
     spaceUnder: {
         marginBottom: spacing.xSmall_8,
