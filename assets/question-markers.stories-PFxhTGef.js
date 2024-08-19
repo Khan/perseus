e7f8e6b762eee3ea7034ed10f6564c1a8ab80767
jeremywiebe <jeremy.wiebe@gmail.com>
2024-08-19 21:25:11 +0000

    Deploying to gh-pages from @ Khan/perseus@426a3ae1d5a7f0aef20ccea6b99ada6929e1abc4 🚀

---
+++ assets/question-markers.stories-PFxhTGef.js
@@ -0,0 +1,28 @@
+import{j as r}from"./jsx-runtime-5BUNAZ9W.js";import{l as p}from"./index-awljIyHI.js";import{r as W}from"./index-4g5l5LRQ.js";import{Q as b}from"./question-markers-BUpM_MHq.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./article-renderer-eZD1-vSC.js";import"./util-KY8kJaBW.js";import"./version-akiLXZts.js";import"./jquery-yG1GhClm.js";import"./prop-types-6Qz-9GtK.js";import"./mobile-keypad-cY_XnmKN.js";import"./index-jmm5gWkb.js";import"./index-FsYHUvK_.js";import"./index-J2t_5nK1.js";import"./index-e4P84RkC.js";import"./index-lUErx3pE.js";import"./tabbar-bBu8F0LR.js";import"./item-m8Nh1pVx.js";import"./index-Cz55-Vre.js";import"./index-tvtfaFq4.js";import"./button-assets-Z9EPvaqZ.js";import"./keypad-button--EiKtrcz.js";import"./operators-page-6_sBHI_n.js";import"./navigation-pad-Mri7Skbs.js";import"./key-translator-TewQj1fb.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-CDkxU16B.js";import"./index-dnMhQZ-1.js";import"./asset-context-4nzQV6k0.js";import"./svg-image-w4vAVvEs.js";import"./index-zXbQRqKp.js";import"./dependencies-9B_Bv_mA.js";import"./fixed-to-responsive-xwrcC5GD.js";import"./constants-CTNUT-ej.js";import"./client-MU6fCXSs.js";import"./inline-icon-QIU9thzn.js";import"./icon-paths-AuJwhOz7.js";import"./image-loader-s-naDkf8.js";import"./tex-Co-L2gRx.js";import"./zoomable-fMxvjK02.js";import"./zoomable-tex-ZKYDI9ak.js";import"./perseus-api-GqFoxb6z.js";import"./stub-tag-editor--1JgemZ6.js";import"./text-list-editor--7RKQsFY.js";import"./lint-nhuHdd9z.js";import"./index-z05-Lc8k.js";import"./index-skotlSua.js";import"./index-T3HXbpha.js";import"./Popper-bdbcdpLA.js";import"./index-zE8cp1oq.js";import"./index-wjVcXLkf.js";import"./i18n-context-SHL040F7.js";import"./strings-U4LenZoO.js";import"./math-input-8DeMRXLB.js";import"./index-WyPu8vtE.js";import"./input-with-examples-6953sV_1.js";import"./text-input-wkUsDgzz.js";import"./index-YQYcT5qz.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-vkx2pUmk.js";import"./simple-keypad-input-4VUVJbnD.js";import"./base-radio-Kq3rgYOa.js";import"./media-queries-cW_QgwNP.js";import"./shared-43ihVE4t.js";import"./choice-RL6irUBp.js";import"./icon-TA3bBVIW.js";import"./choice-icon-Uwdm3B_j.js";import"./focus-ring-z-znzPiA.js";import"./option-status-_NngQfjZ.js";import"./choice-none-above-xqvD6jJd.js";import"./video-transcript-link-jDAvsjOz.js";import"./answer-choices--Sssr-eZ.js";import"./button-group-KR3umc1e.js";import"./graph-pxFsqRWv.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-ylPat90q.js";import"./hud-McwHJ8wr.js";import"./index-Z7PPA4aZ.js";import"./multi-button-group-AxfQn4Cq.js";import"./number-input-_vmR2dDD.js";import"./range-input-W6BaCFKq.js";import"./marker-HNvhLreX.js";import"./answer-pill-WP7EFtov.js";import"./sortable-1fSPVr9M.js";import"./multi-renderer-khpmYV9b.js";import"./hints-renderer-ojm-2f9p.js";import"./marker-E1qHHgb3.js";import"./components-0ldmtYBm.js";import"./util-icZzwtXO.js";import"./form-wrapped-text-field-lOn8aufV.js";const mt={title:"PerseusEditor/Widgets/Label Image/Question Markers"},f=p.StyleSheet.create({wrapper:{width:338}}),k=t=>r("div",{className:p.css(f.wrapper),children:r(b,{...t})});class w extends W.Component{constructor(){super(...arguments),this.state={markers:[{answers:[],label:"",x:50,y:50}]}}render(){const{markers:m}=this.state;return r("div",{className:p.css(f.wrapper),children:r(b,{choices:["Lamborghini","BMW","Volkswagen","Fiat","Porsche","Ferrari"],imageUrl:"https://ka-perseus-images.s3.amazonaws.com/2ee5fc32e35c5178373b39fd304b325b2994c913.png",imageWidth:1280,imageHeight:1024,markers:m,onChange:y=>this.setState({markers:y})})})}}const o=t=>r(k,{...{choices:[],imageUrl:"",imageWidth:0,imageHeight:0,markers:[],onChange:()=>{}}}),e=t=>r(k,{...{choices:[],imageUrl:"https://ka-perseus-images.s3.amazonaws.com/2ee5fc32e35c5178373b39fd304b325b2994c913.png",imageWidth:1280,imageHeight:1024,markers:[{answers:[],label:"",x:50,y:50}],onChange:()=>{}}}),i=t=>r(w,{});o.__docgenInfo={description:"",methods:[],displayName:"Empty"};e.__docgenInfo={description:"",methods:[],displayName:"Filled"};i.__docgenInfo={description:"",methods:[],displayName:"Interactive"};var s,a,n;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const props = ({
+    choices: [],
+    imageUrl: "",
+    imageWidth: 0,
+    imageHeight: 0,
+    markers: [],
+    onChange: () => {}
+  } as const);
+  return <Wrapper {...props} />;
+}`,...(n=(a=o.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var c,g,d;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const props = ({
+    choices: [],
+    imageUrl: "https://ka-perseus-images.s3.amazonaws.com/2ee5fc32e35c5178373b39fd304b325b2994c913.png",
+    imageWidth: 1280,
+    imageHeight: 1024,
+    markers: [{
+      answers: [],
+      label: "",
+      x: 50,
+      y: 50
+    }],
+    onChange: () => {}
+  } as const);
+  return <Wrapper {...props} />;
+}`,...(d=(g=e.parameters)==null?void 0:g.docs)==null?void 0:d.source}}};var l,h,u;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  return <WithState />;
+}`,...(u=(h=i.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};const pt=["Empty","Filled","Interactive"];export{o as Empty,e as Filled,i as Interactive,pt as __namedExportsOrder,mt as default};
