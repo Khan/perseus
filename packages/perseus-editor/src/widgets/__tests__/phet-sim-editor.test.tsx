59687cb76052cdf76cc79ba334e02b3520872c48
Anna Mistele <aemandine@gmail.com>
2024-08-20 17:07:48 -0700

    Add content editor tests

---
+++ packages/perseus-editor/src/widgets/__tests__/phet-sim-editor.test.tsx
@@ -0,0 +1,55 @@
+import {Dependencies} from "@khanacademy/perseus";
+import {render, screen} from "@testing-library/react";
+import {userEvent as userEventLib} from "@testing-library/user-event";
+import * as React from "react";
+
+import {testDependencies} from "../../../../../testing/test-dependencies";
+import PhetSimEditor from "../phet-sim-editor";
+
+import type {UserEvent} from "@testing-library/user-event";
+
+describe("phet-sim editor", () => {
+    let userEvent: UserEvent;
+    beforeEach(() => {
+        userEvent = userEventLib.setup({
+            advanceTimers: jest.advanceTimersByTime,
+        });
+
+        jest.spyOn(Dependencies, "getDependencies").mockReturnValue(
+            testDependencies,
+        );
+    });
+
+    it("renders", async () => {
+        // Act
+        render(<PhetSimEditor onChange={() => {}} />);
+
+        // Assert
+        expect(screen.getByLabelText("URL")).toBeInTheDocument();
+        expect(screen.getByLabelText("Description")).toBeInTheDocument();
+    });
+
+    it("should be possible to change URL", async () => {
+        // Arrange
+        const onChangeMock = jest.fn();
+
+        // Act
+        render(<PhetSimEditor onChange={onChangeMock} />);
+        await userEvent.type(screen.getByLabelText("URL"), "h");
+
+        // Assert
+        expect(onChangeMock).toBeCalledWith({url: "h"});
+    });
+
+    it("should be possible to change Description", async () => {
+        // Arrange
+        const onChangeMock = jest.fn();
+
+        // Act
+        render(<PhetSimEditor onChange={onChangeMock} />);
+        await userEvent.type(screen.getByLabelText("Description"), "P");
+
+        // Assert
+        expect(onChangeMock).toBeCalledWith({description: "P"});
+    });
+});
