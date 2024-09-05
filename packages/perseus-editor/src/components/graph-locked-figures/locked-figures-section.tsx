024221799cbdbe5c2cd5e75ebce636d4ddf815b2
Ben Christel <benchristel@khanacademy.org>
2024-09-05 11:34:28 -0700

    Rename index.tsx to interactive-graph-editor.tsx

--- packages/perseus-editor/src/components/graph-locked-figures/locked-figures-section.tsx
+++ packages/perseus-editor/src/components/graph-locked-figures/locked-figures-section.tsx
@@ -17,7 +17,7 @@ import LockedFigureSelect from "./locked-figure-select";
 import LockedFigureSettings from "./locked-figure-settings";
 
 import type {LockedFigureSettingsMovementType} from "./locked-figure-settings-actions";
-import type {Props as InteractiveGraphEditorProps} from "../../widgets/interactive-graph-editor";
+import type {Props as InteractiveGraphEditorProps} from "../../widgets/interactive-graph-editor/interactive-graph-editor";
 import type {LockedFigure, LockedFigureType} from "@khanacademy/perseus";
 
 type Props = {
