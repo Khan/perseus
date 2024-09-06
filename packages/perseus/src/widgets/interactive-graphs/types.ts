1a8a1c1705b62afcf5ed6eb78892906196f44549
Nicole Watts <nicole@khanacademy.org>
2024-09-05 21:24:21 -0600

    wip: types

--- packages/perseus/src/widgets/interactive-graphs/types.ts
+++ packages/perseus/src/widgets/interactive-graphs/types.ts
@@ -54,7 +54,7 @@ export interface PointGraphState extends InteractiveGraphStateCommon {
     coords: Coord[];
     numPoints?: number | "unlimited";
     focusedPointIndex: number | null;
-    previouslyFocusedPoint: number | null;
+    previouslyFocusedPointIndex: number | null;
 }
 
 export interface RayGraphState extends InteractiveGraphStateCommon {
