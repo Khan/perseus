6e78909dea44a0f5ea9848114f21cc3a394db155
Kevin Barabash <kevinbarabash@khanacademy.org>
2024-09-06 17:19:06 -0400

    refactor to use a reducer

--- packages/perseus/src/widgets/show-your-work/show-your-work.tsx
+++ packages/perseus/src/widgets/show-your-work/show-your-work.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable no-console */
 import {View} from "@khanacademy/wonder-blocks-core";
-import {checkStep} from "@math-blocks/tutor";
 import {StyleSheet} from "aphrodite";
 import React from "react";
 
-import {parse} from "./parser";
+import {reducer} from "./practice-reducer";
 import {Step} from "./step";
 
 type Props = {
@@ -12,12 +11,10 @@ type Props = {
 };
 
 export const ShowYourWork = (props: Props) => {
-    const [steps, setSteps] = React.useState<Array<Step>>(() => {
-        return [
-            {value: props.question, status: "correct"},
-            {value: props.question, status: "waiting"},
-        ];
-    });
+    const [steps, dispatch] = React.useReducer(reducer, [
+        {value: props.question, status: "correct"},
+        {value: props.question, status: "waiting"},
+    ]);
 
     return (
         <View style={styles.contentWrapper}>
@@ -29,73 +26,24 @@ export const ShowYourWork = (props: Props) => {
                         steps[steps.length - 1].value ||
                         steps[steps.length - 1].status === "incorrect");
 
-                // TODO: if the current step is exactly the same as the previous step
-                // we need to disable the "Check" button
                 return (
                     <Step
                         key={i}
                         step={step}
                         onChange={(step) => {
                             if (isLast) {
-                                const newSteps = [...steps];
-                                newSteps[newSteps.length - 1] = {
-                                    value: step.value,
-                                    status: "waiting",
-                                };
-                                setSteps(newSteps);
+                                dispatch({kind: "Update", value: step.value});
                             }
                         }}
                         disableCheck={disableCheck}
                         onCheckStep={() => {
                             if (isLast) {
-                                // Replace the current step
-                                const newSteps = [...steps];
-                                newSteps[newSteps.length - 1].status =
-                                    "waiting";
-
-                                const prev = parse(
-                                    newSteps[newSteps.length - 2].value,
-                                );
-                                const next = parse(
-                                    newSteps[newSteps.length - 1].value,
-                                );
-
-                                const {result, mistakes: _} = checkStep(
-                                    prev,
-                                    next,
-                                );
-
-                                if (result) {
-                                    newSteps[newSteps.length - 1].status =
-                                        "correct";
-                                    newSteps.push({
-                                        value: newSteps[newSteps.length - 1]
-                                            .value,
-                                        status: "waiting",
-                                    });
-                                } else {
-                                    // It's possible for a student to make a correct step
-                                    // where we aren't able to find to from their previous
-                                    // step.
-                                    //
-                                    // In that case, we need to ask the solver for help to
-                                    // see if solving their next step results in the same
-                                    // solution as solving their previous step.
-                                    //
-                                    // If those results don't match, then the student has
-                                    // made a mistake.
-                                    //
-                                    // For now we're going to assume that this case is
-                                    // incorrect even though we need to do some more work
-                                    // verify that that is indeed the case.
-                                    newSteps[newSteps.length - 1].status =
-                                        "incorrect";
-                                }
-
-                                setSteps(newSteps);
+                                dispatch({kind: "Check"});
                             }
                         }}
-                        onDeleteStep={() => console.log(`onDeleteStep`)}
+                        onDeleteStep={() => {
+                            dispatch({kind: "Delete"});
+                        }}
                         isLast={isLast}
                     />
                 );
