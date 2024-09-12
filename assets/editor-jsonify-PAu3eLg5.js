9f6ff8c1ecf5b5ff35772c14a02779b90585cb4f
mark-fitzgerald <13896410+mark-fitzgerald@users.noreply.github.com>
2024-09-12 22:28:50 +0000

    Deploying to gh-pages from @ Khan/perseus@e533822aa7e54517b19ded83720c0fa706805041 🚀

---
+++ assets/editor-jsonify-PAu3eLg5.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./jquery-yG1GhClm.js";import{d as i}from"./article-renderer-y7y1K-jm.js";const s={serialize:function(){return o.omit(this.props,i)}};export{s as E};
---
+++ assets/register-all-widgets-for-testing-Jwfc0ju8.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-y7y1K-jm.js";import{r,a as s}from"./renderer-Bc3o6rku.js";const a=()=>{r(e),s()};export{a as r};
