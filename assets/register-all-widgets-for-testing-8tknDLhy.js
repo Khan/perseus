9ed07cb6b12fb830e29fe8cdc90ee7aa1cfbd951
benchristel <benchristel@users.noreply.github.com>
2024-09-16 20:45:24 +0000

    Deploying to gh-pages from @ Khan/perseus@ddc3f5d057da2d2c96ba92c5f5784c245ce6f573 🚀

---
+++ assets/register-all-widgets-for-testing-8tknDLhy.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-jdgtJ8wC.js";import{r,a as s}from"./renderer-mjINBdlP.js";const a=()=>{r(e),s()};export{a as r};
--- assets/register-all-widgets-for-testing-hdl3tg94.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-4FCn8tJf.js";import{r,a as s}from"./renderer-Bc3o6rku.js";const a=()=>{r(e),s()};export{a as r};
