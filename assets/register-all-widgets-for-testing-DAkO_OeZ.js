778430a8951236ad0bbe03e2ffb095a467ed645e
nishasy <nishasy@gmail.com>
2024-09-18 15:01:52 +0000

    Deploying to gh-pages from @ Khan/perseus@56166be340c7c408767884be975ea157052e93df 🚀

---
+++ assets/register-all-widgets-for-testing-DAkO_OeZ.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-8uzAHBVn.js";import{r,a as s}from"./renderer-vNHzJQJF.js";const a=()=>{r(e),s()};export{a as r};
--- assets/register-all-widgets-for-testing-I14QlJNk.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-zdAS4D4q.js";import{r,a as s}from"./renderer-Uzh2rw5e.js";const a=()=>{r(e),s()};export{a as r};
