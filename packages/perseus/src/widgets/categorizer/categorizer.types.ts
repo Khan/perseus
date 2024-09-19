f5ceabb7dbdfc984d08eed0ea55cf4202ca276e2
Tamara <60857422+Myranae@users.noreply.github.com>
2024-09-19 11:27:19 -0500

    Split out validation logic for categorizer (#1641)
    ## Summary:
    For the servier-side scoring project, we are moving validation logic out of each widget component. This moves the validation logic for the categorizer widget.
    
    Issue: LEMS-2341
    
    ## Test plan:
    - All tests should pass
    - Nothing should chang as code is just being moved into separate files
    
    Author: Myranae
    
    Reviewers: handeyeco, Myranae, jeremywiebe
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ❌ codecov/project, ✅ codecov/patch, ✅ Upload Coverage (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Jest Coverage (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/1641

---
+++ packages/perseus/src/widgets/categorizer/categorizer.types.ts
@@ -0,0 +1,7 @@
+import type {PerseusCategorizerWidgetOptions} from "../../perseus-types";
+
+export type UserInput = {
+    values: ReadonlyArray<number>;
+};
+
+export type Rubric = PerseusCategorizerWidgetOptions;
