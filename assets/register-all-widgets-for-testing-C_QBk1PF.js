002e5445cf3f025c59457c63438e99e220abd1d9
handeyeco <matthew.c.tx@gmail.com>
2024-09-19 14:50:10 +0000

    Deploying to gh-pages from @ Khan/perseus@64bcde0a1f98b4232e5347a06f4c8cad7265d006 🚀

---
+++ assets/register-all-widgets-for-testing-C_QBk1PF.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-ywHNW_uB.js";import{r,a as s}from"./renderer-r-xbMaHS.js";const a=()=>{r(e),s()};export{a as r};
--- assets/register-all-widgets-for-testing-sHvh7oPq.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-jYGMoPG8.js";import{r,a as s}from"./renderer-r-xbMaHS.js";const a=()=>{r(e),s()};export{a as r};
