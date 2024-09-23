84d0d4c26ab44b6e994b1a9beb652ca10ec8fd89
Tamara <tamara@khanacademy.org>
2024-09-23 10:55:21 -0500

    Update names and types

--- packages/perseus/src/widgets/sorter/sorter-validator.test.ts
+++ packages/perseus/src/widgets/sorter/sorter-validator.test.ts
@@ -1,20 +1,41 @@
 import sorterValidator from "./sorter-validator";
 
-describe('SorterValidator', () => {
+import type {Rubric, UserInput} from "./sorter.types";
+
+describe("SorterValidator", () => {
     it("gives points when the answer is correct", () => {
-        const state = {
-            options: {
-                padding: true,
-                layout: "horizontal",
-                correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
-            }, };
-        const rubric = { correct: ["a", "b", "c"] };
-        const result = sorterValidator(state, rubric);
+        const userInput: UserInput = {
+            values: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
+        };
+        const rubric: Rubric = {
+            padding: true,
+            layout: "horizontal",
+            correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
+        };
+        const result = sorterValidator(userInput, rubric);
         expect(result).toEqual({
             type: "points",
             earned: 1,
             total: 1,
             message: null,
         });
-    }
-}
+    });
+
+    it("does not give points when the answer is incorrect", () => {
+        const userInput: UserInput = {
+            values: ["$15$ grams", "$55$ grams", "$0.005$ kilograms"],
+        };
+        const rubric: Rubric = {
+            padding: true,
+            layout: "horizontal",
+            correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
+        };
+        const result = sorterValidator(userInput, rubric);
+        expect(result).toEqual({
+            type: "points",
+            earned: 0,
+            total: 1,
+            message: null,
+        });
+    });
+});
