cce97f800be536e188f1c01dbb5e43bb4bbbacad
Tamara <tamara@khanacademy.org>
2024-09-23 16:33:55 -0500

    Add invalid logic to validator, revert values to options

--- packages/perseus/src/widgets/sorter/sorter-validator.ts
+++ packages/perseus/src/widgets/sorter/sorter-validator.ts
@@ -4,8 +4,20 @@ import type {Rubric, UserInput} from "./sorter.types";
 import type {PerseusScore} from "../../types";
 
 function sorterValidator(userInput: UserInput, rubric: Rubric): PerseusScore {
-    const correct = Util.deepEq(userInput.values, rubric.correct);
+    // If the sorter widget hasn't been changed yet, we treat it as "empty" which
+    // prevents the "Check" button from becoming active. We want the user
+    // to make a change before trying to move forward. This makes an
+    // assumption that the initial order isn't the correct order! However,
+    // this should be rare if it happens, and interacting with the list
+    // will enable the button, so they won't be locked out of progressing.
+    if (!userInput.changed) {
+        return {
+            type: "invalid",
+            message: null,
+        };
+    }
 
+    const correct = Util.deepEq(userInput.options, rubric.correct);
     return {
         type: "points",
         earned: correct ? 1 : 0,
