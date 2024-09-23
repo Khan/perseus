d38bcce28c28678cfb939d04a09b49929215c5f2
Tamara <tamara@khanacademy.org>
2024-09-23 10:52:12 -0500

    Pull out validator types and add UserInput

---
+++ packages/perseus/src/widgets/sorter/sorter.types.ts
@@ -0,0 +1,7 @@
+import type {PerseusSorterWidgetOptions} from "../../perseus-types";
+
+export type Rubric = PerseusSorterWidgetOptions;
+
+export type UserInput = {
+    values: PerseusSorterWidgetOptions["correct"];
+};
