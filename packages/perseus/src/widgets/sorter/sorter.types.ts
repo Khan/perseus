b6d1831cd5cec7fd1223ca8143d440794eb6b424
Tamara <tamara@khanacademy.org>
2024-09-23 16:29:26 -0500

    Update UserInput type and revert values to options

--- packages/perseus/src/widgets/sorter/sorter.types.ts
+++ packages/perseus/src/widgets/sorter/sorter.types.ts
@@ -3,5 +3,6 @@ import type {PerseusSorterWidgetOptions} from "../../perseus-types";
 export type Rubric = PerseusSorterWidgetOptions;
 
 export type UserInput = {
-    values: PerseusSorterWidgetOptions["correct"];
+    options: PerseusSorterWidgetOptions["correct"];
+    changed: boolean;
 };
