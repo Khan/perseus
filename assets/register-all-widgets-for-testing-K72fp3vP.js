aadab4b6016d9f90cd678ca77440dd43ceac3f8f
nishasy <nishasy@gmail.com>
2024-09-24 17:49:39 +0000

    Deploying to gh-pages from @ Khan/perseus@20b3a2485e2ba8deea798acc2732d9570c0dac45 🚀

---
+++ assets/register-all-widgets-for-testing-K72fp3vP.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-1FyerbIv.js";import{r,a as s}from"./renderer-PaLhxIPv.js";const a=()=>{r(e),s()};export{a as r};
--- assets/register-all-widgets-for-testing-st9vv0j8.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-ifCq1gRW.js";import{r,a as s}from"./renderer-_6daufkV.js";const a=()=>{r(e),s()};export{a as r};
