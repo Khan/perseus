890bb76d0639237cf99ee245cb7af7c252e42758
Nisha Yerunkar <nisha@khanacademy.org>
2024-09-25 15:34:02 -0700

    more streamlining

--- packages/perseus/src/widgets/interactive-graphs/graph-locked-labels-layer.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graph-locked-labels-layer.tsx
@@ -45,14 +45,14 @@ export default function GraphLockedLabelsLayer(props: Props) {
                     {figure.type === "line" && (
                         <>
                             {/* Point 1 labels */}
-                            {(figure.points[0].labels ?? []).map((label, k) => (
+                            {figure.points[0].labels?.map((label, k) => (
                                 <LockedLabel
                                     key={`locked-figure-${i}-point-1-label-${k}`}
                                     {...label}
                                 />
                             ))}
                             {/* Point 2 labels */}
-                            {(figure.points[1].labels ?? []).map((label, k) => (
+                            {figure.points[1].labels?.map((label, k) => (
                                 <LockedLabel
                                     key={`locked-figure-${i}-point-2-label-${k}`}
                                     {...label}
