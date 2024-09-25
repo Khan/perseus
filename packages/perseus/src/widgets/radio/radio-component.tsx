3a10f6b1fe85d915fbf947434d7ebdc0b35607f5
Matthew <matthewcurtis@khanacademy.org>
2024-09-25 08:49:38 -0500

    Radio: split out validator (#1662)
    ## Summary:
    Split out the validator logic from Radio
    
    Issue: LEMS-2368
    
    ## Test plan:
    Nothing should change, just moving code.
    
    Author: handeyeco
    
    Reviewers: handeyeco, Myranae, jeremywiebe
    
    Required Reviewers:
    
    Approved By: Myranae
    
    Checks: ✅ Upload Coverage (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Jest Coverage (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/1662

--- packages/perseus/src/widgets/radio/radio-component.tsx
+++ packages/perseus/src/widgets/radio/radio-component.tsx
@@ -7,6 +7,7 @@ import Util from "../../util";
 import PassageRef from "../passage-ref/passage-ref";
 
 import BaseRadio from "./base-radio";
+import radioValidator from "./radio-validator";
 
 import type {FocusFunction, ChoiceType} from "./base-radio";
 import type {
@@ -77,56 +78,7 @@ class Radio extends React.Component<Props> {
         rubric: PerseusRadioRubric,
         strings: PerseusStrings,
     ): PerseusScore {
-        const numSelected = userInput.choicesSelected.reduce(
-            (sum, selected) => {
-                return sum + (selected ? 1 : 0);
-            },
-            0,
-        );
-
-        if (numSelected === 0) {
-            return {
-                type: "invalid",
-                message: null,
-            };
-        }
-
-        if (
-            userInput.numCorrect &&
-            userInput.numCorrect > 1 &&
-            numSelected !== userInput.numCorrect
-        ) {
-            return {
-                type: "invalid",
-                message: strings.chooseCorrectNum,
-            };
-            // If NOTA and some other answer are checked, ...
-        }
-        if (userInput.noneOfTheAboveSelected && numSelected > 1) {
-            return {
-                type: "invalid",
-                message: strings.notNoneOfTheAbove,
-            };
-        }
-
-        const correct = userInput.choicesSelected.every((selected, i) => {
-            let isCorrect;
-            if (userInput.noneOfTheAboveIndex === i) {
-                isCorrect = rubric.choices.every((choice, j) => {
-                    return i === j || !choice.correct;
-                });
-            } else {
-                isCorrect = !!rubric.choices[i].correct;
-            }
-            return isCorrect === selected;
-        });
-
-        return {
-            type: "points",
-            earned: correct ? 1 : 0,
-            total: 1,
-            message: null,
-        };
+        return radioValidator(userInput, rubric, strings);
     }
 
     static getUserInputFromProps(props: Props): PerseusRadioUserInput {
@@ -343,7 +295,7 @@ class Radio extends React.Component<Props> {
     }
 
     simpleValidate(rubric: PerseusRadioRubric): PerseusScore {
-        return Radio.validate(
+        return radioValidator(
             this.getUserInput(),
             rubric,
             this.context.strings,
