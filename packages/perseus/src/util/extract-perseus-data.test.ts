b56cfbe88a2059919ad0b6d93d6c1c5b6b2d0013
Nick <nickbreen@khanacademy.org>
2024-09-26 17:04:05 -0400

    Removes  describe("image") from test as its not necessary
    <full summary>
    
    Issue: <url or "none">
    
    Test plan:
    <see below>

--- packages/perseus/src/util/extract-perseus-data.test.ts
+++ packages/perseus/src/util/extract-perseus-data.test.ts
@@ -96,53 +96,51 @@ describe("injectWidgets", () => {
 });
 
 describe("getImagesWithoutAltData", () => {
-    describe("image", () => {
-        it("should return images with an img url that lack alt text", () => {
-            // Arrange
-            const widgets: {[key: `image ${number}`]: ImageWidget} = {
-                "image 1": {
-                    type: "image",
-                    options: {
-                        alt: "",
-                        backgroundImage: {
-                            url: "https://example.com/image1.jpg",
-                        },
+    it("should return images with an img url that lack alt text", () => {
+        // Arrange
+        const widgets: {[key: `image ${number}`]: ImageWidget} = {
+            "image 1": {
+                type: "image",
+                options: {
+                    alt: "",
+                    backgroundImage: {
+                        url: "https://example.com/image1.jpg",
                     },
                 },
-                "image 2": {
-                    type: "image",
-                    options: {
-                        alt: "Has alt data!",
-                        backgroundImage: {
-                            url: "https://example.com/image2.jpg",
-                        },
+            },
+            "image 2": {
+                type: "image",
+                options: {
+                    alt: "Has alt data!",
+                    backgroundImage: {
+                        url: "https://example.com/image2.jpg",
                     },
                 },
-                "image 3": {
-                    type: "image",
-                    options: {
-                        alt: "",
-                        backgroundImage: {
-                            url: "",
-                        },
+            },
+            "image 3": {
+                type: "image",
+                options: {
+                    alt: "",
+                    backgroundImage: {
+                        url: "",
                     },
                 },
-            };
-            const perseusRenderer: PerseusRenderer = {
-                content: "Content",
-                images: {},
-                widgets: widgets,
-            };
+            },
+        };
+        const perseusRenderer: PerseusRenderer = {
+            content: "Content",
+            images: {},
+            widgets: widgets,
+        };
 
-            // Act
-            const result = getImagesWithoutAltData(perseusRenderer);
+        // Act
+        const result = getImagesWithoutAltData(perseusRenderer);
 
-            // Assert
-            // Note: only Image 1 is returned in results because only that img lacks alt data
-            // and has an img url.
-            expect(result).toEqual(
-                '[{"widgetId":"image 1","imgUrl":"https://example.com/image1.jpg"}]',
-            );
-        });
+        // Assert
+        // Note: only Image 1 is returned in results because only that img lacks alt data
+        // and has an img url.
+        expect(result).toEqual(
+            '[{"widgetId":"image 1","imgUrl":"https://example.com/image1.jpg"}]',
+        );
     });
 });
