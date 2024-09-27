f5af2437133d68dcbaa42830850341c46d7affee
Ben Christel <benchristel@users.noreply.github.com>
2024-09-27 12:40:07 -0700

    Delete unused component param of InteractiveGraph.validate() (#1676)
    This parameter appeared to be used to score point graphs. It was passed to
    `getPointCoords()`. However, on closer inspection, there was no way this code
    could have worked: `getPointCoords` takes the interactive graph props as an
    argument, but we were passing it the entire component. This props param was
    only being used if the rubric contained no coords. Maybe that's a realistic
    case, or maybe it's not, but either way, it was throwing an error trying to
    access `props.range[0]`.
    
    Long story short, we lose nothing by deleting the `component` param and
    explicitly throwing an error if the rubric has no coords.
    
    Issue: LEMS-2448
    
    ## Test plan:
    
    Run this script to copy point graph data to your clipboard:
    
    ```
    grep -rl '"type":"point"' data/questions/ | xargs cat | pbcopy
    ```
    
    Paste it into `http://localhost:5173#flipbook`. Solve some point graph
    questions.
    
    Author: benchristel
    
    Reviewers: handeyeco, benchristel
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/1676

--- packages/perseus/src/widgets/interactive-graph.test.tsx
+++ packages/perseus/src/widgets/interactive-graph.test.tsx
@@ -1,5 +1,7 @@
 import invariant from "tiny-invariant";
 
+import {clone} from "../../../../testing/object-utils";
+
 import InteractiveGraph, {shouldUseMafs} from "./interactive-graph";
 
 import type {
@@ -28,7 +30,7 @@ describe("InteractiveGraph.validate on a segment question", () => {
             ],
         });
 
-        const result = InteractiveGraph.widget.validate(guess, rubric, null);
+        const result = InteractiveGraph.widget.validate(guess, rubric);
 
         expect(result).toHaveInvalidInput();
     });
@@ -53,7 +55,7 @@ describe("InteractiveGraph.validate on a segment question", () => {
             ],
         });
 
-        const result = InteractiveGraph.widget.validate(guess, rubric, null);
+        const result = InteractiveGraph.widget.validate(guess, rubric);
 
         expect(result).toHaveBeenAnsweredIncorrectly();
     });
@@ -78,7 +80,7 @@ describe("InteractiveGraph.validate on a segment question", () => {
             ],
         });
 
-        const result = InteractiveGraph.widget.validate(guess, rubric, null);
+        const result = InteractiveGraph.widget.validate(guess, rubric);
 
         expect(result).toHaveBeenAnsweredCorrectly();
     });
@@ -103,7 +105,7 @@ describe("InteractiveGraph.validate on a segment question", () => {
             ],
         });
 
-        const result = InteractiveGraph.widget.validate(guess, rubric, null);
+        const result = InteractiveGraph.widget.validate(guess, rubric);
 
         expect(result).toHaveBeenAnsweredCorrectly();
     });
@@ -128,7 +130,7 @@ describe("InteractiveGraph.validate on a segment question", () => {
             ],
         });
 
-        InteractiveGraph.widget.validate(guess, rubric, null);
+        InteractiveGraph.widget.validate(guess, rubric);
 
         expect(guess.coords).toEqual([
             [
@@ -158,7 +160,7 @@ describe("InteractiveGraph.validate on a segment question", () => {
             ],
         });
 
-        InteractiveGraph.widget.validate(guess, rubric, null);
+        InteractiveGraph.widget.validate(guess, rubric);
 
         // Narrow the type of `rubric.correct` to segment graph; otherwise TS
         // thinks it might not have a `coords` property.
@@ -169,6 +171,138 @@ describe("InteractiveGraph.validate on a segment question", () => {
                 [0, 0],
             ],
         ]);
+    });
+});
+
+describe("InteractiveGraph.validate on a point question", () => {
+    it("marks the answer invalid if guess.coords is missing", () => {
+        const guess: PerseusGraphType = {type: "point"};
+        const rubric: PerseusInteractiveGraphRubric = createRubric({
+            type: "point",
+            coords: [[0, 0]],
+        });
+
+        const result = InteractiveGraph.widget.validate(guess, rubric);
+
+        expect(result).toHaveInvalidInput();
+    });
+
+    it("throws an exception if correct.coords is missing", () => {
+        // Characterization test: this might not be desirable behavior, but
+        // it's the current behavior as of 2024-09-25.
+        const guess: PerseusGraphType = {
+            type: "point",
+            coords: [[0, 0]],
+        };
+        const rubric: PerseusInteractiveGraphRubric = createRubric({
+            type: "point",
+        });
+
+        expect(() =>
+            InteractiveGraph.widget.validate(guess, rubric),
+        ).toThrowError();
+    });
+
+    it("does not award points if guess.coords is wrong", () => {
+        const guess: PerseusGraphType = {
+            type: "point",
+            coords: [[9, 9]],
+        };
+        const rubric: PerseusInteractiveGraphRubric = createRubric({
+            type: "point",
+            coords: [[0, 0]],
+        });
+
+        const result = InteractiveGraph.widget.validate(guess, rubric);
+
+        expect(result).toHaveBeenAnsweredIncorrectly();
+    });
+
+    it("awards points if guess.coords is right", () => {
+        const guess: PerseusGraphType = {
+            type: "point",
+            coords: [[7, 8]],
+        };
+        const rubric: PerseusInteractiveGraphRubric = createRubric({
+            type: "point",
+            coords: [[7, 8]],
+        });
+
+        const result = InteractiveGraph.widget.validate(guess, rubric);
+
+        expect(result).toEqual({
+            type: "points",
+            earned: 1,
+            total: 1,
+            message: null,
+        });
+    });
+
+    it("allows points to be specified in any order", () => {
+        const guess: PerseusGraphType = {
+            type: "point",
+            coords: [
+                [7, 8],
+                [5, 6],
+            ],
+        };
+        const rubric: PerseusInteractiveGraphRubric = createRubric({
+            type: "point",
+            coords: [
+                [5, 6],
+                [7, 8],
+            ],
+        });
+
+        const result = InteractiveGraph.widget.validate(guess, rubric);
+
+        expect(result).toHaveBeenAnsweredCorrectly();
+    });
+
+    it("does not modify the `guess` data", () => {
+        const guess: PerseusGraphType = {
+            type: "point",
+            coords: [
+                [7, 8],
+                [5, 6],
+            ],
+        };
+        const rubric: PerseusInteractiveGraphRubric = createRubric({
+            type: "point",
+            coords: [
+                [5, 6],
+                [7, 8],
+            ],
+        });
+
+        const guessClone = clone(guess);
+
+        InteractiveGraph.widget.validate(guess, rubric);
+
+        expect(guess).toEqual(guessClone);
+    });
+
+    it("does not modify the `rubric` data", () => {
+        const guess: PerseusGraphType = {
+            type: "point",
+            coords: [
+                [7, 8],
+                [5, 6],
+            ],
+        };
+        const rubric: PerseusInteractiveGraphRubric = createRubric({
+            type: "point",
+            coords: [
+                [5, 6],
+                [7, 8],
+            ],
+        });
+
+        const rubricClone = clone(rubric);
+
+        InteractiveGraph.widget.validate(guess, rubric);
+
+        expect(rubric).toEqual(rubricClone);
     });
 });
 
