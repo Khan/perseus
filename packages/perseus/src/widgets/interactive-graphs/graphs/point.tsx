7f5178f14887952164922bd8b4c946b4c0e7f7b7
Nicole Watts <nicole@khanacademy.org>
2024-09-30 12:36:47 -0600

    maybe fixup

--- packages/perseus/src/widgets/interactive-graphs/graphs/point.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/point.tsx
@@ -52,7 +52,7 @@ export function UnlimitedPointGraph(props: PointGraphProps) {
         const focusedIndex = props.graphState.focusedPointIndex;
 
         if (focusedIndex !== null && itemsRef.current[focusedIndex] !== null) {
-            itemsRef.current[focusedIndex].focus();
+            itemsRef.current[focusedIndex]?.focus();
         }
     }, [props.graphState.focusedPointIndex, itemsRef]);
 
