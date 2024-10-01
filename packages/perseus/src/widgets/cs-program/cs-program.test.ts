8be3e303e86eb8f3fdb91864523361ceea2e8929
Tamara <tamara@khanacademy.org>
2024-10-01 13:05:05 -0500

    Add tests for getUserInput

--- packages/perseus/src/widgets/cs-program/cs-program.test.ts
+++ packages/perseus/src/widgets/cs-program/cs-program.test.ts
@@ -4,6 +4,8 @@ import {renderQuestion} from "../__testutils__/renderQuestion";
 
 import {question1} from "./cs-program.testdata";
 
+import type {PerseusCSProgramUserInput} from "../../validation.types";
+
 describe("cs-program widget", () => {
     beforeEach(() => {
         jest.spyOn(Dependencies, "getDependencies").mockReturnValue(
@@ -35,5 +37,36 @@ describe("cs-program widget", () => {
 
         // Assert
         expect(container).toMatchSnapshot("first mobile render");
+    });
+
+    it("should show default user input before user interaction", () => {
+        const apiOptions = {
+            isMobile: false,
+        } as const;
+
+        const {renderer} = renderQuestion(question1, apiOptions);
+        const userInput =
+            renderer.getUserInput()[0] as PerseusCSProgramUserInput;
+
+        expect(userInput.status).toBe("incomplete");
+        expect(userInput.message).toBe(null);
+    });
+
+    it("should snapshot default user input before user interaction", () => {
+        // Arrange
+        const {renderer} = renderQuestion(question1);
+
+        // Act
+        const userInput = renderer.getUserInput();
+
+        // Assert
+        expect(userInput).toMatchInlineSnapshot(`
+            [
+              {
+                "message": null,
+                "status": "incomplete",
+              },
+            ]
+        `);
     });
 });
