95650177a4c42797ecfcd0720c896242c66cfbd1
Tamara <tamara@khanacademy.org>
2024-10-01 16:49:22 -0500

    Add React-less tests for validator

--- packages/perseus/src/widgets/orderer/orderer-validator.test.ts
+++ packages/perseus/src/widgets/orderer/orderer-validator.test.ts
@@ -1,9 +1,75 @@
-import {act} from "@testing-library/react";
+import {ordererValidator} from "./orderer-validator";
+import {question1} from "./orderer.testdata";
 
-import {renderQuestion} from "../__testutils__/renderQuestion";
+import type {
+    PerseusOrdererRubric,
+    PerseusOrdererUserInput,
+} from "../../validation.types";
 
-import {question2} from "./orderer.testdata";
+describe("ordererValiator", () => {
+    it("is correct when the userInput is in the same order and is the same length as the rubric's correctOption content items", () => {
+        // Arrange
+        const rubric: PerseusOrdererRubric =
+            question1.widgets["orderer 1"].options;
 
-import type {APIOptions} from "@khanacademy/perseus";
+        const userInput: PerseusOrdererUserInput = {
+            current: question1.widgets["orderer 1"].options.correctOptions.map(
+                (option) => option.content,
+            ),
+        };
 
-describe("ordererValiator", () => {});
+        // Act
+        const result = ordererValidator(userInput, rubric);
+
+        // assert
+        expect(result).toHaveBeenAnsweredCorrectly();
+    });
+
+    it("is incorrect when the userInput is not in the same order as the rubric's correctOption content items", () => {
+        // Arrange
+        const rubric: PerseusOrdererRubric =
+            question1.widgets["orderer 1"].options;
+
+        const userInput: PerseusOrdererUserInput = {
+            current: ["$10.9$", "$11$", "$\\sqrt{120}$"],
+        };
+
+        // Act
+        const result = ordererValidator(userInput, rubric);
+
+        // assert
+        expect(result).toHaveBeenAnsweredIncorrectly();
+    });
+
+    it("is incorrect when the userInput is not the same length as the rubric's correctOption content items", () => {
+        // Arrange
+        const rubric: PerseusOrdererRubric =
+            question1.widgets["orderer 1"].options;
+
+        const userInput: PerseusOrdererUserInput = {
+            current: ["$10.9$", "$11$"],
+        };
+
+        // Act
+        const result = ordererValidator(userInput, rubric);
+
+        // assert
+        expect(result).toHaveBeenAnsweredIncorrectly();
+    });
+
+    it("is invalid when the when the user has not started ordering the options and current is empty", () => {
+        // Arrange
+        const rubric: PerseusOrdererRubric =
+            question1.widgets["orderer 1"].options;
+
+        const userInput: PerseusOrdererUserInput = {
+            current: [],
+        };
+
+        // Act
+        const result = ordererValidator(userInput, rubric);
+
+        // assert
+        expect(result).toHaveInvalidInput();
+    });
+});
