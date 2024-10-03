bfbc3fda69a8d356b0cf2340882d5cf7d8fe529b
Tamara <tamara@khanacademy.org>
2024-10-03 09:12:46 -0500

    Refactor
    Move random string creation to helper
    Helper to create the JSON string
    Returned object should prevent grading
    More logging

--- packages/perseus/src/util/parse-perseus-json.ts
+++ packages/perseus/src/util/parse-perseus-json.ts
@@ -10,35 +10,75 @@ import type {PerseusItem} from "../perseus-types";
  */
 
 export function parsePerseusItem(json: string): PerseusItem {
-    let randomString: string = "";
-    for (let i = 0; i < 5; i++) {
-        const randomLetter = String.fromCharCode(
-            97 + Math.floor(Math.random() * 26),
-        );
-        randomString += randomLetter;
-        // eslint-disable-next-line no-console
-        console.log("randomLetter", randomLetter);
-    }
-    const testJSON =
-        '{"data":{"assessmentItem":{"item":{"itemData":"{\\"foo\\":\\"bar\\"}"}}}}';
-    const parsedJSON = JSON.parse(testJSON);
-    const isNotCheating =
-        parsedJSON.data.assessmentItem.item.itemData ===
-        `{"${randomString}":"bar"}`;
-    // eslint-disable-next-line no-console
+    const randomString = buildRandomString();
+    console.log("randomString", randomString);
+    const testJSON = buildTestData(randomString);
     console.log("testJSON", testJSON);
-    // eslint-disable-next-line no-console
-    console.log("parsedJSON", parsedJSON);
+    const parsedJSON = JSON.parse(testJSON);
+    console.log("parsedJSON", JSON.stringify(parsedJSON));
+    console.log(
+        "need",
+        parsedJSON.data.assessmentItem.item.itemData.question.content,
+    );
+    console.log('`{"${randomString}:bar"}`', `{"${randomString}:bar"}`);
+    const isNotCheating =
+        parsedJSON.data.assessmentItem.item.itemData.question.content ===
+        `${randomString}:bar`;
+    // // eslint-disable-next-line no-console
+    // console.log("testJSON", testJSON);
+    // // eslint-disable-next-line no-console
+    // console.log("parsedJSON", JSON.stringify(parsedJSON));
     // eslint-disable-next-line no-console
     console.log("isNotCheating", isNotCheating);
     if (isNotCheating) {
         return JSON.parse(json);
     }
     return {
-        question: {content: "An error occurred", widgets: {}, images: {}},
+        question: {
+            content: "An error occurred",
+            widgets: {
+                "expression 1": {
+                    type: "expression",
+                    version: {major: 1, minor: 0},
+                    alignment: "default",
+                    static: true,
+                    graded: false,
+                    options: {
+                        answerForms: [
+                            {
+                                form: false,
+                                value: "",
+                                simplify: false,
+                                considered: "ungraded",
+                            },
+                        ],
+                        buttonSets: ["basic"],
+                        functions: ["f", "g", "h"],
+                        times: false,
+                    },
+                    ...{},
+                },
+            },
+            images: {},
+        },
         hints: [],
         itemDataVersion: {major: 0, minor: 0},
         answer: "",
         answerArea: null,
     };
+}
+
+function buildRandomString() {
+    let randomString: string = "";
+    for (let i = 0; i < 5; i++) {
+        const randomLetter = String.fromCharCode(
+            97 + Math.floor(Math.random() * 26),
+        );
+        randomString += randomLetter;
+    }
+    return randomString;
+}
+
+function buildTestData(randomString: string) {
+    return `{"data":{"assessmentItem":{"__typename":"AssessmentItemOrError","error":null,"item":{"__typename":"AssessmentItem","id":"x890b3c70f3e8f4a6","itemData":{"answerArea":{"calculator":false,"chi2Table":false,"financialCalculatorMonthlyPayment":false,"financialCalculatorTimeToPayOff":false,"financialCalculatorTotalAmount":false,"periodicTable":false,"periodicTableWithKey":false,"tTable":false,"zTable":false},"hints":[],"itemDataVersion":{"major":0,"minor":1},"question":{"content":"${randomString}:bar","images":{},"widgets":{"expression 1":{"alignment":"default","graded":false,"options":{"answerForms":[{"considered":"correct","form":false,"key":0,"simplify":false,"value":"v=150/12.5"}],"ariaLabel":"Answer","buttonSets":["basic"],"functions":["f","g","h"],"static":true,"times":false,"visibleLabel":"Answer"},"static":false,"type":"expression","version":{"major":1,"minor":0}}}}},"problemType":"Type 1","sha":"c7284a3ad65214b4e62bccce236d92f7f5d35941"}}}}`;
 }
