faa2e89f754ccaf08813253becd7bd31de4a6216
catandthemachines <123020281+catandthemachines@users.noreply.github.com>
2024-10-04 00:13:04 +0000

    Deploying to gh-pages from @ Khan/perseus@d4f4e2be1408c4531a146bcd496344a629d90bd1 🚀

---
+++ assets/editor-jsonify-u9Y-culI.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./jquery-yG1GhClm.js";import{d as i}from"./article-renderer-m_Mbocpj.js";const s={serialize:function(){return o.omit(this.props,i)}};export{s as E};
--- assets/register-all-widgets-for-testing-C3W4zlzO.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-Cj0qqEHS.js";import{r,a as s}from"./renderer-hPmI_Chp.js";const a=()=>{r(e),s()};export{a as r};
