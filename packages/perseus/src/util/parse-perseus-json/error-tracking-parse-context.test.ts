2e2788f5b454734154308c36900e0780d7625cea
Ben Christel <benchristel@khanacademy.org>
2024-10-08 13:47:10 -0700

    Add parse function

--- packages/perseus/src/util/parse-perseus-json/error-tracking-parse-context.test.ts
+++ packages/perseus/src/util/parse-perseus-json/error-tracking-parse-context.test.ts
@@ -2,27 +2,30 @@ import {ErrorTrackingParseContext} from "./error-tracking-parse-context";
 
 describe("ErrorTrackingParseContext", () => {
     it("adds its `path` to reported failures", () => {
-        const ctx = new ErrorTrackingParseContext(["foo", 1, "bar"])
+        const ctx = new ErrorTrackingParseContext(["foo", 1, "bar"]);
 
         expect(ctx.failure("oops", 4)).toEqual({
             type: "failure",
             detail: {
                 message: "oops",
                 badValue: 4,
-                path: ["foo", 1, "bar"]
+                path: ["foo", 1, "bar"],
             },
-        })
-    })
+        });
+    });
 
     it("spawns a new context for a subtree of the object being parsed", () => {
-        const rootCtx = new ErrorTrackingParseContext([])
-        const subCtx = rootCtx.forSubtree("blah")
-        expect(subCtx.failure("", 99).detail.path).toEqual(["blah"])
-    })
+        const rootCtx = new ErrorTrackingParseContext([]);
+        const subCtx = rootCtx.forSubtree("blah");
+        expect(subCtx.failure("", 99).detail.path).toEqual(["blah"]);
+    });
 
     it("is not modified by spawning a subcontext", () => {
-        const rootCtx = new ErrorTrackingParseContext(["original", "path"])
-        rootCtx.forSubtree("changed")
-        expect(rootCtx.failure("", 99).detail.path).toEqual(["original", "path"])
-    })
-})
+        const rootCtx = new ErrorTrackingParseContext(["original", "path"]);
+        rootCtx.forSubtree("changed");
+        expect(rootCtx.failure("", 99).detail.path).toEqual([
+            "original",
+            "path",
+        ]);
+    });
+});
