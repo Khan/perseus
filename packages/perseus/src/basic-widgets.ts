3de001eff1747ba5a5ce5ca0f1dab91b44b4b423
Sarah Third <sarahthird@gmail.com>
2024-10-08 14:39:19 -0700

    Returning the export for temporary linter appeasement

--- packages/perseus/src/basic-widgets.ts
+++ packages/perseus/src/basic-widgets.ts
@@ -2,6 +2,7 @@
 // As new widgets get added here, please also make sure they get added in
 // content_internal/perseus_traversal.py so they can be properly translated.
 import Expression from "./widgets/expression";
+import InputNumber from "./widgets/input-number";
 import NumericInput from "./widgets/numeric-input";
 import Radio from "./widgets/radio";
 
@@ -9,6 +10,7 @@ import type {WidgetExports} from "./types";
 
 export default [
     Radio,
+    InputNumber,
     NumericInput,
     Expression,
 ] as ReadonlyArray<WidgetExports>;
