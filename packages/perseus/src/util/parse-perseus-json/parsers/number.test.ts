7d90a20c873cb1f090cab847303cf73ac1967d96
Ben Christel <benchristel@khanacademy.org>
2024-10-08 14:15:16 -0700

    Add string parser

--- packages/perseus/src/util/parse-perseus-json/parsers/number.test.ts
+++ packages/perseus/src/util/parse-perseus-json/parsers/number.test.ts
@@ -1,35 +1,37 @@
-import {number} from "./number";
+import invariant from "tiny-invariant";
+
 import {ErrorTrackingParseContext} from "../error-tracking-parse-context";
 import {failure, success} from "../result";
-import invariant from "tiny-invariant";
+
+import {number} from "./number";
 
 describe("number", () => {
     function ctx() {
-        return new ErrorTrackingParseContext([])
+        return new ErrorTrackingParseContext([]);
     }
 
-    const anyFailure = failure(expect.anything())
+    const anyFailure = failure(expect.anything());
 
     it("accepts a real number", () => {
-        expect(number(3.5, ctx())).toEqual(success(3.5))
-    })
+        expect(number(3.5, ctx())).toEqual(success(3.5));
+    });
 
     it("accepts Infinity", () => {
-        expect(number(Infinity, ctx())).toEqual(success(Infinity))
-    })
+        expect(number(Infinity, ctx())).toEqual(success(Infinity));
+    });
 
     it("accepts NaN", () => {
-        expect(number(NaN, ctx())).toEqual(success(NaN))
-    })
+        expect(number(NaN, ctx())).toEqual(success(NaN));
+    });
 
     it("rejects a string", () => {
-        expect(number("foobar", ctx())).toEqual(anyFailure)
-    })
+        expect(number("foobar", ctx())).toEqual(anyFailure);
+    });
 
     it("returns a nice error message on failure", () => {
         const result = number("foobar", ctx());
 
-        invariant(result.type === "failure")
-        expect(result.detail.message).toBe(`expected number, but got "foobar"`)
-    })
-})
+        invariant(result.type === "failure");
+        expect(result.detail.message).toBe(`expected number, but got "foobar"`);
+    });
+});
