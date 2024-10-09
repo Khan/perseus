843f599cd21e2d9290673bfb7726ac27720ef66b
Anakaren Rojas <anakaren@khanacademy.org>
2024-10-09 16:39:09 -0700

    POC code redo

--- packages/perseus/src/perseus-types.ts
+++ packages/perseus/src/perseus-types.ts
@@ -470,6 +470,7 @@ export type LegacyButtonSets = ReadonlyArray<
     | "logarithms"
     | "basic relations"
     | "advanced relations"
+    | "scientific"
 >;
 
 export type PerseusExpressionWidgetOptions = {
