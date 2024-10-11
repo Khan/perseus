2c1b4c886fd2e1d5056ca54cf7beed32e3fcfb70
Tamara <tamara@khanacademy.org>
2024-10-11 11:09:00 -0500

    Super simplify changes to grapher validation logic
    Create a helper function to determine if there is an asymptote to remove expect error and remove previous if/else logic

--- packages/perseus/src/widgets/grapher/grapher-validator.ts
+++ packages/perseus/src/widgets/grapher/grapher-validator.ts
@@ -1,6 +1,7 @@
 import {functionForType} from "./util";
 
-import type {FunctionTypes} from "./grapher-types";
+import type {FunctionTypes, Coords} from "./grapher-types";
+import type {GrapherAnswerTypes} from "../../perseus-types";
 import type {PerseusScore} from "../../types";
 import type {
     PerseusGrapherRubric,
@@ -20,9 +21,6 @@ function grapherValidator(
         };
     }
 
-    const userInputType = userInput.type;
-    const rubricType = rubric.correct.type;
-
     // We haven't moved the coords
     if (userInput.coords == null) {
         return {
@@ -32,37 +30,20 @@ function grapherValidator(
     }
 
     // Get new function handler for grading
-    let guessCoeffs: ReadonlyArray<number> | null = null;
-    let correctCoeffs: ReadonlyArray<number> | null = null;
-    let grader: FunctionTypes | null = null;
+    const grader = functionForType(userInput.type);
 
-    if (
-        (userInputType === "logarithm" || userInputType === "exponential") &&
-        rubricType === userInputType
-    ) {
-        grader = functionForType(userInput.type);
-        guessCoeffs = grader.getCoefficients(
-            userInput.coords,
-            userInput.asymptote,
-        );
-        correctCoeffs = grader.getCoefficients(
-            rubric.correct.coords,
-            rubric.correct.asymptote,
-        );
-    } else if (
-        (userInputType === "linear" ||
-            userInputType === "quadratic" ||
-            userInputType === "tangent" ||
-            userInputType === "sinusoid" ||
-            userInputType === "absolute_value") &&
-        rubricType === userInputType
-    ) {
-        grader = functionForType(userInputType);
-        guessCoeffs = grader.getCoefficients(userInput.coords);
-        correctCoeffs = grader.getCoefficients(rubric.correct.coords);
-    } else {
-        throw new Error("Type not yet supported.");
+    function getCoefficientsFunc(
+        data: GrapherAnswerTypes,
+    ): ReadonlyArray<number> {
+        let asymptote: Coords | undefined;
+        if ("asymptote" in data) {
+            asymptote = data.asymptote;
+        }
+        return grader.getCoefficients(data.coords, asymptote);
     }
+
+    const guessCoeffs = getCoefficientsFunc(userInput);
+    const correctCoeffs = getCoefficientsFunc(rubric.correct);
 
     if (guessCoeffs == null || correctCoeffs == null) {
         return {
@@ -70,8 +51,7 @@ function grapherValidator(
             message: null,
         };
     }
-
-    if (grader?.areEqual(guessCoeffs, correctCoeffs)) {
+    if (grader.areEqual(guessCoeffs, correctCoeffs)) {
         return {
             type: "points",
             earned: 1,
@@ -79,7 +59,6 @@ function grapherValidator(
             message: null,
         };
     }
-
     return {
         type: "points",
         earned: 0,
