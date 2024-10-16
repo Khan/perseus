bf13a380a75d91043ebd05ce9333ec853c578716
jeremywiebe <jeremy.wiebe@gmail.com>
2024-10-16 17:40:42 +0000

    Deploying to gh-pages from @ Khan/perseus@8a40e99e6cd6dd2424d84ec4d03744984aa68275 🚀

--- assets/editor-jsonify-gtbtRqO3.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./jquery-yG1GhClm.js";import{d as i}from"./article-renderer-0ma35t4N.js";const s={serialize:function(){return o.omit(this.props,i)}};export{s as E};
---
+++ assets/register-all-widgets-for-testing-8oD16KVS.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-hEkiWU47.js";import{r,a as s}from"./renderer-HeQMBZ0Y.js";const a=()=>{r(e),s()};export{a as r};
