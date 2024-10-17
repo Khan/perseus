47365cecf97bb93af311b6b0ebc0f8b1e1d7c960
Ben Christel <benchristel@khanacademy.org>
2024-10-17 10:24:26 -0700

    Add optional parser

---
+++ packages/perseus/src/util/parse-perseus-json/general-purpose-parsers/optional.test.ts
@@ -0,0 +1,30 @@
+import {string} from "./string";
+import {optional} from "./optional";
+import {anyFailure, ctx, parseFailureWith} from "./test-helpers";
+import {success} from "../result";
+
+describe("optional()", () => {
+    const optionalString = optional(string);
+
+    it("modifies a parser to also accept `undefined`", () => {
+        expect(optionalString(undefined, ctx())).toEqual(success(undefined))
+    })
+
+    it("accepts a value that the given parser accepts", () => {
+        expect(optionalString("ok", ctx())).toEqual(success("ok"))
+    })
+
+    it("rejects a value that the given parser rejects", () => {
+        expect(optionalString(999, ctx())).toEqual(anyFailure)
+    })
+
+    it("rejects null", () => {
+        expect(optionalString(null, ctx())).toEqual(anyFailure)
+    })
+
+    it("fails with the same error message as the wrapped parser", () => {
+        expect(optionalString(999, ctx())).toEqual(parseFailureWith({
+            message: "expected string, but got 999"
+        }))
+    })
+})
