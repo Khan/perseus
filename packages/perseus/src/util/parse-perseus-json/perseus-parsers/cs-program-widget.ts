4bbf6d487a6ff44324c9bb22223ab4105b5e16c5
Ben Christel <benchristel@khanacademy.org>
2024-10-17 14:54:43 -0700

    Fix lint

--- packages/perseus/src/util/parse-perseus-json/perseus-parsers/cs-program-widget.ts
+++ packages/perseus/src/util/parse-perseus-json/perseus-parsers/cs-program-widget.ts
@@ -1,13 +1,15 @@
-import {parseWidget} from "./widget";
-import {constant} from "../general-purpose-parsers/constant";
-import {object} from "../general-purpose-parsers/object";
-import {CSProgramWidget, PerseusCSProgramSetting} from "../../../perseus-types";
-import {Parser} from "../parser-types";
 import {any} from "../general-purpose-parsers/any";
-import {string} from "../general-purpose-parsers/string";
 import {array} from "../general-purpose-parsers/array";
 import {boolean} from "../general-purpose-parsers/boolean";
+import {constant} from "../general-purpose-parsers/constant";
 import {number} from "../general-purpose-parsers/number";
+import {object} from "../general-purpose-parsers/object";
+import {string} from "../general-purpose-parsers/string";
+
+import {parseWidget} from "./widget";
+
+import type {CSProgramWidget} from "../../../perseus-types";
+import type {Parser} from "../parser-types";
 
 export const parseCSProgramWidget: Parser<CSProgramWidget> = parseWidget(
     constant("cs-program"),
@@ -21,4 +23,4 @@ export const parseCSProgramWidget: Parser<CSProgramWidget> = parseWidget(
         height: number,
         static: boolean,
     }),
-)
+);
