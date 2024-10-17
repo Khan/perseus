46f435d79c003259229c00df689f748bc861630d
Ben Christel <benchristel@khanacademy.org>
2024-10-17 14:02:01 -0700

    Fix lint

--- packages/perseus/src/util/parse-perseus-json/general-purpose-parsers/any.ts
+++ packages/perseus/src/util/parse-perseus-json/general-purpose-parsers/any.ts
@@ -1,3 +1,3 @@
-import {Parser} from "../parser-types";
+import type {Parser} from "../parser-types";
 
 export const any: Parser<any> = (rawValue, ctx) => ctx.success(rawValue);
--- packages/perseus/src/util/parse-perseus-json/perseus-parsers/perseus-item.ts
+++ packages/perseus/src/util/parse-perseus-json/perseus-parsers/perseus-item.ts
@@ -1,19 +1,19 @@
 // TODO(benchristel): export all parsers from general-purpose-parsers/index.ts
 // so we only need to import from one place.
+import {ItemExtras} from "../../../perseus-types";
+import {any} from "../general-purpose-parsers/any";
 import {array} from "../general-purpose-parsers/array";
+import {boolean} from "../general-purpose-parsers/boolean";
+import {enumeration} from "../general-purpose-parsers/enumeration";
 import {number} from "../general-purpose-parsers/number";
 import {object} from "../general-purpose-parsers/object";
+import {record} from "../general-purpose-parsers/record";
 
 import {parseHint} from "./hint";
 import {parsePerseusRenderer} from "./perseus-renderer";
 
 import type {PerseusItem} from "../../../perseus-types";
 import type {Parser} from "../parser-types";
-import {record} from "../general-purpose-parsers/record";
-import {ItemExtras} from "../../../perseus-types";
-import {enumeration} from "../general-purpose-parsers/enumeration";
-import {boolean} from "../general-purpose-parsers/boolean";
-import {any} from "../general-purpose-parsers/any";
 
 export const parsePerseusItem: Parser<PerseusItem> = object({
     question: parsePerseusRenderer,
