a68a4540670b2d30502ed58df2ff70e61c7ade30
YiSheng <jiaojiaojiao.cpbr@gmail.com>
2024-10-24 18:02:01 +0800

    fix: ios < 17 not support look behind

--- packages/simple-markdown/src/legacyMarkdownAdapter.ts
+++ packages/simple-markdown/src/legacyMarkdownAdapter.ts
@@ -1,18 +1,18 @@
 export const adaptLegacyMarkdown = (str: string) => {
     // heading 前只有一個 \n 時，將 \n 改為 \n\n
     const addedNewLineAboveHeadingStr = str.replace(
-        /(?<=[^\n]\n *)(#{1,6}.*)/g,
-        "\n$1",
+        /([^\n]\n *)(#{1,6}.*)/g,
+        "$1\n$2",
     );
     // heading 後只有一個 \n 時，將 \n 改為 \n\n
     const addedNewLineUnderHeadingStr = addedNewLineAboveHeadingStr.replace(
-        /(?<=\n *|^)(#{1,6}[^\n]*)(?=\n[^\n])/g,
-        "$1\n",
+        /(\n *|^)(#{1,6}[^\n]*)(?=\n[^\n])/g,
+        "$1$2\n",
     );
     // hr 後只有一個 \n 時，將 \n 改為 \n\n
     const addedNewLineUnderHrStr = addedNewLineUnderHeadingStr.replace(
-        /(?<=\n *)([-*_]{3,})(?=\n[^\n])/g,
-        "$1\n",
+        /(\n *)([-*_]{3,})(?=\n[^\n])/g,
+        "$1$2\n",
     );
 
     return addedNewLineUnderHrStr;
