25bb4abff563974f914c55bfb81482bd780712ed
Myranae <60857422+Myranae@users.noreply.github.com>
2024-10-28 15:55:37 +0000

    Deploying to gh-pages from @ Khan/perseus@d6edf18ef07c6b82ae585e77d75e9132d554baf5 🚀

---
+++ assets/editor-jsonify--zQSWgL9.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./jquery-yG1GhClm.js";import{d as i}from"./article-renderer-WFJM0wSM.js";const s={serialize:function(){return o.omit(this.props,i)}};export{s as E};
---
+++ assets/register-all-widgets-for-testing-tazCU3Lb.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-WFJM0wSM.js";import{r,a as s}from"./renderer-44HkJ58y.js";const a=()=>{r(e),s()};export{a as r};
