1a6b42f6a466de145848f498a0fe340eb7b537a7
Mike Klemarewski <mikeklemarewski@khanacademy.org>
2024-10-29 15:55:49 -0700

    Add getPromptJSON for categorizer widget

--- packages/perseus/src/prompt-types.ts
+++ packages/perseus/src/prompt-types.ts
@@ -1,12 +1,17 @@
+import type {CategorizerPromptJSON} from "./widgets/categorizer/prompt-utils";
 import type {NumericInputPromptJSON} from "./widgets/numeric-input/prompt-utils";
 import type {RadioPromptJSON} from "./widgets/radio/prompt-utils";
 
 export enum WidgetType {
+    CATEGORIZER = "categorizer",
     RADIO = "radio",
     NUMERIC_INPUT = "numeric-input",
 }
 
-export type WidgetPromptJSON = RadioPromptJSON | NumericInputPromptJSON;
+export type WidgetPromptJSON =
+    | CategorizerPromptJSON
+    | RadioPromptJSON
+    | NumericInputPromptJSON;
 
 export type RendererPromptJSON = {
     content: string;
