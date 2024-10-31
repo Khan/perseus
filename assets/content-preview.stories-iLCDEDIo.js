49da87d0afd553c23dce5843f1f1d5edae97d612
handeyeco <matthew.c.tx@gmail.com>
2024-10-31 16:03:45 +0000

    Deploying to gh-pages from @ Khan/perseus@faf62afd094a758e376c5a8ac2acce140dedffdd 🚀

---
+++ assets/content-preview.stories-iLCDEDIo.js
@@ -0,0 +1,56 @@
+import{j as e,a as x,F as W}from"./jsx-runtime-FVsy8kgq.js";import"./article-renderer-sNHDlIcJ.js";import"./jquery-yG1GhClm.js";import"./util-F8-MDmsT.js";import"./phet-simulation-sBNltwka.js";import"./version-akiLXZts.js";import"./dependencies-8XILypbq.js";import"./perseus-api-mOiZT07d.js";import"./multi-renderer-QuO2NIvO.js";import"./hints-renderer-7EYiI2Vf.js";import{R as D}from"./renderer-_CNcy5Wv.js";import"./base-radio-jBWwpzIs.js";import"./button-group-nsoLlHtM.js";import"./graph-bSP67F6m.js";import"./svg-image-QKR8YXWf.js";import"./hud-WFKWq8xK.js";import"./icon-YuYiVxsK.js";import"./index-K9BSJPWl.js";import"./inline-icon-tKY1iMkH.js";import"./math-input-CBpol4VY.js";import"./multi-button-group-a0iIfrwx.js";import"./number-input-7sNeIRmO.js";import"./range-input-wtuw2vYt.js";import"./text-input-15gEhfDF.js";import"./text-list-editor-pe7AGDAl.js";import"./index-0C4KXdeC.js";import{u as R,a as A,m as $}from"./i18n-context-P7VpLqM1.js";import"./index-IIMKO4_x.js";import{V as m}from"./index-6h5t6F0w.js";import{s as b}from"./index-deFLJwr4.js";import{r as I}from"./index-TT1qJ6UJ.js";import{c as L}from"./article-renderer.testdata-spwNviYy.js";import{b as O}from"./radio.testdata-sITJ8lxE.js";import{D as _}from"./device-framer-aLdrjARa.js";import{V as k}from"./viewport-resizer-vem8eC5k.js";import{S as V,a as H,M}from"./mobile-keypad-wPUeR8mM.js";import"./prop-types-SKtvHVtl.js";import"./key-translator-Bl2XtQm9.js";import"./button-assets-txIojR3b.js";import{l as j}from"./constants-hQyjCzwz.js";import{l as z}from"./index-awljIyHI.js";/* empty css                       */import"./enums-x5qaTru7.js";import"./index-h_CiYGGb.js";import"./index-dnMhQZ-1.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-7vsPyIck.js";import"./input-with-examples-haC6QBKl.js";import"./tooltip-WOCyk4ho.js";import"./simple-keypad-input-LEkrmlga.js";import"./icon-paths-AuJwhOz7.js";import"./media-queries-gbofTbz-.js";import"./constants-iPV6vHZm.js";import"./shared-4TdECMLk.js";import"./index-2dmvOE3q.js";import"./react-router-dom-W_e8xVUu.js";import"./index-tvtfaFq4.js";import"./index-BPSWBbj5.js";import"./index-YCTzZMik.js";import"./index-rfN0X25E.js";import"./index-o3wWn3Y5.js";import"./index-CkAxGj88.js";import"./Popper-kGnKOid7.js";import"./answer-choices-G__zfjGb.js";import"./index-xuPsLuPk.js";import"./minus-bold-ONmDo3Ve.js";import"./choice-sLCDogEV.js";import"./index-ngddCaVG.js";import"./index-qUyqkRvh.js";import"./choice-icon-Y1Ekd8Xq.js";import"./focus-ring-2b4ybtc7.js";import"./option-status-vrNywrR6.js";import"./asset-context-I7yIqWki.js";import"./index-J2t_5nK1.js";import"./tiny-invariant-bHgPayXn.js";import"./marker-DbMwA9Zp.js";import"./answer-pill-DRFc4VT2.js";import"./sortable-ck-K6DSA.js";import"./fixed-to-responsive-AkRQxH3P.js";import"./video-transcript-link-44eQkwGl.js";import"./arrow-square-out-bold-090vdhrx.js";import"./stub-tag-editor-Hmby24Jq.js";import"./invariant-bu5zBsRS.js";import"./tex-VP4hirbI.js";import"./zoomable-CA1NzpZD.js";import"./zoomable-tex-MZi7Hqbp.js";import"./lint--ywkTlk8.js";import"./choice-none-above-l6J3NwNT.js";import"./client-rbWgHzHN.js";import"./image-loader-mgyZcN0j.js";import"./index-QCAhLhLD.js";import"./bundle.esm-7jAAHVxW.js";import"./components-ojLC5Vkn.js";import"./tabbar-WYCYDof0.js";import"./item-X9tjzx12.js";import"./index-WNT3sUKf.js";import"./keypad-button-rKskH7oM.js";import"./operators-page-C_EsmjPW.js";import"./navigation-pad-ss7r0FKW.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";function p({question:t,apiOptions:r,seamless:s,linterContext:P,legacyPerseusLint:T,previewDevice:q}){const C=R(),a=q!=="desktop";return e(m,{className:`framework-perseus ${a?"perseus-mobile":""}`,style:[c.container,s?void 0:c.gutter],children:e(V,{children:e(H.Consumer,{children:({setKeypadActive:S,keypadElement:E,setKeypadElement:N})=>x(W,{children:[e(D,{strings:C.strings,apiOptions:{...r,isMobile:a},keypadElement:E,linterContext:P,legacyPerseusLint:T,...t}),e(M,{onAnalyticsEvent:()=>Promise.resolve(),onDismiss:()=>S(!1),onElementMounted:N})]})})})})}const c=z.StyleSheet.create({container:{padding:b.xxxSmall_4,containerType:"inline-size",containerName:"perseus-root"},gutter:{marginRight:j}});p.__docgenInfo={description:"The `ContentPreview` component provides a simple preview system for Perseus\nContent. Due to how Persus styles are built, the preview styling matches the\ncurrent device based on the viewport width (using `@media` queries for\n`min-width` and `max-width`).\n\nThe preview will render the mobile variant (styling and layout) when the\n`previewDevice` is phone or tablet. Note that the styling cannot be matched\n100% due to the above `@media` query limitation.",methods:[],displayName:"ContentPreview",props:{question:{required:!1,tsType:{name:"PerseusRenderer"},description:""},apiOptions:{required:!1,tsType:{name:"APIOptions"},description:""},seamless:{required:!1,tsType:{name:"boolean"},description:""},linterContext:{required:!1,tsType:{name:"LinterContextProps"},description:""},legacyPerseusLint:{required:!1,tsType:{name:"ReadonlyArray",elements:[{name:"string"}],raw:"ReadonlyArray<string>"},description:""},previewDevice:{required:!0,tsType:{name:"DeviceType"},description:""}}};const F=t=>{const[r,s]=I.useState("phone");return x(m,{children:[e(k,{deviceType:r,onViewportSizeChanged:s}),e(_,{nochrome:!1,deviceType:r,children:e(p,{...t})})]})},kt={title:"PerseusEditor/Content Preview",component:p,decorators:[t=>e(m,{style:{margin:b.xxSmall_6},children:e(A,{strings:$,locale:"en",children:e(t,{})})})],render:t=>e(F,{...t})},o={args:{question:O}},i={args:{question:L}},n={args:{linterContext:{contentType:"exercise",highlightLint:!0,stack:[],paths:[]},question:{content:`# H1s bad
+
+Here is some unclosed math: $1+1=3
+
+We should use \`\\dfrac{}\` instead of \`\\frac{}\`: $\\frac{3}{5}$
+
+What is the best color in the world?
+
+[[☃ radio 1]]`,widgets:{"radio 1":{type:"radio",options:{choices:[{content:"Red"},{content:"# Green"},{content:"Blue",correct:!0},{content:"None of these!",isNoneOfTheAbove:!0}]}}},images:{}}}};var d,l,u;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    question
+  }
+}`,...(u=(l=o.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var h,g,f;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    question: articleWithImages
+  }
+}`,...(f=(g=i.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var y,v,w;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    linterContext: {
+      contentType: "exercise",
+      highlightLint: true,
+      stack: [],
+      paths: []
+    },
+    question: {
+      content: \`# H1s bad
+
+Here is some unclosed math: $1+1=3
+
+We should use \\\`\\\\dfrac{}\\\` instead of \\\`\\\\frac{}\\\`: $\\\\frac{3}{5}$
+
+What is the best color in the world?
+
+[[☃ radio 1]]\`,
+      widgets: {
+        "radio 1": {
+          type: "radio",
+          options: {
+            choices: [{
+              content: "Red"
+            }, {
+              content: "# Green"
+            }, {
+              content: "Blue",
+              correct: true
+            }, {
+              content: "None of these!",
+              isNoneOfTheAbove: true
+            }]
+          }
+        }
+      },
+      images: {}
+    }
+  }
+}`,...(w=(v=n.parameters)==null?void 0:v.docs)==null?void 0:w.source}}};const Vt=["Exercise","Article","WithLintErrors"];export{i as Article,o as Exercise,n as WithLintErrors,Vt as __namedExportsOrder,kt as default};
---
+++ assets/register-all-widgets-for-testing-r3iC504P.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-sNHDlIcJ.js";import{r,a as s}from"./renderer-_CNcy5Wv.js";const a=()=>{r(e),s()};export{a as r};
