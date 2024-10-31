015e203c05ccf82f05dcbaa7673f8fe758eb83d8
Matthew Curtis <matthewcurtis@khanacademy.org>
2024-10-31 11:20:13 -0500

    add a comment

--- packages/perseus/src/index.ts
+++ packages/perseus/src/index.ts
@@ -36,6 +36,8 @@ export {default as TableWidget} from "./widgets/table";
 export {default as PlotterWidget} from "./widgets/plotter";
 export {default as GrapherWidget} from "./widgets/grapher";
 
+// Some utils in grapher/utils don't need to be used outside of `perseus`,
+// so only export the stuff that does need to be exposed
 import {
     allTypes,
     DEFAULT_GRAPHER_PROPS,
