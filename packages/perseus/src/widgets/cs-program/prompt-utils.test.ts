9b70e56e2dd41d38711522fd2a11fbcec466882a
Mike Klemarewski <mikeklemarewski@khanacademy.org>
2024-10-31 11:44:11 -0700

    The cs widget should be unsupported

--- packages/perseus/src/widgets/cs-program/prompt-utils.test.ts
+++ packages/perseus/src/widgets/cs-program/prompt-utils.test.ts
@@ -1,26 +1,14 @@
 import {WidgetType} from "../../prompt-types";
 
-import {getPromptJSON} from "./prompt-utils";
-
-import type {PerseusCSProgramUserInput} from "../../validation.types";
+import {getPromptJSON, UNSUPPORTED_MESSAGE} from "./prompt-utils";
 
 describe("CS Program getPromptJSON", () => {
     it("it returns JSON with the expected format and fields", () => {
-        const renderProps: any = {
-            programID: 12345,
-        };
-
-        const userInput: PerseusCSProgramUserInput = {
-            status: "incomplete",
-            message: "Keep going!",
-        };
-
-        const resultJSON = getPromptJSON(renderProps, userInput);
+        const resultJSON = getPromptJSON();
 
         expect(resultJSON).toEqual({
             type: WidgetType.CS_PROGRAM,
-            programID: renderProps.programID,
-            userInput,
+            description: UNSUPPORTED_MESSAGE,
         });
     });
 });
