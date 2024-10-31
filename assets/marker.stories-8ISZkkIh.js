a36bdfb4da541124891e50319c776fab5c32851c
benchristel <benchristel@users.noreply.github.com>
2024-10-31 20:51:26 +0000

    Deploying to gh-pages from @ Khan/perseus@33891dc0b5e477b0b6db2f0d3733d81215d13ebe 🚀

---
+++ assets/marker.stories-8ISZkkIh.js
@@ -0,0 +1,31 @@
+import{j as i}from"./jsx-runtime-FVsy8kgq.js";import{a as p}from"./chunk-WFFRPTHA-zlNDxfrq.js";import{l as h}from"./index-awljIyHI.js";import{M as u}from"./marker-Pndudccx.js";import"./index-TT1qJ6UJ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./preview-errors-7FWlPnjy.js";import"./index-PPLHz8o0.js";import"./article-renderer-G6OZTDY6.js";import"./util-F8-MDmsT.js";import"./version-akiLXZts.js";import"./jquery-yG1GhClm.js";import"./prop-types-SKtvHVtl.js";import"./mobile-keypad-wPUeR8mM.js";import"./index-7vsPyIck.js";import"./index-CkAxGj88.js";import"./index-J2t_5nK1.js";import"./index-6h5t6F0w.js";import"./index-deFLJwr4.js";import"./tabbar-WYCYDof0.js";import"./item-X9tjzx12.js";import"./index-2dmvOE3q.js";import"./react-router-dom-W_e8xVUu.js";import"./index-tvtfaFq4.js";import"./button-assets-txIojR3b.js";import"./keypad-button-rKskH7oM.js";import"./operators-page-C_EsmjPW.js";import"./navigation-pad-ss7r0FKW.js";import"./key-translator-Bl2XtQm9.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./invariant-bu5zBsRS.js";import"./enums-x5qaTru7.js";import"./renderer-I_qGl5bw.js";import"./index-dnMhQZ-1.js";import"./asset-context-I7yIqWki.js";import"./i18n-context-ETfl8K0U.js";import"./svg-image-x98AV6aF.js";import"./dependencies-8XILypbq.js";import"./fixed-to-responsive-AkRQxH3P.js";import"./constants-iPV6vHZm.js";import"./client-rbWgHzHN.js";import"./inline-icon-tKY1iMkH.js";import"./icon-paths-5JCXzGsq.js";import"./image-loader-mgyZcN0j.js";import"./tex-VP4hirbI.js";import"./zoomable-CA1NzpZD.js";import"./zoomable-tex-MZi7Hqbp.js";import"./perseus-api-mOiZT07d.js";import"./index-k-0mNqHS.js";import"./stub-tag-editor-Hmby24Jq.js";import"./text-list-editor-pe7AGDAl.js";import"./lint--ywkTlk8.js";import"./index-K9BSJPWl.js";import"./index-xuPsLuPk.js";import"./index-YCTzZMik.js";import"./index-rfN0X25E.js";import"./index-o3wWn3Y5.js";import"./index-h_CiYGGb.js";import"./Popper-kGnKOid7.js";import"./math-input-NGFURlp6.js";import"./index-WNT3sUKf.js";import"./index-BPSWBbj5.js";import"./input-with-examples-FTgJzjhr.js";import"./text-input-15gEhfDF.js";import"./index-QCAhLhLD.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-WOCyk4ho.js";import"./simple-keypad-input-LEkrmlga.js";import"./base-radio-D7Lv5zu4.js";import"./media-queries-gbofTbz-.js";import"./shared-4TdECMLk.js";import"./choice-pZVqrutg.js";import"./index-ngddCaVG.js";import"./index-qUyqkRvh.js";import"./icon-YuYiVxsK.js";import"./choice-icon-VTL-b8yI.js";import"./focus-ring-2b4ybtc7.js";import"./option-status-8rF8JLxh.js";import"./choice-none-above-yJs5-lHD.js";import"./phet-simulation-tjElQyHJ.js";import"./arrow-square-out-bold-090vdhrx.js";import"./answer-choices-eQKK1QZP.js";import"./button-group-nsoLlHtM.js";import"./graph-2roxTC5p.js";import"./index-IIMKO4_x.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-7jAAHVxW.js";import"./hud-WFKWq8xK.js";import"./multi-button-group-a0iIfrwx.js";import"./number-input-eyn_U5ik.js";import"./range-input-ERbW1pG6.js";import"./marker-lnnnXEVk.js";import"./answer-pill-a1ESTX8G.js";import"./sortable-isNJN5Bh.js";import"./video-transcript-link-Is7xzZkJ.js";import"./multi-renderer-Eawdz1yd.js";import"./hints-renderer-qx0EA8hp.js";import"./components-aFUrXS9S.js";import"./global-colors-VIVU4Od4.js";import"./util-qk2aeK8X.js";import"./form-wrapped-text-field-A2kf-LD-.js";const co={title:"PerseusEditor/Widgets/Label Image/Marker"},y=h.StyleSheet.create({wrapper:{marginTop:150,position:"relative",width:32,height:32}}),l=m=>i("div",{className:h.css(y.wrapper),children:i(u,{...m})}),o=m=>i(l,{...{answers:[],choices:[],label:"",onChange:(...r)=>{p("onChange")(...r)},onRemove:(...r)=>{p("onRemove")(...r)},x:50,y:50}}),t=m=>i(l,{...{answers:["BMW","Ferrari"],choices:["Lamborghini","BMW","Volkswagen","Fiat","Porsche","Ferrari"],label:"Automotive",onChange:(...r)=>{p("onChange")(...r)},onRemove:(...r)=>{p("onRemove")(...r)},x:50,y:50}});o.__docgenInfo={description:"",methods:[],displayName:"Empty"};t.__docgenInfo={description:"",methods:[],displayName:"WithAnswers"};var e,s,n;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const props = ({
+    answers: [],
+    choices: [],
+    label: "",
+    onChange: (...args) => {
+      action("onChange")(...args);
+    },
+    onRemove: (...args) => {
+      action("onRemove")(...args);
+    },
+    x: 50,
+    y: 50
+  } as const);
+  return <Wrapper {...props} />;
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var a,c,g;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`(args: StoryArgs): React.ReactElement => {
+  const props = ({
+    answers: ["BMW", "Ferrari"],
+    choices: ["Lamborghini", "BMW", "Volkswagen", "Fiat", "Porsche", "Ferrari"],
+    label: "Automotive",
+    onChange: (...args) => {
+      action("onChange")(...args);
+    },
+    onRemove: (...args) => {
+      action("onRemove")(...args);
+    },
+    x: 50,
+    y: 50
+  } as const);
+  return <Wrapper {...props} />;
+}`,...(g=(c=t.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const go=["Empty","WithAnswers"];export{o as Empty,t as WithAnswers,go as __namedExportsOrder,co as default};
--- assets/register-all-widgets-for-testing-jnG7wCsy.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer--Ky9v_Jg.js";import{r,a as s}from"./renderer-srblPfL7.js";const a=()=>{r(e),s()};export{a as r};
