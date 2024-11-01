37ee7206480b20bbfba95914e7cb6157a7258291
Mike Klemarewski <mikeklemarewski@khanacademy.org>
2024-11-01 12:10:27 -0700

    Add prompt utils for python program widget

---
+++ packages/perseus/src/widget-ai-utils/python-program/prompt-utils.test.ts
@@ -0,0 +1,12 @@
+import {getPromptJSON} from "./prompt-utils";
+
+describe("Python Program getPromptJSON", () => {
+    it("it returns JSON with the expected format and fields", () => {
+        const resultJSON = getPromptJSON();
+
+        expect(resultJSON).toEqual({
+            type: "python-program",
+            isSupported: false,
+        });
+    });
+});
