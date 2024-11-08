35f46748c41993f4699b0d3e0062f709be2b193d
benchristel <benchristel@users.noreply.github.com>
2024-11-08 17:10:25 +0000

    Deploying to gh-pages from @ Khan/perseus@32b27322c78fc818900d3c48dc738cb50d086505 🚀

---
+++ assets/register-all-widgets-for-testing-lZqlz1ut.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-2j5RefEH.js";import{r,a as s}from"./renderer-vyhYTcrl.js";const a=()=>{r(e),s()};export{a as r};
---
+++ assets/sortable.stories-OIxyBXaD.js
@@ -0,0 +1,32 @@
+import{S as z}from"./sortable-fWGDPU8i.js";import"./jsx-runtime-FVsy8kgq.js";import"./index-TT1qJ6UJ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./renderer-vyhYTcrl.js";import"./version-akiLXZts.js";import"./util-F8-MDmsT.js";import"./jquery-yG1GhClm.js";import"./invariant-bu5zBsRS.js";import"./index-J2t_5nK1.js";import"./index-dnMhQZ-1.js";import"./index-7vsPyIck.js";import"./asset-context-I7yIqWki.js";import"./i18n-context-P5sgPFep.js";import"./svg-image-4Vh9uTQ6.js";import"./index-awljIyHI.js";import"./index-6h5t6F0w.js";import"./index-deFLJwr4.js";import"./dependencies-d8cZibFS.js";import"./fixed-to-responsive-AkRQxH3P.js";import"./constants-iPV6vHZm.js";import"./client-rbWgHzHN.js";import"./inline-icon-NjJlm7d0.js";import"./icon-paths-5JCXzGsq.js";import"./image-loader-3HliPbpF.js";import"./tex-6yhnhbtf.js";import"./zoomable-CA1NzpZD.js";import"./zoomable-tex-hbM7rxrP.js";import"./perseus-api-Nq3s7IMx.js";import"./index-0C4KXdeC.js";import"./stub-tag-editor-74YQ4o9G.js";import"./text-list-editor-Jz35fIN1.js";import"./lint-kpBgzQ8K.js";const pr={title:"Perseus/Components/Sortable",component:z,args:{options:["Option 1","Option 2","Option 3"]}},r={args:{layout:"horizontal",options:["a","b","c"],waitForTexRendererToLoad:!1}},o={args:{layout:"vertical",options:["a","b","c"],waitForTexRendererToLoad:!1}},t={},a={args:{padding:!1}},e={args:{margin:64}},s={args:{disabled:!0}},i={args:{constraints:{height:128,width:256}}};var n,p,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    layout: "horizontal",
+    options: ["a", "b", "c"],
+    waitForTexRendererToLoad: false
+  }
+}`,...(c=(p=r.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var m,d,l;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    layout: "vertical",
+    options: ["a", "b", "c"],
+    waitForTexRendererToLoad: false
+  }
+}`,...(l=(d=o.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var g,u,b;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(b=(u=t.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};var S,h,T;a.parameters={...a.parameters,docs:{...(S=a.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    padding: false
+  }
+}`,...(T=(h=a.parameters)==null?void 0:h.docs)==null?void 0:T.source}}};var O,x,B;e.parameters={...e.parameters,docs:{...(O=e.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    margin: 64
+  }
+}`,...(B=(x=e.parameters)==null?void 0:x.docs)==null?void 0:B.source}}};var f,W,w;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(w=(W=s.parameters)==null?void 0:W.docs)==null?void 0:w.source}}};var L,E,y;i.parameters={...i.parameters,docs:{...(L=i.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    constraints: {
+      height: 128,
+      width: 256
+    }
+  }
+}`,...(y=(E=i.parameters)==null?void 0:E.docs)==null?void 0:y.source}}};const cr=["SortableHorizontalExample","SortableVerticalExample","BasicSortableOptionsTest","BasicSortableOptionsTestWithNoPadding","BasicSortableOptionsTestWithLargeMargin","BasicSortableOptionsTestDisabled","BasicSortableOptionsTestWithWidthAndHeightConstraints"];export{t as BasicSortableOptionsTest,s as BasicSortableOptionsTestDisabled,e as BasicSortableOptionsTestWithLargeMargin,a as BasicSortableOptionsTestWithNoPadding,i as BasicSortableOptionsTestWithWidthAndHeightConstraints,r as SortableHorizontalExample,o as SortableVerticalExample,cr as __namedExportsOrder,pr as default};
