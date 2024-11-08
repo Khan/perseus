c3db7b0cb415269c932a39fcd174f47cdff8daf3
Ben Christel <benchristel@khanacademy.org>
2024-11-08 11:37:26 -0800

    Type group widget parser

--- packages/perseus/src/util/parse-perseus-json/perseus-parsers/group-widget.ts
+++ packages/perseus/src/util/parse-perseus-json/perseus-parsers/group-widget.ts
@@ -3,7 +3,10 @@ import {constant} from "../general-purpose-parsers";
 import {parsePerseusRenderer} from "./perseus-renderer";
 import {parseWidget} from "./widget";
 
-export const parseGroupWidget = parseWidget(
+import type {GroupWidget} from "../../../perseus-types";
+import type {Parser} from "../parser-types";
+
+export const parseGroupWidget: Parser<GroupWidget> = parseWidget(
     constant("group"),
     // This module has an import cycle with parsePerseusRenderer.
     // The anonymous function below ensures that we don't try to access
