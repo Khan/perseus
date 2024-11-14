e6f0b850f9fa52d03801fad92187d3cc2259859a
Tamara <tamara@khanacademy.org>
2024-11-14 16:01:53 -0600

    Split validation tests from scoring tests

--- packages/perseus/src/widgets/categorizer/score-categorizer.test.ts
+++ packages/perseus/src/widgets/categorizer/score-categorizer.test.ts
@@ -30,19 +30,4 @@ describe("scoreCategorizer", () => {
 
         expect(score).toHaveBeenAnsweredIncorrectly();
     });
-
-    it("tells the learner its not complete if not selected", () => {
-        const rubric: PerseusCategorizerRubric = {
-            values: [1, 3],
-        };
-
-        const userInput = {
-            values: [2],
-        } as const;
-        const score = scoreCategorizer(userInput, rubric, mockStrings);
-
-        expect(score).toHaveInvalidInput(
-            "Make sure you select something for every row.",
-        );
-    });
 });
---
+++ packages/perseus/src/widgets/categorizer/validate-categorizer.test.ts
@@ -0,0 +1,22 @@
+import {mockStrings} from "../../strings";
+
+import validateCategorizer from "./validate-categorizer";
+
+import type {PerseusCategorizerRubric} from "../../validation.types";
+
+describe("validateCategorizer", () => {
+    it("tells the learner its not complete if not selected", () => {
+        const rubric: PerseusCategorizerRubric = {
+            values: [1, 3],
+        };
+
+        const userInput = {
+            values: [2],
+        } as const;
+        const score = validateCategorizer(userInput, rubric, mockStrings);
+
+        expect(score).toHaveInvalidInput(
+            "Make sure you select something for every row.",
+        );
+    });
+});
