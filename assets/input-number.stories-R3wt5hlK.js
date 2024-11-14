e6e87ea07cb65cb92bb72207c15e8d24aa1c2979
SonicScrewdriver <sarahthird@gmail.com>
2024-11-14 20:46:58 +0000

    Deploying to gh-pages from @ Khan/perseus@27126aa00b92ce1facd97abd38989e6981836e3f 🚀

---
+++ assets/input-number.stories-R3wt5hlK.js
@@ -0,0 +1,64 @@
+import{j as a}from"./jsx-runtime-FVsy8kgq.js";import{R as o}from"./renderer-with-debug-ui-09q14AaH.js";import"./index-TT1qJ6UJ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./choice-BQ3Yn5CQ.js";import"./index-6h5t6F0w.js";import"./index-awljIyHI.js";import"./react-router-dom-W_e8xVUu.js";import"./index-tvtfaFq4.js";import"./index-o3wWn3Y5.js";import"./index-deFLJwr4.js";import"./index-h_CiYGGb.js";import"./index-ngddCaVG.js";import"./index-rfN0X25E.js";import"./index-xuPsLuPk.js";import"./index-_pgwEZ-g.js";import"./index-qUyqkRvh.js";import"./index-BPSWBbj5.js";import"./index-7vsPyIck.js";import"./index-YCTzZMik.js";import"./index-CkAxGj88.js";import"./Popper-kGnKOid7.js";import"./index-Dfd6auV6.js";import"./index-dnMhQZ-1.js";import"./i18n-context-P5sgPFep.js";import"./icon-R5gZamfG.js";import"./perseus-api-Nq3s7IMx.js";import"./index-0C4KXdeC.js";import"./invariant-bu5zBsRS.js";import"./stub-tag-editor-74YQ4o9G.js";import"./text-list-editor-Jz35fIN1.js";import"./jquery-yG1GhClm.js";import"./media-queries-gbofTbz-.js";import"./constants-iPV6vHZm.js";import"./choice-icon-S0OeoyAm.js";import"./inline-icon-NjJlm7d0.js";import"./icon-paths-5JCXzGsq.js";import"./focus-ring-2b4ybtc7.js";import"./option-status-9of4QS8W.js";import"./article-renderer-hxa78u3L.js";import"./util-XR-uqOh-.js";import"./version-akiLXZts.js";import"./prop-types-SKtvHVtl.js";import"./mobile-keypad-wPUeR8mM.js";import"./index-J2t_5nK1.js";import"./tabbar-WYCYDof0.js";import"./item-X9tjzx12.js";import"./index-WNT3sUKf.js";import"./button-assets-txIojR3b.js";import"./keypad-button-rKskH7oM.js";import"./operators-page-C_EsmjPW.js";import"./navigation-pad-ss7r0FKW.js";import"./key-translator-Bl2XtQm9.js";import"./_commonjs-dynamic-modules-h-SxKiO4.js";import"./enums-x5qaTru7.js";import"./renderer-92xIG6DE.js";import"./asset-context-I7yIqWki.js";import"./svg-image-h96M64n1.js";import"./dependencies-d8cZibFS.js";import"./fixed-to-responsive-AkRQxH3P.js";import"./client-rbWgHzHN.js";import"./image-loader-3HliPbpF.js";import"./tex-6yhnhbtf.js";import"./zoomable-CA1NzpZD.js";import"./zoomable-tex-hbM7rxrP.js";import"./lint-kpBgzQ8K.js";import"./index-uzeeKkRr.js";import"./math-input-w9J4jE0k.js";import"./input-with-examples-yYn1C4Y7.js";import"./text-input-15gEhfDF.js";import"./index-QCAhLhLD.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip--mhQefx0.js";import"./simple-keypad-input-LEkrmlga.js";import"./base-radio-S1z7zWx7.js";import"./shared-4TdECMLk.js";import"./choice-none-above-q3Wgs6Jz.js";import"./phet-simulation-AAtfWFw-.js";import"./arrow-square-out-bold-090vdhrx.js";import"./answer-choices-rMWiQP61.js";import"./button-group-nsoLlHtM.js";import"./graph-LHyvBqdA.js";import"./index-IIMKO4_x.js";import"./tiny-invariant-bHgPayXn.js";import"./bundle.esm-7jAAHVxW.js";import"./hud-4BOtaFc1.js";import"./multi-button-group-atHe8dYZ.js";import"./number-input-csbDd4Oh.js";import"./range-input-VXCy7Y7P.js";import"./marker-ilX0RTyC.js";import"./answer-pill-N5s4FM71.js";import"./sortable-Tnv_EkCu.js";import"./video-transcript-link-MKIRfGVo.js";import"./side-by-side-kGWaKY6T.js";import"./perseus-item-2A0iWC1_.js";import"./hints-renderer-Fox2JPII.js";import"./register-all-widgets-for-testing-9rPr5AQ-.js";const f={content:`Denis baked a peach pie and cut it into $3$ equal-sized pieces.  Denis's dad eats $1$ section of the pie.  
+
+**What fraction of the pie did Denis's dad eat?**  
+![](https://ka-perseus-graphie.s3.amazonaws.com/74a2b7583a2c26ebfb3ad714e29867541253fc97.png)    
+[[☃ input-number 1]]  
+
+
+
+`,images:{"https://ka-perseus-graphie.s3.amazonaws.com/74a2b7583a2c26ebfb3ad714e29867541253fc97.png":{width:200,height:200}},widgets:{"input-number 1":{version:{major:0,minor:0},type:"input-number",graded:!0,alignment:"default",options:{maxError:.1,inexact:!1,value:.3333333333333333,simplify:"optional",answerType:"rational",size:"normal",rightAlign:!0}}}},$={content:`A washing machine is being redesigned to handle a greater volume of water.  One part is a pipe with a radius of $3 \\,\\text{cm}$ and a length of $11\\,\\text{cm}$.  It gets replaced with a pipe of radius $4\\,\\text{cm}$, and the same length. 
+
+**How many more cubic centimeters of water can the new pipe hold?**
+
+ [[☃ input-number 1]] $\\text{cm}^3$`,images:Object.freeze({}),widgets:{"input-number 1":{type:"input-number",graded:!0,options:{maxError:.1,inexact:!1,value:241.90263432641407,simplify:"required",answerType:"pi",size:"normal"}}}},w={content:`Akshat works in a hospital lab.
+
+To project blood quantities, he wants to know the probability that more than $1$ of the next $7$ donors will have type-A blood. From his previous work, Sorin knows that $\\dfrac14$ of donors have type-A blood.
+
+Akshat uses a computer to produce many samples that simulate the next $7$ donors. The first $8$ samples are shown in the table below where "$\\text{\\red{A}}$" represents a donor *with* type-A blood, and "$\\text{\\blue{Z}}$" represents a donor *without* type-A blood.
+
+**Based on the samples below, estimate the probability that  more than $1$ of the next $7$ donors will have type-A blood.** If necessary, round your answer to the nearest hundredth. [[☃ input-number 1]]
+
+*Note: This a small sample to practice with. A larger sample could give a much better estimate.*
+
+ | Sample |
+:-: | :-: | 
+$1$ | $\\text{\\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\red{A}, \\blue{Z}, \\blue{Z}}$
+$2$ | $\\text{\\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}}$
+$3$ | $\\text{\\blue{Z}, \\blue{Z}, \\red{A}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}}$
+$4$ | $\\text{\\red{A}, \\red{A}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}}$
+$5$ | $\\text{\\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\red{A}, \\red{A}}$
+$6$ | $\\text{\\blue{Z}, \\red{A}, \\red{A}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}}$
+$7$ | $\\text{\\blue{Z}, \\red{A}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\red{A}, \\blue{Z}}$
+$8$ | $\\text{\\blue{Z}, \\blue{Z}, \\blue{Z}, \\blue{Z}, \\red{A}, \\blue{Z}, \\blue{Z}}$
+
+`,images:Object.freeze({}),widgets:{"input-number 1":{type:"input-number",graded:!0,options:{maxError:.1,inexact:!1,value:.5,simplify:"optional",answerType:"percent",size:"small"}}}},ut={title:"Perseus/Widgets/InputNumber",argTypes:{maxError:{control:{type:"range",min:0,max:1,step:.1}},inexact:{control:{type:"boolean"}},value:{control:{type:"number"}},simplify:{control:{type:"select",options:["required","optional","enforced"]}},answerType:{control:{type:"select",options:["number","decimal","integer","rational","improper","mixed","percent","pi"]}},size:{control:{type:"select",options:["normal","small"]}},rightAlign:{control:{type:"boolean"}}}},m=(e,t,v)=>{const l=e.widgets[t];return{...e,widgets:{[t]:{...l,options:{...l.options,...v}}}}},r=e=>{const t=m(f,"input-number 1",e);return a(o,{question:t})};r.args=f.widgets["input-number 1"].options;const n=e=>{const t=m($,"input-number 1",e);return a(o,{question:t})};n.args=$.widgets["input-number 1"].options;const i=e=>{const t=m(w,"input-number 1",e);return a(o,{question:t})};i.args=w.widgets["input-number 1"].options;r.__docgenInfo={description:"",methods:[],displayName:"Rational",props:{answerType:{required:!1,tsType:{name:"union",raw:`| "number"
+| "decimal"
+| "integer"
+| "rational"
+| "improper"
+| "mixed"
+| "percent"
+| "pi"`,elements:[{name:"literal",value:'"number"'},{name:"literal",value:'"decimal"'},{name:"literal",value:'"integer"'},{name:"literal",value:'"rational"'},{name:"literal",value:'"improper"'},{name:"literal",value:'"mixed"'},{name:"literal",value:'"percent"'},{name:"literal",value:'"pi"'}]},description:""},inexact:{required:!1,tsType:{name:"boolean"},description:""},maxError:{required:!1,tsType:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}]},description:""},rightAlign:{required:!1,tsType:{name:"boolean"},description:""},simplify:{required:!0,tsType:{name:"union",raw:'"required" | "optional" | "enforced"',elements:[{name:"literal",value:'"required"'},{name:"literal",value:'"optional"'},{name:"literal",value:'"enforced"'}]},description:""},size:{required:!0,tsType:{name:"union",raw:'"normal" | "small"',elements:[{name:"literal",value:'"normal"'},{name:"literal",value:'"small"'}]},description:""},value:{required:!0,tsType:{name:"union",raw:"string | number",elements:[{name:"string"},{name:"number"}]},description:""},customKeypad:{required:!1,tsType:{name:"boolean"},description:""}}};n.__docgenInfo={description:"",methods:[],displayName:"PiSimplify",props:{answerType:{required:!1,tsType:{name:"union",raw:`| "number"
+| "decimal"
+| "integer"
+| "rational"
+| "improper"
+| "mixed"
+| "percent"
+| "pi"`,elements:[{name:"literal",value:'"number"'},{name:"literal",value:'"decimal"'},{name:"literal",value:'"integer"'},{name:"literal",value:'"rational"'},{name:"literal",value:'"improper"'},{name:"literal",value:'"mixed"'},{name:"literal",value:'"percent"'},{name:"literal",value:'"pi"'}]},description:""},inexact:{required:!1,tsType:{name:"boolean"},description:""},maxError:{required:!1,tsType:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}]},description:""},rightAlign:{required:!1,tsType:{name:"boolean"},description:""},simplify:{required:!0,tsType:{name:"union",raw:'"required" | "optional" | "enforced"',elements:[{name:"literal",value:'"required"'},{name:"literal",value:'"optional"'},{name:"literal",value:'"enforced"'}]},description:""},size:{required:!0,tsType:{name:"union",raw:'"normal" | "small"',elements:[{name:"literal",value:'"normal"'},{name:"literal",value:'"small"'}]},description:""},value:{required:!0,tsType:{name:"union",raw:"string | number",elements:[{name:"string"},{name:"number"}]},description:""},customKeypad:{required:!1,tsType:{name:"boolean"},description:""}}};i.__docgenInfo={description:"",methods:[],displayName:"Percent",props:{answerType:{required:!1,tsType:{name:"union",raw:`| "number"
+| "decimal"
+| "integer"
+| "rational"
+| "improper"
+| "mixed"
+| "percent"
+| "pi"`,elements:[{name:"literal",value:'"number"'},{name:"literal",value:'"decimal"'},{name:"literal",value:'"integer"'},{name:"literal",value:'"rational"'},{name:"literal",value:'"improper"'},{name:"literal",value:'"mixed"'},{name:"literal",value:'"percent"'},{name:"literal",value:'"pi"'}]},description:""},inexact:{required:!1,tsType:{name:"boolean"},description:""},maxError:{required:!1,tsType:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}]},description:""},rightAlign:{required:!1,tsType:{name:"boolean"},description:""},simplify:{required:!0,tsType:{name:"union",raw:'"required" | "optional" | "enforced"',elements:[{name:"literal",value:'"required"'},{name:"literal",value:'"optional"'},{name:"literal",value:'"enforced"'}]},description:""},size:{required:!0,tsType:{name:"union",raw:'"normal" | "small"',elements:[{name:"literal",value:'"normal"'},{name:"literal",value:'"small"'}]},description:""},value:{required:!0,tsType:{name:"union",raw:"string | number",elements:[{name:"string"},{name:"number"}]},description:""},customKeypad:{required:!1,tsType:{name:"boolean"},description:""}}};var p,s,u;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`(args: InputNumberOptions): React.ReactElement => {
+  const question = updateWidgetOptions(question1, "input-number 1", args);
+  return <RendererWithDebugUI question={question} />;
+}`,...(u=(s=r.parameters)==null?void 0:s.docs)==null?void 0:u.source}}};var d,c,b;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`(args: InputNumberOptions): React.ReactElement => {
+  const question = updateWidgetOptions(question2, "input-number 1", args);
+  return <RendererWithDebugUI question={question} />;
+}`,...(b=(c=n.parameters)==null?void 0:c.docs)==null?void 0:b.source}}};var g,y,h;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`(args: InputNumberOptions): React.ReactElement => {
+  const question = updateWidgetOptions(question3, "input-number 1", args);
+  return <RendererWithDebugUI question={question} />;
+}`,...(h=(y=i.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};const dt=["Rational","PiSimplify","Percent"];export{i as Percent,n as PiSimplify,r as Rational,dt as __namedExportsOrder,ut as default};
---
+++ assets/register-all-widgets-for-testing-9rPr5AQ-.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-hxa78u3L.js";import{r,a as s}from"./renderer-92xIG6DE.js";const a=()=>{r(e),s()};export{a as r};
