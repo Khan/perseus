70e13470b43f0a5af10f24dc84be78e1edaac0a6
Ben Christel <benchristel@khanacademy.org>
2024-11-15 15:23:52 -0800

    Add and pass one regression test for radio widget parser

---
+++ packages/perseus/src/util/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
@@ -0,0 +1,16 @@
+import * as fs from "fs"
+import {join} from "path"
+import {parseAndTypecheckPerseusItem} from "../index";
+
+const dataFiles = fs.readdirSync(join(__dirname, "data"))
+
+describe("parseAndTypecheckPerseusItem", () => {
+    it.each(dataFiles)("accepts data/%s", (filename) => {
+        const json = fs.readFileSync(join(__dirname, "data", filename), "utf-8")
+        const result = parseAndTypecheckPerseusItem(json)
+
+        // This strange-looking assertion style results in the failure message
+        // being printed if parsing fails, so the test is easier to debug.
+        expect(result).toEqual(expect.objectContaining({type: "success"}))
+    })
+})
