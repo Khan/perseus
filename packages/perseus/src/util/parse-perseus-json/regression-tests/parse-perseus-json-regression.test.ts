f9f6ad3797f6ce888f27ebf8c26dfd98af4881ca
Ben Christel <benchristel@khanacademy.org>
2024-11-15 15:45:52 -0800

    Fix lint

--- packages/perseus/src/util/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
+++ packages/perseus/src/util/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
@@ -1,16 +1,20 @@
-import * as fs from "fs"
-import {join} from "path"
+import * as fs from "fs";
+import {join} from "path";
+
 import {parseAndTypecheckPerseusItem} from "../index";
 
-const dataFiles = fs.readdirSync(join(__dirname, "data"))
+const dataFiles = fs.readdirSync(join(__dirname, "data"));
 
 describe("parseAndTypecheckPerseusItem", () => {
     it.each(dataFiles)("accepts data/%s", (filename) => {
-        const json = fs.readFileSync(join(__dirname, "data", filename), "utf-8")
-        const result = parseAndTypecheckPerseusItem(json)
+        const json = fs.readFileSync(
+            join(__dirname, "data", filename),
+            "utf-8",
+        );
+        const result = parseAndTypecheckPerseusItem(json);
 
         // This strange-looking assertion style results in the failure message
         // being printed if parsing fails, so the test is easier to debug.
-        expect(result).toEqual(expect.objectContaining({type: "success"}))
-    })
-})
+        expect(result).toEqual(expect.objectContaining({type: "success"}));
+    });
+});
