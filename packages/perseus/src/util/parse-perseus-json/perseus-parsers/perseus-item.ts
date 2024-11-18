e5d82f1e36009c3a75fc2c6635c19576050a7f1f
Ben Christel <benchristel@khanacademy.org>
2024-11-18 10:26:02 -0800

    Revert changes to PerseusItem parser

--- packages/perseus/src/util/parse-perseus-json/perseus-parsers/perseus-item.ts
+++ packages/perseus/src/util/parse-perseus-json/perseus-parsers/perseus-item.ts
@@ -3,14 +3,11 @@ import {
     any,
     array,
     boolean,
-    constant,
     enumeration,
     number,
     object,
     record,
-    union,
 } from "../general-purpose-parsers";
-import {composeParsers} from "../general-purpose-parsers/compose-parsers";
 
 import {parseHint} from "./hint";
 import {parsePerseusRenderer} from "./perseus-renderer";
@@ -21,13 +18,7 @@ import type {Parser} from "../parser-types";
 export const parsePerseusItem: Parser<PerseusItem> = object({
     question: parsePerseusRenderer,
     hints: array(parseHint),
-    answerArea: record(
-        enumeration(...ItemExtras),
-        composeParsers(
-            union(boolean).or(constant("multiple")).parser,
-            (val, ctx) => ctx.success(Boolean(val)),
-        ),
-    ),
+    answerArea: record(enumeration(...ItemExtras), boolean),
     itemDataVersion: object({
         major: number,
         minor: number,
