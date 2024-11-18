2420ea31377deb7da663c8473e03354c84fa4303
Tamara <tamara@khanacademy.org>
2024-11-18 10:51:54 -0600

    Split out validation logic to new function

---
+++ packages/perseus/src/widgets/sorter/validate-sorter.ts
@@ -0,0 +1,26 @@
+import type {PerseusScore} from "../../types";
+import type {
+    PerseusSorterRubric,
+    PerseusSorterUserInput,
+} from "../../validation.types";
+
+function validateSorter(
+    userInput: PerseusSorterUserInput,
+    rubric: PerseusSorterRubric,
+): Extract<PerseusScore, {type: "invalid"}> | null {
+    // If the sorter widget hasn't been changed yet, we treat it as "empty" which
+    // prevents the "Check" button from becoming active. We want the user
+    // to make a change before trying to move forward. This makes an
+    // assumption that the initial order isn't the correct order! However,
+    // this should be rare if it happens, and interacting with the list
+    // will enable the button, so they won't be locked out of progressing.
+    if (!userInput.changed) {
+        return {
+            type: "invalid",
+            message: null,
+        };
+    }
+    return null;
+}
+
+export default validateSorter;
