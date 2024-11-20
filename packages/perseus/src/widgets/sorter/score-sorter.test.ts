5327ece59893f47e2b6d804845a09e69fb8a303e
Tamara <tamara@khanacademy.org>
2024-11-20 15:43:33 -0600

    Add arrange, act, assert comments

--- packages/perseus/src/widgets/sorter/score-sorter.test.ts
+++ packages/perseus/src/widgets/sorter/score-sorter.test.ts
@@ -8,6 +8,7 @@ import type {
 
 describe("scoreSorter", () => {
     it("is correct when the user input values are in the order defined in the rubric", () => {
+        // Arrange
         const userInput: PerseusSorterUserInput = {
             options: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
             changed: true,
@@ -15,11 +16,16 @@ describe("scoreSorter", () => {
         const rubric: PerseusSorterRubric = {
             correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
         };
+
+        // Act
         const result = scoreSorter(userInput, rubric);
+
+        // Assert
         expect(result).toHaveBeenAnsweredCorrectly();
     });
 
     it("is incorrect when the user input values are not in the order defined in the rubric", () => {
+        // Arrange
         const userInput: PerseusSorterUserInput = {
             options: ["$15$ grams", "$55$ grams", "$0.005$ kilograms"],
             changed: true,
@@ -27,7 +33,11 @@ describe("scoreSorter", () => {
         const rubric: PerseusSorterRubric = {
             correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
         };
+
+        // Act
         const result = scoreSorter(userInput, rubric);
+
+        // Assert
         expect(result).toHaveBeenAnsweredIncorrectly();
     });
 
@@ -46,6 +56,7 @@ describe("scoreSorter", () => {
             correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
         };
 
+        // Act
         const result = scoreSorter(userInput, rubric);
 
         // Assert
--- packages/perseus/src/widgets/sorter/validate-sorter.test.ts
+++ packages/perseus/src/widgets/sorter/validate-sorter.test.ts
@@ -4,22 +4,30 @@ import type {PerseusSorterUserInput} from "../../validation.types";
 
 describe("validateSorter", () => {
     it("is invalid when the user has not made any changes", () => {
+        // Arrange
         const userInput: PerseusSorterUserInput = {
             options: ["$15$ grams", "$55$ grams", "$0.005$ kilograms"],
             changed: false,
         };
 
+        // Act
         const result = validateSorter(userInput);
+
+        // Assert
         expect(result).toHaveInvalidInput();
     });
 
     it("returns null when the user has made any changes", () => {
+        // Arrange
         const userInput: PerseusSorterUserInput = {
             options: ["$55$ grams", "$0.005$ kilograms", "$15$ grams"],
             changed: true,
         };
 
+        // Act
         const result = validateSorter(userInput);
+
+        // Assert
         expect(result).toBeNull();
     });
 });
