74bf0e65ca77a4258bbaf24deb2164dfd30db825
Tamara <tamara@khanacademy.org>
2024-11-20 16:09:16 -0600

    Add comment to validation function

--- packages/perseus/src/widgets/orderer/validate-orderer.ts
+++ packages/perseus/src/widgets/orderer/validate-orderer.ts
@@ -1,6 +1,12 @@
 import type {PerseusScore} from "../../types";
 import type {PerseusOrdererUserInput} from "../../validation.types";
 
+/**
+ * Checks user inputfrom the orderer widget to see if the user has started
+ * ordering the options, making the widget scorable.
+ * @param userInput
+ * @see `scoreOrderer` for more details.
+ */
 function validateOrderer(
     userInput: PerseusOrdererUserInput,
 ): Extract<PerseusScore, {type: "invalid"}> | null {
