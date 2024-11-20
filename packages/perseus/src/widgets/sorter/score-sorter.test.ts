e602bfa8a8f599ef3ea92b722fcc091d355345e5
Tamara <tamara@khanacademy.org>
2024-11-20 15:14:30 -0600

    Confirm scoring handles validation responses

--- packages/perseus/src/widgets/sorter/score-sorter.test.ts
+++ packages/perseus/src/widgets/sorter/score-sorter.test.ts
@@ -1,4 +1,5 @@
 import scoreSorter from "./score-sorter";
+import * as SorterValidator from "./validate-sorter";
 
 import type {
     PerseusSorterRubric,
@@ -28,5 +29,50 @@ describe("scoreSorter", () => {
         };
         const result = scoreSorter(userInput, rubric);
         expect(result).toHaveBeenAnsweredIncorrectly();
+    });
+
+    it("should score if validator passes", () => {
+        // Arrange
+        // Mock validator saying "all good"
+        const mockValidate = jest
+            .spyOn(SorterValidator, "default")
+            .mockReturnValue(null);
+
+        const userInput: PerseusSorterUserInput = {
+            options: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
+            changed: true,
+        };
+        const rubric: PerseusSorterRubric = {
+            correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
+        };
+
+        // Act
+        const score = scoreSorter(userInput, rubric);
+
+        // Assert
+        expect(mockValidate).toHaveBeenCalled();
+        expect(score).toHaveBeenAnsweredCorrectly();
+    });
+
+    it("should abort if validator returns invalid", () => {
+        // Arrange
+        // Mock validator saying input is invalid
+        const mockValidate = jest
+            .spyOn(SorterValidator, "default")
+            .mockReturnValue({type: "invalid", message: null});
+
+        const userInput: PerseusSorterUserInput = {
+            options: ["$15$ grams", "$55$ grams", "$0.005$ kilograms"],
+            changed: true,
+        };
+        const rubric: PerseusSorterRubric = {
+            correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
+        };
+
+        const score = scoreSorter(userInput, rubric);
+
+        // Assert
+        expect(mockValidate).toHaveBeenCalled();
+        expect(score).toHaveInvalidInput();
     });
 });
