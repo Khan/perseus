d0e7a0383002414d874297ccc88bf5c9863b4ea5
Sarah Third <sarahthird@khanacademy.org>
2024-11-20 09:26:12 -0800

    Fixing issue with Input Numbers that have a value of 0  (#1888)
    ## Summary:
    This PR fixes an issue with the Input Number Conversion project that specifically pertains to widgets that have a value of 0, as it results in a falsy situation. As a result of this issue, exercises that contain the offending widget settings would fail to render. 
    
    This PR updates the logic to a safer key, and ensures that it is not undefined. 
    
    # Test:
    - Manual testing both in Storybook and Webapp to confirm that failing cases now render properly.
    
    Author: SonicScrewdriver
    
    Reviewers: jeremywiebe, mark-fitzgerald
    
    Required Reviewers:
    
    Approved By: mark-fitzgerald
    
    Checks: ✅ gerald, ✅ Cypress (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ gerald, ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), 🚫 Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ gerald, ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/1888

--- packages/perseus/src/widgets/numeric-input/numeric-input.tsx
+++ packages/perseus/src/widgets/numeric-input/numeric-input.tsx
@@ -345,10 +345,10 @@ const propsTransform = function (
 const propUpgrades = {
     /* c8 ignore next */
     "1": (initialProps: any): PerseusNumericInputWidgetOptions => {
-        // If the initialProps has a value, it means we're upgrading from
+        // If the initialProps has simplify, it means we're upgrading from
         // input-number to numeric-input. In this case, we need to upgrade
         // the widget options accordingly.
-        if (initialProps.value) {
+        if (initialProps.simplify !== undefined) {
             // If the answerType is not number or percent, we need to provide
             // the answer form for the numeric-input widget
             const provideAnswerForm =
