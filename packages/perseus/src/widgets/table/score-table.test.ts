f5ef6ddc9ea78e0d2abe1081689ee1185683ce16
Jeremy Wiebe <jeremy@khanacademy.org>
2024-11-20 11:54:38 -0800

    Table: Validation/Scoring add unit tests (#1881)
    ## Summary:
    
    Adding two unit tests to ensure that our validation is called and handled properly in the scoring function.
    
    Issue: LEMS-2598
    
    ## Test plan:
    
    `yarn test`
    `yarn typecheck`
    
    Author: jeremywiebe
    
    Reviewers: Myranae, handeyeco, jeremywiebe
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ gerald
    
    Pull Request URL: https://github.com/Khan/perseus/pull/1881

--- packages/perseus/src/widgets/table/score-table.test.ts
+++ packages/perseus/src/widgets/table/score-table.test.ts
@@ -1,6 +1,7 @@
 import {mockStrings} from "../../strings";
 
 import scoreTable from "./score-table";
+import * as TableValidator from "./validate-table";
 
 import type {
     PerseusTableRubric,
@@ -8,6 +9,58 @@ import type {
 } from "../../validation.types";
 
 describe("scoreTable", () => {
+    it("should be correctly answerable if validation passes", function () {
+        // Arrange
+        const mockValidator = jest
+            .spyOn(TableValidator, "default")
+            .mockReturnValue(null);
+
+        const userInput: PerseusTableUserInput = [
+            ["1", "2"],
+            ["3", "4"],
+        ];
+
+        const rubric: PerseusTableRubric = {
+            answers: [
+                ["1", "2"],
+                ["3", "4"],
+            ],
+        };
+
+        // Act
+        const score = scoreTable(userInput, rubric, mockStrings);
+
+        // Assert
+        expect(mockValidator).toHaveBeenCalledWith(userInput);
+        expect(score).toHaveBeenAnsweredCorrectly();
+    });
+
+    it("should return 'empty' result if validation fails", function () {
+        // Arrange
+        const mockValidator = jest
+            .spyOn(TableValidator, "default")
+            .mockReturnValue({type: "invalid", message: null});
+
+        const userInput: PerseusTableUserInput = [
+            ["1", "2"],
+            ["3", "4"],
+        ];
+
+        const rubric: PerseusTableRubric = {
+            answers: [
+                ["1", "2"],
+                ["3", "4"],
+            ],
+        };
+
+        // Act
+        const score = scoreTable(userInput, rubric, mockStrings);
+
+        // Assert
+        expect(mockValidator).toHaveBeenCalledWith(userInput);
+        expect(score).toHaveInvalidInput();
+    });
+
     it("is invalid if there is an empty cell", () => {
         // Arrange
         const userInput: PerseusTableUserInput = [
