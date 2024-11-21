d5bb04c211e321bb34d9a9085514a7f94e844222
Tamara <tamara@khanacademy.org>
2024-11-21 10:26:04 -0600

    Clarify valid state in doc comment

--- packages/perseus/src/widgets/dropdown/validate-dropdown.ts
+++ packages/perseus/src/widgets/dropdown/validate-dropdown.ts
@@ -3,6 +3,7 @@ import type {PerseusDropdownUserInput} from "../../validation.types";
 
 /**
  * Checks if the user has selected an item from the dropdown before scoring.
+ * This is shown with a userInput value / index other than 0.
  * @param userInput
  * @see `scoreDropdown` for details on scoring
  */
