5e9df85c5a0ca897b016237e067a0eb02e60fe43
Danielle Whyte <daniellewhyte@khanacademy.org>
2024-11-21 12:47:12 -0600

    add new story

--- packages/perseus/src/widgets/dropdown/dropdown.stories.tsx
+++ packages/perseus/src/widgets/dropdown/dropdown.stories.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import {RendererWithDebugUI} from "../../../../../testing/renderer-with-debug-ui";
 
-import {question1} from "./dropdown.testdata";
+import {dropdownWithVisibleLabel, question1} from "./dropdown.testdata";
 
 export default {
     title: "Perseus/Widgets/Dropdown",
@@ -12,4 +12,10 @@ type StoryArgs = Record<any, any>;
 
 export const Question1 = (args: StoryArgs): React.ReactElement => {
     return <RendererWithDebugUI question={question1} />;
+};
+
+export const DropdownWithVisibleLabel = (
+    args: StoryArgs,
+): React.ReactElement => {
+    return <RendererWithDebugUI question={dropdownWithVisibleLabel} />;
 };
