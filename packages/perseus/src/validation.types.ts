47f4c3f10d5d64ac90fa8bd9b1610d1714d9c0be
Tamara <tamara@khanacademy.org>
2024-11-21 14:46:53 -0600

    Remove unneeded import

--- packages/perseus/src/validation.types.ts
+++ packages/perseus/src/validation.types.ts
@@ -37,7 +37,6 @@ import type {
     PerseusGroupWidgetOptions,
     PerseusMatcherWidgetOptions,
     PerseusMatrixWidgetAnswers,
-    PerseusNumberLineWidgetOptions,
     PerseusNumericInputAnswer,
     PerseusOrdererWidgetOptions,
     PerseusPlotterWidgetOptions,
