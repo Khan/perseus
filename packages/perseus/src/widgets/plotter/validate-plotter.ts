c75de6df23bf3ded7303ffd1cc2bd77f78d55ed2
Tamara <tamara@khanacademy.org>
2024-11-22 09:24:21 -0600

    Remove unnecessary part of doc

--- packages/perseus/src/widgets/plotter/validate-plotter.ts
+++ packages/perseus/src/widgets/plotter/validate-plotter.ts
@@ -11,8 +11,7 @@ const {deepEq} = Util;
 /**
  * Checks user input to confirm it is not the same as the starting values for the graph.
  * This means the user has modified the graph, and the question can be scored.
- * @param userInput
- * @param validationData
+ *
  * @see 'scorePlotter' for more details on scoring.
  */
 function validatePlotter(
