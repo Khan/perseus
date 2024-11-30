1a52198e3a7ad1d99878629fcb19baee7d9e0eaa
Jeremy Wiebe <jeremy@khanacademy.org>
2024-11-29 19:01:35 -0800

    Ready!

--- .eslintrc.js
+++ .eslintrc.js
@@ -309,10 +309,10 @@ module.exports = {
         /**
          * react
          */
-        "react/no-string-refs": "off", // on in recommended, but #legacy-code
-        "react/no-find-dom-node": "off", // on in recommended, but #legacy-code
-        "react/display-name": "off", // on in recommended, but we have many components that don't have a displayName
-        "react/no-unescaped-entities": "off", // on in recommended, but #legacy-code
+        "react/no-string-refs": "off", // on in react/recommended, but we have #legacy-code
+        "react/no-find-dom-node": "off", // on in react/recommended, but we have #legacy-code
+        "react/display-name": "off", // on in react/recommended, but doesn't seem that useful to fix
+        "react/no-unescaped-entities": "off", // on in react/recommended, but we have #legacy-code
         // This rule results in false-positives when using some types of React
         // components (such as functional components or hooks). Since
         // TypeScript is already checking that components are only using props
