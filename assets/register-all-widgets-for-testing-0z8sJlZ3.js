46c6291914c6854290dbc41df68882ec0a425ad6
SonicScrewdriver <sarahthird@gmail.com>
2024-12-03 21:24:04 +0000

    Deploying to gh-pages from @ Khan/perseus@1d2b4e7bf13ec299f92da43024889702230014fa 🚀

---
+++ assets/register-all-widgets-for-testing-0z8sJlZ3.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-m2DxR4DL.js";import{r,a as s}from"./renderer-IhFUya5i.js";const a=()=>{r(e),s()};export{a as r};
--- assets/register-all-widgets-for-testing-IqvoTTry.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-jwwMfp2y.js";import{r,a as s}from"./renderer-2Ae1Nml7.js";const a=()=>{r(e),s()};export{a as r};
