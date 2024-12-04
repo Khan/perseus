772674b92ef81e02dc0b86654cffad1d8688c386
Ben Christel <benchristel@khanacademy.org>
2024-12-04 12:23:26 -0800

    Fix lint

--- packages/perseus/src/util/parse-perseus-json/general-purpose-parsers/discriminated-union.test.ts
+++ packages/perseus/src/util/parse-perseus-json/general-purpose-parsers/discriminated-union.test.ts
@@ -1,9 +1,10 @@
-import {discriminatedUnion} from "./discriminated-union";
-import {constant} from "./constant";
-import {object} from "./object";
-import {number} from "./number";
 import {parse} from "../parse";
 import {failure, success} from "../result";
+
+import {constant} from "./constant";
+import {discriminatedUnion} from "./discriminated-union";
+import {number} from "./number";
+import {object} from "./object";
 
 describe("a discriminatedUnion with one variant", () => {
     const unionParser = discriminatedUnion(
@@ -14,25 +15,25 @@ describe("a discriminatedUnion with one variant", () => {
     it("parses a valid value", () => {
         const input = {type: "ok", value: 3};
 
-        expect(parse(input, unionParser)).toEqual(success(input))
-    })
+        expect(parse(input, unionParser)).toEqual(success(input));
+    });
 
     it("rejects a value with the wrong `type`", () => {
         const input = {type: "bad", value: 3};
 
         expect(parse(input, unionParser)).toEqual(
             failure(`At (root).type -- expected "ok", but got "bad"`),
-        )
-    })
+        );
+    });
 
     it("rejects a value with a valid type but wrong fields", () => {
         const input = {type: "ok", value: "foobar"};
 
         expect(parse(input, unionParser)).toEqual(
             failure(`At (root).value -- expected number, but got "foobar"`),
-        )
-    })
-})
+        );
+    });
+});
 
 describe("a discriminatedUnion with two variants", () => {
     const unionParser = discriminatedUnion(
@@ -46,35 +47,38 @@ describe("a discriminatedUnion with two variants", () => {
     it("parses a valid rectangle", () => {
         const input = {type: "rectangle", width: 42};
 
-        expect(parse(input, unionParser)).toEqual(success(input))
-    })
+        expect(parse(input, unionParser)).toEqual(success(input));
+    });
 
     it("rejects a rectangle with no width", () => {
         const input = {type: "rectangle", radius: 99};
 
         expect(parse(input, unionParser)).toEqual(
-            failure(`At (root).width -- expected number, but got undefined`))
-    })
+            failure(`At (root).width -- expected number, but got undefined`),
+        );
+    });
 
     it("parses a valid circle", () => {
         const input = {type: "circle", radius: 7};
 
-        expect(parse(input, unionParser)).toEqual(success(input))
-    })
+        expect(parse(input, unionParser)).toEqual(success(input));
+    });
 
     it("rejects a circle with no radius", () => {
         const input = {type: "circle", width: 99};
 
         expect(parse(input, unionParser)).toEqual(
-            failure(`At (root).radius -- expected number, but got undefined`)
-        )
-    })
+            failure(`At (root).radius -- expected number, but got undefined`),
+        );
+    });
 
     it("rejects a value with an unrecognized `type`", () => {
         const input = {type: "triangle", width: -1, radius: 99};
 
         expect(parse(input, unionParser)).toEqual(
-            failure(`At (root).type -- expected "rectangle", but got "triangle"`)
-        )
-    })
-})
+            failure(
+                `At (root).type -- expected "rectangle", but got "triangle"`,
+            ),
+        );
+    });
+});
