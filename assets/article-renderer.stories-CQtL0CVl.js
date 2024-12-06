0ccf1ead0aca4a1866a82b2e44c06c26ac666d13
mark-fitzgerald <13896410+mark-fitzgerald@users.noreply.github.com>
2024-12-06 23:50:47 +0000

    Deploying to gh-pages from @ Khan/perseus@e22a931d987291258b66f2c80db3536970a4555d 🚀

---
+++ assets/article-renderer.stories-CQtL0CVl.js
@@ -0,0 +1,40 @@
+import{j as r}from"./jsx-runtime-63Ea5SlK.js";import{a as K}from"./mobile-keypad-Muuas9zV.js";import{a as b}from"./chunk-D5ZWXAHU-vR62DWf0.js";import{s as p}from"./test-dependencies-96x8L18U.js";import{s as k,m as M,p as W,a as T,b as D}from"./article-renderer.testdata-spwNviYy.js";import{A as m}from"./article-renderer-7XllJxYp.js";import{T as _}from"./test-keypad-context-wrapper-qNJz2rBH.js";import"./index-6oxdNXpR.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-awljIyHI.js";import"./index-9gkyvru-.js";import"./index-7-BESUpx.js";import"./index-J2t_5nK1.js";import"./index-0DbkllkJ.js";import"./index-deFLJwr4.js";import"./tabbar-WEwAggnz.js";import"./item-ct0mKlsr.js";import"./index-xfryX26Z.js";import"./react-router-dom-VIBHfbW6.js";import"./button-assets-ozecF1qE.js";import"./keypad-button-BlFICMi6.js";import"./operators-page-pCrK11a-.js";import"./navigation-pad-29vyLJ52.js";import"./v4-yQnnJER4.js";import"./register-all-widgets-for-testing-6dt8Z-9i.js";import"./renderer-BVUdCRvY.js";import"./version-akiLXZts.js";import"./perseus-error-l3K_anoI.js";import"./invariant-bu5zBsRS.js";import"./index-dnMhQZ-1.js";import"./jquery-yG1GhClm.js";import"./asset-context-H6Iqp7Gi.js";import"./i18n-context-fsWEgybQ.js";import"./svg-image-1rx1Oqc_.js";import"./dependencies-CP7Uh8Kq.js";import"./util-_iDv4tVD.js";import"./fixed-to-responsive-for_tVF1.js";import"./constants-iPV6vHZm.js";import"./index-Dfd6auV6.js";import"./client-Rb4DelHy.js";import"./inline-icon-6fh0Wu1y.js";import"./icon-paths-5JCXzGsq.js";import"./image-loader-qoercD2n.js";import"./tex-MX5FPdQh.js";import"./zoomable-_uYFBX1Q.js";import"./zoomable-tex-Jjwex-Ep.js";import"./perseus-api-1-ethIrW.js";import"./index-k-0mNqHS.js";import"./stub-tag-editor-qMhJW4bS.js";import"./text-list-editor-aj1SAzcA.js";import"./lint-CRWxUAIQ.js";import"./key-translator-hixns6dh.js";import"./prop-types-d_cUcR3L.js";import"./enums-x5qaTru7.js";import"./index-BIPwuHvF.js";import"./index-469F30Ub.js";import"./index-jxhLXBHb.js";import"./index-f-3iKkZU.js";import"./index-i1pBWAmI.js";import"./index-18qWGOW7.js";import"./Popper-Y5KDXl-P.js";import"./math-input-ILg8RrvB.js";import"./index-CyYhwSUV.js";import"./index-zFNZn_u0.js";import"./input-with-examples-PjHsVh2J.js";import"./text-input-e6f7u9S5.js";import"./index-V5gl6frG.js";import"./minus-bold-ONmDo3Ve.js";import"./tooltip-Lp8Mlfe3.js";import"./simple-keypad-input-7tb5_po1.js";import"./base-radio-7wWw7WIj.js";import"./media-queries-gbofTbz-.js";import"./shared-4TdECMLk.js";import"./choice-Xr-SKHrB.js";import"./index-vITVWi7a.js";import"./index-nqMmpXbO.js";import"./icon-7RFbyLiL.js";import"./choice-icon-9x4jcq5T.js";import"./focus-ring-qYEF-qqz.js";import"./option-status-8mB213vS.js";import"./choice-none-above-95ONlWyd.js";import"./phet-simulation-_7qOO4_B.js";import"./arrow-square-out-bold-090vdhrx.js";import"./answer-choices-cvQnMMeC.js";import"./button-group-G5CZaedn.js";import"./graph-58KDocyc.js";import"./index-smZ6iCr_.js";import"./tiny-invariant-bHgPayXn.js";import"./hud-ifw9Ofbw.js";import"./multi-button-group-QUVHbBcE.js";import"./number-input-ITajNf-m.js";import"./range-input-vPGmBWlE.js";import"./marker-Ir6SsImJ.js";import"./answer-pill-6DaqAiMP.js";import"./sortable-Uri8BYYT.js";import"./video-transcript-link-_gpkjmR8.js";const Er={title:"Perseus/Renderers/Article Renderer",argTypes:{useNewStyles:{control:"boolean"}}},t=e=>r.jsx(m,{json:k,dependencies:p,useNewStyles:e.useNewStyles}),o=e=>r.jsx(m,{json:M,dependencies:p,useNewStyles:e.useNewStyles}),i=({useNewStyles:e})=>r.jsx(m,{json:W,dependencies:p,useNewStyles:e}),s=({useNewStyles:e})=>r.jsx(_,{children:r.jsx(K.Consumer,{children:({keypadElement:a,setRenderer:c})=>r.jsx(m,{ref:d=>{c(d)},json:T,dependencies:p,useNewStyles:e,apiOptions:{isMobile:!0,customKeypad:!0,onFocusChange:b("onFocusChange")},keypadElement:a})})}),n=({useNewStyles:e})=>r.jsx(_,{children:r.jsx(K.Consumer,{children:({keypadElement:a,setRenderer:c})=>r.jsx(m,{ref:d=>{c(d)},json:D,dependencies:p,useNewStyles:e,apiOptions:{isMobile:!0,customKeypad:!0},keypadElement:a})})});t.__docgenInfo={description:"",methods:[],displayName:"ASingleSectionArticle"};o.__docgenInfo={description:"",methods:[],displayName:"BMultiSectionArticle"};i.__docgenInfo={description:"",methods:[],displayName:"PassageArticle"};s.__docgenInfo={description:"",methods:[],displayName:"ExpressionArticle"};n.__docgenInfo={description:"",methods:[],displayName:"MultiSectionedExpressionArticle"};var l,u,y;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`(args: {
+  useNewStyles;
+}): React.ReactElement => {
+  return <ArticleRenderer json={singleSectionArticle} dependencies={storybookDependenciesV2} useNewStyles={args.useNewStyles} />;
+}`,...(y=(u=t.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};var S,A,x;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`(args: {
+  useNewStyles;
+}): React.ReactElement => {
+  return <ArticleRenderer json={multiSectionArticle} dependencies={storybookDependenciesV2} useNewStyles={args.useNewStyles} />;
+}`,...(x=(A=o.parameters)==null?void 0:A.docs)==null?void 0:x.source}}};var g,N,j;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`({
+  useNewStyles
+}): any => <ArticleRenderer json={passageArticle} dependencies={storybookDependenciesV2} useNewStyles={useNewStyles} />`,...(j=(N=i.parameters)==null?void 0:N.docs)==null?void 0:j.source}}};var w,C,E;s.parameters={...s.parameters,docs:{...(w=s.parameters)==null?void 0:w.docs,source:{originalSource:`({
+  useNewStyles
+}): any => <TestKeypadContextWrapper>
+        <KeypadContext.Consumer>
+            {({
+      keypadElement,
+      setRenderer
+    }) => <ArticleRenderer ref={node => {
+      setRenderer(node);
+    }} json={articleSectionWithExpression} dependencies={storybookDependenciesV2} useNewStyles={useNewStyles} apiOptions={{
+      isMobile: true,
+      customKeypad: true,
+      onFocusChange: action("onFocusChange")
+    }} keypadElement={keypadElement} />}
+        </KeypadContext.Consumer>
+    </TestKeypadContextWrapper>`,...(E=(C=s.parameters)==null?void 0:C.docs)==null?void 0:E.source}}};var R,f,h;n.parameters={...n.parameters,docs:{...(R=n.parameters)==null?void 0:R.docs,source:{originalSource:`({
+  useNewStyles
+}): any => <TestKeypadContextWrapper>
+        <KeypadContext.Consumer>
+            {({
+      keypadElement,
+      setRenderer
+    }) => <ArticleRenderer ref={node => {
+      setRenderer(node);
+    }} json={multiSectionArticleWithExpression} dependencies={storybookDependenciesV2} useNewStyles={useNewStyles} apiOptions={{
+      isMobile: true,
+      customKeypad: true
+    }} keypadElement={keypadElement} />}
+        </KeypadContext.Consumer>
+    </TestKeypadContextWrapper>`,...(h=(f=n.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};const Rr=["ASingleSectionArticle","BMultiSectionArticle","PassageArticle","ExpressionArticle","MultiSectionedExpressionArticle"];export{t as ASingleSectionArticle,o as BMultiSectionArticle,s as ExpressionArticle,n as MultiSectionedExpressionArticle,i as PassageArticle,Rr as __namedExportsOrder,Er as default};
