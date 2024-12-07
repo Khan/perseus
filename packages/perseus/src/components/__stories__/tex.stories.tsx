4c5a90f7b9a28c7d828e5c61a9ff13cfab8cd7de
Jeremy Wiebe <jeremy@khanacademy.org>
2024-12-06 16:16:17 -0800

    Update snapshots

--- packages/perseus/src/components/__stories__/tex.stories.tsx
+++ packages/perseus/src/components/__stories__/tex.stories.tsx
@@ -1,12 +1,16 @@
+import {action} from "@storybook/addon-actions";
+
 import TeX from "../tex";
 
 import type {Meta, StoryObj} from "@storybook/react";
 
-const meta: Meta = {
+const meta: Meta<typeof TeX> = {
     title: "Perseus/Components/Tex",
     component: TeX,
     args: {
         children: "f(x) = x + 1",
+        onRender: action("onRender"),
+        setAssetStatus: action("setAssetStatus"),
     },
 };
 export default meta;
