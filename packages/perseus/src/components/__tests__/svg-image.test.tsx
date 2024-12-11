824386e57cb378755ab8f3ce7d5d7d656609be7b
Sarah Third <sarahthird@gmail.com>
2024-12-11 09:48:09 -0800

    New parseDataFromJSONP to protect against previous JSONP implementation

--- packages/perseus/src/components/__tests__/svg-image.test.tsx
+++ packages/perseus/src/components/__tests__/svg-image.test.tsx
@@ -1,14 +1,56 @@
-import {render, screen} from "@testing-library/react";
+import {act, render} from "@testing-library/react";
 import * as React from "react";
 
+import {testDependencies} from "../../../../../testing/test-dependencies";
+import * as Dependencies from "../../dependencies";
+import {typicalCase} from "../../util/graphie-utils.testdata";
 import SvgImage from "../svg-image";
 
 describe("SvgImage", () => {
+    const images: Array<Record<any, any>> = [];
+    let originalImage;
+
+    beforeEach(() => {
+        jest.spyOn(Dependencies, "getDependencies").mockReturnValue(
+            testDependencies,
+        );
+        originalImage = window.Image;
+        // Mock HTML Image so we can trigger onLoad callbacks and see full
+        // image rendering.
+        // @ts-expect-error - TS2322 - Type 'Mock<Record<string, any>, [], any>' is not assignable to type 'new (width?: number | undefined, height?: number | undefined) => HTMLImageElement'.
+        window.Image = jest.fn(() => {
+            const img: Record<string, any> = {};
+            images.push(img);
+            return img;
+        });
+    });
+
+    afterEach(() => {
+        window.Image = originalImage;
+    });
+
+    // Tells the image loader 1, or all, of our images loaded
+    const markImagesAsLoaded = (imageIndex?: number) => {
+        if (imageIndex != null) {
+            const img = images[imageIndex];
+            if (img?.onload) {
+                act(() => img.onload());
+            }
+        } else {
+            images.forEach((i) => {
+                if (i?.onload) {
+                    act(() => i.onload());
+                }
+            });
+        }
+    };
     it("should render a spinner initially", () => {
+        // Act
         const {container} = render(
             <SvgImage src="http://localhost/sample.png" alt="png image" />,
         );
 
+        // Assert
         expect(
             // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
             container.querySelector("div[class*=spinnerContainer]"),
@@ -17,12 +59,34 @@ describe("SvgImage", () => {
     });
 
     it("should load and render a png", () => {
+        // Act
         const {container} = render(
             <SvgImage src="http://localhost/sample.png" alt="png image" />,
         );
 
+        markImagesAsLoaded(); // Tell the ImageLoader that our images are loaded
+
+        // Assert
         expect(container).toMatchSnapshot();
     });
 
-    // web+graphie://ka-perseus-graphie.s3.amazonaws.com/2ec91c96a937091d4473a1c6a5d97bb6f0427d05
+    it("should load and render a normal graphie svg", async () => {
+        // Arrange
+        global.fetch = jest.fn((url) => {
+            return Promise.resolve({
+                text: () => Promise.resolve(typicalCase.jsonpString),
+                ok: true,
+            });
+        }) as jest.Mock;
+
+        // Act
+        const {container} = render(
+            <SvgImage src={typicalCase.url} alt="svg image" />,
+        );
+
+        markImagesAsLoaded(); // Tell the ImageLoader that our images are loaded
+
+        // Assert
+        expect(container).toMatchSnapshot();
+    });
 });
