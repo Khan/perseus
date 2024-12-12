4c8cda6c5a88a8df143549cdc2d5e5648d515828
Tamara <tamara@khanacademy.org>
2024-12-12 15:26:26 -0600

    Remove unused rubric type

--- packages/perseus/src/validation.types.ts
+++ packages/perseus/src/validation.types.ts
@@ -102,9 +102,6 @@ export type PerseusGrapherRubric = {
 
 export type PerseusGrapherUserInput = PerseusGrapherRubric["correct"];
 
-// TODO(LEMS-2440): Can possibly be removed during 2440; only userInput used
-export type PerseusIFrameRubric = Empty;
-
 export type PerseusIFrameUserInput = {
     status: UserInputStatus;
     message: string | null;
@@ -244,7 +241,6 @@ export type Rubric =
     | PerseusGradedGroupRubric
     | PerseusGradedGroupSetRubric
     | PerseusGrapherRubric
-    | PerseusIFrameRubric
     | PerseusInputNumberRubric
     | PerseusInteractiveGraphRubric
     | PerseusLabelImageRubric
