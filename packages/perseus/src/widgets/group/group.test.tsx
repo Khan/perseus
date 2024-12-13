ac07da03cb332f9dd9ebf82820e409b9fb0844be
Sarah Third <sarahthird@khanacademy.org>
2024-12-12 17:16:44 -0800

    Revert svg-image changes  (#2001)
    ## Summary:
    Undoing svg image changes to unblock Perseus. 
    
    ## Test plan:
    - simple revert.
    
    Author: SonicScrewdriver
    
    Reviewers: mark-fitzgerald
    
    Required Reviewers:
    
    Approved By: mark-fitzgerald
    
    Checks: ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2001

--- packages/perseus/src/widgets/group/group.test.tsx
+++ packages/perseus/src/widgets/group/group.test.tsx
@@ -27,14 +27,6 @@ describe("group widget", () => {
         jest.spyOn(Dependencies, "getDependencies").mockReturnValue(
             testDependencies,
         );
-
-        // Mocked for loading graphie in svg-image
-        global.fetch = jest.fn(() =>
-            Promise.resolve({
-                text: () => "",
-                ok: true,
-            }),
-        ) as jest.Mock;
     });
 
     it("should snapshot", () => {
