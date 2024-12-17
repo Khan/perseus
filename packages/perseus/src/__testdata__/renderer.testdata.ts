7d516af6b5068b42d0f5e9b5d12757e693eb35e8
Jeremy Wiebe <jeremy@khanacademy.org>
2024-12-16 17:38:57 -0800

    Add missing (and now needed) version 1.0 to expression widget config

--- packages/perseus/src/__testdata__/renderer.testdata.ts
+++ packages/perseus/src/__testdata__/renderer.testdata.ts
@@ -22,6 +22,7 @@ export const expressionWidget: ExpressionWidget = {
         functions: [],
         times: true,
     },
+    version: {major: 1, minor: 0},
 };
 
 export const dropdownWidget: DropdownWidget = {
