a7c3a793bfc074ceee8ffc5ffdd34cac5e8775d1
nishasy <nishasy@gmail.com>
2024-12-18 23:48:14 +0000

    Deploying to gh-pages from @ Khan/perseus@881c0aac33b5425efffc4d191f6c0c031f3a9127 🚀

---
+++ assets/editor.stories-OdGOxH-O.js
@@ -0,0 +1,77 @@
+import{j as r}from"./jsx-runtime-63Ea5SlK.js";import{V as R}from"./index-CgfdZoMj.js";import{a as x}from"./chunk-D5ZWXAHU-vR62DWf0.js";import{r as e}from"./index-6oxdNXpR.js";import"./index-iJE49B3L.js";import{S as O}from"./side-by-side-Eet8D1ek.js";import{r as C}from"./register-all-widgets-and-editors-for-testing-_0MVu9BS.js";import{a as u}from"./flags-for-api-options-LZsWJ1n-.js";import{E as f}from"./editor-HB8mJF2U.js";import"./index-awljIyHI.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./v4-yQnnJER4.js";import"./version-akiLXZts.js";import"./article-editor-Yvc2xVWA.js";import"./article-renderer-5xl0gLPn.js";import"./util-_iDv4tVD.js";import"./jquery-yG1GhClm.js";import"./perseus-error-l3K_anoI.js";import"./prop-types-AvCwrZVu.js";import"./mobile-keypad-XsEQndtf.js";import"./index-9gkyvru-.js";import"./index-7-BESUpx.js";import"./index-J2t_5nK1.js";import"./index-NVT999Ia.js";import"./index-dmcq622U.js";import"./assertThisInitialized-4q6YPdh3.js";import"./tabbar-D-BeWJQY.js";import"./item-W49ZrBwd.js";import"./button-assets-ozecF1qE.js";import"./keypad-button-h2VccZ5R.js";import"./operators-page-Gy2UJcU5.js";import"./navigation-pad-d1QCNA_k.js";import"./key-translator-eazZ4BGP.js";import"./invariant-bu5zBsRS.js";import"./enums-x5qaTru7.js";import"./renderer-HtK56tvc.js";import"./index-dnMhQZ-1.js";import"./asset-context-H6Iqp7Gi.js";import"./i18n-context-qFcP8xyW.js";import"./svg-image--maxZNcn.js";import"./index-JG9NzwvL.js";import"./dependencies-CP7Uh8Kq.js";import"./fixed-to-responsive-for_tVF1.js";import"./constants-iPV6vHZm.js";import"./client-Rb4DelHy.js";import"./inline-icon-6fh0Wu1y.js";import"./icon-paths-5JCXzGsq.js";import"./image-loader-17De8yEj.js";import"./tex-MX5FPdQh.js";import"./zoomable-_uYFBX1Q.js";import"./zoomable-tex-Jjwex-Ep.js";import"./perseus-api-1-ethIrW.js";import"./index-k-0mNqHS.js";import"./stub-tag-editor-qMhJW4bS.js";import"./text-list-editor-aj1SAzcA.js";import"./lint-CRWxUAIQ.js";import"./index-OpAdY7qh.js";import"./index-sJx7e_Q7.js";import"./index-fsOXV5-c.js";import"./index-XlX4hVME.js";import"./index-6ID5EQ9c.js";import"./index-C1N8lBvx.js";import"./Popper-Y5KDXl-P.js";import"./math-input-R5Mruqlt.js";import"./index-Zf2-LtSj.js";import"./input-with-examples-MQoVHeXV.js";import"./text-input-uyhLZIJm.js";import"./index-n_qhmlFr.js";import"./tooltip-Lp8Mlfe3.js";import"./simple-keypad-input-6oTXvj90.js";import"./base-radio-OSd6wost.js";import"./media-queries-gbofTbz-.js";import"./shared-4TdECMLk.js";import"./choice-frxJuMp2.js";import"./index-69t9pbsh.js";import"./icon-7RFbyLiL.js";import"./choice-icon-8F5JDzaG.js";import"./focus-ring-qYEF-qqz.js";import"./option-status-r0iv7O2k.js";import"./choice-none-above-i7dEW9xk.js";import"./phet-simulation-JYjt2qd4.js";import"./index-Vn7fg-lP.js";import"./answer-choices-eclNagsK.js";import"./index-hgFKmD12.js";import"./button-group-G5CZaedn.js";import"./graph-XirA6zBn.js";import"./index-smZ6iCr_.js";import"./tiny-invariant-bHgPayXn.js";import"./hud-ifw9Ofbw.js";import"./index-1hft2BpG.js";import"./multi-button-group-QUVHbBcE.js";import"./number-input-4r4-rPkp.js";import"./range-input-EK5SrXBv.js";import"./marker-o4Osk1ii.js";import"./answer-pill-ayk0IJHN.js";import"./sortable-poyXm_VX.js";import"./video-transcript-link-_BcPHEf9.js";import"./perseus-item--Nbb4nkX.js";import"./hints-renderer-6fXokGw-.js";import"./components-JfElkP3I.js";import"./device-framer-MUDr0WgQ.js";import"./constants-p_UgHLsb.js";import"./section-control-button-gmv6EQcz.js";import"./icon-paths-EoBkHOLI.js";import"./viewport-resizer-lbifcNj-.js";import"./index-0C4KXdeC.js";import"./text-diff-B8xt84bM.js";import"./editor-page-r-pSdo_x.js";import"./item-extras-editor-quP7-EIP.js";/* empty css                       */import"./graph-settings-gK4HbVpS.js";import"./categorizer-editor-9Y-G72Ba.js";import"./editor-jsonify-FPqZEIhi.js";import"./blur-input-6j4bgLwx.js";import"./definition-editor-TyvfQs_A.js";import"./dropdown-editor-pCsJKWE_.js";import"./explanation-editor-zEo81GF9.js";import"./expression-editor-Ug5vAUcM.js";import"./image-editor-gHDYSPwB.js";import"./input-number-editor-rhOY0mv6.js";import"./interaction-editor-8BvruStA.js";import"./interactive-graph-editor-oNFR8sj-.js";import"./heading-ZiSKfWRf.js";import"./toggleable-caret-XbIDsoUY.js";import"./interactive-graph-settings-qgtcAcqt.js";import"./locked-figures-section-puWOebfK.js";import"./locked-ellipse-settings-vSVK-uOp.js";import"./locked-figure-aria-C7fBzi4x.js";import"./scrollless-number-text-field-9u0BIHxJ.js";import"./util-qk2aeK8X.js";import"./locked-label-settings-szdKieVD.js";import"./color-select-gOuI3kpW.js";import"./util-NoG5nAQy.js";import"./line-stroke-select-1l2xBOMH.js";import"./locked-function-settings-gdZWlUX6.js";import"./line-swatch-KNm1qAfr.js";import"./locked-line-settings-SXlGYSQT.js";import"./locked-point-settings-pVOE1p8J.js";import"./labeled-switch-Q84J7cX_.js";import"./locked-polygon-settings--38K3gNa.js";import"./locked-vector-settings-U8iTyiG3.js";import"./label-image-editor-qFJonUHO.js";import"./form-wrapped-text-field-q2LxQQ4u.js";import"./global-colors-VIVU4Od4.js";import"./answer-choices-RXqDqy9e.js";import"./behavior-GdncIvY8.js";import"./question-markers-pFnFy3DN.js";import"./marker-CvonsY_m.js";import"./select-image-gvTGGedV.js";import"./matcher-editor-a47pRFZ6.js";import"./number-line-editor-w5sE2aBv.js";import"./numeric-input-editor-_K0y1Bbt.js";import"./phet-simulation-editor-QgHJRkEK.js";import"./python-program-editor-PT0b47ev.js";import"./editor-qpiuPvZz.js";import"./sorter-editor-2nRev6cU.js";import"./tex-error-view-oMHBYXtJ.js";const s={content:`A sequence is defined recursively as follows:
+
+
+$\\qquad\\displaystyle{{a}_{n}}=-\\frac{1}{a_{n-1}-1} 
+~~~~~~\\text{ with}\\qquad\\displaystyle{{a}_{0}}=\\frac{1}{2}\\,$
+
+
+Find the term $a_3$ in the sequence.
+
+[[☃ input-number 1]]`,images:{},widgets:{"input-number 1":{graded:!0,version:{major:0,minor:0},static:!1,type:"input-number",options:{maxError:.1,inexact:!1,value:.5,simplify:"required",answerType:"number",size:"normal"},alignment:"default"}}};C();const vr={title:"PerseusEditor/Editor"},i=()=>r.jsx(f,{apiOptions:u,content:s.content,placeholder:"",widgets:s.widgets,images:s.images,disabled:!1,widgetEnabled:!0,immutableWidgets:!1,showWordCount:!0,warnNoPrompt:!0,warnNoWidgets:!0,onChange:p=>{}}),o=()=>{const p=e.useRef(null),[h,w]=e.useState({}),[S,b]=e.useState("[[☃ interactive-graph 1]]"),[y,W]=e.useState({}),[E,v]=e.useState({"interactive-graph 1":{options:{labels:["x","y"],range:[[-10,10],[-10,10]],gridStep:[1,1],snapStep:[1,1],step:[1,1],backgroundImage:{url:null},markings:"graph",showProtractor:!1,showTooltips:!1,graph:{type:"linear",coords:[[1,1],[5,5]]},correct:{type:"linear"}},type:"interactive-graph",version:{major:0,minor:0}}});return r.jsx("div",{className:"framework-perseus",children:r.jsx(O,{leftTitle:"Editor",left:r.jsx(R,{style:{width:"360px",margin:"20px"},children:r.jsx(f,{ref:p,apiOptions:u,content:S,placeholder:"",widgets:E,images:y,disabled:!1,widgetEnabled:!0,immutableWidgets:!1,showWordCount:!0,warnNoPrompt:!1,warnNoWidgets:!0,onChange:t=>{x("onChange")(t),t.content?b(t.content):t.widgets?v(t.widgets):t.images&&W(t.images),setTimeout(()=>{var m;w(((m=p.current)==null?void 0:m.serialize())||{})},0)}})}),rightTitle:"Serialized Widget Options",jsonObject:h})})};i.__docgenInfo={description:"",methods:[],displayName:"Demo"};o.__docgenInfo={description:"",methods:[],displayName:"DemoInteractiveGraph"};var a,n,d;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`(): React.ReactElement => {
+  return <Editor apiOptions={apiOptionsWithDefaults} content={question1.content} placeholder="" widgets={question1.widgets} images={question1.images} disabled={false} widgetEnabled={true} immutableWidgets={false} showWordCount={true} warnNoPrompt={true} warnNoWidgets={true} onChange={props => {}} />;
+}`,...(d=(n=i.parameters)==null?void 0:n.docs)==null?void 0:d.source}}};var l,c,g;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`(): React.ReactElement => {
+  const editorRef = React.useRef<Editor>(null);
+  const [options, setOptions] = React.useState({});
+  const [content, setContent] = React.useState("[[\\u2603 interactive-graph 1]]");
+  const [images, setImages] = React.useState<PerseusRenderer["images"]>({});
+  const [widgets, setWidgets] = React.useState<PerseusRenderer["widgets"]>({
+    "interactive-graph 1": {
+      options: {
+        labels: ["x", "y"],
+        range: [[-10, 10], [-10, 10]],
+        gridStep: [1, 1],
+        snapStep: [1, 1],
+        step: [1, 1],
+        backgroundImage: {
+          url: null
+        },
+        markings: "graph",
+        showProtractor: false,
+        showTooltips: false,
+        graph: {
+          type: "linear",
+          coords: [[1, 1], [5, 5]]
+        },
+        correct: {
+          type: "linear"
+        }
+      },
+      type: "interactive-graph",
+      version: {
+        major: 0,
+        minor: 0
+      }
+    }
+  });
+  return (
+    // Many of the editor components use scoped CSS that requires this
+    // class to be above it.
+    // TODO: Refactor to aphrodite styles instead of scoped CSS in Less.
+    <div className="framework-perseus">
+            <SideBySide leftTitle="Editor" left={<View style={{
+        width: "360px",
+        margin: "20px"
+      }}>
+                        <Editor ref={editorRef} apiOptions={apiOptionsWithDefaults} content={content} placeholder="" widgets={widgets} images={images} disabled={false} widgetEnabled={true} immutableWidgets={false} showWordCount={true} warnNoPrompt={false} warnNoWidgets={true}
+        // TODO(LEMS-2656): remove TS suppression
+        onChange={((props: Partial<PerseusRenderer>) => {
+          action("onChange")(props);
+          if (props.content) {
+            setContent(props.content);
+          } else if (props.widgets) {
+            setWidgets(props.widgets);
+          } else if (props.images) {
+            setImages(props.images);
+          }
+          // We need to wait for one tick so that the editor
+          // has been re-rendered with the changed props. If
+          // we don't wait, we get the values from the n-1
+          // render and miss the latest change.
+          setTimeout(() => {
+            setOptions(editorRef.current?.serialize() || {});
+          }, 0);
+        }) as any} />
+                    </View>} rightTitle="Serialized Widget Options" jsonObject={options} />
+        </div>
+  );
+}`,...(g=(c=o.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const Rr=["Demo","DemoInteractiveGraph"];export{i as Demo,o as DemoInteractiveGraph,Rr as __namedExportsOrder,vr as default};
