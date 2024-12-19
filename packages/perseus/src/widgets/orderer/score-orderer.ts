7a90068192672404eb34e1cf3fce26919560d127
Tamara <tamara@khanacademy.org>
2024-12-19 09:40:21 -0600

    Rename validateError to validationError

--- packages/perseus/src/widgets/orderer/score-orderer.ts
+++ packages/perseus/src/widgets/orderer/score-orderer.ts
@@ -12,9 +12,9 @@ export function scoreOrderer(
     userInput: PerseusOrdererUserInput,
     scoringData: PerseusOrdererScoringData,
 ): PerseusScore {
-    const validateError = validateOrderer(userInput);
-    if (validateError) {
-        return validateError;
+    const validationError = validateOrderer(userInput);
+    if (validationError) {
+        return validationError;
     }
 
     const correct = _.isEqual(
