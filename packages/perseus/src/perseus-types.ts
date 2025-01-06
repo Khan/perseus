dbbc82f2dd33545b12c6073174b05ebcf8d551ba
Anakaren <anakaren@khanacademy.org>
2025-01-06 09:11:55 -0800

    feat(LEMS-2272): Add scientific notation button to the first tab of expression widget (#1738)
    ## Summary:
    Adds scientific notation button to the first tab of expression widget when added via `button sets` in expression editor
    
    Issue: LEMS-2272
    
    ## Test plan:
    In Expression Widget Editor, add scientific button to first tab by toggling the `scientific` option under the `Button Sets` section of the editor.
    
    ### Expected Behavior 
    1. Within the expression editor, toggle adds and removes scientific notation button 
    2. When toggled, the first tab of expression widget has scientific notation button in the `Shared Keys` section and subsequent tabs of expression widget do NOT have scientific notation button in the `Shared Keys` section 
    3. When not toggled, none of the tabs have the scientific notation button in the `Shared Keys` section of the widget
    4. Scientific Notation button maintains the same functionality as the original 
    
    ## Screen Recording:
    
    https://github.com/user-attachments/assets/c6814815-5654-4b51-8dc6-7eb8173a0bcf
    
    Author: anakaren-rojas
    
    Reviewers: catandthemachines
    
    Required Reviewers:
    
    Approved By: catandthemachines
    
    Checks: ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ⏹️  [cancelled] Publish npm snapshot (ubuntu-latest, 20.x), ⏹️  [cancelled] Cypress (ubuntu-latest, 20.x), ⏹️  [cancelled] Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ⏹️  [cancelled] Check builds for changes in size (ubuntu-latest, 20.x), ⏹️  [cancelled] Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ .github/dependabot.yml
    
    Pull Request URL: https://github.com/Khan/perseus/pull/1738

--- packages/perseus/src/perseus-types.ts
+++ packages/perseus/src/perseus-types.ts
@@ -471,6 +471,7 @@ export type LegacyButtonSets = ReadonlyArray<
     | "logarithms"
     | "basic relations"
     | "advanced relations"
+    | "scientific"
 >;
 
 export type PerseusExpressionWidgetOptions = {
