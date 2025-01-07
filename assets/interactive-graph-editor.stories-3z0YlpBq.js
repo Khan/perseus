333691042353909cae95202df5369b68fe87be76
nishasy <nishasy@gmail.com>
2025-01-07 19:15:25 +0000

    Deploying to gh-pages from @ Khan/perseus@eefcf5c5c434455295e17bf4cb411d029f24fe49 🚀

---
+++ assets/interactive-graph-editor.stories-3z0YlpBq.js
@@ -0,0 +1,126 @@
+import{j as X}from"./jsx-runtime-63Ea5SlK.js";import{a as Y}from"./chunk-D5ZWXAHU-vR62DWf0.js";import{r as Z}from"./index-6oxdNXpR.js";import{a as $}from"./flags-for-api-options-enFNtkRA.js";import{I as oo}from"./interactive-graph-editor-AM7XsRhx.js";import{g as ro}from"./util-MQuAq01v.js";import"./v4-yQnnJER4.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./article-renderer-6DL34wtl.js";import"./util-_iDv4tVD.js";import"./jquery-yG1GhClm.js";import"./version-akiLXZts.js";import"./perseus-error-l3K_anoI.js";import"./prop-types-ch_7vUTQ.js";import"./mobile-keypad-3xFFuAxL.js";import"./index-awljIyHI.js";import"./index-9gkyvru-.js";import"./index-7-BESUpx.js";import"./index-J2t_5nK1.js";import"./index-NVT999Ia.js";import"./index-CgfdZoMj.js";import"./index-dmcq622U.js";import"./assertThisInitialized-4q6YPdh3.js";import"./tabbar-D-BeWJQY.js";import"./item-W49ZrBwd.js";import"./button-assets-ozecF1qE.js";import"./keypad-button-h2VccZ5R.js";import"./operators-page-3PR1Ra4Y.js";import"./navigation-pad-d1QCNA_k.js";import"./key-translator-HspkCZQ2.js";import"./enums-x5qaTru7.js";import"./renderer-GTfDWY1h.js";import"./index-dnMhQZ-1.js";import"./asset-context-H6Iqp7Gi.js";import"./i18n-context-ei4f54eq.js";import"./svg-image-wIdVOlIq.js";import"./index-JG9NzwvL.js";import"./dependencies-CP7Uh8Kq.js";import"./fixed-to-responsive-for_tVF1.js";import"./constants-iPV6vHZm.js";import"./client-Rb4DelHy.js";import"./inline-icon-6fh0Wu1y.js";import"./icon-paths-5JCXzGsq.js";import"./image-loader-cBoFrbCq.js";import"./tex-MX5FPdQh.js";import"./zoomable-_uYFBX1Q.js";import"./zoomable-tex-Jjwex-Ep.js";import"./perseus-api-ooj0_ZRv.js";import"./index-IiwcBdIZ.js";import"./stub-tag-editor-zsURaARz.js";import"./text-list-editor-K49FGdd7.js";import"./lint-CRWxUAIQ.js";import"./index-OpAdY7qh.js";import"./index-sJx7e_Q7.js";import"./index-fsOXV5-c.js";import"./index-XlX4hVME.js";import"./index-6ID5EQ9c.js";import"./index-C1N8lBvx.js";import"./Popper-Y5KDXl-P.js";import"./math-input-NBo1H9JC.js";import"./index-Zf2-LtSj.js";import"./input-with-examples-9wcKxwa0.js";import"./text-input-uyhLZIJm.js";import"./index-n_qhmlFr.js";import"./tooltip-Lp8Mlfe3.js";import"./simple-keypad-input-pdqW2OJZ.js";import"./base-radio-v-o9nB7x.js";import"./media-queries-gbofTbz-.js";import"./shared-4TdECMLk.js";import"./choice-RyQZyvQo.js";import"./index-69t9pbsh.js";import"./icon-7RFbyLiL.js";import"./choice-icon-BD8zPTU8.js";import"./focus-ring-qYEF-qqz.js";import"./option-status-drlCsbdK.js";import"./choice-none-above--oubLGhE.js";import"./phet-simulation-hVRVRfpl.js";import"./index-Vn7fg-lP.js";import"./answer-choices-oYEtQ1Me.js";import"./index-hgFKmD12.js";import"./button-group-G5CZaedn.js";import"./graph-JHUHcoXy.js";import"./index-smZ6iCr_.js";import"./tiny-invariant-bHgPayXn.js";import"./hud-ifw9Ofbw.js";import"./index-1hft2BpG.js";import"./multi-button-group-QUVHbBcE.js";import"./number-input-T6lG2NEQ.js";import"./range-input-Gg2JKhNA.js";import"./marker-FXrJpCQh.js";import"./answer-pill-02l7Uqt6.js";import"./sortable-wFwW1DC4.js";import"./video-transcript-link-K3ULCxyT.js";import"./perseus-item-M3M3RGq4.js";import"./hints-renderer-gQ8w2vuM.js";import"./components-_SOfL9--.js";import"./heading-ZiSKfWRf.js";import"./toggleable-caret-XbIDsoUY.js";import"./interactive-graph-settings-xM0Re771.js";import"./locked-figures-section-0w1K8V61.js";import"./locked-ellipse-settings-qRTgY8km.js";import"./locked-figure-aria-2m3cENft.js";import"./scrollless-number-text-field-9u0BIHxJ.js";import"./util-qk2aeK8X.js";import"./locked-label-settings-73lAIBWF.js";import"./color-select-oh7G5t9F.js";import"./line-stroke-select-GyuNRytJ.js";import"./locked-function-settings-BpknLke6.js";import"./line-swatch-zWmx-Aqj.js";import"./locked-line-settings-TOLLi7Fw.js";import"./locked-point-settings-4LRcxcEd.js";import"./labeled-switch-_gT8vcQF.js";import"./locked-polygon-settings-lrHbIkcO.js";import"./locked-vector-settings-KB8j77vE.js";const t={apiOptions:$,graph:{type:"segment"},correct:{type:"segment"}},o=ro("point"),ct={title:"PerseusEditor/Widgets/Interactive Graph Editor",component:oo,args:{box:[3e3,3e3],gridStep:[1,1],labels:["x","y"],markings:"graph",range:[[-10,10],[-10,10]],rulerLabel:"",rulerTicks:10,showProtractor:!1,showRuler:!1,showTooltips:!1,snapStep:[1,1],step:[1,1],onChange:Y("onChange")},argTypes:{lockedFigures:{table:{readonly:!0}}}};function r(q,z){const B=(N,Q)=>({...N,...Q}),[J,K]=Z.useReducer(B,z);return X.jsx(q,{...J,onChange:K})}const a={},p={decorators:[r]},e={args:t,decorators:[r]},n={args:{...t,graph:{type:"polygon"},correct:{type:"polygon",numSides:4,showAngles:!0,showSides:!0,snapTo:"angles"}},decorators:[r]},i={args:{...t,lockedFigures:[{...o,coord:[1,1]},{...o,coord:[-1,-1]}]},decorators:[r]},s={args:{...t,lockedFigures:[{type:"line",kind:"line",points:[{...o,coord:[0,2]},{...o,coord:[2,3]}],color:"green",lineStyle:"solid",showPoint1:!1,showPoint2:!1},{type:"line",kind:"ray",points:[{...o,color:"pink",coord:[0,0]},{...o,color:"pink",coord:[4,2]}],color:"pink",lineStyle:"solid",showPoint1:!0,showPoint2:!1},{type:"line",kind:"segment",points:[{...o,color:"grayH",coord:[0,-2]},{...o,color:"grayH",coord:[4,0]}],color:"grayH",lineStyle:"solid",showPoint1:!0,showPoint2:!0}]},decorators:[r]},c={args:{...t,lockedFigures:[{type:"ellipse",center:[0,0],radius:[5,2],angle:0,color:"green",fillStyle:"translucent",strokeStyle:"solid"}]},decorators:[r]},m={args:{...t,lockedFigures:[{type:"polygon",points:[[-9,4],[-6,4],[-6,1],[-9,1]],color:"green",fillStyle:"translucent",strokeStyle:"solid",showVertices:!0}]},decorators:[r]};var l,d,u;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(u=(d=a.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,h,f;p.parameters={...p.parameters,docs:{...(g=p.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  decorators: [StatefulDecorator]
+}`,...(f=(h=p.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var y,k,P,S,w;e.parameters={...e.parameters,docs:{...(y=e.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: mafsOptions,
+  decorators: [StatefulDecorator]
+}`,...(P=(k=e.parameters)==null?void 0:k.docs)==null?void 0:P.source},description:{story:`Example of what the InteractiveGraphEditor experience is when using
+a Mafs-based InteractiveGraph.`,...(w=(S=e.parameters)==null?void 0:S.docs)==null?void 0:w.description}}};var E,W,L,x,D;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    ...mafsOptions,
+    graph: {
+      type: "polygon"
+    },
+    correct: {
+      type: "polygon",
+      numSides: 4,
+      showAngles: true,
+      showSides: true,
+      snapTo: "angles"
+    }
+  },
+  decorators: [StatefulDecorator]
+}`,...(L=(W=n.parameters)==null?void 0:W.docs)==null?void 0:L.source},description:{story:`Example of what the InteractiveGraphEditor experience is when using
+a Mafs-based InteractiveGraph to create Polygons.`,...(D=(x=n.parameters)==null?void 0:x.docs)==null?void 0:D.description}}};var F,b,v,O,T;i.parameters={...i.parameters,docs:{...(F=i.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    // Use locked figures with mafs only.
+    ...mafsOptions,
+    lockedFigures: [{
+      ...defaultPointProps,
+      coord: [1, 1]
+    }, {
+      ...defaultPointProps,
+      coord: [-1, -1]
+    }]
+  },
+  decorators: [StatefulDecorator]
+}`,...(v=(b=i.parameters)==null?void 0:b.docs)==null?void 0:v.source},description:{story:`This InteractiveGraphEditor has locked points.
+
+Locked figures are graph elements such as points, lines, line segements,
+etc. that are locked in place and not interactive. They can be added
+with the "Add element" dropdown at the bottom.`,...(T=(O=i.parameters)==null?void 0:O.docs)==null?void 0:T.description}}};var I,G,H,M,C;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    // Use locked figures with mafs only.
+    ...mafsOptions,
+    lockedFigures: [{
+      type: "line",
+      kind: "line",
+      points: [{
+        ...defaultPointProps,
+        coord: [0, 2]
+      }, {
+        ...defaultPointProps,
+        coord: [2, 3]
+      }],
+      color: "green",
+      lineStyle: "solid",
+      showPoint1: false,
+      showPoint2: false
+    }, {
+      type: "line",
+      kind: "ray",
+      points: [{
+        ...defaultPointProps,
+        color: "pink",
+        coord: [0, 0]
+      }, {
+        ...defaultPointProps,
+        color: "pink",
+        coord: [4, 2]
+      }],
+      color: "pink",
+      lineStyle: "solid",
+      showPoint1: true,
+      showPoint2: false
+    }, {
+      type: "line",
+      kind: "segment",
+      points: [{
+        ...defaultPointProps,
+        color: "grayH",
+        coord: [0, -2]
+      }, {
+        ...defaultPointProps,
+        color: "grayH",
+        coord: [4, 0]
+      }],
+      color: "grayH",
+      lineStyle: "solid",
+      showPoint1: true,
+      showPoint2: true
+    }]
+  },
+  decorators: [StatefulDecorator]
+}`,...(H=(G=s.parameters)==null?void 0:G.docs)==null?void 0:H.source},description:{story:`This InteractiveGraphEditor has a locked line segment, line, and ray.
+
+Locked figures are graph elements such as points, lines, line segements,
+etc. that are locked in place and not interactive. They can be added
+with the "Add element" dropdown at the bottom.`,...(C=(M=s.parameters)==null?void 0:M.docs)==null?void 0:C.description}}};var A,U,j;c.parameters={...c.parameters,docs:{...(A=c.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    // Use locked figures with mafs only.
+    ...mafsOptions,
+    lockedFigures: [{
+      type: "ellipse",
+      center: [0, 0],
+      radius: [5, 2],
+      angle: 0,
+      color: "green",
+      fillStyle: "translucent",
+      strokeStyle: "solid"
+    }]
+  },
+  decorators: [StatefulDecorator]
+}`,...(j=(U=c.parameters)==null?void 0:U.docs)==null?void 0:j.source}}};var R,V,_;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    // Use locked figures with mafs only.
+    ...mafsOptions,
+    lockedFigures: [{
+      type: "polygon",
+      points: [[-9, 4], [-6, 4], [-6, 1], [-9, 1]],
+      color: "green",
+      fillStyle: "translucent",
+      strokeStyle: "solid",
+      showVertices: true
+    }]
+  },
+  decorators: [StatefulDecorator]
+}`,...(_=(V=m.parameters)==null?void 0:V.docs)==null?void 0:_.source}}};const mt=["Default","Controlled","WithMafs","WithMafsPolygon","WithLockedPoints","WithLockedLines","WithLockedEllipses","WithLockedPolygons"];export{p as Controlled,a as Default,c as WithLockedEllipses,s as WithLockedLines,i as WithLockedPoints,m as WithLockedPolygons,e as WithMafs,n as WithMafsPolygon,mt as __namedExportsOrder,ct as default};
---
+++ assets/register-all-widgets-for-testing-XbFGieIf.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-6DL34wtl.js";import{r,a as s}from"./renderer-GTfDWY1h.js";const a=()=>{r(e),s()};export{a as r};
