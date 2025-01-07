cd1ed89786cad390647aff1d8d8bcc0eb3528dec
Mark Fitzgerald <markfitzgerald@markfitgeraldm3.home>
2025-01-06 17:15:49 -0800

    Test use of imported styles (currently failing).

--- packages/perseus/src/widgets/explanation/explanation.tsx
+++ packages/perseus/src/widgets/explanation/explanation.tsx
@@ -3,7 +3,6 @@ import Button from "@khanacademy/wonder-blocks-button";
 import {Id, View} from "@khanacademy/wonder-blocks-core";
 import caretDown from "@phosphor-icons/core/regular/caret-down.svg";
 import caretUp from "@phosphor-icons/core/regular/caret-up.svg";
-import {StyleSheet} from "aphrodite";
 import * as React from "react";
 import _ from "underscore";
 
@@ -12,6 +11,8 @@ import * as Changeable from "../../mixins/changeable";
 import Renderer from "../../renderer";
 import {getPromptJSON as _getPromptJSON} from "../../widget-ai-utils/explanation/explanation-ai-utils";
 import scoreNoop from "../__shared__/score-noop";
+
+import styles from "./explanation.module.css";
 
 import type {PerseusExplanationWidgetOptions} from "../../perseus-types";
 import type {Widget, WidgetExports, WidgetProps} from "../../types";
@@ -95,27 +96,6 @@ class Explanation extends React.Component<Props, State> implements Widget {
             this._mounted &&
             mediaQueryIsMatched("(prefers-reduced-motion: no-preference)");
 
-        // Special styling is needed to fit the button in a block of text without throwing off the line spacing.
-        // While the button is not normally included in a block of text, it needs to be able to accommodate such a case.
-        const buttonStyleOverrides = {
-            height: "auto",
-            lineHeight: "inherit",
-            marginLeft: "-2px", // transfer space on the left side to the padding
-            marginRight: "2px", // adds space between the right border and any text that follows
-            paddingLeft: "2px", // adds space between the left border and the button text
-        };
-
-        const labelStyle = {
-            fontSize: "18px",
-            lineHeight: "inherit",
-            "text-align": "left",
-            // The following property adjusts the large space between the button text and the caret icon.
-            // Since we are unable to adjust the styling of the icon (where the extra space exists),
-            //      we are adjusting it on the text label by using a negative margin.
-            marginRight: "-6px",
-            "white-space": "normal",
-        };
-
         const contentStyling = [
             styles.content,
             this.state.expanded
@@ -136,21 +116,21 @@ class Explanation extends React.Component<Props, State> implements Widget {
                             aria-controls={contentId}
                             endIcon={caretIcon}
                             kind="tertiary"
-                            labelStyle={labelStyle}
+                            labelStyle={styles.labelStyle}
                             onClick={this._onClick}
                             size="small"
-                            style={buttonStyleOverrides}
+                            style={styles.buttonStyleOverrides}
                         >
                             {promptText}
                         </Button>
 
                         <View
                             id={contentId}
-                            style={contentStyling}
+                            className={contentStyling.join(" ")}
                             aria-hidden={!this.state.expanded}
                             testId="content-container"
                         >
-                            <View style={styles.contentWrapper}>
+                            <View className={styles.contentWrapper}>
                                 <Renderer
                                     apiOptions={this.props.apiOptions}
                                     content={this.props.explanation}
@@ -166,58 +146,6 @@ class Explanation extends React.Component<Props, State> implements Widget {
         );
     }
 }
-
-const leftBorderSpacing = 23;
-const verticalContentPadding = 10;
-const arrowHeight = 14;
-
-const styles = StyleSheet.create({
-    content: {
-        borderLeft: "0px solid #ccc",
-        display: "inline-grid",
-        position: "relative",
-    },
-
-    contentCollapsed: {
-        gridTemplateColumns: "0fr",
-        gridTemplateRows: "0fr",
-        marginBottom: 0,
-        marginTop: 0,
-        minWidth: "0",
-        paddingBottom: 0,
-        visibility: "hidden",
-    },
-
-    contentExpanded: {
-        borderLeftWidth: "5px",
-        gridTemplateColumns: "1fr",
-        gridTemplateRows: "1fr",
-        marginLeft: -leftBorderSpacing,
-        minWidth: "100%",
-        paddingLeft: leftBorderSpacing,
-        paddingBottom: verticalContentPadding,
-        visibility: "visible",
-
-        // Note: we still use arrow height as the vertical margin, even on
-        // desktop when there is no arrow, but it's good enough.
-        marginBottom: arrowHeight,
-        marginTop: arrowHeight,
-    },
-
-    contentWrapper: {
-        overflow: "hidden",
-    },
-
-    transitionCollapsed: {
-        transition:
-            "all 0.25s step-end, grid-template-rows 0.25s, margin-top 0.25s, margin-bottom 0.25s, padding-bottom 0.25s",
-    },
-
-    transitionExpanded: {
-        transition:
-            "grid-template-rows 0.5s, margin-top 0.5s, margin-bottom 0.5s, padding-bottom 0.5s",
-    },
-});
 
 export default {
     name: "explanation",
