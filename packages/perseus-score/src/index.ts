29514fcbfbfd6760a66e946944022d993918e403
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-01-08 15:10:09 -0600

    try something else

--- packages/perseus-score/src/index.ts
+++ packages/perseus-score/src/index.ts
@@ -1,1 +1,2 @@
-export {default as KhanAnswerTypes, Score} from "./util/answer-types";
+export {default as KhanAnswerTypes} from "./util/answer-types";
+export type {Score} from "./util/answer-types";
