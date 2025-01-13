7175b38dfd100e267012ba17c93ab45f8a1514d7
Tamara <tamara@khanacademy.org>
2025-01-13 11:05:33 -0600

    Add test for new function

---
+++ packages/perseus/src/widgets/orderer/orderer.util.test.ts
@@ -0,0 +1,26 @@
+import {question1} from "./orderer.testdata";
+import getOrdererPublicOptions from "./orderer.util";
+
+import type {PerseusOrdererWidgetOptions} from "@khanacademy/perseus-core";
+
+describe("getOrdererPublicOptions", () => {
+    it("should return the correct public options without any answer data", () => {
+        // Arrange
+        const options: PerseusOrdererWidgetOptions =
+            question1.widgets["orderer 1"].options;
+
+        // Act
+        const result = getOrdererPublicOptions(options);
+
+        // Assert
+        expect(result).toEqual({
+            layout: "horizontal",
+            options: [
+                {content: "$10.9$", images: {}, widgets: {}},
+                {content: "$11$", images: {}, widgets: {}},
+                {content: "$\\sqrt{120}$", images: {}, widgets: {}},
+            ],
+            height: "normal",
+        });
+    });
+});
