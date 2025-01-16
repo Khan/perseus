36c736f4f6c02dfb012feb7e685797829b144565
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-01-16 09:26:07 -0600

    move expression scorer

--- packages/perseus-core/src/index.ts
+++ packages/perseus-core/src/index.ts
@@ -12,6 +12,7 @@ export type {ErrorKind} from "./error/errors";
 // Careful, `version.ts` uses this function so it _must_ be imported above it
 export {addLibraryVersionToPerseusDebug} from "./utils/add-library-version-to-perseus-debug";
 export {default as getMatrixSize} from "./utils/get-matrix-size";
+export {default as getDecimalSeparator} from "./utils/get-decimal-separator";
 
 export {libVersion} from "./version";
 
