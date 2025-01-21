75bab8b32b289ec6ad2de772cfea7335b5ad7405
Jeremy Wiebe <jeremy@khanacademy.org>
2025-01-21 10:31:53 -0800

    Hopefully clarify extending the widget types interface

--- packages/perseus/src/widgets/mock-widgets/mock-widget-types.ts
+++ packages/perseus/src/widgets/mock-widgets/mock-widget-types.ts
@@ -16,6 +16,10 @@ export type PerseusMockWidgetUserInput = {
 };
 
 // Extend the widget registries for testing
+// See @khanacademy/perseus-core's PerseusWidgetTypes for a full explanation.
+// Basically, we're extending the interface from that package so that our
+// testing code knows of the MockWidget. In production code, there's no
+// knowledge of the mock widget.
 declare module "@khanacademy/perseus-core" {
     export interface PerseusWidgetTypes {
         "mock-widget": MockWidget;
