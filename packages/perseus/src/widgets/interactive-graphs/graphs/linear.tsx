e2f2cee9fe39313885e7c6aaed963612f807caca
Nisha Yerunkar <nisha@khanacademy.org>
2025-01-22 14:14:07 -0800

    [SR] Linear - Add the interactive elements linear description to the whole graph container (#2110)
    ## Summary:
    Add the interactive Linear graph description to the full graph container.
    
    This adds the "Interactive elements: Line..." description to the outermost graph container.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-1726
    
    ## Test plan:
    `yarn jest packages/perseus/src/widgets/interactive-graphs/graphs/linear.test.tsx`
    
    Storybook
    - Go to http://localhost:6006/iframe.html?globals=&args=&id=perseuseditor-widgets-interactive-graph--interactive-graph-linear&viewMode=story
    - Confirm that there is the "interactive elements: ..." section in the SR tree
    - Use screen reader to confirm it's read out loud
    
    Author: nishasy
    
    Reviewers: catandthemachines, anakaren-rojas
    
    Required Reviewers:
    
    Approved By: catandthemachines
    
    Checks: ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2110

--- packages/perseus/src/widgets/interactive-graphs/graphs/linear.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/linear.tsx
@@ -8,6 +8,7 @@ import {MovableLine} from "./components/movable-line";
 import {srFormatNumber} from "./screenreader-text";
 import {getInterceptStringForLine, getSlopeStringForLine} from "./utils";
 
+import type {I18nContextType} from "../../../components/i18n-context";
 import type {
     MafsGraphProps,
     LinearGraphState,
@@ -22,7 +23,9 @@ export function renderLinearGraph(
 ): InteractiveGraphElementSuite {
     return {
         graph: <LinearGraph graphState={state} dispatch={dispatch} />,
-        interactiveElementsDescription: null,
+        interactiveElementsDescription: (
+            <LinearGraphDescription state={state} />
+        ),
     };
 }
 
@@ -38,21 +41,14 @@ const LinearGraph = (props: LinearGraphProps, key: number) => {
     const interceptDescriptionId = id + "-intercept";
     const slopeDescriptionId = id + "-slope";
 
-    // Aria label strings
-    const linearGraphPointsDescription = strings.srLinearGraphPoints({
-        point1X: srFormatNumber(line[0][0], locale),
-        point1Y: srFormatNumber(line[0][1], locale),
-        point2X: srFormatNumber(line[1][0], locale),
-        point2Y: srFormatNumber(line[1][1], locale),
-    });
-    const grabHandleAriaLabel = strings.srLinearGrabHandle({
-        point1X: srFormatNumber(line[0][0], locale),
-        point1Y: srFormatNumber(line[0][1], locale),
-        point2X: srFormatNumber(line[1][0], locale),
-        point2Y: srFormatNumber(line[1][1], locale),
-    });
-    const slopeString = getSlopeStringForLine(line, strings);
-    const interceptString = getInterceptStringForLine(line, strings, locale);
+    // Aria strings
+    const {
+        srLinearGraph,
+        srLinearGraphPoints,
+        srLinearGrabHandle,
+        slopeString,
+        interceptString,
+    } = describeLinearGraph(props.graphState, {strings, locale});
 
     // Linear graphs only have one line
     // (LEMS-2050): Update the reducer so that we have a separate action for moving one line
@@ -60,12 +56,12 @@ const LinearGraph = (props: LinearGraphProps, key: number) => {
     return (
         <g
             // Outer line minimal description
-            aria-label={strings.srLinearGraph}
+            aria-label={srLinearGraph}
             aria-describedby={`${pointsDescriptionId} ${interceptDescriptionId} ${slopeDescriptionId}`}
         >
             <MovableLine
                 key={0}
-                ariaLabels={{grabHandleAriaLabel: grabHandleAriaLabel}}
+                ariaLabels={{grabHandleAriaLabel: srLinearGrabHandle}}
                 ariaDescribedBy={`${interceptDescriptionId} ${slopeDescriptionId}`}
                 points={line}
                 onMoveLine={(delta: vec.Vector2) => {
@@ -88,7 +84,7 @@ const LinearGraph = (props: LinearGraphProps, key: number) => {
             {/* Hidden elements to provide the descriptions for the
                 circle and radius point's `aria-describedby` properties. */}
             <g id={pointsDescriptionId} style={a11y.srOnly}>
-                {linearGraphPointsDescription}
+                {srLinearGraphPoints}
             </g>
             <g id={interceptDescriptionId} style={a11y.srOnly}>
                 {interceptString}
@@ -99,3 +95,52 @@ const LinearGraph = (props: LinearGraphProps, key: number) => {
         </g>
     );
 };
+
+function LinearGraphDescription({state}: {state: LinearGraphState}) {
+    // The reason that LinearGraphDescription is a component (rather than a
+    // function that returns a string) is because it needs to use a
+    // hook: `usePerseusI18n`.
+    const i18n = usePerseusI18n();
+    const strings = describeLinearGraph(state, i18n);
+
+    return strings.srLinearInteractiveElement;
+}
+
+// Exported for testing
+export function describeLinearGraph(
+    state: LinearGraphState,
+    i18n: I18nContextType,
+): Record<string, string> {
+    const {coords: line} = state;
+    const {strings, locale} = i18n;
+
+    // Aria label strings
+    const srLinearGraph = strings.srLinearGraph;
+    const srLinearGraphPoints = strings.srLinearGraphPoints({
+        point1X: srFormatNumber(line[0][0], locale),
+        point1Y: srFormatNumber(line[0][1], locale),
+        point2X: srFormatNumber(line[1][0], locale),
+        point2Y: srFormatNumber(line[1][1], locale),
+    });
+    const srLinearGrabHandle = strings.srLinearGrabHandle({
+        point1X: srFormatNumber(line[0][0], locale),
+        point1Y: srFormatNumber(line[0][1], locale),
+        point2X: srFormatNumber(line[1][0], locale),
+        point2Y: srFormatNumber(line[1][1], locale),
+    });
+    const slopeString = getSlopeStringForLine(line, strings);
+    const interceptString = getInterceptStringForLine(line, strings, locale);
+
+    const srLinearInteractiveElement = strings.srInteractiveElements({
+        elements: [srLinearGraph, srLinearGraphPoints].join(" "),
+    });
+
+    return {
+        srLinearGraph,
+        srLinearGraphPoints,
+        srLinearGrabHandle,
+        slopeString,
+        interceptString,
+        srLinearInteractiveElement,
+    };
+}
