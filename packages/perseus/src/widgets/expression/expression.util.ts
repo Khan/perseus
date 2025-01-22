554cb9545418bbe3452169daf0c4d1b00ba14b93
Tamara <tamara@khanacademy.org>
2025-01-22 15:56:24 -0600

    [tb/LEMS-2758/expression-public-options] Create function to build public widget options

---
+++ packages/perseus/src/widgets/expression/expression.util.ts
@@ -0,0 +1,36 @@
+import type {
+    LegacyButtonSets,
+    PerseusExpressionWidgetOptions,
+} from "@khanacademy/perseus-core";
+
+/**
+ * For details on the individual options, see the
+ * PerseusOrdererWidgetOptions type
+ */
+type ExpressionPublicWidgetOptions = {
+    buttonSets: LegacyButtonSets;
+    functions: ReadonlyArray<string>;
+    times: boolean;
+    visibleLabel?: string;
+    ariaLabel?: string;
+    buttonsVisible?: "always" | "never" | "focused";
+};
+
+/**
+ * Given a PerseusExpressionWidgetOptions object, return a new object with only
+ * the public options that should be exposed to the client.
+ */
+function getExpressionPublicWidgetOptions(
+    options: PerseusExpressionWidgetOptions,
+): ExpressionPublicWidgetOptions {
+    return {
+        buttonSets: options.buttonSets,
+        functions: options.functions,
+        times: options.times,
+        visibleLabel: options.visibleLabel,
+        ariaLabel: options.ariaLabel,
+        buttonsVisible: options.buttonsVisible,
+    };
+}
+
+export default getExpressionPublicWidgetOptions;
