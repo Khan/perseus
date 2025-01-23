9ac5a109b00a0536d36088bc723db597fe31da32
Tamara <tamara@khanacademy.org>
2025-01-23 09:47:41 -0600

    [tb/LEMS-2758/expression-public-options] Update test to define input inline

--- packages/perseus/src/widgets/expression/expression.util.test.ts
+++ packages/perseus/src/widgets/expression/expression.util.test.ts
@@ -1,4 +1,3 @@
-import {expressionItem2} from "./expression.testdata";
 import getExpressionPublicWidgetOptions from "./expression.util";
 
 import type {PerseusExpressionWidgetOptions} from "@khanacademy/perseus-core";
@@ -6,8 +5,22 @@ import type {PerseusExpressionWidgetOptions} from "@khanacademy/perseus-core";
 describe("getExpressionPublicWidgetOptions", () => {
     it("should return the correct public options without any answer data", () => {
         // Arrange
-        const options: PerseusExpressionWidgetOptions =
-            expressionItem2.question.widgets["expression 1"].options;
+        const options: PerseusExpressionWidgetOptions = {
+            answerForms: [
+                {
+                    considered: "correct",
+                    form: false,
+                    simplify: false,
+                    value: "123-x",
+                },
+            ],
+            buttonSets: ["basic"],
+            functions: ["f", "g", "h"],
+            times: false,
+            visibleLabel: "the visible label",
+            ariaLabel: "the aria label",
+            buttonsVisible: "always",
+        };
 
         // Act
         const publicWidgetOptions = getExpressionPublicWidgetOptions(options);
@@ -18,6 +31,8 @@ describe("getExpressionPublicWidgetOptions", () => {
             buttonSets: ["basic"],
             functions: ["f", "g", "h"],
             buttonsVisible: "always",
+            visibleLabel: "the visible label",
+            ariaLabel: "the aria label",
         });
     });
 });
