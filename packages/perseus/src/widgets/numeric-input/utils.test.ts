1b40412c8db8d29b1e56e7031df445e6e8fe949c
Sarah Third <sarahthird@khanacademy.org>
2025-01-23 12:21:37 -0800

    Refactoring Numeric Input helper functions to remove underscore (#2128)
    This PR is part of the Numeric Input Project, and will be landed onto the feature branch for a full QA pass.
    
    The intended goal was to remove all cases of underscore in the Numeric Input component, and to improve the commenting / documentation of the code.
    
    Some things to note:
    - I've changed the logic of `generateExamples` to match `shouldShowExamples`, as we were generating a list of all possible examples and simply not displaying it. This seemed unnecessary and we can exit both functions early.
    - I've added more specific types for `PerseusNumericInputWidgetOptions.simplify`
    
    Issue: LEMS-2446
    
    - New tests
    - Manual testing in storybook
    
    Author: SonicScrewdriver
    
    Reviewers: SonicScrewdriver, mark-fitzgerald, jeremywiebe
    
    Required Reviewers:
    
    Approved By: mark-fitzgerald
    
    Checks: ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2128

--- packages/perseus/src/widgets/numeric-input/utils.test.ts
+++ packages/perseus/src/widgets/numeric-input/utils.test.ts
@@ -1,10 +1,12 @@
-import {generateExamples, shouldShowExamples} from "./utils";
+import {mockStrings} from "../../strings";
 
-import type {PerseusStrings} from "../../strings";
+import {generateExamples, shouldShowExamples, unionAnswerForms} from "./utils";
+
 import type {PerseusNumericInputAnswerForm} from "@khanacademy/perseus-core";
 
 describe("generateExamples", () => {
     it("returns an array of examples", () => {
+        // Arrange
         const answerForms: readonly PerseusNumericInputAnswerForm[] = [
             {
                 name: "integer",
@@ -15,25 +17,67 @@ describe("generateExamples", () => {
                 simplify: "required",
             },
         ];
-        const strings: Partial<PerseusStrings> = {
-            yourAnswer: "Your answer",
-            integerExample: "Integer example",
-            properExample: "Proper example",
-            simplifiedProperExample: "Simplified proper example",
-        };
+
         const expected = [
-            "Your answer",
-            "Integer example",
-            "Simplified proper example",
+            "**Your answer should be** ",
+            "an integer, like $6$",
+            "a *simplified proper* fraction, like $3/5$",
         ];
-        expect(
-            generateExamples(answerForms, strings as PerseusStrings),
-        ).toEqual(expected);
+
+        // Act
+        const result = generateExamples(answerForms, mockStrings);
+
+        // Assert
+        expect(result).toEqual(expected);
+    });
+
+    it("returns only unique entries in an array of examples", () => {
+        // Arrange
+        const answerForms: readonly PerseusNumericInputAnswerForm[] = [
+            {
+                name: "integer",
+                simplify: "optional",
+            },
+            {
+                name: "proper",
+                simplify: "required",
+            },
+            {
+                name: "integer",
+                simplify: "required",
+            },
+        ];
+
+        const expected = [
+            "**Your answer should be** ",
+            "an integer, like $6$",
+            "a *simplified proper* fraction, like $3/5$",
+        ];
+
+        // Act
+        const result = generateExamples(answerForms, mockStrings);
+
+        // Assert
+        expect(result).toEqual(expected);
+    });
+
+    it("returns an empty array if no answer forms are provided", () => {
+        // Arrange
+        const answerForms: readonly PerseusNumericInputAnswerForm[] = [];
+
+        const expected = [];
+
+        // Act
+        const result = generateExamples(answerForms, mockStrings);
+
+        // Assert
+        expect(result).toEqual(expected);
     });
 });
 
 describe("shouldShowExamples", () => {
-    it("returns true when not all forms are accepted", () => {
+    it("returns true when only some forms are accepted", () => {
+        // Arrange
         const answerForms: readonly PerseusNumericInputAnswerForm[] = [
             {
                 name: "integer",
@@ -44,10 +88,16 @@ describe("shouldShowExamples", () => {
                 simplify: "required",
             },
         ];
-        expect(shouldShowExamples(answerForms)).toBe(true);
+
+        // Act
+        const result = shouldShowExamples(answerForms);
+
+        // Assert
+        expect(result).toBe(true);
     });
 
     it("returns false when all forms are accepted", () => {
+        // Arrange
         const answerForms: readonly PerseusNumericInputAnswerForm[] = [
             {
                 name: "integer",
@@ -74,11 +124,70 @@ describe("shouldShowExamples", () => {
                 simplify: "required",
             },
         ];
-        expect(shouldShowExamples(answerForms)).toBe(false);
+
+        // Act
+        const result = shouldShowExamples(answerForms);
+
+        // Assert
+        expect(result).toBe(false);
     });
 
     it("returns false when no forms are accepted", () => {
+        // Arrange
         const answerForms = [];
-        expect(shouldShowExamples(answerForms)).toBe(false);
+
+        // Act
+        const result = shouldShowExamples(answerForms);
+
+        // Assert
+        expect(result).toBe(false);
+    });
+});
+
+describe("unionAnswerForms", () => {
+    it("returns an array of unique answer forms in the order provided", () => {
+        // Arrange
+        const answerForms: readonly PerseusNumericInputAnswerForm[][] = [
+            [
+                {
+                    name: "integer",
+                    simplify: "optional",
+                },
+                {
+                    name: "proper",
+                    simplify: "required",
+                },
+            ],
+            [
+                {
+                    name: "integer",
+                    simplify: "optional",
+                },
+                {
+                    name: "improper",
+                    simplify: "required",
+                },
+            ],
+        ];
+        const expected: readonly PerseusNumericInputAnswerForm[] = [
+            {
+                name: "integer",
+                simplify: "optional",
+            },
+            {
+                name: "proper",
+                simplify: "required",
+            },
+            {
+                name: "improper",
+                simplify: "required",
+            },
+        ];
+
+        // Act
+        const result = unionAnswerForms(answerForms);
+
+        // Assert
+        expect(result).toEqual(expected);
     });
 });
