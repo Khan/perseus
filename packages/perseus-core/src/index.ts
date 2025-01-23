5ba26de1f3098434ccf68f3386f40ac0d50fd209
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-01-23 10:36:40 -0600

    [LEMS-2737/default-collection] graded-group-set

--- packages/perseus-core/src/index.ts
+++ packages/perseus-core/src/index.ts
@@ -46,5 +46,7 @@ export {default as expressionLogic} from "./widgets/expression";
 export type {ExpressionDefaultWidgetOptions} from "./widgets/expression";
 export {default as gradedGroupLogic} from "./widgets/graded-group";
 export type {GradedGroupDefaultWidgetOptions} from "./widgets/graded-group";
+export {default as gradedGroupSetLogic} from "./widgets/graded-group-set";
+export type {GradedGroupSetDefaultWidgetOptions} from "./widgets/graded-group-set";
 
 export type * from "./widgets/logic-export.types";
