a32692da7c9caa0df8de2775b6b176535b63e8fb
handeyeco <matthew.c.tx@gmail.com>
2025-01-24 20:14:11 +0000

    Deploying to gh-pages from @ Khan/perseus@459c2507472f104f521b5410feaa64402d473a43 🚀

---
+++ assets/locked-point-settings.stories-4mFYWHMB.js
@@ -0,0 +1,38 @@
+import{j as c}from"./jsx-runtime-63Ea5SlK.js";import{r as s}from"./index-6oxdNXpR.js";import{L as i}from"./locked-point-settings-hVutJuJj.js";import{g as w}from"./util-l5rbt41W.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-sN6aPTh8.js";import"./index-Cj1jPHW9.js";import"./index-awljIyHI.js";import"./index-bRdc6KRF.js";import"./index-dmcq622U.js";import"./index-SAZ92ybN.js";import"./index-wB6JGB7j.js";import"./index-cljMqpCc.js";import"./index-TVweWxDl.js";import"./index-GucNpBmh.js";import"./locked-figure-aria-LU0KXpbc.js";import"./article-renderer-LJJ0dzmI.js";import"./util-FOHsj4WS.js";import"./version-zYAx5FCH.js";import"./index-default-4_ZsnO94.js";import"./perseus-error-l3K_anoI.js";import"./jquery-5v7aFUvu.js";import"./prop-types-217IGPZa.js";import"./mobile-keypad-Mpl4LMcg.js";import"./index-9gkyvru-.js";import"./index-7-BESUpx.js";import"./index-J2t_5nK1.js";import"./assertThisInitialized-4q6YPdh3.js";import"./tabbar-1DjuEemW.js";import"./item-E1d38aVD.js";import"./button-assets-CfriXPG5.js";import"./keypad-button-3XwtPQAL.js";import"./operators-page-U6PD7Rk8.js";import"./navigation-pad-lZjiukRW.js";import"./key-translator-UdoFRmc0.js";import"./enums-x5qaTru7.js";import"./renderer-wMIfRMtJ.js";import"./svg-image-5xpyV1oq.js";import"./index-dnMhQZ-1.js";import"./dependencies-CP7Uh8Kq.js";import"./fixed-to-responsive-for_tVF1.js";import"./constants-iPV6vHZm.js";import"./client-Rb4DelHy.js";import"./inline-icon-6fh0Wu1y.js";import"./icon-paths-5JCXzGsq.js";import"./image-loader-cBoFrbCq.js";import"./asset-context-H6Iqp7Gi.js";import"./i18n-context-QDJ9FYgZ.js";import"./tex-MX5FPdQh.js";import"./zoomable-_uYFBX1Q.js";import"./zoomable-tex-Jjwex-Ep.js";import"./perseus-api-_PWPZoDE.js";import"./index-IiwcBdIZ.js";import"./stub-tag-editor-3VEaZ-53.js";import"./text-list-editor-ND6Qift6.js";import"./lint-CRWxUAIQ.js";import"./index-0kkDMKyq.js";import"./index-ocYiwKv6.js";import"./Popper-Y5KDXl-P.js";import"./math-input-uzki6SP1.js";import"./index-SpvvvrhE.js";import"./input-with-examples-6hvrR8-q.js";import"./text-input-seUszbFD.js";import"./index-5tV1Z6Un.js";import"./tooltip-Lp8Mlfe3.js";import"./simple-keypad-input-34G9maEv.js";import"./base-radio-wC6868ig.js";import"./media-queries-gbofTbz-.js";import"./shared-4TdECMLk.js";import"./choice-L1bg8moc.js";import"./icon-7RFbyLiL.js";import"./choice-icon-KW9pymTU.js";import"./focus-ring-qYEF-qqz.js";import"./option-status-1BFtDegh.js";import"./choice-none-above-IQwhLS2a.js";import"./phet-simulation-FbTArG7F.js";import"./index-kNGCvM5b.js";import"./answer-choices-s3P-mnH5.js";import"./index-YAvX_VSA.js";import"./button-group-G5CZaedn.js";import"./graph-rY0-ZyCF.js";import"./index-smZ6iCr_.js";import"./tiny-invariant-bHgPayXn.js";import"./hud-ifw9Ofbw.js";import"./index-ioZjKB__.js";import"./multi-button-group-QUVHbBcE.js";import"./number-input-onekiaR5.js";import"./range-input-02KGzB5J.js";import"./marker-zdh3b9Bh.js";import"./answer-pill-D_PKRNTa.js";import"./sortable-EzVx6mI-.js";import"./video-transcript-link-S0aMUHE4.js";import"./perseus-item-_R-GGF4T.js";import"./hints-renderer-nfWPPow8.js";import"./components-t6YFtKn7.js";import"./locked-label-settings-0MX4-fCW.js";import"./scrollless-number-text-field-3cMs0Glk.js";import"./color-select-Sla6GnvT.js";import"./labeled-switch-CvF1khBj.js";const Et={title:"PerseusEditor/Components/Locked Point Settings",component:i},r=m=>c.jsx(i,{...m}),u={...w("point"),onChangeProps:()=>{},onMove:()=>{},onRemove:()=>{}};r.args=u;const n={render:function(){const[t,e]=s.useState(u),o=p=>{e({...t,...p})};return c.jsx(i,{...t,onChangeProps:o})}};n.parameters={chromatic:{disableSnapshot:!0}};const a={render:function(){const[t,e]=s.useState(!0),[o,p]=s.useState(u),P=l=>{p({...o,...l})};return c.jsx(i,{...o,expanded:t,onToggle:e,onChangeProps:P})}},d={render:function(){const[t,e]=s.useState(!0),[o,p]=s.useState(u),P=l=>{p({...o,...l})};return c.jsx(i,{...o,expanded:t,onToggle:e,onChangeProps:P})}};r.__docgenInfo={description:"",methods:[],displayName:"Default"};var g,x,f;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`(args): React.ReactElement => {
+  return <LockedPointSettings {...args} />;
+}`,...(f=(x=r.parameters)==null?void 0:x.docs)==null?void 0:f.source}}};var S,h,E;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: function Render() {
+    const [props, setProps] = React.useState(defaultProps);
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedPointSettings {...props} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(E=(h=n.parameters)==null?void 0:h.docs)==null?void 0:E.source}}};var R,C,U;a.parameters={...a.parameters,docs:{...(R=a.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: function Render() {
+    const [expanded, setExpanded] = React.useState(true);
+    const [props, setProps] = React.useState(defaultProps);
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedPointSettings {...props} expanded={expanded} onToggle={setExpanded} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(U=(C=a.parameters)==null?void 0:C.docs)==null?void 0:U.source}}};var k,L,j;d.parameters={...d.parameters,docs:{...(k=d.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: function Render() {
+    const [expanded, setExpanded] = React.useState(true);
+    const [props, setProps] = React.useState(defaultProps);
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedPointSettings {...props} expanded={expanded} onToggle={setExpanded} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(j=(L=d.parameters)==null?void 0:L.docs)==null?void 0:j.source}}};const Rt=["Default","Controlled","Expanded","ExpandedNondefaultProps"];export{n as Controlled,r as Default,a as Expanded,d as ExpandedNondefaultProps,Rt as __namedExportsOrder,Et as default};
---
+++ assets/register-all-widgets-for-testing-lBbNJnNF.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-LJJ0dzmI.js";import{r,a as s}from"./renderer-wMIfRMtJ.js";const a=()=>{r(e),s()};export{a as r};
