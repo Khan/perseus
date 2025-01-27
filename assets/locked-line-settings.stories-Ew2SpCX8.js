2a7c9bb07887badadc05f69f2dcf66342d1be396
mark-fitzgerald <13896410+mark-fitzgerald@users.noreply.github.com>
2025-01-27 19:34:39 +0000

    Deploying to gh-pages from @ Khan/perseus@46623c8f19bedd15b3b29a1b156889aef3f91876 🚀

---
+++ assets/locked-line-settings.stories-Ew2SpCX8.js
@@ -0,0 +1,56 @@
+import{j as d}from"./jsx-runtime-63Ea5SlK.js";import{r as n}from"./index-6oxdNXpR.js";import{L as s}from"./locked-line-settings-FAjBtINl.js";import{g as h}from"./util-6fn7Qq5r.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./util-IRB86axJ.js";import"./index-sg7rMpMn.js";import"./index-default-4_ZsnO94.js";import"./perseus-error-l3K_anoI.js";import"./jquery-5v7aFUvu.js";import"./svg-image-baEPfRKx.js";import"./index-wB6JGB7j.js";import"./index-awljIyHI.js";import"./index-Cj1jPHW9.js";import"./index-dmcq622U.js";import"./index-dnMhQZ-1.js";import"./dependencies-CP7Uh8Kq.js";import"./fixed-to-responsive-for_tVF1.js";import"./constants-iPV6vHZm.js";import"./index-J2t_5nK1.js";import"./client-Rb4DelHy.js";import"./index-9gkyvru-.js";import"./inline-icon-6fh0Wu1y.js";import"./icon-paths-5JCXzGsq.js";import"./image-loader-cBoFrbCq.js";import"./index-sN6aPTh8.js";import"./index-bRdc6KRF.js";import"./index-SAZ92ybN.js";import"./index-cljMqpCc.js";import"./index-TVweWxDl.js";import"./answer-choices-ZZEE2d0p.js";import"./index-GucNpBmh.js";import"./index-5tV1Z6Un.js";import"./index-7-BESUpx.js";import"./assertThisInitialized-4q6YPdh3.js";import"./Popper-Y5KDXl-P.js";import"./index-ocYiwKv6.js";import"./index-YAvX_VSA.js";import"./i18n-context-QDJ9FYgZ.js";import"./renderer-7Qx8ywNo.js";import"./asset-context-H6Iqp7Gi.js";import"./tex-MX5FPdQh.js";import"./zoomable-_uYFBX1Q.js";import"./zoomable-tex-Jjwex-Ep.js";import"./perseus-api-_PWPZoDE.js";import"./index-o42urCig.js";import"./stub-tag-editor-3VEaZ-53.js";import"./text-list-editor-ND6Qift6.js";import"./lint-CRWxUAIQ.js";import"./locked-figure-aria-hswLgzwU.js";import"./article-renderer-peYbzV9W.js";import"./prop-types-N3hRzVvo.js";import"./mobile-keypad-ouUQoZEt.js";import"./tabbar-1DjuEemW.js";import"./item-E1d38aVD.js";import"./button-assets-Da0RVw4N.js";import"./keypad-button-Y9pECVQW.js";import"./operators-page-mstaIoXO.js";import"./navigation-pad-xInTxGPV.js";import"./key-translator-S9inRGT8.js";import"./enums-x5qaTru7.js";import"./index-0kkDMKyq.js";import"./math-input-6HIKB8TT.js";import"./index-SpvvvrhE.js";import"./input-with-examples-s8OwiJ6h.js";import"./text-input-dNVzcDwH.js";import"./tooltip-Lp8Mlfe3.js";import"./simple-keypad-input-PQTXqna_.js";import"./base-radio-YK_gwAr5.js";import"./media-queries-gbofTbz-.js";import"./shared-4TdECMLk.js";import"./choice-L1bg8moc.js";import"./icon-7RFbyLiL.js";import"./choice-icon-KW9pymTU.js";import"./focus-ring-qYEF-qqz.js";import"./option-status-1BFtDegh.js";import"./choice-none-above-WPZedyeQ.js";import"./phet-simulation-FbTArG7F.js";import"./index-kNGCvM5b.js";import"./button-group-G5CZaedn.js";import"./graph-FZyQZ4hX.js";import"./index-smZ6iCr_.js";import"./tiny-invariant-bHgPayXn.js";import"./hud-ifw9Ofbw.js";import"./index-ioZjKB__.js";import"./multi-button-group-QUVHbBcE.js";import"./number-input-cOifiEn8.js";import"./range-input-fLCjpotd.js";import"./marker-bv-gzDt6.js";import"./answer-pill-8HOjOXsO.js";import"./sortable-3tEWFv0U.js";import"./video-transcript-link-S0aMUHE4.js";import"./perseus-item-wAAt8iHv.js";import"./hints-renderer-Ki3ObVzO.js";import"./components-0Om0f5Tw.js";import"./trash-bold-YHlF4CST.js";import"./color-select-XDSJW_Ew.js";import"./line-stroke-select-tIsgraSY.js";import"./line-swatch-KfOefa_t.js";import"./locked-label-settings-ODb2zpy3.js";import"./scrollless-number-text-field-3cMs0Glk.js";import"./locked-point-settings-QA3dyTSc.js";import"./labeled-switch-qCGI4AVh.js";const Tt={title:"PerseusEditor/Components/Locked Line Settings",component:s},p=i=>d.jsx(s,{...i}),c={...h("line"),onChangeProps:()=>{},onMove:()=>{},onRemove:()=>{}};p.args=c;const m={render:function(){const[e,r]=n.useState(c),t=o=>{r({...e,...o})};return d.jsx(s,{...e,onChangeProps:t})}};m.parameters={chromatic:{disableSnapshot:!0}};const a={render:function(){const[e,r]=n.useState(c),t=o=>{r({...e,...o})};return d.jsx(s,{...e,points:[h("point"),h("point")],expanded:!0,onChangeProps:t})}},u={render:function(){const[e,r]=n.useState(!0),[t,o]=n.useState(c),P=g=>{o({...t,...g})};return d.jsx(s,{...t,expanded:e,onToggle:r,onChangeProps:P})}},l={render:function(){const[e,r]=n.useState(!0),[t,o]=n.useState({...c,kind:"segment",color:"green",lineStyle:"dashed"}),P=g=>{o({...t,...g})};return d.jsx(s,{...t,expanded:e,onToggle:r,onChangeProps:P})}};p.__docgenInfo={description:"",methods:[],displayName:"Default"};var f,x,S;p.parameters={...p.parameters,docs:{...(f=p.parameters)==null?void 0:f.docs,source:{originalSource:`(args): React.ReactElement => {
+  return <LockedLineSettings {...args} />;
+}`,...(S=(x=p.parameters)==null?void 0:x.docs)==null?void 0:S.source}}};var R,L,E;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: function Render() {
+    const [props, setProps] = React.useState(defaultProps);
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedLineSettings {...props} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(E=(L=m.parameters)==null?void 0:L.docs)==null?void 0:E.source}}};var C,U,k,w,y;a.parameters={...a.parameters,docs:{...(C=a.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: function Render() {
+    const [props, setProps] = React.useState(defaultProps);
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedLineSettings {...props} points={[getDefaultFigureForType("point"), getDefaultFigureForType("point")]} expanded={true} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(k=(U=a.parameters)==null?void 0:U.docs)==null?void 0:k.source},description:{story:`If the two points defining the line are the same, the line is invalid
+as that would give it a length of 0. An error message is displayed
+in this case.`,...(y=(w=a.parameters)==null?void 0:w.docs)==null?void 0:y.description}}};var j,T,v;u.parameters={...u.parameters,docs:{...(j=u.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: function Render() {
+    const [expanded, setExpanded] = React.useState(true);
+    const [props, setProps] = React.useState(defaultProps);
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedLineSettings {...props} expanded={expanded} onToggle={setExpanded} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(v=(T=u.parameters)==null?void 0:T.docs)==null?void 0:v.source}}};var D,F,_;l.parameters={...l.parameters,docs:{...(D=l.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: function Render() {
+    const [expanded, setExpanded] = React.useState(true);
+    const [props, setProps] = React.useState({
+      ...defaultProps,
+      kind: "segment" as const,
+      color: "green" as const,
+      lineStyle: "dashed" as const
+    });
+    const handlePropsUpdate = newProps => {
+      setProps({
+        ...props,
+        ...newProps
+      });
+    };
+    return <LockedLineSettings {...props} expanded={expanded} onToggle={setExpanded} onChangeProps={handlePropsUpdate} />;
+  }
+}`,...(_=(F=l.parameters)==null?void 0:F.docs)==null?void 0:_.source}}};const vt=["Default","Controlled","WithInvalidPoints","Expanded","ExpandedNondefaultProps"];export{m as Controlled,p as Default,u as Expanded,l as ExpandedNondefaultProps,a as WithInvalidPoints,vt as __namedExportsOrder,Tt as default};
