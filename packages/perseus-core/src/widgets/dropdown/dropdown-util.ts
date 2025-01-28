b266ffda1e42c5775852009df0e5b5b2bb0a4381
Tamara <tamara@khanacademy.org>
2025-01-28 16:00:18 -0600

    [tb/LEMS-2597/dropdown-split-out-validation] Add dropdown public widget options and type

---
+++ packages/perseus-core/src/widgets/dropdown/dropdown-util.ts
@@ -0,0 +1,31 @@
+import type {PerseusDropdownWidgetOptions} from "@khanacademy/perseus-core";
+
+/**
+ * For details on the individual options, see the
+ * PerseusDropdownWidgetOptions type
+ */
+type DropdownPublicWidgetOptions = {
+    choices: ReadonlyArray<{content: string}>;
+    placeholder: PerseusDropdownWidgetOptions["placeholder"];
+    static: PerseusDropdownWidgetOptions["static"];
+    visibleLabel?: PerseusDropdownWidgetOptions["visibleLabel"];
+    ariaLabel?: PerseusDropdownWidgetOptions["ariaLabel"];
+};
+
+/**
+ * Given a PerseusDropdownWidgetOptions object, return a new object with only
+ * the public options that should be exposed to the client.
+ */
+function getDropdownPublicWidgetOptions(
+    options: PerseusDropdownWidgetOptions,
+): DropdownPublicWidgetOptions {
+    return {
+        choices: options.choices.map((choice) => ({content: choice.content})),
+        placeholder: options.placeholder,
+        static: options.static,
+        visibleLabel: options.visibleLabel,
+        ariaLabel: options.ariaLabel,
+    };
+}
+
+export default getDropdownPublicWidgetOptions;
