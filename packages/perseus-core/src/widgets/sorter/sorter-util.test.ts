a1c36307ec10769d7927aec0ff9ef1d473052a4b
Tamara <tamara@khanacademy.org>
2025-01-28 11:09:34 -0600

    [tb/LEMS-2765/sorter-split] Update expect to have sorted options

--- packages/perseus-core/src/widgets/sorter/sorter-util.test.ts
+++ packages/perseus-core/src/widgets/sorter/sorter-util.test.ts
@@ -6,7 +6,7 @@ describe("getSorterPublicWidgetOptions", () => {
     it("should return the correct public options without any answer data", () => {
         // Arrange
         const options: PerseusSorterWidgetOptions = {
-            correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
+            correct: ["$15$ grams", "$55$ grams", "$0.005$ kilograms"],
             layout: "horizontal",
             padding: true,
         };
@@ -16,6 +16,7 @@ describe("getSorterPublicWidgetOptions", () => {
 
         // Assert
         expect(publicWidgetOptions).toEqual({
+            correct: ["$0.005$ kilograms", "$15$ grams", "$55$ grams"],
             layout: "horizontal",
             padding: true,
         });
