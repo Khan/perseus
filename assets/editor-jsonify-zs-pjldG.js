7791c2f7ed4a375583d82138d70939c4723dc04e
benchristel <benchristel@users.noreply.github.com>
2025-01-29 22:31:34 +0000

    Deploying to gh-pages from @ Khan/perseus@a470c799eb53c87e08fb2f829b27e114ca80f63f 🚀

--- assets/editor-jsonify-zs-pjldG.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index-default-4_ZsnO94.js";import{d as i}from"./article-renderer-NINt4j4t.js";const s={serialize:function(){return o.omit(this.props,i)}};export{s as E};
---
+++ assets/register-all-widgets-for-testing-xSlcQI4z.js
@@ -0,0 +1,1 @@
+import{w as e}from"./article-renderer-IGp9zLf8.js";import{r,a as s}from"./renderer-BI1FyBLz.js";const a=()=>{r(e),s()};export{a as r};
