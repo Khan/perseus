140345c0dc1ce02f19765d4e781cbea8a628dfcb
Tamara <tamara@khanacademy.org>
2025-01-30 09:52:18 -0600

    [tb/LEMS-2767/numeric-input-public-options] Add a test for the new function

---
+++ packages/perseus-core/src/widgets/numeric-input/numeric-input-util.test.ts
@@ -0,0 +1,39 @@
+import getNumericInputPublicWidgetOptions from "./numeric-input-util";
+
+import type {PerseusNumericInputWidgetOptions} from "../../data-schema";
+
+describe("getNumericInputPublicWidgetOptions", () => {
+    it("should return the correct public options without any answer data", () => {
+        // Arrange
+        const options: PerseusNumericInputWidgetOptions = {
+            answers: [
+                {
+                    status: "correct",
+                    maxError: null,
+                    strict: false,
+                    value: 1252,
+                    simplify: "required",
+                    message: "",
+                },
+            ],
+            labelText: "labelText",
+            size: "Normal",
+            coefficient: false,
+            rightAlign: false,
+            static: false,
+            answerForms: [{simplify: "required", name: "decimal"}],
+        };
+
+        // Act
+        const publicWidgetOptions = getNumericInputPublicWidgetOptions(options);
+
+        // Assert
+        expect(publicWidgetOptions).toEqual({
+            labelText: "labelText",
+            size: "Normal",
+            coefficient: false,
+            rightAlign: false,
+            static: false,
+        });
+    });
+});
