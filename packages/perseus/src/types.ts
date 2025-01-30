c7b55efc8065406d6a706567d990af5c4e369b84
Tamara <tamara@khanacademy.org>
2025-01-30 09:53:18 -0600

    [tb/LEMS-2767/numeric-input-public-options] Add new function to union of public widget functions

--- packages/perseus/src/types.ts
+++ packages/perseus/src/types.ts
@@ -18,6 +18,7 @@ import type {
     getExpressionPublicWidgetOptions,
     getSorterPublicWidgetOptions,
     getDropdownPublicWidgetOptions,
+    getNumericInputPublicWidgetOptions,
 } from "@khanacademy/perseus-core";
 import type {LinterContextProps} from "@khanacademy/perseus-linter";
 import type {
@@ -544,6 +545,7 @@ export type WidgetScorerFunction = (
  * A union type of all the functions that provide public widget options.
  */
 export type PublicWidgetOptionsFunction =
+    | typeof getNumericInputPublicWidgetOptions
     | typeof getDropdownPublicWidgetOptions
     | typeof getCategorizerPublicWidgetOptions
     | typeof getOrdererPublicWidgetOptions
