c53e138b7f9141e8135df541fef66588ff6b8245
Nisha Yerunkar <nisha@khanacademy.org>
2025-02-04 14:19:40 -0800

    [kmath-radian-to-degree] Add a `convertRadiansToDegrees` function to kmath
    Currently kmath has a `convertDegreesToRadians` function, but not the other way around.
    
    - Add `convertRadiansToDegrees` function to kmath
    - Replace all the identical function in other repos with this new one
    
    Issue: none
    
    Test plan:
    `yarn jest packages/kmath/src/angles.test.ts`
    `yarn jest`

--- packages/kmath/src/angles.test.ts
+++ packages/kmath/src/angles.test.ts
@@ -1,4 +1,8 @@
-import {calculateAngleInDegrees} from "./angles";
+import {
+    calculateAngleInDegrees,
+    convertDegreesToRadians,
+    convertRadiansToDegrees,
+} from "./angles";
 
 describe("calculateAngleInDegrees", () => {
     it.each`
@@ -14,4 +18,42 @@ describe("calculateAngleInDegrees", () => {
         const {expected, x, y} = params;
         expect(calculateAngleInDegrees([x, y])).toBe(expected);
     });
+});
+
+describe("convertDegreesToRadians", () => {
+    test.each`
+        degrees | radians
+        ${0}    | ${0}
+        ${45}   | ${Math.PI / 4}
+        ${90}   | ${Math.PI / 2}
+        ${180}  | ${Math.PI}
+        ${270}  | ${Math.PI * 1.5}
+        ${360}  | ${Math.PI * 2}
+        ${-45}  | ${-Math.PI / 4}
+        ${-90}  | ${-Math.PI / 2}
+    `(
+        "should convert $degrees degrees to $radians radians",
+        ({degrees, radians}) => {
+            expect(convertDegreesToRadians(degrees)).toBe(radians);
+        },
+    );
+});
+
+describe("convertRadiansToDegrees", () => {
+    test.each`
+        radians          | degrees
+        ${0}             | ${0}
+        ${Math.PI / 4}   | ${45}
+        ${Math.PI / 2}   | ${90}
+        ${Math.PI}       | ${180}
+        ${Math.PI * 1.5} | ${270}
+        ${Math.PI * 2}   | ${360}
+        ${-Math.PI / 4}  | ${-45}
+        ${-Math.PI / 2}  | ${-90}
+    `(
+        "should convert $radians radians to $degrees degrees",
+        ({radians, degrees}) => {
+            expect(convertRadiansToDegrees(radians)).toBe(degrees);
+        },
+    );
 });
