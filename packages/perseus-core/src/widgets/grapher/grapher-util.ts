c820d94ae06f2e3887ab82def5bf427e1ce0f119
Ben Christel <benchristel@khanacademy.org>
2025-02-05 13:12:41 -0800

    [benc/grapher] Add GrapherPublicWidgetOptions type

--- packages/perseus-core/src/widgets/grapher/grapher-util.ts
+++ packages/perseus-core/src/widgets/grapher/grapher-util.ts
@@ -1,8 +1,13 @@
 import type {PerseusGrapherWidgetOptions} from "@khanacademy/perseus-core";
 
+type GrapherPublicWidgetOptions = Pick<
+    PerseusGrapherWidgetOptions,
+    "availableTypes" | "graph"
+>;
+
 export default function getGrapherPublicWidgetOptions(
     options: PerseusGrapherWidgetOptions,
-) {
+): GrapherPublicWidgetOptions {
     const {correct: _, ...publicOptions} = options;
     return publicOptions;
 }
