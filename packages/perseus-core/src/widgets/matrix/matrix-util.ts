dadff7a7868d1ddcaf28130d48c89dfa05f2839a
Ben Christel <benchristel@khanacademy.org>
2025-02-06 13:13:08 -0800

    [benc/matrix] Add MatrixPublicWidgetOptions type

--- packages/perseus-core/src/widgets/matrix/matrix-util.ts
+++ packages/perseus-core/src/widgets/matrix/matrix-util.ts
@@ -1,8 +1,13 @@
 import type {PerseusMatrixWidgetOptions} from "@khanacademy/perseus-core";
 
+type MatrixPublicWidgetOptions = Pick<
+    PerseusMatrixWidgetOptions,
+    "prefix" | "suffix" | "cursorPosition" | "matrixBoardSize" | "static"
+>;
+
 export default function getMatrixPublicWidgetOptions(
     options: PerseusMatrixWidgetOptions,
-) {
+): MatrixPublicWidgetOptions {
     const {answers: _, ...publicOptions} = options;
     return publicOptions;
 }
