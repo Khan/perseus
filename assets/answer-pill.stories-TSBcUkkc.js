277c2f404a4ae7e688c9aeea2b347a465ba1868e
nishasy <nishasy@gmail.com>
2025-02-06 19:44:17 +0000

    Deploying to gh-pages from @ Khan/perseus@47cebd20fdcf1f47c50f30901f0a1e1654ae4790 🚀

---
+++ assets/answer-pill.stories-TSBcUkkc.js
@@ -0,0 +1,35 @@
+import{j as C}from"./jsx-runtime-63Ea5SlK.js";import{A as t}from"./answer-pill-t0u0FSHa.js";import"./index-6oxdNXpR.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-JoFbAW28.js";import"./no-important-xCWWYXQR.js";import"./index-1DoKu8mP.js";import"./index-vSZRSFNi.js";import"./index-dmcq622U.js";import"./index-cEypnc5R.js";import"./Popper-Y5KDXl-P.js";import"./index-9gkyvru-.js";import"./i18n-context-dE2bX2K_.js";import"./renderer-s0nXcNLd.js";import"./index-default-4_ZsnO94.js";import"./core-widget-registry-uiKfW1Am.js";import"./perseus-error-l3K_anoI.js";import"./util-CFc6mjZH.js";import"./jquery-5v7aFUvu.js";import"./get-decimal-separator-c07pHhM9.js";import"./index-J2t_5nK1.js";import"./index-dnMhQZ-1.js";import"./asset-context-H6Iqp7Gi.js";import"./svg-image-ZSuPTax3.js";import"./index-N3o2sWJb.js";import"./dependencies-CP7Uh8Kq.js";import"./fixed-to-responsive-m4E_8Ehf.js";import"./constants-NhstHO4m.js";import"./client-Rb4DelHy.js";import"./inline-icon-6fh0Wu1y.js";import"./icon-paths-5JCXzGsq.js";import"./image-loader-cBoFrbCq.js";import"./tex-MX5FPdQh.js";import"./zoomable-pOEbOEqK.js";import"./zoomable-tex-ZNdgOHRd.js";import"./perseus-api-DO0X8arb.js";import"./index-IiwcBdIZ.js";import"./stub-tag-editor-3VEaZ-53.js";import"./text-list-editor-ND6Qift6.js";import"./lint-nh0b87yM.js";const nr={title:"Perseus/Widgets/Label Image/Answer Pill",component:t,render:k=>C.jsx(t,{...k}),argTypes:{showCorrectness:{options:["not answered","correct","incorrect"],control:{type:"radio"}}}},r={args:{id:"1",selectedAnswers:["Answer Pill"],markerRef:null,side:"top",onClick:()=>{}}},e={args:{id:"1",selectedAnswers:["Answer 1","Answer 2"],markerRef:null,side:"top",onClick:()=>{}}},s={args:{id:"1",selectedAnswers:["Right Answer"],showCorrectness:"correct",markerRef:null,side:"top",onClick:()=>{}}},o={args:{id:"1",selectedAnswers:["Wrong Answer"],showCorrectness:"incorrect",markerRef:null,side:"top",onClick:()=>{}}};var n,i,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    id: "1",
+    selectedAnswers: ["Answer Pill"],
+    markerRef: null,
+    side: "top",
+    onClick: () => {}
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var c,m,a;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    id: "1",
+    selectedAnswers: ["Answer 1", "Answer 2"],
+    markerRef: null,
+    side: "top",
+    onClick: () => {}
+  }
+}`,...(a=(m=e.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var l,d,w;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    id: "1",
+    selectedAnswers: ["Right Answer"],
+    showCorrectness: "correct",
+    markerRef: null,
+    side: "top",
+    onClick: () => {}
+  }
+}`,...(w=(d=s.parameters)==null?void 0:d.docs)==null?void 0:w.source}}};var u,A,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    id: "1",
+    selectedAnswers: ["Wrong Answer"],
+    showCorrectness: "incorrect",
+    markerRef: null,
+    side: "top",
+    onClick: () => {}
+  }
+}`,...(g=(A=o.parameters)==null?void 0:A.docs)==null?void 0:g.source}}};const ir=["SingleAnswer","MultipleAnswers","Correct","Incorrect"];export{s as Correct,o as Incorrect,e as MultipleAnswers,r as SingleAnswer,ir as __namedExportsOrder,nr as default};
--- assets/register-all-widgets-for-testing-6GRLHj9G.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./article-renderer-znkpT2H4.js";import{r,a as s}from"./renderer-_GA2c-kZ.js";const a=()=>{r(e),s()};export{a as r};
