a2a0b69fb24933f18d8eba42d2915ec12589a45b
Ben Christel <benchristel@khanacademy.org>
2025-02-07 09:25:01 -0800

    [benc/grapher] Add magic comment to fix the build

--- packages/perseus/src/types.ts
+++ packages/perseus/src/types.ts
@@ -3,6 +3,12 @@ import type {PerseusStrings} from "./strings";
 import type {SizeClass} from "./util/sizing-utils";
 import type {WidgetPromptJSON} from "./widget-ai-utils/prompt-types";
 import type {KeypadAPI} from "@khanacademy/math-input";
+// MAGIC: Removal of this comment may cause `tsc --build` to output a syntax
+// error in packages/perseus/dist/server-item-renderer.d.ts and then fail. This
+// appears to be a bug introduced in TS 5.6. If you are curious about this, try
+// deleting this comment and running:
+//     rm -rf packages/*/{dist,tsconfig-build.tsbuildinfo} && yarn build:types
+// If that succeeds, maybe the bug has been fixed.
 import type {
     getGrapherPublicWidgetOptions,
     getInteractiveGraphPublicWidgetOptions,
