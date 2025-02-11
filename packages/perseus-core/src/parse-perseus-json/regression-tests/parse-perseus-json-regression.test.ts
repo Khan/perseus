63d5371d8f230ee18a13c026c29728c43c5de5af
Ben Christel <benchristel@khanacademy.org>
2025-02-11 14:32:32 -0800

    [benc/parse-scientific] Simplify processing of parse failures in regression tests

--- packages/perseus-core/src/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
+++ packages/perseus-core/src/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
@@ -1,7 +1,9 @@
 import * as fs from "fs";
 import {join} from "path";
 
+import {anySuccess} from "../general-purpose-parsers/test-helpers";
 import {parseAndMigratePerseusItem} from "../index";
+import {mapFailure} from "../result";
 
 const dataFiles = fs.readdirSync(join(__dirname, "data"));
 
@@ -11,13 +13,16 @@ describe("parseAndTypecheckPerseusItem", () => {
             join(__dirname, "data", filename),
             "utf-8",
         );
-        const result = parseAndMigratePerseusItem(json);
 
-        // This strange-looking assertion style results in the failure message
-        // being printed if parsing fails, so the test is easier to debug.
-        expect((result as any).detail?.message).toBe(undefined);
-        // Backstop assertion in case the format of failures changes so
-        // `detail.message` doesn't exist anymore.
-        expect(result).toEqual(expect.objectContaining({type: "success"}));
+        // If the parse fails, get just the error message. This makes the test
+        // failure easier to read, since otherwise the entire `invalidObject`
+        // from the ParseFailureDetail would be printed.
+        const result = mapFailure(getMessage)(parseAndMigratePerseusItem(json));
+
+        expect(result).toEqual(anySuccess);
     });
 });
+
+function getMessage(obj: {message: string}): string {
+    return obj.message;
+}
