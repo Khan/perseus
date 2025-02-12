f3ea4363ce0912ddcd93ab84f2fb4dbce87c9d2a
Nisha Yerunkar <nisha@khanacademy.org>
2025-02-12 15:27:55 -0800

    [circle-hairlines] [LX] Add hairlines when Circle center has focus
    Right now, all Interactive Graph movable points have hairlines when
    they're being focused or drags. The exception being the center of a
    Circle graph because it's not a MovablePoint.
    
    Adding the Circle center hairlines here.
    
    I also refactored the hairlines to be a separate component so they
    can be reused in multiple places.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-2310
    
    Test plan:
    `yarn jest packages/perseus/src/widgets/interactive-graphs/graphs/circle.test.tsx`
    
    Storybook
    http://localhost:6006/?path=/story/perseuseditor-widgets-interactive-graph--interactive-graph-circle

--- packages/perseus/src/widgets/interactive-graphs/graphs/circle.tsx
+++ packages/perseus/src/widgets/interactive-graphs/graphs/circle.tsx
@@ -9,6 +9,7 @@ import {actions} from "../reducer/interactive-graph-action";
 import {getRadius} from "../reducer/interactive-graph-state";
 import useGraphConfig from "../reducer/use-graph-config";
 
+import Hairlines from "./components/hairlines";
 import {MovablePoint} from "./components/movable-point";
 import {srFormatNumber} from "./screenreader-text";
 import {useDraggable} from "./use-draggable";
@@ -40,7 +41,8 @@ export function renderCircleGraph(
 
 type CircleGraphProps = MafsGraphProps<CircleGraphState>;
 
-function CircleGraph(props: CircleGraphProps) {
+// Exported for testing
+export function CircleGraph(props: CircleGraphProps) {
     const {dispatch, graphState} = props;
     const {center, radiusPoint} = graphState;
 
@@ -124,6 +126,7 @@ function MovableCircle(props: {
 }) {
     const {id, ariaLabel, ariaDescribedBy, center, radius, onMove} = props;
     const {snapStep, disableKeyboardInteraction} = useGraphConfig();
+    const [focused, setFocused] = React.useState(false);
 
     const draggableRef = useRef<SVGGElement>(null);
 
@@ -146,6 +149,8 @@ function MovableCircle(props: {
             role="button"
             tabIndex={disableKeyboardInteraction ? -1 : 0}
             className={`movable-circle ${dragging ? "movable-circle--dragging" : ""}`}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
         >
             <ellipse
                 className="focus-ring"
@@ -162,21 +167,31 @@ function MovableCircle(props: {
                 rx={radiiPx[X]}
                 ry={radiiPx[Y]}
             />
-            <DragHandle center={center} />
+            <DragHandle center={center} dragging={dragging} focused={focused} />
         </g>
     );
 }
 
 const dragHandleDimensions: vec.Vector2 = [24, 14];
 const dragHandleDotPositions = crossProduct([-4.4, 0, 4.4], [-2.1, 2.1]);
-function DragHandle(props: {center: [x: number, y: number]}) {
-    const {center} = props;
-    const cornerRadius = Math.min(...dragHandleDimensions) / 2;
+function DragHandle(props: {
+    center: [x: number, y: number];
+    dragging: boolean;
+    focused: boolean;
+}) {
+    const {center, dragging, focused} = props;
+
     const [centerPx] = useTransformVectorsToPixels(center);
+    const {markings} = useGraphConfig();
+
+    const cornerRadius = Math.min(...dragHandleDimensions) / 2;
     const topLeft = vec.sub(centerPx, vec.scale(dragHandleDimensions, 0.5));
+
+    const showHairlines = (dragging || focused) && markings !== "none";
 
     return (
         <>
+            {showHairlines && <Hairlines point={center} />}
             <rect
                 className="movable-circle-handle"
                 x={topLeft[X]}
