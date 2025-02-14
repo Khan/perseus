353528be376c15985b3caddc3a3e8f0033b0e96b
Ben Christel <benchristel@khanacademy.org>
2025-02-14 11:47:55 -0800

    [benc/fix-prod-parser-errors] Add failing regression test for article

--- packages/perseus-core/src/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
+++ packages/perseus-core/src/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
@@ -2,14 +2,21 @@ import * as fs from "fs";
 import {join} from "path";
 
 import {anySuccess} from "../general-purpose-parsers/test-helpers";
-import {parseAndMigratePerseusItem} from "../index";
+import {
+    parseAndMigratePerseusArticle,
+    parseAndMigratePerseusItem
+} from "../index";
 import {assertSuccess, mapFailure} from "../result";
 
 const itemDataDir = join(__dirname, "item-data");
-const dataFiles = fs.readdirSync(itemDataDir);
+const itemDataFiles = fs.readdirSync(itemDataDir);
+
+const articleDataDir = join(__dirname, "article-data");
+const articleDataFiles = fs.readdirSync(articleDataDir);
+
 
 describe("parseAndMigratePerseusItem", () => {
-    describe.each(dataFiles)("given %s", (filename) => {
+    describe.each(itemDataFiles)("given %s", (filename) => {
         const json = fs.readFileSync(join(itemDataDir, filename), "utf-8");
         const result = parseAndMigratePerseusItem(json);
 
@@ -18,6 +25,27 @@ describe("parseAndMigratePerseusItem", () => {
             // failure easier to read, since otherwise the entire `invalidObject`
             // from the ParseFailureDetail would be printed.
             const result = mapFailure(getMessage)(parseAndMigratePerseusItem(json));
+
+            expect(result).toEqual(anySuccess);
+        });
+
+        it("returns the same result as before", () => {
+            assertSuccess(result);
+            expect(result.value).toMatchSnapshot();
+        })
+    });
+});
+
+describe("parseAndMigratePerseusArticle", () => {
+    describe.each(articleDataFiles)("given %s", (filename) => {
+        const json = fs.readFileSync(join(articleDataDir, filename), "utf-8");
+        const result = parseAndMigratePerseusArticle(json);
+
+        it("parses successfully", () => {
+            // If the parse fails, get just the error message. This makes the test
+            // failure easier to read, since otherwise the entire `invalidObject`
+            // from the ParseFailureDetail would be printed.
+            const result = mapFailure(getMessage)(parseAndMigratePerseusArticle(json));
 
             expect(result).toEqual(anySuccess);
         });
