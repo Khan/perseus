c565e26d4cb56ec7114beb5a563f82a2993751b6
Nisha Yerunkar <nisha@khanacademy.org>
2025-02-14 10:30:08 -0800

    [Interactive Graph] Replace string-based function call with switch statement (#2244)
    ## Summary:
    There's a part in the interactive graph code where the equation string
    getter function is called by a code that concatonates a string together
    and uses that string as a function name to call.
    
    For the sake of type safety and the wellbeing of our engineers, updating
    this to a switch statement.
    
    Issue: none
    
    ## Test plan:
    `yarn jest packages/perseus-editor/src/widgets/__tests__/interactive-graph-editor.test.tsx`
    
    Storybook
    - http://localhost:6006/?path=/story/perseuseditor-editorpage--demo
    
    Author: nishasy
    
    Reviewers: benchristel
    
    Required Reviewers:
    
    Approved By: benchristel
    
    Checks: ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2244

---
+++ .changeset/spotty-humans-prove.md
@@ -0,0 +1,6 @@
+---
+"@khanacademy/perseus": patch
+"@khanacademy/perseus-editor": patch
+---
+
+[Interactive Graph] Replace string-based function call with switch statement
--- packages/perseus-editor/src/widgets/__tests__/interactive-graph-editor.test.tsx
+++ packages/perseus-editor/src/widgets/__tests__/interactive-graph-editor.test.tsx
@@ -808,4 +808,34 @@ describe("InteractiveGraphEditor", () => {
         await userEvent.click(dropdown);
         expect(screen.getByRole("option", {name: "None"})).toBeInTheDocument();
     });
+
+    test.each`
+        graphType          | expectedNumber | correctAnswer
+        ${"linear"}        | ${1}           | ${"y = 5.000"}
+        ${"quadratic"}     | ${2}           | ${"y = 0.400x^2 + 0.000x + -5.000"}
+        ${"sinusoid"}      | ${2}           | ${"y = 2.000sin(0.524x - 0.000) + 0.000"}
+        ${"circle"}        | ${1}           | ${"center (0, 0), radius 2"}
+        ${"linear-system"} | ${1}           | ${"y = 0.000x + 5.000 y = 0.000x + -5.000 Lines are parallel"}
+        ${"point"}         | ${1}           | ${"(0, 0)"}
+        ${"segment"}       | ${1}           | ${"[(-5, 5) (5, 5)]"}
+        ${"ray"}           | ${1}           | ${"y = 5.000 (for x >= -5.000)"}
+        ${"polygon"}       | ${1}           | ${"(3, -2) (0, 4) (-3, -2)"}
+        ${"angle"}         | ${2}           | ${"20° angle at (0, 0)"}
+    `(
+        "Should show 'correct answer' box for $graphType graph",
+        ({graphType, expectedNumber, correctAnswer}) => {
+            render(
+                <InteractiveGraphEditor
+                    {...baseProps}
+                    graph={{type: graphType}}
+                    correct={{type: graphType}}
+                />,
+            );
+
+            const correctAnswerBoxes = screen.getAllByText(correctAnswer);
+            // Quadratic, Sinusoid, and Angle also have a second answer box
+            // in the "Start coordinates" section.
+            expect(correctAnswerBoxes).toHaveLength(expectedNumber);
+        },
+    );
 });
