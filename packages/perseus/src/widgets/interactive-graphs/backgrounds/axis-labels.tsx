daa8c1b7fcc06183b83273928d533916c2d1f65a
Nisha Yerunkar <nisha@khanacademy.org>
2025-02-14 12:07:05 -0800

    [rotated-axis-labels] Get the positioning for rotated labels down

--- packages/perseus/src/widgets/interactive-graphs/backgrounds/axis-labels.tsx
+++ packages/perseus/src/widgets/interactive-graphs/backgrounds/axis-labels.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 import {getDependencies} from "../../../dependencies";
 import {pointToPixel} from "../graphs/use-transform";
-import {MAX, X, Y} from "../math";
+import {MAX, MIN, X, Y} from "../math";
 import useGraphConfig from "../reducer/use-graph-config";
 import {replaceOutsideTeX} from "../utils";
 
@@ -14,8 +14,38 @@ export default function AxisLabels({i18n}: {i18n: I18nContextType}) {
     const {range, labels, width, height} = useGraphConfig();
     const {strings} = i18n;
 
-    const yAxisLabelLocation: vec.Vector2 = [0, range[Y][MAX]];
-    const xAxisLabelLocation: vec.Vector2 = [range[X][MAX], 0];
+    const rotatedLabels = true;
+
+    // By default, the rotated x-axis label is positioned using its
+    // top-left corner, which won't work for longer labels. To center
+    // the label, we need to offset it by half the label's width.
+    const rotatedXCenterOffset = (labels[Y].length / 2) * -8;
+
+    // Default labels don't need a rotation ([0, 0]).
+    // Rotated labels need an offset of 12px to account for
+    // their positioning being determined by their top-left corner.
+    // If the label is rotated and the axes start at 0, the label
+    // needs to move additionally to avoid overlapping with the ticks.
+    const xAxisLabelOffset: [number, number] = rotatedLabels
+        ? [rotatedXCenterOffset, 12]
+        : [0, 0];
+    const yAxisLabelOffset: [number, number] = rotatedLabels
+        ? [-12, -12]
+        : [0, -24];
+
+    // By default, the x-axis label is at the right center of the graph
+    // (right of the x-axis), and the y-axis label is at the top center
+    // of the graph (above the y-axis).
+    // If `rotatedLabels` is true, the x-axis label is at the bottom
+    // center of the graph (below the x-axis), and the y-axis label is
+    // at the left center of the graph (left of the y-axis) and rotated
+    // to take up vertical space instead of horizontal space.
+    const xAxisLabelLocation: vec.Vector2 = rotatedLabels
+        ? [(range[X][MIN] + range[X][MAX]) / 2, range[Y][MIN]]
+        : [range[X][MAX], 0];
+    const yAxisLabelLocation: vec.Vector2 = rotatedLabels
+        ? [range[X][MIN], (range[Y][MIN] + range[Y][MAX]) / 2]
+        : [0, range[Y][MAX]];
 
     const [xAxisLabelText, yAxisLabelText] = labels;
     const graphInfo: GraphDimensions = {
@@ -23,13 +53,17 @@ export default function AxisLabels({i18n}: {i18n: I18nContextType}) {
         width,
         height,
     };
-    const [x1, y1] = pointToPixel(xAxisLabelLocation, graphInfo);
+
+    const [xLabelX, xLabelY] = vec.add(
+        pointToPixel(xAxisLabelLocation, graphInfo),
+        xAxisLabelOffset,
+    );
     // The default location for the y-axis-label is at the maximum y point
     // This is just underneath the tick line for that point. The -24 moves the
     // label up one grid square, so it sits on top of the graph
-    const [x2, y2] = vec.add(
+    const [yLabelX, yLabelY] = vec.add(
         pointToPixel(yAxisLabelLocation, graphInfo),
-        [0, -24],
+        yAxisLabelOffset,
     );
 
     const {TeX} = getDependencies();
@@ -40,8 +74,8 @@ export default function AxisLabels({i18n}: {i18n: I18nContextType}) {
                 aria-label={strings.xAxis}
                 style={{
                     position: "absolute",
-                    left: x1,
-                    top: y1,
+                    left: xLabelX,
+                    top: xLabelY,
                     fontSize: "14px",
                     transform: "translate(7px, -50%)",
                 }}
@@ -52,10 +86,12 @@ export default function AxisLabels({i18n}: {i18n: I18nContextType}) {
                 aria-label={strings.yAxis}
                 style={{
                     position: "absolute",
-                    left: x2,
-                    top: y2,
+                    left: yLabelX,
+                    top: yLabelY,
                     fontSize: "14px",
-                    transform: "translate(-50%, 0px)",
+                    transform: rotatedLabels
+                        ? "translate(-50%, 0px) rotate(-90deg)"
+                        : "translate(-50%, 0px)",
                 }}
             >
                 <TeX>{replaceOutsideTeX(yAxisLabelText)}</TeX>
