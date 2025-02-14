4cc8689d77186eb1f1986e009a3023cde579fc3e
Ben Christel <benchristel@khanacademy.org>
2025-02-14 11:43:53 -0800

    [benc/fix-prod-parser-errors] Consolidate snapshot and regression tests in one file

--- packages/perseus-core/src/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
+++ packages/perseus-core/src/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
@@ -3,23 +3,31 @@ import {join} from "path";
 
 import {anySuccess} from "../general-purpose-parsers/test-helpers";
 import {parseAndMigratePerseusItem} from "../index";
-import {mapFailure} from "../result";
+import {assertSuccess, mapFailure} from "../result";
 
 const dataFiles = fs.readdirSync(join(__dirname, "item-data"));
 
 describe("parseAndMigratePerseusItem", () => {
-    it.each(dataFiles)("accepts data/%s", (filename) => {
+    describe.each(dataFiles)("given item-data/%s", (filename) => {
         const json = fs.readFileSync(
             join(__dirname, "item-data", filename),
             "utf-8",
         );
+        const result = parseAndMigratePerseusItem(json);
 
-        // If the parse fails, get just the error message. This makes the test
-        // failure easier to read, since otherwise the entire `invalidObject`
-        // from the ParseFailureDetail would be printed.
-        const result = mapFailure(getMessage)(parseAndMigratePerseusItem(json));
+        it("parses successfully", () => {
+            // If the parse fails, get just the error message. This makes the test
+            // failure easier to read, since otherwise the entire `invalidObject`
+            // from the ParseFailureDetail would be printed.
+            const result = mapFailure(getMessage)(parseAndMigratePerseusItem(json));
 
-        expect(result).toEqual(anySuccess);
+            expect(result).toEqual(anySuccess);
+        });
+
+        it("returns the same result as before", () => {
+            assertSuccess(result);
+            expect(result.value).toMatchSnapshot();
+        })
     });
 });
 
