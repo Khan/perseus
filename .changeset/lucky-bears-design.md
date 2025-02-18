e63f83d0d89fd5b8e7aee3ab7248bcb19ec9be8a
Ben Christel <benchristel@users.noreply.github.com>
2025-02-18 12:47:48 -0800

    Fix Perseus JSON parser issues identified via production logs (#2246)
    There are a few lingering issues with Perseus data that somehow didn't
    show up in my content data dump. This PR updates the parser to handle them.
    
    Issue: none
    
    ## Test plan:
    
    - Deploy to webapp
    - Search Sentry for "Perseus parser"
    - There should be fewer errors after the deploy.
    
    Author: benchristel
    
    Reviewers: benchristel, handeyeco, Myranae
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Lint, Typecheck, Format, and Test (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ Check builds for changes in size (ubuntu-latest, 20.x), ✅ Cypress (ubuntu-latest, 20.x), ✅ Publish Storybook to Chromatic (ubuntu-latest, 20.x)
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2246

---
+++ .changeset/lucky-bears-design.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/perseus-core": minor
+---
+
+Update `parseAndMigratePerseusItem` and `parseAndMigratePerseusArticle` to accept legacy data formats observed in production
--- packages/perseus-core/src/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
+++ packages/perseus-core/src/parse-perseus-json/regression-tests/parse-perseus-json-regression.test.ts
@@ -2,24 +2,61 @@ import * as fs from "fs";
 import {join} from "path";
 
 import {anySuccess} from "../general-purpose-parsers/test-helpers";
-import {parseAndMigratePerseusItem} from "../index";
-import {mapFailure} from "../result";
+import {
+    parseAndMigratePerseusArticle,
+    parseAndMigratePerseusItem,
+} from "../index";
+import {assertSuccess, mapFailure} from "../result";
 
-const dataFiles = fs.readdirSync(join(__dirname, "data"));
+const itemDataDir = join(__dirname, "item-data");
+const itemDataFiles = fs.readdirSync(itemDataDir);
 
-describe("parseAndTypecheckPerseusItem", () => {
-    it.each(dataFiles)("accepts data/%s", (filename) => {
-        const json = fs.readFileSync(
-            join(__dirname, "data", filename),
-            "utf-8",
-        );
+const articleDataDir = join(__dirname, "article-data");
+const articleDataFiles = fs.readdirSync(articleDataDir);
 
-        // If the parse fails, get just the error message. This makes the test
-        // failure easier to read, since otherwise the entire `invalidObject`
-        // from the ParseFailureDetail would be printed.
-        const result = mapFailure(getMessage)(parseAndMigratePerseusItem(json));
+describe("parseAndMigratePerseusItem", () => {
+    describe.each(itemDataFiles)("given %s", (filename) => {
+        const json = fs.readFileSync(join(itemDataDir, filename), "utf-8");
+        const result = parseAndMigratePerseusItem(json);
 
-        expect(result).toEqual(anySuccess);
+        it("parses successfully", () => {
+            // If the parse fails, get just the error message. This makes the test
+            // failure easier to read, since otherwise the entire `invalidObject`
+            // from the ParseFailureDetail would be printed.
+            const result = mapFailure(getMessage)(
+                parseAndMigratePerseusItem(json),
+            );
+
+            expect(result).toEqual(anySuccess);
+        });
+
+        it("returns the same result as before", () => {
+            assertSuccess(result);
+            expect(result.value).toMatchSnapshot();
+        });
+    });
+});
+
+describe("parseAndMigratePerseusArticle", () => {
+    describe.each(articleDataFiles)("given %s", (filename) => {
+        const json = fs.readFileSync(join(articleDataDir, filename), "utf-8");
+        const result = parseAndMigratePerseusArticle(json);
+
+        it("parses successfully", () => {
+            // If the parse fails, get just the error message. This makes the test
+            // failure easier to read, since otherwise the entire `invalidObject`
+            // from the ParseFailureDetail would be printed.
+            const result = mapFailure(getMessage)(
+                parseAndMigratePerseusArticle(json),
+            );
+
+            expect(result).toEqual(anySuccess);
+        });
+
+        it("returns the same result as before", () => {
+            assertSuccess(result);
+            expect(result.value).toMatchSnapshot();
+        });
     });
 });
 
