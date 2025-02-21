e8b41829a88cfc950a677422051c02a4c21529f3
Tamara <tamara@khanacademy.org>
2025-02-21 12:01:34 -0600

    [tb/LEMS-2727/editor-image-linting] Remove unneeded argument

--- packages/perseus-linter/src/__tests__/rules.test.ts
+++ packages/perseus-linter/src/__tests__/rules.test.ts
@@ -445,24 +445,16 @@ describe("Individual lint rules tests", () => {
         },
     });
 
-    expectWarning(
-        imageUrlEmptyRule,
-        [
-            "![alt-text]()", // empty URL
-            "![alt-text](  )", // empty URL with spaces
-            "![alt-text](\n)", // empty URL with newline
-        ],
-        null,
-    );
-    expectPass(
-        imageUrlEmptyRule,
-        [
-            "![alt-text]('something')", // text should pass, though not a valid URL
-            "![alt-text]('www.test.com')", // example URL
-            "![alt-text](56)", // example number should pass, though not a valid URL
-        ],
-        null,
-    );
+    expectWarning(imageUrlEmptyRule, [
+        "![alt-text]()", // empty URL
+        "![alt-text](  )", // empty URL with spaces
+        "![alt-text](\n)", // empty URL with newline
+    ]);
+    expectPass(imageUrlEmptyRule, [
+        "![alt-text]('something')", // text should pass, though not a valid URL
+        "![alt-text]('www.test.com')", // example URL
+        "![alt-text](56)", // example number should pass, though not a valid URL
+    ]);
 
     expectWarning(expressionWidgetRule, "[[☃ expression 1]]", {
         widgets: {
