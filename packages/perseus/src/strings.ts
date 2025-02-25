9ec2c5456494d57dee5b71d7d3962548a8534c4b
Nisha Yerunkar <nisha@khanacademy.org>
2025-02-25 15:17:09 -0800

    [control-shift-arrow-keys] Update strings

--- packages/perseus/src/strings.ts
+++ packages/perseus/src/strings.ts
@@ -939,9 +939,9 @@ export const mockStrings: PerseusStrings = {
 
     // The following strings are used for interactive graph SR descriptions.
     srGraphInstructions:
-        "Use the Tab key to move through the interactive elements in the graph. When an interactive element has focus, use the Arrow keys to move it.",
+        "Use the Tab key to move through the interactive elements in the graph. When an interactive element has focus, use the modifier key + Control + Shift + the Arrow keys to move it.",
     srUnlimitedGraphInstructions:
-        "Press Shift + Enter to interact with the graph. Use the Tab key to move through the interactive elements in the graph and access the graph Action Bar. When an interactive element has focus, use the Arrow keys to move it or use the Delete key to remove it from the graph. Use the buttons in the Action Bar to add or adjust elements within the graph.",
+        "Press Shift + Enter to interact with the graph. Use the Tab key to move through the interactive elements in the graph and access the graph Action Bar. When an interactive element has focus, use the modifier key + Control + Shift + the Arrow keys to move it or use the Delete key to remove it from the graph. Use the buttons in the Action Bar to add or adjust elements within the graph.",
     xAxis: "X-axis",
     yAxis: "Y-axis",
     graphKeyboardPrompt: "Press Shift + Enter to interact with the graph",
