e03373404f04e2be50200edd24369ddfd104c360
Sarah Third <sarahthird@gmail.com>
2025-03-03 17:02:08 -0800

    [control-aria-z] Undo change request to remove yourAnswerLabel

--- packages/perseus/src/widgets/numeric-input/numeric-input.tsx
+++ packages/perseus/src/widgets/numeric-input/numeric-input.tsx
@@ -98,12 +98,19 @@ export const NumericInputComponent = forwardRef(
             );
         }
         // (desktop-only) Otherwise, use the InputWithExamples component
+
+        // If the labelText is not provided by the Content Creators, use the default label text
+        let labelText = props.labelText;
+        if (labelText == null || labelText === "") {
+            labelText = context.strings.yourAnswerLabel;
+        }
+
         return (
             <InputWithExamples
                 ref={inputRef as React.RefObject<typeof InputWithExamples>}
                 value={props.currentValue}
                 onChange={handleChange}
-                labelText={props.labelText}
+                labelText={labelText}
                 examples={generateExamples(props.answerForms, context.strings)}
                 shouldShowExamples={shouldShowExamples(props.answerForms)}
                 onFocus={handleFocus}
