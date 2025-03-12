6b3bc5c51f6024442469753a68f36d2b8b8885bd
Adam Goforth <adamgoforth@khanacademy.org>
2025-03-12 08:37:27 -0400

    Add scoring criteria to Free Response Widget (#2286)
    ## Summary:
    This commit adds a new scoringCriteria option to the Free Response
    Widget editor. It allows users to input one or more scoring criteria
    texts, which can be used when grading the answers to a Free Response
    question.
    
    This commit also changes the "Question" input to be a textarea instead
    of an input element. This allows longer questions to wrap and be fully
    visible even when the widget editor is narrow.
    
    Issue: https://khanacademy.atlassian.net/browse/LIT-1663
    
    ## Test plan:
    - Run `pnpm test packages/perseus-editor/src/widgets/__tests__/free-response-editor.test.tsx`
    - Open Storybook.
    - Open the Free Response Editor stories.
    - Verify that the rendered stories look right.
    - Interact with the "Editable" story, verifying that it's possible to
      add new criteria and delete existing ones.
    - Verify that the "Delete" button is grayed out on the "One Criterion" story.
    ![image](https://github.com/user-attachments/assets/5e431b75-e017-400a-8534-4471b82e2c27)
    
    Author: aag
    
    Reviewers: rgpass, handeyeco, benchristel, aag
    
    Required Reviewers:
    
    Approved By: benchristel, handeyeco
    
    Checks: ✅ 8 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2286

--- packages/perseus-core/src/widgets/free-response/index.ts
+++ packages/perseus-core/src/widgets/free-response/index.ts
@@ -1,18 +1,29 @@
+import getFreeResponsePublicWidgetOptions from "./free-response-util";
+
 import type {PerseusFreeResponseWidgetOptions} from "../../data-schema";
 import type {WidgetLogic} from "../logic-export.types";
 
 export type FreeResponseDefaultWidgetOptions = Pick<
     PerseusFreeResponseWidgetOptions,
-    "question"
+    "question" | "scoringCriteria"
 >;
 
 const defaultWidgetOptions: FreeResponseDefaultWidgetOptions = {
     question: "",
+
+    // Always display one criterion, since the user will always have to input
+    // at least one.
+    scoringCriteria: [
+        {
+            text: "",
+        },
+    ],
 };
 
 const freeResponseWidgetLogic: WidgetLogic = {
     name: "free-response",
     defaultWidgetOptions,
+    getPublicWidgetOptions: getFreeResponsePublicWidgetOptions,
 };
 
 export default freeResponseWidgetLogic;
--- packages/perseus-editor/src/widgets/__stories__/free-response-editor.stories.tsx
+++ packages/perseus-editor/src/widgets/__stories__/free-response-editor.stories.tsx
@@ -1,6 +1,11 @@
+import * as React from "react";
+
 import FreeResponseEditor from "../free-response-editor";
 
+import type {PropsFor} from "@khanacademy/wonder-blocks-core";
 import type {Meta, StoryObj} from "@storybook/react";
+
+type StoryArgs = Record<any, any>;
 
 const meta: Meta<typeof FreeResponseEditor> = {
     component: FreeResponseEditor,
@@ -10,6 +15,51 @@ const meta: Meta<typeof FreeResponseEditor> = {
 export default meta;
 type Story = StoryObj<typeof FreeResponseEditor>;
 
-export const Primary: Story = {
+export const NoCriteria: Story = {
     args: {},
+};
+
+export const OneCriterion: Story = {
+    args: {
+        scoringCriteria: [
+            {
+                text: "50 points for Gryffindor!",
+            },
+        ],
+    },
+};
+
+export const ThreeCriteria: Story = {
+    args: {
+        scoringCriteria: [
+            {
+                text: "50 points for Gryffindor!",
+            },
+            {
+                text: "50 points for Slytherin!",
+            },
+            {
+                text: "50 points for Hufflepuff!",
+            },
+        ],
+    },
+};
+
+type State = Partial<PropsFor<typeof FreeResponseEditor>>;
+
+const WithState = () => {
+    const [state, setState] = React.useState<State>({
+        question: "What is the truth?",
+        scoringCriteria: [{text: ""}],
+    });
+
+    const onChange = (options: State) => {
+        setState({...state, ...options});
+    };
+
+    return <FreeResponseEditor {...state} onChange={onChange} />;
+};
+
+export const Editable = (args: StoryArgs): React.ReactElement => {
+    return <WithState />;
 };
