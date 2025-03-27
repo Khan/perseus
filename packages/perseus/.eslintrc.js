c416d12f78c29bec10346a8759c88be25273b951
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-03-27 15:09:32 -0500

    [no-restricted-imports] add no-restricted-imports rule

--- packages/perseus/.eslintrc.js
+++ packages/perseus/.eslintrc.js
@@ -11,5 +11,14 @@ module.exports = {
                 includeTypes: true,
             },
         ],
+        // To avoid circular dependencies,
+        // we only allow imports from specific Perseus packages.
+        // This is overly strict to prevent accidental interdependencies,
+        // so feel free to update if you know what you're doing.
+        "no-restricted-imports": [
+            "error",
+            "@khanacademy/perseus",
+            "@khanacademy/perseus-editor",
+        ],
     },
 };
