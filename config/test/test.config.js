57ce16203e5cec5910262a083646f5c4c9ba8289
Jeremy Wiebe <jeremy@khanacademy.org>
2025-04-01 16:40:30 -0700

    Vendor in mathquill release to work around Github issue (#2346)
    ## Summary:
    
    Github has started returning a `401 Unauthorized` error for our mathquill dependency. It is referenced using a url to the release asset. 
    
    ![image](https://github.com/user-attachments/assets/7c81d0ab-7111-4ffa-a2e5-df1b6479b266)
    
    Over in webapp, they've vendored in the tarball and just install from it locally. This feels like a more reliable solution as we know we'll have the tarball if we clone the repo!
    
    Issue: "none"
    
    ## Test plan:
    
    Clear pnpm cache
    `rm -rf $(pnpm store path)`
    
    `pnpm clean`
    `pnpm install --frozen-lockfile`
    
    Should all succeed
    
    Author: jeremywiebe
    
    Reviewers: SonicScrewdriver, catandthemachines
    
    Required Reviewers:
    
    Approved By: SonicScrewdriver, catandthemachines
    
    Checks: ✅ 8 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2346

--- config/test/test.config.js
+++ config/test/test.config.js
@@ -7,37 +7,35 @@ const fs = require("fs");
 const path = require("path");
 
 const ancesdir = require("ancesdir");
+const fg = require("fast-glob");
 
 const root = ancesdir(__dirname);
-const vendorMap = fs
-    .readdirSync(path.join(root, "vendor"))
-    .reduce((map, name) => {
-        const pkgJson = JSON.parse(
-            fs.readFileSync(path.join(root, "vendor", name, "package.json")),
-        );
+const vendorMap = fg
+    .globSync(path.join(root, "vendor/*/package.json"))
+    .reduce((map, pkgJsonPath) => {
+        const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath).toString());
         return {
             ...map,
-            [`^${name}$`]: `<rootDir>/vendor/${name}/${pkgJson.main}`,
+            [`^${pkgJson.name}$`]: `<rootDir>/vendor/${pkgJson.name}/${pkgJson.main}`,
         };
     }, {});
 
-const pkgMap = fs
-    .readdirSync(path.join(root, "packages"))
-    .filter((name) => {
-        const stat = fs.statSync(path.join(root, "packages", name));
-        return stat.isDirectory();
-    })
-    .reduce((map, name) => {
-        const pkgJson = JSON.parse(
-            fs.readFileSync(path.join(root, "packages", name, "package.json")),
-        );
+const pkgMap = fg
+    .globSync(path.join(root, "packages/*/package.json"))
+    .reduce((map, pkgJsonPath) => {
+        const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath).toString());
         return {
             ...map,
             // NOTE(kevinb): we use the 'source' field here so that we can run our
             // tests without having to compile all of the packages first.
-            [`^@khanacademy/${name}$`]: `<rootDir>/packages/${name}/${pkgJson.source}`,
+            // NOTE(jeremy): We use strip the leading "@khanacademy/" namespace
+            // from all package names because our local directory structure
+            // doesn't include that. So "@khanacademy/perseus" becomes "perseus"
+            [`^${pkgJson.name}$`]: `<rootDir>/packages/${pkgJson.name.replace(/^@khanacademy\//, "")}/${pkgJson.source}`,
         };
     }, {});
+
+console.log(pkgMap);
 
 /** @type {import('jest').Config} */
 module.exports = {
