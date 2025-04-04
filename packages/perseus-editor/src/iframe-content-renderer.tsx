fa83c2c127bb802a8a109c48f87b1ab0da579a23
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2025-04-04 15:04:42 -0700

    [poc/accessibility-panel-in-editor] Fix linting error

--- packages/perseus-editor/src/iframe-content-renderer.tsx
+++ packages/perseus-editor/src/iframe-content-renderer.tsx
@@ -141,17 +141,21 @@ class IframeContentRenderer extends React.Component<Props> {
         frame.style.height = "100%";
         frame.src = this.props.url;
         frame.onload = () => {
+            // eslint-disable-next-line no-console
             console.log("Iframe loaded");
             const iframeDoc = frame.contentDocument || frame.contentWindow?.document;
             if (iframeDoc) {
+                // eslint-disable-next-line no-console
                 console.log("Adding axe-core script to iframe");
                 const axeCoreScriptElement = iframeDoc.createElement("script");
                 axeCoreScriptElement.src = "https://unpkg.com/axe-core@4.10.3/axe.js";
                 iframeDoc.body.appendChild(axeCoreScriptElement);
             } else {
+                // eslint-disable-next-line no-console
                 console.log("Unable to access iframe document");
             }
             setTimeout(() => {
+                // eslint-disable-next-line no-console
                 console.log("Axe-core in iFrame: ", frame.contentWindow?.axe);
             }, 1000);
         };
