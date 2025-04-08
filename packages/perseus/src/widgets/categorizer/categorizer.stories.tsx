adb967616062e3e28db29cffda60f3a278af87ac
Tamara <tamara@khanacademy.org>
2025-04-08 13:04:51 -0500

    [tb/LEMS-2974/answerless-categorizer] Add answerless rendering story

--- packages/perseus/src/widgets/categorizer/categorizer.stories.tsx
+++ packages/perseus/src/widgets/categorizer/categorizer.stories.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 
+import {generateTestPerseusItem} from "@khanacademy/perseus";
+
 import {RendererWithDebugUI} from "../../../../../testing/renderer-with-debug-ui";
+import {ServerItemRendererWithDebugUI} from "../../../../../testing/server-item-renderer-with-debug-ui";
 
 import {question1} from "./categorizer.testdata";
 
@@ -12,4 +15,15 @@ type StoryArgs = Record<any, any>;
 
 export const Question1 = (args: StoryArgs): React.ReactElement => {
     return <RendererWithDebugUI question={question1} />;
+};
+
+export const AnswerlessCategorizer = (args: StoryArgs): React.ReactElement => {
+    return (
+        <ServerItemRendererWithDebugUI
+            item={generateTestPerseusItem({
+                question: question1,
+            })}
+            startAnswerless={true}
+        />
+    );
 };
