346a67bd2e8d5466dd2f1b2e49b70c537a5a2f1a
Jeremy Wiebe <jeremy@khanacademy.org>
2025-04-10 12:22:50 -0700

    Revert changes to test data

--- packages/perseus/src/widgets/graded-group-set/graded-group-set.testdata.ts
+++ packages/perseus/src/widgets/graded-group-set/graded-group-set.testdata.ts
@@ -2,7 +2,7 @@ import type {PerseusRenderer} from "@khanacademy/perseus-core";
 
 export const article1: PerseusRenderer = {
     content:
-        "#Section 1: Adding tenths less than one\n\n[[☃ graded-group-set 1]]\n\n\nBeautiful, let's move on to problems with whole numbers and tenths. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "#Section 1: Adding tenths less than one\n\n[[☃ graded-group-set 1]]\n\n\nBeautiful, let's move on to problems with whole numbers and tenths.",
     images: {},
     widgets: {
         "graded-group-set 1": {
