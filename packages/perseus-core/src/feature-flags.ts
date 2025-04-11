33e25cb0baefc5f848d3529d4e640be93f18244f
Ivy Olamit <ivy@khanacademy.org>
2025-04-11 14:34:00 -0700

    [LEMS-3039-add-flags-in-api-options] Add flags in APIOption, this will be used for feature flags that can be passed from consuming application.

---
+++ packages/perseus-core/src/feature-flags.ts
@@ -0,0 +1,7 @@
+/**
+ * IMPORTANT: Do not delete this file or the variable below.
+ * This will be the definition of the active feature flags in Perseus
+ * that will hide features/changes under development. And will be used
+ * by flags of APIOptions type.
+ */
+export const PerseusFeatureFlags = ["new-radio-widget"];
