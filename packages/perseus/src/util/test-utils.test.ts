d311079b472f4271cc772a5abbe63caf471b907a
Tamara <tamara@khanacademy.org>
2025-04-16 15:16:03 -0500

    [tb/LEMS-2975/answerless-matrix] Add tests for 2 functions added to test-utils

--- packages/perseus/src/util/test-utils.test.ts
+++ packages/perseus/src/util/test-utils.test.ts
@@ -1,12 +1,16 @@
-import {generateTestPerseusItem} from "./test-utils";
+import {
+    generateTestPerseusItem,
+    getAnswerfulRenderer,
+    getAnswerlessRenderer,
+} from "./test-utils";
 import {
     basicObject,
-    customQuestionInfo,
-    expectedQuestionInfoAdded,
     customAnswerAreaInfo,
-    expectedAnswerAreaInfoAdded,
     customHintsInfo,
+    customQuestionInfo,
+    expectedAnswerAreaInfoAdded,
     expectedHintsInfoAdded,
+    expectedQuestionInfoAdded,
 } from "./test-utils.testdata";
 
 describe("generateTestPerseusItem", () => {
@@ -30,5 +34,91 @@ describe("generateTestPerseusItem", () => {
         expect(generateTestPerseusItem(customHintsInfo)).toEqual(
             expectedHintsInfoAdded,
         );
+    });
+});
+
+describe("getAnswerfulRenderer", () => {
+    it("should return an answerful renderer using the type given", () => {
+        const answerfulRenderer = getAnswerfulRenderer("dropdown", {
+            static: false,
+            placeholder: "greater/less than or equal to",
+            choices: [
+                {
+                    content: "greater than or equal to",
+                    correct: false,
+                },
+                {
+                    content: "less than or equal to",
+                    correct: true,
+                },
+            ],
+        });
+        expect(answerfulRenderer).toEqual({
+            content: "[[☃ dropdown 1]]",
+            images: {},
+            widgets: {
+                "dropdown 1": {
+                    type: "dropdown",
+                    options: {
+                        static: false,
+                        placeholder: "greater/less than or equal to",
+                        choices: [
+                            {
+                                content: "greater than or equal to",
+                                correct: false,
+                            },
+                            {
+                                content: "less than or equal to",
+                                correct: true,
+                            },
+                        ],
+                    },
+                },
+            },
+        });
+    });
+});
+
+describe("getAnswerlessRenderer", () => {
+    it("should return an answerless renderer using the type given with upgraded widget options", () => {
+        const answerlessRenderer = getAnswerlessRenderer("dropdown", {
+            static: false,
+            placeholder: "greater/less than or equal to",
+            choices: [
+                {
+                    content: "greater than or equal to",
+                    correct: false,
+                },
+                {
+                    content: "less than or equal to",
+                    correct: true,
+                },
+            ],
+        });
+        expect(answerlessRenderer).toEqual({
+            content: "[[☃ dropdown 1]]",
+            images: {},
+            widgets: {
+                "dropdown 1": {
+                    type: "dropdown",
+                    options: {
+                        static: false,
+                        placeholder: "greater/less than or equal to",
+                        choices: [
+                            {
+                                content: "greater than or equal to",
+                            },
+                            {
+                                content: "less than or equal to",
+                            },
+                        ],
+                    },
+                    alignment: "default",
+                    version: {major: 0, minor: 0},
+                    graded: true,
+                    static: false,
+                },
+            },
+        });
     });
 });
