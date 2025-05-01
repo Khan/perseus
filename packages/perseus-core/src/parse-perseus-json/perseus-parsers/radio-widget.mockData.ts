a6ad770b1674e66ab1cd08a8651a0505bb7d4e0f
Anakaren Rojas <anakaren@khanacademy.org>
2025-05-01 14:33:50 -0700

    [LEMS-3061-remove-deprecated-types] flesh out unit tests

---
+++ packages/perseus-core/src/parse-perseus-json/perseus-parsers/radio-widget.mockData.ts
@@ -0,0 +1,183 @@
+export const v0Widget = {
+    type: "radio" as const,
+    version: {major: 0, minor: 0},
+    graded: true,
+    options: {
+        choices: [
+            {
+                content: "I am a correct choice",
+                correct: true,
+                clue: "I am a clue for a choice",
+                isNoneOfTheAbove: true,
+                widgets: undefined,
+            },
+            {
+                content: "I am an incorrect choice",
+                correct: false,
+                clue: "I am a clue for a choice",
+                widgets: undefined,
+            },
+            {
+                content: "I am an incorrect choice",
+                correct: false,
+                isNoneOfTheAbove: false,
+                widgets: {},
+            },
+            {
+                content: "I am content for a choice",
+                clue: "I am a clue for a choice",
+                isNoneOfTheAbove: false,
+            },
+            {
+                content: "I am a choice with only content",
+            }
+        ],
+        countChoices: false,
+        randomize: false,
+        multipleSelect: false,
+        deselectEnabled: false,
+        onePerLine: false,
+        displayCount: false,
+        noneOfTheAbove: false as const,
+    },
+};
+
+export const v1Widget = {
+    type: "radio" as const,
+    graded: true,
+    options: {
+        choices: [
+            {
+                content: "I am a correct choice",
+                correct: true,
+                clue: "I am a clue for a choice",
+                isNoneOfTheAbove: true,
+                widgets: undefined,
+            },
+            {
+                content: "I am an incorrect choice",
+                correct: false,
+                clue: "I am a clue for a choice",
+                widgets: undefined,
+            },
+            {
+                content: "I am an incorrect choice",
+                correct: false,
+                isNoneOfTheAbove: false,
+                widgets: {},
+            },
+            {
+                content: "I am content for a choice",
+                clue: "I am a clue for a choice",
+                isNoneOfTheAbove: false,
+            },
+            {
+                content: "I am a choice with only content",
+            }
+        ],
+        hasNoneOfTheAbove: false,
+        countChoices: false,
+        randomize: false,
+        multipleSelect: false,
+        deselectEnabled: false,
+        onePerLine: false,
+        displayCount: false,
+        noneOfTheAbove: undefined,
+    },
+    version: {
+        major: 1,
+        minor: 0,
+    },
+};
+
+export const v2Widget = {
+    type: "radio" as const,
+    graded: true,
+    options: {
+        choices: [
+            {
+                content: "I am a correct choice",
+                correct: true,
+                clue: "I am a clue for a choice",
+                isNoneOfTheAbove: true,
+                widgets: undefined,
+            },
+            {
+                content: "I am an incorrect choice",
+                correct: false,
+                clue: "I am a clue for a choice",
+                widgets: undefined,
+            },
+            {
+                content: "I am an incorrect choice",
+                correct: false,
+                isNoneOfTheAbove: false,
+                widgets: {},
+            },
+            {
+                content: "I am content for a choice",
+                clue: "I am a clue for a choice",
+                isNoneOfTheAbove: false,
+            },
+            {
+                content: "I am a choice with only content",
+            }
+        ],
+        numCorrect: 1,
+        hasNoneOfTheAbove: false,
+        countChoices: false,
+        randomize: false,
+        multipleSelect: false,
+        deselectEnabled: false,
+        onePerLine: false,
+        displayCount: false,
+        noneOfTheAbove: undefined,
+    },
+    version: {
+        major: 2,
+        minor: 0,
+    },
+};
+
+export const v3Widget = {
+    type: "radio" as const,
+    graded: true,
+    options: {
+        choices: [
+            {
+                content: "I am a correct choice",
+                correct: true,
+                clue: "I am a clue for a choice",
+                isNoneOfTheAbove: true,
+            },
+            {
+                content: "I am an incorrect choice",
+                correct: false,
+                clue: "I am a clue for a choice",
+            },
+            {
+                content: "I am an incorrect choice",
+                correct: false,
+                isNoneOfTheAbove: false,
+            },
+            {
+                content: "I am content for a choice",
+                clue: "I am a clue for a choice",
+                isNoneOfTheAbove: false,
+            },
+            {
+                content: "I am a choice with only content",
+            }
+        ],
+        numCorrect: 1,
+        hasNoneOfTheAbove: false,
+        countChoices: false,
+        randomize: false,
+        multipleSelect: false,
+        deselectEnabled: false,
+    },
+    version: {
+        major: 3,
+        minor: 0,
+    },
+};
