38e6806670772cb9b99fe661d1e9caee98e803bf
Ben Christel <benchristel@khanacademy.org>
2025-05-06 09:09:36 -0700

    [benc/parse-cross-realm-objects] Add doc comment

--- packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/is-plain-object.ts
+++ packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/is-plain-object.ts
@@ -3,6 +3,13 @@ const objectConstructorString = Object.prototype.constructor.toString();
 
 const functionToString = Function.prototype.toString;
 
+/**
+ * Checks whether a value is a plain JS object (e.g. one constructed with `{}`
+ * literal syntax or `Object.create(null)`. Returns true given a plain object
+ * from another realm.
+ *
+ * @param x - the value to check for objectiness
+ */
 export function isPlainObject(x: unknown): x is Record<keyof any, unknown> {
     if (x == null) {
         return false;
