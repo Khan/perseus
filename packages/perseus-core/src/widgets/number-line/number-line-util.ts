4282de2b214e15a7043ee3786c1df11b346c2e97
Ben Christel <benchristel@users.noreply.github.com>
2025-05-06 11:19:27 -0700

    Add `isInequality` to the `PerseusNumberLineWidgetOptions` type. (#2425)
    The NumberLine component was using this field, but it wasn't represented in the types.
    
    Issue: none
    
    ## Test plan:
    
    `pnpm test`
    
    Review the Number Line stories in Storybook.
    
    Author: benchristel
    
    Reviewers: handeyeco, Myranae
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ✅ 8 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2425

--- packages/perseus-core/src/widgets/number-line/number-line-util.ts
+++ packages/perseus-core/src/widgets/number-line/number-line-util.ts
@@ -7,6 +7,7 @@ type NumberLinePublicWidgetOptions = Pick<
     | "labelStyle"
     | "labelTicks"
     | "isTickCtrl"
+    | "isInequality"
     | "divisionRange"
     | "numDivisions"
     | "snapDivisions"
