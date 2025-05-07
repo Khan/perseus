f1662239e40b0db4fd75823746a7fc37662f7494
Tamara <60857422+Myranae@users.noreply.github.com>
2025-05-07 10:40:03 -0500

    Answerless Orderer (#2441)
    ## Summary:
    Update the Orderer widget for answerless rendering. Small updates to remove correctOptions and confirm types are correct. Added tests and an answerless story.
    
    Issue: LEMS-2979
    
    ## Test plan:
    - Confirm all checks pass
    - Confirm widget still acts as expected
    - No changes should be noted in user experience
    
    Author: Myranae
    
    Reviewers: copilot-pull-request-reviewer[bot], benchristel, Myranae, handeyeco
    
    Required Reviewers:
    
    Approved By: benchristel
    
    Checks: ✅ 8 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2441

--- packages/perseus-core/src/widgets/orderer/orderer-util.ts
+++ packages/perseus-core/src/widgets/orderer/orderer-util.ts
@@ -4,24 +4,20 @@ import type {PerseusOrdererWidgetOptions} from "../../data-schema";
  * For details on the individual options, see the
  * PerseusOrdererWidgetOptions type
  */
-type OrdererPublicWidgetOptions = {
-    options: PerseusOrdererWidgetOptions["options"];
-    height: PerseusOrdererWidgetOptions["height"];
-    layout: PerseusOrdererWidgetOptions["layout"];
-};
+export type OrdererPublicWidgetOptions = Pick<
+    PerseusOrdererWidgetOptions,
+    "options" | "height" | "layout"
+>;
 
 /**
  * Given a PerseusOrdererWidgetOptions object, return a new object with only
  * the public options that should be exposed to the client.
  */
 function getOrdererPublicWidgetOptions(
-    options: PerseusOrdererWidgetOptions,
+    fullOptions: PerseusOrdererWidgetOptions,
 ): OrdererPublicWidgetOptions {
-    return {
-        options: options.options,
-        height: options.height,
-        layout: options.layout,
-    };
+    const {options, height, layout} = fullOptions;
+    return {options, height, layout};
 }
 
 export default getOrdererPublicWidgetOptions;
