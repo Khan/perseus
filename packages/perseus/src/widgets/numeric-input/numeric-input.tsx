4fc5a2e87554d7940ed5013c9239a26823c0637b
Jeremy Wiebe <jeremy@khanacademy.org>
2025-05-07 14:21:57 -0700

    Don't need to specify props type twice - and reformat

--- packages/perseus/src/widgets/numeric-input/numeric-input.tsx
+++ packages/perseus/src/widgets/numeric-input/numeric-input.tsx
@@ -22,96 +22,97 @@ import type {NumericInputHandle} from "../../types";
  * component. It is responsible for rendering the UI elements of the Numeric
  * Input widget.
  */
-export const NumericInputComponent = forwardRef<NumericInputHandle, NumericInputProps>(
-    (props: NumericInputProps, ref) => {
-        const context = useContext(PerseusI18nContext);
-        const inputRef = useRef<NumericInputHandle>(null);
-        const [isFocused, setIsFocused] = useState<boolean>(false);
+export const NumericInputComponent = forwardRef<
+    NumericInputHandle,
+    NumericInputProps
+>((props, ref) => {
+    const context = useContext(PerseusI18nContext);
+    const inputRef = useRef<NumericInputHandle>(null);
+    const [isFocused, setIsFocused] = useState<boolean>(false);
 
-        // Pass the focus and blur methods to the Numeric Input Class component
-        useImperativeHandle(ref, () => ({
-            current: inputRef.current,
-            focus: () => {
-                if (inputRef.current) {
-                    inputRef.current?.focus();
-                    setIsFocused(true);
-                }
-            },
-            blur: () => {
-                if (inputRef.current) {
-                    inputRef.current?.blur();
-                    setIsFocused(false);
-                }
-            },
-        }));
+    // Pass the focus and blur methods to the Numeric Input Class component
+    useImperativeHandle(ref, () => ({
+        current: inputRef.current,
+        focus: () => {
+            if (inputRef.current) {
+                inputRef.current?.focus();
+                setIsFocused(true);
+            }
+        },
+        blur: () => {
+            if (inputRef.current) {
+                inputRef.current?.blur();
+                setIsFocused(false);
+            }
+        },
+    }));
 
-        const handleChange = (
-            newValue: string,
-            cb?: () => unknown | null | undefined,
-        ): void => {
-            props.onChange({currentValue: newValue}, cb);
-            props.trackInteraction();
-        };
+    const handleChange = (
+        newValue: string,
+        cb?: () => unknown | null | undefined,
+    ): void => {
+        props.onChange({currentValue: newValue}, cb);
+        props.trackInteraction();
+    };
 
-        const handleFocus = (): void => {
-            props.onFocus([]);
-            setIsFocused(true);
-        };
+    const handleFocus = (): void => {
+        props.onFocus([]);
+        setIsFocused(true);
+    };
 
-        const handleBlur = (): void => {
-            props.onBlur([]);
-            setIsFocused(false);
-        };
+    const handleBlur = (): void => {
+        props.onBlur([]);
+        setIsFocused(false);
+    };
 
-        // Styles for the InputWithExamples
-        const styles = StyleSheet.create({
-            inputWithExamples: {
-                borderRadius: "3px",
-                borderWidth: isFocused ? "2px" : "1px",
-                display: "inline-block",
-                fontFamily: `Symbola, "Times New Roman", serif`,
-                fontSize: "18px",
-                height: "32px",
-                lineHeight: "18px",
-                padding: isFocused ? "4px" : "4px 5px",
-                textAlign: props.rightAlign ? "right" : "left",
-                width: props.size === "small" ? 40 : 80,
-            },
-        });
+    // Styles for the InputWithExamples
+    const styles = StyleSheet.create({
+        inputWithExamples: {
+            borderRadius: "3px",
+            borderWidth: isFocused ? "2px" : "1px",
+            display: "inline-block",
+            fontFamily: `Symbola, "Times New Roman", serif`,
+            fontSize: "18px",
+            height: "32px",
+            lineHeight: "18px",
+            padding: isFocused ? "4px" : "4px 5px",
+            textAlign: props.rightAlign ? "right" : "left",
+            width: props.size === "small" ? 40 : 80,
+        },
+    });
 
-        // (mobile-only) If the custom keypad is enabled, use the SimpleKeypadInput component
-        if (props.apiOptions.customKeypad) {
-            const alignmentClass = props.rightAlign
-                ? "perseus-input-right-align"
-                : undefined;
-            return (
-                <div className={alignmentClass}>
-                    <SimpleKeypadInput
-                        ref={inputRef as React.RefObject<SimpleKeypadInput>}
-                        value={props.currentValue}
-                        keypadElement={props.keypadElement}
-                        onChange={handleChange}
-                        onFocus={handleFocus}
-                        onBlur={handleBlur}
-                    />
-                </div>
-            );
-        }
-        // (desktop-only) Otherwise, use the InputWithExamples component
+    // (mobile-only) If the custom keypad is enabled, use the SimpleKeypadInput component
+    if (props.apiOptions.customKeypad) {
+        const alignmentClass = props.rightAlign
+            ? "perseus-input-right-align"
+            : undefined;
         return (
-            <InputWithExamples
-                ref={inputRef}
-                value={props.currentValue}
-                onChange={handleChange}
-                labelText={props.labelText}
-                examples={generateExamples(props.answerForms, context.strings)}
-                shouldShowExamples={shouldShowExamples(props.answerForms)}
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-                id={props.widgetId}
-                disabled={props.apiOptions.readOnly}
-                style={styles.inputWithExamples}
-            />
+            <div className={alignmentClass}>
+                <SimpleKeypadInput
+                    ref={inputRef as React.RefObject<SimpleKeypadInput>}
+                    value={props.currentValue}
+                    keypadElement={props.keypadElement}
+                    onChange={handleChange}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                />
+            </div>
         );
-    },
-);
+    }
+    // (desktop-only) Otherwise, use the InputWithExamples component
+    return (
+        <InputWithExamples
+            ref={inputRef}
+            value={props.currentValue}
+            onChange={handleChange}
+            labelText={props.labelText}
+            examples={generateExamples(props.answerForms, context.strings)}
+            shouldShowExamples={shouldShowExamples(props.answerForms)}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            id={props.widgetId}
+            disabled={props.apiOptions.readOnly}
+            style={styles.inputWithExamples}
+        />
+    );
+});
