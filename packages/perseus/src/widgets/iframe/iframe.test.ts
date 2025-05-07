25a27a5c59d19c74047b7682c27765ef55d586a4
Tamara <tamara@khanacademy.org>
2025-05-07 15:46:07 -0500

    [tb/LEMS-2980/answerless-iframe] Add tests for postMessage handling

--- packages/perseus/src/widgets/iframe/iframe.test.ts
+++ packages/perseus/src/widgets/iframe/iframe.test.ts
@@ -1,3 +1,6 @@
+import {act} from "@testing-library/react";
+import $ from "jquery";
+
 import {testDependencies} from "../../../../../testing/test-dependencies";
 import * as Dependencies from "../../dependencies";
 import {renderQuestion} from "../__testutils__/renderQuestion";
@@ -40,4 +43,77 @@ describe("iframe widget", () => {
     });
 
     //There isn't testable behavior for this widget
+});
+
+describe("iframe widget postMessage handling", () => {
+    it("should update widget props to correct when testsPassed is true", () => {
+        const {renderer} = renderQuestion(question1);
+
+        const messageData = {testsPassed: true, message: "Nicely done!"};
+
+        act(() => {
+            const fakeEvent = new $.Event("message", {
+                originalEvent: {data: JSON.stringify(messageData)},
+            });
+            $(window).trigger(fakeEvent);
+        });
+
+        const updatedProps = renderer.getWidgetProps("iframe 1");
+        expect(updatedProps.status).toBe("correct");
+        expect(updatedProps.message).toBe("Nicely done!");
+    });
+
+    it("should update widget props to incorrect when testsPassed is false", () => {
+        const apiOptions: APIOptions = {isMobile: false};
+        const {renderer} = renderQuestion(question1, apiOptions);
+
+        const messageData = {testsPassed: false, message: "Try again."};
+
+        act(() => {
+            const fakeEvent = new $.Event("message", {
+                originalEvent: {data: JSON.stringify(messageData)},
+            });
+            $(window).trigger(fakeEvent);
+        });
+
+        const updatedProps = renderer.getWidgetProps("iframe 1");
+        expect(updatedProps.status).toBe("incorrect");
+        expect(updatedProps.message).toBe("Try again.");
+    });
+
+    it("should not update widget props if testsPassed is missing", () => {
+        const apiOptions: APIOptions = {isMobile: false};
+        const {renderer} = renderQuestion(question1, apiOptions);
+        const initialProps = renderer.getWidgetProps("iframe 1");
+
+        const messageData = {info: "This is just an informational message."};
+
+        act(() => {
+            const fakeEvent = new $.Event("message", {
+                originalEvent: {data: JSON.stringify(messageData)},
+            });
+            $(window).trigger(fakeEvent);
+        });
+
+        const currentProps = renderer.getWidgetProps("iframe 1");
+        expect(currentProps.status).toBe(initialProps.status);
+        expect(currentProps.message).toBe(initialProps.message);
+    });
+
+    it("should not update widget props if message data is not valid JSON", () => {
+        const apiOptions: APIOptions = {isMobile: false};
+        const {renderer} = renderQuestion(question1, apiOptions);
+        const initialProps = renderer.getWidgetProps("iframe 1");
+
+        act(() => {
+            const fakeEvent = new $.Event("message", {
+                originalEvent: {data: "this is not json"},
+            });
+            $(window).trigger(fakeEvent);
+        });
+
+        const currentProps = renderer.getWidgetProps("iframe 1");
+        expect(currentProps.status).toBe(initialProps.status);
+        expect(currentProps.message).toBe(initialProps.message);
+    });
 });
