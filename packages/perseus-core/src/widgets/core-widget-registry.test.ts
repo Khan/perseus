705b0ba9ddbc27543997b1f43211d8c79c683486
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-05-15 10:28:27 -0500

    [registry-guard] iterate on tests

--- packages/perseus-core/src/widgets/core-widget-registry.test.ts
+++ packages/perseus-core/src/widgets/core-widget-registry.test.ts
@@ -1,8 +1,17 @@
-import {getPublicWidgetOptionsFunction} from "./core-widget-registry";
+import * as CoreWidgetRegistry from "./core-widget-registry";
 
 describe("core-widget-registry", () => {
-    it("throws when getPublicWidgetOptionsFunction is called before registerWidget", () => {
-        expect(() => getPublicWidgetOptionsFunction("radio")).toThrow(
+    test.each([
+        "isWidgetRegistered",
+        "getCurrentVersion",
+        "getPublicWidgetOptionsFunction",
+        "getWidgetOptionsUpgrades",
+        "getDefaultWidgetOptions",
+        "getSupportedAlignments",
+        "getDefaultAlignment",
+    ])("%s throws when called before registerWidget", (funName) => {
+        // eslint-disable-next-line import/namespace
+        expect(() => CoreWidgetRegistry[funName]?.("radio")).toThrow(
             "Core widget registry accessed before initialization!",
         );
     });
