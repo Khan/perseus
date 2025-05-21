49c8d302a4b86ef0c00021f698c088b655f13a0d
Sarah Third <sarahthird@khanacademy.org>
2025-05-21 14:16:38 -0700

    Ensure that our parser can handle empty strings for labelLocation for Interactive Graphs (#2509)
    ## Summary:
    This PR implements a fix to ensure that our parser can handle empty string values for our labelLocation property on our Interactive Graphs. 
    
    I have also updated several locations to be very specific about the default value, to ensure that this issue doesn't persist with new content. 
    
    Issue: LEMS-3122
    
    ## Test plan:
    - Tests pass 
      - Including new test
    
    Author: SonicScrewdriver
    
    Reviewers: benchristel
    
    Required Reviewers:
    
    Approved By: benchristel
    
    Checks: ✅ 8 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2509

--- packages/perseus-core/src/utils/split-perseus-renderer.test.ts
+++ packages/perseus-core/src/utils/split-perseus-renderer.test.ts
@@ -284,6 +284,7 @@ describe("splitPerseusRenderer", () => {
                         snapStep: [1, 1],
                         markings: "none",
                         labels: [],
+                        labelLocation: "onAxis",
                         showProtractor: false,
                         range: [
                             [0, 1],
@@ -310,6 +311,7 @@ describe("splitPerseusRenderer", () => {
                         snapStep: [1, 1],
                         markings: "none",
                         labels: [],
+                        labelLocation: "onAxis",
                         showProtractor: false,
                         range: [
                             [0, 1],
