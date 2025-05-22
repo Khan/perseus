e5b2be81b07ed61a2fe018dd56e28d41b153e870
Myranae <60857422+Myranae@users.noreply.github.com>
2025-05-22 16:55:43 +0000

    Deploying to gh-pages from @ Khan/perseus@86ac76f79c2fd4ee8b822393f89a064af92c0339 🚀

--- assets/item-version-D_h3BKdS.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./index-DktHmiAd.js";import{w as r}from"./all-widgets-Bv7QRJhH.js";import{r as s,g as i}from"./renderer-Bf3vLkvc.js";const o="@khanacademy/perseus",a="__lib_version__";e(o,a);s(r);i();
---
+++ assets/plotter.stories-CCi7Uvaz.js
@@ -0,0 +1,28 @@
+import"./index-default-BcKQpA1a.js";import{g as n}from"./index-DktHmiAd.js";import{S as c}from"./server-item-renderer-with-debug-ui-C6Jelntw.js";import"./jsx-runtime-BT65X5dW.js";import"./index-C6mWTJJr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./no-important-DlFk8a1I.js";import"./index-BN1f3DJf.js";import"./index-B1Gws05u.js";import"./index-C0xJ1VDw.js";import"./index-BfjDPqC2.js";import"./split-view-Bj3Zehqg.js";import"./index-B_N1aciG.js";import"./index-DyG-XbLr.js";import"./index-BDsoIsav.js";import"./index-BPL71wmx.js";import"./test-keypad-context-wrapper--C-uYlz0.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./mobile-keypad-DvhXuqfO.js";import"./index-3H81sEQ1.js";import"./index-BzwLglMS.js";import"./assertThisInitialized-B_E71Lmv.js";import"./tabbar-CdwZ7NtL.js";import"./item-CU19aERO.js";import"./button-assets-C9T5fNLz.js";import"./get-decimal-separator-B2cicA45.js";import"./keypad-button-CXqAcJRc.js";import"./operators-page-C3H2X4aA.js";import"./navigation-pad-i78wd4WL.js";import"./prop-types-BXAxUNpE.js";import"./key-translator-D-skzBJe.js";import"./index-CrGd2QqM.js";import"./split-perseus-item-JKIdQbrf.js";import"./util-AEEC48XJ.js";import"./perseus-error-CSETqePQ.js";import"./jquery-CkHB0_Mt.js";import"./server-item-renderer-DMUML-QJ.js";import"./renderer-DrjTKFcr.js";import"./index-D7h-teXI.js";import"./zoomable-tex-Dy-nElJT.js";import"./tex-BONImhZG.js";import"./dependencies-BsVPGK1s.js";import"./zoomable-C_CCSKDG.js";import"./i18n-context-BT3CWGFO.js";import"./svg-image-8T6Bqk5w.js";import"./index-DD9muzfp.js";import"./fixed-to-responsive-CTsII9Xx.js";import"./constants-BIpV3g0K.js";import"./client-CAS5PaPY.js";import"./inline-icon-AJRwMA4Z.js";import"./icon-paths-C3bPmxpL.js";import"./image-loader-CdkY-jNs.js";import"./perseus-api-DFYLNfT-.js";import"./stub-tag-editor-CZXxAW63.js";import"./text-list-editor-C2gddtxS.js";import"./lint-D0FI20JF.js";import"./hints-renderer-COyQwuVx.js";import"./media-queries-D4w_O5TS.js";import"./shared-Dtpp87RV.js";import"./test-dependencies-Bo9vAyf0.js";import"./all-widgets-DUOAfzV1.js";import"./index-HPnoKG0_.js";import"./index-BOy187jj.js";import"./index-CcqtyHPG.js";import"./index-BWwc-H8c.js";import"./Popper-Bj3TCzZA.js";import"./math-input-CG7LDH-Z.js";import"./index-W1pK7nxW.js";import"./simple-keypad-input-Bl9QRvgR.js";import"./input-with-examples-2YKqsWc5.js";import"./text-input-CQ4Ha-J8.js";import"./index-CkYjwmgs.js";import"./base-radio-CbqtFMIq.js";import"./choice-BG23lCZ9.js";import"./index-C_76Tegg.js";import"./icon-BfyZ3piz.js";import"./choice-icon-x_McTlZQ.js";import"./focus-ring-CuRoZzyy.js";import"./option-status-CK7EF2QT.js";import"./choice-none-above-CSj3Ys-J.js";import"./phet-simulation-Bo8Pw36Z.js";import"./index-DE1wyAWu.js";import"./warning-circle-BLJ1dLdU.js";import"./answer-choices-DPNv9k5F.js";import"./index-Bl4treR9.js";import"./free-response-BLh4IDUx.js";import"./index-BMWTWptC.js";import"./button-group-B24xCvY2.js";import"./index-Dd-cahjY.js";import"./tiny-invariant-CopsF_GD.js";import"./hud-CP_ly967.js";import"./index-ChAlgF23.js";import"./multi-button-group-U6RQwTwG.js";import"./number-input-Bqp2ujXM.js";import"./range-input--OsOJO6O.js";import"./marker-CFwOioE6.js";import"./answer-pill-CmZSMnU9.js";import"./sortable-BSP4IWpH.js";import"./video-transcript-link-DfQd4tMq.js";const a={content:`The elementary school principal asked teachers to report the number of students absent in each grade during the past week.
+
+**Create a bar graph to show how many students were absent in each grade.**
+
+School grade | Number of absent students  
+:- | :-: 
+$1^{\\text{st}} \\text{ grade}$ | $15$ 
+$2^{\\text{nd}} \\text{ grade}$ |$25$ 
+$3^{\\text{rd}} \\text{ grade}$ | $5$  
+$4^{\\text{th}} \\text{ grade}$ | $10$  
+$5^{\\text{th}} \\text{ grade}$ | $10$  
+
+[[☃ plotter 1]]
+
+`,images:{},widgets:{"plotter 1":{alignment:"default",graded:!0,options:{categories:["$1^{\\text{st}} \\text{}$","$2^{\\text{nd}} \\text{}$","$3^{\\text{rd}} \\text{}$","$4^{\\text{th}} \\text{}$","$5^{\\text{th}} \\text{}$"],picBoxHeight:300,picSize:300,picUrl:"",plotDimensions:[380,300],correct:[15,25,5,10,10],labelInterval:1,labels:["School grade","Number of absent students"],maxY:30,scaleY:5,snapsPerLine:1,starting:[0,0,0,0,0],type:"bar"},static:!1,type:"plotter",version:{major:0,minor:0}}}},or={title:"Perseus/Widgets/Plotter",component:c},t={args:{item:n({question:a})}},r={args:{item:n({question:a}),startAnswerless:!0}};var e,o,i;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    item: generateTestPerseusItem({
+      question: question1
+    })
+  }
+}`,...(i=(o=t.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};var m,p,s;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    item: generateTestPerseusItem({
+      question: question1
+    }),
+    startAnswerless: true
+  }
+}`,...(s=(p=r.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const ir=["Basic","AnswerlessPlotter"];export{r as AnswerlessPlotter,t as Basic,ir as __namedExportsOrder,or as default};
