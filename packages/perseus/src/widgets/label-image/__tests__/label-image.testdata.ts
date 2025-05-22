86ac76f79c2fd4ee8b822393f89a064af92c0339
Tamara <60857422+Myranae@users.noreply.github.com>
2025-05-22 11:53:56 -0500

    Update label image widget to handle answerless data and do some cleanup (#2495)
    ## Summary:
    There are two main methods of Label Image that use answers: showRationalesForCurrentlySelectedChoices and renderMarkers. Both were updated to handle answerless data, though the first one is mainly used after scoring has begun and should have access to answers already. Additional tests were added, props were updated to account for answerless data, and an answerless story was added.
    
    Issue: LEMS-2981
    
    ## Test plan:
    - Confirm all checks and tests pass
    - `pnpm storybook`: Confirm widget renders, is interactive, and scores as expected for both answerless and answerful stories
    
    Author: Myranae
    
    Reviewers: Myranae, copilot-pull-request-reviewer[bot], benchristel, handeyeco
    
    Required Reviewers:
    
    Approved By: benchristel
    
    Checks: ✅ 8 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2495

--- packages/perseus/src/widgets/label-image/__tests__/label-image.testdata.ts
+++ packages/perseus/src/widgets/label-image/__tests__/label-image.testdata.ts
@@ -106,68 +106,6 @@ export const shortTextQuestion: PerseusRenderer = {
     },
 };
 
-export const textWithoutAnswersQuestion: PerseusRenderer = {
-    content:
-        "Carol created a chart and a bar graph to show how many of each type of vehicle were in her supermarket parking lot.\n\nVehicle Type | Number in the parking lot\n:- | :-: \nTrucks| $25$ \nVans | $5$ \nCars| $40$ \nSUVs | $10$ \n\n**Label each bar on the bar graph.**\n\n[[☃ label-image 1]]\n\n",
-    images: {
-        "web+graphie://ka-perseus-graphie.s3.amazonaws.com/1e28332fd2321975639ab50c9ce442e568c18421":
-            {
-                width: 341,
-                height: 310,
-            },
-    },
-    widgets: {
-        "label-image 1": {
-            type: "label-image",
-            alignment: "default",
-            static: false,
-            graded: true,
-            options: {
-                static: false,
-                choices: ["Trucks", "Vans", "Cars", "SUVs"],
-                imageAlt:
-                    "A bar graph with four bar lines that shows the horizontal axis labeled Number in the parking lot and the vertical axis labeled Vehicle Type. The horizontal axis is labeled, from left to right: 0, 10, 20, 30, 40, and 50. The vertical axis has, from the bottom to the top, four unlabeled bar lines as follows: the first unlabeled bar line extends to the middle of 0 and 10, the second unlabeled bar line extends to 40, the third unlabeled bar line extends to the middle of 20 and 30, and fourth unlabeled bar line extends to 10.",
-                imageUrl:
-                    "web+graphie://ka-perseus-graphie.s3.amazonaws.com/56c60c72e96cd353e4a8b5434506cd3a21e717af",
-                imageWidth: 415,
-                imageHeight: 314,
-                markers: [
-                    {
-                        answers: [],
-                        label: "The fourth unlabeled bar line.",
-                        x: 25,
-                        y: 17.7,
-                    },
-                    {
-                        answers: [],
-                        label: "The third unlabeled bar line.",
-                        x: 25,
-                        y: 35.3,
-                    },
-                    {
-                        answers: [],
-                        label: "The second unlabeled bar line.",
-                        x: 25,
-                        y: 53,
-                    },
-                    {
-                        answers: [],
-                        label: "The first unlabeled bar line.",
-                        x: 25,
-                        y: 70.3,
-                    },
-                ],
-                multipleAnswers: false,
-                hideChoicesFromInstructions: true,
-            },
-            version: {
-                major: 0,
-                minor: 0,
-            },
-        },
-    },
-};
-
 export const mathQuestion: PerseusRenderer = {
     content:
         "Carol created a chart and a bar graph to show how many of each type of vehicle were in her supermarket parking lot.\n\nVehicle Type | Number in the parking lot\n:- | :-: \nTrucks| $25$ \nVans | $5$ \nCars| $40$ \nSUVs | $10$ \n\n**Label each bar on the bar graph.**\n\n[[☃ label-image 1]]\n\n",
