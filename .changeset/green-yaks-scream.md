57c593675b89f9f2a848c9085eaab894e45a8dda
Tamara <60857422+Myranae@users.noreply.github.com>
2025-05-22 11:53:56 -0500

    Update label image widget to handle answerless data and do some cleanup (#2495)
    ## Summary:
    There are two main methods of Label Image that use answers: showRationalesForCurrentlySelectedChoices and renderMarkers. Both were updated to handle answerless data, though the first one is mainly used after scoring has begun and should have access to answers already. Additional tests were added, props were updated to account for answerless data, and an answerless story was added.
    
    Issue: LEMS-2981
    
    ## Test plan:
    - Confirm all checks and tests pass
    - `pnpm storybook`: Confirm widget renders, is interactive, and scores as expected for both answerless and answerful stories
    
    Author: Myranae
    
    Reviewers: Myranae, copilot-pull-request-reviewer[bot], benchristel, handeyeco
    
    Required Reviewers:
    
    Approved By: benchristel
    
    Checks: ✅ 8 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2495

---
+++ .changeset/green-yaks-scream.md
@@ -0,0 +1,6 @@
+---
+"@khanacademy/perseus": minor
+"@khanacademy/perseus-core": minor
+---
+
+Update label image widget to handle answerless data and do some cleanup
--- packages/perseus-core/src/index.ts
+++ packages/perseus-core/src/index.ts
@@ -160,6 +160,8 @@ export {
     default as getSorterPublicWidgetOptions,
     shuffleSorter,
 } from "./widgets/sorter/sorter-util";
+export type {LabelImagePublicWidgetOptions} from "./widgets/label-image/label-image-util";
+export type {LabelImageMarkerPublicData} from "./widgets/label-image/label-image-util";
 export {default as getDropdownPublicWidgetOptions} from "./widgets/dropdown/dropdown-util";
 export type {DropdownPublicWidgetOptions} from "./widgets/dropdown/dropdown-util";
 export {default as getNumericInputPublicWidgetOptions} from "./widgets/numeric-input/numeric-input-util";
