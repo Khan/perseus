a349d1af41724cb6d81cfa5d4fa63cbc8355d2ef
Sarah Third <sarahthird@gmail.com>
2025-05-28 11:45:54 -0700

    [non-selective-rationality] Initial prototype with full removal of showRationalesForcurrentlySelectedChoices

--- packages/perseus/src/widgets/group/group.test.tsx
+++ packages/perseus/src/widgets/group/group.test.tsx
@@ -511,20 +511,6 @@ describe("group widget", () => {
         expect(cb).toHaveBeenCalled();
     });
 
-    it("should show rationales for contained widgets", async () => {
-        // Arrange
-        const {renderer} = renderQuestion(question1);
-        await userEvent.click(screen.getAllByRole("radio")[2]); // Incorrect!
-
-        // Act
-        act(() => renderer.showRationalesForCurrentlySelectedChoices());
-
-        // Assert
-        expect(
-            screen.getByText("Here's a clue, this isn't the correct answer!"),
-        ).toBeInTheDocument();
-    });
-
     describe("traverseChildWidgets", () => {
         it("should call the contentCallback for each top-level piece of content", () => {
             // Arrange
