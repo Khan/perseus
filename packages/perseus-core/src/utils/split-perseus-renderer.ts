66cadfea617f00a47689cfab156f213530cc247b
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-06-16 15:22:57 -0500

    [LEMS-3194/static-answerless] add comment

--- packages/perseus-core/src/utils/split-perseus-renderer.ts
+++ packages/perseus-core/src/utils/split-perseus-renderer.ts
@@ -22,6 +22,13 @@ export default function splitPerseusRenderer(
     const splitWidgets = {};
 
     for (const [id, widget] of Object.entries(upgradedWidgets)) {
+        /**
+         * Static widgets are presentational, used to represent concepts
+         * rather than collect answers from learners. For instance we might
+         * want a static InteractiveGraph that shows learners a chart before
+         * we give them a Radio widget to check their understanding of the chart.
+         * We need to keep the answers in this case because it's pre-scoring information.
+         */
         if (widget.static) {
             splitWidgets[id] = widget;
         } else {
