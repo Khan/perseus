b6e035e61497a81f2c60ac5caab554eeb7b7b374
Sarah Third <sarahthird@gmail.com>
2025-06-20 14:57:12 -0700

    [mc-radio-coming-at-ya] Fixing a couple comments

--- packages/perseus/src/widgets/radio/multiple-choice-widget.new.tsx
+++ packages/perseus/src/widgets/radio/multiple-choice-widget.new.tsx
@@ -115,9 +115,9 @@ class MultipleChoiceWidget extends React.Component<Props> implements Widget {
             return {
                 choicesSelected,
             };
-            // Support legacy choiceState implementation
         }
 
+        // Support legacy choiceState implementation
         const {values} = props;
         if (values) {
             const choicesSelected = [...values];
@@ -131,6 +131,7 @@ class MultipleChoiceWidget extends React.Component<Props> implements Widget {
                 choicesSelected,
             };
         }
+
         // Nothing checked
         return {
             choicesSelected: props.choices.map(() => false),
