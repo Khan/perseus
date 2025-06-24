4af4b1f23bf406fa609805f1209860ca8ed3058e
Matthew <matthewcurtis@khanacademy.org>
2025-06-24 14:36:37 -0500

    FreeResponse: renderer user input state (#2585)
    ## Summary:
    
    See the [parent PR](https://github.com/Khan/perseus/pull/2566) for more context. Part of [LEMS-3208](https://khanacademy.atlassian.net/browse/LEMS-3208).
    
    In #2566 I add additional APIs to let widgets consume `userInput` from a parent and to update that parent state using `handleUserInput`, while also supporting the legacy way of storing user input (which was a combination of internal widget state and stashing state in a random blob in Renderer). This PR is part of a series of PRs that go widget-by-widget to use the new API.
    
    This is a generic summary I'm putting on each PR and not every point will be applicable to every widget, but the common themes are:
    
    - I added a test for serialization to make sure we're still backwards compatible (see [LEMS-3185](https://khanacademy.atlassian.net/browse/LEMS-3185)), then implemented the helpers `getSerializedState` and `getUserInputFromSerializedState` to keep supporting the expected results.
    - I moved user input for the component into the new `userInput` state in Renderer. This means consuming `userInput` in the component and calling `handleUserInput` on change.
    - When `transform` did something to initialize user input state, I moved the logic to `getStartUserInput`.
    - When `staticTransform` did something to get correct state in static widgets, I moved the logic to `getCorrectUserInput`.
    - Editors that use a widget to collect answer data are changed to support the new API.
    
    Please see the [parent PR](https://github.com/Khan/perseus/pull/2566) for more information.
    
    ---
    
    FreeResponseEditor doesn't use FreeResponse, so this shouldn't affect the editing experience
    
    [LEMS-3208]: https://khanacademy.atlassian.net/browse/LEMS-3208?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [LEMS-3185]: https://khanacademy.atlassian.net/browse/LEMS-3185?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    Author: handeyeco
    
    Reviewers: aag, benchristel, catandthemachines, ivyolamit, mark-fitzgerald, SonicScrewdriver, anakaren-rojas, nishasy
    
    Required Reviewers:
    
    Approved By: benchristel
    
    Checks: ✅ 8 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2585

--- packages/perseus/src/widgets/free-response/free-response.tsx
+++ packages/perseus/src/widgets/free-response/free-response.tsx
@@ -34,36 +34,35 @@ type RenderProps = Pick<
 >;
 type Props = WidgetProps<RenderProps, PerseusFreeResponseUserInput>;
 
-type State = {
-    currentValue: string;
-};
+type DefaultProps = Pick<Props, "userInput">;
 
 // TODO(agoforth): Create a custom validator for the widget that will cause
 //   renderer.emptyWidgets() to work when there is no user input.
 
-export class FreeResponse
-    extends React.Component<Props, State>
-    implements Widget
-{
+export class FreeResponse extends React.Component<Props> implements Widget {
     static contextType = PerseusI18nContext;
     declare context: React.ContextType<typeof PerseusI18nContext>;
 
-    state: State = {
-        currentValue: "",
+    static defaultProps: DefaultProps = {
+        userInput: {
+            currentValue: "",
+        },
     };
 
     characterCount = () => {
-        return this.state.currentValue.replace(/\n/g, "").length;
+        return this.props.userInput.currentValue.replace(/\n/g, "").length;
     };
 
+    /**
+     * TODO: remove this when everything is pulling from Renderer state
+     * @deprecated get user input from Renderer state
+     * */
     getUserInput(): PerseusFreeResponseUserInput {
-        return {
-            currentValue: this.state.currentValue,
-        };
+        return this.props.userInput;
     }
 
-    handleChange = (newValue: string) => {
-        this.setState({currentValue: newValue});
+    _handleUserInput = (newValue: string) => {
+        this.props.handleUserInput({currentValue: newValue});
     };
 
     isOverLimit() {
@@ -123,10 +122,10 @@ export class FreeResponse
                     field={
                         <TextArea
                             error={this.isOverLimit()}
-                            onChange={this.handleChange}
+                            onChange={this._handleUserInput}
                             placeholder={this.props.placeholder}
                             style={styles.textarea}
-                            value={this.state.currentValue}
+                            value={this.props.userInput.currentValue}
                         />
                     }
                     styles={{
