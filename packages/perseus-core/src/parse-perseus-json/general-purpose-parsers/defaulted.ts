33bb9f07107d81c0834d84615fc4d22d16f99935
Ben Christel <benchristel@khanacademy.org>
2025-06-30 13:41:00 -0700

    [benc/assign-radio-ids-by-location] WIP

--- packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/defaulted.ts
+++ packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/defaulted.ts
@@ -1,14 +1,14 @@
 import {success} from "../result";
 
-import type {Parser} from "../parser-types";
+import type {ParseContext, Parser} from "../parser-types";
 
 export function defaulted<T>(
     parser: Parser<T>,
-    fallback: (missingValue: null | undefined) => NoInfer<T>,
+    fallback: (missingValue: null | undefined, ctx: ParseContext) => NoInfer<T>,
 ): Parser<T> {
     return (rawValue, ctx) => {
         if (rawValue == null) {
-            return success(fallback(rawValue));
+            return success(fallback(rawValue, ctx));
         }
         return parser(rawValue, ctx);
     };
