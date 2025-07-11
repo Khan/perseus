7bb33f106948dd1085445abc0f6c306b66d3304c
Ivy Olamit <ivy@khanacademy.org>
2025-07-11 15:26:18 -0700

    [LEMS-3038/improve-perseus-docs] reorganized docs files, renamed __stories__ to __docs__ for new structure

---
+++ packages/perseus/src/components/__docs__/simple-keypad-input.stories.tsx
@@ -0,0 +1,24 @@
+import {action} from "storybook/actions";
+
+import SimpleKeypadInput from "../simple-keypad-input";
+
+import type {Meta, StoryObj} from "@storybook/react-vite";
+
+const meta: Meta = {
+    title: "Components/Simple Keypad Input",
+    component: SimpleKeypadInput,
+    args: {
+        onChange: action("onChange"),
+        onFocus: action("onFocus"),
+        onBlur: action("onBlur"),
+    },
+};
+export default meta;
+
+type Story = StoryObj<typeof SimpleKeypadInput>;
+
+export const EmptyPropsObject: Story = {};
+
+export const CustomValue: Story = {
+    args: {value: "Test value"},
+};
