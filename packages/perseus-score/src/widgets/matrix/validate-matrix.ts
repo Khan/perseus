d0d3f5c400f0b7250359bf157022dcc28233f7e7
Sarah Third <sarahthird@gmail.com>
2025-07-11 08:49:24 -0700

    [lets-see-how-deep-the-rabbit-hole-goes] A quick PR for testing the matrix widget upstream, as we want to try to recreate a nebulous bug <see below>

--- packages/perseus-score/src/widgets/matrix/validate-matrix.ts
+++ packages/perseus-score/src/widgets/matrix/validate-matrix.ts
@@ -18,7 +18,10 @@ import type {
 function validateMatrix(userInput: PerseusMatrixUserInput): ValidationResult {
     const supplied = userInput.answers;
     const suppliedSize = getMatrixSize(supplied);
-
+    // eslint-disable-next-line no-console
+    console.log("suppliedSize", suppliedSize);
+    // eslint-disable-next-line no-console
+    console.log("supplied", supplied);
     for (let row = 0; row < suppliedSize[0]; row++) {
         for (let col = 0; col < suppliedSize[1]; col++) {
             if (
