fe8acbe12dea4919edc9c853ed7d2fab318f2616
Ivy Olamit <ivy@khanacademy.org>
2025-07-23 10:38:35 -0700

    [LEMS-3038/improve-perseus-docs] resolve merge conflict

--- packages/perseus-editor/src/widgets/__docs__/radio-editor.stories.tsx
+++ packages/perseus-editor/src/widgets/__docs__/radio-editor.stories.tsx
@@ -1,13 +1,20 @@
-/* eslint-disable @khanacademy/ts-no-error-suppressions */
-import {StyleSheet, css} from "aphrodite";
+import {css, StyleSheet} from "aphrodite";
 import * as React from "react";
 import {action} from "storybook/actions";
 
+import {
+    multiChoiceQuestion,
+    question,
+} from "../../../../perseus/src/widgets/radio/__tests__/radio.testdata";
+import EditorPageWithStorybookPreview from "../../__docs__/editor-page-with-storybook-preview";
+import {registerAllWidgetsAndEditorsForTesting} from "../../util/register-all-widgets-and-editors-for-testing";
 import RadioEditor from "../radio/editor";
 
 import type {APIOptions} from "@khanacademy/perseus";
 import type {PerseusRadioWidgetOptions} from "@khanacademy/perseus-core";
 import type {Meta, StoryObj} from "@storybook/react-vite";
+
+registerAllWidgetsAndEditorsForTesting(); // SIDE_EFFECTY!!!! :cry:
 
 type StoryArgs = StoryObj<RadioEditor>;
 
@@ -19,8 +26,6 @@ export default {
     parameters: {
         componentSubtitle:
             "An editor for adding a radio widget that allows users to select a single option from multiple choices.",
-        // Since by default, we don't show all stories in autodocs; this will show all stories in autodocs only for this component.
-        docs: {disable: false},
     },
 } as Story;
 
@@ -53,3 +58,11 @@ class WithState extends React.Component<Empty, PerseusRadioWidgetOptions> {
 export const Default = (args: StoryArgs): React.ReactElement => {
     return <WithState />;
 };
+
+export const SingleChoice = (): React.ReactElement => (
+    <EditorPageWithStorybookPreview question={question} />
+);
+
+export const MultiChoice = (): React.ReactElement => (
+    <EditorPageWithStorybookPreview question={multiChoiceQuestion} />
+);
