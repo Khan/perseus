d9616b73549985754c71109d92906e2d3c9f8e8e
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2025-07-25 18:08:53 -0700

    [LEMS-3300-refactor-multiple-choice-component] Finish styling to work with scrollable view component. Clean up JSX and CSS code (remove unused items).

--- packages/perseus/src/widgets/radio/multiple-choice-component.new.tsx
+++ packages/perseus/src/widgets/radio/multiple-choice-component.new.tsx
@@ -1,5 +1,3 @@
-import {type PerseusRadioWidgetOptions} from "@khanacademy/perseus-core";
-import classNames from "classnames";
 import * as React from "react";
 
 import {usePerseusI18n} from "../../components/i18n-context";
@@ -10,46 +8,21 @@ import styles from "./multiple-choice.module.css";
 import {getChoiceLetter} from "./util";
 import {getInstructionsText} from "./utils/string-utils";
 
-import type {APIOptions} from "../../types";
-
-// TODO(LEMS-3170): Simplify the ChoiceType by using ChoiceProps directly.
-/**
- * Represents a single choice in the MultipleChoiceComponent
- */
-export interface ChoiceType {
-    checked: boolean;
-    content: React.ReactNode;
-    rationale: React.ReactNode;
-    hasRationale: boolean;
-    showRationale: boolean;
-    showCorrectness: boolean;
-    correct: boolean;
-    isNoneOfTheAbove: boolean;
-    previouslyAnswered: boolean;
-    revealNoneOfTheAbove: boolean;
-    disabled: boolean;
-}
+import type {ChoiceType} from "./multiple-choice-widget.new";
+import type {PerseusStrings} from "@khanacademy/perseus/strings";
 
 /**
  * Props for the MultipleChoiceComponent
  */
 interface MultipleChoiceComponentProps {
-    apiOptions: APIOptions;
     choices: ReadonlyArray<ChoiceType>;
     countChoices: boolean | null | undefined;
-    // Edit mode is only ever used in the Content Editor, and will be removed
-    // once we stop using BaseRadio in editor.
-    // TODO(LEMS-3229): Remove this prop once we stop using BaseRadio in editor
-    editMode?: boolean;
-    isLastUsedWidget?: boolean;
-    labelWrap: boolean;
     multipleSelect?: boolean;
     numCorrect: number;
     onChoiceChange: (choiceIndex: number, newCheckedState: boolean) => void;
     // Review mode is used when the user has successfully answered the question
     // and is now reviewing their answer.
     reviewMode: boolean;
-    reviewModeRubric?: PerseusRadioWidgetOptions | null;
 }
 
 /**
@@ -64,26 +37,14 @@ interface MultipleChoiceComponentProps {
  * Created as part of the Radio Revitalization Project (LEMS-2933).
  */
 const MultipleChoiceComponent = ({
-    apiOptions,
-    reviewModeRubric,
-    reviewMode,
-    editMode = false,
-    multipleSelect = false,
-    labelWrap,
-    countChoices,
-    numCorrect,
-    isLastUsedWidget,
     choices,
+    countChoices,
+    multipleSelect = false,
+    numCorrect,
     onChoiceChange,
+    reviewMode,
 }: MultipleChoiceComponentProps): React.ReactElement => {
     const {strings} = usePerseusI18n();
-
-    const choiceListClassName = classNames(
-        "perseus-widget-radio",
-        // TODO(LEMS-3229): Remove this line after we stop using BaseRadio in editor
-        !editMode && "perseus-rendered-radio", // Styles to be applied when not in the editor
-        styles.choiceList, // Main class for the choice list
-    );
 
     const instructions = getInstructionsText({
         multipleSelect,
@@ -92,6 +53,10 @@ const MultipleChoiceComponent = ({
         strings,
     });
 
+    const choiceListClasses = [styles.choiceList]
+        .concat(reviewMode ? [styles.reviewAnswers] : [])
+        .join(" ");
+
     return (
         <fieldset
             className={styles.container}
@@ -99,9 +64,10 @@ const MultipleChoiceComponent = ({
         >
             <legend className={styles.instructions}>{instructions}</legend>
             <ScrollableView overflowX="auto">
-                <ul className={choiceListClassName}>
+                <ul className={choiceListClasses}>
                     <ChoiceListItems
                         choices={choices}
+                        i18nStrings={strings}
                         onChoiceChange={onChoiceChange}
                         reviewMode={reviewMode}
                         multipleSelect={multipleSelect}
@@ -112,15 +78,26 @@ const MultipleChoiceComponent = ({
     );
 };
 
-const ChoiceListItems = (props: MultipleChoiceComponentProps): React.ReactElement => {
-    const {choices, onChoiceChange, reviewMode, multipleSelect = false} = props;
-    const {strings} = usePerseusI18n();
+/**
+ * Props for the ChoiceList
+ */
+interface ChoiceListItemsProps {
+    choices: ReadonlyArray<ChoiceType>;
+    i18nStrings: PerseusStrings;
+    multipleSelect: boolean;
+    onChoiceChange: (choiceIndex: number, newCheckedState: boolean) => void;
+    reviewMode: boolean;
+}
+
+const ChoiceListItems = (props: ChoiceListItemsProps): React.ReactElement => {
+    const {choices, i18nStrings, multipleSelect, onChoiceChange, reviewMode} =
+        props;
 
     const items = choices.map((choice, i) => {
         const updateChecked = (isChecked: boolean) => {
             onChoiceChange(i, isChecked);
         };
-        const choiceLetter = getChoiceLetter(i, strings);
+        const choiceLetter = getChoiceLetter(i, i18nStrings);
         const showCorrectness =
             reviewMode === true
                 ? choice.correct
@@ -128,7 +105,7 @@ const ChoiceListItems = (props: MultipleChoiceComponentProps): React.ReactElemen
                     : "wrong"
                 : undefined;
         const content = choice.isNoneOfTheAbove
-            ? strings.noneOfTheAbove
+            ? i18nStrings.noneOfTheAbove
             : choice.content;
         const rationaleClasses = [styles.rationale]
             .concat(showCorrectness ? [styles["is-" + showCorrectness]] : [])
@@ -147,7 +124,7 @@ const ChoiceListItems = (props: MultipleChoiceComponentProps): React.ReactElemen
                 showCorrectness={showCorrectness}
                 updateChecked={updateChecked}
             >
-                <div className={classNames(styles.content)}>
+                <div className={styles.content}>
                     {content}
                     {rationale}
                 </div>
