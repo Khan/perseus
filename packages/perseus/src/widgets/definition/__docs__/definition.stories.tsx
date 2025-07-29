8463339128f5f8206186104142997914b1a1e38c
Ivy Olamit <ivy@khanacademy.org>
2025-07-29 12:59:16 -0700

    [LEMS-3038/improve-diner-docs] add additional a11y docs

--- packages/perseus/src/widgets/definition/__docs__/definition.stories.tsx
+++ packages/perseus/src/widgets/definition/__docs__/definition.stories.tsx
@@ -1,11 +1,10 @@
 import {generateTestPerseusItem} from "@khanacademy/perseus-core";
 import * as React from "react";
 
-import {ServerItemRendererWithDebugUI} from "../../../../../testing/server-item-renderer-with-debug-ui";
-import {storybookDependenciesV2} from "../../../../../testing/test-dependencies";
-import ArticleRenderer from "../../article-renderer";
-
-import {article, question} from "./definition.testdata";
+import {ServerItemRendererWithDebugUI} from "../../../../../../testing/server-item-renderer-with-debug-ui";
+import {storybookDependenciesV2} from "../../../../../../testing/test-dependencies";
+import ArticleRenderer from "../../../article-renderer";
+import {article, question} from "../definition.testdata";
 
 import type {Meta, StoryObj} from "@storybook/react-vite";
 
