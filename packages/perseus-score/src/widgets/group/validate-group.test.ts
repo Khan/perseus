9c845804857b072161c89f6e760f5839e2ebd1f8
Ben Christel <benchristel@khanacademy.org>
2025-07-31 15:10:35 -0700

    [benc/score-missing-widgets] Handle undefined user input for group

---
+++ packages/perseus-score/src/widgets/group/validate-group.test.ts
@@ -0,0 +1,25 @@
+import {PerseusGroupValidationData} from "@khanacademy/perseus-core";
+import validateGroup from "./validate-group";
+
+describe("validateGroup", () => {
+    it("returns invalid when the user input is undefined", () => {
+        // Arrange:
+        const userInput = undefined;
+        const validationData: PerseusGroupValidationData = {
+            widgets: {
+                "categorizer 1": {
+                    type: "categorizer",
+                    options: {
+                        items: ["a"],
+                    }
+                }
+            },
+        };
+
+        // Act:
+        const result = validateGroup(userInput, validationData, "en");
+
+        // Assert:
+        expect(result).toHaveInvalidInput();
+    })
+})
