2cb3b2181a966c6ac4f41ab2d9e752948a699d38
Ivy Olamit <ivy@khanacademy.org>
2025-08-05 16:46:10 -0700

    [ivy/add-perseus-logo] Add perseus logo in storybook

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -6,4 +6,51 @@ addons.setConfig({
         // See docs https://storybook.js.org/docs/configure/user-interface/sidebar-and-urls#roots
         showRoots: false,
     },
+    theme: {
+        base: "light",
+
+        // Brand
+        brandTitle: "Perseus",
+        brandUrl: "/",
+        brandImage: "./perseus-logo.svg",
+        brandTarget: "_self",
+
+        // Colors
+        colorPrimary: "#1865f2", // blue
+        colorSecondary: "#1865f2", // blue
+
+        // UI
+        appBg: "#f7f8fa", // offWhite
+        appContentBg: "#ffffff",
+        appBorderColor: "#e0e0e0",
+        appBorderRadius: 4,
+
+        // Typography
+        fontBase: '"Lato", sans-serif',
+        fontCode: "Inconsolata",
+
+        // Text colors
+        textColor: "#21242c", // offBlack
+        textInverseColor: "#ffffff",
+        textMutedColor: "#757575",
+
+        // Toolbar
+        barTextColor: "#2e3438",
+        barSelectedColor: "#1865f2",
+        barBg: "#ffffff",
+
+        // Form
+        inputBg: "#ffffff",
+        inputBorder: "#e0e0e0",
+        inputTextColor: "#2e3438",
+        inputBorderRadius: 4,
+
+        // Additional required properties
+        appPreviewBg: "#ffffff",
+        barHoverColor: "#f0f0f0",
+        buttonBg: "#ffffff",
+        buttonBorder: "#e0e0e0",
+        booleanBg: "#f7f8fa",
+        booleanSelectedBg: "#1865f2",
+    },
 });
