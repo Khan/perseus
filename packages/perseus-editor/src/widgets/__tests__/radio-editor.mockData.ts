54cf974a66f38b1cce0f37195006c44d510185d0
Anakaren Rojas <anakaren@khanacademy.org>
2025-08-07 14:14:05 -0700

    [add-choice-ids] remove uuid dependency; update mock data to use array indices

--- packages/perseus-editor/src/widgets/__tests__/radio-editor.mockData.ts
+++ packages/perseus-editor/src/widgets/__tests__/radio-editor.mockData.ts
@@ -7,60 +7,60 @@ export const mockIds = [
 ];
 
 export const singleChoice = {
-    id: "1-1-1-1-1",
+    id: mockIds[0],
     content: "Choice 1",
 };
 export const singleChoiceWithCorrect = {...singleChoice, correct: true};
 
 const threeChoices = [
-    {id: "1-1-1-1-1", content: "Choice 1"},
-    {id: "2-2-2-2-2", content: "Choice 2"},
-    {id: "3-3-3-3-3", content: "Choice 3"},
+    {id: mockIds[0], content: "Choice 1"},
+    {id: mockIds[1], content: "Choice 2"},
+    {id: mockIds[2], content: "Choice 3"},
 ];
 
 export const threeChoicesWithCorrect = [
-    {id: "1-1-1-1-1", content: "Choice 1"},
+    {id: mockIds[0], content: "Choice 1"},
     {
-        id: "2-2-2-2-2",
+        id: mockIds[1],
         content: "Choice 2",
         correct: true,
     },
-    {id: "3-3-3-3-3", content: "Choice 3"},
+    {id: mockIds[2], content: "Choice 3"},
 ];
 
 export const threeChoicesWithCorrectAndNoneOfTheAbove = [
     {
-        id: "2-2-2-2-2",
+        id: mockIds[1],
         content: "Choice 2",
         correct: true,
     },
-    {id: "3-3-3-3-3", content: "Choice 3"},
+    {id: mockIds[2], content: "Choice 3"},
     {id: mockIds[4], content: "None of the above", isNoneOfTheAbove: true},
 ];
 
 export const fourChoices = [
     ...threeChoices,
-    {id: "4-4-4-4-4", content: "Choice 4"},
+    {id: mockIds[3], content: "Choice 4"},
 ];
 
 export const fourChoicesWithAllIncorrect = [
     {
-        id: "1-1-1-1-1",
+        id: mockIds[0],
         content: "Choice 1",
         correct: false,
     },
     {
-        id: "2-2-2-2-2",
+        id: mockIds[1],
         content: "Choice 2",
         correct: false,
     },
     {
-        id: "3-3-3-3-3",
+        id: mockIds[2],
         content: "Choice 3",
         correct: false,
     },
     {
-        id: "4-4-4-4-4",
+        id: mockIds[3],
         content: "Choice 4",
         correct: false,
     },
@@ -68,22 +68,22 @@ export const fourChoicesWithAllIncorrect = [
 
 export const fourChoicesWithFirstCorrect = [
     {
-        id: "1-1-1-1-1",
+        id: mockIds[0],
         content: "Choice 1",
         correct: true,
     },
     {
-        id: "2-2-2-2-2",
+        id: mockIds[1],
         content: "Choice 2",
         correct: false,
     },
     {
-        id: "3-3-3-3-3",
+        id: mockIds[2],
         content: "Choice 3",
         correct: false,
     },
     {
-        id: "4-4-4-4-4",
+        id: mockIds[3],
         content: "Choice 4",
         correct: false,
     },
@@ -91,22 +91,22 @@ export const fourChoicesWithFirstCorrect = [
 
 export const fourChoicesWithSecondCorrect = [
     {
-        id: "1-1-1-1-1",
+        id: mockIds[0],
         content: "Choice 1",
         correct: false,
     },
     {
-        id: "2-2-2-2-2",
+        id: mockIds[1],
         content: "Choice 2",
         correct: true,
     },
     {
-        id: "3-3-3-3-3",
+        id: mockIds[2],
         content: "Choice 3",
         correct: false,
     },
     {
-        id: "4-4-4-4-4",
+        id: mockIds[3],
         content: "Choice 4",
         correct: false,
     },
@@ -114,22 +114,22 @@ export const fourChoicesWithSecondCorrect = [
 
 export const fourChoicesWithTwoCorrect = [
     {
-        id: "1-1-1-1-1",
+        id: mockIds[0],
         content: "Choice 1",
         correct: true,
     },
     {
-        id: "2-2-2-2-2",
+        id: mockIds[1],
         content: "Choice 2",
         correct: true,
     },
     {
-        id: "3-3-3-3-3",
+        id: mockIds[2],
         content: "Choice 3",
         correct: false,
     },
     {
-        id: "4-4-4-4-4",
+        id: mockIds[3],
         content: "Choice 4",
         correct: false,
     },
@@ -141,19 +141,19 @@ export const fourChoicesWithNoneOfTheAboveAndCorrect = [
 ];
 
 export const fourChoicesWithOneChoice1A = [
-    {id: "1-1-1-1-1", content: "Choice 1A"},
-    {id: "2-2-2-2-2", content: "Choice 2"},
-    {id: "3-3-3-3-3", content: "Choice 3"},
-    {id: "4-4-4-4-4", content: "Choice 4"},
+    {id: mockIds[0], content: "Choice 1A"},
+    {id: mockIds[1], content: "Choice 2"},
+    {id: mockIds[2], content: "Choice 3"},
+    {id: mockIds[3], content: "Choice 4"},
 ];
 
 export const fourChoicesWithOneRationale = [
     {
-        id: "1-1-1-1-1",
+        id: mockIds[0],
         content: "Choice 1",
         rationale: "A",
     },
     {id: "2-2-2-2-2", content: "Choice 2"},
-    {id: "3-3-3-3-3", content: "Choice 3"},
-    {id: "4-4-4-4-4", content: "Choice 4"},
+    {id: mockIds[2], content: "Choice 3"},
+    {id: mockIds[3], content: "Choice 4"},
 ];
