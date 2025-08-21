196edeca8eea6f4cb1c27683ff0baec126f27e38
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2025-08-21 15:46:29 -0700

    [bugfix/LEMS-3428-radio-missing-correctness-styling-in-graded-group] Add story in Radio widget section for testing.

--- packages/perseus/src/widgets/radio/__docs__/radio.new.stories.tsx
+++ packages/perseus/src/widgets/radio/__docs__/radio.new.stories.tsx
@@ -2,6 +2,7 @@ import {generateTestPerseusItem} from "@khanacademy/perseus-core";
 import * as React from "react";
 
 import {ServerItemRendererWithDebugUI} from "../../../../../../testing/server-item-renderer-with-debug-ui";
+import {groupedRadioRationaleQuestion} from "../../graded-group/graded-group.testdata";
 import {
     questionWithPassage,
     choicesWithImages,
@@ -162,13 +163,22 @@ export const MultiSelectWithScroll = {
     },
 };
 
-export const ScrollingInGradedGroupSet = {
+export const GradedGroupSetWithScroll = {
     args: {
         item: generateTestPerseusItem({
             question: overflowContentInGradedGroupSet,
         }),
     },
 };
+
+export const GradedGroup = {
+    args: {
+        item: generateTestPerseusItem({
+            question: groupedRadioRationaleQuestion,
+        }),
+    },
+};
+
 
 // NOTE(Tamara): For answerless stories, the user's selection disappears after
 // clicking the Check button the first time. This is because the widget
