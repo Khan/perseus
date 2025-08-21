12240d04fb6c6593ca5bc3905604745a2394a474
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-08-21 15:23:46 -0500

    [experiment-rm-randomness] export sharedRestoreUserInputFromSerializedState

--- packages/perseus/src/index.ts
+++ packages/perseus/src/index.ts
@@ -154,7 +154,10 @@ export {
     getQuadraticCoords,
     getAngleCoords,
 } from "./widgets/interactive-graphs/reducer/initialize-graph-state";
-export {default as UserInputManager} from "./user-input-manager";
+export {
+    default as UserInputManager,
+    sharedRestoreUserInputFromSerializedState,
+} from "./user-input-manager";
 
 export {makeSafeUrl} from "./widgets/phet-simulation";
 
