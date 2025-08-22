953a83bdb9f3c655683c7c03d4c9636c3623ce6d
Nisha Yerunkar <nisha@khanacademy.org>
2025-08-21 17:06:04 -0700

    [update-image-editor-code] [Image] | (DX) | Modernize the Image widget edtior code
    - Remove unused code
    - Break out the extra settings into another component (functional component :) )
    
    NOTE: Changes to style are part of a different ticket. This is just moving code around.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-3383
    
    Test plan:
    Storybook
    - Go to `/?path=/story/editors-editorpage--demo`
    - Create an image widget
    - Paste an image URL (I'm using `https://cdn.kastatic.org/ka-content-images/61831c1329dbc32036d7dd0d03e06e7e2c622718.jpg`)
    - Confirm that changing the caption changes the caption in the preview
    - Change the alt and use the browser inspector to confirm it updated in the preview

---
+++ packages/perseus-editor/src/widgets/image-editor/image-editor.tsx
@@ -0,0 +1,190 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import {
+    components,
+    Changeable,
+    EditorJsonify,
+    Util,
+} from "@khanacademy/perseus";
+import {
+    imageLogic,
+    type ImageDefaultWidgetOptions,
+    type Range,
+    type Size,
+} from "@khanacademy/perseus-core";
+import * as React from "react";
+import _ from "underscore";
+
+import BlurInput from "../../components/blur-input";
+
+import ImageSettings from "./image-settings";
+
+import type {APIOptions} from "@khanacademy/perseus";
+
+type ChangeFn = typeof Changeable.change;
+
+const {InfoTip} = components;
+
+// Match any image URL (including "web+graphie" links) that is hosted by KA.
+// We're somewhat generous in our AWS URL matching
+// ("ka-<something>.s3.amazonaws.com") so that we don't have to update Perseus
+// every time we add a new proxied AWS bucket.
+const INTERNALLY_HOSTED_DOMAINS =
+    "(" +
+    "ka-.*.s3.amazonaws.com|" +
+    "(fastly|cdn).kastatic.org|" +
+    "khanacademy.org|" +
+    "kasandbox.org" +
+    ")";
+const INTERNALLY_HOSTED_URL_RE = new RegExp(
+    "^(https?|web\\+graphie)://[^/]*" + INTERNALLY_HOSTED_DOMAINS,
+);
+
+type Props = Changeable.ChangeableProps & {
+    apiOptions: APIOptions;
+
+    title: string;
+    range: [Readonly<Range>, Readonly<Range>];
+    box: Size;
+    backgroundImage: any;
+    labels: ReadonlyArray<string>;
+    alt: string;
+    caption: string;
+};
+
+type State = {
+    backgroundImageError?: string;
+};
+
+// JSDoc will be shown in Storybook widget editor description
+/**
+ * An editor for adding an image widget that allows users to display and configure images within content.
+ */
+class ImageEditor extends React.Component<Props> {
+    static displayName = "ImageEditor";
+    static widgetName = "image";
+    _isMounted = false;
+
+    static defaultProps: ImageDefaultWidgetOptions =
+        imageLogic.defaultWidgetOptions;
+
+    state: State = {
+        backgroundImageError: "",
+    };
+
+    componentDidMount() {
+        // TODO(scottgrant): This is a hack to remove the deprecated call to
+        // this.isMounted() but is still considered an anti-pattern.
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    change: ChangeFn = (...args) => {
+        return Changeable.change.apply(this, args);
+    };
+
+    setUrl(url, width, height, silent) {
+        // Because this calls into WidgetEditor._handleWidgetChange, which
+        // checks for this widget's ref to serialize it.
+        //
+        // Errors if you switch items before the `Image` from `onUrlChange`
+        // loads.
+        if (!this._isMounted) {
+            return;
+        }
+
+        const image = _.clone(this.props.backgroundImage);
+        image.url = url;
+        image.width = width;
+        image.height = height;
+        const box = [image.width, image.height];
+        this.props.onChange(
+            {
+                backgroundImage: image,
+                box: box,
+            },
+            null,
+            silent,
+        );
+    }
+
+    // silently load the image when the component mounts
+    // silently update url and sizes when the image loads
+    // noisily load the image in response to the author changing it
+    async onUrlChange(url: string | undefined | null, silent: boolean) {
+        // Check if we've been passed something that looks like a URL
+        if (!url) {
+            this.setUrl(url, 0, 0, silent);
+            return;
+        }
+
+        // All article content must be KA-owned!
+        if (!INTERNALLY_HOSTED_URL_RE.test(url)) {
+            this.setState({
+                backgroundImageError:
+                    "Images must be from sites hosted by Khan Academy. " +
+                    "Please input a Khan Academy-owned address, or use the " +
+                    "Add Image tool to rehost an existing image",
+            });
+            return;
+        }
+
+        // Clear previous errors
+        this.setState({backgroundImageError: ""});
+
+        try {
+            const size = await Util.getImageSizeModern(url);
+            this.setUrl(url, size[0], size[1], true);
+        } catch (error) {
+            this.setState({
+                backgroundImageError: `There was an error loading the image URL: ${JSON.stringify(
+                    error,
+                    null,
+                    2,
+                )}`,
+            });
+        }
+    }
+
+    serialize() {
+        return EditorJsonify.serialize.call(this);
+    }
+
+    render() {
+        const backgroundImage = this.props.backgroundImage;
+
+        const backgroundImageErrorText = (
+            <div className="renderer-widget-error">
+                {this.state.backgroundImageError}
+            </div>
+        );
+
+        return (
+            <div className="perseus-image-editor">
+                <label>
+                    Image url:
+                    <InfoTip>Paste an image or graphie image URL.</InfoTip>
+                    {this.state.backgroundImageError &&
+                        backgroundImageErrorText}
+                    <BlurInput
+                        value={backgroundImage.url || ""}
+                        style={{width: 332}}
+                        onChange={(url) => this.onUrlChange(url, false)}
+                    />
+                </label>
+
+                {backgroundImage.url && (
+                    <ImageSettings
+                        apiOptions={this.props.apiOptions}
+                        backgroundImage={backgroundImage}
+                        onChange={this.change}
+                    />
+                )}
+            </div>
+        );
+    }
+}
+
+export default ImageEditor;
---
+++ packages/perseus-editor/src/widgets/image-editor/image-settings.tsx
@@ -0,0 +1,97 @@
+import {Util, components} from "@khanacademy/perseus";
+import * as React from "react";
+
+import Editor from "../../editor";
+
+import type {APIOptions, Changeable} from "@khanacademy/perseus";
+import type {PerseusImageWidgetOptions} from "@khanacademy/perseus-core";
+
+type ChangeFn = typeof Changeable.change;
+
+const {InfoTip} = components;
+
+interface ImageSettingsProps extends PerseusImageWidgetOptions {
+    apiOptions: APIOptions;
+    onChange: ChangeFn;
+}
+
+export default function ImageSettings({
+    apiOptions,
+    alt,
+    backgroundImage,
+    caption,
+    onChange,
+}: ImageSettingsProps) {
+    if (!backgroundImage.url) {
+        return null;
+    }
+
+    return (
+        <div className="image-settings">
+            {!Util.isLabeledSVG(backgroundImage.url) && (
+                <div>
+                    {/* eslint-disable-next-line jsx-a11y/label-has-associated-control -- TODO(LEMS-2871): Address a11y error */}
+                    <label>
+                        <div>Preview:</div>
+                        {/* eslint-disable-next-line jsx-a11y/img-redundant-alt -- TODO(LEMS-2871): Address a11y error */}
+                        <img
+                            alt="Editor preview of image"
+                            src={backgroundImage.url}
+                            style={{
+                                width: "100%",
+                            }}
+                        />
+                    </label>
+                </div>
+            )}
+            <div>
+                <label>
+                    <div>Dimensions:</div>
+                    <p>
+                        {backgroundImage.width}x{backgroundImage.height}
+                    </p>
+                </label>
+            </div>
+
+            <div>
+                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control -- TODO(LEMS-2871): Address a11y error */}
+                <label>
+                    <div>
+                        Alt text:
+                        <InfoTip>
+                            This is important for screenreaders. The content of
+                            this alt text will be formatted as markdown (tables,
+                            emphasis, etc. are supported).
+                        </InfoTip>
+                    </div>
+                    <Editor
+                        apiOptions={apiOptions}
+                        content={alt}
+                        onChange={(props) => {
+                            if (props.content != null) {
+                                onChange("alt", props.content);
+                            }
+                        }}
+                        widgetEnabled={false}
+                    />
+                </label>
+            </div>
+            <div>
+                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control -- TODO(LEMS-2871): Address a11y error */}
+                <label>
+                    <div>Caption:</div>
+                    <Editor
+                        apiOptions={apiOptions}
+                        content={caption}
+                        onChange={(props) => {
+                            if (props.content != null) {
+                                onChange("caption", props.content);
+                            }
+                        }}
+                        widgetEnabled={false}
+                    />
+                </label>
+            </div>
+        </div>
+    );
+}
