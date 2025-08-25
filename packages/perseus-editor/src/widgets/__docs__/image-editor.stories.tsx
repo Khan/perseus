a95e65d7e06a3aa7b970f4b1586843df3f0c7f55
Nisha Yerunkar <nisha@khanacademy.org>
2025-08-25 10:18:01 -0700

    [Image] | (DX) | Update the Storybook experience for Image Widget (#2823)
    ## Summary:
    - Update image.stories.tsx
      - Move images.stories.tsx under `__docs__` folder
      - Update the questions so they make sense and are not redundant
    - Update image-editor.stories.tsx
      - Add argTypes so that unused props are not editable
      - Add a "WithinEditorPage" story
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-3384
    
    ## Test plan:
    - http://localhost:6006/?path=/docs/widgets-image--docs
    - http://localhost:6006/?path=/docs/widgets-image-editor-demo--docs
    
    Author: nishasy
    
    Reviewers: jeremywiebe, ivyolamit, nishasy, catandthemachines
    
    Required Reviewers:
    
    Approved By: ivyolamit
    
    Checks: ✅ 17 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2823

--- packages/perseus-editor/src/widgets/__docs__/image-editor.stories.tsx
+++ packages/perseus-editor/src/widgets/__docs__/image-editor.stories.tsx
@@ -1,4 +1,9 @@
 import {ApiOptions} from "@khanacademy/perseus";
+import {
+    generateImageOptions,
+    generateImageWidget,
+    generateTestPerseusRenderer,
+} from "@khanacademy/perseus-core";
 import {View} from "@khanacademy/wonder-blocks-core";
 import {spacing} from "@khanacademy/wonder-blocks-tokens";
 import {LabelSmall} from "@khanacademy/wonder-blocks-typography";
@@ -6,67 +11,112 @@ import {StyleSheet} from "aphrodite";
 import * as React from "react";
 import {action} from "storybook/actions";
 
+import EditorPageWithStorybookPreview from "../../__docs__/editor-page-with-storybook-preview";
+import {registerAllWidgetsAndEditorsForTesting} from "../../util/register-all-widgets-and-editors-for-testing";
 import ImageEditor from "../image-editor";
 
 import type {PropsFor} from "@khanacademy/wonder-blocks-core";
-import type {Meta} from "@storybook/react-vite";
+import type {Meta, StoryObj} from "@storybook/react-vite";
 
-type StoryArgs = Record<any, any>;
+const PROD_EDITOR_WIDTH = 330;
+
+// This is to address timing - Perseus widget editor registry accessed before initialization!
+registerAllWidgetsAndEditorsForTesting();
 
 const meta: Meta = {
     title: "Widgets/Image/Editor Demo",
     component: ImageEditor,
     tags: ["!dev"],
+    argTypes: {
+        labels: {
+            control: false,
+            description: "Deprecated",
+        },
+        box: {
+            control: false,
+            description: "Set automatically",
+        },
+        range: {
+            control: false,
+            description: "Deprecated",
+        },
+    },
 };
 export default meta;
+
+const onChangeAction = action("onChange");
+
+type Story = StoryObj<typeof ImageEditor>;
+
+export const Default: Story = {
+    args: {},
+};
+
+export const WithState: Story = {
+    render: function Render() {
+        const widgetRef = React.useRef<ImageEditor>(null);
+        const [state, setState] = React.useState<
+            Partial<PropsFor<typeof ImageEditor>>
+        >({
+            backgroundImage: {
+                url: "https://ka-perseus-images.s3.amazonaws.com/2ee5fc32e35c5178373b39fd304b325b2994c913.png",
+            },
+        });
+
+        return (
+            <View style={styles.wrapper}>
+                <LabelSmall
+                    style={{
+                        fontStyle: "italic",
+                        marginBottom: spacing.small_12,
+                    }}
+                >
+                    <b>Note</b> that this editor has a known-issue where it does
+                    not calculate the image dimensions initially if they
+                    aren&apos;t provided. It does update the dimensions when you
+                    blur the &apos;Image url:&apos; field.
+                </LabelSmall>
+                <ImageEditor
+                    {...state}
+                    apiOptions={ApiOptions.defaults}
+                    onChange={(props) => {
+                        onChangeAction(props);
+
+                        setState({
+                            ...widgetRef.current?.serialize(),
+                            ...props,
+                        });
+                    }}
+                    ref={widgetRef}
+                />
+            </View>
+        );
+    },
+};
+
+export const WithinEditorPage = (): React.ReactElement => (
+    <div style={{width: PROD_EDITOR_WIDTH}}>
+        <EditorPageWithStorybookPreview
+            question={generateTestPerseusRenderer({
+                content: "[[☃ image 1]]",
+                widgets: {
+                    "image 1": generateImageWidget({
+                        options: generateImageOptions({
+                            backgroundImage: {
+                                url: "https://cdn.kastatic.org/ka-content-images/61831c1329dbc32036d7dd0d03e06e7e2c622718.jpg",
+                            },
+                        }),
+                    }),
+                },
+            })}
+        />
+    </div>
+);
 
 const styles = StyleSheet.create({
     wrapper: {
         // The maximum width of a widget in the editor.
-        width: 338,
+        width: PROD_EDITOR_WIDTH,
         margin: 20,
     },
 });
-
-const onChangeAction = action("onChange");
-
-const WithState = () => {
-    const widgetRef = React.useRef<ImageEditor>(null);
-    const [state, setState] = React.useState<
-        Partial<PropsFor<typeof ImageEditor>>
-    >({
-        backgroundImage: {
-            url: "https://ka-perseus-images.s3.amazonaws.com/2ee5fc32e35c5178373b39fd304b325b2994c913.png",
-        },
-    });
-
-    return (
-        <View style={styles.wrapper}>
-            <LabelSmall
-                style={{fontStyle: "italic", marginBottom: spacing.small_12}}
-            >
-                <b>Note</b> that this editor has a known-issue where it does not
-                calculate the image dimensions initially if they aren&apos;t
-                provided. It does update the dimensions when you blur the
-                &apos;Image url:&apos; field.
-            </LabelSmall>
-            <ImageEditor
-                {...state}
-                apiOptions={ApiOptions.defaults}
-                onChange={(props) => {
-                    onChangeAction(props);
-
-                    setState({
-                        ...widgetRef.current?.serialize(),
-                        ...props,
-                    });
-                }}
-                ref={widgetRef}
-            />
-        </View>
-    );
-};
-
-export const Default = (args: StoryArgs): React.ReactElement => {
-    return <WithState />;
-};
--- packages/perseus/src/widgets/image/image.testdata.ts
+++ packages/perseus/src/widgets/image/image.testdata.ts
@@ -25,13 +25,14 @@ export const question = generateTestPerseusRenderer({
 
 export const questionWithZoom = generateTestPerseusRenderer({
     content:
-        "[[☃ image 1]]\n\n=====\n\nA quilter wants to make the design shown at left using the Golden Ratio. Specifically, he wants the ratio of the triangle heights $A:B$ and $B:C$ to each equal $1.62$. If the quilter makes the triangle height $A=8\\ \\text{in}$, approximately how tall should he make triangle height $C$?",
+        "[[☃ image 1]]\n\n=====\n\nIn the apse, or semicircular recess, The Offer of the Casa Madre to Victory (L’Offerta della Casa Madre alla Vittoria) fresco recalls medieval apse decorative schemes with Christ surrounded by saints to whom the Church is dedicated. Santagata replaced Mary with a triumphant and wingless figure representing Victory, and he replaced saints with sentries. The charismatic wounded veteran Carlo Delcroix, who became the AMNIG president, is depicted presenting a model of the Casa Madre to Victory (not unlike the medieval patron Enrico Scrovegni, who offered the Arena chapel he commissioned to the Virgin Mary).",
     widgets: {
         "image 1": generateImageWidget({
             options: generateImageOptions({
-                title: "Image Title",
-                caption: "Image Caption",
-                alt: "An array of isosceles triangles. A triangle has height A. Two smaller triangle, one with height B and one with height C, have approximately the same combined height as A.",
+                title: "The Offer of the Casa Madre to Victory, 1932",
+                caption:
+                    "Carlo Delcroix presenting the Casa Madre (highlighted) to Victory. Antonio Giuseppe Santagata, The Offer of the Casa Madre to Victory, 1932, fresco (apse, assembly hall, Home for Wounded War Veterans, Rome, photo ©ANMIG)",
+                alt: "Carlo Delcroix presenting the Casa Madre (highlighted) to Victory. Antonio Giuseppe Santagata, The Offer of the Casa Madre to Victory, 1932, fresco (apse, assembly hall, Home for Wounded War Veterans, Rome, photo ©ANMIG)",
                 backgroundImage: {
                     url: "https://cdn.kastatic.org/ka-perseus-images/01f44d5b73290da6bec97c75a5316fb05ab61f12.jpg",
                     width: 1698,
