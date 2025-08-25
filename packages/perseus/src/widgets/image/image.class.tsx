27304a4c229967853d05b09bf8ff0849544fa8e3
Nisha Yerunkar <nisha@khanacademy.org>
2025-08-25 15:20:25 -0700

    [Image] | (DX) | Modernize the Image widget code (#2824)
    ## Summary:
    Convert the image widget component so it's a functional component.
    The top level is still a class component.
    
    Taking inspiration from the Numeric Input widget.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-3382
    
    ## Test plan:
    `pnpm jest`
    `pnpm typecheck`
    `pnpm lint`
    
    Check the regression stories in this PR
    
    `/?path=/docs/widgets-image-visual-regression-tests--docs`
    
    Author: nishasy
    
    Reviewers: ivyolamit, nishasy, jeremywiebe, catandthemachines
    
    Required Reviewers:
    
    Approved By: catandthemachines
    
    Checks: ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2824

---
+++ packages/perseus/src/widgets/image/image.class.tsx
@@ -0,0 +1,82 @@
+import {linterContextDefault} from "@khanacademy/perseus-linter";
+import * as React from "react";
+import _ from "underscore";
+
+import {PerseusI18nContext} from "../../components/i18n-context";
+import {getPromptJSON as _getPromptJSON} from "../../widget-ai-utils/image/image-ai-utils";
+
+import {ImageComponent} from "./image";
+
+import type {WidgetExports, WidgetProps, Widget} from "../../types";
+import type {ImagePromptJSON} from "../../widget-ai-utils/image/image-ai-utils";
+import type {Range, PerseusImageWidgetOptions} from "@khanacademy/perseus-core";
+
+const defaultBoxSize = 400;
+const defaultRange: Range = [0, 10];
+const defaultBackgroundImage = {
+    url: null,
+    width: 0,
+    height: 0,
+} as const;
+
+type ExternalProps = WidgetProps<PerseusImageWidgetOptions>;
+
+export interface Props extends ExternalProps {
+    alignment: NonNullable<ExternalProps["alignment"]>;
+    title: NonNullable<ExternalProps["title"]>;
+    range: NonNullable<ExternalProps["range"]>;
+    box: NonNullable<ExternalProps["box"]>;
+    backgroundImage: NonNullable<ExternalProps["backgroundImage"]>;
+    labels: NonNullable<ExternalProps["labels"]>;
+    alt: NonNullable<ExternalProps["alt"]>;
+    caption: NonNullable<ExternalProps["caption"]>;
+    linterContext: NonNullable<ExternalProps["linterContext"]>;
+}
+
+interface DefaultProps extends Partial<Props> {
+    alignment: Props["alignment"];
+    title: Props["title"];
+    range: Props["range"];
+    box: Props["box"];
+    backgroundImage: Props["backgroundImage"];
+    labels: Props["labels"];
+    alt: Props["alt"];
+    caption: Props["caption"];
+    linterContext: Props["linterContext"];
+}
+
+class ImageWidget extends React.Component<Props> implements Widget {
+    static contextType = PerseusI18nContext;
+    declare context: React.ContextType<typeof PerseusI18nContext>;
+
+    static defaultProps: DefaultProps = {
+        alignment: "block",
+        title: "",
+        range: [defaultRange, defaultRange],
+        box: [defaultBoxSize, defaultBoxSize],
+        backgroundImage: defaultBackgroundImage,
+        labels: [],
+        alt: "",
+        caption: "",
+        linterContext: linterContextDefault,
+    };
+
+    // this just helps with TS weak typing when a Widget
+    // doesn't implement any Widget methods
+    isWidget = true as const;
+
+    getPromptJSON(): ImagePromptJSON {
+        return _getPromptJSON(this.props);
+    }
+
+    render(): React.ReactNode {
+        return <ImageComponent {...this.props} />;
+    }
+}
+
+export default {
+    name: "image",
+    displayName: "Image",
+    widget: ImageWidget,
+    isLintable: true,
+} satisfies WidgetExports<typeof ImageWidget>;
--- packages/perseus/src/widgets/image/image.tsx
+++ packages/perseus/src/widgets/image/image.tsx
@@ -1,99 +1,63 @@
-import {linterContextDefault} from "@khanacademy/perseus-linter";
 import * as React from "react";
-import _ from "underscore";
 
 import AssetContext from "../../asset-context";
 import {PerseusI18nContext} from "../../components/i18n-context";
 import SvgImage from "../../components/svg-image";
 import Renderer from "../../renderer";
-import {getPromptJSON as _getPromptJSON} from "../../widget-ai-utils/image/image-ai-utils";
 
-import type {WidgetExports, WidgetProps, Widget} from "../../types";
-import type {ImagePromptJSON} from "../../widget-ai-utils/image/image-ai-utils";
-import type {Range, PerseusImageWidgetOptions} from "@khanacademy/perseus-core";
+import type {Props} from "./image.class";
 
-const defaultBoxSize = 400;
-const defaultRange: Range = [0, 10];
-const defaultBackgroundImage = {
-    url: null,
-    width: 0,
-    height: 0,
-} as const;
+export const ImageComponent = ({
+    apiOptions,
+    alt,
+    backgroundImage,
+    box,
+    caption,
+    linterContext,
+    labels,
+    range,
+    title,
+    trackInteraction,
+}: Props) => {
+    const context = React.useContext(PerseusI18nContext);
 
-type ExternalProps = WidgetProps<PerseusImageWidgetOptions>;
+    return (
+        <figure
+            className="perseus-image-widget"
+            style={{
+                maxWidth: backgroundImage.width,
+            }}
+        >
+            {/* Title */}
+            {title && (
+                <div className="perseus-image-title">
+                    {/* The Renderer component is used here so that the title
+                        can support markdown and TeX. */}
+                    <Renderer
+                        content={title}
+                        apiOptions={apiOptions}
+                        linterContext={linterContext}
+                        strings={context.strings}
+                    />
+                </div>
+            )}
 
-type Props = ExternalProps & {
-    alignment: NonNullable<ExternalProps["alignment"]>;
-    title: NonNullable<ExternalProps["title"]>;
-    range: NonNullable<ExternalProps["range"]>;
-    box: NonNullable<ExternalProps["box"]>;
-    backgroundImage: NonNullable<ExternalProps["backgroundImage"]>;
-    labels: NonNullable<ExternalProps["labels"]>;
-    alt: NonNullable<ExternalProps["alt"]>;
-    caption: NonNullable<ExternalProps["caption"]>;
-    linterContext: NonNullable<ExternalProps["linterContext"]>;
-};
-
-type DefaultProps = {
-    alignment: Props["alignment"];
-    title: Props["title"];
-    range: Props["range"];
-    box: Props["box"];
-    backgroundImage: Props["backgroundImage"];
-    labels: Props["labels"];
-    alt: Props["alt"];
-    caption: Props["caption"];
-    linterContext: Props["linterContext"];
-};
-
-class ImageWidget extends React.Component<Props> implements Widget {
-    static contextType = PerseusI18nContext;
-    declare context: React.ContextType<typeof PerseusI18nContext>;
-
-    static defaultProps: DefaultProps = {
-        alignment: "block",
-        title: "",
-        range: [defaultRange, defaultRange],
-        box: [defaultBoxSize, defaultBoxSize],
-        backgroundImage: defaultBackgroundImage,
-        labels: [],
-        alt: "",
-        caption: "",
-        linterContext: linterContextDefault,
-    };
-
-    // this just helps with TS weak typing when a Widget
-    // doesn't implement any Widget methods
-    isWidget = true as const;
-
-    getPromptJSON(): ImagePromptJSON {
-        return _getPromptJSON(this.props);
-    }
-
-    render(): React.ReactNode {
-        let image;
-        const alt = this.props.caption === this.props.alt ? "" : this.props.alt;
-        const {apiOptions} = this.props;
-
-        const backgroundImage = this.props.backgroundImage;
-
-        if (backgroundImage.url) {
-            const url = backgroundImage.url;
-            image = (
+            {/* Image */}
+            {backgroundImage.url && (
                 <AssetContext.Consumer>
                     {({setAssetStatus}) => (
                         <SvgImage
-                            src={url}
-                            alt={alt}
+                            src={backgroundImage.url!}
+                            alt={caption === alt ? "" : alt}
                             width={backgroundImage.width}
                             height={backgroundImage.height}
                             preloader={apiOptions.imagePreloader}
                             extraGraphie={{
-                                box: this.props.box,
-                                range: this.props.range,
-                                labels: this.props.labels,
+                                box: box,
+                                range: range,
+                                labels: labels,
                             }}
-                            trackInteraction={this.props.trackInteraction}
+                            trackInteraction={trackInteraction}
                             zoomToFullSizeOnMobile={apiOptions.isMobile}
                             constrainHeight={apiOptions.isMobile}
                             allowFullBleed={apiOptions.isMobile}
@@ -101,61 +65,26 @@ class ImageWidget extends React.Component<Props> implements Widget {
                         />
                     )}
                 </AssetContext.Consumer>
-            );
-        }
+            )}
 
-        let title;
-        let caption;
-
-        if (this.props.title) {
-            title = (
-                <div className="perseus-image-title">
-                    <Renderer
-                        content={this.props.title}
-                        apiOptions={apiOptions}
-                        linterContext={this.props.linterContext}
-                        strings={this.context.strings}
-                    />
-                </div>
-            );
-        }
-
-        if (this.props.caption) {
-            caption = (
+            {/* Caption */}
+            {caption && (
                 <figcaption
                     className="perseus-image-caption"
                     style={{
                         maxWidth: backgroundImage.width,
                     }}
                 >
+                    {/* The Renderer component is used here so that the caption
+                        can support markdown and TeX. */}
                     <Renderer
-                        content={this.props.caption}
+                        content={caption}
                         apiOptions={apiOptions}
-                        linterContext={this.props.linterContext}
-                        strings={this.context.strings}
+                        linterContext={linterContext}
+                        strings={context.strings}
                     />
                 </figcaption>
-            );
-        }
-
-        return (
-            <figure
-                className="perseus-image-widget"
-                style={{
-                    maxWidth: backgroundImage.width,
-                }}
-            >
-                {title}
-                {image}
-                {caption}
-            </figure>
-        );
-    }
-}
-
-export default {
-    name: "image",
-    displayName: "Image",
-    widget: ImageWidget,
-    isLintable: true,
-} satisfies WidgetExports<typeof ImageWidget>;
+            )}
+        </figure>
+    );
+};
