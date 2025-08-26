ae8f7b2d0d98d0bc11654fc086ace74c96384eff
Nisha Yerunkar <nisha@khanacademy.org>
2025-08-26 12:08:09 -0700

    [Image] | (DX) | Modernize the Image widget editor code (#2825)
    ## Summary:
    - Remove unused code
    - Break out the extra settings into another component (functional component :) )
    
    NOTE: Changes to style are part of a different ticket. This is just moving code around.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-3383
    
    ## Test plan:
    Storybook
    - Go to `/?path=/story/editors-editorpage--demo`
    - Create an image widget
    - Paste an image URL (I'm using `https://cdn.kastatic.org/ka-content-images/61831c1329dbc32036d7dd0d03e06e7e2c622718.jpg`)
    - Confirm that changing the caption changes the caption in the preview
    - Change the alt and use the browser inspector to confirm it updated in the preview
    
    Author: nishasy
    
    Reviewers: nishasy, ivyolamit, catandthemachines
    
    Required Reviewers:
    
    Approved By: ivyolamit, catandthemachines
    
    Checks: ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2825

--- packages/perseus-editor/src/widgets/__docs__/image-editor.stories.tsx
+++ packages/perseus-editor/src/widgets/__docs__/image-editor.stories.tsx
@@ -1,24 +1,36 @@
-import {ApiOptions} from "@khanacademy/perseus";
 import {
     generateImageOptions,
     generateImageWidget,
     generateTestPerseusRenderer,
 } from "@khanacademy/perseus-core";
-import {View} from "@khanacademy/wonder-blocks-core";
-import {spacing} from "@khanacademy/wonder-blocks-tokens";
-import {LabelSmall} from "@khanacademy/wonder-blocks-typography";
-import {StyleSheet} from "aphrodite";
 import * as React from "react";
-import {action} from "storybook/actions";
 
 import EditorPageWithStorybookPreview from "../../__docs__/editor-page-with-storybook-preview";
 import {registerAllWidgetsAndEditorsForTesting} from "../../util/register-all-widgets-and-editors-for-testing";
-import ImageEditor from "../image-editor";
+import ImageEditor from "../image-editor/image-editor";
 
-import type {PropsFor} from "@khanacademy/wonder-blocks-core";
 import type {Meta, StoryObj} from "@storybook/react-vite";
 
 const PROD_EDITOR_WIDTH = 330;
+
+const withinEditorPageDecorator = (_, {args}) => {
+    return (
+        <div style={{width: PROD_EDITOR_WIDTH}}>
+            <EditorPageWithStorybookPreview
+                question={generateTestPerseusRenderer({
+                    content: "[[☃ image 1]]",
+                    widgets: {
+                        "image 1": generateImageWidget({
+                            options: generateImageOptions({
+                                ...args,
+                            }),
+                        }),
+                    },
+                })}
+            />
+        </div>
+    );
+};
 
 // This is to address timing - Perseus widget editor registry accessed before initialization!
 registerAllWidgetsAndEditorsForTesting();
@@ -44,79 +56,37 @@ const meta: Meta = {
 };
 export default meta;
 
-const onChangeAction = action("onChange");
-
 type Story = StoryObj<typeof ImageEditor>;
 
 export const Default: Story = {
-    args: {},
-};
-
-export const WithState: Story = {
-    render: function Render() {
-        const widgetRef = React.useRef<ImageEditor>(null);
-        const [state, setState] = React.useState<
-            Partial<PropsFor<typeof ImageEditor>>
-        >({
-            backgroundImage: {
-                url: "https://ka-perseus-images.s3.amazonaws.com/2ee5fc32e35c5178373b39fd304b325b2994c913.png",
-            },
-        });
-
-        return (
-            <View style={styles.wrapper}>
-                <LabelSmall
-                    style={{
-                        fontStyle: "italic",
-                        marginBottom: spacing.small_12,
-                    }}
-                >
-                    <b>Note</b> that this editor has a known-issue where it does
-                    not calculate the image dimensions initially if they
-                    aren&apos;t provided. It does update the dimensions when you
-                    blur the &apos;Image url:&apos; field.
-                </LabelSmall>
-                <ImageEditor
-                    {...state}
-                    apiOptions={ApiOptions.defaults}
-                    onChange={(props) => {
-                        onChangeAction(props);
-
-                        setState({
-                            ...widgetRef.current?.serialize(),
-                            ...props,
-                        });
-                    }}
-                    ref={widgetRef}
-                />
-            </View>
-        );
+    args: {
+        backgroundImage: {},
     },
 };
 
-export const WithinEditorPage = (): React.ReactElement => (
-    <div style={{width: PROD_EDITOR_WIDTH}}>
-        <EditorPageWithStorybookPreview
-            question={generateTestPerseusRenderer({
-                content: "[[☃ image 1]]",
-                widgets: {
-                    "image 1": generateImageWidget({
-                        options: generateImageOptions({
-                            backgroundImage: {
-                                url: "https://cdn.kastatic.org/ka-content-images/61831c1329dbc32036d7dd0d03e06e7e2c622718.jpg",
-                            },
-                        }),
-                    }),
-                },
-            })}
-        />
-    </div>
-);
-
-const styles = StyleSheet.create({
-    wrapper: {
-        // The maximum width of a widget in the editor.
-        width: PROD_EDITOR_WIDTH,
-        margin: 20,
+/**
+ * This Image widget editor does not have any options set.
+ */
+export const Empty: Story = {
+    name: "Empty (Within Editor Page)",
+    decorators: [withinEditorPageDecorator],
+    args: {
+        backgroundImage: {},
     },
-});
+};
+
+/**
+ * This Image widget editor has all options set.
+ */
+export const Populated: Story = {
+    name: "Populated (Within Editor Page)",
+    decorators: [withinEditorPageDecorator],
+    args: {
+        backgroundImage: {
+            url: "https://cdn.kastatic.org/ka-content-images/61831c1329dbc32036d7dd0d03e06e7e2c622718.jpg",
+        },
+        alt: "The moon showing behind the Earth in space.",
+        caption: "Captured via XYZ Telescope",
+        title: "The Moon",
+    },
+};
