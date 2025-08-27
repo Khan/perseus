8943735ea40002ef06f3c56cf90b8eee5125eeaf
Nisha Yerunkar <nisha@khanacademy.org>
2025-08-27 16:03:49 -0700

    [Image] | (CX) | Modernize the Image widget editor UI (#2842)
    ## Summary:
    Make the Image widget editor UI nicer to look at and use.
    
    - Replace existing inputs with Wonder Blocks components
    - Make the spacing look nicer
    
    Bonus:
    - Add more tests
    - Break out the image URL input into a new file
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-3386
    
    ## Screenshots
    
    Case | Before | After |
    | --- | --- | --- |
    | Empty | <img width="391" height="474" alt="Screenshot 2025-08-27 at 12 59 50 PM" src="https://github.com/user-attachments/assets/bf13d446-9af6-4ae7-afa5-6c23feef9950" /> | <img width="386" height="505" alt="Screenshot 2025-08-27 at 1 00 08 PM" src="https://github.com/user-attachments/assets/cf93a284-5dba-4ef3-a57e-ad2dbf748c02" /> |
    | Warning | <img width="396" height="525" alt="Screenshot 2025-08-27 at 1 00 30 PM" src="https://github.com/user-attachments/assets/0039bb9f-82ab-434d-9c7d-4044fbad9de5" /> | <img width="388" height="625" alt="Screenshot 2025-08-27 at 1 00 36 PM" src="https://github.com/user-attachments/assets/75b4d78a-3ec1-4c51-8286-9814caab6e0b" /> |
    | Populated | <img width="374" height="535" alt="Screenshot 2025-08-27 at 1 01 00 PM" src="https://github.com/user-attachments/assets/841fc55f-eafd-49e3-b33d-6fea0226b239" /> | <img width="358" height="544" alt="Screenshot 2025-08-27 at 1 01 07 PM" src="https://github.com/user-attachments/assets/3bb1bc2e-b826-4e7a-a07c-72187ba0b326" /> |
    
    
    ## Test plan:
    `pnpm jest packages/perseus-editor/src/widgets/__tests__/image-editor.test.tsx`
    
    Storybook
    `/?path=/docs/widgets-image-editor-demo--docs#populated-within-editor-page`
    
    Author: nishasy
    
    Reviewers: nishasy, ivyolamit, catandthemachines
    
    Required Reviewers:
    
    Approved By: ivyolamit
    
    Checks: ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2842

--- packages/perseus-editor/src/widgets/__tests__/image-editor.test.tsx
+++ packages/perseus-editor/src/widgets/__tests__/image-editor.test.tsx
@@ -10,6 +10,8 @@ import type {UserEvent} from "@testing-library/user-event";
 
 const realKhanImageUrl =
     "https://cdn.kastatic.org/ka-content-images/61831c1329dbc32036d7dd0d03e06e7e2c622718.jpg";
+const nonKhanImageWarning =
+    "Images must be from sites hosted by Khan Academy. Please input a Khan Academy-owned address, or use the Add Image tool to rehost an existing image";
 
 const apiOptions = ApiOptions.defaults;
 
@@ -35,6 +37,13 @@ describe("image editor", () => {
         // Assert
         const urlField = screen.getByRole("textbox", {name: "Image url:"});
         expect(urlField).toBeInTheDocument();
+
+        // None of the rest of the UI should be rendered.
+        expect(screen.queryByText(nonKhanImageWarning)).not.toBeInTheDocument();
+        expect(screen.queryByText("Preview:")).not.toBeInTheDocument();
+        expect(screen.queryByText("Dimensions:")).not.toBeInTheDocument();
+        expect(screen.queryByText("Alt text:")).not.toBeInTheDocument();
+        expect(screen.queryByText("Caption:")).not.toBeInTheDocument();
     });
 
     it("should render populated image editor", () => {
@@ -45,24 +54,27 @@ describe("image editor", () => {
             <ImageEditor
                 apiOptions={apiOptions}
                 backgroundImage={{url: realKhanImageUrl}}
-                alt="Earth and moon"
-                caption="Earth and moon"
+                alt="Earth and moon alt"
+                caption="Earth and moon caption"
                 onChange={() => {}}
             />,
         );
 
+        const dimensionsLabel = screen.getByText("Dimensions:");
         const urlField = screen.getByRole("textbox", {name: "Image url:"});
-        const altField = screen.getAllByRole("textbox")[1]; // Second textarea is alt
-        const captionField = screen.getAllByRole("textbox")[2]; // Third textarea is caption
+        const altField = screen.getByRole("textbox", {name: "Alt text:"});
+        const captionField = screen.getByRole("textbox", {name: "Caption:"});
 
         // Assert
+        expect(dimensionsLabel).toBeInTheDocument();
         expect(urlField).toBeInTheDocument();
         expect(altField).toBeInTheDocument();
         expect(captionField).toBeInTheDocument();
 
+        expect(screen.getByText("unknown")).toBeInTheDocument();
         expect(urlField).toHaveValue(realKhanImageUrl);
-        expect(altField).toHaveValue("Earth and moon");
-        expect(captionField).toHaveValue("Earth and moon");
+        expect(altField).toHaveValue("Earth and moon alt");
+        expect(captionField).toHaveValue("Earth and moon caption");
     });
 
     it("should render warning for non-Khan Academy image", async () => {
@@ -83,11 +95,39 @@ describe("image editor", () => {
         await userEvent.tab();
 
         // Assert
-        expect(
-            screen.getByText(
-                "Images must be from sites hosted by Khan Academy. Please input a Khan Academy-owned address, or use the Add Image tool to rehost an existing image",
-            ),
-        ).toBeInTheDocument();
+        expect(screen.getByText(nonKhanImageWarning)).toBeInTheDocument();
+    });
+
+    it("should show unknown dimensions if the image size is not known", () => {
+        // Arrange
+        render(
+            <ImageEditor
+                apiOptions={apiOptions}
+                backgroundImage={{url: realKhanImageUrl}}
+                onChange={() => {}}
+            />,
+        );
+
+        // Assert
+        expect(screen.getByText("unknown")).toBeInTheDocument();
+    });
+
+    it("should show the image dimensions if the image size is known", () => {
+        // Arrange
+        render(
+            <ImageEditor
+                apiOptions={apiOptions}
+                backgroundImage={{
+                    url: realKhanImageUrl,
+                    width: 400,
+                    height: 225,
+                }}
+                onChange={() => {}}
+            />,
+        );
+
+        // Assert
+        expect(screen.getByText("400 x 225")).toBeInTheDocument();
     });
 
     it("should call onChange with the new image url", async () => {
@@ -119,6 +159,53 @@ describe("image editor", () => {
         });
     });
 
+    it("should call onChange with empty image url", async () => {
+        // Arrange
+        const onChangeMock = jest.fn();
+        render(<ImageEditor apiOptions={apiOptions} onChange={onChangeMock} />);
+
+        // Act
+        const urlField = screen.getByRole("textbox", {name: "Image url:"});
+        urlField.focus();
+        await userEvent.clear(urlField);
+        await userEvent.tab();
+
+        // Assert
+        expect(onChangeMock).toHaveBeenCalledWith({
+            backgroundImage: {},
+            box: undefined,
+        });
+    });
+
+    it("should should clear the warning when the image url is cleared", async () => {
+        // Arrange
+        const onChangeMock = jest.fn();
+        render(
+            <ImageEditor
+                apiOptions={apiOptions}
+                onChange={onChangeMock}
+                backgroundImage={{url: "abc"}}
+            />,
+        );
+
+        // Act
+
+        const urlField = screen.getByRole("textbox", {name: "Image url:"});
+        urlField.focus();
+        await userEvent.paste("abc");
+        await userEvent.tab();
+
+        // Assert - make sure the warning is shown first
+        expect(screen.getByText(nonKhanImageWarning)).toBeInTheDocument();
+
+        // Act
+        await userEvent.clear(urlField);
+        await userEvent.tab();
+
+        // Assert - warning goes away when the image url is cleared
+        expect(screen.queryByText(nonKhanImageWarning)).not.toBeInTheDocument();
+    });
+
     it("should call onChange with new alt text", async () => {
         // Arrange
         const onChangeMock = jest.fn();
@@ -141,6 +228,28 @@ describe("image editor", () => {
         });
     });
 
+    it("should call onChange with undefined alt", async () => {
+        // Arrange
+        const onChangeMock = jest.fn();
+        render(
+            <ImageEditor
+                apiOptions={apiOptions}
+                backgroundImage={{url: realKhanImageUrl}}
+                alt="Earth and moon"
+                onChange={onChangeMock}
+            />,
+        );
+
+        // Act
+        const altField = screen.getByRole("textbox", {name: "Alt text:"});
+        await userEvent.clear(altField);
+
+        // Assert
+        expect(onChangeMock).toHaveBeenCalledWith({
+            alt: undefined,
+        });
+    });
+
     it("should call onChange with new caption", async () => {
         // Arrange
         const onChangeMock = jest.fn();
@@ -160,6 +269,28 @@ describe("image editor", () => {
         // Assert
         expect(onChangeMock).toHaveBeenCalledWith({
             caption: "Earth and moon",
+        });
+    });
+
+    it("should call onChange with undefined caption", async () => {
+        // Arrange
+        const onChangeMock = jest.fn();
+        render(
+            <ImageEditor
+                apiOptions={apiOptions}
+                backgroundImage={{url: realKhanImageUrl}}
+                caption="Earth and moon"
+                onChange={onChangeMock}
+            />,
+        );
+
+        // Act
+        const captionField = screen.getByRole("textbox", {name: "Caption:"});
+        await userEvent.clear(captionField);
+
+        // Assert
+        expect(onChangeMock).toHaveBeenCalledWith({
+            caption: undefined,
         });
     });
 });
