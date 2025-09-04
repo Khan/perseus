e3555d456b2508f5401ad2d926cb233611555616
Ben Christel <benchristel@khanacademy.org>
2025-09-04 11:08:19 -0700

    [benc/use-ts-data-files] Add comments to regression test data files

---
+++ packages/perseus-core/src/parse-perseus-json/regression-tests/user-input-data/number-line.ts
@@ -0,0 +1,10 @@
+// WARNING: Do not change or delete this file! If you do, Perseus might become
+// unable to parse the current user input format, which will break clients.
+// If you need to add more regression tests, add a new file to this directory.
+export default {
+    "number-line 1": {
+        numLinePosition: 2,
+        rel: "eq",
+        numDivisions: 3,
+    },
+};
---
+++ packages/perseus-core/src/parse-perseus-json/regression-tests/user-input-data/table.ts
@@ -0,0 +1,9 @@
+// WARNING: Do not change or delete this file! If you do, Perseus might become
+// unable to parse the current user input format, which will break clients.
+// If you need to add more regression tests, add a new file to this directory.
+export default {
+    "table 1": [
+        ["a", "b"],
+        ["c", "d"],
+    ],
+};
