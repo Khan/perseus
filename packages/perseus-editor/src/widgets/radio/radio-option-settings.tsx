da03a2c650b2c76994a2fd2a1844a3866af52e76
Anakaren Rojas <anakaren@khanacademy.org>
2025-09-09 15:50:57 -0700

    [update-radio-editor-to-normalize-ids] remove data test ids

--- packages/perseus-editor/src/widgets/radio/radio-option-settings.tsx
+++ packages/perseus-editor/src/widgets/radio/radio-option-settings.tsx
@@ -43,7 +43,7 @@ export function RadioOptionSettings({
     const rationaleTextAreaId = `${uniqueId}-rationale-textarea`;
 
     return (
-        <div className={styles.tile} data-choice-id={choice.id}>
+        <div className={styles.tile}>
             {/* Correct / Incorrect status selection */}
             <fieldset className="perseus-widget-row">
                 <RadioStatusPill
