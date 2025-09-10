86aad6791a75efac5dc4b4a9914e150a8108763e
Ivy Olamit <ivy@khanacademy.org>
2025-09-10 13:44:48 -0700

    [LEMS-3519/add-image-widget-upgrade-flag] [Image] Add image-widget-upgrade feature flag

--- packages/perseus-core/src/feature-flags.ts
+++ packages/perseus-core/src/feature-flags.ts
@@ -8,6 +8,9 @@
  * 3. When no active feature flag is available value will be:
  *    export const PerseusFeatureFlags = [];
  */
-const PerseusFeatureFlags = ["new-radio-widget"] as const;
+const PerseusFeatureFlags = [
+    "new-radio-widget", // TODO(LEMS-2994): cleanup feature flag
+    "image-widget-upgrade", // TODO(LEMS-3521): cleanup feature flag
+] as const;
 
 export default PerseusFeatureFlags;
