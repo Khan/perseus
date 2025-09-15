ea34d98074914fff2d99f776abe9447346090ee6
Tamara <tamara@khanacademy.org>
2025-09-15 13:29:51 -0500

    [tb/LEMS-3492/sync-definition] Simplify interactive regression tests

--- packages/perseus/src/widgets/definition/__docs__/definition-interaction-regression.stories.tsx
+++ packages/perseus/src/widgets/definition/__docs__/definition-interaction-regression.stories.tsx
@@ -1,26 +1,12 @@
 import {generateTestPerseusItem} from "@khanacademy/perseus-core";
-import * as React from "react";
 
+import {themeModes} from "../../../../../../.storybook/modes";
 import {ServerItemRendererWithDebugUI} from "../../../../../../testing/server-item-renderer-with-debug-ui";
 import {question} from "../definition.testdata";
 
-import type {PerseusItem} from "@khanacademy/perseus-core";
-import type {Meta} from "@storybook/react-vite";
-import {themeModes} from "../../../../../../.storybook/modes";
+import type {Meta, StoryObj} from "@storybook/react-vite";
 
-type StoryArgs = {
-    // Story Option
-    item: PerseusItem;
-    // Definition Options
-    static: boolean;
-    // Testing Options
-    startAnswerless: boolean;
-} & Pick<
-    React.ComponentProps<typeof ServerItemRendererWithDebugUI>,
-    "reviewMode" | "showSolutions"
->;
-
-export default {
+const meta: Meta = {
     title: "Widgets/Definition/Visual Regression Tests/Interactive",
     component: ServerItemRendererWithDebugUI,
     tags: ["!autodocs"],
@@ -33,51 +19,18 @@ export default {
         },
         chromatic: {disableSnapshot: false, modes: themeModes},
     },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ServerItemRendererWithDebugUI>;
+
+export const FocusedState = {
     args: {
-        static: false,
-        startAnswerless: false,
-        reviewMode: false,
-        showSolutions: "none",
         item: generateTestPerseusItem({
             question,
         }),
-    } satisfies StoryArgs,
-    argTypes: {
-        showSolutions: {
-            options: ["none", "all", "selected"],
-            control: {
-                type: "select",
-            },
-        },
     },
-    render: (args: StoryArgs) => (
-        <ServerItemRendererWithDebugUI
-            item={applyStoryArgs(args)}
-            reviewMode={args.reviewMode}
-            showSolutions={args.showSolutions}
-            startAnswerless={args.startAnswerless}
-        />
-    ),
-} satisfies Meta<StoryArgs>;
-
-const applyStoryArgs = (args: StoryArgs): PerseusItem => {
-    const storyItem = {
-        ...args.item,
-        question: {
-            ...args.item.question,
-            widgets: {},
-        },
-    };
-    for (const [widgetId, widget] of Object.entries(
-        args.item.question.widgets,
-    )) {
-        storyItem.question.widgets[widgetId] = {...widget, static: args.static};
-    }
-
-    return storyItem;
-};
-
-export const FocusedState = {
     play: async ({canvas}) => {
         // eslint-disable-next-line testing-library/prefer-screen-queries
         const definitionTrigger = canvas.getByRole("button", {
@@ -88,6 +41,11 @@ export const FocusedState = {
 };
 
 export const ClickedState = {
+    args: {
+        item: generateTestPerseusItem({
+            question,
+        }),
+    },
     play: async ({canvas, userEvent}) => {
         // eslint-disable-next-line testing-library/prefer-screen-queries
         const definitionTrigger = canvas.getByRole("button", {
