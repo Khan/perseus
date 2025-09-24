2a176446f53b50a554065f112dff2d7448ba7e6e
Tamara <tamara@khanacademy.org>
2025-09-24 10:44:28 -0500

    [tb/LEMS-3491/sync-explanation] Update token size and lint fix

--- packages/perseus/src/widgets/explanation/explanation.tsx
+++ packages/perseus/src/widgets/explanation/explanation.tsx
@@ -1,6 +1,7 @@
 import {linterContextDefault} from "@khanacademy/perseus-linter";
 import Button from "@khanacademy/wonder-blocks-button";
 import {Id, View} from "@khanacademy/wonder-blocks-core";
+import {font} from "@khanacademy/wonder-blocks-tokens";
 import caretDown from "@phosphor-icons/core/regular/caret-down.svg";
 import caretUp from "@phosphor-icons/core/regular/caret-up.svg";
 import {StyleSheet} from "aphrodite";
@@ -13,7 +14,6 @@ import {getPromptJSON as _getPromptJSON} from "../../widget-ai-utils/explanation
 import type {Widget, WidgetExports, WidgetProps} from "../../types";
 import type {ExplanationPromptJSON} from "../../widget-ai-utils/explanation/explanation-ai-utils";
 import type {PerseusExplanationWidgetOptions} from "@khanacademy/perseus-core";
-import {font} from "@khanacademy/wonder-blocks-tokens";
 
 type Props = WidgetProps<PerseusExplanationWidgetOptions>;
 
@@ -98,7 +98,7 @@ class Explanation extends React.Component<Props, State> implements Widget {
         };
 
         const labelStyle = {
-            fontSize: font.body.size.medium,
+            fontSize: font.heading.size.medium,
             lineHeight: "inherit",
             "text-align": "left",
             // The following property adjusts the large space between the button text and the caret icon.
