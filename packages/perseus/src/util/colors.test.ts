950bd83bee03d7989364fc18386b13cdcf12a811
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2025-09-23 17:42:23 -0700

    [bugfix/LEMS-3545-fade-area-shows-white-when-grouped-in-articles] Add more unit tests.

--- packages/perseus/src/util/colors.test.ts
+++ packages/perseus/src/util/colors.test.ts
@@ -35,5 +35,30 @@ describe("Color Utilities", () => {
                 "--wb-semanticColor-core-background-base-default",
             );
         });
+
+        it("returns a semantic default background color if the element is transparent and its parent is the HTML tag", () => {
+            testElement.style.backgroundColor = "rgba(0, 0, 0, 0)"; // transparent
+            const htmlElement = document.createElement("html");
+            htmlElement.appendChild(testElement);
+            let result = getBackgroundColor(testElement);
+            expect(result).toBe(
+                "--wb-semanticColor-core-background-base-default",
+            );
+
+            testElement.style.backgroundColor = "transparent";
+            result = getBackgroundColor(testElement);
+            expect(result).toBe(
+                "--wb-semanticColor-core-background-base-default",
+            );
+        });
+
+        it("returns the background color of the parent if the element is transparent", () => {
+            testElement.style.backgroundColor = "rgba(0, 0, 0, 0)"; // transparent
+            const parentElement = document.createElement("div");
+            parentElement.style.backgroundColor = "green";
+            parentElement.appendChild(testElement);
+            const result = getBackgroundColor(testElement);
+            expect(result).toBe("green");
+        });
     });
 });
