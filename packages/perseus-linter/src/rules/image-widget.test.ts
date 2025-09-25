a18bbab804d8e273a0474993811813ce90c3bf3c
Nisha Yerunkar <nisha@khanacademy.org>
2025-09-25 16:34:32 -0700

    [image-remove-caption-math-linter] WIP - do not review [Image] | (CX) | Remove "math in caption" linter error
    todo
    
    Issue: none
    
    Test plan:
    todo

--- packages/perseus-linter/src/rules/image-widget.test.ts
+++ packages/perseus-linter/src/rules/image-widget.test.ts
@@ -33,40 +33,4 @@ describe("image-widget", () => {
             },
         },
     });
-
-    // Warn for image widget with math in its caption
-    expectWarning(imageWidgetRule, "[[☃ image 1]]", {
-        widgets: {
-            "image 1": {
-                options: {
-                    alt: "1234567890",
-                    caption: "Test: $x$",
-                },
-            },
-        },
-    });
-
-    // Pass for image widget with caption and no math
-    expectPass(imageWidgetRule, "[[☃ image 1]]", {
-        widgets: {
-            "image 1": {
-                options: {
-                    alt: "1234567890",
-                    caption: "Test: x",
-                },
-            },
-        },
-    });
-
-    // Pass for image widget with escaped dollar in its caption
-    expectPass(imageWidgetRule, "[[☃ image 1]]", {
-        widgets: {
-            "image 1": {
-                options: {
-                    alt: "1234567890",
-                    caption: "Test: \\$10",
-                },
-            },
-        },
-    });
 });
