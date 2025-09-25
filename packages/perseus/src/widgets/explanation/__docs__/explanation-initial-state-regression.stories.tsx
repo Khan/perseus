7cff511b4682dcd5153b28f5eb420bebbed24876
Tamara <tamara@khanacademy.org>
2025-09-25 15:59:11 -0500

    [tb/LEMS-3491/sync-explanation] Add wide button story

--- packages/perseus/src/widgets/explanation/__docs__/explanation-initial-state-regression.stories.tsx
+++ packages/perseus/src/widgets/explanation/__docs__/explanation-initial-state-regression.stories.tsx
@@ -2,7 +2,7 @@ import {generateTestPerseusItem} from "@khanacademy/perseus-core";
 
 import {themeModes} from "../../../../../../.storybook/modes";
 import {ServerItemRendererWithDebugUI} from "../../../../../../testing/server-item-renderer-with-debug-ui";
-import {question1} from "../explanation.testdata";
+import {question1, wideButton} from "../explanation.testdata";
 
 import type {Meta, StoryObj} from "@storybook/react-vite";
 
@@ -33,5 +33,11 @@ type Story = StoryObj<typeof ServerItemRendererWithDebugUI>;
 export const Question1: Story = {
     args: {
         item: generateTestPerseusItem({question: question1}),
+    },
+};
+
+export const WideButton: Story = {
+    args: {
+        item: generateTestPerseusItem({question: wideButton}),
     },
 };
