96341cf42fa253055edbf22cf28b5133e024140c
Tamara <60857422+Myranae@users.noreply.github.com>
2025-09-26 13:30:20 -0500

    [Color - NAW] Refactor Image widget to use semantic fonts and colors (#2911)
    ## Summary:
    To allow theming with Perseus, we are updating a number of key widgets to use WonderBlocks semantic colors and font styling. This work updates the Image widget with semantic color tokens and semantic font tokens.
    
    **Prod**
     <table><tr><td>WonderBlocks</td><td>Shape Your Learning / Thunder Blocks</td></tr><tr><td><img width="451" height="387" alt="image" src="https://github.com/user-attachments/assets/46319e2b-bf8f-4bf1-9f91-df1ff13913ca" /></td><td><img width="435" height="387" alt="image" src="https://github.com/user-attachments/assets/31067e65-8599-4064-9fb2-6100ecb0ec1b" /></td></tr></table>
    
    **From PR**
     <table><tr><td>WonderBlocks</td><td>Shape Your Learning / Thunder Blocks</td></tr><tr><td><img width="443" height="393" alt="image" src="https://github.com/user-attachments/assets/1dfdca97-c418-4509-a947-abcaeb12fb8d" /></td><td><img width="433" height="387" alt="image" src="https://github.com/user-attachments/assets/5a98e7d9-1eda-4bd2-bf32-c04b3d169fd7" /></td></tr></table>
    
    Issue: LEMS-3490
    
    ## Test plan:
    - Manually check changes using Storybook theme button
    - Add Chromatic visual regression tests and confirm update is as intended
    - Confirm all checks pass
    - Will be QE tested
    
    Author: Myranae
    
    Reviewers: Myranae, ivyolamit, mark-fitzgerald, beaesguerra, nishasy, catandthemachines
    
    Required Reviewers:
    
    Approved By: mark-fitzgerald, beaesguerra
    
    Checks: ✅ 17 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/2911

---
+++ packages/perseus/src/widgets/image/__docs__/image-interactions-regression.stories.tsx
@@ -0,0 +1,165 @@
+import {
+    generateImageOptions,
+    generateImageWidget,
+    generateTestPerseusRenderer,
+} from "@khanacademy/perseus-core";
+import * as React from "react";
+
+import {themeModes} from "../../../../../../.storybook/modes";
+import {getFeatureFlags} from "../../../../../../testing/feature-flags-util";
+import {ServerItemRendererWithDebugUI} from "../../../../../../testing/server-item-renderer-with-debug-ui";
+import {ApiOptions} from "../../../perseus-api";
+import Renderer from "../../../renderer";
+import {mockStrings} from "../../../strings";
+import UserInputManager from "../../../user-input-manager";
+import {earthMoonImage} from "../utils";
+
+import type {PerseusRenderer} from "@khanacademy/perseus-core";
+import type {Meta} from "@storybook/react-vite";
+
+const earthMoonImageCaption =
+    "The Moon above Earth's horizon, captured by the International Space Station, [NASA](https://images.nasa.gov/details/iss071e515452)";
+
+// Breaking this out instead of using it globally, so that the
+// right-to-left story can wrap the ImageQuestionRenderer with the
+// right-to-left wrapper.
+const rendererDecorator = (_, {args, parameters}) => {
+    return (
+        <ImageQuestionRenderer
+            question={generateTestPerseusRenderer({
+                content: parameters?.content ?? "[[☃ image 1]]",
+                widgets: {
+                    "image 1": generateImageWidget({
+                        options: generateImageOptions({
+                            ...args,
+                        }),
+                    }),
+                },
+            })}
+        />
+    );
+};
+
+const meta: Meta = {
+    title: "Widgets/Image/Visual Regression Tests/Interactions",
+    component: ServerItemRendererWithDebugUI,
+    tags: ["!autodocs"],
+    parameters: {
+        docs: {
+            description: {
+                component:
+                    "Regression tests for the Image widget that DO need some sort of interaction to test, which will be used with Chromatic. Stories are displayed on their own page.",
+            },
+        },
+        chromatic: {disableSnapshot: false, modes: themeModes},
+    },
+};
+
+export default meta;
+
+export const ImageFocusedState = {
+    decorators: [rendererDecorator],
+    args: {
+        backgroundImage: earthMoonImage,
+    },
+    play: async ({userEvent}) => {
+        await userEvent.tab();
+    },
+};
+
+export const LongDescriptionNoCaptionFocusedState = {
+    decorators: [rendererDecorator],
+    args: {
+        backgroundImage: earthMoonImage,
+        longDescription:
+            "This is a *very* long description of the earth and moon.",
+    },
+    play: async ({canvas}) => {
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        const imageTrigger = canvas.getByRole("button", {
+            name: "Explore image",
+        });
+        imageTrigger.focus();
+    },
+};
+
+export const LongDescriptionFocusedState = {
+    decorators: [rendererDecorator],
+    args: {
+        backgroundImage: earthMoonImage,
+        alt: "Earth and Moon",
+        longDescription:
+            "This is a *very* long description of the earth and moon.",
+        title: "Earth and Moon",
+        caption: earthMoonImageCaption,
+    },
+    play: async ({canvas}) => {
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        const imageTrigger = canvas.getByRole("button", {
+            name: "Explore image",
+        });
+        imageTrigger.focus();
+    },
+};
+
+export const LongDescriptionClickedState = {
+    decorators: [rendererDecorator],
+    args: {
+        backgroundImage: earthMoonImage,
+        alt: "Earth and Moon",
+        longDescription:
+            "This is a *very* long description of the earth and moon.",
+        title: "Earth and Moon",
+        caption: earthMoonImageCaption,
+    },
+    play: async ({canvas, userEvent}) => {
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        const imageTrigger = canvas.getByRole("button", {
+            name: "Explore image",
+        });
+        await userEvent.click(imageTrigger);
+    },
+};
+
+export const ImageFocusedStateAfterLongDescriptionClick = {
+    decorators: [rendererDecorator],
+    args: {
+        backgroundImage: earthMoonImage,
+        alt: "Earth and Moon",
+        longDescription:
+            "This is a *very* long description of the earth and moon.",
+        title: "Earth and Moon",
+        caption: earthMoonImageCaption,
+    },
+    play: async ({canvas, userEvent}) => {
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        const descriptionTrigger = canvas.getByRole("button", {
+            name: "Explore image",
+        });
+        await userEvent.click(descriptionTrigger);
+        await userEvent.tab();
+    },
+};
+
+function ImageQuestionRenderer(props: {question: PerseusRenderer}) {
+    const {question} = props;
+    return (
+        <UserInputManager widgets={question.widgets} problemNum={0}>
+            {({userInput, handleUserInput, initializeUserInput}) => (
+                <Renderer
+                    userInput={userInput}
+                    handleUserInput={handleUserInput}
+                    initializeUserInput={initializeUserInput}
+                    strings={mockStrings}
+                    content={question.content}
+                    widgets={question.widgets}
+                    images={question.images}
+                    apiOptions={{
+                        ...ApiOptions.defaults,
+                        flags: getFeatureFlags({"image-widget-upgrade": true}),
+                    }}
+                />
+            )}
+        </UserInputManager>
+    );
+}
