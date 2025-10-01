9dc497d8e02f39ff908e489a3294a98f3712650e
Ben Christel <benchristel@khanacademy.org>
2025-10-01 11:11:15 -0700

    [benc/a11y-metrics] Remove unused schemas

--- packages/perseus-a11y-metrics/src/parse-snapshot.ts
+++ packages/perseus-a11y-metrics/src/parse-snapshot.ts
@@ -1,17 +1,5 @@
 import {array, number, object, string, type Infer} from "zod";
 
-const ItemSchema = object({
-    // NOTE: there are more fields in the item object. This schema just
-    // lists the fields we currently care about.
-    id: string(),
-});
-
-const ProblemTypeSchema = object({
-    // NOTE: there are more fields in the problem type object. This schema just
-    // lists the fields we currently care about.
-    items: array(ItemSchema),
-});
-
 const ExerciseSchema = object({
     // NOTE: there are more fields in the exercise object. This schema just
     // lists the fields we currently care about.
