b91f872cb82d15ff4e996422aaf87dc4607dc5a0
Ben Christel <benchristel@khanacademy.org>
2025-10-01 10:49:14 -0700

    [benc/a11y-metrics] Remove unused fields from schemas

--- packages/perseus-a11y-metrics/src/parse-snapshot.ts
+++ packages/perseus-a11y-metrics/src/parse-snapshot.ts
@@ -1,53 +1,27 @@
-import {array, boolean, number, object, string, unknown, type Infer} from "zod";
+import {array, number, object, string, type Infer} from "zod";
 
 const ItemSchema = object({
-    // NOTE: there are more fields in the ItemSchema object. This schema just
+    // NOTE: there are more fields in the item object. This schema just
     // lists the fields we currently care about.
     id: string(),
 });
 
 const ProblemTypeSchema = object({
-    // NOTE: there are more fields in the ProblemTypeSchema object. This schema just
+    // NOTE: there are more fields in the problem type object. This schema just
     // lists the fields we currently care about.
     items: array(ItemSchema),
 });
 
 const ExerciseSchema = object({
-    // NOTE: there are more fields in the ExerciseSchema object. This schema just
+    // NOTE: there are more fields in the exercise object. This schema just
     // lists the fields we currently care about.
-    // TODO: what is the difference between contentId and id?
-    contentId: string(),
     exerciseLength: number(),
     id: string(),
-    listed: boolean(),
-    problemTypes: array(ProblemTypeSchema),
     translatedPerseusContentSha: string(),
 });
 
 const SnapshotSchema = object({
     exercises: array(ExerciseSchema),
-    // TODO
-    articles: unknown(),
-    // TODO
-    quizzes: unknown(),
-    // TODO
-    unitTests: unknown(),
-
-    // We don't care about the fields below for now
-    aiGuideActivities: unknown(),
-    challenges: unknown(),
-    courseMenus: unknown(),
-    courses: unknown(),
-    curationPages: unknown(),
-    curriculums: unknown(),
-    domains: unknown(),
-    interactives: unknown(),
-    lessons: unknown(),
-    projects: unknown(),
-    talkthroughs: unknown(),
-    units: unknown(),
-    version: unknown(),
-    video: unknown(),
 });
 
 export type Exercise = Infer<typeof ExerciseSchema>;
