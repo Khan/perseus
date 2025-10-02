3d8d18a5ab5af7ff95bbb413c3959795196e780d
Nisha Yerunkar <nisha@khanacademy.org>
2025-10-01 17:15:48 -0700

    [image-resize-zoom] stories

--- packages/perseus/src/widgets/image/__docs__/image-demo.stories.tsx
+++ packages/perseus/src/widgets/image/__docs__/image-demo.stories.tsx
@@ -1,11 +1,21 @@
+import {
+    generateImageOptions,
+    generateImageWidget,
+    generateTestPerseusRenderer,
+} from "@khanacademy/perseus-core";
+import * as React from "react";
+
 import {getWidget} from "../../../widgets";
-import {imageRendererDecorator} from "../../__testutils__/image-renderer-decorator";
+import {
+    ImageQuestionRenderer,
+    imageRendererDecorator,
+} from "../../__testutils__/image-renderer-decorator";
 import {
     mobileDecorator,
     articleDecorator,
     mobileArticleDecorator,
 } from "../../__testutils__/story-decorators";
-import {earthMoonImage, monasteryImage} from "../utils";
+import {earthMoonImage, frescoImage, monasteryImage} from "../utils";
 
 import type {Meta, StoryObj} from "@storybook/react-vite";
 
@@ -112,5 +122,53 @@ export const DecorativeImageMobile: Story = {
         longDescription:
             "This is a *very* long description of the earth and moon.",
         decorative: true,
+    },
+};
+
+export const ImageWithDifferentSizes: Story = {
+    render: function Render() {
+        return (
+            // Limit width so zoom becomes possible.
+            <div style={{width: 600}}>
+                <ImageQuestionRenderer
+                    question={generateTestPerseusRenderer({
+                        content:
+                            "[[☃ image 1]]\n\n[[☃ image 2]]\n\n[[☃ image 3]]",
+                        widgets: {
+                            "image 1": generateImageWidget({
+                                options: generateImageOptions({
+                                    backgroundImage: {
+                                        url: frescoImage.url,
+                                        width: 800,
+                                        height: 450,
+                                    },
+                                    alt: "Fresco painting",
+                                }),
+                            }),
+                            "image 2": generateImageWidget({
+                                options: generateImageOptions({
+                                    backgroundImage: {
+                                        url: frescoImage.url,
+                                        width: 400,
+                                        height: 225,
+                                    },
+                                    alt: "Fresco painting",
+                                }),
+                            }),
+                            "image 3": generateImageWidget({
+                                options: generateImageOptions({
+                                    backgroundImage: {
+                                        url: frescoImage.url,
+                                        width: 200,
+                                        height: 112,
+                                    },
+                                    alt: "Fresco painting",
+                                }),
+                            }),
+                        },
+                    })}
+                />
+            </div>
+        );
     },
 };
