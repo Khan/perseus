733d1b75958c38dd373b89bbd3e3d4840f908f16
Ben Christel <benchristel@khanacademy.org>
2025-10-03 14:34:48 -0700

    [benc/json-stream] WIP

---
+++ packages/perseus-a11y-metrics/src/json-stream-parser.test.ts
@@ -0,0 +1,134 @@
+import {jsonStreamParser, number, Tokenizer} from "./json-stream-parser";
+
+xdescribe("a JsonStreamParser", () => {
+    it("allows callers to subscribe to a particular path pattern", async () => {
+        const parser = jsonStreamParser()
+        const objectStream = parser.subscribeTo(["foo"])
+
+        parser.push(`{"foo": 42}`);
+
+        expect(await objectStream.next()).toBe(42);
+    })
+
+    it("buffers multiple values to send to subscribers", async () => {
+        const parser = jsonStreamParser()
+        const objectStream = parser.subscribeTo(["foo"])
+
+        parser.push(`{"foo": 42}`);
+        parser.push(`{"foo": 37}`);
+
+        // Consume the 42
+        await objectStream.next()
+
+        expect(await objectStream.next()).toBe(37);
+    })
+
+    it("allows requesting values before they are pushed", async () => {
+        const parser = jsonStreamParser()
+        const objectStream = parser.subscribeTo(["foo"])
+
+        const promise = objectStream.next()
+
+        parser.push(`{"foo": 42}`);
+
+        expect(await promise).toBe(42);
+    })
+
+    it("allows requesting multiple values before they are pushed", async () => {
+        const parser = jsonStreamParser()
+        const objectStream = parser.subscribeTo(["foo"])
+
+        const promise1 = objectStream.next()
+        const promise2 = objectStream.next()
+
+        parser.push(`{"foo": 1}`);
+        parser.push(`{"foo": 2}`);
+
+        expect(await promise1).toBe(1);
+        expect(await promise2).toBe(2);
+    })
+
+    it("allows interleaving calls to next and push", async () => {
+        const parser = jsonStreamParser()
+        const objectStream = parser.subscribeTo(["foo"])
+
+        const promise1 = objectStream.next()
+        const promise2 = objectStream.next()
+
+        parser.push(`{"foo": 1}`);
+
+        const promise3 = objectStream.next()
+
+        parser.push(`{"foo": 2}`);
+        parser.push(`{"foo": 3}`);
+        parser.push(`{"foo": 4}`);
+
+        const promise4 = objectStream.next()
+        const promise5 = objectStream.next()
+
+        parser.push(`{"foo": 5}`);
+
+        expect(await promise1).toBe(1);
+        expect(await promise2).toBe(2);
+        expect(await promise3).toBe(3);
+        expect(await promise4).toBe(4);
+        expect(await promise5).toBe(5);
+    })
+})
+
+xdescribe("a JSON Tokenizer", () => {
+    it("tokenizes a string", () => {
+        const tokenizer = new Tokenizer()
+        const tokens = tokenizer.push(`"hello"`)
+        expect(tokens).toEqual([{type: "primitive", value: "hello"}])
+    })
+
+    it("ignores whitespace", () => {
+        const tokenizer = new Tokenizer()
+        const tokens = tokenizer.push(" \n\t")
+        expect(tokens).toEqual([])
+    })
+})
+
+describe("number", () => {
+    it("accepts valid numbers", () => {
+        const valid = [
+            "0",
+            "-0",
+            "0.0",
+            "1.0",
+            "90123456789.123456789",
+            "1e9",
+            "1e-9",
+            "1e+9",
+            "0e0",
+            "0e0123456789",
+            "1.2E1",
+        ];
+
+        for (const value of valid) {
+            expect(value.match(number)?.[0]).toEqual(value);
+            expect(() => JSON.parse(value)).not.toThrowError();
+        }
+    })
+
+    it("rejects invalid numbers", () => {
+        const invalid = [
+            "01",
+            "-01",
+            "1.",
+            "1..0",
+            "1.0.0",
+            "1-2",
+            ".1",
+            "e9",
+            "1e++9",
+            "1e--9",
+        ]
+
+        for (const value of invalid) {
+            expect(value.match(number)?.[0]).not.toEqual(value);
+            expect(() => JSON.parse(value)).toThrowError();
+        }
+    })
+})
---
+++ packages/perseus-a11y-metrics/src/json-stream-parser.ts
@@ -0,0 +1,117 @@
+export function jsonStreamParser(): JsonStreamParser {
+    return new JsonStreamParser();
+}
+
+export class JsonStreamParser {
+    subscriptions: Subscription[] = []
+    tokenizer = new Tokenizer();
+    parserState = new ParserState();
+
+    subscribeTo(path: string[]): ObjectStream {
+        const stream = new ObjectStream()
+        this.streams.push(stream)
+        return stream;
+    }
+
+    push(data: string) {
+        const tokens = this.tokenizer.push(data)
+
+        for (const token of tokens) {
+            const completedObject = this.parserState.push(token)
+            if (completedObject) {
+                for (const sub of this.subscriptions) {
+                    if (sub.matches(completedObject.path)) {
+                        sub.push(completedObject.value);
+                    }
+                    // sub.push(JSON.parse(data).foo) // FIXME stop referencing foo
+                }
+            }
+        }
+
+        for (const stream of this.streams) {
+            stream.push(JSON.parse(data).foo) // FIXME stop referencing foo
+        }
+    }
+}
+
+export class Subscription {
+    private stream = new ObjectStream();
+
+    matches(path: string[]) {
+        return true // FIXME
+    }
+
+    async next(): Promise<unknown> {
+        return this.stream.next()
+    }
+
+    push(object: unknown): void {
+        this.stream.push(object);
+    }
+}
+
+export class ObjectStream {
+    /**
+     * Stores a queue of the values that have been `push`ed but not yet
+     * retrieved via `next`.
+     */
+    private valueBuffer: unknown[] = []
+
+    /**
+     * Stores a queue of functions that are waiting to receive a value.
+     * Functions are queued here when `next` is called, but we don't have a
+     * value to return yet.
+     */
+    private promiseResolveBuffer: Array<(value: unknown) => void> = [];
+
+    async next(): Promise<unknown> {
+        if (this.valueBuffer.length > 0) {
+            return this.valueBuffer.shift()
+        }
+
+        return await new Promise((resolve) => {
+            this.promiseResolveBuffer.push(resolve);
+        })
+    }
+
+    push(object: unknown) {
+        if (this.promiseResolveBuffer.length > 0) {
+            this.promiseResolveBuffer.shift()?.(object)
+        } else {
+            this.valueBuffer.push(object)
+        }
+    }
+}
+
+type Token =
+    | {type: "primitive", value: string | number | boolean | null}
+    | {type: "{"}
+    | {type: "}"}
+    | {type: "["}
+    | {type: "]"}
+    | {type: ","}
+    | {type: ":"}
+
+type TreeNode = {pathSegment: string, type: ""}
+
+export class ParserState {
+    private path: string[] = [];
+    // private nodeStack: Array<"object" | "array">
+
+    push(token: Token) {
+
+    }
+}
+
+/**
+ * Regexes based on: https://www.json.org/json-en.html
+ */
+const punctuation = /^[,:{}\[\]]/
+const space = /^[\u0020\u000a\u000d\u0009]/
+export const number = /-?(0|[1-9][0-9]*)(\.[0-9]+)?([eE][-+]?[0-9]+)?/
+
+export class Tokenizer {
+    push(data: string): Token[] {
+
+    }
+}
