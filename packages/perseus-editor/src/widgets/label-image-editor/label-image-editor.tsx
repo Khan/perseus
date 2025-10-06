6961f301eb43587947dc95fd0f2269d54d7ce433
Tamara <tamara@khanacademy.org>
2025-10-06 16:18:54 -0500

    [tb/fix-label-image-popover] Move label image editor to its folder

--- packages/perseus-editor/src/widgets/label-image-editor/label-image-editor.tsx
+++ packages/perseus-editor/src/widgets/label-image-editor/label-image-editor.tsx
@@ -3,14 +3,14 @@ import {labelImageLogic} from "@khanacademy/perseus-core";
 import {StyleSheet, css} from "aphrodite";
 import * as React from "react";
 
-import FormWrappedTextField from "../components/form-wrapped-text-field";
+import FormWrappedTextField from "../../components/form-wrapped-text-field";
 
-import AnswerChoices from "./label-image-editor/answer-choices";
-import Behavior from "./label-image-editor/behavior";
-import QuestionMarkers from "./label-image-editor/question-markers";
-import SelectImage from "./label-image-editor/select-image";
+import AnswerChoices from "./answer-choices";
+import Behavior from "./behavior";
+import QuestionMarkers from "./question-markers";
+import SelectImage from "./select-image";
 
-import type {PreferredPopoverDirection} from "./label-image-editor/behavior";
+import type {PreferredPopoverDirection} from "./behavior";
 import type {
     PerseusLabelImageWidgetOptions,
     LabelImageDefaultWidgetOptions,
