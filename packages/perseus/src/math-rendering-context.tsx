4c48bd106d03f6385aa6af700623b83640b0f137
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2025-10-14 15:00:46 -0700

    [LEMS-3571-make-choice-indicator-read-math-for-screen-readers] Add a rendering context for math markdown that can be used to force rendering options, such as when to use aria labels. Use context provider in Radio widget when rendering choice content so that indicators have an aria-label that they can use when announcing content.

---
+++ packages/perseus/src/math-rendering-context.tsx
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview A React context for managing math rendering options.
+ * @param shouldAddAriaLabels - Aria labels are only added when the math is
+ * contained within an element with role="presentation". This option forces the
+ * addition of aria labels for times when the element is not within such
+ * containers.
+ */
+import * as React from "react";
+
+export type MathRenderingContextType = {
+    shouldAddAriaLabels: boolean;
+};
+
+export const MathRenderingContext: React.Context<MathRenderingContextType> =
+    React.createContext<{
+        shouldAddAriaLabels: boolean;
+    }>({
+        shouldAddAriaLabels: false,
+    });
