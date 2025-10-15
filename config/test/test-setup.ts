3b812b35bcc1172daca8af53246c704b4be76d97
Jeremy Wiebe <jeremy@khanacademy.org>
2025-10-15 16:34:15 -0700

    [jer/dev-updates] Remove the hoop-jumping to control URL - not needed

--- config/test/test-setup.ts
+++ config/test/test-setup.ts
@@ -31,25 +31,6 @@ if (typeof window !== "undefined") {
     // See https://github.com/testing-library/dom-testing-library/issues/477
     // Release notes: https://github.com/testing-library/dom-testing-library/releases/tag/v7.0.0
     window.MutationObserver = MutationObserver;
-
-    // Override the window.location implementation to mock out assign()
-    // We need to access window.location.assign to verify that we're
-    // redirecting to the right place.
-    const oldLocation = window.location;
-    // @ts-expect-error - TS2790 - The operand of a 'delete' operator must be optional.
-    delete window.location;
-    const mockedLocation = new URL("http://localhost:8081");
-    window.location = {
-        ...oldLocation,
-        host: mockedLocation.host,
-        hostname: mockedLocation.hostname,
-        href: mockedLocation.href,
-        origin: mockedLocation.origin,
-        port: mockedLocation.port,
-        protocol: mockedLocation.protocol,
-        assign: jest.fn(),
-    };
-    /* eslint-enable no-restricted-syntax */
 }
 
 require("./attach-jsdom-window-shims")(globalThis);
