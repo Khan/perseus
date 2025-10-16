767b0a74f2b56076e3eaafc038daef410e8a7339
Ivy Olamit <ivy@khanacademy.org>
2025-10-15 17:08:27 -0700

    [LEMS-3537/image-widget-alignment] add tests

--- packages/perseus/src/widgets/image/image.test.ts
+++ packages/perseus/src/widgets/image/image.test.ts
@@ -665,4 +665,33 @@ describe.each([[true], [false]])("image widget - isMobile(%j)", (isMobile) => {
             expect(screen.getByAltText("widget alt text")).toBeVisible();
         });
     });
+
+    it("should render image with float-right alignment", () => {
+        // Arrange
+        const imageQuestion = generateTestPerseusRenderer({
+            content: "[[☃ image 1]]",
+            widgets: {
+                "image 1": generateImageWidget({
+                    alignment: "float-right",
+                    options: generateImageOptions({
+                        backgroundImage: earthMoonImage,
+                        alt: "widget alt",
+                        title: "widget title",
+                        caption: "widget caption",
+                    }),
+                }),
+            },
+        });
+
+        // Act, Assert
+        const {container} = renderQuestion(imageQuestion, apiOptions);
+        act(() => {
+            jest.runAllTimers();
+        });
+
+        // Assert
+        const figure = screen.getByRole("figure");
+        expect(figure).toBeVisible();
+        expect(container).toMatchSnapshot("widget-float-right");
+    });
 });
