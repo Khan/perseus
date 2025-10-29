bee3ab51e3ef62249195f260419a0d52ed68fa14
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2025-10-28 17:39:57 -0700

    [bugfix/LEMS-2683-locked-functions-not-rendering-with-null-domains] Add logic to the clampedDomain function to handle null values. These values can be null due to serialization via JSON (see CP-8932 for details).

--- packages/perseus/src/widgets/interactive-graphs/locked-figures/utils.ts
+++ packages/perseus/src/widgets/interactive-graphs/locked-figures/utils.ts
@@ -7,26 +7,34 @@ export const strokeWeights: Record<StrokeWeight, number> = {
 } as const;
 
 export function clampDomain(
-    domain: [number, number],
+    domain: [number | null, number | null],
     graphBounds: [number, number],
 ): [number, number] | null {
+    // Infinity values are lost (converted to null) when passed through
+    // JSON serialization.
+    const normalizedDomain: [number, number] = [
+        domain[0] === null ? -Infinity : domain[0],
+        domain[1] === null ? Infinity : domain[1],
+    ];
     // If the domain is invalid, return the graph bounds
-    if (domain[0] > domain[1]) {
+    if (normalizedDomain[0] > normalizedDomain[1]) {
         return graphBounds;
     }
 
     // If the domain is outside the graph bounds, return null
     if (
-        (domain[0] < graphBounds[0] && domain[1] < graphBounds[0]) ||
-        (domain[0] > graphBounds[1] && domain[1] > graphBounds[1])
+        (normalizedDomain[0] < graphBounds[0] &&
+            normalizedDomain[1] < graphBounds[0]) ||
+        (normalizedDomain[0] > graphBounds[1] &&
+            normalizedDomain[1] > graphBounds[1])
     ) {
         return null;
     }
 
     // Clamp the function to the bounds of the graph to prevent memory
     // leaks when the domain is set to something like [-Infinity, Infinity].
-    const min = Math.max(domain[0], graphBounds[0]);
-    const max = Math.min(domain[1], graphBounds[1]);
+    const min = Math.max(normalizedDomain[0], graphBounds[0]);
+    const max = Math.min(normalizedDomain[1], graphBounds[1]);
 
     return [min, max];
 }
