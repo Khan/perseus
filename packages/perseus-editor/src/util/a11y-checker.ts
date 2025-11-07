32d5dce87c2a93e17202a5155cec2cae073dd788
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2025-11-07 14:45:30 -0800

    [poc/accessibility-checker-in-editor] Add update function for retry callback.

--- packages/perseus-editor/src/util/a11y-checker.ts
+++ packages/perseus-editor/src/util/a11y-checker.ts
@@ -152,7 +152,7 @@ const runAxeCore = (updateIssuesFn: (issues: Issue[]) => void): void => {
                 results.passes.length === 0
             ) {
                 console.log("No valid results - Retrying...");
-                setTimeout(runAxeCore, 1000); // No valid results indicates that content may not be fully loaded yet
+                setTimeout(runAxeCore, 1000, updateIssuesFn); // No valid results indicates that content may not be fully loaded yet
             } else {
                 updateIssuesFn(issues);
             }
