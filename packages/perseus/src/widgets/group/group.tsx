12b61d7390da34867b8b5c93643c4c5a138cdb01
Ivy Olamit <ivy@khanacademy.org>
2025-11-25 15:55:36 -0800

    [LEMS-3652/image-widget-alignment-mobile] implement DOM manipulation solution

--- packages/perseus/src/widgets/group/group.tsx
+++ packages/perseus/src/widgets/group/group.tsx
@@ -7,6 +7,7 @@
  * learner-facing, but new content cannot be made with Group.
  */
 
+import {isFeatureOn} from "@khanacademy/perseus-core";
 import {linterContextDefault} from "@khanacademy/perseus-linter";
 import classNames from "classnames";
 import * as React from "react";
@@ -47,6 +48,7 @@ class Group extends React.Component<Props> implements Widget {
     declare context: React.ContextType<typeof PerseusI18nContext>;
 
     rendererRef: Renderer | null | undefined;
+    groupRef = React.createRef<HTMLDivElement>();
 
     static defaultProps: DefaultProps = {
         content: "",
@@ -60,7 +62,99 @@ class Group extends React.Component<Props> implements Widget {
         // numbering scheme. We force another render so that we can annotate
         // the group with the correct number.
         this.forceUpdate();
+
+        // Reorder image widgets for float wrapping behavior
+        // Use setTimeout to ensure DOM is fully rendered before reordering
+        setTimeout(() => {
+            this.reorderImageWidgetsForViewport();
+        }, 0);
     }
+
+    componentDidUpdate(prevProps: Props) {
+        // Reorder if mobile state changes
+        if (prevProps.apiOptions.isMobile !== this.props.apiOptions.isMobile) {
+            this.reorderImageWidgetsForViewport();
+        }
+    }
+
+    /**
+     * Reorders image widgets within the group to enable proper float wrapping.
+     * On desktop: Moves wrapped images to the beginning for text to wrap around them.
+     * On mobile: Moves images to the end to appear after content.
+     */
+    reorderImageWidgetsForViewport = () => {
+        const imageUpgradeFF = isFeatureOn(
+            {apiOptions: this.props.apiOptions},
+            "image-widget-upgrade",
+        );
+
+        if (!imageUpgradeFF) {
+            return;
+        }
+
+        const groupElement = this.groupRef.current;
+        if (!groupElement) {
+            return;
+        }
+
+        // Find the renderer inside the group (direct child)
+        const rendererElement =
+            groupElement.querySelector<HTMLElement>(".perseus-renderer");
+        if (!rendererElement) {
+            return;
+        }
+
+        // Determine if mobile by checking both apiOptions and CSS class
+        // The .perseus-mobile class is automatically added by the framework
+        const isMobileFromProps = this.props.apiOptions.isMobile;
+        const isMobileFromCSS =
+            groupElement.closest(".perseus-mobile") !== null;
+        const isMobile = isMobileFromProps ?? isMobileFromCSS;
+
+        // Find all wrapped image widgets (handles nested paragraph structures)
+        const wrappedImageWidgets = Array.from(
+            rendererElement.querySelectorAll<HTMLElement>(
+                ".perseus-widget-container.widget-wrap-left, " +
+                    ".perseus-widget-container.widget-wrap-right",
+            ),
+        );
+
+        wrappedImageWidgets.forEach((widget) => {
+            // Find the top-level paragraph within the renderer that contains this widget
+            // This is the paragraph with data-perseus-paragraph-index
+            let currentElement: HTMLElement | null = widget;
+            let topLevelParagraph: HTMLElement | null = null;
+
+            while (currentElement && currentElement !== rendererElement) {
+                if (
+                    currentElement.classList.contains("paragraph") &&
+                    currentElement.hasAttribute("data-perseus-paragraph-index")
+                ) {
+                    topLevelParagraph = currentElement;
+                    break;
+                }
+                currentElement = currentElement.parentElement;
+            }
+
+            if (!topLevelParagraph) {
+                return;
+            }
+
+            if (isMobile) {
+                // Mobile: Move image paragraph to end of renderer
+                // Only move if it's not already the last child
+                if (rendererElement.lastChild !== topLevelParagraph) {
+                    rendererElement.appendChild(topLevelParagraph);
+                }
+            } else {
+                // Desktop: Move image paragraph to start of renderer for float wrapping
+                const firstChild = rendererElement.firstChild;
+                if (firstChild !== topLevelParagraph) {
+                    rendererElement.insertBefore(topLevelParagraph, firstChild);
+                }
+            }
+        });
+    };
 
     getPromptJSON(): GroupPromptJSON {
         return _getPromptJSON(this.rendererRef?.getPromptJSON());
@@ -111,6 +205,7 @@ class Group extends React.Component<Props> implements Widget {
         //     the parent Renderer's `findWidgets` function.
         return (
             <div
+                ref={this.groupRef}
                 className={classNames({
                     "perseus-group": true,
                 })}
