59788fd4a5d52faec8439a3bc07531f1fd3d64bd
Cat Johnson <catjohnson@khanacademy.org>
2025-12-11 14:21:36 -0800

    [catjohnson/editor-typing-2] Updating storybook page to use improved storybook format.

--- packages/perseus-editor/src/diffs/__docs__/text-diff.stories.tsx
+++ packages/perseus-editor/src/diffs/__docs__/text-diff.stories.tsx
@@ -4,21 +4,12 @@ import TextDiff from "../text-diff";
 
 import Wrapper from "./perseus-diff-wrapper";
 
-import type {TextDiffProps} from "../text-diff";
-
 import "../../styles/perseus-editor.css";
+import type {Meta, StoryObj} from "@storybook/react-vite";
 
-type StoryArgs = Record<string, TextDiffProps>;
-
-type Story = {
-    title: string;
-    decorators: ReadonlyArray<
-        (StoryComponent: typeof React.Component) => React.ReactElement
-    >;
-};
-
-export default {
-    title: "PerseusEditor/Diffs/Text Diff",
+const meta: Meta = {
+    title: "Editors/Diffs/Text Diff",
+    component: TextDiff,
     decorators: [
         (StoryComponent) => (
             <Wrapper>
@@ -26,24 +17,24 @@ export default {
             </Wrapper>
         ),
     ],
-} as Story;
-
-export const TextExample = (args: StoryArgs): React.ReactElement => {
-    return (
-        <TextDiff
-            title="A day in the life of a text diff"
-            before="🥱 Hello world!"
-            after="😴 Goodbye world!"
-        />
-    );
 };
 
-export const ImageExample = (args: StoryArgs): React.ReactElement => {
-    return (
-        <TextDiff
-            title="A day in the life of a text diff"
-            before="🥱 Hello world! and ![an image](https://cdn.kastatic.org/ka-content-images/4f38f705977774bcac3b5bed9f81b56710abc8b0.png) ![graphie image](web+graphie://ka-perseus-graphie.s3.amazonaws.com/2ac8f769a7323f55e41c12cfa39e774be08bc138) ![another graphie image](web+graphie://ka-perseus-graphie.s3.amazonaws.com/669d6011774f3c0f6809553d210b4f51b7e3e4fe)"
-            after="😴 Goodbye world! and ![graphie image](web+graphie://ka-perseus-graphie.s3.amazonaws.com/2ac8f769a7323f55e41c12cfa39e774be08bc138) ![another graphie image](web+graphie://ka-perseus-graphie.s3.amazonaws.com/669d6011774f3c0f6809553d210b4f51b7e3e4fe) ![A graph](https://fastly.kastatic.org/ka-perseus-images/9757eb155d1283bb137d0cfdaf9818fd600702ed.png)"
-        />
-    );
+export default meta;
+
+type Story = StoryObj<typeof TextDiff>;
+
+export const TextExample: Story = {
+    args: {
+        title: "A day in the life of a text diff",
+        before: "🥱 Hello world!",
+        after: "😴 Goodbye world!",
+    },
+};
+
+export const ImageExample: Story = {
+    args: {
+        title: "A day in the life of a text diff",
+        before: "🥱 Hello world! and ![an image](https://cdn.kastatic.org/ka-content-images/4f38f705977774bcac3b5bed9f81b56710abc8b0.png) ![graphie image](web+graphie://ka-perseus-graphie.s3.amazonaws.com/2ac8f769a7323f55e41c12cfa39e774be08bc138) ![another graphie image](web+graphie://ka-perseus-graphie.s3.amazonaws.com/669d6011774f3c0f6809553d210b4f51b7e3e4fe)",
+        after: "😴 Goodbye world! and ![graphie image](web+graphie://ka-perseus-graphie.s3.amazonaws.com/2ac8f769a7323f55e41c12cfa39e774be08bc138) ![another graphie image](web+graphie://ka-perseus-graphie.s3.amazonaws.com/669d6011774f3c0f6809553d210b4f51b7e3e4fe) ![A graph](https://fastly.kastatic.org/ka-perseus-images/9757eb155d1283bb137d0cfdaf9818fd600702ed.png)",
+    },
 };
