01b3a35118acab1d1dd77f0575abde853cd62059
Cat Johnson <catjohnson@khanacademy.org>
2025-12-15 15:21:29 -0800

    [catjohnson/label-image-inaccessible-addition] Updating accessibility function, adding tests, and moving to better, more testible location.

--- packages/perseus-core/src/widgets/label-image/label-image-util.test.ts
+++ packages/perseus-core/src/widgets/label-image/label-image-util.test.ts
@@ -1,4 +1,6 @@
-import getLabelImagePublicWidgetOptions from "./label-image-util";
+import getLabelImagePublicWidgetOptions, {
+    isLabelImageAccessible,
+} from "./label-image-util";
 
 import type {PerseusLabelImageWidgetOptions} from "../../data-schema";
 
@@ -42,5 +44,107 @@ describe("getLabelImagePublicWidgetOptions", () => {
             multipleAnswers: false,
             static: false,
         });
+    });
+});
+
+describe("isLabelImageAccessible", () => {
+    const accessibleOptions: PerseusLabelImageWidgetOptions = {
+        imageUrl: "https://example.com/image.png",
+        imageAlt: "A descriptive alt text for the image",
+        choices: [],
+        imageHeight: 0,
+        imageWidth: 0,
+        markers: [
+            {
+                answers: [],
+                label: "A descriptive label for the image",
+                x: 0,
+                y: 0,
+            },
+            {
+                answers: [],
+                label: "Another descriptive label for the image",
+                x: 100,
+                y: 100,
+            },
+        ],
+        multipleAnswers: false,
+        hideChoicesFromInstructions: false,
+        static: false,
+    };
+
+    it("should return true if label image is accessible", () => {
+        const options: PerseusLabelImageWidgetOptions = accessibleOptions;
+
+        // Act
+        const result = isLabelImageAccessible(options);
+
+        // Assert
+        expect(result).toBe(true);
+    });
+
+    it("should return true if accessible and there are no markers", () => {
+        // Arrange
+        const options: PerseusLabelImageWidgetOptions = {
+            ...accessibleOptions,
+            markers: [],
+        };
+
+        // Act
+        const result = isLabelImageAccessible(options);
+
+        // Assert
+        expect(result).toBe(true);
+    });
+
+    it("should return true if accessible and url is empty and alt is empty", () => {
+        // Arrange
+        const options: PerseusLabelImageWidgetOptions = {
+            ...accessibleOptions,
+            imageUrl: "",
+            imageAlt: "",
+        };
+
+        // Act
+        const result = isLabelImageAccessible(options);
+
+        // Assert
+        expect(result).toBe(true);
+    });
+
+    it("should return false if image url is not empty and image alt is empty", () => {
+        // Arrange
+        const options: PerseusLabelImageWidgetOptions = {
+            ...accessibleOptions,
+            imageAlt: "",
+        };
+
+        // Act
+        const result = isLabelImageAccessible(options);
+
+        // Assert
+        expect(result).toBe(false);
+    });
+
+    it("should return false if there is a marker with an empty label", () => {
+        // Arrange
+        const options: PerseusLabelImageWidgetOptions = {
+            ...accessibleOptions,
+            markers: [
+                ...accessibleOptions.markers,
+                {
+                    answers: [],
+                    label: "",
+                    x: 0,
+                    y: 0,
+                },
+            ],
+        };
+
+        // Act
+        const result = isLabelImageAccessible(options);
+
+        // Assert
+        expect(result).toBe(false);
     });
 });
