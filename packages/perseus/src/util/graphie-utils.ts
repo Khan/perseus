d8837d2fadc84de886998e0bd81c6fab7e63f23b
Mahtab Sabet <mahtab@khanacademy.org>
2026-01-13 15:17:47 -0700

    [CP-10010] DRAFT: testing fix for graphies in Crowdin

--- packages/perseus/src/util/graphie-utils.ts
+++ packages/perseus/src/util/graphie-utils.ts
@@ -230,12 +230,13 @@ export function loadGraphie(
         };
 
         if (shouldUseLocalizedData()) {
-            cacheData.localized = false;
-            // If there is isn't any localized data, use the error callback to try to
-            // load the non-localized data. If that fails too, then we will throw an error.
-            retrieveData(getLocalizedDataUrl(url), () =>
-                retrieveData(Util.getDataUrl(url), dataLoadErrorHandler),
-            );
+            // Try to load localized data first. If that fails, fall back to
+            // non-localized data and update the localized flag accordingly.
+            retrieveData(getLocalizedDataUrl(url), () => {
+                // Mark as non-localized since we're falling back to English data
+                cacheData.localized = false;
+                retrieveData(Util.getDataUrl(url), dataLoadErrorHandler);
+            });
         } else {
             retrieveData(Util.getDataUrl(url), dataLoadErrorHandler);
         }
