c6a2806b80171992a075efabbe2019d86d1a5456
Anakaren Rojas <anakaren@khanacademy.org>
2026-01-16 09:09:44 -0800

    [nota-radio-editor-linter] adds linter to radio editor - triggered when NOTA and another choice as marked as correct

--- packages/perseus-linter/src/rules/radio-widget-error.test.ts
+++ packages/perseus-linter/src/rules/radio-widget-error.test.ts
@@ -3,36 +3,76 @@ import {expectPass, expectWarning} from "../__tests__/test-utils";
 import radioWidgetErrorRule from "./radio-widget-error";
 
 describe("radio-widget-error", () => {
-    // Error for radio widget with no choices
-    expectWarning(radioWidgetErrorRule, "[[☃ radio 1]]", {
-        widgets: {
-            "radio 1": {
-                options: {
-                    choices: [],
+    describe("No choice is marked as correct.", () => {
+        // Error for radio widget with no choices
+        expectWarning(radioWidgetErrorRule, "[[☃ radio 1]]", {
+            widgets: {
+                "radio 1": {
+                    options: {
+                        choices: [],
+                    },
                 },
             },
-        },
+        });
+
+        // Error for radio widget with no correct choices
+        expectWarning(radioWidgetErrorRule, "[[☃ radio 1]]", {
+            widgets: {
+                "radio 1": {
+                    options: {
+                        choices: [{content: "Incorrect", correct: false}],
+                    },
+                },
+            },
+        });
+
+        // Pass for radio widget with correct choices
+        expectPass(radioWidgetErrorRule, "[[☃ radio 1]]", {
+            widgets: {
+                "radio 1": {
+                    options: {
+                        choices: [{content: "Correct", correct: true}],
+                    },
+                },
+            },
+        });
     });
 
-    // Error for radio widget with no correct choices
-    expectWarning(radioWidgetErrorRule, "[[☃ radio 1]]", {
-        widgets: {
-            "radio 1": {
-                options: {
-                    choices: [{content: "Incorrect", correct: false}],
+    describe("Cannot mark both None of the Above and another choice as correct.", () => {
+        // Error for radio widget with no correct choices
+        expectWarning(radioWidgetErrorRule, "[[☃ radio 1]]", {
+            widgets: {
+                "radio 1": {
+                    options: {
+                        choices: [
+                            {content: "I'm marked correct", correct: true},
+                            {
+                                content: "None Of the Above",
+                                isNoneOfTheAbove: true,
+                                correct: true,
+                            },
+                        ],
+                    },
                 },
             },
-        },
-    });
+        });
 
-    // Pass for radio widget with correct choices
-    expectPass(radioWidgetErrorRule, "[[☃ radio 1]]", {
-        widgets: {
-            "radio 1": {
-                options: {
-                    choices: [{content: "Correct", correct: true}],
+        // Pass for radio widget with correct choices
+        expectPass(radioWidgetErrorRule, "[[☃ radio 1]]", {
+            widgets: {
+                "radio 1": {
+                    options: {
+                        choices: [
+                            {content: "Correct", correct: true},
+                            {
+                                content: "None Of the Above",
+                                isNoneOfTheAbove: true,
+                                correct: false,
+                            },
+                        ],
+                    },
                 },
             },
-        },
+        });
     });
 });
--- packages/perseus-linter/src/rules/radio-widget-error.ts
+++ packages/perseus-linter/src/rules/radio-widget-error.ts
@@ -22,10 +22,26 @@ export default Rule.makeRule({
             return;
         }
 
-        // Error if no choice is marked as correct.
+        const errors: Array<string> = [];
         const choices = widget.options.choices;
+
+        // Error if no choice is marked as correct
         if (!choices.some((choice) => choice.correct)) {
-            return "No choice is marked as correct.";
+            errors.push("No choice is marked as correct.");
         }
+
+        // Error if NOTA and another choice are both marked as correct
+        const correctChoices = choices.filter((choice) => choice.correct);
+        const hasNotaCorrect = correctChoices.some(
+            (choice) => choice.isNoneOfTheAbove,
+        );
+
+        if (hasNotaCorrect && correctChoices.length > 1) {
+            errors.push(
+                "Cannot mark both None of the Above and another choice as correct.",
+            );
+        }
+
+        return errors.join("\n\n");
     },
 }) as Rule;
