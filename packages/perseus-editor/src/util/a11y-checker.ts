8a9c35cbb97b6c4bce8f1f4945744520a236ea5c
Mark Fitzgerald <13896410+mark-fitzgerald@users.noreply.github.com>
2026-01-26 11:24:56 -0800

    Add accessibility checker (axe-core) in the exercise editor (#3167)
    ## Summary:
    This PR implements the axe-core accessibility library, and automatically runs the checker. However, results are not shown in the Issues panel unless the toggle button is set "On". The reason for defaulting to "Off" is to provide a kind of feature flag option for this feature. The current accessibility results are somewhat noisy, in that content creators can't necessarily address all the issues that are discovered. Once this PR is implemented, time will be spent assessing the results of the scans and either resolving them or writing tickets for follow-up. At some future point, this automated scan will be defaulted to "On" for all content creators, and expanded to other editors (i.e. the Article and Assessments editors).
    
    Issue: LEMS-3822
    
    ## Test plan:
    1. Launch Editor Demo in Storybook, or [view editor in ZND](https://prod-znd-260114-2890-d538d2e.khanacademy.org/)
    2. Open Issues panel at the top of the editor
    3. Toggle the accessibility checker switch (at the bottom of the issues panel)
    <img width="330" height="110" alt="Axe core toggle" src="https://github.com/user-attachments/assets/aca73bda-afa7-4e9e-a2b9-a329b24ff9b5" />
    
    4. Make a change to the content
       * If no issues show, it may be necessary to add content that breaks accessibility to get a result
    5. Note that the accessibility issue(s) show in the panel
    <img width="330" height="148" alt="accessibility issue example" src="https://github.com/user-attachments/assets/ac0db2b2-0b3b-40c7-b2a2-9322e1fde7bc" />
    
    6. Toggle the switch off
    7. Note that the accessibility issue(s) are removed from the panel
    
    Author: mark-fitzgerald
    
    Reviewers: Myranae, mark-fitzgerald
    
    Required Reviewers:
    
    Approved By: Myranae
    
    Checks: ⏭️  1 check has been skipped, ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3167

---
+++ packages/perseus-editor/src/util/a11y-checker.ts
@@ -0,0 +1,200 @@
+import * as axeCore from "axe-core";
+
+import issuesList from "./a11y-issues-list";
+
+import type {Issue} from "../components/issues-panel";
+import type axe from "axe-core";
+
+const assistanceNeededMessage =
+    "Developer assistance needed - Please send this exercise and warning info to the LEMS team for review.";
+
+const previewIframeSelector =
+    '.perseus-question-container iframe[data-name="content-preview"]';
+const axeCoreEditorOptions = {
+    include: "#page-container",
+    exclude: '[target="lint-help-window"]',
+};
+
+// NOTE: The following is the correct way to scan within iframes. However,
+//           this isn't working within the exercise editor (works fine in Storybook).
+//       Keeping this here for reference in case the issue ever gets figured out.
+// const axeCoreEditorOptions = {
+//     include: {
+//         fromFrames: [previewIframeSelector, "#page-container"],
+//     },
+//     exclude: {
+//         fromFrames: [previewIframeSelector, '[target="lint-help-window"]'],
+//     },
+// };
+const axeCoreStorybookOptions = {
+    include: {
+        fromFrames: [
+            'iframe[data-name="content-preview"]',
+            "#storybook-root > .framework-perseus",
+        ],
+    },
+    exclude: {
+        fromFrames: [
+            'iframe[data-name="content-preview"]',
+            '[target="lint-help-window"]',
+        ],
+    },
+};
+
+const convertAxeImpactToIssueImpact = (
+    impact?: axe.ImpactValue,
+): Issue["impact"] => {
+    switch (impact) {
+        case "critical":
+            return "high";
+        case "serious":
+            return "high";
+        case "moderate":
+            return "medium";
+        case "minor":
+            return "low";
+        default:
+            return "low";
+    }
+};
+
+const getIssueMessage = (nodes: axe.NodeResult[]): string => {
+    return nodes
+        .flatMap((node) => {
+            return node.all
+                .concat(node.any, node.none)
+                .map((result) => result.message);
+        })
+        .filter(
+            (message, index, allMessages) =>
+                allMessages.indexOf(message) === index,
+        )
+        .join(". ");
+};
+
+const getIssueElements = (
+    nodes: axe.NodeResult[],
+    iFrameElement: HTMLIFrameElement | null,
+): Element[] => {
+    // @ts-expect-error TS2322: Type 'string[]' is not assignable to type 'Element[]'.
+    return nodes.flatMap((node) => {
+        // @ts-expect-error TS2769: No overload matches this call.
+        return node.target.reduce((elements: Element[], target: string) => {
+            let element: Element | null = null;
+            if (
+                elements.length > 0 &&
+                elements[elements.length - 1].tagName.toLowerCase() === "iframe"
+            ) {
+                elements[elements.length - 1] =
+                    // @ts-expect-error TS2551: Property 'contentDocument' does not exist on type 'Element'
+                    elements[elements.length - 1].contentDocument.querySelector(
+                        target,
+                    );
+            } else if (iFrameElement) {
+                element =
+                    iFrameElement.contentDocument?.querySelector(target) ||
+                    null;
+            } else {
+                element = document.querySelector(target);
+            }
+            if (element) {
+                elements.push(element);
+            }
+            return elements;
+        }, []);
+    });
+};
+
+const mapResultsToIssues = (
+    results: axe.Result[],
+    type: "Warning" | "Alert",
+    iFrameElement: HTMLIFrameElement | null,
+): Issue[] => {
+    return results.map((result) => {
+        const isUserFixable =
+            type === "Alert" &&
+            issuesList["axe-core"]["user-fixable"].some(
+                (testId) => testId === result.id,
+            );
+        const description = isUserFixable ? "" : assistanceNeededMessage;
+        const issueInfo = {
+            id: result.id,
+            description: description,
+            elements: getIssueElements(result.nodes, iFrameElement),
+            helpUrl: result.helpUrl,
+            help: result.help,
+            impact: convertAxeImpactToIssueImpact(result.impact),
+            message: getIssueMessage(result.nodes),
+            type: type,
+        };
+        return issueInfo;
+    });
+};
+
+const runAxeCore = (updateIssuesFn: (issues: Issue[]) => void): void => {
+    const isInStorybook = !!document.getElementById("storybook-root");
+    if (!isInStorybook) {
+        let frameHasLoaded = false;
+        const frame = document.querySelector(previewIframeSelector);
+        if (frame) {
+            const frameDocument =
+                // @ts-expect-error TS2551: Property 'contentDocument' does not exist on type 'Element'.
+                frame.contentDocument || frame.contentWindow?.document;
+            frameHasLoaded = frameDocument?.readyState === "complete";
+        }
+        if (!frameHasLoaded) {
+            setTimeout(runAxeCore, 100, updateIssuesFn);
+            return;
+        }
+    }
+    const options = isInStorybook
+        ? axeCoreStorybookOptions
+        : axeCoreEditorOptions;
+    // eslint-disable-next-line no-console
+    console.log("Axe Core options: ", options);
+    const previewWindow = document.querySelector(
+        previewIframeSelector,
+    ) as HTMLIFrameElement | null;
+    const axeCoreProper = isInStorybook
+        ? axeCore
+        : // @ts-expect-error TS2339: Property axe does not exist on type Window.
+          previewWindow?.contentWindow?.axe;
+    axeCoreProper.configure({reporter: "v2"});
+    axeCoreProper.run(options).then(
+        (results) => {
+            // eslint-disable-next-line no-console
+            console.log(`Accessibility Results: `, results);
+            const violations = mapResultsToIssues(
+                results.violations,
+                "Alert",
+                isInStorybook ? null : previewWindow,
+            );
+            const incompletes = mapResultsToIssues(
+                results.incomplete,
+                "Warning",
+                isInStorybook ? null : previewWindow,
+            );
+            const issues = violations.concat(incompletes);
+            // eslint-disable-next-line no-console
+            console.log(`  Issues: `, issues);
+            if (
+                violations.length === 0 &&
+                incompletes.length === 0 &&
+                results.passes.length === 0
+            ) {
+                setTimeout(runAxeCore, 1500, updateIssuesFn); // No valid results indicates that content may not be fully loaded yet
+            } else {
+                updateIssuesFn(issues);
+            }
+        },
+        (error) => {
+            // eslint-disable-next-line no-console
+            console.log(`*** Error: `, error);
+        },
+    );
+};
+
+export const runAxeCoreOnUpdate = (priorTimeoutId, setState): any => {
+    clearTimeout(priorTimeoutId);
+    return setTimeout(runAxeCore, 1500, setState);
+};
