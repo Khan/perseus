f4f8070a390c8c78500dcbd415c25a09ca34c801
Matthew Curtis <matthewcurtis@khanacademy.org>
2026-01-26 13:24:36 -0600

    [perseus-score-version] export version from perseus-score

--- packages/perseus-score/src/index.ts
+++ packages/perseus-score/src/index.ts
@@ -1,3 +1,5 @@
+export {libVersion} from "./version";
+
 export {default as KhanAnswerTypes} from "./util/answer-types";
 export type {Score} from "./util/answer-types";
 export {default as scoreCategorizer} from "./widgets/categorizer/score-categorizer";
