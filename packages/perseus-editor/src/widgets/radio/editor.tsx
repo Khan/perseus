73436a2933b4b2439cd7f370a737272ec5b7a62e
Anakaren Rojas <anakaren@khanacademy.org>
2026-01-28 12:10:20 -0800

    [typesafe-radio-editor] lint fix

--- packages/perseus-editor/src/widgets/radio/editor.tsx
+++ packages/perseus-editor/src/widgets/radio/editor.tsx
@@ -34,7 +34,7 @@ export interface RadioEditorProps {
 
     onChange: (
         values: Partial<PerseusRadioWidgetOptions>,
-        callback?: (() => void) | null
+        callback?: (() => void) | null,
     ) => void;
 }
 
