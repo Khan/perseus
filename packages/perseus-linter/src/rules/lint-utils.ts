a3da223190adfb1f0a770ce2c5aa33fab5964880
Ben Christel <benchristel@khanacademy.org>
2026-02-06 17:02:22 -0800

    [benc/fix-regex-escape] Fix more issues

--- packages/perseus-linter/src/rules/lint-utils.ts
+++ packages/perseus-linter/src/rules/lint-utils.ts
@@ -1,9 +1,8 @@
-/* eslint-disable no-useless-escape */
 // Return the portion of a URL between // and /. This is the authority
 // portion which is usually just the hostname, but may also include
 // a username, password or port. We don't strip those things out because
 // we typically want to reject any URL that includes them
-const HOSTNAME = /\/\/([^\/]+)/;
+const HOSTNAME = /\/\/([^/]+)/;
 
 // Return the hostname of the URL, with any "www." prefix removed.
 // If this is a relative URL with no hostname, return an empty string.
