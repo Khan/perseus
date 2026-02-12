0ff1d5e5739fa3c9cf10d611b7b14467329008d5
Nisha Yerunkar <nisha@khanacademy.org>
2026-02-11 16:02:06 -0800

    [export-linter-warning] Export LinterWarning type

--- packages/perseus-linter/src/index.ts
+++ packages/perseus-linter/src/index.ts
@@ -3,6 +3,7 @@ export {libVersion} from "./version";
 export {linterContextDefault} from "./proptypes";
 
 export {default as Rule} from "./rule";
+export {type LinterWarning} from "./rule";
 export {runLinter, allLintRules as rules} from "./run-linter";
 
 export type {LinterContextProps} from "./types";
