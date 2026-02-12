1cd9b4d66a839acc99f377ee6081530ed920d4e9
Nisha Yerunkar <nisha@khanacademy.org>
2026-02-12 13:29:38 -0800

    Export LinterWarning type (#3248)
    ## Summary:
    I'm exporting the `LinterWarning` type here so it can be used in the `frontend` repo.
    
    Right now, it's being [duplicated in frontend](https://github.com/Khan/frontend/blob/8f0ebde603eea804e94d27f3048e01681069d13e/apps/devadmin/src/editor-package/types.ts#L117-L131) because it can't be imported from Perseus.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-3876
    
    ## Test plan:
    N/A
    
    Author: nishasy
    
    Reviewers: catandthemachines, jeremywiebe, nishasy
    
    Required Reviewers:
    
    Approved By: catandthemachines, jeremywiebe
    
    Checks: ⏭️  1 check has been skipped, ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3248

--- packages/perseus-linter/src/index.ts
+++ packages/perseus-linter/src/index.ts
@@ -5,6 +5,7 @@ export {linterContextDefault} from "./proptypes";
 export {default as Rule} from "./rule";
 export {runLinter, allLintRules as rules} from "./run-linter";
 
+export type {LinterWarning} from "./rule";
 export type {LinterContextProps} from "./types";
 
 export function pushContextStack(context: any, name: string): any {
