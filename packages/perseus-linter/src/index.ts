c5301180377ddada22749c697e3e37303afdd227
Nisha Yerunkar <nisha@khanacademy.org>
2026-02-12 13:07:18 -0800

    [export-linter-warning] put the 'type' outside

--- packages/perseus-linter/src/index.ts
+++ packages/perseus-linter/src/index.ts
@@ -3,9 +3,9 @@ export {libVersion} from "./version";
 export {linterContextDefault} from "./proptypes";
 
 export {default as Rule} from "./rule";
-export {type LinterWarning} from "./rule";
 export {runLinter, allLintRules as rules} from "./run-linter";
 
+export type {LinterWarning} from "./rule";
 export type {LinterContextProps} from "./types";
 
 export function pushContextStack(context: any, name: string): any {
