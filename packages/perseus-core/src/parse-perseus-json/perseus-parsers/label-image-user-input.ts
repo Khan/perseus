e25d677f41e2362f1a495f97ef3c5c72e7eee146
Ben Christel <benchristel@khanacademy.org>
2026-03-04 08:10:31 -0800

    [benc/discard-unrecognized-fields] Fix lint

--- packages/perseus-core/src/parse-perseus-json/perseus-parsers/label-image-user-input.ts
+++ packages/perseus-core/src/parse-perseus-json/perseus-parsers/label-image-user-input.ts
@@ -1,4 +1,9 @@
-import {array, strictObject, optional, string} from "../general-purpose-parsers";
+import {
+    array,
+    strictObject,
+    optional,
+    string,
+} from "../general-purpose-parsers";
 
 export const parseLabelImageUserInput = strictObject({
     markers: array(
