f3178180ee08b28f2c7edbcc7b787b9d73ab60c4
Nisha Yerunkar <nisha@khanacademy.org>
2026-03-05 15:23:54 -0800

    [remove-image-widget-upgrade-alignment-flag] [Image] | (cleanup) | Remove image-widget-upgrade-alignment feature flag
    Now that the `image-widget-upgrade-alignment` feature flag has been on for everyone for a
    while without issues, we can remove it and have all the features permanently available.
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-3909
    
    Test plan:
    Write "image-widget-upgrade-alignment" in the IDE search and confirm they're all gone.
    
    Storybook
    - Go to `/perseus/?path=/story/editors-articleeditor--demo` (NOT the "with all flags" story)
    - Add an image widget
    - Confirm that there are four alignment options

--- packages/perseus-editor/src/components/widget-editor.tsx
+++ packages/perseus-editor/src/components/widget-editor.tsx
@@ -3,7 +3,6 @@ import {Widgets, excludeDenylistKeys} from "@khanacademy/perseus";
 import {
     CoreWidgetRegistry,
     applyDefaultsToWidget,
-    isFeatureOn,
 } from "@khanacademy/perseus-core";
 import {View} from "@khanacademy/wonder-blocks-core";
 import {Strut} from "@khanacademy/wonder-blocks-layout";
@@ -147,21 +146,11 @@ class WidgetEditor extends React.Component<
 
         const Ed = Widgets.getEditor(widgetInfo.type);
         let supportedAlignments: ReadonlyArray<Alignment>;
-        const imageUpgradeAlignmentFF = isFeatureOn(
-            this.props,
-            "image-widget-upgrade-alignment",
-        );
 
         if (this.props.apiOptions.showAlignmentOptions) {
-            // TODO(LEMS-3520): Feature flag cleanup
-            // Remove if statement once the image alignment upgrade FF is released
-            if (widgetInfo.type === "image" && !imageUpgradeAlignmentFF) {
-                supportedAlignments = ["block", "full-width"];
-            } else {
-                supportedAlignments = CoreWidgetRegistry.getSupportedAlignments(
-                    widgetInfo.type,
-                );
-            }
+            supportedAlignments = CoreWidgetRegistry.getSupportedAlignments(
+                widgetInfo.type,
+            );
         } else {
             // NOTE(kevinb): "default" is not one in `validAlignments` in widgets.js.
             supportedAlignments = ["default"];
