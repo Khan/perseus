770855694bdc868ad70ea4040d3571c3597afcc8
Tamara <tamara@khanacademy.org>
2026-03-11 10:43:39 -0500

    [tb/add-claude-stopship] Add grep exclusion of .claude/plans

--- utils/stopship-check.sh
+++ utils/stopship-check.sh
@@ -4,7 +4,7 @@
 # We use git to search for files that contain the word STOPSHIP. If we find any
 # matches, we emit them as Github Action errors.
 
-if MATCHES=$(git grep --line-number --column  -I -eSTOPSHIP -- ':(exclude).github/workflows/node-ci.yml' ':(exclude)./utils/stopship-check.sh'); then
+if MATCHES=$(git grep --line-number --column  -I -eSTOPSHIP -- ':(exclude).github/workflows/node-ci.yml' ':(exclude)./utils/stopship-check.sh' ':(exclude).claude/plans/'); then
     echo "$MATCHES" | sed -E "s/([^:]*):([^:]*):([^:]*):(.*)/::error file=\1,line=\2,col=\3,title=STOPSHIP found!::\4/"
     exit 1
 fi
