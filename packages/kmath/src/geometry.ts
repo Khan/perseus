d29bd0402d0bc70c87637f09e589a54086ba1934
Ivy Olamit <ivy@khanacademy.org>
2026-03-13 11:27:53 -0700

    [LEMS-3955/tangent-kmath] [Interactive Graph] Add the tangent math utilities to kmath for supporting Tangent graph in Interactive Graph

--- packages/kmath/src/geometry.ts
+++ packages/kmath/src/geometry.ts
@@ -295,6 +295,47 @@ export function canonicalSineCoefficients([
     return [amplitude, angularFrequency, phase, verticalOffset];
 }
 
+export type TangentCoefficient = [
+    number, // amplitude
+    number, // angularFrequency
+    number, // phase
+    number, // verticalOffset
+];
+
+export function canonicalTangentCoefficients([
+    amplitude,
+    angularFrequency,
+    phase,
+    verticalOffset,
+]: TangentCoefficient): TangentCoefficient {
+    // For a curve of the form f(x) = a * tan(b * x - c) + d,
+    // this function ensures that b > 0, and c is its smallest
+    // non-negative value in [0, π).
+    //
+    // Unlike sine, tangent has no half-period phase shift identity
+    // (sin(x + π) = -sin(x)) so we cannot guarantee a > 0.
+    // We can only guarantee b > 0 using the odd function identity:
+    // a * tan(-|b|x - c) = (-a) * tan(|b|x - (-c))
+
+    // Guarantee b > 0
+    if (angularFrequency < 0) {
+        amplitude *= -1;
+        angularFrequency *= -1;
+        phase *= -1;
+    }
+
+    const period = Math.PI;
+    // Guarantee c is smallest non-negative value in [0, π)
+    while (phase > 0) {
+        phase -= period;
+    }
+    while (phase < 0) {
+        phase += period;
+    }
+
+    return [amplitude, angularFrequency, phase, verticalOffset];
+}
+
 // e.g. rotate([1, 2, 3]) -> [2, 3, 1]
 export function rotate<T>(
     array: ReadonlyArray<T>,
