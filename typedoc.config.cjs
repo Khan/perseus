0911d7d1745be68c66096b1b6767f8edacac2539
Jeremy Wiebe <jeremy@khanacademy.org>
2026-03-17 13:04:43 -0700

    [jer/typedoc] Start buildling Typedoc docs

---
+++ typedoc.config.cjs
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const packagesDir = path.join(__dirname, "packages");
+const entryPoints = fs
+    .readdirSync(packagesDir, {withFileTypes: true})
+    .filter(
+        (entry) =>
+            entry.isDirectory() &&
+            fs.existsSync(path.join(packagesDir, entry.name, "package.json")),
+    )
+    .map((entry) => path.join("packages", entry.name));
+
+/** @type {Partial<import("typedoc").TypeDocOptions>} */
+const config = {
+    name: "Perseus",
+    entryPointStrategy: "packages",
+    entryPoints,
+    out: "docs",
+    packageOptions: {
+        excludePrivate: true,
+        excludeInternal: true,
+        readme: "README.md",
+        entryPoints: ["./src/index.ts"],
+    },
+};
+
+module.exports = config;
