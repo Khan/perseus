84b2f21cd6575f2f147178162c4512ca9fc39c30
Ben Christel <benchristel@users.noreply.github.com>
2026-03-24 08:06:13 -0700

    Export a `removeOrphanedWidgetsFromPerseusItem` function from perseus-core (#3405)
    The plan is for the exercise editor to call this before saving.
    
    Issue: LEMS-3941
    
    ## Test plan:
    
    CI checks should pass.
    
    Author: benchristel
    
    Reviewers: claude[bot], jeremywiebe, anakaren-rojas
    
    Required Reviewers:
    
    Approved By: jeremywiebe
    
    Checks: ⏭️  1 check has been skipped, ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3405

---
+++ packages/perseus-core/src/utils/remove-orphaned-widgets.ts
@@ -0,0 +1,45 @@
+import {getWidgetIdsFromContent} from "./widget-id-utils";
+
+import type {
+    PerseusItem,
+    PerseusRenderer,
+    PerseusWidgetsMap,
+} from "../data-schema";
+
+/**
+ * Returns a new PerseusItem whose question and hints contain only widgets
+ * referenced by placeholders in their `content` string. Idempotent. Does not
+ * mutate its argument.
+ */
+export function removeOrphanedWidgetsFromPerseusItem(
+    item: PerseusItem,
+): PerseusItem {
+    return {
+        ...item,
+        question: removeOrphanedWidgets(item.question),
+        hints: item.hints.map(removeOrphanedWidgets),
+    };
+}
+
+/**
+ * Returns a new PerseusRenderer whose `widgets` map contains only widgets
+ * referenced by placeholders in the `content` string. Idempotent. Does not
+ * mutate its argument.
+ */
+export function removeOrphanedWidgets(
+    renderer: PerseusRenderer,
+): PerseusRenderer {
+    const referencedWidgetIds = getWidgetIdsFromContent(renderer.content);
+
+    const referencedWidgets: PerseusWidgetsMap = {};
+    for (const id of referencedWidgetIds) {
+        if (renderer.widgets[id]) {
+            referencedWidgets[id] = renderer.widgets[id];
+        }
+    }
+
+    return {
+        ...renderer,
+        widgets: referencedWidgets,
+    };
+}
