52b3fc763f35ec413c9e61b81d37e3169581da51
Sarah Third <sarahthird@gmail.com>
2026-03-27 14:23:48 -0700

    [maximum-overline] claude

--- packages/perseus/src/widgets/interactive-graphs/interactive-graph.testdata.ts
+++ packages/perseus/src/widgets/interactive-graphs/interactive-graph.testdata.ts
@@ -909,6 +909,7 @@ export const segmentWithLockedFunction = (
     options?: Partial<Omit<LockedFunctionType, "type">>,
 ): PerseusRenderer => {
     return generateInteractiveGraphQuestion({
+        correct: generateIGSegmentGraph(),
         lockedFigures: [generateIGLockedFunction({equation, ...options})],
     });
 };
