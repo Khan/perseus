b1b7e79fcd241b472401dde0620248f79111c112
Jeremy Wiebe <jeremy@khanacademy.org>
2026-03-30 15:01:31 -0700

    [jer/preview-ng-2-editor-minor-cleanups] [jer/editor-minor-cleanups] Remove unused bold support from hint preview

---
+++ .changeset/calm-tigers-sleep.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/perseus-editor": patch
+---
+
+Remove unused bold support from hint editor preview
--- packages/perseus-editor/src/preview/message-types.ts
+++ packages/perseus-editor/src/preview/message-types.ts
@@ -30,7 +30,6 @@ export type QuestionPreviewData = {
  */
 export type HintPreviewData = {
     hint: Hint;
-    bold: boolean;
     pos: number;
     apiOptions: APIOptions;
     linterContext: LinterContextProps;
