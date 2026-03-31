2dcfc5790c02f55737860493dbb666b18bc51c1e
Tamara <tamara@khanacademy.org>
2026-03-31 12:00:04 -0500

    [tb/testing-font-color-conversion] Color conversions

--- packages/perseus/src/widgets/label-image/label-image.tsx
+++ packages/perseus/src/widgets/label-image/label-image.tsx
@@ -9,6 +9,7 @@
 import {scoreLabelImageMarker} from "@khanacademy/perseus-score";
 import Clickable from "@khanacademy/wonder-blocks-clickable";
 import {View} from "@khanacademy/wonder-blocks-core";
+import {semanticColor} from "@khanacademy/wonder-blocks-tokens";
 import {StyleSheet, css} from "aphrodite";
 import classNames from "classnames";
 import * as React from "react";
@@ -862,7 +863,7 @@ const styles = StyleSheet.create({
                 marginLeft: 5,
                 marginRight: 5,
 
-                background: "rgba(33, 36, 44, 0.32)",
+                background: semanticColor.core.border.neutral.subtle,
 
                 borderRadius: 2,
             },
