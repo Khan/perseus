b1e2f8206fadade24732d7358fb5588134723cba
Sarah Third <sarahthird@gmail.com>
2026-03-31 16:57:35 -0700

    [LEMS-3971/vector-pr2] re-export

--- packages/perseus/src/widgets/interactive-graphs/types.ts
+++ packages/perseus/src/widgets/interactive-graphs/types.ts
@@ -92,7 +92,7 @@ export interface RayGraphState extends InteractiveGraphStateCommon {
     coords: PairOfPoints;
 }
 
-interface VectorGraphState extends InteractiveGraphStateCommon {
+export interface VectorGraphState extends InteractiveGraphStateCommon {
     type: "vector";
     coords: PairOfPoints;
 }
