a103935696f1d5144b3d6ea5492536bbc4f0fdc9
Ben Christel <benchristel@khanacademy.org>
2026-04-08 14:58:04 -0700

    [benc/tstyche] Delete unused summonParsedValue() test helper

--- packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/test-helpers.ts
+++ packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/test-helpers.ts
@@ -18,16 +18,12 @@ export function parseFailureWith(
     return failure([expect.objectContaining(expected)]);
 }
 
-// Summons a successfully parsed value out of thin air!*
-// *Terms and conditions apply. Value will not actually be returned.
-// For use in typetests only.
-export function summonParsedValue<P extends Parser<any>>(): ParsedValue<P> {
-    return "fake summoned value" as any;
-}
-
-// Summons a value of type T out of thin air!*
-// *Terms and conditions apply. Value will not actually be returned.
-// For use in typetests only.
+/**
+ * Summons a value of type T out of thin air!
+ *
+ * (Terms and conditions apply. Value will not actually be returned.
+ * For use in typetests only.)
+ */
 export function summon<T>(): T {
     return "fake summoned value" as any;
 }
