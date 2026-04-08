2b02d27760c0ae959100a80fae2a9c7dc009c858
Ben Christel <benchristel@users.noreply.github.com>
2026-04-08 15:29:58 -0700

    Remove unused exports from documentation (#3462)
    ## Summary:
    The hidden exports aren't used in the frontend codebase.
    
    Issue: LEMS-4014
    
    ## Test plan:
    
    View the docs website: https://khan.github.io/perseus/api-docs/index.html.
    The `perseus` package docs should only list useful symbols.
    
    Author: benchristel
    
    Reviewers: claude[bot], jeremywiebe
    
    Required Reviewers:
    
    Approved By: jeremywiebe
    
    Checks: ⏭️  1 check has been skipped, ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3462

--- packages/perseus/src/index.ts
+++ packages/perseus/src/index.ts
@@ -4,10 +4,15 @@
 
 export {default as init} from "./init";
 
-export {ApiOptions, ClassNames} from "./perseus-api";
+export {ApiOptions} from "./perseus-api";
+/** @hidden */
+export {ClassNames} from "./perseus-api";
 
+/** @hidden */
 export {libVersion} from "./version";
+/** @hidden */
 export {apiVersion} from "./perseus-version";
+/** @hidden */
 export {default as itemVersion} from "./item-version";
 
 /**
@@ -23,16 +28,22 @@ export {default as Renderer} from "./renderer";
  * Widgets
  */
 export * as Widgets from "./widgets";
+/** @hidden */
 export {default as widgets} from "./all-widgets";
 export {Expression} from "./widgets/expression";
 export {default as InputNumber} from "./widgets/input-number";
 export {default as NumericInput} from "./widgets/numeric-input";
 export {default as Radio} from "./widgets/radio";
 export {default as Categorizer} from "./widgets/categorizer";
+/** @hidden */
 export {default as InteractiveGraphWidget} from "./widgets/interactive-graphs/interactive-graph";
+/** @hidden */
 export {default as MatrixWidget} from "./widgets/matrix";
+/** @hidden */
 export {default as TableWidget} from "./widgets/table";
+/** @hidden */
 export {default as PlotterWidget} from "./widgets/plotter";
+/** @hidden */
 export {default as GrapherWidget} from "./widgets/grapher";
 
 // Some utils in grapher/utils and scoring don't need to be used outside of
@@ -45,6 +56,7 @@ import {
     typeToButton,
 } from "./widgets/grapher/util";
 
+/** @hidden */
 export const GrapherUtil = {
     DEFAULT_GRAPHER_PROPS,
     chooseType,
@@ -56,16 +68,23 @@ export const GrapherUtil = {
 /**
  * Misc
  */
+
+/** @hidden */
 export * as components from "./components";
+/** @hidden */
 export {iconChevronDown, iconTrash} from "./icon-paths";
+/** @hidden */
 export {bodyXsmallBold} from "./styles/global-styles";
 export * as Dependencies from "./dependencies";
+/** @hidden */
 export {Log} from "./logging/log";
 export {default as JiptParagraphs} from "./jipt-paragraphs";
 export {default as LoadingContext} from "./loading-context";
 export {default as MathRenderingContext} from "./math-rendering-context";
 export {default as PerseusMarkdown} from "./perseus-markdown";
+/** @hidden */
 export {interactiveSizes} from "./styles/constants";
+/** @hidden */
 export {displaySigFigs} from "./sigfigs";
 export {DependenciesContext} from "./dependencies";
 
@@ -73,40 +92,52 @@ export {DependenciesContext} from "./dependencies";
 export {
     PerseusI18nContextProvider,
     PerseusI18nContext,
-    usePerseusI18n,
 } from "./components/i18n-context";
+/** @hidden */
+export {usePerseusI18n} from "./components/i18n-context";
 
 /**
  * Util
  */
 export {default as Util} from "./util";
+/** @hidden */
 export {default as KhanColors} from "./util/colors";
+/** @hidden */
 export {default as preprocessTex} from "./util/tex-preprocess";
 export {registerAllWidgetsForTesting} from "./util/register-all-widgets-for-testing";
+/** @hidden */
 export {
     containerSizeClass,
     getInteractiveBoxFromSizeClass,
 } from "./util/sizing-utils";
+/** @hidden */
 export {mathOnlyParser} from "./widgets/interactive-graphs/utils";
 export {
     isWrongAnswerSupported,
     shouldHaveIndividualAnswer,
-    isWidgetIdInContent,
-    getValidWidgetIds,
     getCorrectAnswerForWidgetId,
     getAnswerFromUserInput,
     getImagesWithoutAltData,
+} from "./util/extract-perseus-data";
+/** @hidden */
+export {
+    isWidgetIdInContent,
+    getValidWidgetIds,
 } from "./util/extract-perseus-data";
 
 export {generateTestCategorizerWidget} from "./util/test-utils";
 export {
     getWidgetTypeByWidgetId,
     getWidgetSubTypeByWidgetId,
+} from "./widget-type-utils";
+/** @hidden */
+export {
     contentHasWidgetType,
     getWidgetFromWidgetMap,
     getWidgetsFromWidgetMap,
 } from "./widget-type-utils";
 export {convertWidgetNameToEnum} from "./util/widget-enum-utils";
+/** @hidden */
 export {
     getAbsoluteValueCoords,
     getCircleCoords,
@@ -128,19 +159,28 @@ export {
 } from "./user-input-manager";
 
 // These exports are to support shared functionality between Perseus and Graphie2000
+/** @hidden */
 export {parseDataFromJSONP} from "./util/graphie-utils";
 export type {
+    /** @hidden */
     GraphieData,
+    /** @hidden */
     GraphieLabel,
+    /** @hidden */
     GraphieRange,
 } from "./util/graphie-utils";
+/** @hidden */
 export {extractWidgetIds} from "./util/extract-widget-ids";
 
 /**
  * Mixins
  */
+
+/** @hidden */
 export * as Changeable from "./mixins/changeable";
+/** @hidden */
 export {default as EditorJsonify} from "./mixins/editor-jsonify";
+/** @hidden */
 export {excludeDenylistKeys} from "./mixins/widget-prop-denylist";
 
 /**
@@ -150,26 +190,43 @@ export type {ILogger, LogErrorOptions} from "./logging/log";
 export type {ServerItemRenderer as ServerItemRendererComponent} from "./server-item-renderer";
 export type {
     APIOptions,
+    /** @hidden */
     APIOptionsWithDefaults,
+    /** @hidden */
     ChangeHandler,
+    /** @hidden */
     DeviceType,
+    /** @hidden */
     DomInsertCheckFn,
+    /** @hidden */
     EditorMode,
+    /** @hidden */
     FocusPath,
     GenerateUrlArgs,
+    /** @hidden */
     GraphieLabelElement,
+    /** @hidden */
     ImageDict,
+    /** @hidden */
     ImageUploader,
+    /** @hidden */
     JiptLabelStore,
+    /** @hidden */
     JiptRenderer,
     PerseusDependencies,
     PerseusDependenciesV2,
+    /** @hidden */
     VideoData,
+    /** @hidden */
     VideoKind,
+    /** @hidden */
     WidgetExports,
+    /** @hidden */
     SharedRendererProps,
 } from "./types";
+/** @hidden */
 export type {ParsedValue} from "./util";
+/** @hidden */
 export type {Coord} from "./interactive2/types";
 export type {
     RendererPromptJSON,
