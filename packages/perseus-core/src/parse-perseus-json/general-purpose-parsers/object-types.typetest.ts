184fdeba44649b60c1a5c271216588298f106aa8
Ben Christel <benchristel@khanacademy.org>
2026-04-09 08:04:27 -0700

    [benc/tstyche] Remove unnecessary summon()s

--- packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/object-types.typetest.ts
+++ packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/object-types.typetest.ts
@@ -1,7 +1,5 @@
 import {describe, it, expect} from "tstyche";
 
-import {summon} from "./test-helpers";
-
 import type {OptionalizeProperties, OptionalKeysOf} from "./object-types";
 
 describe("OptionalizeProperties", () => {
@@ -20,38 +18,38 @@ describe("OptionalizeProperties", () => {
             req2: number;
         };
 
-        expect(summon<OptionalizeProperties<Obj>>()).type.toBe<Optionalized>();
+        expect<OptionalizeProperties<Obj>>().type.toBe<Optionalized>();
     });
 });
 
 describe("OptionalKeysOf", () => {
     it("returns no keys given an empty object type", () => {
         type NoKeys = object;
-        expect(summon<OptionalKeysOf<NoKeys>>()).type.toBe<never>();
+        expect<OptionalKeysOf<NoKeys>>().type.toBe<never>();
     });
 
     it("returns no keys when all are required", () => {
         type AllRequired = {a: 1; b: 2};
-        expect(summon<OptionalKeysOf<AllRequired>>()).type.toBe<never>();
+        expect<OptionalKeysOf<AllRequired>>().type.toBe<never>();
     });
 
     it("returns all keys when all are optional", () => {
         type AllOptional = {a?: 1; b?: 2};
-        expect(summon<OptionalKeysOf<AllOptional>>()).type.toBe<"a" | "b">();
+        expect<OptionalKeysOf<AllOptional>>().type.toBe<"a" | "b">();
     });
 
     it("treats keys as optional if they can be undefined", () => {
         type AMayBeUndefined = {a: 1 | undefined};
-        expect(summon<OptionalKeysOf<AMayBeUndefined>>()).type.toBe<"a">();
+        expect<OptionalKeysOf<AMayBeUndefined>>().type.toBe<"a">();
     });
 
     it("does not treat keys as optional if they can be null but not undefined", () => {
         type AMayBeNull = {a: 1 | null};
-        expect(summon<OptionalKeysOf<AMayBeNull>>()).type.toBe<never>();
+        expect<OptionalKeysOf<AMayBeNull>>().type.toBe<never>();
     });
 
     it("handles objects with both required and optional properties", () => {
         type AAndBOptional = {a?: 1; b?: 1; c: 1};
-        expect(summon<OptionalKeysOf<AAndBOptional>>()).type.toBe<"a" | "b">();
+        expect<OptionalKeysOf<AAndBOptional>>().type.toBe<"a" | "b">();
     });
 });
