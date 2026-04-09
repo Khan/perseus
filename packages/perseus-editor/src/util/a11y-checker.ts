e2df116e4fb35e71f0894d1b39070e023708d24b
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2026-04-09 11:15:12 -0700

    [reduce-noise-from-axe-core-console-logs] Add a flag to the runAxeCore function that indicates whether to output the axe-core information to the console, based upon the toggle button status.

--- packages/perseus-editor/src/util/a11y-checker.ts
+++ packages/perseus-editor/src/util/a11y-checker.ts
@@ -131,7 +131,10 @@ const mapResultsToIssues = (
     });
 };
 
-const runAxeCore = (updateIssuesFn: (issues: Issue[]) => void): void => {
+const runAxeCore = (
+    updateIssuesFn: (issues: Issue[]) => void,
+    logToConsole = false,
+): void => {
     const isInStorybook = !!document.getElementById("storybook-root");
     if (!isInStorybook) {
         let frameHasLoaded = false;
@@ -150,8 +153,10 @@ const runAxeCore = (updateIssuesFn: (issues: Issue[]) => void): void => {
     const options = isInStorybook
         ? axeCoreStorybookOptions
         : axeCoreEditorOptions;
-    // eslint-disable-next-line no-console
-    console.log("Axe Core options: ", options);
+    if (logToConsole) {
+        // eslint-disable-next-line no-console
+        console.log("Axe Core options: ", options);
+    }
     const previewWindow = document.querySelector(
         previewIframeSelector,
     ) as HTMLIFrameElement | null;
@@ -162,8 +167,10 @@ const runAxeCore = (updateIssuesFn: (issues: Issue[]) => void): void => {
     axeCoreProper.configure({reporter: "v2"});
     axeCoreProper.run(options).then(
         (results) => {
-            // eslint-disable-next-line no-console
-            console.log(`Accessibility Results: `, results);
+            if (logToConsole) {
+                // eslint-disable-next-line no-console
+                console.log(`Accessibility Results: `, results);
+            }
             const violations = mapResultsToIssues(
                 results.violations,
                 "Alert",
@@ -175,26 +182,34 @@ const runAxeCore = (updateIssuesFn: (issues: Issue[]) => void): void => {
                 isInStorybook ? null : previewWindow,
             );
             const issues = violations.concat(incompletes);
-            // eslint-disable-next-line no-console
-            console.log(`  Issues: `, issues);
+            if (logToConsole) {
+                // eslint-disable-next-line no-console
+                console.log(`  Issues: `, issues);
+            }
             if (
                 violations.length === 0 &&
                 incompletes.length === 0 &&
                 results.passes.length === 0
             ) {
-                setTimeout(runAxeCore, 1500, updateIssuesFn); // No valid results indicates that content may not be fully loaded yet
+                setTimeout(runAxeCore, 1500, updateIssuesFn, logToConsole); // No valid results indicates that content may not be fully loaded yet
             } else {
                 updateIssuesFn(issues);
             }
         },
         (error) => {
-            // eslint-disable-next-line no-console
-            console.log(`*** Error: `, error);
+            if (logToConsole) {
+                // eslint-disable-next-line no-console
+                console.log(`*** Error: `, error);
+            }
         },
     );
 };
 
-export const runAxeCoreOnUpdate = (priorTimeoutId, setState): any => {
+export const runAxeCoreOnUpdate = (
+    priorTimeoutId,
+    setState,
+    logToConsole: boolean,
+): any => {
     clearTimeout(priorTimeoutId);
-    return setTimeout(runAxeCore, 1500, setState);
+    return setTimeout(runAxeCore, 1500, setState, logToConsole);
 };
