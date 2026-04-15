fcba8840f063f1c09c45cb52ae61104cf53bd66d
Sarah Third <sarahthird@gmail.com>
2026-04-15 11:21:57 -0700

    [back-up-plan-lisa-needs-basis] format

--- packages/perseus-linter/src/rules/numeric-input-missing-label.test.ts
+++ packages/perseus-linter/src/rules/numeric-input-missing-label.test.ts
@@ -4,49 +4,37 @@ import numericInputMissingLabelRule from "./numeric-input-missing-label";
 
 describe("numeric-input-missing-label", () => {
     // Warning when labelText is empty
-    expectWarning(
-        numericInputMissingLabelRule,
-        "[[☃ numeric-input 1]]",
-        {
-            widgets: {
-                "numeric-input 1": {
-                    options: {
-                        answers: [{value: 42}],
-                        labelText: "",
-                    },
+    expectWarning(numericInputMissingLabelRule, "[[☃ numeric-input 1]]", {
+        widgets: {
+            "numeric-input 1": {
+                options: {
+                    answers: [{value: 42}],
+                    labelText: "",
                 },
             },
         },
-    );
+    });
 
     // Warning when labelText is undefined
-    expectWarning(
-        numericInputMissingLabelRule,
-        "[[☃ numeric-input 1]]",
-        {
-            widgets: {
-                "numeric-input 1": {
-                    options: {
-                        answers: [{value: 42}],
-                    },
+    expectWarning(numericInputMissingLabelRule, "[[☃ numeric-input 1]]", {
+        widgets: {
+            "numeric-input 1": {
+                options: {
+                    answers: [{value: 42}],
                 },
             },
         },
-    );
+    });
 
     // Pass when labelText is set
-    expectPass(
-        numericInputMissingLabelRule,
-        "[[☃ numeric-input 1]]",
-        {
-            widgets: {
-                "numeric-input 1": {
-                    options: {
-                        answers: [{value: 42}],
-                        labelText: "What's the answer?",
-                    },
+    expectPass(numericInputMissingLabelRule, "[[☃ numeric-input 1]]", {
+        widgets: {
+            "numeric-input 1": {
+                options: {
+                    answers: [{value: 42}],
+                    labelText: "What's the answer?",
                 },
             },
         },
-    );
+    });
 });
