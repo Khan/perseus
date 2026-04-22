d2fc3079303e3c5739d58a2ca908e08d2ee77fe5
Ben Christel <benchristel@khanacademy.org>
2026-04-22 07:56:10 -0700

    [benc/fixc-filter-unknown] Make `pnpm fixc` filter out unrecognized file types

--- utils/changed-files.sh
+++ utils/changed-files.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 
-# Prints a null-separated list of files changed since the commit referred to by
-# $UPSTREAM, or since the current branch's upstream if $UPSTREAM is empty or
-# not set. Untracked files are included.
+# Prints a newline-separated list of files changed since the commit referred
+# to by $UPSTREAM, which defaults to the current branch's upstream.
 
 if [ -z "${UPSTREAM}" ]; then
     if ! git rev-parse --abbrev-ref '@{u}' > /dev/null 2>&1; then
@@ -12,5 +11,5 @@ if [ -z "${UPSTREAM}" ]; then
     fi
 fi
 
-git ls-files --others --exclude-standard -z
-git diff -z --name-only --diff-filter=ACMRTUB "${UPSTREAM:-@{u}}"
+git ls-files --others --exclude-standard
+git diff --name-only --diff-filter=ACMRTUB "${UPSTREAM:-@{u}}"
---
+++ utils/fix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Reads a list of files from standard input and fixes lint and formatting in
+# each one.
+
+files="$(cat)"
+
+# We use `tr` and `xargs -0` to operate on null-separated filenames. This
+# ensures that xargs won't misinterpret filenames with spaces as multiple
+# files.
+echo "$files" \
+    | grep -E "\.(js|jsx|cjs|mjs|ts|tsx|cts|mts)$" \
+    | tr '\n' '\0' \
+    | xargs -r -0 pnpm eslint --fix
+
+echo "$files" \
+    | tr '\n' '\0' \
+    | xargs -r -0 pnpm prettier --ignore-unknown --write
