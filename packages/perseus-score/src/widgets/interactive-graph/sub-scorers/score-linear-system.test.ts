6392a03f55548209add529d3b8b5d01ab4be8830
Matthew Curtis <matthewcurtis@khanacademy.org>
2026-05-01 12:28:15 -0500

    [break-up-ig-scorer] implement linear-system

---
+++ packages/perseus-score/src/widgets/interactive-graph/sub-scorers/score-linear-system.test.ts
@@ -0,0 +1,146 @@
+import {scoreLinearSystem} from "./score-linear-system";
+
+import type {PerseusGraphTypeLinearSystem} from "@khanacademy/perseus-core";
+
+describe("scoreLinearSystem", () => {
+    it("returns invalid score when missing user input coords", () => {
+        const userInput: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+        };
+        const rubric: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+            coords: [
+                [
+                    [-1, -1],
+                    [1, 1],
+                ],
+                [
+                    [-1, 1],
+                    [1, -1],
+                ],
+            ],
+        };
+
+        expect(scoreLinearSystem(userInput, rubric)).toHaveInvalidInput();
+    });
+
+    it("returns invalid score when missing rubric coords", () => {
+        const userInput: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+            coords: [
+                [
+                    [-1, -1],
+                    [1, 1],
+                ],
+                [
+                    [-1, 1],
+                    [1, -1],
+                ],
+            ],
+        };
+        const rubric: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+        };
+
+        expect(scoreLinearSystem(userInput, rubric)).toHaveInvalidInput();
+    });
+
+    it("returns correct score when lines match in the same order", () => {
+        const userInput: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+            coords: [
+                [
+                    [-1, -1],
+                    [1, 1],
+                ],
+                [
+                    [-1, 1],
+                    [1, -1],
+                ],
+            ],
+        };
+        const rubric: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+            coords: [
+                [
+                    [-1, -1],
+                    [1, 1],
+                ],
+                [
+                    [-1, 1],
+                    [1, -1],
+                ],
+            ],
+        };
+
+        expect(
+            scoreLinearSystem(userInput, rubric),
+        ).toHaveBeenAnsweredCorrectly();
+    });
+
+    it("returns correct score when lines match in swapped order", () => {
+        const userInput: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+            coords: [
+                [
+                    [-1, 1],
+                    [1, -1],
+                ],
+                [
+                    [-1, -1],
+                    [1, 1],
+                ],
+            ],
+        };
+        const rubric: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+            coords: [
+                [
+                    [-1, -1],
+                    [1, 1],
+                ],
+                [
+                    [-1, 1],
+                    [1, -1],
+                ],
+            ],
+        };
+
+        expect(
+            scoreLinearSystem(userInput, rubric),
+        ).toHaveBeenAnsweredCorrectly();
+    });
+
+    it("returns incorrect score when lines do not match", () => {
+        const userInput: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+            coords: [
+                [
+                    [0, 0],
+                    [1, 2],
+                ],
+                [
+                    [0, 0],
+                    [2, 1],
+                ],
+            ],
+        };
+        const rubric: PerseusGraphTypeLinearSystem = {
+            type: "linear-system",
+            coords: [
+                [
+                    [-1, -1],
+                    [1, 1],
+                ],
+                [
+                    [-1, 1],
+                    [1, -1],
+                ],
+            ],
+        };
+
+        expect(
+            scoreLinearSystem(userInput, rubric),
+        ).toHaveBeenAnsweredIncorrectly();
+    });
+});
