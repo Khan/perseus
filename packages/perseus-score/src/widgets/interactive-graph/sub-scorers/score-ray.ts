e78cf3fa96474168aaed8711323aeabe2f0c135c
Matthew Curtis <matthewcurtis@khanacademy.org>
2026-05-01 14:13:51 -0500

    [ig-scorer/ray] copy ray scorer out

---
+++ packages/perseus-score/src/widgets/interactive-graph/sub-scorers/score-ray.ts
@@ -0,0 +1,29 @@
+import {geometry} from "@khanacademy/kmath";
+import {approximateDeepEqual} from "@khanacademy/perseus-core";
+
+const {collinear} = geometry;
+
+import type {PerseusGraphTypeRay, PerseusScore} from "@khanacademy/perseus-core";
+
+export function scoreRay(
+    userInput: PerseusGraphTypeRay,
+    rubric: PerseusGraphTypeRay,
+): PerseusScore {
+    if (!userInput.coords || !rubric.coords) {
+        return {type: "invalid", message: null};
+    }
+
+    const guess = userInput.coords;
+    const correct = rubric.coords;
+
+    const isCorrect =
+        approximateDeepEqual(guess[0], correct[0]) &&
+        collinear(correct[0], correct[1], guess[1]);
+
+    return {
+        type: "points",
+        earned: isCorrect ? 1 : 0,
+        total: 1,
+        message: null,
+    };
+}
