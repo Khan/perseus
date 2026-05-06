74cc84e5e5bf589fd3c5407b0b3894eb9393d3e6
Ben Christel <benchristel@khanacademy.org>
2026-05-06 15:32:15 -0700

    [benc/no-nan] Remove import

--- packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/index.ts
+++ packages/perseus-core/src/parse-perseus-json/general-purpose-parsers/index.ts
@@ -6,7 +6,6 @@ export * from "./defaulted";
 export * from "./enumeration";
 export * from "./is-plain-object";
 export * from "./nullable";
-export * from "./not-a-number";
 export * from "./number";
 export * from "./object";
 export * from "./optional";
