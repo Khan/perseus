f5b70fe19ee044e4437ac917eab9c4c4dfc89c2b
Ben Christel <benchristel@khanacademy.org>
2026-05-07 08:32:42 -0700

    [benc/convert-options] Add function to convert input number options to numeric input options

---
+++ packages/perseus-core/src/widgets/input-number/to-numeric-input.ts
@@ -0,0 +1,54 @@
+import type {
+    MathFormat,
+    PerseusInputNumberAnswerType,
+    PerseusInputNumberWidgetOptions,
+    PerseusNumericInputWidgetOptions,
+} from "../../data-schema";
+
+// NOTE: the information in mathFormatsForAnswerType is duplicated in
+// score-input-number.ts. I think this is okay because inputNumber is
+// deprecated and the scoring logic will be removed as part of this project.
+export const mathFormatsForAnswerType: Record<
+    PerseusInputNumberAnswerType,
+    MathFormat[]
+> = {
+    number: ["integer", "decimal", "proper", "improper", "mixed"],
+    decimal: ["decimal"],
+    integer: ["integer"],
+    rational: ["integer", "proper", "improper", "mixed"],
+    improper: ["integer", "proper", "improper"],
+    mixed: ["integer", "proper", "mixed"],
+    percent: ["integer", "decimal", "proper", "improper", "mixed", "percent"],
+    pi: ["pi"],
+};
+
+export function convertInputNumberOptionsToNumericInput(
+    inputNumberOptions: PerseusInputNumberWidgetOptions,
+): PerseusNumericInputWidgetOptions {
+    const maxError =
+        inputNumberOptions.maxError != null
+            ? Number(inputNumberOptions.maxError)
+            : inputNumberOptions.maxError;
+
+    const answerForms: MathFormat[] = inputNumberOptions.answerType
+        ? mathFormatsForAnswerType[inputNumberOptions.answerType]
+        : ["integer", "proper", "improper", "mixed", "decimal"];
+
+    return {
+        coefficient: false,
+        rightAlign: inputNumberOptions.rightAlign,
+        size: inputNumberOptions.size,
+        static: false,
+        answers: [
+            {
+                status: "correct",
+                value: Number(inputNumberOptions.value),
+                simplify: inputNumberOptions.simplify,
+                message: "",
+                maxError,
+                strict: true,
+                answerForms,
+            },
+        ],
+    };
+}
