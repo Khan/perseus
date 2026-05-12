1df1a90c2518f5caf7f303a6a2a67f47db73b0c2
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2026-05-12 14:51:52 -0700

    [LEMS-4102-renderer-regression-stories] Update regression test to include headers.

--- packages/perseus/src/__docs__/renderer-initial-state-regression.stories.tsx
+++ packages/perseus/src/__docs__/renderer-initial-state-regression.stories.tsx
@@ -36,9 +36,15 @@ const RenderContent = (content: string): (() => React.JSX.Element) => {
 
 export const RegularText: Story = {
     render: RenderContent(
-        `I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of **instantly** interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.
+        `# Sensors and Scanners
+
+I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of **instantly** interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.
+
+## Warp Core Dynamics
 
 Exceeding reaction chamber thermal limit. We have begun power-supply calibration. Force fields have been established on all turbo lifts and crawlways. Computer, run a level-two diagnostic on warp-drive systems. Antimatter containment positive. Warp drive within normal parameters. I read an ion trail characteristic of a freighter escape pod. The bomb had a molecular-decay detonator. Detecting some unusual fluctuations in subspace frequencies.
+
+### Alien Life Forms
 
 Cmdr Riker's nervous system has been invaded by an *unknown microorganism*. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.
 `,
