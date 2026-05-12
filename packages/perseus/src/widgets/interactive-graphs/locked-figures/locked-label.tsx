d9a9eda247176a467080a6df68db8e3efbc995a5
Matthew Curtis <matthewcurtis@khanacademy.org>
2026-05-12 10:42:08 -0500

    [LEMS-4106/label-outlines] Outline Locked Labels with SVG filter

--- packages/perseus/src/widgets/interactive-graphs/locked-figures/locked-label.tsx
+++ packages/perseus/src/widgets/interactive-graphs/locked-figures/locked-label.tsx
@@ -2,7 +2,7 @@ import {
     lockedFigureColors,
     type LockedLabelType,
 } from "@khanacademy/perseus-core";
-import {font} from "@khanacademy/wonder-blocks-tokens";
+import {font, semanticColor} from "@khanacademy/wonder-blocks-tokens";
 import * as React from "react";
 
 import {getDependencies} from "../../../dependencies";
@@ -19,19 +19,75 @@ export default function LockedLabel(props: LockedLabelType) {
 
     // Note: The TeX component cannot be rendered within an SVG
     return (
-        <span
-            className="locked-label"
-            style={{
-                position: "absolute",
-                left: x,
-                top: y,
-                color: lockedFigureColors[color],
-                fontSize: font.size[size],
-                backgroundColor: "rgba(255, 255, 255, 0.8)",
-            }}
-            aria-hidden={true}
-        >
-            <TeX>{replaceOutsideTeX(text)}</TeX>
-        </span>
+        <>
+            <LockedLabelStrokeFilter />
+            <span
+                className="locked-label"
+                style={{
+                    position: "absolute",
+                    left: x,
+                    top: y,
+                    color: lockedFigureColors[color],
+                    fontSize: font.size[size],
+                    filter: "url(#math-stroke)",
+                }}
+                aria-hidden={true}
+            >
+                <TeX>{replaceOutsideTeX(text)}</TeX>
+            </span>
+        </>
+    );
+}
+
+/**
+ * This helps us create a stroke around locked labels
+ * which isn't as obvious as it would seem because Mathjax
+ * renders HTML and uses borders for some lines.
+ *
+ * This creates a composite of the rendered output and
+ * thickens the output.
+ *
+ * See: LEMS-4106 for more information
+ */
+function LockedLabelStrokeFilter() {
+    const color = semanticColor.core.border.knockout.default;
+
+    return (
+        <svg width="0" height="0" style={{position: "absolute"}}>
+            <defs>
+                <filter
+                    id="math-stroke"
+                    x="-5%"
+                    y="-5%"
+                    width="110%"
+                    height="110%"
+                >
+                    {/* expand edges outward */}
+                    <feMorphology
+                        operator="dilate"
+                        radius="2"
+                        in="SourceGraphic"
+                        result="expanded"
+                    />
+
+                    {/* flood with color */}
+                    <feFlood floodColor={color} result={color} />
+
+                    {/* clip the flood to the expanded shape */}
+                    <feComposite
+                        in={color}
+                        in2="expanded"
+                        operator="in"
+                        result="outline"
+                    />
+
+                    {/* draw outline behind original */}
+                    <feMerge>
+                        <feMergeNode in="outline" />
+                        <feMergeNode in="SourceGraphic" />
+                    </feMerge>
+                </filter>
+            </defs>
+        </svg>
     );
 }
