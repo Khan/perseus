1a7796fb626834f3f1f4f40c7feba543530b45c9
Ben Christel <benchristel@users.noreply.github.com>
2026-05-14 14:34:30 -0700

    Update `convertInputNumberOptionsToNumericInput` to account for the `inexact` widget option (#3629)
    ## Summary:
    This PR fixes scoring mismatches found in side-by-side testing.  When `inexact`
    is undefined or false for an input-number widget, the scoring logic changes in
    two ways:
    
    - we force `maxError` to 0.
    - decimal answers are rounded to 9 decimal places.
    
    We can invoke the same logic for a numeric-input widget by setting maxError to
    zero.
    
    This PR also adds data-driven regression tests that do essentially what the
    side-by-side testing in production does. The plan is: any new side-by-side
    errors we see get copied to this test suite. The test should fail initially.
    When the test passes, it means the side-by-side error is fixed.
    
    Issue: LEMS-4110
    
    ## Test plan:
    
    Deploy to the perseus service. Side-by-side testing errors should decrease by
    at least 90%.
    
    Author: benchristel
    
    Reviewers: claude[bot], anakaren-rojas, benchristel
    
    Required Reviewers:
    
    Approved By: anakaren-rojas
    
    Checks: ✅ 12 checks were successful, ⏭️  1 check has been skipped
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3629

---
+++ packages/perseus-score/src/input-number-to-numeric-input-regression.test.ts
@@ -0,0 +1,125 @@
+import {describe, expect, test} from "@jest/globals";
+
+import type {PerseusWidgetsMap, UserInputMap} from "@khanacademy/perseus-core";
+
+import {
+    scorePerseusItem,
+    scorePerseusItemWithInputNumberAsNumericInput,
+} from "./index";
+
+interface TestCase {
+    title: string;
+    userInput: UserInputMap;
+    widgets: PerseusWidgetsMap;
+}
+
+// NOTE: To add a test case to this list:
+// - Go to https://console.cloud.google.com/logs
+// - Search for "side-by-side" in the Perseus service
+// - select "Copy > Copy as JSON"
+// - `pbpaste | jq '.jsonPayload.metadata | {userInput: .inputNumberUserInputs, widgets: .inputNumberWidgets}'`
+// TODO(LEMS-4085): Delete this test file.
+const cases: TestCase[] = [
+    {
+        title: "when inexact = false",
+        userInput: {
+            "input-number 1": {
+                currentValue: "6.336",
+            },
+            "input-number 2": {
+                currentValue: "6.308",
+            },
+        },
+        widgets: {
+            "input-number 1": {
+                options: {
+                    value: 6.338,
+                    answerType: "number",
+                    rightAlign: false,
+                    maxError: 0.1,
+                    size: "normal",
+                    simplify: "required",
+                    inexact: false,
+                },
+                graded: true,
+                static: false,
+                version: {
+                    minor: 0,
+                    major: 0,
+                },
+                type: "input-number",
+                alignment: "default",
+            },
+            "input-number 2": {
+                type: "input-number",
+                version: {
+                    major: 0,
+                    minor: 0,
+                },
+                static: false,
+                alignment: "default",
+                options: {
+                    rightAlign: false,
+                    maxError: 0.1,
+                    size: "normal",
+                    inexact: false,
+                    simplify: "required",
+                    value: 6.34,
+                    answerType: "number",
+                },
+                graded: true,
+            },
+        },
+    },
+    {
+        title: "when inexact = false and the correct answer has more than 9 decimal places",
+        userInput: {
+            "input-number 1": {
+                currentValue: "1.123456789",
+            },
+        },
+        widgets: {
+            "input-number 1": {
+                type: "input-number",
+                version: {
+                    major: 0,
+                    minor: 0,
+                },
+                static: false,
+                alignment: "default",
+                options: {
+                    rightAlign: false,
+                    maxError: 0.1,
+                    size: "normal",
+                    inexact: false,
+                    simplify: "required",
+                    // rounds to 1.123456789
+                    value: 1.1234567885,
+                    answerType: "number",
+                },
+                graded: true,
+            },
+        },
+    },
+];
+
+describe("scoring with input-number converted to numeric-input", () => {
+    test.each(cases)("$title", ({widgets, userInput}) => {
+        const content = Object.keys(widgets)
+            .map((id) => `[[\u2603 ${id}]]`)
+            .join("");
+
+        const scoringArgs: Parameters<typeof scorePerseusItem> = [
+            {content, widgets, images: {}},
+            userInput,
+            "en",
+        ];
+
+        const officialScore = scorePerseusItem(...scoringArgs);
+        const inniScore = scorePerseusItemWithInputNumberAsNumericInput(
+            ...scoringArgs,
+        );
+
+        expect(inniScore).toEqual(officialScore);
+    });
+});
