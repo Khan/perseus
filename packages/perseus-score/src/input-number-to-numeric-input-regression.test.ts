6e3c5d1212bb795d761a81303afff4c12b4b8e7f
Ben Christel <benchristel@khanacademy.org>
2026-05-18 15:02:02 -0700

    [benc/sxs-round-2] Add failing regression test

--- packages/perseus-score/src/input-number-to-numeric-input-regression.test.ts
+++ packages/perseus-score/src/input-number-to-numeric-input-regression.test.ts
@@ -101,6 +101,73 @@ const cases: TestCase[] = [
             },
         },
     },
+    {
+        title: "when the user inputs long decimals",
+        userInput: {
+            "input-number 1": {
+                currentValue: "0.7692307692307693",
+            },
+            "input-number 2": {
+                currentValue: "0.3076923076923077",
+            },
+            "input-number 3": {
+                currentValue: "0.2692307692307692",
+            },
+            "input-number 4": {
+                currentValue: "0.8076923076923077",
+            },
+        },
+        widgets: {
+            "input-number 1": {
+                options: {
+                    maxError: 0.031,
+                    simplify: "optional",
+                    answerType: "number",
+                    inexact: false,
+                    size: "normal",
+                    value: 0.7692307692307693,
+                },
+                graded: true,
+                type: "input-number",
+            },
+            "input-number 2": {
+                graded: true,
+                options: {
+                    value: 0.3076923076923077,
+                    simplify: "optional",
+                    maxError: 0.0077,
+                    answerType: "number",
+                    size: "normal",
+                    inexact: false,
+                },
+                type: "input-number",
+            },
+            "input-number 3": {
+                type: "input-number",
+                graded: true,
+                options: {
+                    value: 0.2692307692307692,
+                    inexact: false,
+                    size: "normal",
+                    maxError: 0.031,
+                    simplify: "optional",
+                    answerType: "number",
+                },
+            },
+            "input-number 4": {
+                type: "input-number",
+                options: {
+                    inexact: false,
+                    size: "normal",
+                    maxError: 0.0077,
+                    simplify: "optional",
+                    answerType: "number",
+                    value: 0.8076923076923077,
+                },
+                graded: true,
+            },
+        },
+    },
 ];
 
 describe("scoring with input-number converted to numeric-input", () => {
