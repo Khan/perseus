33e55752df5dcec46ed53f73fa85d07d96ba6cce
Ben Christel <benchristel@users.noreply.github.com>
2026-05-20 14:06:06 -0700

    Disallow decimal input when input-number answer has > 10 decimal places (#3639)
    ## Summary:
    When scoring input-number widgets using the numeric-input logic, we now
    disallow decimal and integer input under the following conditions: `answerType`
    is `number` or not set, `inexact` is not `true`, and the correct answer `value`
    has more than 10 decimal places. This ensures that learners are not
    approximating a rational answer with a decimal. It preserves the existing
    scoring behavior of input-number.
    
    This fixes a side-by-side test mismatch. I added a regression test to reproduce
    the mismatch observed in production.
    
    See this doc for more info: https://khanacademy.atlassian.net/wiki/spaces/LC/pages/4767219957/Side-by-side+test+failure+analysis+2026-05-18
    
    Issue: LEMS-4110
    
    ## Test plan:
    
    Deploy and watch the perseus service logs. Side-by-side errors should decrease.
    
    Author: benchristel
    
    Reviewers: SonicScrewdriver, anakaren-rojas
    
    Required Reviewers:
    
    Approved By: SonicScrewdriver
    
    Checks: ✅ 12 checks were successful, ⏭️  1 check has been skipped
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3639

--- packages/perseus-core/src/widgets/input-number/to-numeric-input.test.ts
+++ packages/perseus-core/src/widgets/input-number/to-numeric-input.test.ts
@@ -120,10 +120,10 @@ describe("convertInputNumberOptionsToNumericInput", () => {
         expect(result.answers).toHaveLength(1);
         expect(result.answers[0].answerForms).toEqual([
             "integer",
+            "decimal",
             "proper",
             "improper",
             "mixed",
-            "decimal",
         ]);
     });
 
@@ -137,5 +137,113 @@ describe("convertInputNumberOptionsToNumericInput", () => {
 
         expect(result.answers).toHaveLength(1);
         expect(result.answers[0].value).toEqual(1.5);
+    });
+
+    it("excludes `decimal` and `integer` from `answerForms` when answerType is number, inexact is false, and `value` has more than 10 decimal places", () => {
+        const options: PerseusInputNumberWidgetOptions = {
+            ...baseOptions,
+            answerType: "number",
+            inexact: false,
+            value: "1.12345678901",
+        };
+
+        const result = convertInputNumberOptionsToNumericInput(options);
+        expect(result.answers).toHaveLength(1);
+        expect(result.answers[0].answerForms).toEqual([
+            "proper",
+            "improper",
+            "mixed",
+        ]);
+    });
+
+    it("excludes `decimal` and `integer` from `answerForms` when answerType is undefined, inexact is false, and `value` has more than 10 decimal places", () => {
+        const options: PerseusInputNumberWidgetOptions = {
+            ...baseOptions,
+            answerType: undefined,
+            inexact: false,
+            value: "1.12345678901",
+        };
+
+        const result = convertInputNumberOptionsToNumericInput(options);
+        expect(result.answers).toHaveLength(1);
+        expect(result.answers[0].answerForms).toEqual([
+            "proper",
+            "improper",
+            "mixed",
+        ]);
+    });
+
+    it("excludes `decimal` and `integer` from `answerForms` when inexact is undefined and `value` has more than 10 decimal places", () => {
+        const options: PerseusInputNumberWidgetOptions = {
+            ...baseOptions,
+            answerType: undefined,
+            inexact: undefined,
+            value: "1.12345678901",
+        };
+
+        const result = convertInputNumberOptionsToNumericInput(options);
+        expect(result.answers).toHaveLength(1);
+        expect(result.answers[0].answerForms).toEqual([
+            "proper",
+            "improper",
+            "mixed",
+        ]);
+    });
+
+    it("does not exclude `decimal` and `integer` from `answerForms` when inexact is true", () => {
+        const options: PerseusInputNumberWidgetOptions = {
+            ...baseOptions,
+            answerType: undefined,
+            inexact: true,
+            value: "1.12345678901",
+        };
+
+        const result = convertInputNumberOptionsToNumericInput(options);
+        expect(result.answers).toHaveLength(1);
+        expect(result.answers[0].answerForms).toEqual([
+            "integer",
+            "decimal",
+            "proper",
+            "improper",
+            "mixed",
+        ]);
+    });
+
+    it("does not exclude `decimal` and `integer` from `answerForms` when the answer has 10 decimal places", () => {
+        const options: PerseusInputNumberWidgetOptions = {
+            ...baseOptions,
+            answerType: undefined,
+            inexact: undefined,
+            value: "0.1231231234",
+        };
+
+        const result = convertInputNumberOptionsToNumericInput(options);
+        expect(result.answers).toHaveLength(1);
+        expect(result.answers[0].answerForms).toEqual([
+            "integer",
+            "decimal",
+            "proper",
+            "improper",
+            "mixed",
+        ]);
+    });
+
+    it("does not exclude `decimal` and `integer` from `answerForms` when the answer has fewer than 10 decimal places", () => {
+        const options: PerseusInputNumberWidgetOptions = {
+            ...baseOptions,
+            answerType: undefined,
+            inexact: undefined,
+            value: "0.3",
+        };
+
+        const result = convertInputNumberOptionsToNumericInput(options);
+        expect(result.answers).toHaveLength(1);
+        expect(result.answers[0].answerForms).toEqual([
+            "integer",
+            "decimal",
+            "proper",
+            "improper",
+            "mixed",
+        ]);
     });
 });
