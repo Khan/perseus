843dcd5520f7ba5c0749f221634b0205b60a95ef
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2026-05-22 14:12:58 -0700

    [LEMS-4102-renderer-regression-stories] Add regression stories for text in Articles.

--- packages/perseus/src/__docs__/renderer-initial-state-regression.stories.tsx
+++ packages/perseus/src/__docs__/renderer-initial-state-regression.stories.tsx
@@ -2,6 +2,12 @@ import {generateTestPerseusRenderer} from "@khanacademy/perseus-core";
 import * as React from "react";
 
 import {themeModes} from "../../../../.storybook/modes";
+import {
+    regularTextContent,
+    listsContent,
+} from "../__testdata__/renderer.testdata";
+import ArticleRenderer from "../article-renderer";
+import {storybookDependenciesV2} from "../testing/test-dependencies";
 import QuestionRendererForStories from "../widgets/__testutils__/question-renderer-for-stories";
 
 import type {Meta, StoryObj} from "@storybook/react-vite";
@@ -22,69 +28,47 @@ const meta: Meta = {
 };
 export default meta;
 
-const RenderContent = (content: string): (() => React.JSX.Element) => {
-    return function Render(){
-        return (
-            <QuestionRendererForStories
-                question={generateTestPerseusRenderer({
-                    content: content,
-                })}
-            />
-        );
+const RenderContent = (
+    content: string,
+    asArticle: boolean,
+): (() => React.JSX.Element) => {
+    return function Render() {
+        if (asArticle) {
+            const json = {
+                content,
+                widgets: {},
+                images: {},
+            };
+            return (
+                <ArticleRenderer
+                    json={json}
+                    dependencies={storybookDependenciesV2}
+                />
+            );
+        } else {
+            return (
+                <QuestionRendererForStories
+                    question={generateTestPerseusRenderer({
+                        content: content,
+                    })}
+                />
+            );
+        }
     };
 };
 
-export const RegularText: Story = {
-    render: RenderContent(
-        `# Sensors and Scanners
-
-I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of **instantly** interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.
-
-## Warp Core Dynamics
-
-Exceeding reaction chamber thermal limit. We have begun power-supply calibration. Force fields have been established on all turbo lifts and crawlways. Computer, run a level-two diagnostic on warp-drive systems. Antimatter containment positive. Warp drive within normal parameters. I read an ion trail characteristic of a freighter escape pod. The bomb had a molecular-decay detonator. Detecting some unusual fluctuations in subspace frequencies.
-
-### Alien Life Forms
-
-Cmdr Riker's nervous system has been invaded by an *unknown microorganism*. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.
-`,
-    ),
+export const ArticleRegularText: Story = {
+    render: RenderContent(regularTextContent, true),
 };
 
-export const Lists: Story = {
-    render: RenderContent(
-        `## Primary Federation ships involved in the tachyon blockade during the Klingon Civil War:
+export const ArticleLists: Story = {
+    render: RenderContent(listsContent, true),
+};
 
-1. USS Enterprise-D
-1. USS Excalibur
-1. USS Merrimack
-1. USS Sutherland
-1. USS Charleston
-1. USS Hood
-1. USS Tian An Men
+export const ExerciseRegularText: Story = {
+    render: RenderContent(regularTextContent, false),
+};
 
-## Other Federation ships involved in the blockade:
-
-- USS Ahwahnee
-- USS Apollo
-- USS Aries
-- USS Goddard
-- USS Thomas Paine
-- USS Trieste
-
-## Federation ships and their executive officers:
-
-- USS Enterprise-D
-
-   1. Captain Jean-Luc Picard
-   1. Commander William T. Riker
-- USS Defiant (DS9)
-
-   1. Captain Benjamin Sisko
-   1. Major Kira Nerys
-- USS Voyager
-
-   1. Captain Kathryn Janeway
-   1. Commander Chakotay`,
-    ),
+export const ExerciseLists: Story = {
+    render: RenderContent(listsContent, false),
 };
--- packages/perseus/src/__testdata__/renderer.testdata.ts
+++ packages/perseus/src/__testdata__/renderer.testdata.ts
@@ -197,3 +197,50 @@ export const mockedRandomItem: PerseusRenderer = {
         },
     },
 } as PerseusRenderer;
+
+export const regularTextContent = `# Sensors and Scanners
+
+I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of **instantly** interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.
+
+## Warp Core Dynamics
+
+Exceeding reaction chamber thermal limit. We have begun power-supply calibration. Force fields have been established on all turbo lifts and crawlways. Computer, run a level-two diagnostic on warp-drive systems. Antimatter containment positive. Warp drive within normal parameters. I read an ion trail characteristic of a freighter escape pod. The bomb had a molecular-decay detonator. Detecting some unusual fluctuations in subspace frequencies.
+
+### Alien Life Forms
+
+Cmdr Riker's nervous system has been invaded by an *unknown microorganism*. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.
+`;
+
+export const listsContent = `## Primary Federation ships involved in the tachyon blockade during the Klingon Civil War:
+
+1. USS Enterprise-D
+1. USS Excalibur
+1. USS Merrimack
+1. USS Sutherland
+1. USS Charleston
+1. USS Hood
+1. USS Tian An Men
+
+## Other Federation ships involved in the blockade:
+
+- USS Ahwahnee
+- USS Apollo
+- USS Aries
+- USS Goddard
+- USS Thomas Paine
+- USS Trieste
+
+## Federation ships and their executive officers:
+
+- USS Enterprise-D
+
+   1. Captain Jean-Luc Picard
+   1. Commander William T. Riker
+- USS Defiant (DS9)
+
+   1. Captain Benjamin Sisko
+   1. Major Kira Nerys
+- USS Voyager
+
+   1. Captain Kathryn Janeway
+   1. Commander Chakotay`;
