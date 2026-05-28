2973a052b8c28f645546ae3f68cce9d0bd710b76
Ben Christel <benchristel@khanacademy.org>
2026-05-28 14:39:58 -0700

    [benc/item-flipbook] Document view model

--- packages/perseus/src/testing/item-flipbook/item-flipbook-view-model.ts
+++ packages/perseus/src/testing/item-flipbook/item-flipbook-view-model.ts
@@ -4,6 +4,12 @@ export type NoItem = {status: "no-item"};
 export type ParseError = {status: "parse-error"; message: string};
 export type Item = {status: "item"; item: PerseusItem; key: string};
 
+/**
+ * The information displayed in the item display section of the UI. One of:
+ * - no item (if no data has been entered)
+ * - a parse error from the currently selected item
+ * - a valid item
+ */
 export type ItemDisplay = NoItem | ParseError | Item;
 
 export function noItem(): NoItem {
@@ -33,14 +39,25 @@ export interface InputField {
     onChange: (event: InputFieldChangeEvent) => void;
 }
 
+/**
+ * An abstract representation of the view. The ViewModel contains exactly the
+ * values that will be rendered in React components, but it has no dependency
+ * on React. This makes it easy to write tests that assert on exactly what the
+ * user will see, yet don't need to render components.
+ */
 export interface ViewModel {
+    /**
+     * Input where the user can type in newline-separated Perseus item JSON
+     * objects to render.
+     */
     itemJsonInput: InputField;
 
     /**
-     * The 1-indexed number of the selected item, formatted for display in an
-     * input box.
+     * Displays and controls which Perseus item from the `itemJsonInput` is
+     * currently selected for display.
      */
     selectedItemNumber: InputField;
+
     /** The number of items, formatted for display. */
     totalItems: string;
     nextItem: () => void;
