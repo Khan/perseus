34559517597d6da9e14860802eb3b400e06df3b0
Cat Johnson <catjohnson@khanacademy.org>
2026-05-29 14:46:49 -0700

    [catjohnson/gif-linter-warning] Added new legacy gif url linter warning for our perseus content editor.

---
+++ packages/perseus-linter/src/rules/legacy-gif-url.ts
@@ -0,0 +1,45 @@
+import Rule from "../rule";
+
+import {getHostname} from "./lint-utils";
+
+const LEGACY_HOSTNAME = "ka-perseus-images.s3.amazonaws.com";
+
+export default Rule.makeRule({
+    name: "legacy-gif-url",
+    severity: Rule.Severity.GUIDELINE,
+    selector: "widget",
+    lint: function (state, content, nodes, match, context) {
+        // This rule only looks at image widgets.
+        if (state.currentNode().widgetType !== "image") {
+            return;
+        }
+
+        const nodeId = state.currentNode().id;
+        if (!nodeId) {
+            return;
+        }
+
+        // If it can't find a definition for the widget it does nothing.
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        const widget = context && context.widgets && context.widgets[nodeId];
+        if (!widget) {
+            return;
+        }
+
+        const url = widget.options?.backgroundImage?.url;
+        if (!url) {
+            return;
+        }
+
+        // Legacy gifs hosted on the old S3 bucket don't render in the editor
+        // iframe, so we encourage content creators to use the CDN URL instead.
+        if (
+            getHostname(url) === LEGACY_HOSTNAME &&
+            url.toLowerCase().endsWith(".gif")
+        ) {
+            return `Legacy gif URL:
+gifs hosted at ${LEGACY_HOSTNAME} won't appear in the editor preview.
+Use a https://cdn.kastatic.org/ka-perseus-images/ URL instead.`;
+        }
+    },
+});
