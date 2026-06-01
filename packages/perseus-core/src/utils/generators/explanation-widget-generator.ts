4e5082eeff77583a596c7b5bd21c7fdbcd94208c
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2026-06-01 15:04:26 -0700

    [LEMS-4130-add-visual-regression-stories-for-explanation-widget] Restore comment in code.

--- packages/perseus-core/src/utils/generators/explanation-widget-generator.ts
+++ packages/perseus-core/src/utils/generators/explanation-widget-generator.ts
@@ -22,6 +22,9 @@ export function generateExplanationWidget(
         // Explanations are not graded
         graded: false,
         version: {major: 0, minor: 0},
+        // NOTE: The explanation widget doesn't consume this directly,
+        // instead, Perseus renders an overlay <div /> over top of the
+        // widget that intercepts interactions to it.
         static: false,
         alignment: "default",
         options: generateExplanationOptions(),
