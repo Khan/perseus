513f0f1858276a36a3941491b963d8cc11462365
Mark Fitzgerald <markfitzgerald@khanacademy.org>
2026-06-01 15:56:41 -0700

    [LEMS-4130-add-visual-regression-stories-for-explanation-widget] Restored standard Storybook file for hints renderer so that documentation is in place for that renderer.

---
+++ packages/perseus/src/__docs__/hints-renderer.stories.tsx
@@ -0,0 +1,66 @@
+import {
+    generateTestPerseusRenderer,
+    generateImageWidget,
+    generateImageOptions,
+} from "@khanacademy/perseus-core";
+import {View} from "@khanacademy/wonder-blocks-core";
+import React from "react";
+
+import {themeModes} from "../../../../.storybook/modes";
+import HintsRenderer from "../hints-renderer";
+import {storybookDependenciesV2} from "../testing/test-dependencies";
+import {earthMoonImage} from "../widgets/image/utils";
+
+import {bibliotronExerciseDecorator} from "./hints-renderer-decorator";
+
+import type {Meta, StoryObj} from "@storybook/react-vite";
+
+const meta: Meta<typeof HintsRenderer> = {
+    title: "Renderers/Hints Renderer",
+    component: HintsRenderer,
+    decorators: [
+        (Story) => {
+            return (
+                <View style={{paddingLeft: 80}}>
+                    <Story />
+                </View>
+            );
+        },
+    ],
+    parameters: {
+        docs: {
+            description: {
+                component: "Examples of Hint renderer.",
+            },
+        },
+        chromatic: {disableSnapshot: false, modes: themeModes},
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof HintsRenderer>;
+
+export const ImageWidgetInHint: Story = {
+    decorators: [bibliotronExerciseDecorator],
+    args: {
+        dependencies: storybookDependenciesV2,
+        hints: [
+            {
+                ...generateTestPerseusRenderer({
+                    content: "[[☃ image 1]]",
+                    widgets: {
+                        "image 1": generateImageWidget({
+                            options: generateImageOptions({
+                                backgroundImage: earthMoonImage,
+                                alt: "Earth and Moon",
+                                title: "Earth and Moon",
+                                caption: "Earth and Moon",
+                            }),
+                        }),
+                    },
+                }),
+            },
+        ],
+    },
+};
