ed3cb4503bb9fd56ef9a273351a56f2738a02d38
Tamara <tamara@khanacademy.org>
2026-06-02 10:45:28 -0500

    [tb/LEMS-3496/expression-colorsync] Update to use QuestionRendererForStories

--- packages/perseus/src/widgets/__testutils__/expression-renderer-decorator.tsx
+++ packages/perseus/src/widgets/__testutils__/expression-renderer-decorator.tsx
@@ -1,27 +1,24 @@
 import {
     generateExpressionOptions,
     generateExpressionWidget,
-    generateTestPerseusItem,
     generateTestPerseusRenderer,
 } from "@khanacademy/perseus-core";
 import * as React from "react";
 
-import {ServerItemRendererWithDebugUI} from "../../testing/server-item-renderer-with-debug-ui";
+import QuestionRendererForStories from "./question-renderer-for-stories";
 
 export const expressionRendererDecorator = (_, {args, parameters}) => {
     return (
-        <ServerItemRendererWithDebugUI
-            item={generateTestPerseusItem({
-                question: generateTestPerseusRenderer({
-                    content: parameters?.content ?? "[[☃ expression 1]]",
-                    widgets: {
-                        "expression 1": generateExpressionWidget({
-                            options: generateExpressionOptions({
-                                ...args,
-                            }),
+        <QuestionRendererForStories
+            question={generateTestPerseusRenderer({
+                content: parameters?.content ?? "[[☃ expression 1]]",
+                widgets: {
+                    "expression 1": generateExpressionWidget({
+                        options: generateExpressionOptions({
+                            ...args,
                         }),
-                    },
-                }),
+                    }),
+                },
             })}
             apiOptions={parameters?.apiOptions}
         />
