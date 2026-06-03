2fc16617f50511ff1db575983cd2c7385318018c
Sarah Third <sarahthird@gmail.com>
2026-06-03 16:20:16 -0700

    [and-now-for-your-feature-presentation] Cast list :)

--- packages/perseus/src/testing/feature-flags-context.tsx
+++ packages/perseus/src/testing/feature-flags-context.tsx
@@ -3,9 +3,12 @@ import * as React from "react";
 
 type PerseusFlags = Record<(typeof PerseusFeatureFlags)[number], boolean>;
 
+// We need to cast here in order to create
+// the correct type for this internal tool
+// eslint-disable-next-line no-restricted-syntax
 export const defaultFeatureFlags = Object.fromEntries(
     PerseusFeatureFlags.map((flag) => [flag, false]),
-) satisfies PerseusFlags;
+) as PerseusFlags;
 
 export const StorybookFeatureFlagsContext =
     React.createContext<PerseusFlags>(defaultFeatureFlags);
