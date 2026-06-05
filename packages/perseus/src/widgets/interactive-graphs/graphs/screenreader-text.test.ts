2d620ae8ee782a77e19ac76d7d913df5880c4794
Cat Johnson <catjohnson@khanacademy.org>
2026-06-05 15:28:09 -0700

    [catjohnson/wb-announcer-quad] fixing linting.

--- packages/perseus/src/widgets/interactive-graphs/graphs/screenreader-text.test.ts
+++ packages/perseus/src/widgets/interactive-graphs/graphs/screenreader-text.test.ts
@@ -828,14 +828,14 @@ describe("getPiMultiple", () => {
 
 describe("getCoordQuadrant", () => {
     test.each`
-        coord        | expected
-        ${[0, 0]}    | ${"origin"}
-        ${[3, 0]}    | ${"x-axis"}
-        ${[0, 3]}    | ${"y-axis"}
-        ${[2, 4]}    | ${1}
-        ${[-2, 4]}   | ${2}
-        ${[-2, -4]}  | ${3}
-        ${[2, -4]}   | ${4}
+        coord       | expected
+        ${[0, 0]}   | ${"origin"}
+        ${[3, 0]}   | ${"x-axis"}
+        ${[0, 3]}   | ${"y-axis"}
+        ${[2, 4]}   | ${1}
+        ${[-2, 4]}  | ${2}
+        ${[-2, -4]} | ${3}
+        ${[2, -4]}  | ${4}
     `("returns $expected for coord $coord", ({coord, expected}) => {
         expect(getCoordQuadrant(coord)).toBe(expected);
     });
