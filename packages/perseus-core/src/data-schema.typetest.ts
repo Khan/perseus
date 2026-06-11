4dc3bb19bd01dae2aba73204e405dcc6e6393a5b
Ben Christel <benchristel@khanacademy.org>
2026-06-11 08:50:59 -0700

    [benc/input-number-v2-take-3] Make InputNumber types alias NumericInput types

--- packages/perseus-core/src/data-schema.typetest.ts
+++ packages/perseus-core/src/data-schema.typetest.ts
@@ -6,12 +6,12 @@ import type {
 } from "./data-schema";
 
 describe("PerseusInputNumberWidgetOptions", () => {
-    it("is assignable to PerseusNumericInputWidgetOptions", () => {
+    it("is identical to PerseusNumericInputWidgetOptions", () => {
         // This test is needed because the PerseusInputNumberWidgetOptions now
         // get passed to the NumericInput component. We are currently (May
         // 2026) removing the deprecated InputNumber code in favor of using
         // NumericInput everywhere, but we need to keep separate types for
         // InputNumber to support legacy content.
-        expect<PerseusInputNumberWidgetOptions>().type.toBeAssignableTo<PerseusNumericInputWidgetOptions>();
+        expect<PerseusInputNumberWidgetOptions>().type.toBe<PerseusNumericInputWidgetOptions>();
     });
 });
--- packages/perseus/src/widgets/input-number/input-number.tsx
+++ packages/perseus/src/widgets/input-number/input-number.tsx
@@ -1,24 +1,7 @@
-import {NumericInput} from "../numeric-input/numeric-input.class";
+import numericInput from "../numeric-input/numeric-input.class";
 
 import type {WidgetExports} from "../../types";
-import type {
-    PerseusInputNumberUserInput,
-    PerseusInputNumberWidgetOptions,
-} from "@khanacademy/perseus-core";
-
-function getOneCorrectAnswerFromRubric(
-    rubric: PerseusInputNumberWidgetOptions,
-): string | undefined {
-    const answer = rubric.answers[0];
-    if (answer.value == null) {
-        return;
-    }
-    let answerString = String(answer.value);
-    if (answer.maxError != null && answer.maxError > 0) {
-        answerString += " \u00B1 " + answer.maxError;
-    }
-    return answerString;
-}
+import type {PerseusInputNumberUserInput} from "@khanacademy/perseus-core";
 
 /**
  * @deprecated and likely a very broken API
@@ -32,16 +15,6 @@ function getUserInputFromSerializedState(
     };
 }
 
-function getStartUserInput(): PerseusInputNumberUserInput {
-    return {currentValue: ""};
-}
-
-function getCorrectUserInput(
-    options: PerseusInputNumberWidgetOptions,
-): PerseusInputNumberUserInput {
-    return {currentValue: String(options.answers[0].value)};
-}
-
 export default {
     name: "input-number",
     version: {major: 1, minor: 0},
@@ -49,10 +22,10 @@ export default {
     hidden: true,
     // NOTE(benchristel): We replaced the InputNumber component with
     // NumericInput in 2026.
-    widget: NumericInput,
+    widget: numericInput.widget,
     isLintable: true,
-    getOneCorrectAnswerFromRubric,
-    getStartUserInput,
-    getCorrectUserInput,
+    getOneCorrectAnswerFromRubric: numericInput.getOneCorrectAnswerFromRubric,
+    getStartUserInput: numericInput.getStartUserInput,
+    getCorrectUserInput: numericInput.getCorrectUserInput,
     getUserInputFromSerializedState,
-} satisfies WidgetExports<typeof NumericInput>;
+} satisfies WidgetExports<typeof numericInput.widget>;
