93e1dd7a60b1c116ef7c1b55b8e2c6c1584ecd79
Nisha Yerunkar <nisha@khanacademy.org>
2026-06-12 11:20:30 -0700

    [renderer-ff] [WIP] Add feature flag for renderer upgrade
    Adding a feature flag so we can start making updates to the markdown renderer.
    Also split up Perseus markdown rules and Simple markdown rules to make feature
    flag branching easier.
    
    Still remaining
    - Run the frontend script for new Growthbook flag
    - Do we want to split up the Pure markdown rules too?
    - Still need to add TODO comments about where to remove the feature flag later
    
    Issue: https://khanacademy.atlassian.net/browse/LEMS-4279
    
    Test plan:
    todo

--- packages/perseus-editor/src/testing/feature-flags-util.ts
+++ packages/perseus-editor/src/testing/feature-flags-util.ts
@@ -6,6 +6,7 @@ const DEFAULT_FEATURE_FLAGS = {
     "interactive-graph-vector": false,
     "interactive-graph-not-scored": false,
     "input-number-to-numeric-input": false,
+    "perseus-renderer-upgrade": false,
     // ...add new flags here
 };
 
