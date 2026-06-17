a2e57e31ba4bd2cda66bf840f60899c657b93d9f
Nisha Yerunkar <nisha@khanacademy.org>
2026-06-17 10:57:01 -0700

    [renderer-ff] forward refs

--- packages/perseus/src/renderer.tsx
+++ packages/perseus/src/renderer.tsx
@@ -6,13 +6,26 @@ import RendererOld from "./renderer.old";
 
 import type {Props as RendererProps} from "./renderer.old";
 
-const Renderer = (props: RendererProps) => {
+export {isDifferentQuestion} from "./renderer.old";
+
+export type {Props, DifferentQuestionPartialProps} from "./renderer.old";
+
+const Renderer = React.forwardRef(function Renderer(
+    props: RendererProps,
+    ref: React.ForwardedRef<
+        InstanceType<typeof RendererNew> | InstanceType<typeof RendererOld>
+    >,
+) {
     const rendererFF = isFeatureOn(
         {apiOptions: props.apiOptions},
         "perseus-renderer-upgrade",
     );
 
-    return rendererFF ? <RendererNew {...props} /> : <RendererOld {...props} />;
-};
+    return rendererFF ? (
+        <RendererNew ref={ref} {...props} />
+    ) : (
+        <RendererOld ref={ref} {...props} />
+    );
+});
 
 export default Renderer;
