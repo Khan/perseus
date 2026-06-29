1f21bc3e773cb1a3997fe4958158e407aeab8f6e
Jeremy Wiebe <jeremy@khanacademy.org>
2026-06-28 22:05:20 -0700

    [jer/number-line-fixes] Fix null picUrl passed to BlurInput in PlotterEditor
    picUrl is optional and nullable in the schema but was typed as required
    string in the editor props, hiding the gap. Align the type, guard
    fetchPic internally so callers don't need to check, and add a fallback
    empty string on the controlled input.

--- packages/perseus-editor/src/widgets/plotter-editor.tsx
+++ packages/perseus-editor/src/widgets/plotter-editor.tsx
@@ -51,7 +51,7 @@ type Props = {
     snapsPerLine: number;
     picSize: number;
     picBoxHeight: number;
-    picUrl: string;
+    picUrl?: string | null;
     plotDimensions: ReadonlyArray<number>;
     labelInterval: number;
     starting: ReadonlyArray<number>;
@@ -105,7 +105,10 @@ class PlotterEditor extends React.Component<Props, State> {
         }
     }
 
-    fetchPic: (arg1: string) => any = (url) => {
+    fetchPic: (url: string | null | undefined) => void = (url) => {
+        if (!url) {
+            return;
+        }
         if (this.state.loadedUrl !== url) {
             const pic = new Image();
             pic.src = url;
@@ -394,7 +397,7 @@ class PlotterEditor extends React.Component<Props, State> {
                             Picture:{" "}
                             <BlurInput
                                 className="pic-url"
-                                value={this.props.picUrl}
+                                value={this.props.picUrl ?? ""}
                                 onChange={this.changePicUrl}
                             />
                             <InfoTip>
