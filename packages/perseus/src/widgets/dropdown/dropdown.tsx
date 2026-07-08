109cbeb917b147688683ee4b8ea144a1bf072609
Ben Christel <benchristel@users.noreply.github.com>
2026-07-08 09:23:26 -0700

    Move Dropdown widget options to an `options` prop as a POC (#3869)
    ## Summary:
    Currently, widget options are spread into each widget component's props.
    This has been a source of confusion and annoyance in the past. The goal
    of this PR is to show how we might migrate to a design where each widget
    receives its options as a single `options` prop.
    
    Note: I plan to land this and subsequent PRs onto the
    `benc/widget-props-redesign` branch, not onto `main`. Once all the
    widgets are updated, I'll land `benc/widget-props-redesign`.
    
    Issue: LEMS-4354
    
    ## Test plan:
    
    - `pnpm storybook`
    - You should be able to add and edit a Dropdown widget in http://localhost:6006/?path=/docs/editors-editorpage--docs
    
    Author: benchristel
    
    Reviewers: benchristel, handeyeco, nishasy, Myranae
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3869

--- packages/perseus/src/widgets/dropdown/dropdown.tsx
+++ packages/perseus/src/widgets/dropdown/dropdown.tsx
@@ -19,7 +19,7 @@ import type {
     PerseusDependenciesV2,
     Widget,
     WidgetExports,
-    WidgetProps,
+    WidgetPropsV2,
 } from "../../types";
 import type {DropdownPromptJSON} from "../../widget-ai-utils/dropdown/dropdown-ai-utils";
 import type {
@@ -27,7 +27,7 @@ import type {
     PerseusDropdownWidgetOptions,
 } from "@khanacademy/perseus-core";
 
-type Props = WidgetProps<
+type Props = WidgetPropsV2<
     PerseusDropdownWidgetOptions,
     PerseusDropdownUserInput
 > & {
@@ -47,12 +47,14 @@ const Dropdown = forwardRef<WidgetHandle, Props>(function Dropdown(props, ref) {
     const {
         choices = [],
         placeholder = "",
+        visibleLabel,
+        ariaLabel,
+    } = props.options;
+    const {
         apiOptions = ApiOptions.defaults,
         userInput = {value: 0},
         static: isStatic = false,
         dependencies,
-        visibleLabel,
-        ariaLabel,
         widgetId,
         trackInteraction,
         handleUserInput,
@@ -126,9 +128,11 @@ const Dropdown = forwardRef<WidgetHandle, Props>(function Dropdown(props, ref) {
          * [LEMS-3185] do not trust serializedState
          */
         getSerializedState: (): any => {
-            const {userInput, choices, ...rest} = props;
+            const {userInput, options, ...otherProps} = props;
+            const {choices, ...otherOptions} = options;
             return {
-                ...rest,
+                ...otherOptions,
+                ...otherProps,
                 choices: choices.map((choice) => choice.content),
                 selected: userInput.value,
             };
@@ -181,7 +185,7 @@ const Dropdown = forwardRef<WidgetHandle, Props>(function Dropdown(props, ref) {
                 className="perseus-dropdown"
                 onChange={(value) => handleChange(parseInt(value))}
                 selectedValue={String(userInput.value)}
-                disabled={apiOptions.readOnly || isStatic}
+                disabled={Boolean(apiOptions.readOnly || isStatic)}
                 aria-label={ariaLabel || visibleLabel || strings.selectAnAnswer}
                 showOpenerLabelAsText={false}
             >
