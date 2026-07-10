f8b40e030c9dae6f0df926bf4f9c637823d8e1c6
Jeremy Wiebe <jeremy@khanacademy.org>
2026-07-10 14:38:51 -0700

    fixup! [claude/stupefied-gates-c80d2e] Replace PropTypes with TypeScript types in molecule-editor

--- packages/perseus-editor/src/widgets/molecule-editor.tsx
+++ packages/perseus-editor/src/widgets/molecule-editor.tsx
@@ -1,4 +1,4 @@
-import {components, Changeable, EditorJsonify} from "@khanacademy/perseus";
+import {components, EditorJsonify} from "@khanacademy/perseus";
 import * as React from "react";
 
 import type {PerseusMoleculeRendererWidgetOptions} from "@khanacademy/perseus-core";
@@ -14,18 +14,14 @@ type Props = {
 class MoleculeWidgetEditor extends React.Component<Props> {
     static widgetName = "molecule-renderer" as const;
 
-    change: (arg1: any, arg2: any, arg3: any) => any = (...args) => {
-        return Changeable.change.apply(this, args);
-    };
-
     updateMolecule: (arg1: string) => void = (newValue) => {
-        // @ts-expect-error - TS2554 - Expected 3 arguments, but got 1.
-        this.change({smiles: newValue});
+        this.props.onChange({smiles: newValue});
     };
 
     updateRotation: (newValue: number | null) => void = (newValue) => {
-        // @ts-expect-error - TS2554 - Expected 3 arguments, but got 1.
-        this.change({rotationAngle: newValue});
+        if (newValue != null) {
+            this.props.onChange({rotationAngle: newValue});
+        }
     };
 
     serialize: () => any = () => {
