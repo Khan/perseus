9afb3016317ab4063089fefba2185d05db22d7a2
Ben Christel <benchristel@khanacademy.org>
2026-07-15 14:45:23 -0700

    Rewrite ImageWidget as a functional component

--- packages/perseus/src/widgets/image/image.class.tsx
+++ packages/perseus/src/widgets/image/image.class.tsx
@@ -1,6 +1,5 @@
-import * as React from "react";
+import React, {forwardRef, useImperativeHandle} from "react";
 
-import {PerseusI18nContext} from "../../components/i18n-context";
 import {getPromptJSON as _getPromptJSON} from "../../widget-ai-utils/image/image-ai-utils";
 
 import {ImageComponent} from "./image";
@@ -11,22 +10,18 @@ import type {PerseusImageWidgetOptions} from "@khanacademy/perseus-core";
 
 export type ImageWidgetProps = WidgetProps<PerseusImageWidgetOptions>;
 
-class ImageWidget extends React.Component<ImageWidgetProps> implements Widget {
-    static contextType = PerseusI18nContext;
-    declare context: React.ContextType<typeof PerseusI18nContext>;
+// Widget interface methods exposed via ref
+type WidgetHandle = Pick<Widget, "getPromptJSON">;
 
-    // this just helps with TS weak typing when a Widget
-    // doesn't implement any Widget methods
-    isWidget = true as const;
+const ImageWidget = forwardRef<WidgetHandle, ImageWidgetProps>(
+    function ImageWidget(props, ref) {
+        useImperativeHandle(ref, () => ({
+            getPromptJSON: (): ImagePromptJSON => _getPromptJSON(props),
+        }));
 
-    getPromptJSON(): ImagePromptJSON {
-        return _getPromptJSON(this.props);
-    }
-
-    render(): React.ReactNode {
-        return <ImageComponent {...this.props} />;
-    }
-}
+        return <ImageComponent {...props} />;
+    },
+);
 
 export default {
     name: "image",
