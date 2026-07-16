81ba6dadd40400f565157fdb575bd1dda7961ad3
Jeremy Wiebe <jeremy@khanacademy.org>
2026-07-16 14:21:17 -0700

    [jer/preview-ng-17-preview-with-iframe] Remove component that isn't used yet

--- packages/perseus-editor/src/components/highlight-context.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export type HighlightContextValue = {
-    /** Activate highlight for an issue (null clears it). */
-    setIssueHighlight: (issueId: string, previewId: string | null) => void;
-};
-
-export const HighlightContext =
-    React.createContext<HighlightContextValue | null>(null);
