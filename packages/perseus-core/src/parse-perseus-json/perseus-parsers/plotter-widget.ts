3e2c4a0bce946bf169d336236c7e27e91e1a189e
Ben Christel <benchristel@khanacademy.org>
2026-07-21 13:52:52 -0700

    [benc/remove-plotter-default-props] Make Plotter options required

--- packages/perseus-core/src/parse-perseus-json/perseus-parsers/plotter-widget.ts
+++ packages/perseus-core/src/parse-perseus-json/perseus-parsers/plotter-widget.ts
@@ -4,7 +4,6 @@ import {
     string,
     array,
     number,
-    optional,
     nullable,
     enumeration,
 } from "../general-purpose-parsers";
@@ -27,20 +26,32 @@ export const parsePlotterWidget = parseWidget(
         categories: array(string),
         type: plotterPlotTypes,
         maxY: number,
-        // The default value for scaleY comes from plotter.tsx.
+        // The default value for scaleY comes from widgets/plotter/index.ts.
         // See parse-perseus-json/README.md for why we want to duplicate the
         // defaults here.
         scaleY: defaulted(number, () => 1),
-        labelInterval: optional(nullable(number)),
-        // The default value for snapsPerLine comes from plotter.tsx.
+        // The default value for labelInterval comes from widgets/plotter/index.ts.
+        // See parse-perseus-json/README.md for why we want to duplicate the
+        // defaults here.
+        labelInterval: defaulted(number, () => 1),
+        // The default value for snapsPerLine comes from widgets/plotter/index.ts.
         // See parse-perseus-json/README.md for why we want to duplicate the
         // defaults here.
         snapsPerLine: defaulted(number, () => 2),
         starting: array(number),
-        correct: defaulted(array(number), () => []),
-        picUrl: optional(nullable(string)),
-        picSize: optional(nullable(number)),
-        picBoxHeight: optional(nullable(number)),
+        // The default value for correct comes from widgets/plotter/index.ts.
+        // See parse-perseus-json/README.md for why we want to duplicate the
+        // defaults here.
+        correct: defaulted(array(number), () => [1]),
+        picUrl: defaulted(nullable(string), () => null),
+        // The default value for picSize comes from widgets/plotter/index.ts.
+        // See parse-perseus-json/README.md for why we want to duplicate the
+        // defaults here.
+        picSize: defaulted(number, () => 30),
+        // The default value for picBoxHeight comes from widgets/plotter/index.ts.
+        // See parse-perseus-json/README.md for why we want to duplicate the
+        // defaults here.
+        picBoxHeight: defaulted(number, () => 36),
         // NOTE(benchristel): I copied the default plotDimensions from
         // plotter.tsx. See the parse-perseus-json/README.md for an explanation
         // of why we want to duplicate the defaults here.
