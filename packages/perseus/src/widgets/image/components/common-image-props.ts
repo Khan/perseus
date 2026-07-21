6307ec5ef6021ce624e1016603c9d468ef3eef1a
Ben Christel <benchristel@users.noreply.github.com>
2026-07-21 10:19:00 -0700

    Preserve whole widget options object in Image subcomponents (#3916)
    Rather than spread the options into props (which causes types to be duplicated)
    we can just pass them around.
    
    Issue: LEMS-4354
    
    ## Test plan:
    
    CI checks should pass.
    
    Author: benchristel
    
    Reviewers: benchristel, Myranae, nishasy, handeyeco, ivyolamit, catandthemachines
    
    Required Reviewers:
    
    Approved By: Myranae
    
    Checks: ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3916

---
+++ packages/perseus/src/widgets/image/components/common-image-props.ts
@@ -0,0 +1,12 @@
+import type {APIOptions} from "../../../types";
+import type {PerseusImageWidgetOptions} from "@khanacademy/perseus-core";
+import type {LinterContextProps} from "@khanacademy/perseus-linter";
+
+export interface CommonImageProps {
+    options: PerseusImageWidgetOptions;
+    linterContext: LinterContextProps;
+    apiOptions: APIOptions;
+    widgetId: string;
+    isGifPlaying: boolean;
+    setIsGifPlaying: (isPlaying: boolean) => void;
+}
