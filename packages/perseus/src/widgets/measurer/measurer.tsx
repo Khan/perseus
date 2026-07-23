7faf00e024e8b1e04126be11140334c47b358bc5
Ben Christel <benchristel@users.noreply.github.com>
2026-07-23 14:38:21 -0700

    Migrate Measurer widget to WidgetPropsV2 (#3944)
    This continues the work started in https://github.com/Khan/perseus/pull/3869.
    
    Issue: LEMS-4354
    
    - `pnpm storybook`
    - You should be able to add and edit a Measurer widget in
      http://localhost:6006/?path=/docs/editors-editorpage--docs
    - Test with and without the `perseus-renderer-upgrade` feature flag on.
    
    Author: benchristel
    
    Reviewers: handeyeco, benchristel
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3944

--- packages/perseus/src/widgets/measurer/measurer.tsx
+++ packages/perseus/src/widgets/measurer/measurer.tsx
@@ -2,18 +2,17 @@ import {type PerseusMeasurerWidgetOptions} from "@khanacademy/perseus-core";
 import $ from "jquery";
 import * as React from "react";
 import ReactDOM from "react-dom";
-import _ from "underscore";
 
 import SvgImage from "../../components/svg-image";
 import GraphUtils from "../../util/graph-utils";
 import {getPromptJSON as _getPromptJSON} from "../../widget-ai-utils/measurer/measurer-ai-utils";
 
 import type {Coord} from "../../interactive2/types";
-import type {Widget, WidgetExports, WidgetProps} from "../../types";
+import type {Widget, WidgetExports, WidgetPropsV2} from "../../types";
 import type {Interval} from "../../util/interval";
 import type {UnsupportedWidgetPromptJSON} from "../../widget-ai-utils/unsupported-widget";
 
-type Props = WidgetProps<PerseusMeasurerWidgetOptions>;
+type Props = WidgetPropsV2<PerseusMeasurerWidgetOptions>;
 
 class Measurer extends React.Component<Props> implements Widget {
     // this just helps with TS weak typing when a Widget
@@ -27,21 +26,19 @@ class Measurer extends React.Component<Props> implements Widget {
         this.setupGraphie();
     }
 
-    componentDidUpdate(prevProps) {
-        const shouldSetupGraphie = _.any(
-            [
-                "box",
-                "showProtractor",
-                "showRuler",
-                "rulerLabel",
-                "rulerTicks",
-                "rulerPixels",
-                "rulerLength",
-            ],
-            (prop) => {
-                return prevProps[prop] !== this.props[prop];
-            },
-            this,
+    componentDidUpdate(prevProps: Props) {
+        const propsAffectingGraphie = [
+            "box",
+            "showProtractor",
+            "showRuler",
+            "rulerLabel",
+            "rulerTicks",
+            "rulerPixels",
+            "rulerLength",
+        ] satisfies Array<keyof PerseusMeasurerWidgetOptions>;
+
+        const shouldSetupGraphie = propsAffectingGraphie.some(
+            (prop) => prevProps.options[prop] !== this.props.options[prop],
         );
 
         if (shouldSetupGraphie) {
@@ -59,8 +56,8 @@ class Measurer extends React.Component<Props> implements Widget {
 
         const scale: Coord = [40, 40];
         const range: [Interval, Interval] = [
-            [0, this.props.box[0] / scale[0]],
-            [0, this.props.box[1] / scale[1]],
+            [0, this.props.options.box[0] / scale[0]],
+            [0, this.props.options.box[1] / scale[1]],
         ];
         graphie.init({
             range: range,
@@ -74,7 +71,7 @@ class Measurer extends React.Component<Props> implements Widget {
             this.protractor.remove();
         }
 
-        if (this.props.showProtractor) {
+        if (this.props.options.showProtractor) {
             // @ts-expect-error - Property 'protractor' does not exist on type 'Graphie'.
             this.protractor = graphie.protractor([7.5, 0.5]);
         }
@@ -83,17 +80,17 @@ class Measurer extends React.Component<Props> implements Widget {
             this.ruler.remove();
         }
 
-        if (this.props.showRuler) {
+        if (this.props.options.showRuler) {
             // @ts-expect-error - Property 'ruler' does not exist on type 'Graphie'.
             this.ruler = graphie.ruler({
                 center: [
                     (range[0][0] + range[0][1]) / 2,
                     (range[1][0] + range[1][1]) / 2,
                 ],
-                label: this.props.rulerLabel,
-                pixelsPerUnit: this.props.rulerPixels,
-                ticksPerUnit: this.props.rulerTicks,
-                units: this.props.rulerLength,
+                label: this.props.options.rulerLabel,
+                pixelsPerUnit: this.props.options.rulerPixels,
+                ticksPerUnit: this.props.options.rulerTicks,
+                units: this.props.options.rulerLength,
             });
         }
     }
@@ -103,7 +100,7 @@ class Measurer extends React.Component<Props> implements Widget {
     }
 
     render() {
-        const {image} = this.props;
+        const {image} = this.props.options;
 
         return (
             <div
@@ -111,7 +108,10 @@ class Measurer extends React.Component<Props> implements Widget {
                     "perseus-widget perseus-widget-measurer " +
                     "graphie-container blank-background"
                 }
-                style={{width: this.props.box[0], height: this.props.box[1]}}
+                style={{
+                    width: this.props.options.box[0],
+                    height: this.props.options.box[1],
+                }}
             >
                 {image.url && (
                     <div
