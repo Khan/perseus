7947f2fbefdf37aec838e956782cf71f591ef626
Ben Christel <benchristel@users.noreply.github.com>
2026-07-31 10:11:50 -0700

    Fix math colors in dark mode (#3968)
    ## Summary:
    Previously, MathJax colors weren't properly inverted in some contexts, e.g.
    Radio widget choices. This PR fixes that.
    
    Issue: LEMS-4412
    
    ## Test plan:
    
    - `pnpm storybook`
    - `open http://localhost:6006/?path=/docs/renderers-visual-regression-tests-dark-mode--docs`
    - The SVG details and labels of Graphie images should render in a light color.
    - MathJax in the Radio widget example should be legible.
    
    Author: benchristel
    
    Reviewers: mark-fitzgerald
    
    Required Reviewers:
    
    Approved By: mark-fitzgerald
    
    Checks: ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/3968

--- packages/perseus/src/__docs__/dark-mode.stories.tsx
+++ packages/perseus/src/__docs__/dark-mode.stories.tsx
@@ -1,10 +1,16 @@
-import {generateTestPerseusRenderer} from "@khanacademy/perseus-core";
+import {
+    generateRadioOptions,
+    generateRadioWidget,
+    generateTestPerseusRenderer,
+} from "@khanacademy/perseus-core";
 import {THEME_DATA_ATTRIBUTE} from "@khanacademy/wonder-blocks-theming";
 import * as React from "react";
 import {useEffect} from "react";
 
 import QuestionRendererForStories from "../widgets/__testutils__/question-renderer-for-stories";
 
+import type {PerseusRenderer} from "@khanacademy/perseus-core";
+import type {SupportedThemes} from "@khanacademy/wonder-blocks-theming";
 import type {Meta, StoryObj} from "@storybook/react-vite";
 
 type Story = StoryObj;
@@ -23,14 +29,16 @@ const meta: Meta = {
 };
 export default meta;
 
-const RenderImages = (content: string): (() => React.JSX.Element) => {
+function RenderInDarkMode(renderer: PerseusRenderer): () => React.JSX.Element {
     return function Render() {
-        // This is needed to apply a fictitious dark mode them to the body element.
+        // Apply the dark mode theme to the body element. The element with
+        // THEME_DATA_ATTRIBUTE needs to be outside the .framework-perseus
+        // element for our styles to work.
         useEffect(() => {
             setTimeout(() => {
                 document.body.setAttribute(
                     THEME_DATA_ATTRIBUTE,
-                    "some-theme-in-dark",
+                    "syl-dark" satisfies SupportedThemes,
                 );
             }, 10);
         }, []);
@@ -38,19 +46,23 @@ const RenderImages = (content: string): (() => React.JSX.Element) => {
         return (
             <div
                 style={{
-                    background: "black",
+                    color: "var(--wb-semanticColor-core-foreground-neutral-strong)",
+                    background:
+                        "var(--wb-semanticColor-core-background-instructive-subtle)",
                     display: "flex",
                     flexDirection: "column",
                     gap: "20px",
                     padding: "50px",
                 }}
             >
-                <QuestionRendererForStories
-                    question={generateTestPerseusRenderer({content: content})}
-                />
+                <QuestionRendererForStories question={renderer} />
             </div>
         );
     };
+}
+
+const RenderImages = (content: string): (() => React.JSX.Element) => {
+    return RenderInDarkMode(generateTestPerseusRenderer({content: content}));
 };
 
 export const Icons: Story = {
@@ -111,5 +123,26 @@ export const MathJax: Story = {
         MathJaxColors.map((color) => {
             return `$~\\${color}\\text{${color}}$`;
         }).join("\n\n"),
+    ),
+};
+
+export const RadioWithMathJax: Story = {
+    render: RenderInDarkMode(
+        generateTestPerseusRenderer({
+            content: "[[☃ radio 1]]",
+            widgets: {
+                "radio 1": generateRadioWidget({
+                    options: generateRadioOptions({
+                        choices: [
+                            {
+                                id: "1",
+                                content:
+                                    "$42 \\red{42} \\blue{42} \\green{42}$",
+                            },
+                        ],
+                    }),
+                }),
+            },
+        }),
     ),
 };
