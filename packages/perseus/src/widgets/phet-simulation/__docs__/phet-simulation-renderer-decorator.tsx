8b488a356a267ce59962777b66cafe7b288f3952
Tamara <tamara@khanacademy.org>
2026-08-06 10:29:52 -0500

    [tb/LEMS-4270/phet-color-sync] Add generator and associated tests

--- packages/perseus/src/widgets/phet-simulation/__docs__/phet-simulation-renderer-decorator.tsx
+++ packages/perseus/src/widgets/phet-simulation/__docs__/phet-simulation-renderer-decorator.tsx
@@ -1,26 +1,35 @@
-import {generateTestPerseusRenderer} from "@khanacademy/perseus-core";
+import {
+    generatePhetSimulationOptions,
+    generatePhetSimulationWidget,
+    generateTestPerseusRenderer,
+} from "@khanacademy/perseus-core";
 import * as React from "react";
 
 import QuestionRendererForStories from "../../__testutils__/question-renderer-for-stories";
 
+import type {APIOptions} from "../../../types";
 import type {PerseusPhetSimulationWidgetOptions} from "@khanacademy/perseus-core";
+import type {Decorator} from "@storybook/react-vite";
 
-export const phetSimulationRendererDecorator = (_, {args, parameters}) => {
-    const options = {
-        url: args.url,
-        description: args.description,
-    } satisfies PerseusPhetSimulationWidgetOptions;
-
+export const phetSimulationRendererDecorator: Decorator = (
+    _,
+    {
+        args,
+        parameters,
+    }: {
+        args: Partial<PerseusPhetSimulationWidgetOptions>;
+        parameters?: {apiOptions?: APIOptions; content?: string};
+    },
+) => {
     return (
         <QuestionRendererForStories
             apiOptions={parameters?.apiOptions}
             question={generateTestPerseusRenderer({
                 content: parameters?.content ?? "[[☃ phet-simulation 1]]",
                 widgets: {
-                    "phet-simulation 1": {
-                        type: "phet-simulation",
-                        options,
-                    },
+                    "phet-simulation 1": generatePhetSimulationWidget({
+                        options: generatePhetSimulationOptions({...args}),
+                    }),
                 },
             })}
         />
