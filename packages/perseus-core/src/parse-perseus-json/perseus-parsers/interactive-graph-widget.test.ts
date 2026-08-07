7986dd394167f238daaeaebba4ee65d4778fd88c
Ben Christel <benchristel@khanacademy.org>
2026-08-07 11:34:33 -0700

    [benc/interactive-graph-default-props] wip

--- packages/perseus-core/src/parse-perseus-json/perseus-parsers/interactive-graph-widget.test.ts
+++ packages/perseus-core/src/parse-perseus-json/perseus-parsers/interactive-graph-widget.test.ts
@@ -4,6 +4,8 @@ import {success} from "../result";
 
 import {
     parseInteractiveGraphWidget,
+    parseLabelLocation,
+    parseLockedFigure,
     parseLockedFunctionDomain,
 } from "./interactive-graph-widget";
 
@@ -51,255 +53,43 @@ describe("parseLockedFunctionDomain", () => {
 
 describe("parseInteractiveGraphWidget", () => {
     it("parses onAxis labelLocation correctly", () => {
-        const onAxisResult = parse(
-            {
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    labelLocation: "onAxis",
-                },
-            },
-            parseInteractiveGraphWidget,
-        );
+        const onAxisResult = parse("onAxis", parseLabelLocation);
 
-        expect(onAxisResult).toEqual(
-            success({
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    graph: {
-                        type: "linear",
-                    },
-                    labelLocation: "onAxis",
-                    lockedFigures: [],
-                },
-            }),
-        );
+        expect(onAxisResult).toEqual(success("onAxis"));
     });
 
     it("defaults text, color and size on a locked label when they are missing", () => {
         const result = parse(
             {
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    lockedFigures: [
-                        {
-                            type: "label",
-                            coord: [0, 0],
-                            text: undefined,
-                            color: undefined,
-                            size: undefined,
-                        },
-                    ],
-                },
+                type: "label",
+                coord: [0, 0],
+                text: undefined,
+                color: undefined,
+                size: undefined,
             },
-            parseInteractiveGraphWidget,
+            parseLockedFigure,
         );
         expect(result).toEqual(
             success({
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    graph: {
-                        type: "linear",
-                    },
-                    lockedFigures: [
-                        {
-                            type: "label",
-                            coord: [0, 0],
-                            text: "",
-                            color: "grayH",
-                            size: "medium",
-                        },
-                    ],
-                },
+                type: "label",
+                coord: [0, 0],
+                text: "",
+                color: "grayH",
+                size: "medium",
             }),
         );
     });
-    it("parses alongEdge labelLocation correctly", () => {
-        const alongEdgeResult = parse(
-            {
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    labelLocation: "alongEdge",
-                },
-            },
-            parseInteractiveGraphWidget,
-        );
 
-        expect(alongEdgeResult).toEqual(
-            success({
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    graph: {
-                        type: "linear",
-                    },
-                    labelLocation: "alongEdge",
-                    lockedFigures: [],
-                },
-            }),
-        );
+    it("parses alongEdge labelLocation correctly", () => {
+        const alongEdgeResult = parse("alongEdge", parseLabelLocation);
+
+        expect(alongEdgeResult).toEqual(success("alongEdge"));
     });
 
     it("parses empty strings as onAxis", () => {
-        const emptyLabelLocationResult = parse(
-            {
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    graph: {
-                        type: "linear",
-                    },
-                    labelLocation: "",
-                },
-            },
-            parseInteractiveGraphWidget,
-        );
+        const emptyLabelLocationResult = parse("", parseLabelLocation);
 
-        expect(emptyLabelLocationResult).toEqual(
-            success({
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    graph: {
-                        type: "linear",
-                    },
-                    labelLocation: "onAxis",
-                    lockedFigures: [],
-                },
-            }),
-        );
+        expect(emptyLabelLocationResult).toEqual(success("onAxis"));
     });
 
     it("rejects invalid labelLocation values", () => {
@@ -326,133 +116,30 @@ describe("parseInteractiveGraphWidget", () => {
         expect(invalidResult).toEqual(anyFailure);
     });
 
-    it("parses when labelLocation is not provided", () => {
-        const result = parse(
-            {
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    lockedFigures: [],
-                },
-            },
-            parseInteractiveGraphWidget,
-        );
+    it("defaults labelLocation to onAxis", () => {
+        const result = parse(undefined, parseLabelLocation);
 
-        expect(result).toEqual(
-            success({
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    graph: {
-                        type: "linear",
-                    },
-                    lockedFigures: [],
-                },
-            }),
-        );
+        expect(result).toEqual(success("onAxis"));
     });
 
     it("parses the deprecated 'orange' color on locked figures to 'gold'", () => {
         const result = parse(
             {
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    lockedFigures: [
-                        {
-                            type: "point",
-                            coord: [0, 0],
-                            color: "orange",
-                            filled: true,
-                        },
-                    ],
-                },
+                type: "point",
+                coord: [0, 0],
+                color: "orange",
+                filled: true,
             },
-            parseInteractiveGraphWidget,
+            parseLockedFigure,
         );
 
         expect(result).toEqual(
             success({
-                type: "interactive-graph",
-                options: {
-                    step: [1, 1],
-                    markings: "grid",
-                    showProtractor: false,
-                    range: [
-                        [-10, 10],
-                        [-10, 10],
-                    ],
-                    showAxisArrows: {
-                        xMin: true,
-                        xMax: true,
-                        yMin: true,
-                        yMax: true,
-                    },
-                    showAxisTicks: {x: true, y: true},
-                    correct: {
-                        type: "linear",
-                    },
-                    graph: {
-                        type: "linear",
-                    },
-                    lockedFigures: [
-                        {
-                            type: "point",
-                            coord: [0, 0],
-                            color: "gold",
-                            filled: true,
-                            labels: [],
-                        },
-                    ],
-                },
+                type: "point",
+                coord: [0, 0],
+                color: "gold",
+                filled: true,
+                labels: [],
             }),
         );
     });
