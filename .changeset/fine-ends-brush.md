fbf8ac265ff7b04c13243eb74a0c677bdff09393
Ben Christel <benchristel@users.noreply.github.com>
2026-08-11 13:51:50 -0700

    Migrate orderer widget to WidgetPropsV2 (#4044)
    This continues the work started in https://github.com/Khan/perseus/pull/3869.
    
    Issue: LEMS-4354
    
    - `pnpm storybook`
    - You should be able to add and edit an Orderer widget in
      http://localhost:6006/?path=/docs/editors-editorpage--docs
    - Test with and without the `perseus-renderer-upgrade` feature flag on.
    
    Author: benchristel
    
    Reviewers: handeyeco
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/4044

---
+++ .changeset/fine-ends-brush.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/perseus": patch
+---
+
+Internal: The widget options of Orderer are now grouped under a single `options` prop.
--- packages/perseus/src/widgets/orderer/orderer.tsx
+++ packages/perseus/src/widgets/orderer/orderer.tsx
@@ -13,16 +13,14 @@ import type {
     PerseusDependenciesV2,
     Widget,
     WidgetExports,
-    WidgetProps,
+    WidgetPropsV2,
 } from "../../types";
 import type {OrdererPromptJSON} from "../../widget-ai-utils/orderer/orderer-ai-utils";
 import type {
-    PerseusOrdererWidgetOptions,
     PerseusOrdererUserInput,
     OrdererPublicWidgetOptions,
 } from "@khanacademy/perseus-core";
 import type {LinterContextProps} from "@khanacademy/perseus-linter";
-import type {PropsFor} from "@khanacademy/wonder-blocks-core";
 
 type PlaceholderCardProps = {
     width: number | null | undefined;
@@ -287,7 +285,7 @@ class Card extends React.Component<CardProps, CardState> {
     }
 }
 
-type OrdererProps = WidgetProps<
+type OrdererProps = WidgetPropsV2<
     OrdererPublicWidgetOptions,
     PerseusOrdererUserInput
 > & {dependencies: PerseusDependenciesV2};
@@ -306,19 +304,6 @@ type OrdererState = {
     animateTo: Position | null | undefined;
     onAnimationEnd?: (arg1: any) => void;
 };
-
-// TODO(LEMS-4354): Remove these type assertions from all widgets.
-// eslint-disable-next-line no-restricted-syntax
-0 as any as WidgetProps<
-    PerseusOrdererWidgetOptions,
-    PerseusOrdererUserInput
-> satisfies PropsFor<typeof WrappedOrderer>;
-
-// eslint-disable-next-line no-restricted-syntax
-0 as any as WidgetProps<
-    OrdererPublicWidgetOptions,
-    PerseusOrdererUserInput
-> satisfies PropsFor<typeof WrappedOrderer>;
 
 class Orderer
     extends React.Component<OrdererProps, OrdererState>
@@ -369,7 +354,7 @@ class Orderer
             // @ts-expect-error - TS2322 - Type 'number' is not assignable to type 'null'.
             placeholderIndex = index;
         } else if (type === "bank") {
-            opt = this.props.options[index];
+            opt = this.props.options.options[index];
         }
 
         this.props.handleUserInput({current: list});
@@ -435,7 +420,7 @@ class Orderer
         if (inCardBank) {
             // If we're in the card bank, go through the options to find the
             // one with the same content
-            this.props.options.forEach((opt, i) => {
+            this.props.options.options.forEach((opt, i) => {
                 if (opt.content === this.state.dragContent) {
                     const card = ReactDOM.findDOMNode(this.refs["bank" + i]);
                     // @ts-expect-error - TS2769 - No overload matches this call. | TS2339 - Property 'position' does not exist on type 'JQueryStatic'.
@@ -491,7 +476,7 @@ class Orderer
 
     findCorrectIndex: (arg1: any, arg2: any) => any = (draggable, list) => {
         // Find the correct index for a card given the current cards.
-        const isHorizontal = this.props.layout === "horizontal";
+        const isHorizontal = this.props.options.layout === "horizontal";
         // @ts-expect-error - TS2769 - No overload matches this call.
         const $dragList = $(ReactDOM.findDOMNode(this.refs.dragList));
         // @ts-expect-error - TS2339 - Property 'offset' does not exist on type 'JQueryStatic'.
@@ -538,7 +523,7 @@ class Orderer
             return false;
         }
 
-        const isHorizontal = this.props.layout === "horizontal";
+        const isHorizontal = this.props.options.layout === "horizontal";
         // @ts-expect-error - TS2769 - No overload matches this call.
         const $draggable = $(ReactDOM.findDOMNode(draggable));
         // @ts-expect-error - TS2769 - No overload matches this call.
@@ -584,8 +569,9 @@ class Orderer
      * [LEMS-3185] do not trust serializedState
      */
     getSerializedState(): any {
-        const {userInput, ...rest} = this.props;
+        const {userInput, options, ...rest} = this.props;
         return {
+            ...options,
             ...rest,
             current: userInput.current.map((e) => ({content: e})),
         };
@@ -670,7 +656,7 @@ class Orderer
         // This is the bank of stacks of cards
         const bank = (
             <div ref="bank" className="bank perseus-clearfix">
-                {this.props.options.map((opt, i) => {
+                {this.props.options.options.map((opt, i) => {
                     return (
                         <Card
                             ref={"bank" + i}
@@ -697,10 +683,10 @@ class Orderer
                 className={
                     "draggy-boxy-thing orderer " +
                     "height-" +
-                    this.props.height +
+                    this.props.options.height +
                     " " +
                     "layout-" +
-                    this.props.layout +
+                    this.props.options.layout +
                     " " +
                     "blank-background " +
                     "perseus-clearfix "
