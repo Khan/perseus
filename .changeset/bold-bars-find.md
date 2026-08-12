b975d65735b39800a5d0719a9352228233183a06
Sarah Third <sarahthird@khanacademy.org>
2026-08-11 17:49:04 -0700

    docs(changeset): Fixing a forwardRef issue with the blank widget. (#4056)
    ## Summary:
    Fixing an issue that arose from pushing getPromptJSON to a later date, as it caused several cascading issues. The fix is merely stubbing out the useImperative command. 
    
    ## Test plan:
    - Tests pass 
    - A test passes upstream (can't run all the tests currently)
    
    Author: SonicScrewdriver
    
    Reviewers: ivyolamit
    
    Required Reviewers:
    
    Approved By: ivyolamit
    
    Checks: ✅ 11 checks were successful, ⏭️  2 checks have been skipped
    
    Pull Request URL: https://github.com/Khan/perseus/pull/4056

---
+++ .changeset/bold-bars-find.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/perseus": patch
+---
+
+Fixing a forwardRef issue with the blank widget.
--- packages/perseus/src/widgets/blank/blank.tsx
+++ packages/perseus/src/widgets/blank/blank.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {forwardRef} from "react";
+import {forwardRef, useImperativeHandle} from "react";
 
 import styles from "./blank-widget.module.css";
 
@@ -11,14 +11,20 @@ import type {
 
 type BlankProps = WidgetProps<PerseusBlankWidgetOptions, PerseusBlankUserInput>;
 
-const BlankWidget = forwardRef<Widget, BlankProps>(function BlankWidget(props) {
-    const classes = [styles.container]
-        .concat(props.displayType !== "normal" ? [styles["super-sub"]] : [])
-        .join(" ");
+const BlankWidget = forwardRef<Widget, BlankProps>(
+    function BlankWidget(props, ref) {
+        const classes = [styles.container]
+            .concat(props.displayType !== "normal" ? [styles["super-sub"]] : [])
+            .join(" ");
 
-    //TO-DO (LEMS-4448): Remove testid once we have a better way to identify a blank-widget
-    return <div className={classes} data-testid="blank-widget" />;
-});
+        // TODO(LEMS-4471): Write out the getPromptJSON function after checking
+        // with the TUT team about what data they need from this widget.
+        useImperativeHandle(ref, () => ({}));
+
+        //TO-DO (LEMS-4448): Remove testid once we have a better way to identify a blank-widget
+        return <div className={classes} data-testid="blank-widget" />;
+    },
+);
 export default {
     name: "blank",
     displayName: "Blank",
