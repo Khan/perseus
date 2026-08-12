976b3957d82c7e275231dde24589940bb5a307fa
Sarah Third <sarahthird@gmail.com>
2026-08-11 17:22:35 -0700

    [blank-ref] Fix issue

--- packages/perseus/src/widgets/blank/blank.tsx
+++ packages/perseus/src/widgets/blank/blank.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {forwardRef} from "react";
+import {forwardRef, useImperativeHandle} from "react";
 
 import styles from "./blank-widget.module.css";
 
@@ -11,14 +11,20 @@ import type {
 
 type BlankProps = WidgetProps<PerseusBlankWidgetOptions, PerseusBlankUserInput>;
 
-const BlankWidget = forwardRef<Widget, BlankProps>(function BlankWidget(props) {
-    const classes = [styles.container]
-        .concat(props.displayType !== "normal" ? [styles["super-sub"]] : [])
-        .join(" ");
+const BlankWidget = forwardRef<Widget, BlankProps>(
+    function BlankWidget(props, ref) {
+        const classes = [styles.container]
+            .concat(props.displayType !== "normal" ? [styles["super-sub"]] : [])
+            .join(" ");
 
-    //TO-DO (LEMS-4448): Remove testid once we have a better way to identify a blank-widget
-    return <div className={classes} data-testid="blank-widget" />;
-});
+        // This is stubbed for now in order to ensure ref var is used until
+        // we have the FITB Widget and add the getPromptJSON function here.
+        useImperativeHandle(ref, () => ({}));
+
+        //TO-DO (LEMS-4448): Remove testid once we have a better way to identify a blank-widget
+        return <div className={classes} data-testid="blank-widget" />;
+    },
+);
 export default {
     name: "blank",
     displayName: "Blank",
