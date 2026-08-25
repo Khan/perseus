b152d0347b78a883b757ef278dbcbd06fa76c0a6
Ben Christel <benchristel@khanacademy.org>
2026-08-25 11:44:23 -0700

    [benc/plotter-point-shadows] Hand-edit fixie's changes

--- .changeset/eleven-moons-shine.md
+++
@@ -1,5 +0,0 @@
----
-"@khanacademy/perseus": patch
----
-
-On mobile, the number line's point now loses its halo as soon as the learner makes the circle open, instead of keeping the halo until the widget is re-rendered from scratch.
--- packages/perseus/src/interactive2/wrapped-ellipse.ts
+++ packages/perseus/src/interactive2/wrapped-ellipse.ts
@@ -1,8 +1,6 @@
-import {vector as kvector} from "@khanacademy/kmath";
 import {semanticColor} from "@khanacademy/wonder-blocks-tokens";
 import _ from "underscore";
 
-import InteractiveUtil from "./interactive-util";
 import WrappedDrawing from "./wrapped-drawing";
 
 import type {Coord} from "./types";
@@ -21,7 +19,6 @@ class WrappedEllipse extends WrappedDrawing {
     initialPoint: Coord;
     wrapper: HTMLDivElement;
     visibleShape: VisibleShape;
-    private hasShadow: boolean = false;
 
     constructor(
         graphie: any,
@@ -72,35 +69,10 @@ class WrappedEllipse extends WrappedDrawing {
     }
 
     /**
-     * Turns the ellipse's drop shadow on or off. Can be called at any time.
+     * Turns the ellipse's drop shadow on or off.
      */
     setShadow(shadow: boolean) {
-        this.hasShadow = shadow;
         this.wrapper.style.filter = shadow ? wrappedEllipseShadow : "";
-    }
-
-    moveTo(point: Coord) {
-        if (!this.hasShadow) {
-            super.moveTo(point);
-            return;
-        }
-
-        // Round the translation to whole pixels; subpixel offsets make the
-        // drop shadow look blurry.
-        const delta = kvector.subtract(
-            this.graphie.scalePoint(point),
-            this.graphie.scalePoint(this.initialPoint),
-        );
-        const do3dTransform = InteractiveUtil.getCanUse3dTransform();
-        this.transform(
-            "translateX(" +
-                Math.round(delta[0]) +
-                "px) " +
-                "translateY(" +
-                Math.round(delta[1]) +
-                "px)" +
-                (do3dTransform ? " translateZ(0)" : ""),
-        );
     }
 }
 
