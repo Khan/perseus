ba21b03acdb60769f6a372cb46b9bca1abfc5cc0
Matthew Curtis <matthewcurtis@khanacademy.org>
2026-08-26 10:44:32 -0500

    [server-item-renderer-imperative-handle] restore eslint exception

--- packages/perseus/src/__tests__/test-utils.tsx
+++ packages/perseus/src/__tests__/test-utils.tsx
@@ -39,6 +39,7 @@ export const renderQuestion = (
             />
         </RenderStateRoot>,
     );
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (!renderer) {
         throw new Error(`Failed to render!`);
     }
