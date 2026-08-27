42820aabfff94ab38005ee2b3f90ee1463ed7b5f
Matthew Curtis <matthewcurtis@khanacademy.org>
2026-08-27 13:34:23 -0500

    [LEMS-4497/modern-sorter-editor] switch to WB inputs

--- packages/perseus-editor/src/widgets/sorter-editor/sorter-editor.tsx
+++ packages/perseus-editor/src/widgets/sorter-editor/sorter-editor.tsx
@@ -2,11 +2,13 @@ import {
     sorterLogic,
     type PerseusSorterWidgetOptions,
 } from "@khanacademy/perseus-core";
-import {Checkbox} from "@khanacademy/wonder-blocks-form";
+import {View} from "@khanacademy/wonder-blocks-core";
+import {Checkbox, TextField} from "@khanacademy/wonder-blocks-form";
+import {sizing} from "@khanacademy/wonder-blocks-tokens";
+import {StyleSheet} from "aphrodite";
 import * as React from "react";
 
 import InfoTip from "../../components/info-tip";
-import TextListEditor from "../../components/text-list-editor";
 
 const HORIZONTAL = "horizontal";
 const VERTICAL = "vertical";
@@ -18,15 +20,63 @@ type Props = PerseusSorterWidgetOptions & {
     ) => void;
 };
 
+type State = {
+    // The cards being edited, always with a trailing empty card that authors
+    // type into to add another. Kept in local state (rather than read straight
+    // off `props.correct`) so that a card can be cleared and retyped without
+    // its input disappearing mid-edit.
+    cards: string[];
+};
+
+type CardEditorProps = {
+    // The cards have no visible label, but screen reader users still need each
+    // input to have a distinguishable name, hence the aria-label.
+    ariaLabel: string;
+    value: string;
+    onChange: (value: string) => void;
+};
+
+/**
+ * An editor for a single sorter card.
+ */
+function CardEditor({ariaLabel, value, onChange}: CardEditorProps) {
+    return (
+        <TextField aria-label={ariaLabel} value={value} onChange={onChange} />
+    );
+}
+
 // JSDoc will be shown in Storybook widget editor description
 /**
  * An editor for adding a sorter widget that allows users to arrange items in a specific order.
  */
-class SorterEditor extends React.Component<Props> {
+class SorterEditor extends React.Component<Props, State> {
     static widgetName = "sorter" as const;
 
     static defaultProps: PerseusSorterWidgetOptions =
         sorterLogic.defaultWidgetOptions;
+
+    state: State = {cards: [...this.props.correct, ""]};
+
+    componentDidUpdate(prevProps: Props) {
+        if (prevProps.correct !== this.props.correct) {
+            this.setState({cards: [...this.props.correct, ""]});
+        }
+    }
+
+    onCardChange = (index: number, value: string) => {
+        const cards = [...this.state.cards];
+        cards[index] = value;
+
+        // Typing in the trailing card turns it into a real one, so grow the
+        // list to keep an empty card available for the next one.
+        if (index === cards.length - 1) {
+            cards.push("");
+        }
+
+        this.setState({cards});
+        // Empty cards are editing scaffolding, not answers.
+        this.props.onChange({correct: cards.filter(Boolean)});
+    };
 
     onLayoutChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const layout = e.target.value;
@@ -63,13 +113,23 @@ class SorterEditor extends React.Component<Props> {
                         </p>
                     </InfoTip>
                 </div>
-                <TextListEditor
-                    options={this.props.correct}
-                    onChange={(options) => {
-                        this.props.onChange({correct: options});
-                    }}
-                    layout={this.props.layout}
-                />
+                <View tag="ol" style={styles.cards}>
+                    {this.state.cards.map((card, i) => (
+                        <View tag="li" key={i}>
+                            <CardEditor
+                                ariaLabel={
+                                    i === this.state.cards.length - 1
+                                        ? "Add a card"
+                                        : `Card ${i + 1}`
+                                }
+                                value={card}
+                                onChange={(value) =>
+                                    this.onCardChange(i, value)
+                                }
+                            />
+                        </View>
+                    ))}
+                </View>
                 <div>
                     <label>
                         {" "}
@@ -108,5 +168,16 @@ class SorterEditor extends React.Component<Props> {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    // The cards always stack in the editor, regardless of the `layout` option,
+    // which only controls how the widget renders for students.
+    cards: {
+        listStyle: "none",
+        paddingInlineStart: 0,
+        marginBlock: sizing.size_080,
+        gap: sizing.size_120,
+    },
+});
 
 export default SorterEditor;
