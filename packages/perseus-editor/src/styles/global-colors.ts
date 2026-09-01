750b2e1f5a883b94ad0aa4f338a242318eaa6eef
Nisha Yerunkar <nisha@khanacademy.org>
2026-09-01 10:07:00 -0700

    [label-image-marker-listbox] remove dead code

--- packages/perseus-editor/src/styles/global-colors.ts
+++ packages/perseus-editor/src/styles/global-colors.ts
@@ -1,6 +1,3 @@
 // TODO: use WB colors
-export const gray95 = "#F0F1F2";
-export const gray76 = "#BABEC2";
 export const gray68 = "#888D93";
 export const gray41 = "#626569";
-export const gray17 = "#21242C";
