c28a0cccf4a1178bcc3aaddc7f1f3d4a713bf578
Nisha Yerunkar <nisha@khanacademy.org>
2026-09-03 16:32:32 -0700

    [more-editor-updates] functional component + wb tooltip

--- packages/perseus/src/components/lint.tsx
+++ packages/perseus/src/components/lint.tsx
@@ -1,10 +1,14 @@
+import {semanticColor, sizing} from "@khanacademy/wonder-blocks-tokens";
+import Tooltip, {TooltipContent} from "@khanacademy/wonder-blocks-tooltip";
+import {BodyText} from "@khanacademy/wonder-blocks-typography";
 import {StyleSheet, css} from "aphrodite";
 import * as React from "react";
-import ReactDOM from "react-dom";
 
 import * as constants from "../styles/constants";
 
 import InlineIcon from "./inline-icon";
+
+import type {CSSProperties} from "aphrodite";
 
 const exclamationIcon = {
     path: "M6 11a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm0-9a1 1 0 0 1 1 1v4a1 1 0 1 1-2 0V3a1 1 0 0 1 1-1z",
@@ -42,10 +46,6 @@ type Props = {
     severity?: Severity;
 };
 
-type State = {
-    tooltipAbove: boolean;
-};
-
 /**
  * This component renders "lint" nodes in a markdown parse tree. Lint nodes
  * are inserted into the tree by the Perseus linter (see
@@ -57,8 +57,8 @@ type State = {
  * is lint on (or near) that line.
  *
  * 2) The area around the circle is hoverable: when the mouse moves over it
- * the linty content is highlighted and a tooltip is displayed that explains
- * what the problem is.
+ * the linty content is highlighted and a Wonder Blocks tooltip is displayed
+ * that explains what the problem is.
  *
  * 3) The hoverable area is also an HTML <a> tag. Clicking on it opens
  * a new tab and links to additional details about the specific lint rule.
@@ -68,136 +68,111 @@ type State = {
  * that has a right margin (like anything blockquoted) the circle will appear
  * to the left of where it belongs.  And if there is more
  **/
-class Lint extends React.Component<Props, State> {
-    _positionTimeout: number | undefined;
-
-    state: State = {
-        tooltipAbove: true,
-    };
-
-    componentDidMount() {
-        // TODO(somewhatabstract): Use WB timing
-        this._positionTimeout = window.setTimeout(this.getPosition);
-    }
-
-    componentWillUnmount() {
-        // TODO(somewhatabstract): Use WB timing
-        window.clearTimeout(this._positionTimeout);
-    }
-
-    // We can't call setState in componentDidMount without risking a render
-    // thrash, and we can't call getBoundingClientRect in render, so we
-    // borrow a timeout approach from learnstorm-dashboard.jsx and set our
-    // state once the component has mounted and we can get what we need.
-    getPosition: () => void = () => {
-        // @ts-expect-error - TS2531 - Object is possibly 'null'. | TS2339 - Property 'getBoundingClientRect' does not exist on type 'Element | Text'.
-        const rect = ReactDOM.findDOMNode(this).getBoundingClientRect();
-        // NOTE(scottgrant): This is a magic number! We don't know the size
-        // of the tooltip at this point, so we're arbitrarily choosing a
-        // point at which to flip the tooltip's position.
-        this.setState({tooltipAbove: rect.top > 100});
-    };
-
-    // Render the <a> element that holds the indicator icon and the tooltip
-    // We pass different styles for the inline and block cases
-    renderLink: (arg1: any) => React.ReactElement = (style) => {
-        const tooltipAbove = this.state.tooltipAbove;
-
-        let severityStyle;
-        let warningText;
-        let warningTextStyle;
-        if (this.props.severity === Severity.Error) {
-            severityStyle = styles.indicatorError;
-            warningText = "Error";
-            warningTextStyle = styles.publishBlockingError;
-        } else if (this.props.severity === Severity.Warning) {
-            severityStyle = styles.indicatorWarning;
-            warningText = "Warning";
-            warningTextStyle = styles.warning;
+function Lint({
+    children,
+    inline,
+    message,
+    ruleName,
+    insideTable,
+    blockHighlight,
+    severity,
+}: Props): React.ReactElement {
+    // Render the <a> element that holds the indicator icon, wrapped in the
+    // tooltip that describes the lint. We pass different styles for the
+    // inline and block cases.
+    const renderLink = (style: CSSProperties): React.ReactElement => {
+        let indicatorSeverityStyle;
+        let severityLabel;
+        let severityLabelStyle;
+        if (severity === Severity.Error) {
+            indicatorSeverityStyle = styles.indicatorError;
+            severityLabel = "Error";
+            severityLabelStyle = styles.publishBlockingError;
+        } else if (severity === Severity.Warning) {
+            indicatorSeverityStyle = styles.indicatorWarning;
+            severityLabel = "Warning";
+            severityLabelStyle = styles.warning;
         } else {
-            severityStyle = styles.indicatorGuideline;
-            warningText = "Recommendation";
-            warningTextStyle = styles.warning;
+            indicatorSeverityStyle = styles.indicatorGuideline;
+            severityLabel = "Recommendation";
+            severityLabelStyle = styles.warning;
         }
 
         return (
-            <a
-                href={`https://khanacademy.org/r/linter-rules#${this.props.ruleName}`}
-                target="lint-help-window"
-                className={css(style)}
+            <Tooltip
+                // The anchor is an <a href>, which is already keyboard
+                // focusable, so the tooltip doesn't need to add a tabindex.
+                forceAnchorFocusivity={false}
+                variant="strong"
+                content={
+                    <TooltipContent
+                        title={
+                            <BodyText weight="bold" style={severityLabelStyle}>
+                                {severityLabel}
+                            </BodyText>
+                        }
+                    >
+                        {message.split("\n\n").map((paragraph, i) => (
+                            <BodyText key={i} style={styles.tooltipParagraph}>
+                                {paragraph}
+                            </BodyText>
+                        ))}
+                    </TooltipContent>
+                }
             >
-                <span className={css(styles.indicator, severityStyle)}>
-                    {this.props.severity === 1 && (
-                        <InlineIcon {...exclamationIcon} />
-                    )}
-                </span>
-                <div
-                    className={css(
-                        styles.tooltip,
-                        tooltipAbove && styles.tooltipAbove,
-                    )}
+                <a
+                    href={`https://khanacademy.org/r/linter-rules#${ruleName}`}
+                    target="lint-help-window"
+                    aria-label={`${severityLabel}: ${ruleName}`}
+                    className={css(style)}
                 >
-                    {this.props.message.split("\n\n").map((m, i) => (
-                        <p key={i} className={css(styles.tooltipParagraph)}>
-                            <span className={css(warningTextStyle)}>
-                                {warningText}:{" "}
-                            </span>
-                            {m}
-                        </p>
-                    ))}
-                    <div
+                    <span
                         className={css(
-                            styles.tail,
-                            tooltipAbove && styles.tailAbove,
+                            styles.indicator,
+                            indicatorSeverityStyle,
                         )}
-                    />
-                </div>
-            </a>
+                    >
+                        {severity === Severity.Error && (
+                            <InlineIcon {...exclamationIcon} />
+                        )}
+                    </span>
+                </a>
+            </Tooltip>
         );
     };
 
-    // The main render method surrounds linty content with a block or
-    // inline container and the link element that displays the indicator
-    // and holds the tooltip.
-    render(): React.ReactNode {
-        const {children, inline, blockHighlight, insideTable} = this.props;
-
-        if (insideTable) {
-            // If we're inside a table, then linty nodes just get
-            // a simple wrapper that allows them to be highlighted
-            if (inline) {
-                return <span data-lint-inside-table="true">{children}</span>;
-            }
-            return <div data-lint-inside-table="true">{children}</div>;
-        }
-        if (blockHighlight) {
-            return (
-                <span
-                    className={css(
-                        styles.lintContainer,
-                        styles.lintContainerBlock,
-                    )}
-                >
-                    {this.renderLink(styles.radioWidgetHoverTarget)}
-                    <span>{children}</span>
-                </span>
-            );
-        }
+    if (insideTable) {
+        // If we're inside a table, then linty nodes just get
+        // a simple wrapper that allows them to be highlighted
         if (inline) {
-            return (
-                <span className={css(styles.lintContainer)}>
-                    {this.renderLink(styles.inlineHoverTarget)}
-                    <span>{children}</span>
-                </span>
-            );
+            return <span data-lint-inside-table="true">{children}</span>;
         }
+        return <div data-lint-inside-table="true">{children}</div>;
+    }
+    if (blockHighlight) {
         return (
-            <div className={css(styles.lintContainer)}>
-                {this.renderLink(styles.hoverTarget)}
-                <div>{children}</div>
-            </div>
+            <span
+                className={css(styles.lintContainer, styles.lintContainerBlock)}
+            >
+                {renderLink(styles.radioWidgetHoverTarget)}
+                <span>{children}</span>
+            </span>
         );
     }
+    if (inline) {
+        return (
+            <span className={css(styles.lintContainer)}>
+                {renderLink(styles.inlineHoverTarget)}
+                <span>{children}</span>
+            </span>
+        );
+    }
+    return (
+        <div className={css(styles.lintContainer)}>
+            {renderLink(styles.hoverTarget)}
+            <div>{children}</div>
+        </div>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -240,12 +215,6 @@ const styles = StyleSheet.create({
         // This style changes its color on hover
         ":hover > span": {
             backgroundColor: constants.warningColorHover,
-        },
-
-        // The tooltip is in a div element inside the hover target.
-        // This style displays it on hover
-        ":hover div": {
-            display: "block",
         },
 
         // The linty content is in a <div> sibling that follows the
@@ -298,12 +267,6 @@ const styles = StyleSheet.create({
             backgroundColor: constants.warningColorHover,
         },
 
-        // The tooltip is in a div element inside the hover target.
-        // This style displays it on hover. This is the same as the block case.
-        ":hover div": {
-            display: "block",
-        },
-
         // The linty content is in a <span> sibling that follows the
         // hover target. This style highlights it on hover. In this case
         // we can just set the foreground and background color to really
@@ -327,30 +290,11 @@ const styles = StyleSheet.create({
         width: 24,
         height: 24,
 
-        // By specifying a fixed minimum width, the tooltip will hover in a
-        // readable position above and to the right.
-        minInlineSize: 264,
-
         // The indicator is in a span inside the hover target.
         // This style changes its color on hover.
         // This is the same as the block case.
         ":hover > span": {
             backgroundColor: constants.warningColorHover,
-        },
-
-        // The tooltip is in a div element inside the hover target.
-        // This style displays it on hover. This is the same as the block case.
-        // We specify a fixed-width because of some parent styling.
-        ":hover > div": {
-            display: "block",
-            padding: 8,
-            width: 280,
-        },
-
-        // Move the tooltip tail to an appropriate position relative to the
-        // tooltip.
-        ":hover > div > div": {
-            insetInlineStart: 8,
         },
 
         // The linty content is in a <span> sibling that follows the
@@ -376,85 +320,34 @@ const styles = StyleSheet.create({
         width: 8,
     },
     indicatorError: {
-        backgroundColor: "#be2612",
+        backgroundColor: semanticColor.core.foreground.critical.default,
         borderRadius: 8,
         height: 16,
         width: 16,
     },
     indicatorWarning: {
-        backgroundColor: "#f86700",
+        backgroundColor: semanticColor.core.foreground.warning.strong,
     },
     indicatorGuideline: {
-        backgroundColor: "#ffbe26",
+        backgroundColor: semanticColor.core.foreground.warning.default,
     },
 
-    // These are the styles for the tooltip
-    tooltip: {
-        // Absolute positioning relative to the lint indicator circle.
-        position: "absolute",
-        insetInlineEnd: -12,
-
-        // The tooltip is hidden by default; only displayed on hover
-        display: "none",
-
-        // When it is displayed, it goes on top!
-        zIndex: 1000,
-
-        // These styles control what the tooltip looks like
-        color: constants.white,
-        backgroundColor: constants.gray17,
-        opacity: 0.9,
-        fontFamily: constants.baseFontFamily,
-        fontSize: "12px",
-        lineHeight: "15px",
-        width: "320px",
-        borderRadius: "4px",
-    },
-    // If we're going to render the tooltip above the warning circle, we use
-    // the previous rules in tooltip, but change the position slightly.
-    tooltipAbove: {
-        insetBlockEnd: 32,
-    },
-
-    // We give the tooltip a little triangular "tail" that points down at
-    // the lint indicator circle. This is inside the tooltip and positioned
-    // relative to it. It also shares the opacity of the tooltip. We're using
-    // the standard CSS trick for drawing triangles with a thick border.
-    tail: {
-        position: "absolute",
-        insetBlockStart: -12,
-        insetInlineEnd: 16,
-        width: 0,
-        height: 0,
-
-        // This is the CSS triangle trick
-        borderInlineStart: "8px solid transparent",
-        borderInlineEnd: "8px solid transparent",
-        borderBlockEnd: "12px solid " + constants.gray17,
-    },
-    tailAbove: {
-        insetBlockEnd: -12,
-        borderBlockEnd: "none",
-        borderBlockStart: "12px solid " + constants.gray17,
-        insetBlockStart: "auto",
-    },
-
-    // Each warning in the tooltip is its own <p>. They are 12 pixels from
-    // the edges of the tooltip and 12 pixels from each other.
+    // A lint message can contain several paragraphs. Wonder Blocks resets the
+    // margins on BodyText, so we space them out ourselves.
     tooltipParagraph: {
-        margin: 12,
+        ":not(:first-child)": {
+            marginBlockStart: sizing.size_080,
+        },
     },
 
-    // The text "Warning" inside the tooltip is highlighted like this
+    // The text "Warning" or "Recommendation" in the tooltip title
     warning: {
-        color: constants.warningColor,
-        fontFamily: constants.boldFontFamily,
+        color: semanticColor.status.warning.foreground,
     },
 
-    // The text "Publish-blocking error" instide the tooltip is highlighted
-    // like this
+    // The text "Error" in the tooltip title, for publish-blocking lint
     publishBlockingError: {
-        color: constants.publishBlockingErrorColor,
+        color: semanticColor.status.critical.foreground,
     },
 });
 
