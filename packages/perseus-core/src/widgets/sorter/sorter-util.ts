dc123845614fae331d8517a524212fc1dbbe110f
Anakaren <anakaren@khanacademy.org>
2026-09-10 13:49:27 -0700

    centralize max cards in perseus core - sorter utils (#4204)
    ## Summary:
    <Add your summary here>
    
    Issue: LEMS-XXXX
    
    ## Test plan:
    
    Author: anakaren-rojas
    
    Reviewers: handeyeco
    
    Required Reviewers:
    
    Approved By: handeyeco
    
    Checks: ⏭️  1 check has been skipped, ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/4204

--- packages/perseus-core/src/widgets/sorter/sorter-util.ts
+++ packages/perseus-core/src/widgets/sorter/sorter-util.ts
@@ -2,6 +2,8 @@ import {constrainedShuffle, type RNG, seededRNG} from "../../utils/random-util";
 
 import type {PerseusSorterWidgetOptions} from "../../data-schema";
 
+export const SORTER_MAX_CARDS = 10;
+
 /**
  * For details on the individual options, see the
  * PerseusSorterWidgetOptions type
--- packages/perseus-editor/src/widgets/sorter-editor/sorter-editor.tsx
+++ packages/perseus-editor/src/widgets/sorter-editor/sorter-editor.tsx
@@ -1,5 +1,6 @@
 import {
     sorterLogic,
+    SORTER_MAX_CARDS,
     type PerseusSorterWidgetOptions,
 } from "@khanacademy/perseus-core";
 import Button from "@khanacademy/wonder-blocks-button";
@@ -14,10 +15,6 @@ import {TypedSingleSelect} from "../../components/typed-single-select";
 
 import CardEditor from "./card-editor";
 import styles from "./sorter-editor.module.css";
-
-// Ideally Content Creators would keep it <=7
-// but 10 is our hard limit
-const maxCards = 10;
 
 // There's nothing to sort with fewer than two cards.
 const minCards = 2;
@@ -96,7 +93,7 @@ const SorterEditor = React.forwardRef<SorterEditorHandle, Props>(
         };
 
         const onAddCard = () => {
-            if (correct.length >= maxCards) {
+            if (correct.length >= SORTER_MAX_CARDS) {
                 return;
             }
             onChange({correct: [...correct, ""]});
@@ -156,7 +153,7 @@ const SorterEditor = React.forwardRef<SorterEditorHandle, Props>(
                         kind="tertiary"
                         startIcon={plusCircle}
                         className={styles.addCard}
-                        disabled={correct.length >= maxCards}
+                        disabled={correct.length >= SORTER_MAX_CARDS}
                         onClick={onAddCard}
                     >
                         Add a card
