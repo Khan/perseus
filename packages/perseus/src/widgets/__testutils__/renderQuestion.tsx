56e2f4cebb0fe57e8629343a70a155e880740051
Jeremy Wiebe <jeremy@khanacademy.org>
2026-09-14 10:30:26 -0700

    [jer/render-question-params] Hand-edit fixie's changes

--- packages/perseus/src/widgets/__testutils__/renderQuestion.tsx
+++ packages/perseus/src/widgets/__testutils__/renderQuestion.tsx
@@ -26,7 +26,7 @@ type RenderResult = ReturnType<typeof render>;
 
 type ExtraProps = Omit<PropsFor<typeof Perseus.Renderer>, "strings">;
 
-type RenderQuestionOptions = APIOptions & {
+type RenderQuestionOptions = {
     apiOptions?: APIOptions;
     extraProps?: ExtraProps;
     initialUserInput?: UserInputMap;
@@ -44,19 +44,12 @@ export const renderQuestion = (
     unmount: RenderResult["unmount"];
 } => {
     const {
+        apiOptions = Object.freeze({}),
         extraProps,
         initialUserInput,
         dependencies = testDependenciesV2,
         locale = "en",
     } = options;
-    const apiOptions =
-        options.apiOptions ??
-        ("extraProps" in options ||
-        "initialUserInput" in options ||
-        "dependencies" in options ||
-        "locale" in options
-            ? Object.freeze({})
-            : options);
 
     setDependencies(testDependencies);
     registerAllWidgetsForTesting();
