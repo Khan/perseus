360c4953f4ee7df8292b91b2209807425350290e
Ben Christel <benchristel@khanacademy.org>
2026-09-17 07:29:57 -0700

    [benc/dropdown-editor-onChange-types] Remove DeprecatedStandinEditor props type

--- packages/perseus-editor/src/widgets/deprecated-standin-editor/deprecated-standin-editor.tsx
+++ packages/perseus-editor/src/widgets/deprecated-standin-editor/deprecated-standin-editor.tsx
@@ -2,12 +2,7 @@ import * as React from "react";
 
 import EditorJsonify from "../../mixins/editor-jsonify";
 
-type Props = {
-    /** DeprecatedStandinEditor never calls onChange */
-    onChange: (options: never) => void;
-};
-
-class DeprecatedStandinEditor extends React.Component<Props> {
+class DeprecatedStandinEditor extends React.Component<{}> {
     serialize(): any {
         return EditorJsonify.serialize.call(this);
     }
