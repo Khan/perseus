c64d247da9068c4a000f97a042e6a9f341c72a9d
Anakaren Rojas <anakaren@khanacademy.org>
2026-09-21 15:31:52 -0700

    [gg-explain-a11y] publish snapshot change

--- utils/publish-snapshot.sh
+++ utils/publish-snapshot.sh
@@ -113,7 +113,9 @@ pnpm changeset version --snapshot "$PR_NUMBER"
 # provenance information in the package.json file and surface
 # it in the npm registry.
 # See: https://docs.npmjs.com/generating-provenance-statements
+set +o pipefall
 env NPM_CONFIG_PROVENANCE=true pnpm changeset publish --no-git-tag --tag "${PR_NUMBER}"
+find /home/runner/.npm/_logs/ | xargs -n 1 cat
 
 # Now we export the npm tag name so that later Github Action steps have access
 # to this value in the form of:
