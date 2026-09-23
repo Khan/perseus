ba9b3151c8d1b0ba635a59ce2c2c6c70b6673c1e
Matthew <matthewcurtis@khanacademy.org>
2026-09-23 11:08:23 -0500

    Switch from state to ref for keypad context (#4133)
    ## Summary:
    When I tried to ship https://github.com/Khan/perseus/pull/4126, I ended up with an infinite rerender loop because class component identity is stable and functional component identity is not. Switching to component meant that setRenderer kept getting called which kept rerendering, which created a new component instance, which triggered a setRenderer...etc.
    
    This PR fixes some of those issues:
    - Use `useRef` (which doesn't cause rerenders) rather than `useState` (which does)
    - Abstract away direct `Renderer` access away behind `blurRenderer`
    
    Author: handeyeco
    
    Reviewers: handeyeco, mark-fitzgerald, jeremywiebe
    
    Required Reviewers:
    
    Approved By: mark-fitzgerald
    
    Checks: ⏭️  1 check has been skipped, ✅ 10 checks were successful
    
    Pull Request URL: https://github.com/Khan/perseus/pull/4133

--- packages/keypad-context/src/keypad-context.tsx
+++ packages/keypad-context/src/keypad-context.tsx
@@ -8,20 +8,19 @@
  * - Perseus Renderers (Server/Item/Article)
  */
 import * as React from "react";
-import {useState, useMemo} from "react";
+import {useCallback, useRef, useState, useMemo} from "react";
 
 import type {KeypadContextType} from "./types";
 import type {KeypadContextRendererInterface} from "@khanacademy/perseus-core";
 
-// @ts-expect-error - TS2322 - Type 'Context<{ setKeypadElement: (keypadElement: HTMLElement | null | undefined) => void; keypadElement: null; setRenderer: (renderer: RendererInterface | null | undefined) => void; renderer: null; setScrollableElement: (scrollableElement: HTMLElement | ... 1 more ... | undefined) => void; scrollableElement: null; }>' is not assignable to type 'Context<KeypadContext>'.
 export const KeypadContext: React.Context<KeypadContextType> =
-    React.createContext({
+    React.createContext<KeypadContextType>({
         setKeypadActive: (keypadActive) => {},
         keypadActive: false,
         setKeypadElement: (keypadElement) => {},
         keypadElement: null,
         setRenderer: (renderer) => {},
-        renderer: null,
+        blurRenderer: () => {},
         setScrollableElement: (scrollableElement) => {},
         scrollableElement: null,
     });
@@ -33,32 +32,44 @@ export function StatefulKeypadContextProvider(props: Props) {
     const [keypadActive, setKeypadActive] = useState<boolean>(false);
     // used to communicate between the keypad and the Renderer
     const [keypadElement, setKeypadElement] = useState<any>();
-    // this is a KeypadContextRendererInterface from Perseus
-    const [renderer, setRenderer] =
-        useState<KeypadContextRendererInterface | null>();
     const [scrollableElement, setScrollableElement] =
         useState<HTMLElement | null>();
 
+    const rendererRef = useRef<KeypadContextRendererInterface | null>(null);
+
+    // useCallback is to keep useMemo stable
+    const setRenderer = useCallback<KeypadContextType["setRenderer"]>(
+        (renderer) => {
+            rendererRef.current = renderer ?? null;
+        },
+        [],
+    );
+
+    // useCallback is to keep useMemo stable
+    const blurRenderer = useCallback(() => {
+        rendererRef.current?.blur();
+    }, []);
+
+    // the context is wrapped around most of Khan Academy frontend code
+    // without memoization, we were rerendering the entire application
+    // which affected redirects negatively.
     const memoizedValue = useMemo(
         () => ({
             keypadActive,
             setKeypadActive,
             keypadElement,
             setKeypadElement,
-            renderer,
             setRenderer,
+            blurRenderer,
             scrollableElement,
             setScrollableElement,
         }),
         [
             keypadActive,
-            setKeypadActive,
             keypadElement,
-            setKeypadElement,
-            renderer,
             setRenderer,
+            blurRenderer,
             scrollableElement,
-            setScrollableElement,
         ],
     );
 
